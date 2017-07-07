d365e254a82ef597eddb9049dd3a1b3f556bc129
terrymanu <terrymanu@163.com>
2017-07-07 18:42:23 +0800

    refactor merger into ShardingResultSet 5th version, add AbstractDecoratorResultSetMerger

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/merger/core/decorator/LimitDecoratorResultSetMerger.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/merger/core/decorator/LimitDecoratorResultSetMerger.java
@@ -19,21 +19,15 @@ package com.dangdang.ddframe.rdb.sharding.merger.core.decorator;
 
 import com.dangdang.ddframe.rdb.sharding.merger.core.ResultSetMerger;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.limit.Limit;
-import lombok.RequiredArgsConstructor;
 
-import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Calendar;
 
 /**
  * 分页结果集归并.
  *
  * @author zhangliang
  */
-@RequiredArgsConstructor
-public final class LimitDecoratorResultSetMerger implements ResultSetMerger {
-    
-    private final ResultSetMerger resultSetMerger;
+public final class LimitDecoratorResultSetMerger extends AbstractDecoratorResultSetMerger {
     
     private final Limit limit;
     
@@ -42,14 +36,14 @@ public final class LimitDecoratorResultSetMerger implements ResultSetMerger {
     private int rowNumber;
     
     public LimitDecoratorResultSetMerger(final ResultSetMerger resultSetMerger, final Limit limit) throws SQLException {
-        this.resultSetMerger = resultSetMerger;
+        super(resultSetMerger);
         this.limit = limit;
         skipAll = skipOffset();
     }
     
     private boolean skipOffset() throws SQLException {
         for (int i = 0; i < limit.getOffsetValue(); i++) {
-            if (!resultSetMerger.next()) {
+            if (!getResultSetMerger().next()) {
                 return true;
             }
         }
@@ -62,38 +56,8 @@ public final class LimitDecoratorResultSetMerger implements ResultSetMerger {
             return false;
         }
         if (limit.getRowCountValue() > 0) {
-            return ++rowNumber <= limit.getRowCountValue() && resultSetMerger.next();
+            return ++rowNumber <= limit.getRowCountValue() && getResultSetMerger().next();
         }
-        return resultSetMerger.next();
-    }
-    
-    @Override
-    public Object getValue(final int columnIndex, final Class<?> type) throws SQLException {
-        return resultSetMerger.getValue(columnIndex, type);
-    }
-    
-    @Override
-    public Object getValue(final String columnLabel, final Class<?> type) throws SQLException {
-        return resultSetMerger.getValue(columnLabel, type);
-    }
-    
-    @Override
-    public Object getCalendarValue(final int columnIndex, final Class<?> type, final Calendar calendar) throws SQLException {
-        return resultSetMerger.getCalendarValue(columnIndex, type, calendar);
-    }
-    
-    @Override
-    public Object getCalendarValue(final String columnLabel, final Class<?> type, final Calendar calendar) throws SQLException {
-        return resultSetMerger.getCalendarValue(columnLabel, type, calendar);
-    }
-    
-    @Override
-    public InputStream getInputStream(final int columnIndex, final String type) throws SQLException {
-        return resultSetMerger.getInputStream(columnIndex, type);
-    }
-    
-    @Override
-    public InputStream getInputStream(final String columnLabel, final String type) throws SQLException {
-        return resultSetMerger.getInputStream(columnLabel, type);
+        return getResultSetMerger().next();
     }
 }
