ddf6b8f494e91f67c37f30413cd40ca83c3cf765
terrymanu <terrymanu@163.com>
2017-07-27 13:01:22 +0800

    for checkstyle

--- sharding-jdbc-plugin/src/main/java/com/dangdang/ddframe/rdb/sharding/plugin/keygen/IPSectionKeyGenerator.java
+++ sharding-jdbc-plugin/src/main/java/com/dangdang/ddframe/rdb/sharding/plugin/keygen/IPSectionKeyGenerator.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
 package com.dangdang.ddframe.rdb.sharding.plugin.keygen;
 
 import com.dangdang.ddframe.rdb.sharding.keygen.DefaultKeyGenerator;
@@ -7,7 +24,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
- * 浏览 {@link IPKeyGenerator} workerId生成的规则后，感觉对服务器IP后10位（特别是IPV6）数值比较约束。
+ * 浏览 {@link IPKeyGenerator} workerId生成的规则后，感觉对服务器IP后10位（特别是IPV6）数值比较约束.
+ * 
+ * <p>
  * 有以下优化思路：
  * 因为workerId最大限制是2^10，我们生成的workerId只要满足小于最大workerId即可。
  * 1.针对IPV4:
@@ -16,20 +35,19 @@ import java.net.UnknownHostException;
  * 2.针对IPV6:
  * ....IP最大ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff
  * ....为了保证相加生成出的workerId < 1024,思路是将每个bit位的后6位相加。这样在一定程度上也可以满足workerId不重复的问题。
- * <p/>
- * <p/>
+ * </p>
  * 使用这种IP生成workerId的方法,必须保证IP段相加不能重复
  *
  * @author DogFc
  */
-public class IPSectionKeyGenerator implements KeyGenerator {
-
+public final class IPSectionKeyGenerator implements KeyGenerator {
+    
     private final DefaultKeyGenerator defaultKeyGenerator = new DefaultKeyGenerator();
-
+    
     static {
         initWorkerId();
     }
-
+    
     static void initWorkerId() {
         InetAddress address;
         try {
@@ -42,19 +60,19 @@ public class IPSectionKeyGenerator implements KeyGenerator {
         //IPV4
         if (ipAddressByteArray.length == 4) {
             for (byte byteNum : ipAddressByteArray) {
-                workerId += (byteNum & 0xFF);
+                workerId += byteNum & 0xFF;
             }
             //IPV6
         } else if (ipAddressByteArray.length == 16) {
             for (byte byteNum : ipAddressByteArray) {
-                workerId += (byteNum & 0B111111);
+                workerId += byteNum & 0B111111;
             }
         } else {
             throw new IllegalStateException("Bad LocalHost InetAddress, please check your network!");
         }
         DefaultKeyGenerator.setWorkerId(workerId);
     }
-
+    
     @Override
     public Number generateKey() {
         return defaultKeyGenerator.generateKey();
