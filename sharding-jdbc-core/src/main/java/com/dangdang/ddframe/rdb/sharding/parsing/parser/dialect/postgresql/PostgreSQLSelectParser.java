808679519068cf84590fd40d74925b6c31827ef5
terrymanu <terrymanu@163.com>
2017-08-22 15:14:47 +0800

    refactor PostgreSQL limit

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/dialect/postgresql/PostgreSQLSelectParser.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/dialect/postgresql/PostgreSQLSelectParser.java
@@ -22,11 +22,6 @@ import com.dangdang.ddframe.rdb.sharding.parsing.lexer.LexerEngine;
 import com.dangdang.ddframe.rdb.sharding.parsing.lexer.dialect.postgresql.PostgreSQLKeyword;
 import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword;
 import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Keyword;
-import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Literals;
-import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.Symbol;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.limit.Limit;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.context.limit.LimitValue;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.exception.SQLParsingException;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.exception.SQLParsingUnsupportedException;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.sql.AbstractOrderBySQLParser;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.sql.DistinctSQLParser;
@@ -36,10 +31,6 @@ import com.dangdang.ddframe.rdb.sharding.parsing.parser.sql.SelectListSQLParser;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.sql.WhereSQLParser;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dql.select.AbstractSelectParser;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dql.select.SelectStatement;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.token.OffsetToken;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.token.RowCountToken;
-import com.dangdang.ddframe.rdb.sharding.util.NumberUtil;
-import com.google.common.base.Optional;
 
 /**
  * PostgreSQL Select语句解析器.
@@ -58,6 +49,8 @@ public final class PostgreSQLSelectParser extends AbstractSelectParser {
     
     private final AbstractOrderBySQLParser orderBySQLParser;
     
+    private  final PostgreSQLLimitSQLParser limitSQLParser;
+    
     public PostgreSQLSelectParser(final ShardingRule shardingRule, final LexerEngine lexerEngine) {
         super(shardingRule, lexerEngine, new WhereSQLParser(lexerEngine));
         distinctSQLParser = new DistinctSQLParser(lexerEngine);
@@ -65,6 +58,7 @@ public final class PostgreSQLSelectParser extends AbstractSelectParser {
         groupBySQLParser = new GroupBySQLParser(lexerEngine);
         havingSQLParser = new HavingSQLParser(lexerEngine);
         orderBySQLParser = new PostgreSQLOrderBySQLParser(lexerEngine);
+        limitSQLParser = new PostgreSQLLimitSQLParser(lexerEngine);
     }
     
     @Override
@@ -76,81 +70,9 @@ public final class PostgreSQLSelectParser extends AbstractSelectParser {
         groupBySQLParser.parse(selectStatement);
         havingSQLParser.parse();
         orderBySQLParser.parse(selectStatement);
-        parseLimit(selectStatement);
+        limitSQLParser.parse(selectStatement);
         parseFor();
         parseRest();
-    }
-    
-    private void parseLimit(final SelectStatement selectStatement) {
-        Optional<LimitValue> offset = Optional.absent();
-        Optional<LimitValue> rowCount = Optional.absent();
-        while (true) {
-            if (getLexerEngine().skipIfEqual(PostgreSQLKeyword.LIMIT)) {
-                rowCount = buildRowCount(selectStatement);
-            } else if (getLexerEngine().skipIfEqual(PostgreSQLKeyword.OFFSET)) {
-                offset = buildOffset(selectStatement);
-            } else {
-                break;
-            }
-        }
-        if (offset.isPresent() || rowCount.isPresent()) {
-            setLimit(offset, rowCount, selectStatement);
-        }
-    }
-    
-    private Optional<LimitValue> buildRowCount(final SelectStatement selectStatement) {
-        int parameterIndex = getParametersIndex();
-        int rowCountValue = -1;
-        int rowCountIndex = -1;
-        int valueBeginPosition = getLexerEngine().getCurrentToken().getEndPosition();
-        if (getLexerEngine().equalAny(DefaultKeyword.ALL)) {
-            getLexerEngine().nextToken();
-        } else {
-            if (getLexerEngine().equalAny(Literals.INT, Literals.FLOAT)) {
-                rowCountValue = NumberUtil.roundHalfUp(getLexerEngine().getCurrentToken().getLiterals());
-                valueBeginPosition = valueBeginPosition - (rowCountValue + "").length();
-                selectStatement.getSqlTokens().add(new RowCountToken(valueBeginPosition, rowCountValue));
-            } else if (getLexerEngine().equalAny(Symbol.QUESTION)) {
-                rowCountIndex = parameterIndex++;
-                setParametersIndex(parameterIndex);
-                rowCountValue = -1;
-            } else {
-                throw new SQLParsingException(getLexerEngine());
-            }
-            getLexerEngine().nextToken();
-        }
-        return Optional.of(new LimitValue(rowCountValue, rowCountIndex));
-    }
-    
-    private Optional<LimitValue> buildOffset(final SelectStatement selectStatement) {
-        int parameterIndex = getParametersIndex();
-        int offsetValue = -1;
-        int offsetIndex = -1;
-        int offsetBeginPosition = getLexerEngine().getCurrentToken().getEndPosition();
-        if (getLexerEngine().equalAny(Literals.INT, Literals.FLOAT)) {
-            offsetValue = NumberUtil.roundHalfUp(getLexerEngine().getCurrentToken().getLiterals());
-            offsetBeginPosition = offsetBeginPosition - (offsetValue + "").length();
-            selectStatement.getSqlTokens().add(new OffsetToken(offsetBeginPosition, offsetValue));
-        } else if (getLexerEngine().equalAny(Symbol.QUESTION)) {
-            offsetIndex = parameterIndex++;
-            setParametersIndex(parameterIndex);
-        } else {
-            throw new SQLParsingException(getLexerEngine());
-        }
-        getLexerEngine().nextToken();
-        getLexerEngine().skipIfEqual(DefaultKeyword.ROW, PostgreSQLKeyword.ROWS);
-        return Optional.of(new LimitValue(offsetValue, offsetIndex));
-    }
-    
-    private void setLimit(final Optional<LimitValue> offset, final Optional<LimitValue> rowCount, final SelectStatement selectStatement) {
-        Limit limit = new Limit(true);
-        if (offset.isPresent()) {
-            limit.setOffset(offset.get());
-        }
-        if (rowCount.isPresent()) {
-            limit.setRowCount(rowCount.get());
-        }
-        selectStatement.setLimit(limit);
     }
     
     private void parseFor() {
