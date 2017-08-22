6e27dd888245b320e774a3b82d2663efae3cdeaa
terrymanu <terrymanu@163.com>
2017-08-22 18:25:49 +0800

    add SQLStatementParserFactory

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/SQLParsingEngine.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/SQLParsingEngine.java
@@ -21,18 +21,8 @@ import com.dangdang.ddframe.rdb.sharding.api.rule.ShardingRule;
 import com.dangdang.ddframe.rdb.sharding.constant.DatabaseType;
 import com.dangdang.ddframe.rdb.sharding.parsing.lexer.LexerEngine;
 import com.dangdang.ddframe.rdb.sharding.parsing.lexer.LexerEngineFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.DefaultKeyword;
-import com.dangdang.ddframe.rdb.sharding.parsing.lexer.token.TokenType;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.exception.SQLParsingUnsupportedException;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.SQLStatement;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.ddl.alter.AlterParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.ddl.create.CreateParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.ddl.drop.DropParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.ddl.truncate.TruncateParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dml.delete.DeleteParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dml.insert.InsertParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dml.update.UpdateParserFactory;
-import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.dql.select.SelectParserFactory;
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.statement.SQLStatementParserFactory;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -57,29 +47,6 @@ public final class SQLParsingEngine {
     public SQLStatement parse() {
         LexerEngine lexerEngine = LexerEngineFactory.newInstance(dbType, sql);
         lexerEngine.nextToken();
-        TokenType tokenType = lexerEngine.getCurrentToken().getType();
-        if (!(tokenType instanceof DefaultKeyword)) {
-            throw new SQLParsingUnsupportedException(tokenType);
-        }
-        switch ((DefaultKeyword) tokenType) {
-            case SELECT:
-                return SelectParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case INSERT:
-                return InsertParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case UPDATE:
-                return UpdateParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case DELETE:
-                return DeleteParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case CREATE:
-                return CreateParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case ALTER:
-                return AlterParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case DROP:
-                return DropParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            case TRUNCATE:
-                return TruncateParserFactory.newInstance(dbType, shardingRule, lexerEngine).parse();
-            default:
-                throw new SQLParsingUnsupportedException(lexerEngine.getCurrentToken().getType());
-        }
+        return SQLStatementParserFactory.newInstance(dbType, lexerEngine.getCurrentToken().getType(), shardingRule, lexerEngine).parse();
     }
 }
