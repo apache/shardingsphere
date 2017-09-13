a906ff7592bda2af11bfa49a087ae86c54388f43
terrymanu <terrymanu@163.com>
2017-09-13 16:31:45 +0800

    add orchestration listener for config change init version

--- sharding-jdbc-orchestration/src/main/java/com/dangdang/ddframe/rdb/sharding/api/OrchestrationShardingDataSourceFactory.java
+++ sharding-jdbc-orchestration/src/main/java/com/dangdang/ddframe/rdb/sharding/api/OrchestrationShardingDataSourceFactory.java
@@ -22,8 +22,14 @@ import com.dangdang.ddframe.rdb.sharding.jdbc.core.datasource.ShardingDataSource
 import com.dangdang.ddframe.rdb.sharding.json.DataSourceJsonConverter;
 import com.dangdang.ddframe.rdb.sharding.json.GsonFactory;
 import com.dangdang.ddframe.rdb.sharding.reg.base.CoordinatorRegistryCenter;
+import com.google.common.base.Charsets;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.TreeCache;
+import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
+import org.apache.curator.framework.recipes.cache.TreeCacheListener;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -50,10 +56,10 @@ public final class OrchestrationShardingDataSourceFactory {
      */
     public static DataSource createDataSource(
             final String name, final CoordinatorRegistryCenter registryCenter, final Map<String, DataSource> dataSourceMap, final ShardingRuleConfig shardingRuleConfig) throws SQLException {
-        registryCenter.init();
-        registryCenter.persist("/" + name + "/config/datasource", DataSourceJsonConverter.toJson(dataSourceMap));
-        registryCenter.persist("/" + name + "/config/sharding", GsonFactory.getGson().toJson(shardingRuleConfig));
-        return new ShardingDataSource(shardingRuleConfig.build(dataSourceMap));
+        initRegistryCenter(name, registryCenter, dataSourceMap, shardingRuleConfig);
+        ShardingDataSource result = new ShardingDataSource(shardingRuleConfig.build(dataSourceMap));
+        addConfigurationChangeListener(name, registryCenter, result);
+        return result;
     }
     
     /**
@@ -70,10 +76,42 @@ public final class OrchestrationShardingDataSourceFactory {
     public static DataSource createDataSource(
             final String name, final CoordinatorRegistryCenter registryCenter, final Map<String, DataSource> dataSourceMap, 
             final ShardingRuleConfig shardingRuleConfig, final Properties props) throws SQLException {
-        registryCenter.init();
-        registryCenter.persist(name + "/config/datasource", DataSourceJsonConverter.toJson(dataSourceMap));
-        registryCenter.persist(name + "/config/sharding", GsonFactory.getGson().toJson(shardingRuleConfig));
+        initRegistryCenter(name, registryCenter, dataSourceMap, shardingRuleConfig);
         // TODO props
-        return new ShardingDataSource(shardingRuleConfig.build(dataSourceMap), props);
+        ShardingDataSource result = new ShardingDataSource(shardingRuleConfig.build(dataSourceMap), props);
+        addConfigurationChangeListener(name, registryCenter, result);
+        return result;
+    }
+    
+    private static void initRegistryCenter(final String name, 
+                                           final CoordinatorRegistryCenter registryCenter, final Map<String, DataSource> dataSourceMap, final ShardingRuleConfig shardingRuleConfig) {
+        registryCenter.init();
+        registryCenter.persist("/" + name + "/config/datasource", DataSourceJsonConverter.toJson(dataSourceMap));
+        registryCenter.persist("/" + name + "/config/sharding", GsonFactory.getGson().toJson(shardingRuleConfig));
+        registryCenter.addCacheData("/" + name + "/config");
+    }
+    
+    private static void addConfigurationChangeListener(final String name, final CoordinatorRegistryCenter registryCenter, final ShardingDataSource shardingDataSource) {
+        TreeCache cache = (TreeCache) registryCenter.getRawCache("/" + name + "/config");
+        cache.getListenable().addListener(new TreeCacheListener() {
+            
+            @Override
+            public void childEvent(final CuratorFramework client, final TreeCacheEvent event) throws Exception {
+                ChildData childData = event.getData();
+                if (null == childData || null == childData.getData()) {
+                    return;
+                }
+                String path = childData.getPath();
+                if (path.isEmpty()) {
+                    return;
+                }
+                if (("/" + name + "/config/sharding").equals(path) || ("/" + name + "/config/datasource").equals(path)) {
+                    ShardingRuleConfig newShardingRuleConfig = GsonFactory.getGson().fromJson(new String(childData.getData(), Charsets.UTF_8), ShardingRuleConfig.class);
+                    Map<String, DataSource> newDataSourceMap = DataSourceJsonConverter.fromJson(new String(childData.getData(), Charsets.UTF_8));
+                    // TODO props
+                    shardingDataSource.renew(newShardingRuleConfig.build(newDataSourceMap), new Properties());
+                }
+            }
+        });
     }
 }
