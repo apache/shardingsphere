982bb8425e19351432b3d9e34835635ae5543958
terrymanu <terrymanu@163.com>
2017-09-22 19:39:08 +0800

    fix test case for orchestration

--- sharding-jdbc-orchestration-example/src/main/java/io/shardingjdbc/example/orchestration/Main.java
+++ sharding-jdbc-orchestration-example/src/main/java/io/shardingjdbc/example/orchestration/Main.java
@@ -98,7 +98,6 @@ public final class Main {
         return result;
     }
     
-    
     private static void createTable(final DataSource dataSource) throws SQLException {
         executeUpdate(dataSource, "CREATE TABLE IF NOT EXISTS `t_order` (`order_id` INT NOT NULL, `user_id` INT NOT NULL, `status` VARCHAR(50), PRIMARY KEY (`order_id`))");
         executeUpdate(dataSource, "CREATE TABLE IF NOT EXISTS `t_order_item` (`item_id` INT NOT NULL, `order_id` INT NOT NULL, `user_id` INT NOT NULL, PRIMARY KEY (`item_id`))");
