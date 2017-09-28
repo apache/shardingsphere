22259a9c9683742a5f7fa444553278b9050a01fc
guangyun1013 <liguangyun@dangdang.com>
2017-09-28 10:09:10 +0800

    Add spring namespace test

--- sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/test/java/io/shardingjdbc/spring/cases/WithNamespaceAlgorithmExpressionTest.java
+++ sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/test/java/io/shardingjdbc/spring/cases/WithNamespaceAlgorithmExpressionTest.java
@@ -17,9 +17,70 @@
 
 package io.shardingjdbc.spring.cases;
 
+import io.shardingjdbc.core.api.config.strategy.InlineShardingStrategyConfiguration;
+import io.shardingjdbc.core.jdbc.core.datasource.ShardingDataSource;
+import io.shardingjdbc.core.rule.TableRule;
 import io.shardingjdbc.spring.AbstractShardingBothDataBasesAndTablesSpringDBUnitTest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.annotation.Resource;
+
 import org.springframework.test.context.ContextConfiguration;
 
 @ContextConfiguration(locations = "classpath:META-INF/rdb/withNamespaceAlgorithmExpression.xml")
 public final class WithNamespaceAlgorithmExpressionTest extends AbstractShardingBothDataBasesAndTablesSpringDBUnitTest {
+    
+    @Resource
+    private InlineShardingStrategyConfiguration databaseStrategy;
+    
+    @Resource
+    private InlineShardingStrategyConfiguration orderTableStrategy;
+    
+    @Resource
+    private InlineShardingStrategyConfiguration orderItemTableStrategy;
+    
+    @Resource
+    private ShardingDataSource shardingDataSource;
+    
+    public void testShardingNamespace() {
+        assertShardingStrategy();
+        assertShardingDataSource();
+    }
+    
+    private void assertShardingStrategy() {
+        assertShardingInlineStrategy(databaseStrategy, "user_id", "dbtbl_${user_id % 2}");
+        assertShardingInlineStrategy(orderTableStrategy, "order_id", "t_order_${order_id % 4}");
+        assertShardingInlineStrategy(orderItemTableStrategy, "order_id", "t_order_item_${order_id % 4}");
+    }
+    
+    private void assertShardingDataSource() {
+        assertShardingRule();
+        assertShardingTableRule();
+    }
+    
+    private void assertShardingRule() {
+        assertDataSourceName(shardingDataSource);
+        assertDefaultDataSourceName(shardingDataSource);
+    }
+    
+    private void assertShardingTableRule() {
+        Collection<TableRule> tableRules = getTableRules(shardingDataSource);
+        assertThat(tableRules.size(), is(2));
+        Iterator<TableRule> iter = tableRules.iterator();
+        TableRule orderRule = iter.next();
+        assertLogicTable(orderRule, "t_order");
+        assertActualTables(orderRule, "t_order_");
+        assertShardingRuleStrategy(orderRule.getDatabaseShardingStrategy(), databaseStrategy);
+        assertShardingRuleStrategy(orderRule.getTableShardingStrategy(), orderTableStrategy);
+        TableRule orderItemRule = iter.next();
+        assertLogicTable(orderItemRule, "t_order_item");
+        assertActualTables(orderItemRule, "t_order_item_");
+        assertShardingRuleStrategy(orderItemRule.getDatabaseShardingStrategy(), databaseStrategy);
+        assertShardingRuleStrategy(orderItemRule.getTableShardingStrategy(), orderItemTableStrategy);
+    }
 }
