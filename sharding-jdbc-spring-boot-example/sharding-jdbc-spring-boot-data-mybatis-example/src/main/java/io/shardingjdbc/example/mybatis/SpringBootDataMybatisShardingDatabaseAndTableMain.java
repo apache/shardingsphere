606479b4a09447abc01ed837fa3dc8b4043b5611
haocao <caohaoch@gmail.com>
2017-09-28 16:43:07 +0800

    Refactor example pom files.

--- sharding-jdbc-spring-boot-example/sharding-jdbc-spring-boot-data-mybatis-example/src/main/java/io/shardingjdbc/example/mybatis/SpringBootDataMybatisShardingDatabaseAndTableMain.java
+++ sharding-jdbc-spring-boot-example/sharding-jdbc-spring-boot-data-mybatis-example/src/main/java/io/shardingjdbc/example/mybatis/SpringBootDataMybatisShardingDatabaseAndTableMain.java
@@ -18,13 +18,11 @@
 package io.shardingjdbc.example.mybatis;
 
 import io.shardingjdbc.example.mybatis.service.DemoService;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@MapperScan("io.shardingjdbc.example.mybatis.repository")
 public class SpringBootDataMybatisShardingDatabaseAndTableMain {
     
     // CHECKSTYLE:OFF
