6ca664b85bb4f916a76208cd639d3ace5affdf2f
haocao <caohaoch@gmail.com>
2017-10-09 17:54:22 +0800

    For checkstyle.

--- sharding-jdbc-spring/sharding-jdbc-spring-boot-starter/src/test/java/io.shardingjdbc.spring.boot/SpringBootMain.java
+++ sharding-jdbc-spring/sharding-jdbc-spring-boot-starter/src/test/java/io.shardingjdbc.spring.boot/SpringBootMain.java
@@ -2,13 +2,11 @@ package io.shardingjdbc.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootMain {
     
     public static void main(final String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(SpringBootMain.class, args);
-        System.out.println(applicationContext.getBean("dataSource"));
+        SpringApplication.run(SpringBootMain.class, args);
     }
 }
