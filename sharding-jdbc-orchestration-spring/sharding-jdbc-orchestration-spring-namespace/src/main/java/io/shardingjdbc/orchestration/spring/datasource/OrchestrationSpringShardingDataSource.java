1ca230cb60d5d2f17c8a9cb9c6f9891caed0bf93
terrymanu <terrymanu@163.com>
2017-11-01 15:33:24 +0800

    save actual data source map to zk

--- sharding-jdbc-orchestration-spring/sharding-jdbc-orchestration-spring-namespace/src/main/java/io/shardingjdbc/orchestration/spring/datasource/OrchestrationSpringShardingDataSource.java
+++ sharding-jdbc-orchestration-spring/sharding-jdbc-orchestration-spring-namespace/src/main/java/io/shardingjdbc/orchestration/spring/datasource/OrchestrationSpringShardingDataSource.java
@@ -17,7 +17,9 @@
 
 package io.shardingjdbc.orchestration.spring.datasource;
 
+import io.shardingjdbc.core.api.config.MasterSlaveRuleConfiguration;
 import io.shardingjdbc.core.api.config.ShardingRuleConfiguration;
+import io.shardingjdbc.core.jdbc.core.datasource.MasterSlaveDataSource;
 import io.shardingjdbc.core.jdbc.core.datasource.ShardingDataSource;
 import io.shardingjdbc.orchestration.api.config.OrchestrationShardingConfiguration;
 import io.shardingjdbc.orchestration.internal.config.ConfigurationService;
@@ -29,7 +31,9 @@ import org.springframework.context.ApplicationContextAware;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 
 /**
@@ -45,11 +49,27 @@ public class OrchestrationSpringShardingDataSource extends ShardingDataSource im
     public OrchestrationSpringShardingDataSource(final String name, final boolean overwrite, final CoordinatorRegistryCenter registryCenter, final Map<String, DataSource> dataSourceMap, 
                                                  final ShardingRuleConfiguration shardingRuleConfig, final Properties props) throws SQLException {
         super(shardingRuleConfig.build(dataSourceMap), props);
-        OrchestrationShardingConfiguration config = new OrchestrationShardingConfiguration(name, overwrite, registryCenter, dataSourceMap, shardingRuleConfig);
+        Map<String, DataSource> actualDataSourceMap = new HashMap<>();
+        for (Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
+            if (entry.getValue() instanceof MasterSlaveDataSource) {
+                MasterSlaveDataSource masterSlaveDataSource = (MasterSlaveDataSource) entry.getValue();
+                actualDataSourceMap.putAll(masterSlaveDataSource.getAllDataSources());
+                MasterSlaveRuleConfiguration masterSlaveRuleConfig = new MasterSlaveRuleConfiguration();
+                masterSlaveRuleConfig.setName(masterSlaveDataSource.getMasterSlaveRule().getName());
+                masterSlaveRuleConfig.setMasterDataSourceName(masterSlaveDataSource.getMasterSlaveRule().getMasterDataSourceName());
+                masterSlaveRuleConfig.setSlaveDataSourceNames(masterSlaveDataSource.getMasterSlaveRule().getSlaveDataSourceMap().keySet());
+                masterSlaveRuleConfig.setLoadBalanceAlgorithmClassName(masterSlaveDataSource.getMasterSlaveRule().getStrategy().getClass().getName());
+                shardingRuleConfig.getMasterSlaveRuleConfigs().add(masterSlaveRuleConfig);
+            } else {
+                actualDataSourceMap.put(entry.getKey(), entry.getValue());
+            }
+        }
+        OrchestrationShardingConfiguration config = new OrchestrationShardingConfiguration(name, overwrite, registryCenter, actualDataSourceMap, shardingRuleConfig);
         new ConfigurationService(config.getRegistryCenter(), config.getName()).persistShardingConfiguration(config, props);
         new ConfigurationService(config.getRegistryCenter(), config.getName()).addShardingConfigurationChangeListener(config.getName(), config.getRegistryCenter(), this);
         new InstanceStateService(config.getRegistryCenter(), config.getName()).addShardingState(this);
     }
+    
 //    
 //    @Override
 //    public void renew(final ShardingRule newShardingRule, final Properties newProps) throws SQLException {
