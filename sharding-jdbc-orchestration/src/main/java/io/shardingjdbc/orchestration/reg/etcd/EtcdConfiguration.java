ebeaf11bfde67ff3ad1a7903b9b1ef4bc801c41e
terrymanu <terrymanu@163.com>
2017-11-23 15:28:30 +0800

    consist with ZookeeperConfiguration

--- sharding-jdbc-orchestration/src/main/java/io/shardingjdbc/orchestration/reg/etcd/EtcdConfiguration.java
+++ sharding-jdbc-orchestration/src/main/java/io/shardingjdbc/orchestration/reg/etcd/EtcdConfiguration.java
@@ -1,54 +1,53 @@
 package io.shardingjdbc.orchestration.reg.etcd;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Value;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
- * etcd configuration.
+ * Etcd configuration.
  *
  * @author junxiong
  */
-@Value
-@Builder
-@AllArgsConstructor
-public class EtcdConfiguration {
+@Getter
+@Setter
+public final class EtcdConfiguration {
+    
     /**
-     * etcd server list.
+     * Etcd server list.
      * 
      * <p>Include ip address and port, multiple servers split by comma. Etc: {@code http://host1:2379,http://host2:2379}</p>
      */
-    private final String serverLists;
-
+    private String serverLists;
+    
     /**
-     * root namespace of etcd cluster.
+     * Root namespace of etcd cluster.
      */
-    private final String namespace;
-
+    private String namespace;
+    
     /**
-     * time to live of ephemeral keys.
+     * Time to live of ephemeral keys.
      */
     private long timeToLive;
-
+    
     /**
-     * maximal retries when calling a etcd method.
+     * Maximal retries when calling a etcd method.
      */
     private int maxRetries = 3;
-
+    
     /**
-     * timeout when calling a etcd method in milliseconds.
+     * Timeout when calling a etcd method in milliseconds.
      */
     private long timeout;
-
+    
     /**
-     * username of etcd cluster.
+     * Username of etcd cluster.
      * 
      * <p>Default is not need digest</p>
      */
     private String username;
-
+    
     /**
-     * password of etcd cluster.
+     * Password of etcd cluster.
      */
     private String password;
 }
