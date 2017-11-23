73948189712f356a7d7a672ab7ae27c2fe6f6d56
terrymanu <terrymanu@163.com>
2017-11-23 15:46:10 +0800

    refactor EtcdRegistryCenter

--- sharding-jdbc-orchestration/src/main/java/io/shardingjdbc/orchestration/reg/etcd/EtcdRegistryCenter.java
+++ sharding-jdbc-orchestration/src/main/java/io/shardingjdbc/orchestration/reg/etcd/EtcdRegistryCenter.java
@@ -1,8 +1,6 @@
 package io.shardingjdbc.orchestration.reg.etcd;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
-import groovy.util.logging.Slf4j;
 import io.shardingjdbc.orchestration.reg.base.ChangeEvent;
 import io.shardingjdbc.orchestration.reg.base.ChangeListener;
 import io.shardingjdbc.orchestration.reg.base.CoordinatorRegistryCenter;
@@ -12,27 +10,27 @@ import io.shardingjdbc.orchestration.reg.etcd.internal.WatchEvent;
 import io.shardingjdbc.orchestration.reg.etcd.internal.Watcher;
 import io.shardingjdbc.orchestration.reg.etcd.internal.WatcherListener;
 import io.shardingjdbc.orchestration.reg.exception.RegException;
-import lombok.NonNull;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
- * ETCD based registry center.
+ * Etcd based registry center.
  *
  * @author junxiong
  */
-@Slf4j
 public class EtcdRegistryCenter implements CoordinatorRegistryCenter {
+    
+    private String namespace;
     
     private long timeToLive;
     
     private EtcdClient etcdClient;
     
-    private String namespace;
-    
     public EtcdRegistryCenter(final EtcdConfiguration etcdConfiguration) {
-        this.timeToLive = etcdConfiguration.getTimeToLive();
-        this.etcdClient = EtcdClientBuilder.newBuilder()
+        namespace = etcdConfiguration.getNamespace();
+        timeToLive = etcdConfiguration.getTimeToLive();
+        etcdClient = EtcdClientBuilder.newBuilder()
                 .endpoints(etcdConfiguration.getServerLists())
                 .timeout(etcdConfiguration.getTimeout())
                 .maxRetry(etcdConfiguration.getMaxRetries())
@@ -40,65 +38,9 @@ public class EtcdRegistryCenter implements CoordinatorRegistryCenter {
     }
     
     public EtcdRegistryCenter(final String namespace, final long timeToLive, final EtcdClient etcdClient) {
+        this.namespace = namespace;
         this.timeToLive = timeToLive;
         this.etcdClient = etcdClient;
-        this.namespace = namespace;
-    }
-    
-    private String namespace(final String path) {
-        return "/" + namespace + "/" + path;
-    }
-    
-    @Override
-    public String getDirectly(@NonNull final String key) {
-        return get(namespace(key));
-    }
-    
-    /**
-     * use default time to live.
-     *
-     * @param key   key of data
-     * @param value value of data
-     */
-    @Override
-    public void persistEphemeral(@NonNull final String key, @NonNull final String value) {
-        etcdClient.put(namespace(key), value, timeToLive);
-    }
-    
-    @Override
-    public List<String> getChildrenKeys(@NonNull final String path) {
-        Optional<List<String>> children = etcdClient.list(namespace(path));
-        return children.isPresent() ? children.get() : Lists.<String>newArrayList();
-    }
-    
-    @Override
-    public void watch(@NonNull final String path, @NonNull final ChangeListener changeListener) {
-        Optional<Watcher> watcher = etcdClient.watch(namespace(path));
-        if (watcher.isPresent()) {
-            watcher.get().addWatcherListener(new WatcherListener() {
-                @Override
-                public void onWatch(final WatchEvent watchEvent) {
-                    try {
-                        changeListener.onChange(fromWatchEvent(watchEvent));
-                    } catch (final Exception ex) {
-                        throw new RegException(ex);
-                    }
-                }
-            });
-        }
-    }
-    
-    private ChangeEvent fromWatchEvent(@NonNull final WatchEvent watchEvent) {
-        final ChangeEvent.ChangeData changeData = new ChangeEvent.ChangeData(watchEvent.getKey(), watchEvent.getValue());
-        switch (watchEvent.getWatchEventType()) {
-            case DELETE:
-                return new ChangeEvent(ChangeEvent.ChangeType.DELETED, changeData);
-            case UPDATE:
-                return new ChangeEvent(ChangeEvent.ChangeType.UPDATED, changeData);
-            case UNKNOWN:
-            default: 
-                return new ChangeEvent(ChangeEvent.ChangeType.UNKNOWN, changeData);
-        }
     }
     
     @Override
@@ -110,23 +52,76 @@ public class EtcdRegistryCenter implements CoordinatorRegistryCenter {
     }
     
     @Override
-    public String get(@NonNull final String key) {
-        Optional<String> value = etcdClient.get(namespace(key));
-        return value.orNull();
+    public String get(final String key) {
+        return etcdClient.get(getFullPathWithNamespace(key)).orNull();
     }
     
     @Override
-    public boolean isExisted(@NonNull final String key) {
-        return get(namespace(key)) != null;
+    public boolean isExisted(final String key) {
+        return null != get(getFullPathWithNamespace(key));
     }
     
     @Override
     public void persist(final String key, final String value) {
-        etcdClient.put(namespace(key), value);
+        etcdClient.put(getFullPathWithNamespace(key), value);
     }
     
     @Override
     public void update(final String key, final String value) {
-        etcdClient.put(namespace(key), value);
+        etcdClient.put(getFullPathWithNamespace(key), value);
+    }
+    
+    @Override
+    public String getDirectly(final String key) {
+        return get(getFullPathWithNamespace(key));
+    }
+    
+    @Override
+    public void persistEphemeral(final String key, final String value) {
+        etcdClient.put(getFullPathWithNamespace(key), value, timeToLive);
+    }
+    
+    @Override
+    public List<String> getChildrenKeys(final String path) {
+        Optional<List<String>> children = etcdClient.list(getFullPathWithNamespace(path));
+        return children.isPresent() ? children.get() : Collections.<String>emptyList();
+    }
+    
+    @Override
+    public void watch(final String path, final ChangeListener changeListener) {
+        Optional<Watcher> watcher = etcdClient.watch(getFullPathWithNamespace(path));
+        if (!watcher.isPresent()) {
+            return;
+        }
+        watcher.get().addWatcherListener(new WatcherListener() {
+            
+            @Override
+            public void onWatch(final WatchEvent watchEvent) {
+                try {
+                    changeListener.onChange(createWatchEvent(watchEvent));
+                    // CHECKSTYLE:OFF
+                } catch (final Exception ex) {
+                    // CHECKSTYLE:ON
+                    throw new RegException(ex);
+                }
+            }
+        });
+    }
+    
+    private ChangeEvent createWatchEvent(final WatchEvent watchEvent) {
+        ChangeEvent.ChangeData changeData = new ChangeEvent.ChangeData(watchEvent.getKey(), watchEvent.getValue());
+        switch (watchEvent.getWatchEventType()) {
+            case DELETE:
+                return new ChangeEvent(ChangeEvent.ChangeType.DELETED, changeData);
+            case UPDATE:
+                return new ChangeEvent(ChangeEvent.ChangeType.UPDATED, changeData);
+            case UNKNOWN:
+            default: 
+                return new ChangeEvent(ChangeEvent.ChangeType.UNKNOWN, changeData);
+        }
+    }
+    
+    private String getFullPathWithNamespace(final String path) {
+        return String.format("/%s/%s", namespace, path);
     }
 }
