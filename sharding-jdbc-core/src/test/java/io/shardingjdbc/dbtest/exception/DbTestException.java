4962e86c8a08175ea500edba49796ff54a9d705e
liu ze jian <qiankunshe@126.com>
2018-03-12 17:55:26 +0800

    异常类

---
+++ sharding-jdbc-core/src/test/java/io/shardingjdbc/dbtest/exception/DbTestException.java
@@ -0,0 +1,9 @@
+package io.shardingjdbc.dbtest.exception;
+
+public class DbTestException extends RuntimeException {
+
+    public DbTestException(String message){
+        super(message);
+    }
+
+}
