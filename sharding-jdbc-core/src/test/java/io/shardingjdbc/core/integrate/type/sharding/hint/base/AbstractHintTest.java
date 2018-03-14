2faf90cc5c895c2dea4abdfe38dcf5467c769b2c
terrymanu <terrymanu@163.com>
2018-03-14 15:16:10 +0800

    update copyright

--- sharding-jdbc-core/src/test/java/io/shardingjdbc/core/integrate/type/sharding/hint/base/AbstractHintTest.java
+++ sharding-jdbc-core/src/test/java/io/shardingjdbc/core/integrate/type/sharding/hint/base/AbstractHintTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -42,10 +42,6 @@ import java.util.Map;
 import static org.dbunit.Assertion.assertEquals;
 
 public abstract class AbstractHintTest extends AbstractSQLTest {
-    
-    public AbstractHintTest() {
-        
-    }
     
     @Before
     public void cleanAndInitTable() throws Exception {
