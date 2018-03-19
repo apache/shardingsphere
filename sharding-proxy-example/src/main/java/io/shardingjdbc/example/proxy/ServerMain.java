be285510b1248f3c5161e56609f500f6ce3307fa
caohao <caohao@vip.qq.com>
2018-03-19 17:39:27 +0800

    Renamed sharding-jdbc-server-example relative packages to sharding-proxy-example.

---
+++ sharding-proxy-example/src/main/java/io/shardingjdbc/example/proxy/ServerMain.java
@@ -0,0 +1,42 @@
+package io.shardingjdbc.example.proxy;
+
+import io.shardingjdbc.example.proxy.repository.RawJdbcRepository;
+import io.shardingjdbc.proxy.transport.ShardingProxy;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public final class ServerMain {
+    
+    public static void main(String[] args) throws InterruptedException, SQLException {
+        startServer();
+        RawJdbcRepository rawJdbcRepository = new RawJdbcRepository(createDataSource());
+        rawJdbcRepository.demo();
+    }
+    
+    private static void startServer() throws InterruptedException {
+        new Thread() {
+            
+            @Override
+            public void run() {
+                ShardingProxy server = new ShardingProxy();
+                try {
+                    server.start(3307);
+                } catch (final InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }.start();
+        Thread.sleep(1000L);
+    }
+    
+    private static DataSource createDataSource() {
+        BasicDataSource result = new BasicDataSource();
+        result.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
+        result.setUrl("jdbc:mysql://localhost:3307/demo_ds");
+        result.setUsername("root");
+        result.setPassword("");
+        return result;
+    }
+}
