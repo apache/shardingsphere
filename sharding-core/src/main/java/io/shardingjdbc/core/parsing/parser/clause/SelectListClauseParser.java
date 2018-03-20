38bff2d40bdf7d95a0dbaa761eb772640d3abe1b
terrymanu <terrymanu@163.com>
2018-03-20 12:58:32 +0800

    for #647: move parsing sharding core module

---
+++ sharding-core/src/main/java/io/shardingjdbc/core/parsing/parser/clause/SelectListClauseParser.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingjdbc.core.parsing.parser.clause;
+
+import com.google.common.base.Optional;
+import io.shardingjdbc.core.constant.AggregationType;
+import io.shardingjdbc.core.parsing.lexer.LexerEngine;
+import io.shardingjdbc.core.parsing.lexer.token.DefaultKeyword;
+import io.shardingjdbc.core.parsing.lexer.token.Keyword;
+import io.shardingjdbc.core.parsing.lexer.token.Symbol;
+import io.shardingjdbc.core.parsing.parser.clause.expression.AliasExpressionParser;
+import io.shardingjdbc.core.parsing.parser.context.selectitem.AggregationSelectItem;
+import io.shardingjdbc.core.parsing.parser.context.selectitem.CommonSelectItem;
+import io.shardingjdbc.core.parsing.parser.context.selectitem.SelectItem;
+import io.shardingjdbc.core.parsing.parser.context.selectitem.StarSelectItem;
+import io.shardingjdbc.core.parsing.parser.dialect.ExpressionParserFactory;
+import io.shardingjdbc.core.parsing.parser.sql.dql.select.SelectStatement;
+import io.shardingjdbc.core.parsing.parser.token.TableToken;
+import io.shardingjdbc.core.rule.ShardingRule;
+import io.shardingjdbc.core.util.SQLUtil;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Select list clause parser.
+ *
+ * @author zhangliang
+ */
+@Getter
+public class SelectListClauseParser implements SQLClauseParser {
+    
+    private final ShardingRule shardingRule;
+    
+    private final LexerEngine lexerEngine;
+    
+    private final AliasExpressionParser aliasExpressionParser;
+    
+    public SelectListClauseParser(final ShardingRule shardingRule, final LexerEngine lexerEngine) {
+        this.shardingRule = shardingRule;
+        this.lexerEngine = lexerEngine;
+        aliasExpressionParser = ExpressionParserFactory.createAliasExpressionParser(lexerEngine);
+    }
+    
+    /**
+     * Parse select list.
+     * 
+     * @param selectStatement select statement
+     * @param items select items
+     */
+    public void parse(final SelectStatement selectStatement, final List<SelectItem> items) {
+        do {
+            selectStatement.getItems().add(parseSelectItem(selectStatement));
+        } while (lexerEngine.skipIfEqual(Symbol.COMMA));
+        selectStatement.setSelectListLastPosition(lexerEngine.getCurrentToken().getEndPosition() - lexerEngine.getCurrentToken().getLiterals().length());
+        items.addAll(selectStatement.getItems());
+    }
+    
+    private SelectItem parseSelectItem(final SelectStatement selectStatement) {
+        lexerEngine.skipIfEqual(getSkippedKeywordsBeforeSelectItem());
+        SelectItem result;
+        if (isRowNumberSelectItem()) {
+            result = parseRowNumberSelectItem(selectStatement);
+        } else if (isStarSelectItem()) {
+            selectStatement.setContainStar(true);
+            result = parseStarSelectItem();
+        } else if (isAggregationSelectItem()) {
+            result = parseAggregationSelectItem(selectStatement);
+            parseRestSelectItem(selectStatement);
+        } else {
+            result = new CommonSelectItem(SQLUtil.getExactlyValue(parseCommonSelectItem(selectStatement) + parseRestSelectItem(selectStatement)), aliasExpressionParser.parseSelectItemAlias());
+        }
+        return result;
+    }
+    
+    protected Keyword[] getSkippedKeywordsBeforeSelectItem() {
+        return new Keyword[0];
+    }
+    
+    protected boolean isRowNumberSelectItem() {
+        return false;
+    }
+    
+    protected SelectItem parseRowNumberSelectItem(final SelectStatement selectStatement) {
+        throw new UnsupportedOperationException("Cannot support special select item.");
+    }
+    
+    private boolean isStarSelectItem() {
+        return Symbol.STAR.getLiterals().equals(SQLUtil.getExactlyValue(lexerEngine.getCurrentToken().getLiterals()));
+    }
+    
+    private SelectItem parseStarSelectItem() {
+        lexerEngine.nextToken();
+        aliasExpressionParser.parseSelectItemAlias();
+        return new StarSelectItem(Optional.<String>absent());
+    }
+    
+    private boolean isAggregationSelectItem() {
+        return lexerEngine.equalAny(DefaultKeyword.MAX, DefaultKeyword.MIN, DefaultKeyword.SUM, DefaultKeyword.AVG, DefaultKeyword.COUNT);
+    }
+    
+    private SelectItem parseAggregationSelectItem(final SelectStatement selectStatement) {
+        AggregationType aggregationType = AggregationType.valueOf(lexerEngine.getCurrentToken().getLiterals().toUpperCase());
+        lexerEngine.nextToken();
+        return new AggregationSelectItem(aggregationType, lexerEngine.skipParentheses(selectStatement), aliasExpressionParser.parseSelectItemAlias());
+    }
+    
+    private String parseCommonSelectItem(final SelectStatement selectStatement) {
+        String literals = lexerEngine.getCurrentToken().getLiterals();
+        int position = lexerEngine.getCurrentToken().getEndPosition() - literals.length();
+        StringBuilder result = new StringBuilder();
+        result.append(literals);
+        lexerEngine.nextToken();
+        if (lexerEngine.equalAny(Symbol.LEFT_PAREN)) {
+            result.append(lexerEngine.skipParentheses(selectStatement));
+        } else if (lexerEngine.equalAny(Symbol.DOT)) {
+            String tableName = SQLUtil.getExactlyValue(literals);
+            if (shardingRule.tryFindTableRuleByLogicTable(tableName).isPresent() || shardingRule.findBindingTableRule(tableName).isPresent()) {
+                selectStatement.getSqlTokens().add(new TableToken(position, literals));
+            }
+            result.append(lexerEngine.getCurrentToken().getLiterals());
+            lexerEngine.nextToken();
+            result.append(lexerEngine.getCurrentToken().getLiterals());
+            lexerEngine.nextToken();
+        }
+        return result.toString();
+    }
+    
+    private String parseRestSelectItem(final SelectStatement selectStatement) {
+        StringBuilder result = new StringBuilder();
+        while (lexerEngine.equalAny(Symbol.getOperators())) {
+            result.append(lexerEngine.getCurrentToken().getLiterals());
+            lexerEngine.nextToken();
+            result.append(parseCommonSelectItem(selectStatement));
+        }
+        return result.toString();
+    }
+}
