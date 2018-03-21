8d72cec9370e642868b890f73c30e9db9af3f297
tianbin <Tj!634346496#qq>
2018-03-21 20:39:10 +0800

    add test case

--- sharding-core/src/test/java/io/shardingjdbc/core/rule/DataNodeTest.java
+++ sharding-core/src/test/java/io/shardingjdbc/core/rule/DataNodeTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class DataNodeTest {
+public final  class DataNodeTest {
     
     @Test
     public void assertNewValidDataNode() {
