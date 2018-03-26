896dc177a922b82273bf06b0876083585864af16
liu ze jian <qiankunshe@126.com>
2018-03-26 19:50:41 +0800

    Delete table sql（删除表sql）

--- sharding-jdbc-core/src/test/resources/integrate/schema/table/drop/db.sql
+++ sharding-jdbc-core/src/test/resources/integrate/schema/table/drop/db.sql
@@ -1,2 +1,2 @@
-CREATE TABLE t_order (order_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (order_id));
-CREATE TABLE t_order_item (item_id INT NOT NULL, order_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (item_id));
+DROP t_order;
+DROP t_order_item;
