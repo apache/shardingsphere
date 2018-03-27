b988d60a4af1f60d763a8c8ec3f0e590a08bc037
tristaZero <yezhiji1991@163.com>
2018-03-27 18:33:14 +0800

    for checkstyle

--- sharding-console/src/main/java/io/shardingjdbc/console/controller/UserController.java
+++ sharding-console/src/main/java/io/shardingjdbc/console/controller/UserController.java
@@ -1,15 +1,13 @@
 package io.shardingjdbc.console.controller;
 
-import io.shardingjdbc.console.constant.ResponseCode;
 import io.shardingjdbc.console.entity.DBConnector;
 import io.shardingjdbc.console.entity.SessionRegistry;
-import io.shardingjdbc.console.entity.ResponseObject;
+import io.shardingjdbc.console.entity.Response;
 import io.shardingjdbc.console.entity.UserSession;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -29,53 +27,53 @@ public class UserController {
      * Handle https for user login.
      * 
      * @param userSession user info
-     * @param userUUID id
+     * @param userUUID user uuid
      * @param response response
      * @return response object
      */
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public ResponseObject login(@RequestBody final UserSession userSession, final @CookieValue(value = "userUUID", required = false, defaultValue = "") String userUUID,
+    public Response login(final UserSession userSession, final @CookieValue(value = "userUUID", required = false, defaultValue = "") String userUUID,
                                 final HttpServletResponse response) {
         if (!"".equals(userUUID)) {
-            return new ResponseObject(ResponseCode.SUCCESS);
+            return new Response(200, "OK");
         }
         Connection connection;
         try {
             connection = DBConnector.getConnection(userSession.getUserName(), userSession.getPassWord(), userSession.getTargetURL(), userSession.getDriver());
         } catch (final ClassNotFoundException | SQLException ex) {
-            // TODO
-            ex.getMessage();
-            return new ResponseObject(ResponseCode.FAILURE);
+            return new Response(403, "Authorization failed");
         }
-        setSession(userSession, response, connection);
-        return new ResponseObject(ResponseCode.SUCCESS);
+        setSession(userSession.getId(), response, connection);
+        
+        return new Response(200, "Authorization succeeded");
     }
-
-    private void setSession(final UserSession userSession, final HttpServletResponse response, final Connection connection) {
-        SessionRegistry.getInstance().addSession(userSession.getId(), connection);
-        Cookie cookie = new Cookie("userUUID", userSession.getId());
+    
+    private void setSession(final String uuid, final HttpServletResponse response, final Connection connection) {
+        SessionRegistry.getInstance().addSession(uuid, connection);
+        Cookie cookie = new Cookie("userUUID", uuid);
         cookie.setMaxAge(120 * 60);
         cookie.setPath("/");
         response.addCookie(cookie);
     }
 
     /**
-     * Handle http for user exiting.
+     * Handle http for user exit.
      * 
-     * @param userUUID useruuid
+     * @param userUUID user uuid
      * @param response response
      * @return response object
      */
     @RequestMapping(value = "/exit", method = RequestMethod.POST)
-    public ResponseObject exit(final @CookieValue(value = "userUUID", required = false, defaultValue = "") String userUUID, final HttpServletResponse response) {
+    public Response exit(final @CookieValue(value = "userUUID", required = false, defaultValue = "") String userUUID, final HttpServletResponse response) {
         if (!"".equals(userUUID)) {
             removeSession(userUUID, response);
         }
-        return new ResponseObject(ResponseCode.SUCCESS);
+        return new Response(200,"OK");
     }
 
-    private void removeSession(@CookieValue(value = "userUUID", required = false, defaultValue = "") String userUUID, HttpServletResponse response) {
+    private void removeSession(String userUUID, HttpServletResponse response) {
         SessionRegistry.getInstance().removeSession(userUUID);
+        
         Cookie cookie = new Cookie("userUUID", null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
