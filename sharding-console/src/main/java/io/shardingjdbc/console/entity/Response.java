8378f88dc1675e1ab96557a3024c41453039733a
tristaZero <yezhiji1991@163.com>
2018-03-27 17:57:36 +0800

    response object

---
+++ sharding-console/src/main/java/io/shardingjdbc/console/entity/Response.java
@@ -0,0 +1,30 @@
+package io.shardingjdbc.console.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Return response object.
+ * 
+ * @author panjuan
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+public final class Response {
+    
+    private int status;
+    private String message;
+    private Object data;
+
+    /**
+     * Generate response object.
+     * @param responseCode  response code. 
+     * @param responseMsg response message.
+     */
+    public Response(final int responseCode, final String responseMsg) {
+        status = responseCode;
+        message = responseMsg;
+    }
+}
