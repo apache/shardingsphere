5e74b8aaf841d25697c586e5bc1a1eac4ff0420c
terrymanu <terrymanu@163.com>
2018-03-29 15:38:52 +0800

    for #660: remove table-tokens tag

--- sharding-core/src/test/java/io/shardingjdbc/core/parsing/integrate/jaxb/token/SQLTokenAsserts.java
+++ sharding-core/src/test/java/io/shardingjdbc/core/parsing/integrate/jaxb/token/SQLTokenAsserts.java
@@ -23,7 +23,6 @@ import lombok.Setter;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -32,7 +31,6 @@ import java.util.List;
 @XmlAccessorType(XmlAccessType.FIELD)
 public final class SQLTokenAsserts {
     
-    @XmlElementWrapper(name = "table-tokens")
     @XmlElement(name = "table-token")
     private List<TableTokenAssert> tableTokens;
     
