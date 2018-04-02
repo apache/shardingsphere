0a011cae2ba6eee06ab1f296284c024b411146bb
林嘉琦 Lin Jiaqi <linjiaqi@dangdang.com>
2018-04-02 19:51:55 +0800

    fix the checkstyle errors

--- sharding-jdbc-core-spring/sharding-jdbc-core-spring-boot-starter/src/main/java/io/shardingjdbc/spring/boot/util/PropertyUtil.java
+++ sharding-jdbc-core-spring/sharding-jdbc-core-spring-boot-starter/src/main/java/io/shardingjdbc/spring/boot/util/PropertyUtil.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
 package io.shardingjdbc.spring.boot.util;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.springframework.core.env.Environment;
@@ -10,64 +28,74 @@ import io.shardingjdbc.core.exception.ShardingJdbcException;
 
 public class PropertyUtil {
 
-    private static int SPRING_BOOT_VERSION = 1;
+    private static final int SPRING_BOOT_VERSION;
 
     static {
+        int springBootVersion = 1;
         try {
             Class.forName("org.springframework.boot.bind.RelaxedPropertyResolver");
-        } catch (Exception e) {
-            SPRING_BOOT_VERSION = 2;
+        } catch (ClassNotFoundException e) {
+            springBootVersion = 2;
         }
+        SPRING_BOOT_VERSION = springBootVersion;
     }
 
+    /**
+     * Spring Boot 1.x is compatible with Spring Boot 2.x by Using Java Reflect.
+     * @param environment not null
+     * @param prefix not null
+     * @param targetClass not null
+     * @param <T> not null
+     * @return not null
+     */
     @SuppressWarnings("unchecked")
-    public static <T> T handle(Environment environment, String prefix, Class<T> targetClass) {
+    public static <T> T handle(final Environment environment, final String prefix, final Class<T> targetClass) {
         switch (SPRING_BOOT_VERSION) {
-        case 1:
-            return (T) v1(environment, prefix, targetClass);
-        default:
-            return (T) v2(environment, prefix, targetClass);
+            case 1:
+                return (T) v1(environment, prefix, targetClass);
+            default:
+                return (T) v2(environment, prefix, targetClass);
         }
     }
 
     /**
-     * Spring Boot 1.x
+     * Spring Boot 1.x.
      * @code {
-     * RelaxedPropertyResolver resolver = new RelaxedPropertyResolver(environment);
-     * return resolver.getSubProperties(prefix);
-     * }
+     *     RelaxedPropertyResolver resolver = new RelaxedPropertyResolver(environment);
+     *     return resolver.getSubProperties(prefix);
+     *     }
      */
-    private static Object v1(Environment environment, String prefix, Class<?> targetClass) {
+    private static Object v1(final Environment environment, final String prefix, final Class<?> targetClass) {
         try {
             Class<?> resolverClass = Class.forName("org.springframework.boot.bind.RelaxedPropertyResolver");
             Constructor<?> resolverConstructor = resolverClass.getDeclaredConstructor(PropertyResolver.class);
             Method getSubPropertiesMethod = resolverClass.getDeclaredMethod("getSubProperties", String.class);
             Object resolverObject = resolverConstructor.newInstance(environment);
-            prefix = prefix.endsWith(".") ? prefix : prefix + ".";
-            return getSubPropertiesMethod.invoke(resolverObject, prefix);
-        } catch (Exception e) {
+            String prefixParam = prefix.endsWith(".") ? prefix : prefix + ".";
+            return getSubPropertiesMethod.invoke(resolverObject, prefixParam);
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
             throw new ShardingJdbcException(e.getMessage(), e);
         }
     }
 
     /**
-     * Spring Boot 2.x
+     * Spring Boot 2.x.
      * @code {
-     * Binder binder = Binder.get(environment);
-     * return binder.bind(prefix, targetClass).get();
-     * }
+     *     Binder binder = Binder.get(environment);
+     *     return binder.bind(prefix, targetClass).get();
+     *     }
      */
-    private static Object v2(Environment environment, String prefix, Class<?> targetClass) {
+    private static Object v2(final Environment environment, final String prefix, final Class<?> targetClass) {
         try {
             Class<?> binderClass = Class.forName("org.springframework.boot.context.properties.bind.Binder");
             Method getMethod = binderClass.getDeclaredMethod("get", Environment.class);
             Method bindMethod = binderClass.getDeclaredMethod("bind", String.class, Class.class);
             Object binderObject = getMethod.invoke(null, environment);
-            prefix = prefix.endsWith(".") ? prefix.substring(0, prefix.length() - 1) : prefix;
-            Object bindResultObject = bindMethod.invoke(binderObject, prefix, targetClass);
+            String prefixParam = prefix.endsWith(".") ? prefix.substring(0, prefix.length() - 1) : prefix;
+            Object bindResultObject = bindMethod.invoke(binderObject, prefixParam, targetClass);
             Method resultGetMethod = bindResultObject.getClass().getDeclaredMethod("get");
             return resultGetMethod.invoke(bindResultObject);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
             throw new ShardingJdbcException(e.getMessage(), e);
         }
     }
