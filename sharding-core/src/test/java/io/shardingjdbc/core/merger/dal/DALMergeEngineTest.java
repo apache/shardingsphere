5adf3c67cfcfbae1942af2f7813d4eb9a4cfb815
tuohai666 <tuohaibbb@gmail.com>
2018-04-02 13:59:34 +0800

    for comments in #687

--- sharding-core/src/test/java/io/shardingjdbc/core/merger/dal/DALMergeEngineTest.java
+++ sharding-core/src/test/java/io/shardingjdbc/core/merger/dal/DALMergeEngineTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 1999-2015 dangdang.com.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
 package io.shardingjdbc.core.merger.dal;
 
 import io.shardingjdbc.core.merger.QueryResult;
@@ -11,7 +28,6 @@ import io.shardingjdbc.core.parsing.parser.dialect.mysql.statement.ShowDatabases
 import io.shardingjdbc.core.parsing.parser.dialect.mysql.statement.ShowOtherStatement;
 import io.shardingjdbc.core.parsing.parser.dialect.mysql.statement.ShowTablesStatement;
 import io.shardingjdbc.core.parsing.parser.sql.dal.DALStatement;
-import io.shardingjdbc.core.rule.ShardingRule;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,12 +42,10 @@ import static org.mockito.Mockito.mock;
 
 public final class DALMergeEngineTest {
     
-    private ShardingRule shardingRule;
-    
     private List<QueryResult> queryResults;
     
     @Before
-    public void setUp() throws SQLException {
+    public void setUp() {
         ResultSet resultSet = mock(ResultSet.class);
         queryResults = Collections.<QueryResult>singletonList(new TestQueryResult(resultSet));
     }
@@ -39,28 +53,28 @@ public final class DALMergeEngineTest {
     @Test
     public void assertMergeForShowDatabasesStatement() throws SQLException {
         DALStatement dalStatement = new ShowDatabasesStatement();
-        DALMergeEngine dalMergeEngine = new DALMergeEngine(shardingRule, queryResults, dalStatement);
+        DALMergeEngine dalMergeEngine = new DALMergeEngine(null, queryResults, dalStatement);
         assertThat(dalMergeEngine.merge(), instanceOf(ShowDatabasesMergedResult.class));
     }
     
     @Test
     public void assertMergeForShowShowTablesStatement() throws SQLException {
         DALStatement dalStatement = new ShowTablesStatement();
-        DALMergeEngine dalMergeEngine = new DALMergeEngine(shardingRule, queryResults, dalStatement);
+        DALMergeEngine dalMergeEngine = new DALMergeEngine(null, queryResults, dalStatement);
         assertThat(dalMergeEngine.merge(), instanceOf(ShowTablesMergedResult.class));
     }
     
     @Test
     public void assertMergeForShowCreateTableStatement() throws SQLException {
         DALStatement dalStatement = new ShowCreateTableStatement();
-        DALMergeEngine dalMergeEngine = new DALMergeEngine(shardingRule, queryResults, dalStatement);
+        DALMergeEngine dalMergeEngine = new DALMergeEngine(null, queryResults, dalStatement);
         assertThat(dalMergeEngine.merge(), instanceOf(ShowCreateTableMergedResult.class));
     }
     
     @Test
     public void assertMergeForShowOtherStatement() throws SQLException {
         DALStatement dalStatement = new ShowOtherStatement();
-        DALMergeEngine dalMergeEngine = new DALMergeEngine(shardingRule, queryResults, dalStatement);
+        DALMergeEngine dalMergeEngine = new DALMergeEngine(null, queryResults, dalStatement);
         assertThat(dalMergeEngine.merge(), instanceOf(ShowOtherMergedResult.class));
     }
 }
