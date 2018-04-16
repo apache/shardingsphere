70f68f0bc693e81fee6b9fe2c1d0ceec87dd6595
xiaoyu1 <xiaoyu@globalegrow.com>
2018-04-16 18:26:48 +0800

    Initializer Channel note modification

--- sharding-proxy/src/main/java/io/shardingjdbc/proxy/frontend/netty/ServerHandlerInitializer.java
+++ sharding-proxy/src/main/java/io/shardingjdbc/proxy/frontend/netty/ServerHandlerInitializer.java
@@ -27,7 +27,7 @@ import io.shardingjdbc.proxy.transport.common.codec.PacketCodecFactory;
 import lombok.RequiredArgsConstructor;
 
 /**
- * init Channel.
+ * Initializer Channel.
  * @author xiaoyu
  */
 @RequiredArgsConstructor
