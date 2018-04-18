2c9d0e55f82f24f973db3e484b7612cb5d7ba9b9
saaav <draculav@hotmail.com>
2018-04-18 15:30:00 +0800

    CLEAR

--- src/main/java/com/saaavsaaa/client/ClientFactory.java
+++ src/main/java/com/saaavsaaa/client/ClientFactory.java
@@ -1,41 +1,49 @@
 package com.saaavsaaa.client;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by aaa on 18-4-18.
  */
 public class ClientFactory {
+    private ZookeeperClient client;
+    private String namespace;
+    private String scheme;
+    private byte[] auth;
     
-    private static final CountDownLatch CONNECTED = new CountDownLatch(1);
-    
-    private ClientFactory(){}
+    public ClientFactory(){}
     
     public static void main(String[] args) throws Exception{
         ClientFactory creator = new ClientFactory();
-        creator.newClient("192.168.2.44:2181", 20000).setNamespace("test").authorization("digest", "digest".getBytes());
+        ZookeeperClient client = creator.setNamespace("test").authorization("digest", "digest".getBytes()).newClient("192.168.2.44:2181", 20000).start();
     }
     
-    ZookeeperClient newClient(final String serverAddrs, final int sessionTimeoutMilliseconds) throws IOException, InterruptedException {
-        ZookeeperClient client = new ZookeeperClient(serverAddrs, sessionTimeoutMilliseconds);
-        client.start(connectWatcher());
-        CONNECTED.wait();
+    public ClientFactory newClient(final String servers, final int sessionTimeoutMilliseconds) {
+        client = new ZookeeperClient(servers, sessionTimeoutMilliseconds);
+        return this;
+    }
+    
+    public synchronized ZookeeperClient start() throws IOException, InterruptedException {
+        client.setRootNode(namespace);
+        client.setAuthorities(scheme , auth);
+        client.start();
         return client;
     }
     
-    private Watcher connectWatcher() {
-        return new Watcher(){
-            public void process(WatchedEvent event) {
-                if(Event.KeeperState.SyncConnected == event.getState()){
-                    if(Event.EventType.None == event.getType()){
-                        CONNECTED.countDown();
-                    }
-                }
-            }
-        };
+    public ClientFactory setNamespace(String namespace) {
+        if (!namespace.startsWith("/")){
+            namespace = "/" + namespace;
+        }
+        this.namespace = namespace;
+        return this;
+    }
+    
+    public ClientFactory authorization(String scheme, byte[] auth){
+        if (scheme == null || scheme.trim().length() == 0) {
+            return this;
+        }
+        this.scheme = scheme;
+        this.auth = auth;
+        return this;
     }
 }
--- src/main/java/com/saaavsaaa/client/ZookeeperClient.java
+++ src/main/java/com/saaavsaaa/client/ZookeeperClient.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public class ZookeeperClient{
+    private static final CountDownLatch CONNECTED = new CountDownLatch(1);
     public static final String PATH_SEPARATOR = "/";
     
     private final String servers;
@@ -25,24 +26,21 @@ public class ZookeeperClient{
         this.sessionTimeOut = sessionTimeoutMilliseconds;
     }
     
-    void start(final Watcher watcher) throws IOException, InterruptedException {
-        zooKeeper = new ZooKeeper(servers, sessionTimeOut, watcher);
+    void start() throws IOException, InterruptedException {
+        zooKeeper = new ZooKeeper(servers, sessionTimeOut, connectWatcher());
+        CONNECTED.wait();
     }
     
-    public ZookeeperClient setNamespace(String namespace) {
-        if (!namespace.startsWith("/")){
-            namespace = "/" + namespace;
-        }
-        this.rootNode = namespace;
-        return this;
-    }
-    
-    public void authorization(String scheme, byte[] auth){
-        if (scheme == null || scheme.trim().length() == 0) {
-            return;
-        }
-        zooKeeper.addAuthInfo(scheme , auth);
-        authorities = ZooDefs.Ids.CREATOR_ALL_ACL;
+    private Watcher connectWatcher() {
+        return new Watcher(){
+            public void process(WatchedEvent event) {
+                if(Event.KeeperState.SyncConnected == event.getState()){
+                    if(Event.EventType.None == event.getType()){
+                        CONNECTED.countDown();
+                    }
+                }
+            }
+        };
     }
     
     public byte[] getData(String key) throws KeeperException, InterruptedException {
@@ -69,17 +67,26 @@ public class ZookeeperClient{
         if (path.indexOf("/") > -1){
             System.out.println("exist / need op ..=============================================");
         }
-        zooKeeper.create(path, data, authorities, createMode);
+        this.zooKeeper.create(path, data, authorities, createMode);
     }
     
     private void createRootNode() throws KeeperException, InterruptedException {
         if (checkExists(rootNode)){
             return;
         }
-        zooKeeper.create(getRealPath(rootNode), new byte[0], authorities, CreateMode.PERSISTENT);
+        this.zooKeeper.create(getRealPath(rootNode), new byte[0], authorities, CreateMode.PERSISTENT);
     }
     
     public void update(String key, byte[] data) throws KeeperException, InterruptedException {
-        zooKeeper.transaction().setData(getRealPath(key), data, -1).commit();
+        this.zooKeeper.transaction().setData(getRealPath(key), data, -1).commit();
+    }
+    
+    public void setRootNode(String rootNode) {
+        this.rootNode = rootNode;
+    }
+    
+    public void setAuthorities(String scheme, byte[] auth) {
+        this.zooKeeper.addAuthInfo(scheme , auth);
+        this.authorities = ZooDefs.Ids.CREATOR_ALL_ACL;
     }
 }
