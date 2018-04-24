fe021e2d026583ddd00712772319629bbb26c353
tianbin <tianbin_hysc@163.com>
2018-04-25 00:44:45 +0800

    add test case

--- sharding-core/src/test/java/io/shardingjdbc/core/routing/type/broadcast/DatabaseBroadcastRoutingEngineTest.java
+++ sharding-core/src/test/java/io/shardingjdbc/core/routing/type/broadcast/DatabaseBroadcastRoutingEngineTest.java
@@ -17,21 +17,48 @@
 
 package io.shardingjdbc.core.routing.type.broadcast;
 
+import io.shardingjdbc.core.api.config.ShardingRuleConfiguration;
+import io.shardingjdbc.core.api.config.TableRuleConfiguration;
+import io.shardingjdbc.core.rule.ShardingRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DatabaseBroadcastRoutingEngine.class)
 public final class DatabaseBroadcastRoutingEngineTest {
-    
+
+
     @Test
-    public void assertRoute() {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public void assertRoute() throws NoSuchFieldException, IllegalAccessException {
+        DatabaseBroadcastRoutingEngine databaseBroadcastRoutingEngine = PowerMockito.mock(DatabaseBroadcastRoutingEngine.class);
+        Field field=DatabaseBroadcastRoutingEngine.class.getDeclaredField("shardingRule");
+        field.setAccessible(true);
+        field.set(databaseBroadcastRoutingEngine, createShardingRule());
+        databaseBroadcastRoutingEngine.route();
+    }
+
+    private ShardingRule createShardingRule() {
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        TableRuleConfiguration tableRuleConfig = createTableRuleConfig();
+        shardingRuleConfig.getTableRuleConfigs().add(tableRuleConfig);
+        return new ShardingRule(shardingRuleConfig, createDataSourceNames());
+    }
+
+    private Collection<String> createDataSourceNames() {
+        return Arrays.asList("ds0", "ds1");
+    }
+
+    private TableRuleConfiguration createTableRuleConfig() {
+        TableRuleConfiguration result = new TableRuleConfiguration();
+        result.setLogicTable("LOGIC_TABLE");
+        result.setActualDataNodes("ds${0..1}.table_${0..2}");
+        return result;
     }
 }
