3e91e928e7d6d553fc2ca03757e1238093a56b71
saaav <draculav@hotmail.com>
2018-05-02 16:05:51 +0800

    test

--- src/test/java/com/saaavsaaa/client/zookeeper/UsualWatchClientTest.java
+++ src/test/java/com/saaavsaaa/client/zookeeper/UsualWatchClientTest.java
@@ -9,71 +9,11 @@ import java.io.IOException;
 /**
  * Created by aaa
  */
-public class UsualWatchClientTest extends BaseClientTest {
+public class UsualWatchClientTest extends UsualClientTest {
     
     @Override
     protected Client createClient(final ClientFactory creator) throws IOException, InterruptedException {
         Listener listener = TestSupport.buildListener();
         return creator.setNamespace(TestSupport.ROOT).authorization(TestSupport.AUTH, TestSupport.AUTH.getBytes()).newUsualClient(TestSupport.SERVERS, TestSupport.SESSION_TIMEOUT).watch(listener).start();
-    }
-    
-    @Test
-    public void createRoot() throws KeeperException, InterruptedException {
-        super.createRoot(testClient);
-    }
-    
-    @Test
-    public void createChild() throws KeeperException, InterruptedException {
-        super.createChild(testClient);
-    }
-    
-    @Test
-    public void deleteBranch() throws KeeperException, InterruptedException {
-        super.deleteBranch(testClient);
-    }
-    
-    @Test
-    public void isExisted() throws KeeperException, InterruptedException {
-        super.isExisted(testClient);
-    }
-    
-    @Test
-    public void get() throws KeeperException, InterruptedException {
-        super.get(testClient);
-    }
-    
-    @Test
-    public void asynGet() throws KeeperException, InterruptedException {
-        super.asynGet(testClient);
-    }
-    
-    @Test
-    public void getChildrenKeys() throws KeeperException, InterruptedException {
-        super.getChildrenKeys(testClient);
-    }
-    
-    @Test
-    public void persist() throws KeeperException, InterruptedException {
-        super.persist(testClient);
-    }
-    
-    @Test
-    public void persistEphemeral() throws KeeperException, InterruptedException {
-        super.persistEphemeral(testClient);
-    }
-    
-    @Test
-    public void delAllChildren() throws KeeperException, InterruptedException {
-        super.delAllChildren(testClient);
-    }
-    
-    @Test
-    public void watch() throws KeeperException, InterruptedException {
-        super.watch(testClient);
-    }
-    
-    @Test
-    public void close() throws Exception {
-        super.close(testClient);
     }
 }
