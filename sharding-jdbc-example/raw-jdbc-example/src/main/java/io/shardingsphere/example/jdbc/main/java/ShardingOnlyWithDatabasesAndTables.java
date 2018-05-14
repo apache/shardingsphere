c2cb378d2aa6921227477b0b980909c08fdf0e3c
terrymanu <terrymanu@163.com>
2018-05-15 00:04:25 +0800

    update raw-jdbc-example

---
+++ sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/java/ShardingOnlyWithDatabasesAndTables.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.example.jdbc.main.java;
+
+import io.shardingsphere.example.jdbc.fixture.algorithm.ModuloShardingTableAlgorithm;
+import io.shardingsphere.example.jdbc.fixture.DataRepository;
+import io.shardingsphere.example.jdbc.fixture.DataSourceUtil;
+import io.shardingsphere.core.api.ShardingDataSourceFactory;
+import io.shardingsphere.core.api.config.ShardingRuleConfiguration;
+import io.shardingsphere.core.api.config.TableRuleConfiguration;
+import io.shardingsphere.core.api.config.strategy.InlineShardingStrategyConfiguration;
+import io.shardingsphere.core.api.config.strategy.StandardShardingStrategyConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ShardingOnlyWithDatabasesAndTables {
+    
+    public static void main(final String[] args) throws SQLException {
+        new DataRepository(getShardingDataSource()).demo();
+    }
+    
+    private static DataSource getShardingDataSource() throws SQLException {
+        ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderTableRuleConfiguration());
+        shardingRuleConfig.getTableRuleConfigs().add(getOrderItemTableRuleConfiguration());
+        shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
+        shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new InlineShardingStrategyConfiguration("user_id", "demo_ds_${user_id % 2}"));
+        shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("order_id", new ModuloShardingTableAlgorithm()));
+        return ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig, new HashMap<String, Object>(), new Properties());
+    }
+    
+    private static TableRuleConfiguration getOrderTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration();
+        result.setLogicTable("t_order");
+        result.setActualDataNodes("demo_ds_${0..1}.t_order_${[0, 1]}");
+        result.setKeyGeneratorColumnName("order_id");
+        return result;
+    }
+    
+    private static TableRuleConfiguration getOrderItemTableRuleConfiguration() {
+        TableRuleConfiguration result = new TableRuleConfiguration();
+        result.setLogicTable("t_order_item");
+        result.setActualDataNodes("demo_ds_${0..1}.t_order_item_${[0, 1]}");
+        return result;
+    }
+    
+    private static Map<String, DataSource> createDataSourceMap() {
+        Map<String, DataSource> result = new HashMap<>();
+        result.put("demo_ds_0", DataSourceUtil.createDataSource("demo_ds_0"));
+        result.put("demo_ds_1", DataSourceUtil.createDataSource("demo_ds_1"));
+        return result;
+    }
+}
---
+++ sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/yaml/ShardingAndMasterSlaveTogether.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.example.jdbc.main.yaml;
+
+import io.shardingsphere.core.api.ShardingDataSourceFactory;
+import io.shardingsphere.example.jdbc.fixture.DataRepository;
+
+import javax.sql.DataSource;
+import java.io.File;
+
+/*
+ * Please make sure master-slave data sync on MySQL is running correctly. Otherwise this example will query empty data from slave.
+ */
+public class ShardingAndMasterSlaveTogether {
+    
+    public static void main(final String[] args) throws Exception {
+        DataSource dataSource = ShardingDataSourceFactory.createDataSource(new File(ShardingAndMasterSlaveTogether.class.getResource("/META-INF/sharding-master-slave.yaml").getFile()));
+        new DataRepository(dataSource).demo();
+    }
+}
