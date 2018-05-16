4a5a48ca845e2bee30a3b6a715a2e34e9f4663bf
cherrylzhao <zhaojunv3@126.com>
2018-05-16 10:39:11 +0800

    add junit-test for sharding-proxy

---
+++ sharding-proxy/src/test/java/io/shardingsphere/proxy/config/RuleRegistryTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.proxy.config;
+
+import io.shardingsphere.core.rule.MasterSlaveRule;
+import io.shardingsphere.proxy.metadata.ProxyShardingMetaData;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Test class for RuleRegistry.
+ *
+ * @author zhaojun
+ */
+public class RuleRegistryTest {
+
+    @Test
+    public void assertGetInstance() {
+//        RuleRegistry instance = RuleRegistry.getInstance();
+//        assertThat(instance.getDataSourceMap(), instanceOf(Map.class));
+//        assertThat(instance.getMasterSlaveRule(), instanceOf(MasterSlaveRule.class));
+//        assertThat(instance.getShardingMetaData(), instanceOf(ProxyShardingMetaData.class));
+//        assertThat(instance.isOnlyMasterSlave(), instanceOf(Boolean.class));
+    }
+
+}
