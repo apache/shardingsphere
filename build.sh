cec7a8872cf66a37ff27f7e3db9c883f4f4506ca
tristaZero <yezhiji1991@163.com>
2018-05-28 12:56:02 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -19,12 +19,9 @@ hugo
 find ./ -name '*.html' -exec sed -i -e 's|[[:space:]]*<option id="\([a-zA-Z]\+\)" value="|<option id="\1" value="/document/current|g' {} \;
 
 cd public/en
-sed -i -e 's/cn/current\/en/g' index.html
+sed -i -e 's/cn/en/g' index.html
 cd ../..
 
-cd public/cn
-sed -i -e 's/cn/current\/cn/g' index.html
-cd ../..
 
 cd ..
 cd ..
@@ -32,15 +29,18 @@ mv document/current/public target/document/current
 
 mkdir target/community
 
-cd community/cn
+cd community/
 hugo
-cd ..
-cd ..
-mv community/cn/public target/community/cn
 
-cd community/en
-hugo
+find ./ -name '*.html' -exec sed -i -e 's|[[:space:]]*<option id="\([a-zA-Z]\+\)" value="|<option id="\1" value="/community|g' {} \;
+
+cd public/en
+sed -i -e 's/cn/en/g' index.html
+cd ../..
+
 cd ..
-cd ..
-mv community/en/public target/community/en
+mv community/public/* target/community/
+
+
+
 
