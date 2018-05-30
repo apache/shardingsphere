7488fb96bae25ec769e32404a2f8562bd48e369a
tuohai666 <tuohaibbb@gmail.com>
2018-05-30 20:23:45 +0800

    for #888 check if write buffer is writable before write

--- sharding-proxy/src/main/java/io/shardingsphere/proxy/frontend/mysql/MySQLFrontendHandler.java
+++ sharding-proxy/src/main/java/io/shardingsphere/proxy/frontend/mysql/MySQLFrontendHandler.java
@@ -77,6 +77,9 @@ public final class MySQLFrontendHandler extends FrontendHandler {
                         context.writeAndFlush(each);
                     }
                     while (commandPacket.hasMoreResultValue()) {
+                        while (!context.channel().isWritable()) {
+                            continue;
+                        }
                         context.writeAndFlush(commandPacket.getResultValue());
                     }
                 } finally {
