6c4233396e5b483145836b965fbcd6156b0e4e19
tuohai666 <tuohaibbb@gmail.com>
2018-07-09 18:29:40 +0800

    for #986, refine SagaPostRequest

--- sharding-jdbc-transaction-saga/src/main/java/io/shardingsphere/transaction/saga/util/SagaPostRequest.java
+++ sharding-jdbc-transaction-saga/src/main/java/io/shardingsphere/transaction/saga/util/SagaPostRequest.java
@@ -3,8 +3,8 @@ package io.shardingsphere.transaction.saga.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -12,8 +12,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -23,126 +21,53 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-@NoArgsConstructor
 @Getter
 @Setter
+@Slf4j
 public class SagaPostRequest {
-    private static Logger logger = LoggerFactory.getLogger(SagaPostRequest.class);
-    private static SagaPostRequest instance = new SagaPostRequest();
-    private String ip;
-    private int port;
-    private String username;
-    private String password;
+    
+    private static final SagaPostRequest INSTANCE = new SagaPostRequest();
+    
+    private final String ip = "127.0.0.1";
+    
+    private final int port = 8083;
+    
     private OkHttpClient okHttpClient;
     
-    public static SagaPostRequest getInstance() {
-        return instance;
-    }
-    
-    public void initOkHttpClient() {
-        // 初始化OkHttpClient, 一个实例的请求共享连接池
-        Proxy proxy = null;
-        if (ip != null && port != 0) {
-            InetSocketAddress addr = new InetSocketAddress(ip, port);
-            proxy = new Proxy(Proxy.Type.HTTP, addr);
-        }
-        
+    public void SagaPostRequest() {
+        InetSocketAddress address = new InetSocketAddress(ip, port);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
         try {
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
             builder.retryOnConnectionFailure(true);
             builder.connectTimeout(30, TimeUnit.SECONDS);
             builder.writeTimeout(30, TimeUnit.SECONDS);
             builder.readTimeout(30, TimeUnit.SECONDS);
-            if (proxy != null) {
-                builder.proxy(proxy);
-            }
+            builder.proxy(proxy);
             okHttpClient = builder.build();
-        } catch (Exception e) {
-            logger.error("initOkHttpClient() Error : " + e);
+        } catch (final Exception ex) {
+            log.error("SagaPostRequest() Error: " + ex);
         }
     }
     
-    private Map<String, Map<String, String>> request(String url, Map<String, String> header, String body, String bodyCharset) throws IOException {
-        Map<String, Map<String, String>> result = new HashMap<>();
+    public static SagaPostRequest getInstance() {
+        return INSTANCE;
+    }
+    
+    private String request(String body) {
+        String url = "127.0.0.1:8083/requests/";
+        String result = "";
         Request.Builder builder = new Request.Builder();
         builder.url(url);
         builder.header("Connection", "close");
-        Response response = null;
-        MediaType mediaType;
-        
-        // 添加headers
-        for (Map.Entry<String, String> hd : header.entrySet()) {
-            builder.addHeader(hd.getKey(), hd.getValue());
-        }
-        
-        if (StringUtils.isEmpty(body)) {
-            // 没有body get请求
-            Request request = builder.build();
-            try {
-                logger.info("Get: " + url);
-                response = getOkHttpClient().newCall(request).execute();
-            } catch (Exception e) {
-                logger.error("Get: error: ", e);
-                if (response != null)
-                    response.close();
-                throw new IOException();
-            }
-        } else {
-            // 有请求body体 post请求
-            String code = bodyCharset;
-            if (StringUtils.isBlank(code)) {
-                code = "utf-8";
-            }
-            
-            JSONObject json = null;
-            try {
-                json = JSON.parseObject(body);
-            } catch (Exception e) {
-                // logger.error("JSON.parseObject(body) error!");
-            }
-            
-            if (json != null) {
-                mediaType = MediaType.parse("application/json; charset=" + code);
-            } else {
-                mediaType = MediaType.parse("application/x-www-form-urlencoded; charset=" + code);
-            }
-            
-            Request request = builder.post(RequestBody.create(mediaType, body)).build();
-            try {
-                logger.info("Post: " + url);
-                response = getOkHttpClient().newCall(request).execute();
-            } catch (Exception e) {
-                logger.error("Post: error: ", e);
-                if (response != null)
-                    response.close();
-                throw new IOException();
-            }
-        }
-        
-        if (response != null) {
-            if (!(response.code() >= 200 && response.code() < 300)) {
-                logger.error("HTTP Status Code: " + response.code());
-                response.close();
-                throw new IOException();
-            }
-            
+        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
+        Request request = builder.post(RequestBody.create(mediaType, body)).build();
+        log.info("Post: " + url);
+        try (Response response = getOkHttpClient().newCall(request).execute()) {
             byte[] contentByte = response.body().bytes();
-            String contentString = new String(contentByte);
-            
-            // 添加content
-            Map<String, String> content = new HashMap<>();
-            content.put("content", contentString);
-            result.put("content", content);
-            // 添加headers
-            Map<String, String> hdrs = new HashMap<>();
-            Headers resHeaders = response.headers();
-            Set<String> keys = resHeaders.names();
-            for (String key : keys) {
-                for (String value : resHeaders.values(key)) {
-                    hdrs.put(key, value);
-                }
-            }
-            result.put("headers", hdrs);
+            result = new String(contentByte);
+        } catch (final Exception ex) {
+            log.error("Post: error: ", ex);
         }
         return result;
     }
