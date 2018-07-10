d702b6882eaa28bb315df0759ffe547501ee84af
tuohai666 <tuohaibbb@gmail.com>
2018-07-10 11:08:46 +0800

    for #986, add SagaApi

---
+++ sharding-jdbc-transaction-saga/src/main/java/io/shardingsphere/transaction/saga/api/SagaApi.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.transaction.saga.api;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Saga Api.
+ * @see <a href="https://github.com/apache/incubator-servicecomb-saga/blob/old-saga/docs/api/api.md">Saga API</a>
+ *
+ * @author zhangyonglun
+ */
+@AllArgsConstructor
+public class SagaApi {
+    
+    private final String policy;
+    
+    private List<Request> requests;
+}
