7461baac3e9184826aee87055b9a3bf5d6b3c418
tristaZero <yezhiji1991@163.com>
2018-07-13 20:17:27 +0800

    USE getAuthorityEnvironmentManager

--- sharding-jdbc/src/test/java/io/shardingsphere/dbtest/engine/dcl/BaseDCLIntegrateTest.java
+++ sharding-jdbc/src/test/java/io/shardingsphere/dbtest/engine/dcl/BaseDCLIntegrateTest.java
@@ -17,33 +17,25 @@
 
 package io.shardingsphere.dbtest.engine.dcl;
 
-import com.google.common.base.Strings;
-import io.shardingsphere.core.constant.DatabaseType;
 import io.shardingsphere.dbtest.cases.assertion.dcl.DCLIntegrateTestCaseAssertion;
 import io.shardingsphere.dbtest.engine.SingleIntegrateTest;
 import io.shardingsphere.dbtest.env.DatabaseTypeEnvironment;
 import io.shardingsphere.dbtest.env.EnvironmentPath;
 import io.shardingsphere.dbtest.env.dataset.DataSetEnvironmentManager;
 import io.shardingsphere.test.sql.SQLCaseType;
+import org.junit.After;
 import org.junit.Before;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
-import java.util.Collection;
-import java.util.LinkedList;
 
 public abstract class BaseDCLIntegrateTest extends SingleIntegrateTest {
-    
-    private final DCLIntegrateTestCaseAssertion assertion;
     
     public BaseDCLIntegrateTest(final String sqlCaseId, final String path, final DCLIntegrateTestCaseAssertion assertion, final String shardingRuleType,
                                 final DatabaseTypeEnvironment databaseTypeEnvironment, final SQLCaseType caseType) throws IOException, JAXBException, SQLException, ParseException {
         super(sqlCaseId, path, assertion, shardingRuleType, databaseTypeEnvironment, caseType);
-        this.assertion = assertion;
     }
     
     @Before
@@ -51,42 +43,11 @@ public abstract class BaseDCLIntegrateTest extends SingleIntegrateTest {
         if (getDatabaseTypeEnvironment().isEnabled()) {
             new DataSetEnvironmentManager(EnvironmentPath.getDataInitializeResourceFile(getShardingRuleType()), getDataSourceMap()).initialize();
         }
+        getAuthorityEnvironmentManager().initialize();
     }
     
-    protected void cleanEnvironment(final Connection connection) {
-        if (!Strings.isNullOrEmpty(assertion.getCleanSQLs())) {
-            try (Statement statement = connection.createStatement()) {
-                for (String each : assertion.getCleanSQLs().split(";")) {
-                    statement.execute(each);
-                }
-                // CHECKSTYLE: OFF
-            } catch (final SQLException ignored) {
-                // CHECKSTYLE: ON
-            }
-        }
-    }
-    
-    protected void initEnvironment(final Connection connection) {
-        if (!Strings.isNullOrEmpty(assertion.getInitSQLs())) {
-            try (Statement statement = connection.createStatement()) {
-                for (String each : assertion.getInitSQLs().split(";")) {
-                    statement.execute(each);
-                }
-                // CHECKSTYLE: OFF
-            } catch (final SQLException ignored) {
-                // CHECKSTYLE: ON
-            }
-        }
-    }
-    
-    protected boolean isExecuted() {
-        if (Strings.isNullOrEmpty(assertion.getDbTypes())) {
-            return true;
-        }
-        Collection<DatabaseType> databaseTypeList = new LinkedList<>();
-        for (String each : assertion.getDbTypes().split(",")) {
-            databaseTypeList.add(DatabaseType.valueOf(each));
-        }
-        return databaseTypeList.contains(getDatabaseTypeEnvironment().getDatabaseType());
+    @After
+    public void cleanData() {
+        getAuthorityEnvironmentManager().clean();
     }
 }
