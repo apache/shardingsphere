7a1d67558ae15d24f402a1f3ee26b44e027fb2d1
tristaZero <hannkenn@163.com>
2018-07-16 12:09:25 +0800

    add getActualDataSourceNameByActualTableName

--- sharding-core/src/main/java/io/shardingsphere/core/rule/ShardingRule.java
+++ sharding-core/src/main/java/io/shardingsphere/core/rule/ShardingRule.java
@@ -24,6 +24,7 @@ import io.shardingsphere.core.api.config.MasterSlaveRuleConfiguration;
 import io.shardingsphere.core.api.config.ShardingRuleConfiguration;
 import io.shardingsphere.core.api.config.TableRuleConfiguration;
 import io.shardingsphere.core.exception.ShardingConfigurationException;
+import io.shardingsphere.core.exception.ShardingException;
 import io.shardingsphere.core.keygen.DefaultKeyGenerator;
 import io.shardingsphere.core.keygen.KeyGenerator;
 import io.shardingsphere.core.parsing.parser.context.condition.Column;
@@ -375,5 +376,22 @@ public final class ShardingRule {
             }
         }
         return Optional.absent();
+    }
+    
+    /**
+     * Get actual data source name by actual table name.
+     *
+     * @param actualTableName actual table name
+     * @return actual data source name
+     */
+    public String getActualDataSourceNameByActualTableName(final String actualTableName) {
+        Optional<TableRule> tableRule = tryFindTableRuleByActualTable(actualTableName);
+        if (tableRule.isPresent()) {
+            return tableRule.get().getActualDatasourceNames().iterator().next();
+        }
+        if (!Strings.isNullOrEmpty(getShardingDataSourceNames().getDefaultDataSourceName())) {
+            return getShardingDataSourceNames().getDefaultDataSourceName();
+        }
+        throw new ShardingException("Cannot found actual schema name of '%s' in sharding rule.", actualTableName);
     }
 }
