e589c822c86f6fa1e0880601557b8c024eb1c2a3
tristaZero <hannkenn@163.com>
2018-07-30 17:27:38 +0800

    modify matcher.group(1) for h2

--- sharding-core/src/main/java/io/shardingsphere/core/metadata/datasource/dialect/H2DataSourceMetaData.java
+++ sharding-core/src/main/java/io/shardingsphere/core/metadata/datasource/dialect/H2DataSourceMetaData.java
@@ -45,9 +45,9 @@ public final class H2DataSourceMetaData implements DataSourceMetaData {
     public H2DataSourceMetaData(final String url) {
         Matcher matcher = pattern.matcher(url);
         if (matcher.find()) {
-            hostName = matcher.group(0);
+            hostName = matcher.group(1);
             port = DEFAULT_PORT;
-            schemeName = matcher.group(1);
+            schemeName = matcher.group(2);
         } else {
             throw new ShardingException("The URL of JDBC is not supported.");
         }
