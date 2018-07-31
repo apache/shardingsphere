816a0cdcd8313103afd084094459663b3bce32fe
tristaZero <hannkenn@163.com>
2018-07-31 16:53:00 +0800

    add HandshakePacketTest.java

---
+++ sharding-proxy/src/test/java/io/shardingsphere/proxy/transport/mysql/packet/handshake/HandshakePacketTest.java
@@ -0,0 +1,51 @@
+package io.shardingsphere.proxy.transport.mysql.packet.handshake;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HandshakePacketTest {
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @Test
+    public void testWrite() {
+    }
+    
+    @Test
+    public void testGetProtocolVersion() {
+    }
+    
+    @Test
+    public void testGetServerVersion() {
+    }
+    
+    @Test
+    public void testGetCapabilityFlagsLower() {
+    }
+    
+    @Test
+    public void testGetCharacterSet() {
+    }
+    
+    @Test
+    public void testGetStatusFlag() {
+    }
+    
+    @Test
+    public void testGetCapabilityFlagsUpper() {
+    }
+    
+    @Test
+    public void testGetSequenceId() {
+    }
+    
+    @Test
+    public void testGetConnectionId() {
+    }
+    
+    @Test
+    public void testGetAuthPluginData() {
+    }
+}
