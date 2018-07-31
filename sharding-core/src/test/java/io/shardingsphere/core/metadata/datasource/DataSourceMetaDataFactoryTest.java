2431d99793a4e2580f1f79a3e9007d2d83dcd8a8
tristaZero <hannkenn@163.com>
2018-07-31 11:45:51 +0800

    add test for oracle

--- sharding-core/src/test/java/io/shardingsphere/core/metadata/datasource/DataSourceMetaDataFactoryTest.java
+++ sharding-core/src/test/java/io/shardingsphere/core/metadata/datasource/DataSourceMetaDataFactoryTest.java
@@ -3,6 +3,7 @@ package io.shardingsphere.core.metadata.datasource;
 import io.shardingsphere.core.constant.DatabaseType;
 import io.shardingsphere.core.metadata.datasource.dialect.H2DataSourceMetaData;
 import io.shardingsphere.core.metadata.datasource.dialect.MySQLDataSourceMetaData;
+import io.shardingsphere.core.metadata.datasource.dialect.OracleDataSourceMetaData;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -17,5 +18,10 @@ public class DataSourceMetaDataFactoryTest {
     @Test
     public void assertAllNewInstanceForMySQL() {
         assertTrue((DataSourceMetaDataFactory.newInstance(DatabaseType.MySQL, "jdbc:mysql://127.0.0.1:3306/db_0") instanceof MySQLDataSourceMetaData));
+    }
+    
+    @Test
+    public void assertAllNewInstanceForOracle() {
+        assertTrue((DataSourceMetaDataFactory.newInstance(DatabaseType.Oracle, "jdbc:oracle:thin:@//127.0.0.1:3306/ds_0") instanceof OracleDataSourceMetaData));
     }
 }
