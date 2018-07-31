970697543f95295f7332ff6c781372fa8712b6c0
tristaZero <hannkenn@163.com>
2018-07-31 17:40:43 +0800

    use import static junit.framework.TestCase.assertTrue

--- sharding-proxy/src/test/java/io/shardingsphere/proxy/transport/mysql/packet/handshake/HandshakePacketTest.java
+++ sharding-proxy/src/test/java/io/shardingsphere/proxy/transport/mysql/packet/handshake/HandshakePacketTest.java
@@ -26,7 +26,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class HandshakePacketTest {
