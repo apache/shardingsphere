fda6129b1e37d31aa5fc6fadaae12edbd13cc318
tristaZero <hannkenn@163.com>
2018-08-26 09:35:42 +0800

    add getConnection function

--- sharding-jdbc-orchestration/src/main/java/io/shardingsphere/jdbc/orchestration/internal/OrchestrationShardingDataSource.java
+++ sharding-jdbc-orchestration/src/main/java/io/shardingsphere/jdbc/orchestration/internal/OrchestrationShardingDataSource.java
@@ -17,13 +17,21 @@
 
 package io.shardingsphere.jdbc.orchestration.internal;
 
+import io.shardingsphere.core.api.config.MasterSlaveRuleConfiguration;
 import io.shardingsphere.core.api.config.ShardingRuleConfiguration;
+import io.shardingsphere.core.jdbc.adapter.AbstractDataSourceAdapter;
+import io.shardingsphere.core.jdbc.core.connection.ShardingConnection;
+import io.shardingsphere.core.jdbc.core.datasource.MasterSlaveDataSource;
 import io.shardingsphere.core.jdbc.core.datasource.ShardingDataSource;
+import io.shardingsphere.core.rule.MasterSlaveRule;
 import io.shardingsphere.core.rule.ShardingRule;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Properties;
 
@@ -33,38 +41,62 @@ import java.util.Properties;
  * @author caohao
  */
 @Slf4j
-public final class OrchestrationShardingDataSource extends ShardingDataSource {
+public final class OrchestrationShardingDataSource extends AbstractDataSourceAdapter implements AutoCloseable{
+    
+    private final ShardingDataSource dataSource;
     
     private final OrchestrationFacade orchestrationFacade;
     
-    private final Map<String, DataSource> dataSourceMap;
-    
-    private final ShardingRuleConfiguration shardingRuleConfig;
-    
-    private final Map<String, Object> configMap;
-    
-    private final Properties props;
     
     public OrchestrationShardingDataSource(final Map<String, DataSource> dataSourceMap, final ShardingRuleConfiguration shardingRuleConfig,
                                            final Map<String, Object> configMap, final Properties props, final OrchestrationFacade orchestrationFacade) throws SQLException {
-        super(getRawDataSourceMap(dataSourceMap), new ShardingRule(getShardingRuleConfiguration(dataSourceMap, shardingRuleConfig), getRawDataSourceMap(dataSourceMap).keySet()), configMap, props);
+        this.dataSource = new ShardingDataSource(getRawDataSourceMap(dataSourceMap), new ShardingRule(getShardingRuleConfiguration(dataSourceMap, shardingRuleConfig), getRawDataSourceMap(dataSourceMap).keySet()), configMap, props);
         this.orchestrationFacade = orchestrationFacade;
-        this.dataSourceMap = getRawDataSourceMap(dataSourceMap);
-        this.shardingRuleConfig = getShardingRuleConfiguration(dataSourceMap, shardingRuleConfig);
-        this.configMap = configMap;
-        this.props = props;
+        this.orchestrationFacade.init(dataSourceMap, shardingRuleConfig, configMap, props);
     }
     
-    /**
-     * Initialize for sharding orchestration.
-     */
-    public void init() {
-        orchestrationFacade.init(dataSourceMap, shardingRuleConfig, configMap, props);
+    @Override
+    public final ShardingConnection getConnection() {
+        return dataSource.getConnection();
     }
     
     @Override
     public void close() {
-        super.close();
+        dataSource.close();
         orchestrationFacade.close();
+    }
+    
+    private Map<String, DataSource> getRawDataSourceMap(final Map<String, DataSource> dataSourceMap) {
+        Map<String, DataSource> result = new LinkedHashMap<>();
+        if (null == dataSourceMap) {
+            return result;
+        }
+        for (Map.Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
+            String dataSourceName = entry.getKey();
+            DataSource dataSource = entry.getValue();
+            if (dataSource instanceof MasterSlaveDataSource) {
+                result.putAll(((MasterSlaveDataSource) dataSource).getAllDataSources());
+            } else {
+                result.put(dataSourceName, dataSource);
+            }
+        }
+        return result;
+    }
+    
+    private ShardingRuleConfiguration getShardingRuleConfiguration(final Map<String, DataSource> dataSourceMap, final ShardingRuleConfiguration shardingRuleConfig) {
+        Collection<MasterSlaveRuleConfiguration> masterSlaveRuleConfigs = new LinkedList<>();
+        if (null == dataSourceMap || !shardingRuleConfig.getMasterSlaveRuleConfigs().isEmpty()) {
+            return shardingRuleConfig;
+        }
+        for (DataSource each : dataSourceMap.values()) {
+            if (!(each instanceof MasterSlaveDataSource)) {
+                continue;
+            }
+            MasterSlaveRule masterSlaveRule = ((MasterSlaveDataSource) each).getMasterSlaveRule();
+            masterSlaveRuleConfigs.add(new MasterSlaveRuleConfiguration(
+                    masterSlaveRule.getName(), masterSlaveRule.getMasterDataSourceName(), masterSlaveRule.getSlaveDataSourceNames(), masterSlaveRule.getLoadBalanceAlgorithm()));
+        }
+        shardingRuleConfig.setMasterSlaveRuleConfigs(masterSlaveRuleConfigs);
+        return shardingRuleConfig;
     }
 }
