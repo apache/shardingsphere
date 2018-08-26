1733f38895f079da72f17f5f4c1ffb0fba4ba772
tristaZero <hannkenn@163.com>
2018-08-26 08:49:59 +0800

    add properties.

--- sharding-core/src/main/java/io/shardingsphere/core/event/orche/config/MasterSlaveConfigurationEventBusEvent.java
+++ sharding-core/src/main/java/io/shardingsphere/core/event/orche/config/MasterSlaveConfigurationEventBusEvent.java
@@ -23,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 import javax.sql.DataSource;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Master slave config event bus event.
@@ -36,4 +37,6 @@ public final class MasterSlaveConfigurationEventBusEvent {
     private final Map<String, DataSource> dataSourceMap;
     
     private final MasterSlaveRuleConfiguration masterSlaveRuleConfig;
+    
+    private final Properties props;
 }
