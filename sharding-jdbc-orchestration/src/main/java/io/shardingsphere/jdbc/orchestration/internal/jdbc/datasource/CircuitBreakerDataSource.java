6e6ef255f40f8883ba4be81b680c83b36fdbc0b5
tristaZero <hannkenn@163.com>
2018-08-27 15:08:28 +0800

    move CircuitBreakerDataSource.java

--- sharding-jdbc-orchestration/src/main/java/io/shardingsphere/jdbc/orchestration/internal/jdbc/datasource/CircuitBreakerDataSource.java
+++ sharding-jdbc-orchestration/src/main/java/io/shardingsphere/jdbc/orchestration/internal/jdbc/datasource/CircuitBreakerDataSource.java
@@ -15,10 +15,10 @@
  * </p>
  */
 
-package io.shardingsphere.core.orche.datasource;
+package io.shardingsphere.jdbc.orchestration.internal.jdbc.datasource;
 
 import io.shardingsphere.core.jdbc.unsupported.AbstractUnsupportedOperationDataSource;
-import io.shardingsphere.core.orche.connection.CircuitBreakerConnection;
+import io.shardingsphere.jdbc.orchestration.internal.jdbc.connection.CircuitBreakerConnection;
 
 import java.io.PrintWriter;
 import java.sql.Connection;
