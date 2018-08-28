76caea26aed6a49563305dbd31284d3cea951eb2
terrymanu <terrymanu@163.com>
2018-08-28 18:41:48 +0800

    refactor QueryResponsePacketsTest

--- sharding-proxy/src/test/java/io/shardingsphere/proxy/transport/mysql/packet/command/query/QueryResponsePacketsTest.java
+++ sharding-proxy/src/test/java/io/shardingsphere/proxy/transport/mysql/packet/command/query/QueryResponsePacketsTest.java
@@ -23,11 +23,21 @@ import io.shardingsphere.proxy.transport.mysql.packet.generic.EofPacket;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public final class QueryResponsePacketsTest {
+    
+    @Test
+    public void assertGetColumnDefinition41Packets() {
+        QueryResponsePackets actual = createQueryResponsePackets();
+        assertThat(actual.getColumnDefinition41Packets().size(), is(2));
+        Iterator<ColumnDefinition41Packet> actualColumnDefinition41Packets = actual.getColumnDefinition41Packets().iterator();
+        assertThat(actualColumnDefinition41Packets.next().getSequenceId(), is(2));
+        assertThat(actualColumnDefinition41Packets.next().getSequenceId(), is(3));
+    }
     
     @Test
     public void assertGetColumnCount() {
