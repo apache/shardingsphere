b12a61f48d0f20fe75e1ad32f6bcbfe71d4d3f9d
KomachiSion <263976490@qq.com>
2018-08-31 09:39:45 +0800

    implement saga SQLTransport for sharding-jdbc

---
+++ sharding-jdbc/src/main/java/io/shardingsphere/core/transport/JDBCSqlTransport.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.core.transport;
+
+import io.shardingsphere.core.jdbc.core.connection.ShardingConnection;
+import io.shardingsphere.transaction.manager.base.servicecomb.AbstractSQLTransport;
+import lombok.Setter;
+import org.apache.servicecomb.saga.core.TransportFailedException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * JDBC SQLTransport implement.
+ *
+ * @author yangyi
+ */
+public final class JDBCSqlTransport extends AbstractSQLTransport {
+    
+    @Setter
+    private ShardingConnection shardingConnection;
+    
+    @Override
+    protected Connection getConnection(final String datasource) throws TransportFailedException {
+        try {
+            return shardingConnection.getConnection(datasource);
+        } catch (SQLException e) {
+            throw new TransportFailedException("get connection of [" + datasource + "] occur exception ", e);
+        }
+    }
+    
+    /**
+     * Renew sharding connection.
+     *
+     * @param shardingConnection new sharding connection.
+     * @throws SQLException sql exception
+     */
+    public void renew(final ShardingConnection shardingConnection) throws SQLException {
+        this.shardingConnection.close();
+        this.shardingConnection = shardingConnection;
+    }
+}
