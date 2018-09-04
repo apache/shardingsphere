f52a87aac79f85be5cbd54b628ae35aaafe8340c
tristaZero <hannkenn@163.com>
2018-09-04 11:17:58 +0800

    add OrchestrationSpringMasterSlaveDataSource()

--- sharding-orchestration/sharding-jdbc-orchestration-spring/sharding-jdbc-orchestration-spring-namespace/src/main/java/io/shardingsphere/jdbc/orchestration/spring/datasource/OrchestrationSpringMasterSlaveDataSource.java
+++ sharding-orchestration/sharding-jdbc-orchestration-spring/sharding-jdbc-orchestration-spring-namespace/src/main/java/io/shardingsphere/jdbc/orchestration/spring/datasource/OrchestrationSpringMasterSlaveDataSource.java
@@ -35,5 +35,9 @@ public class OrchestrationSpringMasterSlaveDataSource extends OrchestrationMaste
     public OrchestrationSpringMasterSlaveDataSource(final DataSource dataSource, final OrchestrationConfiguration orchestrationConfig) throws SQLException {
         super((MasterSlaveDataSource) dataSource, new OrchestrationFacade(orchestrationConfig));
     }
+    
+    public OrchestrationSpringMasterSlaveDataSource(final OrchestrationConfiguration orchestrationConfig) throws SQLException {
+        super(new OrchestrationFacade(orchestrationConfig));
+    }
 }
 
