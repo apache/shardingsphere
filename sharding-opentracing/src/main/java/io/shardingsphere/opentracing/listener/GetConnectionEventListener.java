6af3a30233b47a68cddb041b99dba43d8b3a992f
tuohai666 <tuohaibbb@gmail.com>
2018-09-07 18:47:54 +0800

    #1172, refine

--- sharding-opentracing/src/main/java/io/shardingsphere/opentracing/listener/GetConnectionEventListener.java
+++ sharding-opentracing/src/main/java/io/shardingsphere/opentracing/listener/GetConnectionEventListener.java
@@ -36,8 +36,6 @@ public final class GetConnectionEventListener extends OpenTracingListener<GetCon
     
     private final ThreadLocal<Span> branchSpan = new ThreadLocal<>();
 
-//    private final ThreadLocal<ActiveSpan> trunkInBranchSpan = new ThreadLocal<>();
-    
     /**
      * Listen getConnection event.
      *
@@ -51,14 +49,8 @@ public final class GetConnectionEventListener extends OpenTracingListener<GetCon
     
     @Override
     protected void beforeExecute(final GetConnectionEvent event) {
-//        if (ExecutorDataMap.getDataMap().containsKey(OverallExecuteEventListener.OVERALL_SPAN_CONTINUATION) && !OverallExecuteEventListener.isTrunkThread() && null == branchSpan.get()) {
-//            trunkInBranchSpan.set(((ActiveSpan.Continuation) ExecutorDataMap.getDataMap().get(OverallExecuteEventListener.OVERALL_SPAN_CONTINUATION)).activate());
-//        }
-//        trunkInBranchSpan.set(((ActiveSpan.Continuation) ExecutorDataMap.getDataMap().get(OverallExecuteEventListener.OVERALL_SPAN_CONTINUATION)).activate());
-//        if (null == branchSpan.get()) {
         branchSpan.set(ShardingTracer.get().buildSpan(OPERATION_NAME_PREFIX)
             .withTag(Tags.COMPONENT.getKey(), ShardingTags.COMPONENT_NAME).withTag(Tags.DB_INSTANCE.getKey(), event.getDataSource()).startManual());
-//        }
     }
     
     @Override
@@ -68,11 +60,6 @@ public final class GetConnectionEventListener extends OpenTracingListener<GetCon
         }
         branchSpan.get().finish();
         branchSpan.remove();
-//        if (null == trunkInBranchSpan.get()) {
-//            return;
-//        }
-//        trunkInBranchSpan.get().deactivate();
-//        trunkInBranchSpan.remove();
     }
     
     @Override
