d1ca207c3f59d567837011a23d830bc0fec7e195
tristaZero <hannkenn@163.com>
2018-09-13 10:21:29 +0800

    add author

--- sharding-jdbc/src/main/java/io/shardingsphere/core/jdbc/adapter/AbstractConnectionAdapter.java
+++ sharding-jdbc/src/main/java/io/shardingsphere/core/jdbc/adapter/AbstractConnectionAdapter.java
@@ -43,6 +43,7 @@ import java.util.Map;
  * Adapter for {@code Connection}.
  *
  * @author zhangliang
+ * @author panjuan
  */
 public abstract class AbstractConnectionAdapter extends AbstractUnsupportedOperationConnection {
     
