1b86228ff10bbeae839eb0796694b43cba6d825e
tuohai666 <tuohaibbb@gmail.com>
2018-09-13 18:07:37 +0800

    #1172, for comment: refine get connection event mechanism

--- sharding-opentracing/src/main/java/io/shardingsphere/opentracing/listener/GetConnectionEventListener.java
+++ sharding-opentracing/src/main/java/io/shardingsphere/opentracing/listener/GetConnectionEventListener.java
@@ -22,6 +22,8 @@ import com.google.common.eventbus.Subscribe;
 import io.opentracing.Span;
 import io.opentracing.tag.Tags;
 import io.shardingsphere.core.event.connection.GetConnectionEvent;
+import io.shardingsphere.core.event.connection.GetConnectionFinishEvent;
+import io.shardingsphere.core.event.connection.GetConnectionStartEvent;
 import io.shardingsphere.opentracing.ShardingTags;
 import io.shardingsphere.opentracing.ShardingTracer;
 
@@ -48,7 +50,7 @@ public final class GetConnectionEventListener extends OpenTracingListener<GetCon
     @Override
     protected void beforeExecute(final GetConnectionEvent event) {
         getSpan().set(ShardingTracer.get().buildSpan(OPERATION_NAME_PREFIX).withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT)
-            .withTag(Tags.COMPONENT.getKey(), ShardingTags.COMPONENT_NAME).withTag(Tags.DB_INSTANCE.getKey(), event.getDataSource()).startManual());
+            .withTag(Tags.COMPONENT.getKey(), ShardingTags.COMPONENT_NAME).withTag(Tags.DB_INSTANCE.getKey(), ((GetConnectionStartEvent) event).getDataSource()).startManual());
     }
     
     @Override
@@ -56,7 +58,7 @@ public final class GetConnectionEventListener extends OpenTracingListener<GetCon
         if (null == getSpan().get()) {
             return;
         }
-        getSpan().get().setTag(Tags.PEER_HOSTNAME.getKey(), event.getUrl().split("//")[1].split("/")[0]);
+        getSpan().get().setTag(Tags.PEER_HOSTNAME.getKey(), ((GetConnectionFinishEvent) event).getUrl().split("//")[1].split("/")[0]);
         getSpan().get().finish();
         getSpan().remove();
     }
