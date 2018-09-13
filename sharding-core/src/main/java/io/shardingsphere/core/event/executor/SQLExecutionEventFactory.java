c252b527a2367b976b816b1cee5577e7e54318ad
tuohai666 <tuohaibbb@gmail.com>
2018-09-13 21:23:23 +0800

    #1172, fix test case

--- sharding-core/src/main/java/io/shardingsphere/core/event/executor/SQLExecutionEventFactory.java
+++ sharding-core/src/main/java/io/shardingsphere/core/event/executor/SQLExecutionEventFactory.java
@@ -39,15 +39,16 @@ public final class SQLExecutionEventFactory {
      * @param sqlType        SQL type
      * @param sqlExecuteUnit SQL execute unit
      * @param parameters     parameters
+     * @param url            url
      * @return SQL execution event
      */
-    public static SQLExecutionEvent createEvent(final SQLType sqlType, final SQLExecuteUnit sqlExecuteUnit, final List<Object> parameters) {
+    public static SQLExecutionEvent createEvent(final SQLType sqlType, final SQLExecuteUnit sqlExecuteUnit, final List<Object> parameters, final String url) {
         if (SQLType.DQL == sqlType) {
-            return new DQLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters);
+            return new DQLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters, url);
         }
         if (SQLType.DML == sqlType) {
-            return new DMLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters);
+            return new DMLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters, url);
         }
-        return new SQLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters);
+        return new SQLExecutionEvent(sqlExecuteUnit.getRouteUnit(), parameters, url);
     }
 }
