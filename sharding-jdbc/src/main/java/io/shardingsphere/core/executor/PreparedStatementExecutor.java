09ffb474a75cd09ca1bf9e341c0ea57fd2309eac
tristaZero <hannkenn@163.com>
2018-09-17 19:24:30 +0800

    import java.util.concurrent.CopyOnWriteArrayList;

--- sharding-jdbc/src/main/java/io/shardingsphere/core/executor/PreparedStatementExecutor.java
+++ sharding-jdbc/src/main/java/io/shardingsphere/core/executor/PreparedStatementExecutor.java
@@ -40,7 +40,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Prepared statement executor.
