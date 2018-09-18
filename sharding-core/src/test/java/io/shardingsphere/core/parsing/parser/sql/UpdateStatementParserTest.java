40eed4e0e34213408e1621f39463014018bdc1b8
caohao <caohao@huobi.com>
2018-09-18 13:49:17 +0800

    for #1216, move the UpdateStatementParserTest relative test cases to IntegrateParsingTest for oracle special syntax 11th.

--- sharding-core/src/test/java/io/shardingsphere/core/parsing/parser/sql/UpdateStatementParserTest.java
+++ sharding-core/src/test/java/io/shardingsphere/core/parsing/parser/sql/UpdateStatementParserTest.java
@@ -17,57 +17,19 @@
 
 package io.shardingsphere.core.parsing.parser.sql;
 
-import com.google.common.collect.Range;
-import io.shardingsphere.core.api.algorithm.sharding.ListShardingValue;
-import io.shardingsphere.core.api.algorithm.sharding.RangeShardingValue;
 import io.shardingsphere.core.constant.DatabaseType;
-import io.shardingsphere.core.constant.ShardingOperator;
 import io.shardingsphere.core.parsing.SQLParsingEngine;
-import io.shardingsphere.core.parsing.parser.context.condition.Column;
-import io.shardingsphere.core.parsing.parser.context.condition.Condition;
 import io.shardingsphere.core.parsing.parser.sql.dml.DMLStatement;
 import io.shardingsphere.core.rule.ShardingRule;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
-import java.util.Collections;
-import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public final class UpdateStatementParserTest extends AbstractStatementParserTest {
-    
-    @Test
-    public void parseWithoutParameter() {
-        ShardingRule shardingRule = createShardingRule();
-        SQLParsingEngine statementParser = new SQLParsingEngine(DatabaseType.MySQL, "UPDATE TABLE_XXX xxx SET TABLE_XXX.field1=field1+1,xxx.field2=2 WHERE TABLE_XXX.field4<10 AND"
-                + " TABLE_XXX.field1=1 AND xxx.field5>10 AND TABLE_XXX.field2 IN (1,3) AND xxx.field6<=10 AND TABLE_XXX.field3 BETWEEN 5 AND 20 AND xxx.field7>=10", shardingRule, null);
-        DMLStatement updateStatement = (DMLStatement) statementParser.parse(false);
-        assertUpdateStatementWithoutParameter(updateStatement);
-    }
-    
-    private void assertUpdateStatementWithoutParameter(final DMLStatement updateStatement) {
-        assertThat(updateStatement.getTables().find("TABLE_XXX").get().getName(), is("TABLE_XXX"));
-        assertThat(updateStatement.getTables().find("TABLE_XXX").get().getAlias().get(), is("xxx"));
-        Condition condition1 = updateStatement.getConditions().find(new Column("field1", "TABLE_XXX")).get();
-        assertThat(condition1.getOperator(), CoreMatchers.is(ShardingOperator.EQUAL));
-        assertThat(((ListShardingValue<? extends Comparable>) condition1.getShardingValue(Collections.emptyList())).getValues().iterator().next(), is((Comparable) 1));
-        Condition condition2 = updateStatement.getConditions().find(new Column("field2", "TABLE_XXX")).get();
-        assertThat(condition2.getOperator(), is(ShardingOperator.IN));
-        Iterator<?> shardingValues2 = ((ListShardingValue) condition2.getShardingValue(Collections.emptyList())).getValues().iterator();
-        assertThat(shardingValues2.next(), is((Object) 1));
-        assertThat(shardingValues2.next(), is((Object) 3));
-        assertFalse(shardingValues2.hasNext());
-        Condition condition3 = updateStatement.getConditions().find(new Column("field3", "TABLE_XXX")).get();
-        Range shardingValues3 = ((RangeShardingValue) condition3.getShardingValue(Collections.emptyList())).getValueRange();
-        assertThat(condition3.getOperator(), is(ShardingOperator.BETWEEN));
-        assertThat(shardingValues3.lowerEndpoint(), is((Comparable) 5));
-        assertThat(shardingValues3.upperEndpoint(), is((Comparable) 20));
-    }
-    
+
     @Test
     public void parseWithOr() {
         ShardingRule shardingRule = createShardingRule();
