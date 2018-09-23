09ec7397143d249e39a07c9e5f1ce10fa80cb5c5
codefairy08 <codefairy@163.com>
2018-09-23 22:17:59 +0800

    Add MySQL DDL table related statements Parser

---
+++ sharding-jdbc-ddl-parser/src/main/java/io/shardingsphere/core/parsing/antler/parser/mysql/MySQLAdvancedDropTableParser.java
@@ -0,0 +1,49 @@
+package io.shardingsphere.core.parsing.antler.parser.mysql;
+
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+
+import io.shardingsphere.core.parsing.antler.AdvancedErrorStrategy;
+import io.shardingsphere.core.parsing.antler.AdvancedParserATNSimulator;
+import io.shardingsphere.parser.antlr.mysql.MySQLDropTableParser;
+
+public class MySQLAdvancedDropTableParser extends MySQLDropTableParser {
+
+    public MySQLAdvancedDropTableParser(TokenStream input) {
+        super(input);
+        _interp = new AdvancedParserATNSimulator(this, _ATN, _decisionToDFA, _sharedContextCache, ID);
+        this._errHandler = new AdvancedErrorStrategy(ID);
+    }
+
+    public Token match(int ttype) throws RecognitionException {
+        Token t = getCurrentToken();
+
+        boolean compatID = false;
+        if (ID == ttype && ID > t.getType()) {
+            compatID = true;
+        }
+
+        if (t.getType() == ttype || compatID) {
+            if (ttype == Token.EOF) {
+                matchedEOF = true;
+            }
+
+            if (!matchedEOF && compatID && (t instanceof CommonToken)) {
+                CommonToken commonToken = (CommonToken) t;
+                commonToken.setType(ID);
+            }
+            _errHandler.reportMatch(this);
+            consume();
+        } else {
+            t = _errHandler.recoverInline(this);
+            if (_buildParseTrees && t.getTokenIndex() == -1) {
+                // we must have conjured up a new token during single token insertion
+                // if it's not the current symbol
+                _ctx.addErrorNode(createErrorNode(_ctx, t));
+            }
+        }
+        return t;
+    }
+}
