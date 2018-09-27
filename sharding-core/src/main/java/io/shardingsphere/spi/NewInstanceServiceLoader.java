8c594289ed128422665f4f3bb9b2ec05a3a68421
tuohai666 <tuohaibbb@gmail.com>
2018-09-27 19:44:30 +0800

    remove synchronized in NewInstanceServiceLoader

--- sharding-core/src/main/java/io/shardingsphere/spi/NewInstanceServiceLoader.java
+++ sharding-core/src/main/java/io/shardingsphere/spi/NewInstanceServiceLoader.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
 package io.shardingsphere.spi;
 
 import io.shardingsphere.core.exception.ShardingException;
@@ -7,7 +24,6 @@ import lombok.NoArgsConstructor;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.ServiceLoader;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * SPI service loader for new instance for every call.
@@ -18,7 +34,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class NewInstanceServiceLoader<T> {
     
-    private final Collection<Class<T>> serviceClasses = new CopyOnWriteArrayList<>();
+    private final Collection<Class<T>> serviceClasses = new LinkedList<>();
     
     /**
      * Creates a new service class loader for the given service type.
@@ -28,7 +44,7 @@ public final class NewInstanceServiceLoader<T> {
      * @return new service class loader
      */
     @SuppressWarnings("unchecked")
-    public static synchronized <T> NewInstanceServiceLoader<T> load(final Class<T> service) {
+    public static <T> NewInstanceServiceLoader<T> load(final Class<T> service) {
         NewInstanceServiceLoader result = new NewInstanceServiceLoader();
         for (T each : ServiceLoader.load(service)) {
             result.serviceClasses.add(each.getClass());
