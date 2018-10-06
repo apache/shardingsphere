3d0f52c48a3e06c91e0521800b8b06fd1f91a083
tristaZero <hannkenn@163.com>
2018-10-06 21:43:30 +0800

    modify ShardingOnlyWithTables.java

--- sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/orche/java/etcd/ShardingOnlyWithTables.java
+++ sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/orche/java/etcd/ShardingOnlyWithTables.java
@@ -19,14 +19,14 @@ package io.shardingsphere.example.jdbc.main.orche.java.etcd;
 
 import io.shardingsphere.api.config.ShardingRuleConfiguration;
 import io.shardingsphere.api.config.TableRuleConfiguration;
-import io.shardingsphere.example.jdbc.fixture.DataRepository;
-import io.shardingsphere.example.jdbc.fixture.DataSourceUtil;
 import io.shardingsphere.orchestration.config.OrchestrationConfiguration;
 import io.shardingsphere.orchestration.config.OrchestrationType;
 import io.shardingsphere.orchestration.reg.api.RegistryCenterConfiguration;
 import io.shardingsphere.orchestration.reg.etcd.EtcdConfiguration;
 import io.shardingsphere.shardingjdbc.orchestration.api.OrchestrationShardingDataSourceFactory;
 import io.shardingsphere.shardingjdbc.orchestration.internal.datasource.OrchestrationShardingDataSource;
+import repository.DataRepository;
+import repository.DataSourceUtil;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
