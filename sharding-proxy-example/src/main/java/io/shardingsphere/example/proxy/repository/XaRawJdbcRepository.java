0f09250dae0e91e4f2d92a056209a350a8683055
tristaZero <hannkenn@163.com>
2018-10-06 20:58:40 +0800

    insertDataWithoutXA

--- sharding-proxy-example/src/main/java/io/shardingsphere/example/proxy/repository/XaRawJdbcRepository.java
+++ sharding-proxy-example/src/main/java/io/shardingsphere/example/proxy/repository/XaRawJdbcRepository.java
@@ -34,7 +34,7 @@ public class XaRawJdbcRepository {
     
     public void demo() throws SQLException {
         createTable();
-        insertData();
+        insertDataWithXA();
         insertFailure();
         updateData();
         System.out.println("1.Query with EQUAL--------------");
@@ -49,7 +49,7 @@ public class XaRawJdbcRepository {
         execute("CREATE TABLE IF NOT EXISTS t_order_item (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, PRIMARY KEY (order_item_id))");
     }
     
-    private void insertData() throws SQLException {
+    private void insertDataWithXA() throws SQLException {
         Connection connection = dataSource.getConnection();
         connection.setAutoCommit(false);
         Statement statement = connection.createStatement();
@@ -61,6 +61,25 @@ public class XaRawJdbcRepository {
                 statement.execute(String.format("INSERT INTO t_order_item (order_id, user_id) VALUES (%d, 11)", orderId));
             }
             connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+        }
+        finally {
+            connection.close();
+            statement.close();
+        }
+    }
+    
+    private void insertDataWithoutXA() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        try {
+            for (int i = 1; i < 10; i++) {
+                long orderId = insertAndGetGeneratedKey(statement,"INSERT INTO t_order (user_id, status) VALUES (10, 'INIT')");
+                statement.execute(String.format("INSERT INTO t_order_item (order_id, user_id) VALUES (%d, 10)", orderId));
+                orderId = insertAndGetGeneratedKey(statement,"INSERT INTO t_order (user_id, status) VALUES (11, 'INIT')");
+                statement.execute(String.format("INSERT INTO t_order_item (order_id, user_id) VALUES (%d, 11)", orderId));
+            }
         } catch (SQLException ex) {
             connection.rollback();
         }
