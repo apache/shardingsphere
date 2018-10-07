25bc70559d588e0dc7a6ece847718251f5684a9c
tristaZero <hannkenn@163.com>
2018-10-07 09:52:13 +0800

    modify ShardingOnlyWithTables.java

--- sharding-jdbc-example/spring-namespace-example/spring-namespace-mybatis-example/src/main/java/io/shardingsphere/example/spring/namespace/mybatis/main/orche/etcd/ShardingOnlyWithTables.java
+++ sharding-jdbc-example/spring-namespace-example/spring-namespace-mybatis-example/src/main/java/io/shardingsphere/example/spring/namespace/mybatis/main/orche/etcd/ShardingOnlyWithTables.java
@@ -17,7 +17,7 @@
 
 package io.shardingsphere.example.spring.namespace.mybatis.main.orche.etcd;
 
-import io.shardingsphere.example.spring.namespace.mybatis.fixtrue.service.DemoService;
+import io.shardingsphere.repository.mybatis.service.DemoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ShardingOnlyWithTables {
