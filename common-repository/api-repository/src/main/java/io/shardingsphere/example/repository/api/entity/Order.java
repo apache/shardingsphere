e52ec0db7ab0b151c4df91fc8661a7fd6e9e1d93
tristaZero <hannkenn@163.com>
2018-10-10 16:05:47 +0800

    add  implements Serializable

--- common-repository/api-repository/src/main/java/io/shardingsphere/example/repository/api/entity/Order.java
+++ common-repository/api-repository/src/main/java/io/shardingsphere/example/repository/api/entity/Order.java
@@ -17,7 +17,11 @@
 
 package io.shardingsphere.example.repository.api.entity;
 
-public class Order {
+import java.io.Serializable;
+
+public class Order implements Serializable {
+    
+    private static final long serialVersionUID = 661434701950670670L;
     
     private long orderId;
     
