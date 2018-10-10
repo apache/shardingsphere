c85f9d81105b6503416232b0bc87168ed0bbc13f
tristaZero <hannkenn@163.com>
2018-10-10 15:57:51 +0800

    ShardingAndMasterSlaveTogether.java

--- sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/orche/java/etcd/ShardingAndMasterSlaveTogether.java
+++ sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/orche/java/etcd/ShardingAndMasterSlaveTogether.java
@@ -27,7 +27,7 @@ import io.shardingsphere.example.jdbc.fixture.algorithm.ModuloShardingTableAlgor
 import io.shardingsphere.example.jdbc.util.DataSourceUtil;
 import io.shardingsphere.example.repository.jdbc.repository.RawOrderItemRepository;
 import io.shardingsphere.example.repository.jdbc.repository.RawOrderRepository;
-import io.shardingsphere.example.repository.jdbc.service.DemoService;
+import io.shardingsphere.example.repository.jdbc.service.RawDemoService;
 import io.shardingsphere.orchestration.config.OrchestrationConfiguration;
 import io.shardingsphere.orchestration.config.OrchestrationType;
 import io.shardingsphere.orchestration.reg.api.RegistryCenterConfiguration;
@@ -51,7 +51,7 @@ public class ShardingAndMasterSlaveTogether {
     
     public static void main(final String[] args) throws SQLException {
         DataSource dataSource = getDataSource();
-        new DemoService(new RawOrderRepository(dataSource), new RawOrderItemRepository(dataSource)).demo();
+        new RawDemoService(new RawOrderRepository(dataSource), new RawOrderItemRepository(dataSource)).demo();
         ((OrchestrationShardingDataSource) dataSource).close();
     }
     
