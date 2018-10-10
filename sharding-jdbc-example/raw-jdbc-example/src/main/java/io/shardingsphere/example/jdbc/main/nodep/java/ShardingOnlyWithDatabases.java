d7ae5e8a96d455dd943337ba0ed4476af3c69674
tristaZero <hannkenn@163.com>
2018-10-10 15:57:59 +0800

    ShardingOnlyWithDatabases.java

--- sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/nodep/java/ShardingOnlyWithDatabases.java
+++ sharding-jdbc-example/raw-jdbc-example/src/main/java/io/shardingsphere/example/jdbc/main/nodep/java/ShardingOnlyWithDatabases.java
@@ -23,7 +23,7 @@ import io.shardingsphere.api.config.strategy.InlineShardingStrategyConfiguration
 import io.shardingsphere.example.jdbc.util.DataSourceUtil;
 import io.shardingsphere.example.repository.jdbc.repository.RawOrderItemRepository;
 import io.shardingsphere.example.repository.jdbc.repository.RawOrderRepository;
-import io.shardingsphere.example.repository.jdbc.service.DemoService;
+import io.shardingsphere.example.repository.jdbc.service.RawDemoService;
 import io.shardingsphere.shardingjdbc.api.ShardingDataSourceFactory;
 
 import javax.sql.DataSource;
@@ -36,7 +36,7 @@ public class ShardingOnlyWithDatabases {
     
     public static void main(final String[] args) throws SQLException {
         DataSource dataSource = getDataSource();
-        new DemoService(new RawOrderRepository(dataSource), new RawOrderItemRepository(dataSource)).demo();
+        new RawDemoService(new RawOrderRepository(dataSource), new RawOrderItemRepository(dataSource)).demo();
     }
     
     private static DataSource getDataSource() throws SQLException {
