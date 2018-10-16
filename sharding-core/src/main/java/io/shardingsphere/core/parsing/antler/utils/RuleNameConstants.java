708e2137c1e89db36ab7ed10deae618ad84bc017
codefairy08 <codefairy@163.com>
2018-10-16 17:42:44 +0800

    Adjust position

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antler/utils/RuleNameConstants.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antler/utils/RuleNameConstants.java
@@ -18,54 +18,54 @@
 package io.shardingsphere.core.parsing.antler.utils;
 
 public class RuleNameConstants {
-    public static final String ADD_COLUMN = "addColumn";
     
     public static final String COLUMN_DEFINITION = "columnDefinition";
-    
+
     public static final String COLUMN_NAME = "columnName";
     
+    public static final String DATA_TYPE = "dataType";
+
+    public static final String DATA_TYPE_LENGTH = "dataTypeLength";
+
+    public static final String FIRST_OR_AFTER_COLUMN = "firstOrAfterColumn";
+
     public static final String PRIMARY_KEY = "primaryKey";
     
+    public static final String PRIMARY_KEY_OPTION = "primaryKeyOption";
+
+    public static final String KEY_PART = "keyPart";
+
     public static final String COLUMN_LIST = "columnList";
+
+    public static final String ADD_COLUMN = "addColumn";
     
+    public static final String CHANGE_COLUMN = "changeColumn";
+
     public static final String DROP_COLUMN = "dropColumn";
-    
-    public static final String DROP_PRIMARY_KEY = "dropPrimaryKey";
-    
-    public static final String INDEX_NAME = "indexName";
     
     public static final String MODIFY_COLUMN = "modifyColumn";
     
+    public static final String MODIFY_COL_PROPERTIES = "modifyColProperties";
+
     public static final String RENAME_COLUMN = "renameColumn";
-    
+
+    public static final String DROP_PRIMARY_KEY = "dropPrimaryKey";
+
     public static final String RENAME_TABLE = "renameTable";
 
     public static final String TABLE_NAME = "tableName";
 
     public static final String ADD_INDEX = "addIndex";
-
-    public static final String ADD_CONSTRAINT = "addConstraint";
-
-    public static final String PRIMARY_KEY_OPTION = "primaryKeyOption";
-
-    public static final String KEY_PART = "keyPart";
-
-    public static final String CHANGE_COLUMN = "changeColumn";
+    
+    public static final String INDEX_NAME = "indexName";
 
     public static final String DROP_INDEX_REF = "dropIndexDef";
-
-    public static final String DROP_CONSTRAINT_CLAUSE = "dropConstraintClause";
-
-    public static final String MODIFY_COL_PROPERTIES = "modifyColProperties";
-
-    public static final String TABLE_CONSTRAINT = "tableConstraint";
     
     public static final String ALTER_DROP_INDEX = "alterDropIndex";
     
-    public static final String DATA_TYPE = "dataType";
+    public static final String ADD_CONSTRAINT = "addConstraint";
     
-    public static final String DATA_TYPE_LENGTH = "dataTypeLength";
-    
-    public static final String FIRST_OR_AFTER_COLUMN = "firstOrAfterColumn";
-    
+    public static final String DROP_CONSTRAINT_CLAUSE = "dropConstraintClause";
+
+    public static final String TABLE_CONSTRAINT = "tableConstraint";
 }
