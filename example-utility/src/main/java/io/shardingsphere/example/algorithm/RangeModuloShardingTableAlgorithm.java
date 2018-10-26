eec9f55a255e3f6467283182fd1db033f972a277
tuohai666 <tuohaibbb@gmail.com>
2018-10-26 11:15:41 +0800

    add RangeModuloShardingTableAlgorithm

---
+++ example-utility/src/main/java/io/shardingsphere/example/algorithm/RangeModuloShardingTableAlgorithm.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.example.algorithm;
+
+import com.google.common.collect.Range;
+import io.shardingsphere.api.algorithm.sharding.RangeShardingValue;
+import io.shardingsphere.api.algorithm.sharding.standard.RangeShardingAlgorithm;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+
+public class RangeModuloShardingTableAlgorithm implements RangeShardingAlgorithm<Long> {
+    
+    @Override
+    public Collection<String> doSharding(final Collection<String> tableNames, final RangeShardingValue<Long> shardingValue) {
+        LinkedHashSet<String> result = new LinkedHashSet<>();
+        if (Range.closed(1L, 10L).encloses(shardingValue.getValueRange())) {
+            for (String each : tableNames) {
+                if (each.endsWith("0")) {
+                    result.add(each);
+                }
+            }
+        } else if (Range.closed(11L, 20L).encloses(shardingValue.getValueRange())) {
+            for (String each : tableNames) {
+                if (each.endsWith("1")) {
+                    result.add(each);
+                }
+            }
+        } else if (Range.closed(1L, 20L).encloses(shardingValue.getValueRange())) {
+            result.addAll(tableNames);
+        } else {
+            throw new UnsupportedOperationException();
+        }
+        return result;
+    }
+}
