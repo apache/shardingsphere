85eb7cf68993603b2bf6c1fe1e697dade87dfbfe
tristaZero <hannkenn@163.com>
2018-10-29 19:26:40 +0800

    final

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/state/StateNodeTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/state/StateNodeTest.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class StateNodeTest {
+public final class StateNodeTest {
     
     private StateNode stateNode = new StateNode("test");
     
