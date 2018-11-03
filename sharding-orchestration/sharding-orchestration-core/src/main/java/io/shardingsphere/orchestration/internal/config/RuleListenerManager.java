c9e2144671f34de61f0ab94dab7cdf034caf2f32
tristaZero <hannkenn@163.com>
2018-11-03 17:26:21 +0800

    add dataSourceService

--- sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/RuleListenerManager.java
+++ sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/RuleListenerManager.java
@@ -24,6 +24,7 @@ import io.shardingsphere.core.rule.ShardingRule;
 import io.shardingsphere.orchestration.internal.event.config.MasterSlaveConfigurationChangedEvent;
 import io.shardingsphere.orchestration.internal.event.config.ShardingConfigurationChangedEvent;
 import io.shardingsphere.orchestration.internal.listener.ListenerManager;
+import io.shardingsphere.orchestration.internal.state.datasource.DataSourceService;
 import io.shardingsphere.orchestration.reg.api.RegistryCenter;
 import io.shardingsphere.orchestration.reg.listener.DataChangedEvent;
 import io.shardingsphere.orchestration.reg.listener.EventListener;
@@ -47,11 +48,14 @@ public final class RuleListenerManager implements ListenerManager {
     
     private final ConfigurationService configService;
     
+    private final DataSourceService dataSourceService;
+    
     public RuleListenerManager(final String name, final RegistryCenter regCenter, final String shardingSchemaName) {
         configNode = new ConfigurationNode(name);
         this.regCenter = regCenter;
         this.shardingSchemaName = shardingSchemaName;
         configService = new ConfigurationService(name, regCenter);
+        dataSourceService = new DataSourceService(name, regCenter);
     }
     
     @Override
@@ -62,7 +66,7 @@ public final class RuleListenerManager implements ListenerManager {
             public void onChange(final DataChangedEvent event) {
                 if (DataChangedEvent.Type.UPDATED == event.getEventType()) {
                     ShardingEventBusInstance.getInstance().post(configService.isShardingRule(shardingSchemaName)
-                            ? getShardingEvent(dataSourceConfigurations, authentication, props) : getMasterSlaveEvent(dataSourceConfigurations, authentication, props));
+                            ? new ShardingConfigurationChangedEvent() : getMasterSlaveEvent(dataSourceConfigurations, authentication, props));
                 }
             }
             
