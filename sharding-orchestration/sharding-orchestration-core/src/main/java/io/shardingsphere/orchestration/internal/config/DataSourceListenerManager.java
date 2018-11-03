42c92e85648bb8a59fca023692867923e51ff484
tristaZero <hannkenn@163.com>
2018-11-03 16:00:18 +0800

    add Override

--- sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/DataSourceListenerManager.java
+++ sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/DataSourceListenerManager.java
@@ -17,22 +17,13 @@
 
 package io.shardingsphere.orchestration.internal.config;
 
-import io.shardingsphere.core.config.DataSourceConfiguration;
 import io.shardingsphere.core.event.ShardingEventBusInstance;
-import io.shardingsphere.core.rule.Authentication;
-import io.shardingsphere.core.rule.ShardingRule;
-import io.shardingsphere.orchestration.internal.event.config.ConfigurationChangedEvent;
 import io.shardingsphere.orchestration.internal.event.config.DataSourceChangedEvent;
-import io.shardingsphere.orchestration.internal.event.config.MasterSlaveConfigurationChangedEvent;
-import io.shardingsphere.orchestration.internal.event.config.ShardingConfigurationChangedEvent;
 import io.shardingsphere.orchestration.internal.listener.ListenerManager;
 import io.shardingsphere.orchestration.internal.state.datasource.DataSourceService;
 import io.shardingsphere.orchestration.reg.api.RegistryCenter;
 import io.shardingsphere.orchestration.reg.listener.DataChangedEvent;
 import io.shardingsphere.orchestration.reg.listener.EventListener;
-
-import java.util.Map;
-import java.util.Properties;
 
 /**
  * Data source listener manager.
@@ -61,11 +52,6 @@ public final class DataSourceListenerManager implements ListenerManager {
     
     @Override
     public void watch() {
-        watch(configNode.getDataSourcePath(shardingSchemaName));
-        watch(configNode.getRulePath(shardingSchemaName));
-    }
-    
-    private void watch() {
         regCenter.watch(configNode.getDataSourcePath(shardingSchemaName), new EventListener() {
             
             @Override
