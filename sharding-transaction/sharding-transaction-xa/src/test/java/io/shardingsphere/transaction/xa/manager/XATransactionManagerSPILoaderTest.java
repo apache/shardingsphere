16b4bbf432cdf8bac337cd8258279c87df160b68
cherrylzhao <zhaojunv3@126.com>
2018-11-06 19:51:37 +0800

    #1363 Add XATransactionManagerSPILoaderTest.assertServiceLoaderFailed()

--- sharding-transaction/sharding-transaction-xa/src/test/java/io/shardingsphere/transaction/xa/manager/XATransactionManagerSPILoaderTest.java
+++ sharding-transaction/sharding-transaction-xa/src/test/java/io/shardingsphere/transaction/xa/manager/XATransactionManagerSPILoaderTest.java
@@ -17,15 +17,31 @@
 
 package io.shardingsphere.transaction.xa.manager;
 
+import io.shardingsphere.core.exception.ShardingException;
+import io.shardingsphere.spi.NewInstanceServiceLoader;
+import io.shardingsphere.transaction.manager.xa.XATransactionManager;
+import io.shardingsphere.transaction.xa.fixture.ReflectiveUtil;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 public final class XATransactionManagerSPILoaderTest {
     
     @Test
     public void assertGerInstanceWithSPI() {
         assertThat(XATransactionManagerSPILoader.getInstance().getTransactionManager(), instanceOf(AtomikosTransactionManager.class));
+    }
+    
+    @Test(expected = ShardingException.class)
+    @SuppressWarnings("unchecked")
+    public void assertServiceLoaderFailed() {
+        XATransactionManagerSPILoader spiLoader = XATransactionManagerSPILoader.getInstance();
+        NewInstanceServiceLoader<XATransactionManager> serviceLoader = mock(NewInstanceServiceLoader.class);
+        doThrow(ShardingException.class).when(serviceLoader).newServiceInstances();
+        ReflectiveUtil.setProperty(spiLoader, "serviceLoader", serviceLoader);
+        ReflectiveUtil.methodInvoke(spiLoader, "load");
     }
 }
