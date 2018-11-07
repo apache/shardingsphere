d9eeae61d2b8937fe36ec2ebb71681de23dda033
tristaZero <hannkenn@163.com>
2018-11-07 18:19:28 +0800

    use OrchestrationListenerManager

--- sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/OrchestrationFacade.java
+++ sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/OrchestrationFacade.java
@@ -22,7 +22,7 @@ import io.shardingsphere.core.config.DataSourceConfiguration;
 import io.shardingsphere.core.rule.Authentication;
 import io.shardingsphere.orchestration.config.OrchestrationConfiguration;
 import io.shardingsphere.orchestration.internal.config.ConfigurationService;
-import io.shardingsphere.orchestration.internal.listener.ListenerFactory;
+import io.shardingsphere.orchestration.internal.listener.OrchestrationListenerManager;
 import io.shardingsphere.orchestration.internal.state.datasource.DataSourceService;
 import io.shardingsphere.orchestration.internal.state.instance.InstanceStateService;
 import io.shardingsphere.orchestration.reg.api.RegistryCenter;
@@ -56,7 +56,7 @@ public final class OrchestrationFacade implements AutoCloseable {
     private final DataSourceService dataSourceService;
     
     @Getter
-    private final ListenerFactory listenerManager;
+    private final OrchestrationListenerManager listenerManager;
     
     public OrchestrationFacade(final OrchestrationConfiguration orchestrationConfig, final Collection<String> shardingSchemaNames) {
         regCenter = RegistryCenterLoader.load(orchestrationConfig.getRegCenterConfig());
@@ -64,8 +64,8 @@ public final class OrchestrationFacade implements AutoCloseable {
         configService = new ConfigurationService(orchestrationConfig.getName(), regCenter);
         instanceStateService = new InstanceStateService(orchestrationConfig.getName(), regCenter);
         dataSourceService = new DataSourceService(orchestrationConfig.getName(), regCenter);
-        listenerManager = shardingSchemaNames.isEmpty() ? new ListenerFactory(orchestrationConfig.getName(), regCenter, configService.getAllShardingSchemaNames())
-                : new ListenerFactory(orchestrationConfig.getName(), regCenter, shardingSchemaNames);
+        listenerManager = shardingSchemaNames.isEmpty() ? new OrchestrationListenerManager(orchestrationConfig.getName(), regCenter, configService.getAllShardingSchemaNames())
+                : new OrchestrationListenerManager(orchestrationConfig.getName(), regCenter, shardingSchemaNames);
     }
     
     /**
