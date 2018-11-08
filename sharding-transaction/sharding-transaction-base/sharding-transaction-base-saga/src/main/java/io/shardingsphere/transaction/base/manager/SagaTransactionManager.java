6ed038ef27333c7a3a032d556560813eb53aa4d4
KomachiSion <263976490@qq.com>
2018-11-08 18:22:04 +0800

    Refactor SagaExecutionComponent from singleton mode to caches by SagaConfiguration

--- sharding-transaction/sharding-transaction-base/sharding-transaction-base-saga/src/main/java/io/shardingsphere/transaction/base/manager/SagaTransactionManager.java
+++ sharding-transaction/sharding-transaction-base/sharding-transaction-base-saga/src/main/java/io/shardingsphere/transaction/base/manager/SagaTransactionManager.java
@@ -17,14 +17,30 @@
 
 package io.shardingsphere.transaction.base.manager;
 
+import com.google.common.util.concurrent.MoreExecutors;
+import io.shardingsphere.api.config.SagaConfiguration;
 import io.shardingsphere.core.event.transaction.base.SagaTransactionEvent;
+import io.shardingsphere.core.executor.ShardingThreadFactoryBuilder;
 import io.shardingsphere.transaction.manager.base.BASETransactionManager;
-import io.shardingsphere.transaction.base.manager.servicecomb.SagaExecutionComponentHolder;
 import io.shardingsphere.transaction.base.manager.servicecomb.ShardingTransportFactorySPILoader;
+import org.apache.servicecomb.saga.core.EventEnvelope;
+import org.apache.servicecomb.saga.core.PersistentStore;
+import org.apache.servicecomb.saga.core.SagaDefinition;
 import org.apache.servicecomb.saga.core.application.SagaExecutionComponent;
+import org.apache.servicecomb.saga.core.application.interpreter.FromJsonFormat;
+import org.apache.servicecomb.saga.core.dag.GraphBasedSagaFactory;
+import org.apache.servicecomb.saga.format.ChildrenExtractor;
+import org.apache.servicecomb.saga.format.JacksonFromJsonFormat;
+import org.apache.servicecomb.saga.infrastructure.EmbeddedEventStore;
 
 import javax.transaction.Status;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Saga transaction manager.
@@ -38,11 +54,9 @@ public final class SagaTransactionManager implements BASETransactionManager<Saga
     
     private static final ThreadLocal<String> TRANSACTION_IDS = new ThreadLocal<>();
     
-    private final SagaExecutionComponent coordinator;
+    private final Map<String, SagaExecutionComponent> sagaCaches = new HashMap<>();
     
-    private SagaTransactionManager() {
-        this.coordinator = SagaExecutionComponentHolder.getInstance().getSagaExecutionComponent();
-    }
+    private final Map<String, ExecutorService> executorCaches = new HashMap<>();
     
     @Override
     public void begin(final SagaTransactionEvent transactionEvent) {
@@ -53,7 +67,7 @@ public final class SagaTransactionManager implements BASETransactionManager<Saga
     @Override
     public void commit(final SagaTransactionEvent transactionEvent) {
         // TODO Analyse the result of saga coordinator.run, if run failed, throw exception
-        coordinator.run(transactionEvent.getSagaJson());
+        getSagaExecutionComponent(transactionEvent.getSagaConfiguration()).run(transactionEvent.getSagaJson());
         TRANSACTION_IDS.remove();
         ShardingTransportFactorySPILoader.getInstance().getTransportFactory().remove();
     }
@@ -66,7 +80,6 @@ public final class SagaTransactionManager implements BASETransactionManager<Saga
     
     @Override
     public int getStatus() {
-        // TODO :zhaojun need confirm, return Status.STATUS_NO_TRANSACTION or zero?
         if (null != getTransactionId()) {
             return Status.STATUS_ACTIVE;
         }
@@ -85,5 +98,61 @@ public final class SagaTransactionManager implements BASETransactionManager<Saga
      */
     public static SagaTransactionManager getInstance() {
         return INSTANCE;
+    }
+    
+    /**
+     * Remove saga execution component from caches if exist.
+     *
+     * @param config saga configuration
+     */
+    public void removeSagaExecutionComponent(final SagaConfiguration config) {
+        synchronized (sagaCaches) {
+            if (sagaCaches.containsKey(config.getAlias())) {
+                SagaExecutionComponent coordinator = sagaCaches.remove(config.getAlias());
+                try {
+                    coordinator.terminate();
+                    // CHECKSTYLE:OFF
+                } catch (Exception ignore) {
+                    // CHECKSTYLE:ON
+                }
+            }
+            if (executorCaches.containsKey(config.getAlias())) {
+                executorCaches.remove(config.getAlias()).shutdown();
+            }
+        }
+    }
+    
+    private SagaExecutionComponent getSagaExecutionComponent(final SagaConfiguration config) {
+        SagaExecutionComponent result;
+        synchronized (sagaCaches) {
+            if (!sagaCaches.containsKey(config.getAlias())) {
+                sagaCaches.put(config.getAlias(), createSagaExecutionComponent(config, createExecutors(config)));
+            }
+            result = sagaCaches.get(config.getAlias());
+        }
+        return result;
+    }
+    
+    private SagaExecutionComponent createSagaExecutionComponent(final SagaConfiguration config, final ExecutorService executors) {
+        EmbeddedPersistentStore persistentStore = new EmbeddedPersistentStore();
+        FromJsonFormat<SagaDefinition> fromJsonFormat = new JacksonFromJsonFormat(ShardingTransportFactorySPILoader.getInstance().getTransportFactory());
+        GraphBasedSagaFactory sagaFactory = new GraphBasedSagaFactory(config.getCompensationRetryDelay(), persistentStore, new ChildrenExtractor(), executors);
+        return new SagaExecutionComponent(persistentStore, fromJsonFormat, null, sagaFactory);
+    }
+    
+    private ExecutorService createExecutors(final SagaConfiguration config) {
+        ExecutorService result = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(config.getExecutorSize(), ShardingThreadFactoryBuilder.build("Saga-%d")));
+        MoreExecutors.addDelayedShutdownHook(result, 60, TimeUnit.SECONDS);
+        executorCaches.put(config.getAlias(), result);
+        return result;
+    }
+    
+    private static final class EmbeddedPersistentStore extends EmbeddedEventStore implements PersistentStore {
+        
+        @Override
+        public Map<String, List<EventEnvelope>> findPendingSagaEvents() {
+            //TODO find pending saga event from persistent store
+            return null;
+        }
     }
 }
