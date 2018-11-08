255f1eabde8ea45d37be615304875dc1a3e44330
KomachiSion <263976490@qq.com>
2018-11-08 18:33:57 +0800

    Update SagaTransactionManagerTest to test cached SagaExecutionComponent

--- sharding-transaction/sharding-transaction-base/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/base/manager/SagaTransactionManagerTest.java
+++ sharding-transaction/sharding-transaction-base/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/base/manager/SagaTransactionManagerTest.java
@@ -17,6 +17,7 @@
 
 package io.shardingsphere.transaction.base.manager;
 
+import io.shardingsphere.api.config.SagaConfiguration;
 import io.shardingsphere.core.constant.transaction.TransactionOperationType;
 import io.shardingsphere.core.event.transaction.base.SagaTransactionEvent;
 import org.apache.servicecomb.saga.core.SuccessfulSagaResponse;
@@ -29,9 +30,15 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.transaction.Status;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -39,44 +46,95 @@ public final class SagaTransactionManagerTest {
     
     private final SagaTransactionManager transactionManager = SagaTransactionManager.getInstance();
     
+    private final String sagaJson = "{}";
+    
+    private static SagaConfiguration config = new SagaConfiguration();
+    
+    private static SagaConfiguration config2 = new SagaConfiguration();
+    
     @Mock
     private SagaExecutionComponent coordinator;
     
-    private final String sagaJson = "{}";
+    private Map<String, SagaExecutionComponent> sagaCaches;
+    
+    private Map<String, ExecutorService> executorCaches;
+    
+    private Method getSagaExecutionComponentMethod;
     
     @Before
-    public void setUp() throws NoSuchFieldException, IllegalAccessException {
+    public void setUp() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException {
         when(coordinator.run(sagaJson)).thenReturn(new SuccessfulSagaResponse("{}"));
-        Field field = SagaTransactionManager.class.getDeclaredField("coordinator");
-        field.setAccessible(true);
-        field.set(transactionManager, coordinator);
+        Field sagaCachesField = SagaTransactionManager.class.getDeclaredField("sagaCaches");
+        Field executorCachesField = SagaTransactionManager.class.getDeclaredField("executorCaches");
+        sagaCachesField.setAccessible(true);
+        executorCachesField.setAccessible(true);
+        sagaCaches = (Map<String, SagaExecutionComponent>) sagaCachesField.get(transactionManager);
+        executorCaches = (Map<String, ExecutorService>) executorCachesField.get(transactionManager);
+        sagaCaches.clear();
+        executorCaches.clear();
+    
+        getSagaExecutionComponentMethod = SagaTransactionManager.class.getDeclaredMethod("getSagaExecutionComponent", SagaConfiguration.class);
+        getSagaExecutionComponentMethod.setAccessible(true);
+    }
+    
+    
+    @Test
+    public void assertGetSagaExecutionComponentWithSameConfig() throws InvocationTargetException, IllegalAccessException {
+        assertThat(sagaCaches.size(), is(0));
+        assertThat(executorCaches.size(), is(0));
+        SagaExecutionComponent first = (SagaExecutionComponent) getSagaExecutionComponentMethod.invoke(transactionManager, config);
+        assertThat(sagaCaches.size(), is(1));
+        assertThat(executorCaches.size(), is(1));
+        SagaExecutionComponent second = (SagaExecutionComponent) getSagaExecutionComponentMethod.invoke(transactionManager, config);
+        assertTrue(first == second);
+    }
+    
+    @Test
+    public void assertGetSagaExecutionComponentWithDiffConfig() throws InvocationTargetException, IllegalAccessException {
+        assertThat(sagaCaches.size(), is(0));
+        assertThat(executorCaches.size(), is(0));
+        SagaExecutionComponent first = (SagaExecutionComponent) getSagaExecutionComponentMethod.invoke(transactionManager, config);
+        assertThat(sagaCaches.size(), is(1));
+        assertThat(executorCaches.size(), is(1));
+        SagaExecutionComponent second = (SagaExecutionComponent) getSagaExecutionComponentMethod.invoke(transactionManager, config2);
+        assertTrue(first != second);
+    }
+    
+    @Test
+    public void assertRemoveSagaExecutionComponent() throws InvocationTargetException, IllegalAccessException {
+        SagaExecutionComponent first = (SagaExecutionComponent) getSagaExecutionComponentMethod.invoke(transactionManager, config);
+        transactionManager.removeSagaExecutionComponent(config);
+        assertThat(sagaCaches.size(), is(0));
+        assertThat(executorCaches.size(), is(0));
     }
     
     @Test
     public void assertBegin() {
-        transactionManager.begin(new SagaTransactionEvent(TransactionOperationType.BEGIN));
+        transactionManager.begin(new SagaTransactionEvent(TransactionOperationType.BEGIN, config));
         assertThat(36 == transactionManager.getTransactionId().length(), is(true));
     }
     
     @Test
     public void assertCommit() {
-        SagaTransactionEvent event = new SagaTransactionEvent(TransactionOperationType.COMMIT);
+        sagaCaches.put(config.getAlias(), coordinator);
+        SagaTransactionEvent event = new SagaTransactionEvent(TransactionOperationType.COMMIT, config);
         event.setSagaJson(sagaJson);
         transactionManager.commit(event);
         assertThat(null == transactionManager.getTransactionId(), is(true));
+        verify(coordinator).run(sagaJson);
     }
     
     @Test
     public void assertRollback() {
-        transactionManager.rollback(new SagaTransactionEvent(TransactionOperationType.ROLLBACK));
+        transactionManager.rollback(new SagaTransactionEvent(TransactionOperationType.ROLLBACK, config));
         assertThat(null == transactionManager.getTransactionId(), is(true));
     }
     
     @Test
     public void assertGetStatus() {
-        transactionManager.begin(new SagaTransactionEvent(TransactionOperationType.BEGIN));
+        transactionManager.begin(new SagaTransactionEvent(TransactionOperationType.BEGIN, config));
         assertThat(transactionManager.getStatus(), is(Status.STATUS_ACTIVE));
-        transactionManager.rollback(new SagaTransactionEvent(TransactionOperationType.ROLLBACK));
+        transactionManager.rollback(new SagaTransactionEvent(TransactionOperationType.ROLLBACK, config));
         assertThat(transactionManager.getStatus(), is(Status.STATUS_NO_TRANSACTION));
     }
 }
