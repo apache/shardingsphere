75aaeb3cae0124e08505dd71e0597fc2af7e93ee
tristaZero <hannkenn@163.com>
2018-11-09 17:31:36 +0800

    rename to getSlaveDataSourceNamesFromMasterSlaveRule()

--- sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/service/ConfigurationService.java
+++ sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/config/service/ConfigurationService.java
@@ -17,10 +17,8 @@
 
 package io.shardingsphere.orchestration.internal.config.service;
 
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
-import com.google.common.collect.Collections2;
 import io.shardingsphere.api.config.MasterSlaveRuleConfiguration;
 import io.shardingsphere.api.config.RuleConfiguration;
 import io.shardingsphere.api.config.ShardingRuleConfiguration;
@@ -34,7 +32,6 @@ import io.shardingsphere.orchestration.reg.api.RegistryCenter;
 import org.yaml.snakeyaml.Yaml;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -238,32 +235,25 @@ public final class ConfigurationService {
         Map<String, Collection<String>> result = new LinkedHashMap<>();
         for (String each : getAllShardingSchemaNames()) {
             if (isShardingRule(each)) {
-                result.put(each, getMasterDataSourceNamesFromShardingRule(each));
+                result.put(each, getSlaveDataSourceNamesFromShardingRule(each));
             } else {
-                result.put(each, getMasterDataSourceNamesFromMasterSlaveRule(each));
+                result.put(each, getSlaveDataSourceNamesFromMasterSlaveRule(each));
             }
         }
         return result;
     }
     
-    private Collection<String> getMasterDataSourceNamesFromShardingRule(final String schemaName) {
+    private Collection<String> getSlaveDataSourceNamesFromShardingRule(final String schemaName) {
         Collection<String> result = new LinkedList<>();
         ShardingRuleConfiguration shardingConfig = loadShardingRuleConfiguration(schemaName);
-        if (shardingConfig.getMasterSlaveRuleConfigs().isEmpty()) {
-            shardingConfig.
+        for (MasterSlaveRuleConfiguration each : shardingConfig.getMasterSlaveRuleConfigs()) {
+            result.addAll(each.getSlaveDataSourceNames());
         }
-        result.addAll(Collections2.transform(shardingConfig.getMasterSlaveRuleConfigs(), new Function<MasterSlaveRuleConfiguration, String>() {
-
-            @Override
-            public String apply(final MasterSlaveRuleConfiguration masterSlaveRuleConfig) {
-                return masterSlaveRuleConfig.getMasterDataSourceName();
-            }
-        }));
         return result;
     }
     
-    private Collection<String> getMasterDataSourceNamesFromMasterSlaveRule(final String schemaName) {
+    private Collection<String> getSlaveDataSourceNamesFromMasterSlaveRule(final String schemaName) {
         MasterSlaveRuleConfiguration masterSlaveConfig = loadMasterSlaveRuleConfiguration(schemaName);
-        return Collections.singletonList(masterSlaveConfig.getMasterDataSourceName());
+        return masterSlaveConfig.getSlaveDataSourceNames();
     }
 }
