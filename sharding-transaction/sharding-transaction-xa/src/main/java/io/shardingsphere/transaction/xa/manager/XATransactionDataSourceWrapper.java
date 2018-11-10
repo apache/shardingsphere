fd071242b90832e049399bf3e90193dd7e76563c
cherrylzhao <zhaojunv3@126.com>
2018-11-10 10:44:49 +0800

    #1363 Fixed dbcp2 transactional datasource register error.

--- sharding-transaction/sharding-transaction-xa/src/main/java/io/shardingsphere/transaction/xa/manager/XATransactionDataSourceWrapper.java
+++ sharding-transaction/sharding-transaction-xa/src/main/java/io/shardingsphere/transaction/xa/manager/XATransactionDataSourceWrapper.java
@@ -79,7 +79,7 @@ final class XATransactionDataSourceWrapper {
     private void registerRecoveryResource(final String dataSourceName, final XADataSource xaDataSource) {
         JdbcTransactionalResource transactionalResource = new JdbcTransactionalResource(dataSourceName, xaDataSource);
         synchronized (XATransactionDataSourceWrapper.class) {
-            if (null != Configuration.getResource(dataSourceName)) {
+            if (null == Configuration.getResource(dataSourceName)) {
                 Configuration.addResource(transactionalResource);
             }
         }
