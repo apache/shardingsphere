763eeedf2558ab7cd612fe51227d49b88ccd6150
马晓光 <mxg@maxiaoguang.com>
2018-11-10 16:11:38 +0800

    add parser tag for spring namespace

--- sharding-jdbc/sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/main/java/io/shardingsphere/shardingjdbc/spring/namespace/constants/ShardingDataSourceBeanDefinitionParserTag.java
+++ sharding-jdbc/sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/main/java/io/shardingsphere/shardingjdbc/spring/namespace/constants/ShardingDataSourceBeanDefinitionParserTag.java
@@ -48,6 +48,10 @@ public final class ShardingDataSourceBeanDefinitionParserTag {
     
     public static final String MASTER_SLAVE_RULE_TAG = "master-slave-rule";
     
+    public static final String BROADCAST_TABLE_RULES_TAG = "broadcast-table-rules";
+    
+    public static final String BROADCAST_TABLE_RULE_TAG = "broadcast-table-rule";
+    
     public static final String BINDING_TABLE_RULES_TAG = "binding-table-rules";
     
     public static final String BINDING_TABLE_RULE_TAG = "binding-table-rule";
