8dc6196d6e982930ea403dd9654bdcde442a1c97
codefairy08 <codefairy@163.com>
2018-11-13 19:41:42 +0800

    implement ASTExtractHandler1

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/dialect/sqlserver/SQLServerAddIndexExtractHandler.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/dialect/sqlserver/SQLServerAddIndexExtractHandler.java
@@ -17,21 +17,27 @@
 
 package io.shardingsphere.core.parsing.antlr.extractor.statement.handler.dialect.sqlserver;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import com.google.common.base.Optional;
+
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.ASTExtractHandler;
+import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.ASTExtractHandler1;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.RuleName;
+import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.ExtractResult;
+import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.SQLTokenExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.util.ASTUtils;
 import io.shardingsphere.core.parsing.parser.sql.SQLStatement;
 import io.shardingsphere.core.parsing.parser.token.IndexToken;
 import io.shardingsphere.core.util.SQLUtil;
-import org.antlr.v4.runtime.ParserRuleContext;
 
 /**
  * SQLServer Add index extract handler for SQLServer.
  * 
  * @author duhongjun
  */
-public final class SQLServerAddIndexExtractHandler implements ASTExtractHandler {
+public final class SQLServerAddIndexExtractHandler implements ASTExtractHandler,ASTExtractHandler1 {
     
     @Override
     public void extract(final ParserRuleContext ancestorNode, final SQLStatement statement) {
@@ -44,5 +50,20 @@ public final class SQLServerAddIndexExtractHandler implements ASTExtractHandler 
             statement.getSQLTokens().add(
                     new IndexToken(indexNameNode.get().getStop().getStartIndex(), SQLUtil.getNameWithoutSchema(indexNameNode.get().getText()), statement.getTables().getSingleTableName()));
         }
+    }
+
+    @Override
+    public ExtractResult extract(ParserRuleContext ancestorNode) {
+        SQLTokenExtractResult extractResult = new SQLTokenExtractResult();
+        Optional<ParserRuleContext> indexDefOptionNode = ASTUtils.findFirstChildNode(ancestorNode, RuleName.ADD_COLUMN);
+        if (!indexDefOptionNode.isPresent()) {
+            return extractResult;
+        }
+        Optional<ParserRuleContext> indexNameNode = ASTUtils.findFirstChildNode(indexDefOptionNode.get(), RuleName.INDEX_NAME);
+        if (indexNameNode.isPresent()) {
+            extractResult.getSqlTokens().add(
+                    new IndexToken(indexNameNode.get().getStop().getStartIndex(), SQLUtil.getNameWithoutSchema(indexNameNode.get().getText()), null));
+        }
+        return extractResult;
     }
 }
