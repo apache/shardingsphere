3db9d4f158619cd4e5bb81983e40d88698b90fc0
codefairy08 <codefairy@163.com>
2018-11-14 15:58:38 +0800

    remove unused code

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/DropPrimaryKeyExtractHandler.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/DropPrimaryKeyExtractHandler.java
@@ -24,24 +24,13 @@ import com.google.common.base.Optional;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.DropPrimaryKeyExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.ExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.util.ASTUtils;
-import io.shardingsphere.core.parsing.antlr.sql.ddl.AlterTableStatement;
-import io.shardingsphere.core.parsing.parser.sql.SQLStatement;
 
 /**
  * Drop primary key extract handler.
  * 
  * @author duhongjun
  */
-public final class DropPrimaryKeyExtractHandler implements ASTExtractHandler,ASTExtractHandler1 {
-    
-    @Override
-    public void extract(final ParserRuleContext ancestorNode, final SQLStatement statement) {
-        AlterTableStatement alterStatement = (AlterTableStatement) statement;
-        Optional<ParserRuleContext> dropPrimaryKeyNode = ASTUtils.findFirstChildNode(ancestorNode, RuleName.DROP_PRIMARY_KEY);
-        if (dropPrimaryKeyNode.isPresent()) {
-            alterStatement.setDropPrimaryKey(true);
-        }
-    }
+public final class DropPrimaryKeyExtractHandler implements ASTExtractHandler {
 
     @Override
     public ExtractResult extract(ParserRuleContext ancestorNode) {
