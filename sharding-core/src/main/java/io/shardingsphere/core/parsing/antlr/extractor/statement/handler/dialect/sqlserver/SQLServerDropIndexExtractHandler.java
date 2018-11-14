b94f61d328d7e54ec8c215c9687d275f260c4d5b
codefairy08 <codefairy@163.com>
2018-11-14 16:01:23 +0800

    remove unused code

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/dialect/sqlserver/SQLServerDropIndexExtractHandler.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/dialect/sqlserver/SQLServerDropIndexExtractHandler.java
@@ -22,12 +22,10 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import com.google.common.base.Optional;
 
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.ASTExtractHandler;
-import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.ASTExtractHandler1;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.RuleName;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.ExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.SQLTokenExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.util.ASTUtils;
-import io.shardingsphere.core.parsing.parser.sql.SQLStatement;
 import io.shardingsphere.core.parsing.parser.token.IndexToken;
 import io.shardingsphere.core.util.SQLUtil;
 
@@ -36,19 +34,7 @@ import io.shardingsphere.core.util.SQLUtil;
  * 
  * @author duhongjun
  */
-public final class SQLServerDropIndexExtractHandler implements ASTExtractHandler,ASTExtractHandler1 {
-    
-    @Override
-    public void extract(final ParserRuleContext ancestorNode, final SQLStatement statement) {
-        Optional<ParserRuleContext> indexDefOptionNode = ASTUtils.findFirstChildNode(ancestorNode, RuleName.ALTER_DROP_INDEX);
-        if (indexDefOptionNode.isPresent()) {
-            Optional<ParserRuleContext> indexNameNode = ASTUtils.findFirstChildNode(indexDefOptionNode.get(), RuleName.INDEX_NAME);
-            if (indexNameNode.isPresent()) {
-                statement.getSQLTokens().add(
-                        new IndexToken(indexNameNode.get().getStop().getStartIndex(), SQLUtil.getNameWithoutSchema(indexNameNode.get().getText()), statement.getTables().getSingleTableName()));
-            }
-        }
-    }
+public final class SQLServerDropIndexExtractHandler implements ASTExtractHandler {
 
     @Override
     public ExtractResult extract(ParserRuleContext ancestorNode) {
