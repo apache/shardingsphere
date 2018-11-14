3ef2db1c632c571fb810cdcc1e3c8e8778d7d28d
tristaZero <hannkenn@163.com>
2018-11-14 19:59:13 +0800

    add final for RegistryCenterLoaderTest.java

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/RegistryCenterLoaderTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/RegistryCenterLoaderTest.java
@@ -25,7 +25,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
 
-public class RegistryCenterLoaderTest {
+public final class RegistryCenterLoaderTest {
     
     private RegistryCenter regCenter;
     
