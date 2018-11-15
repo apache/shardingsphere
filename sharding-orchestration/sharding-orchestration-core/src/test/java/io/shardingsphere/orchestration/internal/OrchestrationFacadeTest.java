481f591b178faf4d9896b10c9fa855ff47ffd201
tristaZero <hannkenn@163.com>
2018-11-15 18:50:51 +0800

    add orchestrationFacade

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/OrchestrationFacadeTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/OrchestrationFacadeTest.java
@@ -17,10 +17,20 @@
 
 package io.shardingsphere.orchestration.internal;
 
+import io.shardingsphere.orchestration.reg.api.RegistryCenter;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public final class OrchestrationFacadeTest {
+    
+    private OrchestrationFacade orchestrationFacade;
+    
+    @Mock
+    private RegistryCenter regCenter;
     
     @Before
     public void setUp() {
