7039619696cf5c9112906de60c48c18f8a1f18fe
tristaZero <hannkenn@163.com>
2018-11-15 18:11:46 +0800

    add properties

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/config/listener/PropertiesOrchestrationListenerTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/config/listener/PropertiesOrchestrationListenerTest.java
@@ -17,10 +17,20 @@
 
 package io.shardingsphere.orchestration.internal.config.listener;
 
+import io.shardingsphere.orchestration.reg.api.RegistryCenter;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public final class PropertiesOrchestrationListenerTest {
+    
+    private PropertiesOrchestrationListener propertiesOrchestrationListener;
+    
+    @Mock
+    private RegistryCenter regCenter;
     
     @Before
     public void setUp() {
