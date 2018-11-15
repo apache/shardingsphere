8a559cd4df832e2bb6b665315243af34edf43fed
codefairy08 <codefairy@163.com>
2018-11-15 09:45:51 +0800

    for checkstyle

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/result/RenameTableExtractResult.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/result/RenameTableExtractResult.java
@@ -30,18 +30,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class RenameTableExtractResult implements ExtractResult {
+    
     private final String newTableName;
-
+    
     /**
      * Inject new table name to SQLStatement.
+     * 
      * @param statement SQL statement
      */
     @Override
-    public void inject(final SQLStatement statement) {
+    public void fill(final SQLStatement statement) {
         AlterTableStatement alterStatement = (AlterTableStatement) statement;
         if (null != newTableName) {
             alterStatement.setNewTableName(newTableName);
         }
     }
-
 }
