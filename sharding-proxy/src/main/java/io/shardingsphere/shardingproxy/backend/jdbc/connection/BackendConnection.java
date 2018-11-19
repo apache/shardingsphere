80a87766b4f142b04fe401dc99762176c28eb4da
cherrylzhao <zhaojunv3@126.com>
2018-11-19 22:53:59 +0800

    #1238 Remove transaction operation from BackendConnection.

--- sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/backend/jdbc/connection/BackendConnection.java
+++ sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/backend/jdbc/connection/BackendConnection.java
@@ -17,23 +17,16 @@
 
 package io.shardingsphere.shardingproxy.backend.jdbc.connection;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import io.shardingsphere.core.constant.ConnectionMode;
-import io.shardingsphere.core.constant.transaction.TransactionOperationType;
 import io.shardingsphere.core.constant.transaction.TransactionType;
-import io.shardingsphere.core.event.transaction.ShardingTransactionEvent;
-import io.shardingsphere.core.event.transaction.xa.XATransactionEvent;
 import io.shardingsphere.core.routing.router.masterslave.MasterVisitedManager;
 import io.shardingsphere.shardingproxy.runtime.GlobalRegistry;
 import io.shardingsphere.shardingproxy.runtime.schema.LogicSchema;
-import io.shardingsphere.spi.transaction.ShardingTransactionHandler;
-import io.shardingsphere.spi.transaction.ShardingTransactionHandlerRegistry;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -71,15 +64,6 @@ public final class BackendConnection implements AutoCloseable {
     
     @Setter
     private TransactionType transactionType = GlobalRegistry.getInstance().getTransactionType();
-    
-    /**
-     * Get connection size.
-     * 
-     * @return connection size
-     */
-    public int getConnectionSize() {
-        return cachedConnections.values().size();
-    }
     
     /**
      * Get connections of current thread datasource.
@@ -127,6 +111,15 @@ public final class BackendConnection implements AutoCloseable {
     }
     
     /**
+     * Get connection size.
+     *
+     * @return connection size
+     */
+    public int getConnectionSize() {
+        return cachedConnections.values().size();
+    }
+    
+    /**
      * Add statement.
      *
      * @param statement statement to be added
@@ -170,76 +163,6 @@ public final class BackendConnection implements AutoCloseable {
         throwSQLExceptionIfNecessary(exceptions);
     }
     
-    /**
-     * set auto commit.
-     *
-     * @param autoCommit auto commit
-     */
-    private void setAutoCommit(final boolean autoCommit) {
-        if (!autoCommit) {
-            status = ConnectionStatus.TRANSACTION;
-        }
-        cachedConnections.clear();
-        recordMethodInvocation(Connection.class, "setAutoCommit", new Class[]{boolean.class}, new Object[]{autoCommit});
-    }
-    
-    /**
-     * Execute transaction by operation type.
-     *
-     * @param operationType operation type
-     * @throws SQLException SQL exception
-     */
-    public void doInTransactional(final TransactionOperationType operationType) throws SQLException {
-        ShardingTransactionHandler<ShardingTransactionEvent> shardingTransactionHandler = ShardingTransactionHandlerRegistry.getInstance().getHandler(transactionType);
-        if (null != transactionType && transactionType != TransactionType.LOCAL) {
-            Preconditions.checkNotNull(shardingTransactionHandler, String.format("Cannot find transaction manager of [%s]", transactionType));
-        }
-        if (TransactionType.LOCAL == transactionType) {
-            switch (operationType) {
-                case BEGIN:
-                    setAutoCommit(false);
-                    break;
-                case COMMIT:
-                    commit();
-                    break;
-                case ROLLBACK:
-                    rollback();
-                    break;
-                default:
-            }
-        } else if (TransactionType.XA == transactionType) {
-            shardingTransactionHandler.doInTransaction(new XATransactionEvent(operationType));
-        }
-    }
-    
-    /**
-     * Do commit.
-     *
-     * @throws SQLException SQL exception
-     */
-    private void commit() throws SQLException {
-        if (ConnectionStatus.TRANSACTION == status) {
-            Collection<SQLException> exceptions = new LinkedList<>();
-            exceptions.addAll(commitConnections());
-            throwSQLExceptionIfNecessary(exceptions);
-            status = ConnectionStatus.TERMINATED;
-        }
-    }
-    
-    /**
-     * Do rollback.
-     *
-     * @throws SQLException SQL exception
-     */
-    private void rollback() throws SQLException {
-        if (ConnectionStatus.TRANSACTION == status) {
-            Collection<SQLException> exceptions = new LinkedList<>();
-            exceptions.addAll(rollbackConnections());
-            throwSQLExceptionIfNecessary(exceptions);
-            status = ConnectionStatus.TERMINATED;
-        }
-    }
-    
     private Collection<SQLException> closeResultSets() {
         Collection<SQLException> result = new LinkedList<>();
         for (ResultSet each : cachedResultSets) {
@@ -278,30 +201,6 @@ public final class BackendConnection implements AutoCloseable {
         return result;
     }
     
-    private Collection<SQLException> commitConnections() {
-        Collection<SQLException> result = new LinkedList<>();
-        for (Connection each : cachedConnections.values()) {
-            try {
-                each.commit();
-            } catch (SQLException ex) {
-                result.add(ex);
-            }
-        }
-        return result;
-    }
-    
-    private Collection<SQLException> rollbackConnections() {
-        Collection<SQLException> result = new LinkedList<>();
-        for (Connection each : cachedConnections.values()) {
-            try {
-                each.rollback();
-            } catch (SQLException ex) {
-                result.add(ex);
-            }
-        }
-        return result;
-    }
-    
     private void throwSQLExceptionIfNecessary(final Collection<SQLException> exceptions) throws SQLException {
         if (exceptions.isEmpty()) {
             return;
@@ -311,11 +210,6 @@ public final class BackendConnection implements AutoCloseable {
             ex.setNextException(each);
         }
         throw ex;
-    }
-    
-    @SneakyThrows
-    private void recordMethodInvocation(final Class<?> targetClass, final String methodName, final Class<?>[] argumentTypes, final Object[] arguments) {
-        methodInvocations.add(new MethodInvocation(targetClass.getMethod(methodName, argumentTypes), arguments));
     }
     
     private void replayMethodsInvocation(final Object target) {
