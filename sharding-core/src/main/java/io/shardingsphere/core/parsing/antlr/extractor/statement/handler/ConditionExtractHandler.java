ea53777f37e86c5e7c0879b4a1c0f8edd588aa4f
codefairy08 <codefairy@163.com>
2018-11-22 09:33:59 +0800

    optimized code

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/ConditionExtractHandler.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/extractor/statement/handler/ConditionExtractHandler.java
@@ -17,16 +17,32 @@
 
 package io.shardingsphere.core.parsing.antlr.extractor.statement.handler;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ParseTree;
 
 import com.google.common.base.Optional;
 
+import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.ColumnExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.handler.result.ConditionExtractResult;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.util.ASTUtils;
 import io.shardingsphere.core.parsing.antlr.extractor.statement.util.OperatorUtils;
+import io.shardingsphere.core.parsing.lexer.token.DefaultKeyword;
+import io.shardingsphere.core.parsing.lexer.token.Symbol;
+import io.shardingsphere.core.parsing.parser.clause.condition.NullCondition;
 import io.shardingsphere.core.parsing.parser.context.condition.AndCondition;
+import io.shardingsphere.core.parsing.parser.context.condition.Column;
+import io.shardingsphere.core.parsing.parser.context.condition.Condition;
 import io.shardingsphere.core.parsing.parser.context.condition.OrCondition;
+import io.shardingsphere.core.parsing.parser.expression.SQLExpression;
+import io.shardingsphere.core.parsing.parser.expression.SQLNumberExpression;
+import io.shardingsphere.core.parsing.parser.expression.SQLPlaceholderExpression;
+import io.shardingsphere.core.parsing.parser.expression.SQLTextExpression;
+import io.shardingsphere.core.util.NumberUtil;
 
 /**
  * Condition extract handler.
@@ -35,75 +51,225 @@ import io.shardingsphere.core.parsing.parser.context.condition.OrCondition;
  */
 public final class ConditionExtractHandler implements ASTExtractHandler<Optional<ConditionExtractResult>> {
     
+    private final ColumnExtractHandler columnHandler = new ColumnExtractHandler();
+    
     @Override
-    public Optional<ConditionExtractResult> extract(ParserRuleContext ancestorNode) {
+    public Optional<ConditionExtractResult> extract(final ParserRuleContext ancestorNode) {
         Optional<ParserRuleContext> whereNode = ASTUtils.findFirstChildNode(ancestorNode, RuleName.WHERECLAUSE);
+        Collection<ParserRuleContext> questionNodes = ASTUtils.getAllDescendantNodes(ancestorNode, RuleName.QUESTION);
+        Map<ParserRuleContext, Integer> questionNodeIndexMap = new HashMap<>();
+        int index = 0;
+        for(ParserRuleContext each : questionNodes) {
+            questionNodeIndexMap.put(each, index++);
+        }
         if(!whereNode.isPresent()) {
             return Optional.absent();
         }
-        return Optional.of(new ConditionExtractResult(extractCondition(whereNode.get()).get()));
+        return Optional.of(new ConditionExtractResult(extractCondition(questionNodeIndexMap, (ParserRuleContext)whereNode.get().getChild(1)).get()));
     }
     
-    private Optional<OrCondition> extractCondition(ParseTree tree) {
+    private Optional<OrCondition> extractCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext exprNode) {
         int index = -1;
-        for (int i = 0; i < tree.getChildCount(); i++) {
-            if (OperatorUtils.isRationalOperator(tree.getChild(i).getText())) {
+        for (int i = 0; i < exprNode.getChildCount(); i++) {
+            if (OperatorUtils.isRationalOperator(exprNode.getChild(i).getText())) {
                 index = i;
                 break;
             }
         }
         if (index > 0) {
-            OrCondition result = new OrCondition();
-            Optional<OrCondition> leftOrCondition = extractCondition(tree.getChild(index - 1));
-            Optional<OrCondition> rightOrCondition = extractCondition(tree.getChild(index + 1));
-            if (!leftOrCondition.get().getAndConditions().isEmpty() && !rightOrCondition.get().getAndConditions().isEmpty()) {
-                if (OperatorUtils.isOr(tree.getChild(index).getText())) {
-                    result.getAndConditions().addAll(leftOrCondition.get().getAndConditions());
-                    result.getAndConditions().addAll(rightOrCondition.get().getAndConditions());
-                } else if (OperatorUtils.isAnd(tree.getChild(index).getText())) {
-                    for (AndCondition each : leftOrCondition.get().getAndConditions()) {
-                        for (AndCondition eachRightOr : rightOrCondition.get().getAndConditions()) {
-                            AndCondition tempList = new AndCondition();
-                            tempList.getConditions().addAll(each.getConditions());
-                            tempList.getConditions().addAll(eachRightOr.getConditions());
-                            result.getAndConditions().add(tempList);
-                        }
+            Optional<OrCondition> leftOrCondition = extractCondition(questionNodeIndexMap, (ParserRuleContext)exprNode.getChild(index - 1));
+            Optional<OrCondition> rightOrCondition = extractCondition(questionNodeIndexMap, (ParserRuleContext)exprNode.getChild(index + 1));
+            return mergeCondition(leftOrCondition, rightOrCondition, exprNode.getChild(index).getText());
+        } else {
+            return extractConditionForParen(questionNodeIndexMap, exprNode);
+        }
+    }
+    
+    private Optional<OrCondition> mergeCondition(final Optional<OrCondition> leftOrCondition, final Optional<OrCondition> rightOrCondition, final String operator) {
+        if (!leftOrCondition.get().getAndConditions().isEmpty() && !rightOrCondition.get().getAndConditions().isEmpty()) {
+            if (OperatorUtils.isOr(operator)) {
+                leftOrCondition.get().getAndConditions().addAll(rightOrCondition.get().getAndConditions());
+                return leftOrCondition;
+            } 
+            if (OperatorUtils.isAnd(operator)) {
+                OrCondition result = new OrCondition();
+                for (AndCondition each : leftOrCondition.get().getAndConditions()) {
+                    for (AndCondition eachRightOr : rightOrCondition.get().getAndConditions()) {
+                        AndCondition tempList = new AndCondition();
+                        tempList.getConditions().addAll(each.getConditions());
+                        tempList.getConditions().addAll(eachRightOr.getConditions());
+                        result.getAndConditions().add(tempList);
                     }
                 }
                 return Optional.of(result);
-            } 
-            if (!leftOrCondition.get().getAndConditions().isEmpty()) {
-                return leftOrCondition;
-            } 
-            if (!rightOrCondition.get().getAndConditions().isEmpty()) {
-                return rightOrCondition;
             }
-        } else {
-            index = -1;
-            for (int i = 0; i < tree.getChildCount(); i++) {
-                if (OperatorUtils.isStartParen(tree.getChild(i).getText())) {
-                    index = i;
-                    break;
-                }
+        } 
+        if (!leftOrCondition.get().getAndConditions().isEmpty()) {
+            return leftOrCondition;
+        } 
+        if (!rightOrCondition.get().getAndConditions().isEmpty()) {
+            return rightOrCondition;
+        }
+        return Optional.of(new OrCondition());
+    }
+    
+    private Optional<OrCondition> extractConditionForParen(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext exprNode) {
+        int index = -1;
+        for (int i = 0; i < exprNode.getChildCount(); i++) {
+            if (OperatorUtils.isStartParen(exprNode.getChild(i).getText())) {
+                index = i;
+                break;
             }
-            if (0 <= index) {
-                if (tree.getChildCount() != index + 3) {
-                    throw new RuntimeException("invalid expression");
-                }
-                if (OperatorUtils.parenMatch(tree.getChild(index + 2).getText(), tree.getChild(index).getText())) {
-                    throw new RuntimeException("missing right paren");
-                }
-                if (index >= 0 && "ExprContext".equals(tree.getChild(index + 1).getClass().getSimpleName())) {
-                    return extractCondition(tree.getChild(index + 1));
-                }
-            } else {
-                OrCondition result = new OrCondition();
-                AndCondition newAndCondition = new AndCondition();
-                //newAndCondition.getConditions().add(tree.getText());
-                result.getAndConditions().add(newAndCondition);
-                return Optional.of(result);
+        }
+        if (0 <= index) {
+            if (exprNode.getChildCount() != index + 3) {
+                throw new RuntimeException("invalid expression");
+            }
+            if (OperatorUtils.parenMatch(exprNode.getChild(index + 2).getText(), exprNode.getChild(index).getText())) {
+                throw new RuntimeException("missing right paren");
+            }
+            if (index >= 0 && "ExprContext".equals(exprNode.getChild(index + 1).getClass().getSimpleName())) {
+                return extractCondition(questionNodeIndexMap, (ParserRuleContext)exprNode.getChild(index + 1));
+            }
+            return Optional.absent();
+        } 
+        OrCondition result = new OrCondition();
+        AndCondition newAndCondition = new AndCondition();
+        newAndCondition.getConditions().add(buildCondition(questionNodeIndexMap, exprNode).get());
+        result.getAndConditions().add(newAndCondition);
+        return Optional.of(result);
+    }
+    
+    private Optional<Condition> buildCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext exprNode) {
+        Optional<Condition> result = buildEqualCondition(questionNodeIndexMap, exprNode);
+        if(result.isPresent()) {
+            return result;
+        }
+        result = buildPredicateCondition(questionNodeIndexMap, exprNode);
+        if(!result.isPresent()) {
+           return Optional.<Condition>of(new NullCondition());
+        }
+        return result;
+    }
+    
+    private Optional<Condition> buildEqualCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext exprNode) {
+        Optional<ParserRuleContext> comparisionNode = ASTUtils.findFirstChildNode(exprNode, RuleName.COMPARSIONOPERATOR);
+        if(!comparisionNode.isPresent()) {
+            return Optional.absent();
+        }
+        if(3 != comparisionNode.get().getParent().getChildCount()) {
+            return Optional.absent();
+        }
+        if(!Symbol.EQ.name().equalsIgnoreCase(comparisionNode.get().getText())) {
+            return Optional.absent();
+        }
+        Optional<ParserRuleContext> leftNode = ASTUtils.findFirstChildNode((ParserRuleContext)comparisionNode.get().parent.getChild(0), RuleName.COLUMN_NAME);
+        Optional<ParserRuleContext> rightNode = ASTUtils.findFirstChildNode((ParserRuleContext)comparisionNode.get().parent.getChild(2), RuleName.COLUMN_NAME);
+        if(!leftNode.isPresent() && !rightNode.isPresent()) {
+            return Optional.absent();
+        }
+        if(leftNode.isPresent() && rightNode.isPresent()) {
+            return Optional.absent();
+        }
+        ParserRuleContext valueNode = null;
+        if(leftNode.isPresent()) {
+            valueNode = (ParserRuleContext)exprNode.getChild(2);
+        }else if(rightNode.isPresent()) {
+            valueNode = (ParserRuleContext)exprNode.getChild(0);
+        }
+        
+        Optional<Column> column = buildColumn(exprNode);
+        if(!column.isPresent()) {
+            return Optional.absent();
+        }
+        Optional<SQLExpression> sqlExpression = buildExperssion(questionNodeIndexMap, valueNode);
+        if(!sqlExpression.isPresent()) {
+            return Optional.absent();
+        }
+        return Optional.of(new Condition(column.get(), sqlExpression.get()));
+    }
+    
+    private Optional<SQLExpression> buildExperssion(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext valueNode) {
+        Optional<ParserRuleContext> experssionNode = ASTUtils.findFirstChildNode(valueNode, RuleName.STRING);
+        if(experssionNode.isPresent()) {
+            return Optional.<SQLExpression>of(new SQLTextExpression(experssionNode.get().getText()));
+        }
+        experssionNode = ASTUtils.findFirstChildNode(valueNode, RuleName.NUMBER);
+        if(experssionNode.isPresent()) {
+            return Optional.<SQLExpression>of(new SQLNumberExpression(NumberUtil.getExactlyNumber(experssionNode.get().getText(), 10)));
+        }
+        experssionNode = ASTUtils.findFirstChildNode(valueNode, RuleName.QUESTION);
+        if(experssionNode.isPresent()) {
+            Integer index = questionNodeIndexMap.get(experssionNode.get());
+            if(null != index) {
+                return Optional.<SQLExpression>of(new SQLPlaceholderExpression(index.intValue()));
             }
         }
         return Optional.absent();
     }
+    
+    private Optional<Condition> buildPredicateCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext exprNode) {
+        Optional<ParserRuleContext> predicateNode = ASTUtils.findFirstChildNode(exprNode, RuleName.PREDICATE);
+        if(!predicateNode.isPresent()) {
+            return Optional.absent();
+        }
+        if(1 != predicateNode.get().getParent().getChildCount()) {
+            return Optional.absent();
+        }
+        if(5 == predicateNode.get().getChildCount() && DefaultKeyword.BETWEEN.name().equalsIgnoreCase(predicateNode.get().getChild(1).getText())) {
+            Optional<Condition> result = buildBetweenCondition(questionNodeIndexMap, predicateNode.get());
+            if(result.isPresent()) {
+                return result;
+            }
+        }
+        if(5 <= predicateNode.get().getChildCount() && DefaultKeyword.IN.name().equalsIgnoreCase(predicateNode.get().getChild(1).getText())) {
+            Optional<Condition> result = buildInCondition(questionNodeIndexMap, predicateNode.get());
+            if(result.isPresent()) {
+                return result;
+            }
+        }   
+        return Optional.absent();
+    }
+    
+    private Optional<Condition> buildBetweenCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext predicateNode) {
+        Optional<Column> column = buildColumn((ParserRuleContext)predicateNode.getChild(0));
+        if(!column.isPresent()) {
+            return Optional.absent();
+        }
+        Optional<SQLExpression> beginSQLExpression = buildExperssion(questionNodeIndexMap, (ParserRuleContext)predicateNode.getChild(2));
+        Optional<SQLExpression> endSQLExpression = buildExperssion(questionNodeIndexMap, (ParserRuleContext)predicateNode.getChild(4));
+        if(beginSQLExpression.isPresent() && endSQLExpression.isPresent()) {
+            return Optional.of(new Condition(column.get(), beginSQLExpression.get(), endSQLExpression.get())); 
+        }
+        return Optional.absent();
+    }
+    
+    private Optional<Condition> buildInCondition(final Map<ParserRuleContext, Integer> questionNodeIndexMap, final ParserRuleContext predicateNode) {
+        Optional<Column> column = buildColumn((ParserRuleContext)predicateNode.getChild(0));
+        if(!column.isPresent()) {
+            return Optional.absent();
+        }
+        Collection<ParserRuleContext> expressionNodes = ASTUtils.getAllDescendantNodes((ParserRuleContext)predicateNode.getChild(0), RuleName.SIMPLEEXPR);
+        List<SQLExpression> sqlExpressions = new LinkedList<>();
+        for(ParserRuleContext each : expressionNodes) {
+            Optional<SQLExpression> expression = buildExperssion(questionNodeIndexMap, each);
+            if(!expression.isPresent()) {
+                sqlExpressions.clear();
+            }
+            sqlExpressions.add(expression.get());
+        }
+        if(!sqlExpressions.isEmpty()) {
+            return Optional.of(new Condition(column.get(), sqlExpressions)); 
+        }
+        return Optional.absent();
+    }
+    
+    private Optional<Column> buildColumn(final ParserRuleContext parentNode) {
+        Optional<ParserRuleContext> column = ASTUtils.findFirstChildNode(parentNode, RuleName.COLUMN_NAME);
+        if(column.isPresent()) {
+            return Optional.absent();
+        }
+        Optional<ColumnExtractResult> columnExtractResult = columnHandler.extract(column.get());
+        return Optional.of(new Column(columnExtractResult.get().getName(), columnExtractResult.get().getOwner().get()));
+    }     
 }
