294e9a99ddcef159e0f75ff4120300e09aa82ad9
tristaZero <hannkenn@163.com>
2018-11-27 09:44:22 +0800

    add java doc

--- sharding-core/src/main/java/io/shardingsphere/core/executor/sql/execute/result/QueryRow.java
+++ sharding-core/src/main/java/io/shardingsphere/core/executor/sql/execute/result/QueryRow.java
@@ -38,10 +38,10 @@ public final class QueryRow {
     /**
      * Two query rows are equal or not.
      *
-     * @param queryRow
-     * @return
+     * @param queryRow query row
+     * @return is equal or not
      */
-    public boolean isSame(final QueryRow queryRow) {
+    public boolean isEqual(final QueryRow queryRow) {
         if (-1 == distinctColumnIndex) {
             return rowData.equals(queryRow.getRowData());
         }
