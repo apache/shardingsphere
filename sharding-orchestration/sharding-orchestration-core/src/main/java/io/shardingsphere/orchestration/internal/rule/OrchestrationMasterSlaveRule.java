bcb467c1679281bece58881f87d09f218372d0c4
tristaZero <hannkenn@163.com>
2018-11-30 15:54:52 +0800

    fix bug by using register()

--- sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/rule/OrchestrationMasterSlaveRule.java
+++ sharding-orchestration/sharding-orchestration-core/src/main/java/io/shardingsphere/orchestration/internal/rule/OrchestrationMasterSlaveRule.java
@@ -20,6 +20,7 @@ package io.shardingsphere.orchestration.internal.rule;
 import com.google.common.eventbus.Subscribe;
 import io.shardingsphere.api.config.MasterSlaveRuleConfiguration;
 import io.shardingsphere.core.constant.ShardingConstant;
+import io.shardingsphere.core.event.ShardingEventBusInstance;
 import io.shardingsphere.core.rule.MasterSlaveRule;
 import io.shardingsphere.orchestration.internal.state.event.DisabledStateEvent;
 
@@ -37,6 +38,7 @@ public final class OrchestrationMasterSlaveRule extends MasterSlaveRule {
     
     public OrchestrationMasterSlaveRule(final MasterSlaveRuleConfiguration config) {
         super(config);
+        ShardingEventBusInstance.getInstance().register(this);
     }
     
     /**
--- sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/runtime/schema/LogicSchema.java
+++ sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/runtime/schema/LogicSchema.java
@@ -18,6 +18,7 @@
 package io.shardingsphere.shardingproxy.runtime.schema;
 
 import com.google.common.eventbus.Subscribe;
+import io.shardingsphere.core.event.ShardingEventBusInstance;
 import io.shardingsphere.core.metadata.ShardingMetaData;
 import io.shardingsphere.core.rule.DataSourceParameter;
 import io.shardingsphere.orchestration.internal.config.event.DataSourceChangedEvent;
@@ -48,6 +49,7 @@ public abstract class LogicSchema {
         // TODO :jiaqi only use JDBC need connect db via JDBC, netty style should use SQL packet to get metadata
         this.dataSources = dataSources;
         backendDataSource = new JDBCBackendDataSource(dataSources);
+        ShardingEventBusInstance.getInstance().register(this);
     }
     
     protected final Map<String, String> getDataSourceURLs(final Map<String, DataSourceParameter> dataSourceParameters) {
