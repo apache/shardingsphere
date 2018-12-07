06e363fef08ed30ee96b30f4a55a595ff94b764c
cherrylzhao <zhaojunv3@126.com>
2018-12-07 19:11:21 +0800

    for #1238 Refactor ShardingCTLSetBackendHandler.

--- sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/backend/sctl/ShardingCTLSetBackendHandler.java
+++ sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/backend/sctl/ShardingCTLSetBackendHandler.java
@@ -18,7 +18,6 @@
 package io.shardingsphere.shardingproxy.backend.sctl;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Preconditions;
 import io.shardingsphere.core.constant.transaction.TransactionType;
 import io.shardingsphere.core.exception.ShardingException;
 import io.shardingsphere.shardingproxy.backend.AbstractBackendHandler;
@@ -26,9 +25,6 @@ import io.shardingsphere.shardingproxy.backend.jdbc.connection.BackendConnection
 import io.shardingsphere.shardingproxy.transport.mysql.packet.command.CommandResponsePackets;
 import io.shardingsphere.shardingproxy.transport.mysql.packet.generic.OKPacket;
 import lombok.RequiredArgsConstructor;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Sharding CTL backend handler.
@@ -44,47 +40,17 @@ public final class ShardingCTLSetBackendHandler extends AbstractBackendHandler {
     
     @Override
     protected CommandResponsePackets execute0() {
-        Optional<ShardingTCLStatement> shardingTCLStatement = new ShardingCTLParser(sql).doParse();
+        Optional<ShardingCTLSetStatement> shardingTCLStatement = new ShardingCTLSetParser(sql).doParse();
         if (!shardingTCLStatement.isPresent()) {
             throw new ShardingException("please review your sctl format, should be sctl:set xxx=yyy.");
         }
-        switch (shardingTCLStatement.get().key) {
+        switch (shardingTCLStatement.get().getKey()) {
             case "transaction_type":
-                backendConnection.setTransactionType(TransactionType.find(shardingTCLStatement.get().value));
+                backendConnection.setTransactionType(TransactionType.find(shardingTCLStatement.get().getValue()));
                 break;
             default:
                 throw new ShardingException(String.format("could not support this sctl grammar [%s].", sql));
         }
         return new CommandResponsePackets(new OKPacket(1));
-    }
-    
-    public final class ShardingCTLParser {
-    
-        private final String regex = "sctl:set\\s+(\\S*)=(\\S*)";
-        
-        private Matcher matcher;
-        
-        ShardingCTLParser(final String sql) {
-            this.matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(sql);
-        }
-        
-        Optional<ShardingTCLStatement> doParse() {
-            if (matcher.find()) {
-                String key = matcher.group(1);
-                String value = matcher.group(2);
-                Preconditions.checkNotNull(key, "sctl key cannot be null.");
-                Preconditions.checkNotNull(value, "sctl value cannot be null.");
-                return Optional.of(new ShardingTCLStatement(key, value));
-            }
-            return Optional.absent();
-        }
-    }
-    
-    @RequiredArgsConstructor
-    private final class ShardingTCLStatement {
-        
-        private final String key;
-        
-        private final String value;
     }
 }
