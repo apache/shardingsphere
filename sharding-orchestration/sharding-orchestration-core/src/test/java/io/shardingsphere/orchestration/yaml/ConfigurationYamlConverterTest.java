fb2b4d3be8d4c7412c7df3e855fbb7fd022fab8c
tristaZero <hannkenn@163.com>
2018-12-08 15:38:21 +0800

    add static properties

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/yaml/ConfigurationYamlConverterTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/yaml/ConfigurationYamlConverterTest.java
@@ -17,17 +17,51 @@
 
 package io.shardingsphere.orchestration.yaml;
 
+import io.shardingsphere.api.config.ShardingRuleConfiguration;
+import io.shardingsphere.core.config.DataSourceConfiguration;
 import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
 
 public class ConfigurationYamlConverterTest {
     
+    private static final String DATA_SOURCE_YAML = "master_ds: !!io.shardingsphere.orchestration.yaml.YamlDataSourceConfiguration\n"
+            + "  dataSourceClassName: com.zaxxer.hikari.HikariDataSource\n" + "  properties:\n" + "    DATA_SOURCE_POOL_CLASS_NAME: com.zaxxer.hikari.HikariDataSource\n"
+            + "    proxyDatasourceType: !!io.shardingsphere.core.constant.transaction.ProxyPoolType 'VENDOR'\n"
+            + "    url: jdbc:mysql://localhost:3306/demo_ds_master\n" + "    username: root\n" + "    password: null\n";
+    
+    private static final String SHARDING_RULE_YAML = "  tables:\n" + "    t_order:\n" + "      actualDataNodes: ds_${0..1}.t_order_${0..1}\n" + "      tableStrategy:\n"
+            + "        inline:\n" + "          shardingColumn: order_id\n" + "          algorithmExpression: t_order_${order_id % 2}\n"
+            + "      keyGeneratorColumnName: order_id\n" + "    t_order_item:\n" + "      actualDataNodes: ds_${0..1}.t_order_item_${0..1}\n" + "      tableStrategy:\n" + "        inline:\n"
+            + "          shardingColumn: order_id\n" + "          algorithmExpression: t_order_item_${order_id % 2}\n" + "      keyGeneratorColumnName: order_item_id\n" + "  bindingTables:\n"
+            + "    - t_order,t_order_item\n" + "  defaultDataSourceName: ds_1\n" + "  defaultDatabaseStrategy:\n" + "    inline:\n" + "      shardingColumn: user_id\n"
+            + "      algorithmExpression: ds_${user_id % 2}";
+    
+    private static final String MASTER_SLAVE_RULE_YAML = "masterDataSourceName: master_ds\n" + "name: ms_ds\n" + "slaveDataSourceNames:\n" + "- slave_ds_0\n" + "- slave_ds_1\n";
+    
+    private static final String AUTHENTICATION_YAML = "password: root\nusername: root\n";
+    
+    private static final String CONFIG_MAP_YAML = "sharding-key1: sharding-value1";
+    
+    private static final String PROPERTIES_YAML = "executor.size: 16\nsql.show: true";
     
     @Test
     public void assertLoadDataSourceConfigurations() {
+        Map<String, DataSourceConfiguration> actual = ConfigurationYamlConverter.loadDataSourceConfigurations(DATA_SOURCE_YAML);
+        assertThat(actual.size(), is(1));
+        assertTrue(actual.keySet().contains("master_ds"));
     }
     
     @Test
     public void assertLoadShardingRuleConfiguration() {
+        ShardingRuleConfiguration actual = ConfigurationYamlConverter.loadShardingRuleConfiguration(SHARDING_RULE_YAML);
+        assertThat(actual.getTableRuleConfigs().size(), is(2));
+        assertThat(actual.getBindingTableGroups().size(), is(1));
+        assertThat(actual.getDefaultDataSourceName(), is("ds_1"));
     }
     
     @Test
@@ -70,4 +104,3 @@ public class ConfigurationYamlConverterTest {
     public void assertDumpProperties() {
     }
 }
-    
