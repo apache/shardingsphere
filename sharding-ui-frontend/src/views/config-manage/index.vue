6f6e74778f58e669c6d44818be9d5350d729ff9f
wqzwh <wangziqihappy@163.com>
2018-12-11 11:44:35 +0800

    feature：Delete test data

--- sharding-ui-frontend/src/views/config-manage/index.vue
+++ sharding-ui-frontend/src/views/config-manage/index.vue
@@ -22,63 +22,7 @@ export default {
   data() {
     return {
       radioValue: 'schema',
-      data: [
-        {
-          label: 'Level one 1',
-          children: [
-            {
-              label: 'Level two 1-1',
-              children: [
-                {
-                  label: 'Level three 1-1-1'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          label: 'Level one 2',
-          children: [
-            {
-              label: 'Level two 2-1',
-              children: [
-                {
-                  label: 'Level three 2-1-1'
-                }
-              ]
-            },
-            {
-              label: 'Level two 2-2',
-              children: [
-                {
-                  label: 'Level three 2-2-1'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          label: 'Level one 3',
-          children: [
-            {
-              label: 'Level two 3-1',
-              children: [
-                {
-                  label: 'Level three 3-1-1'
-                }
-              ]
-            },
-            {
-              label: 'Level two 3-2',
-              children: [
-                {
-                  label: 'Level three 3-2-1'
-                }
-              ]
-            }
-          ]
-        }
-      ],
+      data: [],
       defaultProps: {
         children: 'children',
         label: 'label'
--- sharding-ui-frontend/src/views/index/module/registConfig.vue
+++ sharding-ui-frontend/src/views/index/module/registConfig.vue
@@ -63,38 +63,7 @@ export default {
         address: '',
         namespaces: ''
       },
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          ss: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        },
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          ss: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          ss: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          ss: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }
-      ]
+      tableData: []
     }
   },
   methods: {
