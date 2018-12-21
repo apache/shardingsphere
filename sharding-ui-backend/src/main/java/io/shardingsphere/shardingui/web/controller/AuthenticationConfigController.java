94ab0166e70638b7006f7a2b50df190ffd44310a
beckhampu <beckhampu@163.com>
2018-12-21 16:08:29 +0800

    feature: add RESTful API of sharding proxy authentication configuration

---
+++ sharding-ui-backend/src/main/java/io/shardingsphere/shardingui/web/controller/AuthenticationConfigController.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.shardingui.web.controller;
+
+import io.shardingsphere.core.rule.Authentication;
+import io.shardingsphere.shardingui.servcie.AuthenticationConfigService;
+import io.shardingsphere.shardingui.web.response.ResponseResult;
+import io.shardingsphere.shardingui.web.response.ResponseResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RESTful API of authentication.
+ * 
+ * @author chenqingyang
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/authentication")
+public final class AuthenticationConfigController {
+    
+    @Autowired
+    private AuthenticationConfigService authenticationConfigService;
+    
+    /**
+     * Load authentication.
+     * 
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseResult<Authentication> loadAuthentication() {
+        return ResponseResultUtil.build(authenticationConfigService.getAuthentication());
+    }
+    
+    /**
+     * Update authentication.
+     * 
+     * @param authentication authentication
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public ResponseResult updateAuthentication(@RequestBody final Authentication authentication) {
+        authenticationConfigService.updateAuthentication(authentication);
+        return ResponseResultUtil.success();
+    }
+}
