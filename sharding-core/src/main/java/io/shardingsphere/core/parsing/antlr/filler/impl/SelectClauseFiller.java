173ea270516e49a4c0949d1b223d3325b0cffb81
codefairy08 <codefairy@163.com>
2018-12-24 18:39:42 +0800

    #1654 bug fixs

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/filler/impl/SelectClauseFiller.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/antlr/filler/impl/SelectClauseFiller.java
@@ -28,8 +28,10 @@ import io.shardingsphere.core.parsing.antlr.filler.SQLStatementFiller;
 import io.shardingsphere.core.parsing.antlr.sql.segment.SQLSegment;
 import io.shardingsphere.core.parsing.antlr.sql.segment.SelectClauseSegment;
 import io.shardingsphere.core.parsing.antlr.sql.segment.expr.ExpressionSegment;
+import io.shardingsphere.core.parsing.antlr.sql.segment.expr.FunctionExpressionSegment;
 import io.shardingsphere.core.parsing.antlr.sql.segment.expr.PropertyExpressionSegment;
 import io.shardingsphere.core.parsing.antlr.sql.segment.expr.StarExpressionSegment;
+import io.shardingsphere.core.parsing.parser.constant.DerivedAlias;
 import io.shardingsphere.core.parsing.parser.context.selectitem.DistinctSelectItem;
 import io.shardingsphere.core.parsing.parser.sql.SQLStatement;
 import io.shardingsphere.core.parsing.parser.sql.dql.select.SelectStatement;
@@ -54,7 +56,9 @@ public final class SelectClauseFiller implements SQLStatementFiller {
             fillForDistinct(selectClauseSegment, selectStatement, sql, shardingRule, shardingTableMetaData);
         } else {
             ExpressionFiller expressionFiller = new ExpressionFiller();
+            int offset = 0;
             for (ExpressionSegment each : selectClauseSegment.getExpressions()) {
+                offset = setDistinctFunctionAlias(each, offset);
                 expressionFiller.fill(each, sqlStatement, sql, shardingRule, shardingTableMetaData);
             }
         }
@@ -67,6 +71,7 @@ public final class SelectClauseFiller implements SQLStatementFiller {
         ExpressionFiller expressionFiller = new ExpressionFiller();
         Set<String> distinctColumnNames = new HashSet<String>();
         DistinctSelectItem distinctSelectItem = null;
+        int offset = 0;
         if (firstExpression instanceof StarExpressionSegment) {
             expressionFiller.fill(firstExpression, selectStatement, sql, shardingRule, shardingTableMetaData);
             selectStatement.getItems().add(new DistinctSelectItem(distinctColumnNames, Optional.<String>absent()));
@@ -76,17 +81,32 @@ public final class SelectClauseFiller implements SQLStatementFiller {
             selectStatement.getItems().add(distinctSelectItem);
             distinctColumnNames.add(propertyExpressionSegment.getName());
         } else {
+            offset = setDistinctFunctionAlias(firstExpression, offset);
             expressionFiller.fill(firstExpression, selectStatement, sql, shardingRule, shardingTableMetaData);
         }
         while (expressionIterator.hasNext()) {
             ExpressionSegment nextExpression = expressionIterator.next();
             expressionFiller.fill(nextExpression, selectStatement, sql, shardingRule, shardingTableMetaData);
             if (nextExpression instanceof PropertyExpressionSegment) {
+                offset = setDistinctFunctionAlias(nextExpression, offset);
                 distinctColumnNames.add(((PropertyExpressionSegment) nextExpression).getName());
             }
         }
         if (null != distinctSelectItem) {
             distinctSelectItem.getDistinctColumnNames().addAll(distinctColumnNames);
         }
+    }
+    
+    private int setDistinctFunctionAlias(final ExpressionSegment expressionSegment, final int offset) {
+        if(expressionSegment instanceof FunctionExpressionSegment) {
+            FunctionExpressionSegment functionExpressionSegment = (FunctionExpressionSegment)expressionSegment;
+            Optional<String> alias = functionExpressionSegment.getAlias();
+            if(functionExpressionSegment.isHasDistinct() && !alias.isPresent()) {
+                alias = Optional.of(DerivedAlias.AGGREGATION_DISTINCT_DERIVED.getDerivedAlias(offset));
+                ((FunctionExpressionSegment)expressionSegment).setAlias(alias);
+                return offset+1;
+            }
+        }
+        return offset;
     }
 }
