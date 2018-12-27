bf585f7a6fa7de6fe37d2684712a3f01b818735a
tristaZero <hannkenn@163.com>
2018-12-27 18:18:05 +0800

    MODIFY  createShardingMetaData()

--- sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/runtime/schema/MasterSlaveSchema.java
+++ sharding-proxy/src/main/java/io/shardingsphere/shardingproxy/runtime/schema/MasterSlaveSchema.java
@@ -62,7 +62,8 @@ public final class MasterSlaveSchema extends LogicSchema {
     private ShardingMetaData createShardingMetaData() {
         return new ShardingMetaData(getDataSourceURLs(getDataSources()), new ShardingRule(new ShardingRuleConfiguration(), getDataSources().keySet()), 
                 DatabaseType.MySQL, BackendExecutorContext.getInstance().getExecuteEngine(), new ProxyTableMetaDataConnectionManager(getBackendDataSource()), 
-                GlobalRegistry.getInstance().getShardingProperties().<Integer>getValue(ShardingPropertiesConstant.MAX_CONNECTIONS_SIZE_PER_QUERY));
+                GlobalRegistry.getInstance().getShardingProperties().<Integer>getValue(ShardingPropertiesConstant.MAX_CONNECTIONS_SIZE_PER_QUERY), 
+                GlobalRegistry.getInstance().getShardingProperties().<Boolean>getValue(ShardingPropertiesConstant.CHECK_TABLE_METADATA_ENABLED));
     }
     
     /**
