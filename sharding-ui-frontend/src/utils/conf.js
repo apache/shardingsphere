d3c4a3fd591121bdb59a3af6aeb40d47354f025c
wqzwh <wangziqihappy@163.com>
2018-12-29 10:13:08 +0800

    feature: add proxy config

--- sharding-ui-frontend/src/utils/conf.js
+++ sharding-ui-frontend/src/utils/conf.js
@@ -6,7 +6,7 @@ let HOST
 if (process.env.NODE_ENV === 'mock') {
   HOST = 'https://easy-mock.com/mock/5c1c861921d37d1c3c4dc5ae/ss-ui'
 } else {
-  HOST = 'http://47.100.190.183:8088'
+  HOST = ''
 }
 
 export default {
