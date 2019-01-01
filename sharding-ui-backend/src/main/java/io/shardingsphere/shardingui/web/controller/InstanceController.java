88fc76753a05d5499f161e59b36cf079ee687da4
beckhampu <beckhampu@163.com>
2019-01-01 20:17:15 +0800

    feature: add RESTful API of instance operation.

---
+++ sharding-ui-backend/src/main/java/io/shardingsphere/shardingui/web/controller/InstanceController.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.shardingui.web.controller;
+
+import io.shardingsphere.shardingui.common.dto.InstanceDTO;
+import io.shardingsphere.shardingui.servcie.InstanceService;
+import io.shardingsphere.shardingui.web.response.ResponseResult;
+import io.shardingsphere.shardingui.web.response.ResponseResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * RESTful API of instance operation.
+ *
+ * @author chenqingyang
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/instance")
+public class InstanceController {
+    
+    @Autowired
+    private InstanceService instanceService;
+    
+    /**
+     * Load all instances.
+     *
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseResult<Collection<InstanceDTO>> loadAllInstances() {
+        return ResponseResultUtil.build(instanceService.getALLInstance());
+    }
+    
+    /**
+     * update instance status.
+     *
+     * @param instanceDTO instance DTO
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public ResponseResult updateInstanceStatus(@RequestBody final InstanceDTO instanceDTO) {
+        instanceService.updateInstanceStatus(instanceDTO.getInstanceId(), instanceDTO.isEnabled());
+        return ResponseResultUtil.success();
+    }
+}
