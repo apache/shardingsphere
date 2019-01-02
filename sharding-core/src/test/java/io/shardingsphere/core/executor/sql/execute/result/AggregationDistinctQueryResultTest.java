c5d65b4367f0d0739fc1518425af3a65f2e0f85c
tristaZero <hannkenn@163.com>
2019-01-02 20:28:26 +0800

    add getAggregationDistinctSelectItems()

--- sharding-core/src/test/java/io/shardingsphere/core/executor/sql/execute/result/AggregationDistinctQueryResultTest.java
+++ sharding-core/src/test/java/io/shardingsphere/core/executor/sql/execute/result/AggregationDistinctQueryResultTest.java
@@ -17,7 +17,13 @@
 
 package io.shardingsphere.core.executor.sql.execute.result;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import io.shardingsphere.core.constant.AggregationType;
 import io.shardingsphere.core.merger.QueryResult;
+import io.shardingsphere.core.parsing.parser.context.selectitem.AggregationDistinctSelectItem;
+import io.shardingsphere.core.parsing.parser.context.selectitem.AggregationSelectItem;
 import lombok.SneakyThrows;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,14 +51,28 @@ public class AggregationDistinctQueryResultTest {
             when(queryResult.next()).thenReturn(true).thenReturn(false);
             when(queryResult.getColumnCount()).thenReturn(3);
             when(queryResult.getColumnLabel(1)).thenReturn("order_id");
-            when(queryResult.getColumnLabel(2)).thenReturn("order_id");
-            when(queryResult.getColumnLabel(3)).thenReturn("order_id");
+            when(queryResult.getColumnLabel(2)).thenReturn("c");
+            when(queryResult.getColumnLabel(3)).thenReturn("a");
             when(queryResult.getValue(1, Object.class)).thenReturn(10 * i);
             when(queryResult.getValue(2, Object.class)).thenReturn(10 * i);
             when(queryResult.getValue(3, Object.class)).thenReturn(10 * i);
             result.add(queryResult);
             result.add(queryResult);
         }
+        return result;
+    }
+    
+    private Collection<AggregationDistinctSelectItem> getAggregationDistinctSelectItems() {
+        Collection<AggregationDistinctSelectItem> result = new LinkedList<>();
+        AggregationDistinctSelectItem distinctCountSelectItem = new AggregationDistinctSelectItem(AggregationType.COUNT, "(DISTINCT order_id)", Optional.of("c"), "order_id");
+        AggregationDistinctSelectItem distinctAvgSelectItem = new AggregationDistinctSelectItem(AggregationType.AVG, "(DISTINCT order_id)", Optional.of("a"), "order_id");
+        distinctAvgSelectItem.getDerivedAggregationSelectItems().add(new AggregationSelectItem(AggregationType.COUNT, "(DISTINCT order_id)", Optional.of("AVG_DERIVED_COUNT_0")));
+        distinctAvgSelectItem.getDerivedAggregationSelectItems().add(new AggregationSelectItem(AggregationType.SUM, "(DISTINCT order_id)", Optional.of("AVG_DERIVED_SUM_0")));
+        result.add(distinctCountSelectItem);
+        result.add(distinctAvgSelectItem);
+        Multimap<String, Integer> columnLabelAndIndexMap = HashMultimap.create();
+        columnLabelAndIndexMap.put("c", 1);
+        columnLabelAndIndexMap.put("a", 2);
         return result;
     }
     
