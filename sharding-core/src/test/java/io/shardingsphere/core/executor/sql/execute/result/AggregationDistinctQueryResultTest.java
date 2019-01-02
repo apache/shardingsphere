f9a4b31cabbbf84f0377c21d24e521290abedae4
tristaZero <hannkenn@163.com>
2019-01-02 19:42:55 +0800

    add AggregationDistinctQueryResultTest

---
+++ sharding-core/src/test/java/io/shardingsphere/core/executor/sql/execute/result/AggregationDistinctQueryResultTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.core.executor.sql.execute.result;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AggregationDistinctQueryResultTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void testDivide() {
+    }
+    
+    @Test
+    public void testGetValue() {
+    }
+    
+    @Test
+    public void testGetValue1() {
+    }
+    
+    @Test
+    public void testGetCalendarValue() {
+    }
+    
+    @Test
+    public void testGetCalendarValue1() {
+    }
+    
+    @Test
+    public void testGetInputStream() {
+    }
+    
+    @Test
+    public void testGetInputStream1() {
+    }
+    
+    @Test
+    public void testWasNull() {
+    }
+    
+    @Test
+    public void testGetColumnCount() {
+    }
+    
+    @Test
+    public void testGetColumnLabel() {
+    }
+    
+    @Test
+    public void testGetColumnIndex() {
+    }
+}
