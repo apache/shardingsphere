f20415280af3ee159a5ac9a9bcebf352dad26a18
tristaZero <hannkenn@163.com>
2019-01-07 17:18:58 +0800

    modify GeneratedKeyCondition

--- sharding-core/src/main/java/io/shardingsphere/core/parsing/parser/clause/InsertValuesClauseParser.java
+++ sharding-core/src/main/java/io/shardingsphere/core/parsing/parser/clause/InsertValuesClauseParser.java
@@ -141,12 +141,12 @@ public abstract class InsertValuesClauseParser implements SQLClauseParser {
     
     private GeneratedKeyCondition createGeneratedKeyCondition(final Column column, final SQLExpression sqlExpression) {
         if (sqlExpression instanceof SQLPlaceholderExpression) {
-            return new GeneratedKeyCondition(column, ((SQLPlaceholderExpression) sqlExpression).getIndex(), null);
-            if (sqlExpression instanceof SQLNumberExpression) {
-                return new GeneratedKeyCondition(column, -1, ((SQLNumberExpression) sqlExpression).getNumber());
-            }
-            return new GeneratedKeyCondition(column, -1, ((SQLTextExpression) sqlExpression).getText());
+            return new GeneratedKeyCondition(column, ((SQLPlaceholderExpression) sqlExpression).getIndex());
         }
+        if (sqlExpression instanceof SQLNumberExpression) {
+            return new GeneratedKeyCondition(column, -1, ((SQLNumberExpression) sqlExpression).getNumber());
+        }
+        return new GeneratedKeyCondition(column, -1, ((SQLTextExpression) sqlExpression).getText());
     }
     
     private void skipsDoubleColon() {
