3bae462789f7f64196d110c4b7a79072df39a77f
tristaZero <hannkenn@163.com>
2019-01-10 11:25:46 +0800

    delete SHARDING_MASTER_SLAVE_RULE_YAML

--- sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/registry/config/service/ConfigurationServiceTest.java
+++ sharding-orchestration/sharding-orchestration-core/src/test/java/io/shardingsphere/orchestration/internal/registry/config/service/ConfigurationServiceTest.java
@@ -76,12 +76,6 @@ public final class ConfigurationServiceTest {
     
     private static final String MASTER_SLAVE_RULE_YAML = "masterDataSourceName: master_ds\n" + "name: ms_ds\n" + "slaveDataSourceNames:\n" + "- slave_ds_0\n" + "- slave_ds_1\n";
     
-    private static final String SHARDING_MASTER_SLAVE_RULE_YAML = "tables:\n" + "  t_order: \n" + "    actualDataNodes: db_ms_${0..1}.t_order_${0..1}\n" + "    databaseStrategy: \n"
-            + "      inline:\n" + "        shardingColumn: user_id\n" + "        algorithmExpression: db_ms_${user_id % 2}\n" + "    tableStrategy: \n" + "      inline:\n"
-            + "        shardingColumn: order_id\n" + "        algorithmExpression: t_order_${order_id % 2}\n" + "    keyGeneratorColumnName: order_id\n"
-            + "bindingTables:\n" + "  - t_order,t_order_item\n" + "masterSlaveRules:\n" + "  db_ms_0:\n" + "    masterDataSourceName: db0_master\n" + "    slaveDataSourceNames:\n"
-            + "      - db0_slave\n" + "  db_ms_1:\n" + "    masterDataSourceName: db1_master\n" + "    slaveDataSourceNames:\n" + "      - db1_slave";
-    
     private static final String AUTHENTICATION_YAML = "password: root\n" + "username: root\n";
     
     private static final String CONFIG_MAP_YAML = "{}\n";
