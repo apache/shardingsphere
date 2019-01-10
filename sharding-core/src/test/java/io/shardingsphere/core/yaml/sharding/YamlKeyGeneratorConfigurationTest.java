8f6b7be3aa9242b31ed361f72528901b982ee12f
tristaZero <hannkenn@163.com>
2019-01-10 16:32:27 +0800

    add YamlKeyGeneratorConfigurationTest

---
+++ sharding-core/src/test/java/io/shardingsphere/core/yaml/sharding/YamlKeyGeneratorConfigurationTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016-2018 shardingsphere.io.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </p>
+ */
+
+package io.shardingsphere.core.yaml.sharding;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class YamlKeyGeneratorConfigurationTest {
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void testGetKeyGenerator() {
+    }
+    
+    @Test
+    public void testGetColumn() {
+    }
+    
+    @Test
+    public void testGetType() {
+    }
+    
+    @Test
+    public void testGetClassName() {
+    }
+    
+    @Test
+    public void testGetProps() {
+    }
+    
+    @Test
+    public void testSetColumn() {
+    }
+    
+    @Test
+    public void testSetType() {
+    }
+    
+    @Test
+    public void testSetClassName() {
+    }
+    
+    @Test
+    public void testSetProps() {
+    }
+}
