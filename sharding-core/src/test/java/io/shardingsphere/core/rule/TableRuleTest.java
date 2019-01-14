602f5261b0aa4523d10c7121a8455dbf5e0d57d1
tristaZero <hannkenn@163.com>
2019-01-14 13:23:07 +0800

    change to keyGenerator

--- sharding-core/src/test/java/io/shardingsphere/core/rule/TableRuleTest.java
+++ sharding-core/src/test/java/io/shardingsphere/core/rule/TableRuleTest.java
@@ -142,7 +142,7 @@ public final class TableRuleTest {
         TableRule actual = new TableRule(createTableRuleConfig(), createShardingDataSourceNames());
         String actualString = "TableRule(logicTable=logic_table, actualDataNodes=[DataNode(dataSourceName=ds0, tableName=table_0), DataNode(dataSourceName=ds0, tableName=table_1), "
             + "DataNode(dataSourceName=ds0, tableName=table_2), DataNode(dataSourceName=ds1, tableName=table_0), DataNode(dataSourceName=ds1, tableName=table_1), "
-            + "DataNode(dataSourceName=ds1, tableName=table_2)], databaseShardingStrategy=null, tableShardingStrategy=null, generateKeyColumn=null, keyGeneratorConfig=null, logicIndex=null)";
+            + "DataNode(dataSourceName=ds1, tableName=table_2)], databaseShardingStrategy=null, tableShardingStrategy=null, generateKeyColumn=null, keyGenerator=null, logicIndex=null)";
         assertThat(actual.toString(), is(actualString));
     }
     
