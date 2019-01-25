3af6d7d04e216f5948b1a3d10f0ca728d4269295
tuohai666 <zhangyonglun@apache.org>
2019-01-25 15:48:33 +0800

    #1517, add admin

---
+++ sharding-proxy-postgres/src/main/java/org/apache/shardingsphere/shardingproxy/transport/mysql/packet/command/admin/UnsupportedCommandPacket.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.admin;
+
+import com.google.common.base.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.constant.ServerErrorCode;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.MySQLPacketPayload;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.CommandPacket;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.CommandPacketType;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.CommandResponsePackets;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.generic.ErrPacket;
+
+/**
+ * Unsupported command packet.
+ *
+ * @author zhangliang
+ */
+@RequiredArgsConstructor
+public final class UnsupportedCommandPacket implements CommandPacket {
+    
+    @Getter
+    private final int sequenceId;
+    
+    private final CommandPacketType type;
+    
+    @Override
+    public Optional<CommandResponsePackets> execute() {
+        return Optional.of(new CommandResponsePackets(new ErrPacket(getSequenceId() + 1, ServerErrorCode.ER_UNSUPPORTED_COMMAND, type)));
+    }
+    
+    @Override
+    public void write(final MySQLPacketPayload payload) {
+    }
+}
