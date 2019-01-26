de31c2132f3c49a75e2630a53132d2c95b800bf9
tristaZero <hannkenn@163.com>
2019-01-26 14:40:01 +0800

    use insertValuesListLastIndex

--- sharding-core/src/main/java/org/apache/shardingsphere/core/parsing/antlr/filler/impl/dml/InsertFiller.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/parsing/antlr/filler/impl/dml/InsertFiller.java
@@ -60,7 +60,7 @@ public final class InsertFiller implements SQLStatementFiller<InsertSegment> {
         createColumn(sqlSegment, insertStatement, shardingRule, shardingTableMetaData);
         createValue(sqlSegment, insertStatement, sql, shardingRule, shardingTableMetaData);
         insertStatement.setColumnsListLastIndex(sqlSegment.getColumnsListLastIndex());
-        insertStatement.setInsertValuesListLastPosition(sqlSegment.getInsertValuesListLastIndex() + 1);
+        insertStatement.setInsertValuesListLastIndex(sqlSegment.getInsertValuesListLastIndex());
         insertStatement.getSQLTokens().add(new InsertValuesToken(sqlSegment.getInsertValueStartIndex(), insertStatement.getTables().getSingleTableName()));
         processGeneratedKey(shardingRule, insertStatement);
         processDuplicateKey(shardingRule, sqlSegment, sqlStatement.getTables().getSingleTableName());
--- sharding-core/src/main/java/org/apache/shardingsphere/core/rewrite/SQLRewriteEngine.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/rewrite/SQLRewriteEngine.java
@@ -222,7 +222,7 @@ public final class SQLRewriteEngine {
     
     private void appendInsertValuesToken(final SQLBuilder sqlBuilder, final InsertValuesToken insertValuesToken, final int count) {
         sqlBuilder.appendPlaceholder(new InsertValuesPlaceholder(insertValuesToken.getTableName().toLowerCase(), shardingConditions));
-        appendRest(sqlBuilder, count, ((InsertStatement) sqlStatement).getInsertValuesListLastPosition());
+        appendRest(sqlBuilder, count, ((InsertStatement) sqlStatement).getInsertValuesListLastIndex() + 1);
     }
     
     private void appendLimitRowCount(final SQLBuilder sqlBuilder, final RowCountToken rowCountToken, final int count, final boolean isRewrite) {
