119d7adad34b5a13fd0ef97a38c2069e7b1a6233
tristaZero <hannkenn@163.com>
2019-01-28 15:55:29 +0800

    modify assertTableToken

--- sharding-core/src/test/java/org/apache/shardingsphere/core/parsing/integrate/asserts/token/TableTokenAssert.java
+++ sharding-core/src/test/java/org/apache/shardingsphere/core/parsing/integrate/asserts/token/TableTokenAssert.java
@@ -53,7 +53,7 @@ final class TableTokenAssert {
     }
     
     private void assertTableToken(final TableToken actual, final ExpectedTableToken expected) {
-        assertThat(assertMessage.getFullAssertMessage("Table tokens begin position assertion error: "), actual.getStartIndex(), is(expected.getBeginPosition()));
+        assertThat(assertMessage.getFullAssertMessage("Table tokens start index assertion error: "), actual.getStartIndex(), is(expected.getStartIndex()));
         if (0 != expected.getSkippedSchemaNameLength()) {
             assertThat(assertMessage.getFullAssertMessage("Table tokens skipped schema name length assertion error: "), actual.getSkippedSchemaNameLength(), is(expected.getSkippedSchemaNameLength()));
         }
