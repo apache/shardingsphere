4d319a604ed798ebc8149827a6aeddba50d1ba18
tuohai666 <zhangyonglun@apache.org>
2019-01-29 20:21:37 +0800

    #1517, refactor PostgreSQLSSLNegativePacket

--- sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/transport/postgresql/codec/PostgreSQLPacketCodec.java
+++ sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/transport/postgresql/codec/PostgreSQLPacketCodec.java
@@ -22,7 +22,7 @@ import io.netty.channel.ChannelHandlerContext;
 import org.apache.shardingsphere.shardingproxypg.transport.common.codec.PacketCodec;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.PostgreSQLPacket;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.PostgreSQLPacketPayload;
-import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.PostgreSQLComSSLNegativePacket;
+import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.PostgreSQLSSLNegativePacket;
 
 import java.util.List;
 
@@ -60,7 +60,7 @@ public final class PostgreSQLPacketCodec extends PacketCodec<PostgreSQLPacket> {
     protected void doEncode(final ChannelHandlerContext context, final PostgreSQLPacket message, final ByteBuf out) {
         try (PostgreSQLPacketPayload payload = new PostgreSQLPacketPayload(context.alloc().buffer())) {
             message.write(payload);
-            if (!(message instanceof PostgreSQLComSSLNegativePacket)) {
+            if (!(message instanceof PostgreSQLSSLNegativePacket)) {
                 out.writeByte(message.getMessageType());
                 out.writeInt(payload.getByteBuf().readableBytes() + message.PAYLOAD_LENGTH);
             }
