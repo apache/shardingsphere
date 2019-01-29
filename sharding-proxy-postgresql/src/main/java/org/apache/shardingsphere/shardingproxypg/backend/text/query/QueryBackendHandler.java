9b520d983da34d10fe8dadbe07f939bd1d7dd180
tuohai666 <zhangyonglun@apache.org>
2019-01-29 11:49:48 +0800

    #1517, refine ErrorResponsePacket

--- sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/backend/text/query/QueryBackendHandler.java
+++ sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/backend/text/query/QueryBackendHandler.java
@@ -25,7 +25,7 @@ import org.apache.shardingsphere.shardingproxypg.backend.communication.DatabaseC
 import org.apache.shardingsphere.shardingproxypg.backend.communication.jdbc.connection.BackendConnection;
 import org.apache.shardingsphere.shardingproxypg.backend.text.TextProtocolBackendHandler;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.command.PostgreSQLCommandResponsePackets;
-import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.generic.ErrorResponse;
+import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.generic.ErrorResponsePacket;
 
 import java.sql.SQLException;
 
@@ -52,7 +52,7 @@ public final class QueryBackendHandler implements TextProtocolBackendHandler {
     @Override
     public PostgreSQLCommandResponsePackets execute() {
         if (null == backendConnection.getLogicSchema()) {
-            return new PostgreSQLCommandResponsePackets(new ErrorResponse());
+            return new PostgreSQLCommandResponsePackets(new ErrorResponsePacket());
         }
         databaseCommunicationEngine = databaseCommunicationEngineFactory.newTextProtocolInstance(backendConnection.getLogicSchema(), sequenceId, sql, backendConnection, databaseType);
         return databaseCommunicationEngine.execute();
