0423f071a19eaaa011e330fa5c9fb273eb620d25
tuohai666 <zhangyonglun@apache.org>
2019-01-29 18:07:37 +0800

    #1517, add PostgreSQLBinaryStatementRegistry

---
+++ sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/transport/postgresql/packet/command/query/binary/PostgreSQLBinaryStatementRegistry.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.command.query.binary;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.shardingsphere.shardingproxypg.transport.mysql.packet.command.query.binary.BinaryStatement;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * PostgreSQL binary prepared statement registry.
+ *
+ * @author zhangyonglun
+ */
+@NoArgsConstructor(access = AccessLevel.NONE)
+public final class PostgreSQLBinaryStatementRegistry {
+    
+    private static final PostgreSQLBinaryStatementRegistry INSTANCE = new PostgreSQLBinaryStatementRegistry();
+    
+    private final ConcurrentMap<String, Integer> statementIdAssigner = new ConcurrentHashMap<>(65535, 1);
+    
+    private final ConcurrentMap<Integer, BinaryStatement> binaryStatements = new ConcurrentHashMap<>(65535, 1);
+    
+    private final AtomicInteger sequence = new AtomicInteger();
+    
+    /**
+     * Get prepared statement registry instance.
+     * 
+     * @return prepared statement registry instance
+     */
+    public static PostgreSQLBinaryStatementRegistry getInstance() {
+        return INSTANCE;
+    }
+    
+    /**
+     * Register SQL.
+     * 
+     * @param sql SQL
+     * @param parametersCount parameters count
+     * @return statement ID
+     */
+    public int register(final String sql, final int parametersCount) {
+        Integer result = statementIdAssigner.get(sql);
+        if (null != result) {
+            return result;
+        }
+        result = sequence.incrementAndGet();
+        statementIdAssigner.putIfAbsent(sql, result);
+        binaryStatements.putIfAbsent(result, new BinaryStatement(sql, parametersCount));
+        return result;
+    }
+    
+    /**
+     * Get binary prepared statement.
+     *
+     * @param statementId statement ID
+     * @return binary prepared statement
+     */
+    public BinaryStatement getBinaryStatement(final int statementId) {
+        return binaryStatements.get(statementId);
+    }
+}
