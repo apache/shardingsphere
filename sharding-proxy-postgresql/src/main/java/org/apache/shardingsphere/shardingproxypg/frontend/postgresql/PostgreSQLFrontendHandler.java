7929e9dd8a7237d0c118c84102af793bbc621a53
tuohai666 <zhangyonglun@apache.org>
2019-01-29 11:51:56 +0800

    #1517, refine AuthenticationOKPacket

--- sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/frontend/postgresql/PostgreSQLFrontendHandler.java
+++ sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/frontend/postgresql/PostgreSQLFrontendHandler.java
@@ -27,7 +27,7 @@ import org.apache.shardingsphere.shardingproxypg.runtime.ChannelRegistry;
 import org.apache.shardingsphere.shardingproxypg.runtime.GlobalRegistry;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.PostgreSQLPacketPayload;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.generic.ReadyForQueryPacket;
-import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.AuthenticationOK;
+import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.AuthenticationOKPacket;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.PostgreSQLConnectionIdGenerator;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.SSLNegative;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.handshake.StartupMessage;
@@ -70,7 +70,7 @@ public final class PostgreSQLFrontendHandler extends FrontendHandler {
             }
             getBackendConnection().setCurrentSchema(databaseName);
             // TODO send a md5 authentication request message
-            context.write(new AuthenticationOK(true));
+            context.write(new AuthenticationOKPacket(true));
             context.writeAndFlush(new ReadyForQueryPacket());
         }
     }
