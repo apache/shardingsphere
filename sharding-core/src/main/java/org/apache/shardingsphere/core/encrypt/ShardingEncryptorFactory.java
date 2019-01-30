f64faa927c45d06d69cc469f55c784f0e0345d68
tristaZero <hannkenn@163.com>
2019-01-30 13:22:25 +0800

    add ShardingEncryptorFactory

---
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/ShardingEncryptorFactory.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.encrypt;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.shardingsphere.core.encrypt.encryptor.ShardingEncryptor;
+import org.apache.shardingsphere.core.exception.ShardingConfigurationException;
+import org.apache.shardingsphere.spi.NewInstanceServiceLoader;
+
+import java.util.Collection;
+
+/**
+ * Sharding encryptor factory.
+ * 
+ * @author panjuan
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ShardingEncryptorFactory {
+    
+    
+    static {
+        NewInstanceServiceLoader.register(ShardingEncryptor.class);
+    }
+    
+    /**
+     * Create sharding encryptor.
+     * 
+     * @param encryptorType encryptor type
+     * @return encryptor instance
+     */
+    public static ShardingEncryptor newInstance(final String encryptorType) {
+        Collection<ShardingEncryptor> shardingEncryptors = loadEncryptors(encryptorType);
+        if (shardingEncryptors.isEmpty()) {
+            throw new ShardingConfigurationException("Invalid encryptor type.");
+        }
+        return shardingEncryptors.iterator().next();
+    }
+    
+    private static Collection<ShardingEncryptor> loadEncryptors(final String encryptorType) {
+        return Collections2.filter(NewInstanceServiceLoader.newServiceInstances(ShardingEncryptor.class), new Predicate<ShardingEncryptor>() {
+            
+            @Override
+            public boolean apply(final ShardingEncryptor input) {
+                return encryptorType.equalsIgnoreCase(input.getType());
+            }
+        });
+    }
+}
