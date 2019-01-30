3500e2f9e06d664aff40492903a93d00911313e0
tuohai666 <zhangyonglun@apache.org>
2019-01-30 13:55:05 +0800

    #1517, for checkstyle

--- sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/backend/text/transaction/SkipBackendHandler.java
+++ sharding-proxy-postgresql/src/main/java/org/apache/shardingsphere/shardingproxypg/backend/text/transaction/SkipBackendHandler.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.shardingproxypg.backend.text.transaction;
 
 import org.apache.shardingsphere.shardingproxypg.backend.ResultPacket;
 import org.apache.shardingsphere.shardingproxypg.backend.text.TextProtocolBackendHandler;
-import org.apache.shardingsphere.shardingproxypg.transport.mysql.packet.command.CommandResponsePackets;
 import org.apache.shardingsphere.shardingproxypg.transport.mysql.packet.generic.OKPacket;
 import org.apache.shardingsphere.shardingproxypg.transport.postgresql.packet.command.PostgreSQLCommandResponsePackets;
 
