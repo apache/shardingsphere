ed8d36901185cfda45bc14681f6e3e2c06962f0a
tristaZero <hannkenn@163.com>
2019-01-30 13:16:37 +0800

    add encryptor

--- sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/encryptor/ShardingEncryptor.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/encryptor/ShardingEncryptor.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.encrypt;
+package org.apache.shardingsphere.core.encrypt.encryptor;
 
 import java.util.Properties;
 
--- sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/encryptor/ShardingQueryAssistedEncryptor.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/encryptor/ShardingQueryAssistedEncryptor.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.encrypt;
+package org.apache.shardingsphere.core.encrypt.encryptor;
 
 /**
  * Sharding query assisted encryptor.
