e0af17348835e1d9135af8146c9eedbfccd94573
tristaZero <hannkenn@163.com>
2019-01-30 16:20:49 +0800

    use encryptorConfig

--- sharding-core/src/main/java/org/apache/shardingsphere/api/config/rule/TableRuleConfiguration.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/api/config/rule/TableRuleConfiguration.java
@@ -43,7 +43,7 @@ public final class TableRuleConfiguration implements RuleConfiguration {
     
     private KeyGeneratorConfiguration keyGeneratorConfig;
     
-    private EncryptorConfiguration encryptorConfiguration;
+    private EncryptorConfiguration encryptorConfig;
     
     private String logicIndex;
 }
