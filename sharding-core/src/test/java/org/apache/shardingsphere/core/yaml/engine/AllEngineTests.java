4151be1875083309d18b289bdb429e27cd801895
terrymanu <terrymanu@163.com>
2019-02-01 16:07:06 +0800

    add YamlEngineTest

--- sharding-core/src/test/java/org/apache/shardingsphere/core/yaml/engine/AllEngineTests.java
+++ sharding-core/src/test/java/org/apache/shardingsphere/core/yaml/engine/AllEngineTests.java
@@ -23,6 +23,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+        YamlEngineTest.class, 
         YamlEngineMasterSlaveConfigurationTest.class, 
         YamlEngineShardingConfigurationTest.class, 
         DefaultYamlRepresenterTest.class
