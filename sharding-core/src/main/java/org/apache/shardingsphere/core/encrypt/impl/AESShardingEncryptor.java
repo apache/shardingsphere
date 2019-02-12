a8bf20633b02e88a9f93f7d59c6abe923e5bfbf9
terrymanu <terrymanu@163.com>
2019-02-12 12:46:02 +0800

    refactor AESShardingEncryptor

--- sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/impl/AESShardingEncryptor.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/encrypt/impl/AESShardingEncryptor.java
@@ -17,17 +17,20 @@
 
 package org.apache.shardingsphere.core.encrypt.impl;
 
+import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.shardingsphere.core.exception.ShardingConfigurationException;
 import org.apache.shardingsphere.spi.algorithm.encrypt.ShardingEncryptor;
 
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -52,34 +55,25 @@ public final class AESShardingEncryptor implements ShardingEncryptor {
     @Override
     @SneakyThrows
     public Object encrypt(final Object plaintext) {
-        if (!hasSecretKey()) {
-            throw new ShardingConfigurationException("No available secret key for `%s`.", AESShardingEncryptor.class.getName());
-        }
-        Cipher cipher = Cipher.getInstance(getType().toUpperCase());
-        cipher.init(Cipher.ENCRYPT_MODE, generateSecretKey());
-        byte[] result = cipher.doFinal(String.valueOf(plaintext).getBytes("UTF8"));
+        byte[] result = getCipher(Cipher.ENCRYPT_MODE).doFinal(StringUtils.getBytesUtf8(String.valueOf(plaintext)));
         return Base64.encodeBase64String(result);
     }
     
     @Override
     @SneakyThrows
     public Object decrypt(final Object ciphertext) {
-        if (!hasSecretKey()) {
-            throw new ShardingConfigurationException("No available secret key for `%s`.", AESShardingEncryptor.class.getName());
-        }
-        Cipher cipher = Cipher.getInstance(getType().toUpperCase());
-        cipher.init(Cipher.DECRYPT_MODE, generateSecretKey());
-        byte[] result = Base64.decodeBase64(String.valueOf(ciphertext));
-        return new String(cipher.doFinal(result));
+        byte[] result = getCipher(Cipher.DECRYPT_MODE).doFinal(Base64.decodeBase64(String.valueOf(ciphertext)));
+        return new String(result);
     }
     
-    private boolean hasSecretKey() {
-        return null != properties.get(AES_KEY);
+    private Cipher getCipher(final int decryptMode) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        Preconditions.checkArgument(properties.containsKey(AES_KEY), "No available secret key for `%s`.", AESShardingEncryptor.class.getName());
+        Cipher result = Cipher.getInstance(getType());
+        result.init(decryptMode, new SecretKeySpec(createSecretKey(), getType()));
+        return result;
     }
     
-    @SneakyThrows
-    private Key generateSecretKey() {
-        byte[] keyValue = String.valueOf(properties.get(AES_KEY)).getBytes("UTF8");
-        return new SecretKeySpec(Arrays.copyOf(DigestUtils.sha1(keyValue), 16), getType());
+    private byte[] createSecretKey() {
+        return Arrays.copyOf(DigestUtils.sha1(properties.getProperty(AES_KEY)), 16);
     }
 }
