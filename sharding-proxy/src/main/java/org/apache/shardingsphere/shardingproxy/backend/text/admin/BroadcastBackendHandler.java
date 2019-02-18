54344ee20b4fc0a1a270a59aff4a174021583dd3
tuohai666 <zhangyonglun@apache.org>
2019-02-18 17:33:37 +0800

    #1830, for GUI and CLI compatibility

--- sharding-proxy/src/main/java/org/apache/shardingsphere/shardingproxy/backend/text/admin/BroadcastBackendHandler.java
+++ sharding-proxy/src/main/java/org/apache/shardingsphere/shardingproxy/backend/text/admin/BroadcastBackendHandler.java
@@ -55,6 +55,7 @@ public final class BroadcastBackendHandler implements TextProtocolBackendHandler
     public CommandResponsePackets execute() {
         List<DatabasePacket> packets = new LinkedList<>();
         for (String each : GlobalRegistry.getInstance().getSchemaNames()) {
+            backendConnection.setCurrentSchema(each);
             packets.addAll(databaseCommunicationEngineFactory.newTextProtocolInstance(
                     GlobalRegistry.getInstance().getLogicSchema(each), sequenceId, sql, backendConnection, databaseType).execute().getPackets());
         }
