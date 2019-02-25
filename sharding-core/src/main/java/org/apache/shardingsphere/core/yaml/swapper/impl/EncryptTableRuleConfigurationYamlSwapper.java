7a7dcdc694d22fd87d5943fa8d7fa559f7da1022
tristaZero <hannkenn@163.com>
2019-02-25 19:54:05 +0800

    add swap()

---
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/yaml/swapper/impl/EncryptTableRuleConfigurationYamlSwapper.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.yaml.swapper.impl;
+
+import org.apache.shardingsphere.api.config.encryptor.EncryptRuleConfiguration;
+import org.apache.shardingsphere.api.config.encryptor.EncryptTableRuleConfiguration;
+import org.apache.shardingsphere.core.yaml.config.encrypt.YamlEncryptRuleConfiguration;
+import org.apache.shardingsphere.core.yaml.config.encrypt.YamlEncryptTableRuleConfiguration;
+import org.apache.shardingsphere.core.yaml.config.encrypt.YamlEncryptorConfiguration;
+import org.apache.shardingsphere.core.yaml.swapper.YamlSwapper;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * Encrypt rule configuration yaml swapper.
+ *
+ * @author panjuan
+ */
+public final class EncryptTableRuleConfigurationYamlSwapper implements YamlSwapper<YamlEncryptTableRuleConfiguration, EncryptTableRuleConfiguration> {
+    
+    @Override
+    public YamlEncryptTableRuleConfiguration swap(final EncryptTableRuleConfiguration encryptTableRuleConfiguration) {
+        YamlEncryptTableRuleConfiguration result = new YamlEncryptTableRuleConfiguration();
+        result.setTable(encryptTableRuleConfiguration.getTable());
+        result.setEncryptor(new EncryptorConfigurationYamlSwapper().swap(encryptTableRuleConfiguration.getEncryptorConfig()));
+        return result;
+    }
+}
