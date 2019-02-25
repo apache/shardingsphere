0b747d8e26471308ac2bcf7377280f7f9d8c380a
tristaZero <hannkenn@163.com>
2019-02-25 10:50:32 +0800

    toStringWithoutPlaceholderWithEqual()

---
+++ sharding-core/src/test/java/org/apache/shardingsphere/core/rewrite/placeholder/EncryptWhereColumnPlaceholderTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.rewrite.placeholder;
+
+import org.apache.shardingsphere.core.constant.ShardingOperator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class EncryptWhereColumnPlaceholderTest {
+    
+    private EncryptWhereColumnPlaceholder encryptWhereColumnPlaceholder;
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void toStringWithoutPlaceholderWithEqual() {
+        Map<Integer, Comparable<?>> indexValues = new LinkedHashMap<>();
+        indexValues.put(0, "a");
+        encryptWhereColumnPlaceholder = new EncryptWhereColumnPlaceholder("table_x", "column_x", indexValues, Collections.<Integer>emptyList(), ShardingOperator.EQUAL);
+        assertThat(encryptWhereColumnPlaceholder.toString(), is("column_x = 'a'"));
+    }
+}
