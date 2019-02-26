13b0c2a60bd3c03fe423889dc3d33c8f7de3b18b
terrymanu <terrymanu@163.com>
2019-02-26 19:52:32 +0800

    for #1898, move sharding-proxy to sharding-proxy-core

---
+++ sharding-proxy/sharding-proxy-core/src/main/java/org/apache/shardingsphere/shardingproxy/transport/mysql/packet/command/admin/MySQLUnsupportedCommandPacket.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.admin;
+
+import com.google.common.base.Optional;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.shardingproxy.transport.common.packet.command.CommandResponsePackets;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.constant.MySQLServerErrorCode;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.MySQLPacketPayload;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.MySQLCommandPacket;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.command.MySQLCommandPacketType;
+import org.apache.shardingsphere.shardingproxy.transport.mysql.packet.generic.MySQLErrPacket;
+
+/**
+ * MySQL unsupported command packet.
+ *
+ * @author zhangliang
+ */
+@RequiredArgsConstructor
+public final class MySQLUnsupportedCommandPacket implements MySQLCommandPacket {
+    
+    @Getter
+    private final int sequenceId;
+    
+    private final MySQLCommandPacketType type;
+    
+    @Override
+    public Optional<CommandResponsePackets> execute() {
+        return Optional.of(new CommandResponsePackets(new MySQLErrPacket(getSequenceId() + 1, MySQLServerErrorCode.ER_UNSUPPORTED_COMMAND, type)));
+    }
+    
+    @Override
+    public void write(final MySQLPacketPayload payload) {
+    }
+}
