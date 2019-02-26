44f4beeb4dc1db4c9a5a2667ee531a7fa76683c1
tristaZero <hannkenn@163.com>
2019-02-26 18:35:40 +0800

    delete files

--- sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/datasource/EncryptDataSource.java
+++ sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/datasource/EncryptDataSource.java
@@ -17,51 +17,38 @@
 
 package org.apache.shardingsphere.shardingjdbc.jdbc.core.datasource;
 
-import lombok.Getter;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.api.config.encryptor.EncryptRuleConfiguration;
-import org.apache.shardingsphere.core.rule.EncryptRule;
-import org.apache.shardingsphere.shardingjdbc.jdbc.core.datasource.metadata.CachedDatabaseMetaData;
-import org.apache.shardingsphere.shardingjdbc.jdbc.unsupported.AbstractUnsupportedOperationDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
-
 /**
  * Encrypt data source.
  *
  * @author panjuan
  */
-@Getter
-@Slf4j
-public class EncryptDataSource extends AbstractUnsupportedOperationDataSource implements AutoCloseable {
-    
-    private final DataSource dataSource;
-
-    private final DatabaseMetaData cachedDatabaseMetaData;
-
-    private final EncryptRule encryptRule;
-
-    @SneakyThrows
-    public EncryptDataSource(final DataSource dataSource, final EncryptRuleConfiguration encryptRuleConfiguration) {
-        this.dataSource = dataSource;
-        cachedDatabaseMetaData = createCachedDatabaseMetaData(dataSource);
-        encryptRule = new EncryptRule(encryptRuleConfiguration);
-    }
-
-    private DatabaseMetaData createCachedDatabaseMetaData(final DataSource dataSource) throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            return new CachedDatabaseMetaData(connection.getMetaData());
-        }
-    }
-
-    @Override
-    @SneakyThrows
-    public final Connection getConnection() {
-        cachedDatabaseMetaData.getColumns()
-        return dataSource.getConnection();
-    }
-}
+//@Getter
+//@Slf4j
+//public class EncryptDataSource extends AbstractUnsupportedOperationDataSource implements AutoCloseable {
+//    
+//    private final DataSource dataSource;
+//
+//    private final DatabaseMetaData cachedDatabaseMetaData;
+//
+//    private final EncryptRule encryptRule;
+//
+//    @SneakyThrows
+//    public EncryptDataSource(final DataSource dataSource, final EncryptRuleConfiguration encryptRuleConfiguration) {
+//        this.dataSource = dataSource;
+//        cachedDatabaseMetaData = createCachedDatabaseMetaData(dataSource);
+//        encryptRule = new EncryptRule(encryptRuleConfiguration);
+//    }
+//
+//    private DatabaseMetaData createCachedDatabaseMetaData(final DataSource dataSource) throws SQLException {
+//        try (Connection connection = dataSource.getConnection()) {
+//            return new CachedDatabaseMetaData(connection.getMetaData());
+//        }
+//    }
+//
+//    @Override
+//    @SneakyThrows
+//    public final Connection getConnection() {
+//        cachedDatabaseMetaData.getColumns()
+//        return dataSource.getConnection();
+//    }
+//}
