c5da55cefd76d925e84affa45c49920b1d6029e8
tristaZero <hannkenn@163.com>
2019-02-27 15:21:10 +0800

    move it

--- sharding-core/src/main/java/org/apache/shardingsphere/core/optimizer/engine/sharding/insert/InsertOptimizeEngine.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/optimizer/engine/sharding/insert/InsertOptimizeEngine.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.optimizer.insert;
+package org.apache.shardingsphere.core.optimizer.engine.sharding.insert;
 
 import com.google.common.base.Optional;
 import lombok.RequiredArgsConstructor;
