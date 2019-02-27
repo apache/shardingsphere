41facf8fb652dcf0158d5bfc7b73239790a87bf8
tristaZero <hannkenn@163.com>
2019-02-27 15:22:40 +0800

    move interface

--- sharding-core/src/main/java/org/apache/shardingsphere/core/optimizer/engine/OptimizeEngine.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/optimizer/engine/OptimizeEngine.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.optimizer;
+package org.apache.shardingsphere.core.optimizer.engine;
 
 import org.apache.shardingsphere.core.optimizer.condition.ShardingConditions;
 
