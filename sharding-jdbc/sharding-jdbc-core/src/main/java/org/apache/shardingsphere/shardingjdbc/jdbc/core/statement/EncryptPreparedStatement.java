80bd0281c96df6a00bf2274ff4d40779ffac5d32
tristaZero <hannkenn@163.com>
2019-02-28 17:53:13 +0800

    resultSet.close();

--- sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptPreparedStatement.java
+++ sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptPreparedStatement.java
@@ -117,7 +117,6 @@ public final class EncryptPreparedStatement extends AbstractShardingPreparedStat
             return preparedStatement.executeUpdate();
         } finally {
             clearParameters();
-            resultSet.close();
         }
     }
     
