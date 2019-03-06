01caf6278bb390dc0f7274aaa1dac22079feccc4
acseed <acseed1991@gmail.com>
2019-03-06 14:34:32 +0800

    resolve resetLimitTokens removeLimitToken (#1974)

--- sharding-core/src/main/java/org/apache/shardingsphere/core/parsing/parser/sql/dql/select/SelectStatement.java
+++ sharding-core/src/main/java/org/apache/shardingsphere/core/parsing/parser/sql/dql/select/SelectStatement.java
@@ -38,6 +38,7 @@ import org.apache.shardingsphere.core.parsing.parser.token.SQLToken;
 import org.apache.shardingsphere.core.util.SQLUtil;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -310,17 +311,14 @@ public final class SelectStatement extends DQLStatement {
     }
     
     private void resetLimitTokens(final SelectStatement selectStatement, final List<SQLToken> limitSQLTokens) {
-        int count = 0;
-        List<Integer> toBeRemovedIndexes = new LinkedList<>();
         List<SQLToken> sqlTokens = selectStatement.getSQLTokens();
-        for (SQLToken each : sqlTokens) {
+
+        Iterator<SQLToken> iter = sqlTokens.iterator();
+        while (iter.hasNext()) {
+            SQLToken each = iter.next();
             if (each instanceof RowCountToken || each instanceof OffsetToken) {
-                toBeRemovedIndexes.add(count);
+                iter.remove();
             }
-            count++;
-        }
-        for (int each : toBeRemovedIndexes) {
-            sqlTokens.remove(each);
         }
         sqlTokens.addAll(limitSQLTokens);
     }
