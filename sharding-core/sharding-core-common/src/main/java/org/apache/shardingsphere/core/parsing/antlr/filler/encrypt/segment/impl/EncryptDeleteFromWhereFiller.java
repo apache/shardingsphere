0b82949a40e6bff19230ecc8af79ad0139efdcf8
terrymanu <terrymanu@163.com>
2019-03-08 17:58:08 +0800

    add sharding-core-common module

---
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/parsing/antlr/filler/encrypt/segment/impl/EncryptDeleteFromWhereFiller.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.parsing.antlr.filler.encrypt.segment.impl;
+
+import org.apache.shardingsphere.core.metadata.table.ShardingTableMetaData;
+import org.apache.shardingsphere.core.parsing.antlr.sql.segment.FromWhereSegment;
+import org.apache.shardingsphere.core.parsing.antlr.sql.segment.dml.DeleteFromWhereSegment;
+import org.apache.shardingsphere.core.parsing.parser.sql.SQLStatement;
+import org.apache.shardingsphere.core.parsing.parser.sql.dml.DMLStatement;
+import org.apache.shardingsphere.core.rule.EncryptRule;
+
+import java.util.Map.Entry;
+
+/**
+ * Encrypt delete from where filler.
+ *
+ * @author duhongjun
+ */
+public class EncryptDeleteFromWhereFiller extends EncryptFromWhereFiller {
+    
+    @Override
+    public void fill(final FromWhereSegment sqlSegment, final SQLStatement sqlStatement, final String sql, final EncryptRule encryptRule, final ShardingTableMetaData shardingTableMetaData) {
+        super.fill(sqlSegment, sqlStatement, sql, encryptRule, shardingTableMetaData);
+        DeleteFromWhereSegment deleteFromWhereSegment = (DeleteFromWhereSegment) sqlSegment;
+        DMLStatement dmlStatement = (DMLStatement) sqlStatement;
+        dmlStatement.setDeleteStatement(true);
+        for (Entry<String, String> each : sqlSegment.getTableAliases().entrySet()) {
+            dmlStatement.getUpdateTableAlias().put(each.getKey(), each.getValue());
+        }
+        dmlStatement.setWhereStartIndex(deleteFromWhereSegment.getWhereStartIndex());
+        dmlStatement.setWhereStopIndex(deleteFromWhereSegment.getWhereStopIndex());
+        dmlStatement.setWhereParameterStartIndex(deleteFromWhereSegment.getWhereParameterStartIndex());
+        dmlStatement.setWhereParameterEndIndex(deleteFromWhereSegment.getWhereParameterEndIndex());
+    }
+}
---
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/parsing/antlr/rule/registry/ShardingParsingRuleRegistry.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.parsing.antlr.rule.registry;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.shardingsphere.core.constant.DatabaseType;
+import org.apache.shardingsphere.core.parsing.antlr.rule.jaxb.loader.RuleDefinitionFileConstant;
+
+import java.util.List;
+
+/**
+ * Sharding parsing rule registry.
+ *
+ * @author duhongjun
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ShardingParsingRuleRegistry extends ParsingRuleRegistry {
+    
+    private static volatile ParsingRuleRegistry instance;
+    
+    /**
+     * Get singleton instance of parsing rule registry.
+     *
+     * @return instance of parsing rule registry
+     */
+    public static ParsingRuleRegistry getInstance() {
+        if (null == instance) {
+            synchronized (ShardingParsingRuleRegistry.class) {
+                if (null == instance) {
+                    instance = new ShardingParsingRuleRegistry();
+                    instance.init();
+                }
+            }
+        }
+        return instance;
+    }
+    
+    @Override
+    protected void fillSelfCommonFilePath(final DatabaseType databaseType, final List<String> fillerFilePaths, final List<String> extractorFilePaths, final List<String> sqlStateRuleFilePaths) {
+        fillerFilePaths.add(RuleDefinitionFileConstant.getShardingCommonFillerRuleDefinitionFileName());
+        if (DatabaseType.MySQL == databaseType) {
+            fillerFilePaths.add(RuleDefinitionFileConstant.getFillerRuleDefinitionFileName(RuleDefinitionFileConstant.SHARDING_ROOT_PATH, databaseType));
+        }
+    }
+    
+    @Override
+    protected String getExtractorRuleDefinitionFileName(final DatabaseType databaseType) {
+        return RuleDefinitionFileConstant.getExtractorRuleDefinitionFileName(RuleDefinitionFileConstant.SHARDING_ROOT_PATH, databaseType);
+    }
+    
+    @Override
+    protected String getStatementRuleDefinitionFileName(final DatabaseType databaseType) {
+        return RuleDefinitionFileConstant.getSQLStatementRuleDefinitionFileName(RuleDefinitionFileConstant.SHARDING_ROOT_PATH, databaseType);
+    }
+}
+
