428333b9de546a596779e6bde15b45301b88088b
tristaZero <hannkenn@163.com>
2019-03-09 21:38:01 +0800

    change package

--- sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/optimizer/engine/OptimizeEngine.java
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/optimizer/engine/OptimizeEngine.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.optimizer.engine.sharding;
+package org.apache.shardingsphere.core.optimizer.engine;
 
 import org.apache.shardingsphere.core.optimizer.result.OptimizeResult;
 
