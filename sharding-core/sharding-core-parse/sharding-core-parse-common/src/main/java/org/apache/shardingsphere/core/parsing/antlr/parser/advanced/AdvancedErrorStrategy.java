9db496fcdb4dc77bdc15b85d3a741f85aa554414
terrymanu <terrymanu@163.com>
2019-03-14 14:28:29 +0800

    for #1864, move parser.advance from spi to common module

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parsing/antlr/parser/advanced/AdvancedErrorStrategy.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parsing/antlr/parser/advanced/AdvancedErrorStrategy.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.core.parsing.advanced;
+package org.apache.shardingsphere.core.parsing.antlr.parser.advanced;
 
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.CommonToken;
