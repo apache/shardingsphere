98ccef6e1678b719556674ce9dee467239450c6c
tristaZero <hannkenn@163.com>
2019-03-21 20:20:07 +0800

    appendIndexAndParameters()

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/SQLRewriteEngine.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/SQLRewriteEngine.java
@@ -420,7 +420,7 @@ public final class SQLRewriteEngine {
         List<Comparable<?>> encryptAssistedColumnValues = shardingEncryptor instanceof ShardingQueryAssistedEncryptor 
                 ? getEncryptAssistedColumnValues((ShardingQueryAssistedEncryptor) shardingEncryptor, originalColumnValues) : new LinkedList<Comparable<?>>();
         encryptParameters(getPositionIndexesFromUpdateItem(encryptColumnToken), encryptColumnValues);
-        fillParameters(encryptColumnToken, encryptAssistedColumnValues);
+        appendIndexAndParameters(encryptColumnToken, encryptAssistedColumnValues);
         return shardingEncryptor instanceof ShardingQueryAssistedEncryptor ? getEncryptUpdateItemColumnPlaceholder(encryptColumnToken, encryptColumnValues, encryptAssistedColumnValues) 
                 : getEncryptUpdateItemColumnPlaceholder(encryptColumnToken, encryptColumnValues);
     }
@@ -446,14 +446,14 @@ public final class SQLRewriteEngine {
         return new LinkedHashMap<>();
     }
     
-    private void fillParameters(final EncryptColumnToken encryptColumnToken, final List<Comparable<?>> encryptAssistedColumnValues) {
+    private void appendIndexAndParameters(final EncryptColumnToken encryptColumnToken, final List<Comparable<?>> encryptAssistedColumnValues) {
         if (encryptAssistedColumnValues.isEmpty()) {
             return;
         }
         if (!isUsingParameters(encryptColumnToken)) {
             return;
         }
-        parameters.add(getEncryptAssistedParameterIndex(encryptColumnToken), encryptAssistedColumnValues.get(0));
+        appendedIndexAndParameters.put(getEncryptAssistedParameterIndex(encryptColumnToken), encryptAssistedColumnValues.get(0));
     }
     
     private boolean isUsingParameters(final EncryptColumnToken encryptColumnToken) {
