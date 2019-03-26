3221f195d47af28bd9590cb44aaeb7723194f72c
tristaZero <hannkenn@163.com>
2019-03-26 13:12:55 +0800

    EncryptPreparedStatementTest

---
+++ sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptPreparedStatementTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.shardingjdbc.jdbc.core.statement;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public final class EncryptPreparedStatementTest {
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @Test
+    public void testExecuteQuery() {
+    }
+    
+    @Test
+    public void testGetResultSet() {
+    }
+    
+    @Test
+    public void testExecuteUpdate() {
+    }
+    
+    @Test
+    public void testExecute() {
+    }
+    
+    @Test
+    public void testAddBatch() {
+    }
+    
+    @Test
+    public void testExecuteBatch() {
+    }
+    
+    @Test
+    public void testClearBatch() {
+    }
+    
+    @Test
+    public void testGetGeneratedKeys() {
+    }
+    
+    @Test
+    public void testGetConnection() {
+    }
+    
+    @Test
+    public void testGetResultSetConcurrency() {
+    }
+    
+    @Test
+    public void testGetResultSetType() {
+    }
+    
+    @Test
+    public void testGetResultSetHoldability() {
+    }
+    
+    @Test
+    public void testIsAccumulate() {
+    }
+    
+    @Test
+    public void testGetRoutedStatements() {
+    }
+}
