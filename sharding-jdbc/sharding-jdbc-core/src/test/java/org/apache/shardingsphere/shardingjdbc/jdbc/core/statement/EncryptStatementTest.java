3a194580e2007a38cbcd76dcc1f36d1c4df67b8b
tristaZero <hannkenn@163.com>
2019-03-27 20:35:46 +0800

    assertDeleteWithExecute()

--- sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptStatementTest.java
+++ sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptStatementTest.java
@@ -17,16 +17,94 @@
 
 package org.apache.shardingsphere.shardingjdbc.jdbc.core.statement;
 
-import org.junit.After;
+import org.apache.shardingsphere.shardingjdbc.common.base.AbstractEncryptJDBCDatabaseAndTableTest;
+import org.apache.shardingsphere.shardingjdbc.jdbc.core.connection.EncryptConnection;
 import org.junit.Before;
+import org.junit.Test;
 
-public final class EncryptStatementTest {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public final class EncryptStatementTest extends AbstractEncryptJDBCDatabaseAndTableTest {
+    private static final String INSERT_SQL = "insert into t_query_encrypt(id, pwd) values(2,'b')";
+    
+    private static final String DELETE_SQL = "delete from t_query_encrypt where pwd = 'a' and id = 1";
+    
+    private static final String UPDATE_SQL = "update t_query_encrypt set pwd =? where pwd = ?";
+    
+    private static final String SELECT_SQL = "select * from t_query_encrypt where pwd = ? ";
+    
+    private static final String SELECT_ALL_SQL = "select id, pwd, assist_pwd from t_query_encrypt";
+    
+    private EncryptConnection encryptConnection;
     
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        encryptConnection = getEncryptDataSource().getConnection();
     }
     
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void assertInsertWithExecute() throws SQLException {
+        try (Statement statement = encryptConnection.createStatement()) {
+            statement.execute(INSERT_SQL);
+        }
+        assertResultSet(3, 2, "encryptValue", "assistedEncryptValue");
+    }
+    
+    @Test
+    public void assertDeleteWithExecute() throws SQLException {
+        try (Statement statement = encryptConnection.createStatement()) {
+            statement.execute(DELETE_SQL);
+        }
+        assertResultSet(1, 5, "encryptValue", "assistedEncryptValue");
+    }
+    
+    @Test
+    public void assertUpdateWithExecuteUpdate() throws SQLException {
+        int result;
+        try (PreparedStatement statement = encryptConnection.prepareStatement(UPDATE_SQL)) {
+            statement.setObject(1, 'f');
+            statement.setObject(2, 'a');
+            result = statement.executeUpdate();
+        }
+        assertThat(result, is(2));
+        assertResultSet(2, 1, "encryptValue", "assistedEncryptValue");
+    }
+    
+    @Test
+    public void assertSelectWithExecuteQuery() throws SQLException {
+        try (PreparedStatement statement = encryptConnection.prepareStatement(SELECT_SQL)) {
+            statement.setObject(1, 'a');
+            ResultSet resultSet = statement.executeQuery();
+            assertTrue(resultSet.next());
+            assertThat(resultSet.getInt(1), is(1));
+            assertThat(resultSet.getString(2), is("decryptValue"));
+            assertTrue(resultSet.next());
+            assertThat(resultSet.getInt(1), is(5));
+            assertThat(resultSet.getString(2), is("decryptValue"));
+        }
+    }
+    
+    private void assertResultSet(final int resultSetCount, final int id, final Object pwd, final Object assistPwd) throws SQLException {
+        try (Connection conn = getDatabaseTypeMap().values().iterator().next().values().iterator().next().getConnection();
+             Statement stmt = conn.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery(SELECT_ALL_SQL);
+            int count = 1;
+            while (resultSet.next()) {
+                if (id == count) {
+                    assertThat(pwd, is(resultSet.getObject("pwd")));
+                    assertThat(assistPwd, is(resultSet.getObject("assist_pwd")));
+                }
+                count += 1;
+            }
+            assertThat(count - 1, is(resultSetCount));
+        }
     }
 }
