425977db6dd23a26de774d4d3b1419f6ff5de695
tristaZero <hannkenn@163.com>
2019-03-27 14:03:33 +0800

    reconstruct it

--- sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/common/base/AbstractShardingJDBCDatabaseAndTableTest.java
+++ sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/common/base/AbstractShardingJDBCDatabaseAndTableTest.java
@@ -18,11 +18,12 @@
 package org.apache.shardingsphere.shardingjdbc.common.base;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
 import org.apache.shardingsphere.api.config.sharding.KeyGeneratorConfiguration;
 import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
 import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
 import org.apache.shardingsphere.api.config.sharding.strategy.StandardShardingStrategyConfiguration;
-import org.apache.shardingsphere.core.constant.DatabaseType;
 import org.apache.shardingsphere.core.rule.ShardingRule;
 import org.apache.shardingsphere.shardingjdbc.fixture.PreciseOrderShardingAlgorithm;
 import org.apache.shardingsphere.shardingjdbc.fixture.RangeOrderShardingAlgorithm;
@@ -31,6 +32,7 @@ import org.apache.shardingsphere.shardingjdbc.jdbc.core.datasource.ShardingDataS
 import org.h2.tools.RunScript;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 import javax.sql.DataSource;
 import java.io.InputStreamReader;
@@ -39,54 +41,61 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 public abstract class AbstractShardingJDBCDatabaseAndTableTest extends AbstractSQLTest {
     
-    private static final List<String> DB_NAMES = Arrays.asList("jdbc_0", "jdbc_1");
-    
     private static ShardingDataSource shardingDataSource;
     
+    private static final List<String> SHARDING_DB_NAMES = Arrays.asList("jdbc_0", "jdbc_1");
+    
+    @BeforeClass
+    public static void initShardingDataSources() throws SQLException {
+        if (null != shardingDataSource) {
+            return;
+        }
+        Map<String, DataSource> dataSources = getDataSources();
+        final ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
+        List<String> orderActualDataNodes = new LinkedList<>();
+        for (String dataSourceName : dataSources.keySet()) {
+            orderActualDataNodes.add(dataSourceName + ".t_order_${0..1}");
+        }
+        TableRuleConfiguration orderTableRuleConfig = new TableRuleConfiguration("t_order", Joiner.on(",").join(orderActualDataNodes));
+        shardingRuleConfig.getTableRuleConfigs().add(orderTableRuleConfig);
+        List<String> orderItemActualDataNodes = new LinkedList<>();
+        for (String dataSourceName : dataSources.keySet()) {
+            orderItemActualDataNodes.add(dataSourceName + ".t_order_item_${0..1}");
+        }
+        TableRuleConfiguration orderItemTableRuleConfig = new TableRuleConfiguration("t_order_item", Joiner.on(",").join(orderItemActualDataNodes));
+        orderItemTableRuleConfig.setKeyGeneratorConfig(new KeyGeneratorConfiguration("INCREMENT", "item_id", new Properties()));
+        shardingRuleConfig.getTableRuleConfigs().add(orderItemTableRuleConfig);
+        TableRuleConfiguration configTableRuleConfig = new TableRuleConfiguration("t_config");
+        shardingRuleConfig.getTableRuleConfigs().add(configTableRuleConfig);
+        shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
+        shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("order_id", new PreciseOrderShardingAlgorithm(), new RangeOrderShardingAlgorithm()));
+        shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new StandardShardingStrategyConfiguration("user_id", new PreciseOrderShardingAlgorithm(), new RangeOrderShardingAlgorithm()));
+        ShardingRule shardingRule = new ShardingRule(shardingRuleConfig, dataSources.keySet());
+        shardingDataSource = new ShardingDataSource(dataSources, shardingRule);
+    }
+    
+    private static Map<String, DataSource> getDataSources() {
+        return Maps.filterKeys(getDatabaseTypeMap().values().iterator().next(), new Predicate<String>() {
+            
+            @Override
+            public boolean apply(final String input) {
+                return SHARDING_DB_NAMES.contains(input);
+            }
+        });
+    }
+    
     @Before
-    public void cleanAndInitTable() {
+    public void initTable() {
         try {
             ShardingConnection conn = shardingDataSource.getConnection();
             RunScript.execute(conn, new InputStreamReader(AbstractSQLTest.class.getClassLoader().getResourceAsStream("integrate/cases/jdbc/jdbc_data.sql")));
             conn.close();
         } catch (final SQLException ex) {
             ex.printStackTrace();
-        }
-    }
-    
-    @Before
-    public void initShardingDataSources() throws SQLException {
-        if (null != shardingDataSource) {
-            return;
-        }
-        Map<DatabaseType, Map<String, DataSource>> dataSourceMap = createDataSourceMap(DB_NAMES);
-        for (Entry<DatabaseType, Map<String, DataSource>> entry : dataSourceMap.entrySet()) {
-            final ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
-            List<String> orderActualDataNodes = new LinkedList<>();
-            for (String dataSourceName : entry.getValue().keySet()) {
-                orderActualDataNodes.add(dataSourceName + ".t_order_${0..1}");
-            }
-            TableRuleConfiguration orderTableRuleConfig = new TableRuleConfiguration("t_order", Joiner.on(",").join(orderActualDataNodes));
-            shardingRuleConfig.getTableRuleConfigs().add(orderTableRuleConfig);
-            List<String> orderItemActualDataNodes = new LinkedList<>();
-            for (String dataSourceName : entry.getValue().keySet()) {
-                orderItemActualDataNodes.add(dataSourceName + ".t_order_item_${0..1}");
-            }
-            TableRuleConfiguration orderItemTableRuleConfig = new TableRuleConfiguration("t_order_item", Joiner.on(",").join(orderItemActualDataNodes));
-            orderItemTableRuleConfig.setKeyGeneratorConfig(new KeyGeneratorConfiguration("INCREMENT", "item_id", new Properties()));
-            shardingRuleConfig.getTableRuleConfigs().add(orderItemTableRuleConfig);
-            TableRuleConfiguration configTableRuleConfig = new TableRuleConfiguration("t_config");
-            shardingRuleConfig.getTableRuleConfigs().add(configTableRuleConfig);
-            shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
-            shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("order_id", new PreciseOrderShardingAlgorithm(), new RangeOrderShardingAlgorithm()));
-            shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new StandardShardingStrategyConfiguration("user_id", new PreciseOrderShardingAlgorithm(), new RangeOrderShardingAlgorithm()));
-            ShardingRule shardingRule = new ShardingRule(shardingRuleConfig, entry.getValue().keySet());
-            shardingDataSource = new ShardingDataSource(entry.getValue(), shardingRule);
         }
     }
 
