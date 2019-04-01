5a3c5b3034f593b57e02eaa0e272b658fa6a0e7c
tristaZero <hannkenn@163.com>
2019-04-01 15:04:26 +0800

    modify EncryptorBeanDefinitionParserTag

--- sharding-spring/sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/main/java/org/apache/shardingsphere/shardingjdbc/spring/namespace/constants/EncryptorBeanDefinitionParserTag.java
+++ sharding-spring/sharding-jdbc-spring/sharding-jdbc-spring-namespace/src/main/java/org/apache/shardingsphere/shardingjdbc/spring/namespace/constants/EncryptorBeanDefinitionParserTag.java
@@ -28,25 +28,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class EncryptorBeanDefinitionParserTag {
     
-    public static final String STANDARD_STRATEGY_ROOT_TAG = "standard-strategy";
+    public static final String ENCRYPTOR_TYPE_ATTRIBUTE = "type";
     
-    public static final String COMPLEX_STRATEGY_ROOT_TAG = "complex-strategy";
+    public static final String ENCRYPTOR_COLUMNS_ATTRIBUTE = "columns";
     
-    public static final String INLINE_STRATEGY_ROOT_TAG = "inline-strategy";
+    public static final String ENCRYPTOR_ASSISTED_QUERY_COLUMNS_ATTRIBUTE = "assisted-query-columns";
     
-    public static final String HINT_STRATEGY_ROOT_TAG = "hint-strategy";
-    
-    public static final String NONE_STRATEGY_ROOT_TAG = "none-strategy";
-    
-    public static final String SHARDING_COLUMN_ATTRIBUTE = "sharding-column";
-    
-    public static final String SHARDING_COLUMNS_ATTRIBUTE = "sharding-columns";
-    
-    public static final String ALGORITHM_REF_ATTRIBUTE = "algorithm-ref";
-    
-    public static final String PRECISE_ALGORITHM_REF_ATTRIBUTE = "precise-algorithm-ref";
-    
-    public static final String RANGE_ALGORITHM_REF_ATTRIBUTE = "range-algorithm-ref";
-    
-    public static final String ALGORITHM_EXPRESSION_ATTRIBUTE = "algorithm-expression";
+    public static final String ENCRYPTOR_PROPERTY_REF_ATTRIBUTE = "props-ref";
 }
