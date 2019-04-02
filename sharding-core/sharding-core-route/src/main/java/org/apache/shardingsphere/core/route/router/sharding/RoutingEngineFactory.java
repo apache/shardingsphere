7b6ab31948242b5a9fd9d461dcfe909ce17bf64e
terrymanu <terrymanu@163.com>
2019-04-02 18:09:40 +0800

    fix #2153, use unicast route for show tables

--- sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/RoutingEngineFactory.java
+++ sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/RoutingEngineFactory.java
@@ -24,8 +24,6 @@ import org.apache.shardingsphere.core.metadata.datasource.ShardingDataSourceMeta
 import org.apache.shardingsphere.core.optimize.result.OptimizeResult;
 import org.apache.shardingsphere.core.parse.antlr.sql.statement.dcl.DCLStatement;
 import org.apache.shardingsphere.core.parse.parser.dialect.mysql.statement.ShowDatabasesStatement;
-import org.apache.shardingsphere.core.parse.parser.dialect.mysql.statement.ShowTableStatusStatement;
-import org.apache.shardingsphere.core.parse.parser.dialect.mysql.statement.ShowTablesStatement;
 import org.apache.shardingsphere.core.parse.parser.dialect.mysql.statement.UseStatement;
 import org.apache.shardingsphere.core.parse.parser.dialect.postgresql.statement.ResetParamStatement;
 import org.apache.shardingsphere.core.parse.parser.dialect.postgresql.statement.SetParamStatement;
@@ -93,11 +91,10 @@ public final class RoutingEngineFactory {
     }
     
     private static RoutingEngine getDALRoutingEngine(final ShardingRule shardingRule, final SQLStatement sqlStatement, final Collection<String> tableNames) {
-        if (sqlStatement instanceof UseStatement) {
+        if (sqlStatement instanceof ShowDatabasesStatement || sqlStatement instanceof UseStatement) {
             return new IgnoreRoutingEngine();
         }
-        if (sqlStatement instanceof ShowDatabasesStatement || ((sqlStatement instanceof ShowTablesStatement || sqlStatement instanceof ShowTableStatusStatement) && tableNames.isEmpty())
-                || sqlStatement instanceof SetParamStatement || sqlStatement instanceof ResetParamStatement) {
+        if (sqlStatement instanceof SetParamStatement || sqlStatement instanceof ResetParamStatement) {
             return new DatabaseBroadcastRoutingEngine(shardingRule);
         }
         return new UnicastRoutingEngine(shardingRule, tableNames);
