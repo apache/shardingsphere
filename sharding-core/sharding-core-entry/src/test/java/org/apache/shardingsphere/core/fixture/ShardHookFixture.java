054fb621678a757bbe9330a1779378ee86f425bc
cherrylzhao <zhaojunv3@126.com>
2019-04-26 11:48:24 +0800

    for #2273, add ShardHookFixture for test.

---
+++ sharding-core/sharding-core-entry/src/test/java/org/apache/shardingsphere/core/fixture/ShardHookFixture.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.fixture;
+
+import org.apache.shardingsphere.core.hook.ShardHook;
+import org.apache.shardingsphere.core.metadata.table.ShardingTableMetaData;
+import org.apache.shardingsphere.core.route.SQLRouteResult;
+
+/**
+ * Shard hook fixture.
+ *
+ * @author zhaojun
+ */
+public class ShardHookFixture implements ShardHook {
+    
+    @Override
+    public void start(final String sql) {
+    }
+    
+    @Override
+    public void finishSuccess(final SQLRouteResult sqlRouteResult, final ShardingTableMetaData shardingTableMetaData) {
+    }
+    
+    @Override
+    public void finishFailure(final Exception cause) {
+    }
+}
