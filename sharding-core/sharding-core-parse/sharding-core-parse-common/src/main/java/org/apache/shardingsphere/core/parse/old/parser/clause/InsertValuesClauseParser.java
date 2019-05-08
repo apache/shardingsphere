d74222777d337575ad17c0c74001e0bcccf98350
tristaZero <hannkenn@163.com>
2019-05-08 16:20:40 +0800

    modify insert value token for old parser

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/old/parser/clause/InsertValuesClauseParser.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/old/parser/clause/InsertValuesClauseParser.java
@@ -90,7 +90,7 @@ public abstract class InsertValuesClauseParser implements SQLClauseParser {
         Optional<InsertValuesToken> insertValuesToken = insertStatement.findSQLToken(InsertValuesToken.class);
         Preconditions.checkState(insertValuesToken.isPresent());
         insertStatement.getSQLTokens().remove(insertValuesToken.get());
-        insertStatement.addSQLToken(new InsertValuesToken(insertValuesToken.get().getStartIndex(), insertStatement.getLogicSQL().length() - 1));
+        insertStatement.addSQLToken(new InsertValuesToken(insertValuesToken.get().getStartIndex(), insertValuesToken.get().getStopIndex()));
         do {
             lexerEngine.accept(Symbol.LEFT_PAREN);
             List<SQLExpression> sqlExpressions = new LinkedList<>();
