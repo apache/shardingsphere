9298ea66188dc5f0d915f851d47d6e00f7977c53
tristaZero <hannkenn@163.com>
2019-05-09 19:13:59 +0800

    add toString()

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/placeholder/InsertValuesPlaceholder.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/placeholder/InsertValuesPlaceholder.java
@@ -17,12 +17,16 @@
 
 package org.apache.shardingsphere.core.rewrite.placeholder;
 
+import com.google.common.base.Joiner;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.shardingsphere.core.optimize.result.insert.InsertOptimizeResultUnit;
+import org.apache.shardingsphere.core.route.type.TableUnit;
+import org.apache.shardingsphere.core.rule.DataNode;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Insert values placeholder for rewrite.
@@ -39,4 +43,32 @@ public final class InsertValuesPlaceholder implements ShardingPlaceholder {
     private final Collection<String> columnNames;
     
     private final List<InsertOptimizeResultUnit> units;
+    
+    public String toString(final TableUnit tableUnit, final Map<String, String> logicAndActualTableMap) {
+        StringBuilder result = new StringBuilder();
+        result.append(" (").append(Joiner.on(", ").join(columnNames)).append(") VALUES ");
+        appendUnits(tableUnit, result);
+        result.delete(result.length() - 2, result.length());
+        return result.toString();
+    }
+    
+    private void appendUnits(final TableUnit tableUnit, final StringBuilder result) {
+        for (InsertOptimizeResultUnit each : units) {
+            if (isToAppendInsertOptimizeResult(tableUnit, each)) {
+                result.append(each).append(", ");
+            }
+        }
+    }
+    
+    private boolean isToAppendInsertOptimizeResult(final TableUnit tableUnit, final InsertOptimizeResultUnit unit) {
+        if (unit.getDataNodes().isEmpty() || null == tableUnit) {
+            return true;
+        }
+        for (DataNode each : unit.getDataNodes()) {
+            if (tableUnit.getRoutingTable(each.getDataSourceName(), each.getTableName()).isPresent()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
