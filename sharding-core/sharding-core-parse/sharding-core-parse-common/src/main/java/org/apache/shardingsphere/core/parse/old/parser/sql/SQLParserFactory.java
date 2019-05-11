b882f3a8e2e5fac1234e3688b36e08b154ba6df6
terrymanu <terrymanu@163.com>
2019-05-12 01:07:08 +0800

    for #2084, remove 2nd generation parse engine reference for SQLParserFactory

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/old/parser/sql/SQLParserFactory.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/old/parser/sql/SQLParserFactory.java
@@ -22,16 +22,7 @@ import lombok.NoArgsConstructor;
 import org.apache.shardingsphere.core.constant.DatabaseType;
 import org.apache.shardingsphere.core.metadata.table.ShardingTableMetaData;
 import org.apache.shardingsphere.core.parse.antlr.AntlrParsingEngine;
-import org.apache.shardingsphere.core.parse.antlr.sql.statement.dml.DMLStatement;
-import org.apache.shardingsphere.core.parse.antlr.sql.statement.dml.DQLStatement;
 import org.apache.shardingsphere.core.parse.old.lexer.LexerEngine;
-import org.apache.shardingsphere.core.parse.old.lexer.token.DefaultKeyword;
-import org.apache.shardingsphere.core.parse.old.lexer.token.TokenType;
-import org.apache.shardingsphere.core.parse.old.parser.exception.SQLParsingUnsupportedException;
-import org.apache.shardingsphere.core.parse.old.parser.sql.dml.delete.DeleteParserFactory;
-import org.apache.shardingsphere.core.parse.old.parser.sql.dml.insert.InsertParserFactory;
-import org.apache.shardingsphere.core.parse.old.parser.sql.dml.select.SelectParserFactory;
-import org.apache.shardingsphere.core.parse.old.parser.sql.dml.update.UpdateParserFactory;
 import org.apache.shardingsphere.core.rule.EncryptRule;
 import org.apache.shardingsphere.core.rule.ShardingRule;
 
@@ -57,17 +48,6 @@ public final class SQLParserFactory {
      */
     public static SQLParser newInstance(
             final DatabaseType dbType, final ShardingRule shardingRule, final LexerEngine lexerEngine, final ShardingTableMetaData shardingTableMetaData, final String sql) {
-        if (DatabaseType.MySQL == dbType || DatabaseType.H2 == dbType || DatabaseType.PostgreSQL == dbType || DatabaseType.Oracle == dbType || DatabaseType.SQLServer == dbType) {
-            return new AntlrParsingEngine(dbType, sql, shardingRule, shardingTableMetaData);
-        }
-        lexerEngine.nextToken();
-        TokenType tokenType = lexerEngine.getCurrentToken().getType();
-        if (DQLStatement.isDQL(tokenType)) {
-            return getDQLParser(dbType, shardingRule, lexerEngine, shardingTableMetaData);
-        }
-        if (DMLStatement.isDML(tokenType)) {
-            return getDMLParser(dbType, sql, tokenType, shardingRule, lexerEngine, shardingTableMetaData);
-        }
         return new AntlrParsingEngine(dbType, sql, shardingRule, shardingTableMetaData);
     }
     
@@ -81,27 +61,6 @@ public final class SQLParserFactory {
      * @return sql parser
      */
     public static SQLParser newInstance(final DatabaseType dbType, final EncryptRule encryptRule, final ShardingTableMetaData shardingTableMetaData, final String sql) {
-        if (DatabaseType.MySQL == dbType || DatabaseType.H2 == dbType || DatabaseType.PostgreSQL == dbType || DatabaseType.Oracle == dbType || DatabaseType.SQLServer == dbType) {
-            return new AntlrParsingEngine(dbType, sql, encryptRule, shardingTableMetaData);
-        }
-        throw new SQLParsingUnsupportedException(String.format("Can not support %s", dbType)); 
-    }
-    
-    private static SQLParser getDQLParser(final DatabaseType dbType, final ShardingRule shardingRule, final LexerEngine lexerEngine, final ShardingTableMetaData shardingTableMetaData) {
-        return SelectParserFactory.newInstance(dbType, shardingRule, lexerEngine, shardingTableMetaData);
-    }
-    
-    private static SQLParser getDMLParser(
-            final DatabaseType dbType, final String sql, final TokenType tokenType, final ShardingRule shardingRule, final LexerEngine lexerEngine, final ShardingTableMetaData shardingTableMetaData) {
-        switch ((DefaultKeyword) tokenType) {
-            case INSERT:
-                return InsertParserFactory.newInstance(dbType, sql, shardingRule, lexerEngine, shardingTableMetaData);
-            case UPDATE:
-                return UpdateParserFactory.newInstance(dbType, shardingRule, lexerEngine);
-            case DELETE:
-                return DeleteParserFactory.newInstance(dbType, shardingRule, lexerEngine);
-            default:
-                throw new SQLParsingUnsupportedException(tokenType);
-        }
+        return new AntlrParsingEngine(dbType, sql, encryptRule, shardingTableMetaData); 
     }
 }
