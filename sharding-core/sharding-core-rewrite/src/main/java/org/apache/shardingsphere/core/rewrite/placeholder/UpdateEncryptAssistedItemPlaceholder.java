d51c4c59c85d233261de8dd0888bf80e2c8368ce
tristaZero <hannkenn@163.com>
2019-05-13 13:11:26 +0800

    modify java doc

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/placeholder/UpdateEncryptAssistedItemPlaceholder.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/placeholder/UpdateEncryptAssistedItemPlaceholder.java
@@ -22,7 +22,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
- * Encrypt update item column placeholder for rewrite.
+ * Update encrypt item placeholder for rewrite.
  *
  * @author panjuan
  */
