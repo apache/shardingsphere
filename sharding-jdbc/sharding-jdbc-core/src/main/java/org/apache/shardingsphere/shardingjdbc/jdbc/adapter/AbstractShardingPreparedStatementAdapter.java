732dbf91a66b3b2044b6d07d75112780d6e72c20
tuohai666 <zhangyonglun@apache.org>
2019-05-15 18:22:51 +0800

    #1567, cancel current plan

--- sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/adapter/AbstractShardingPreparedStatementAdapter.java
+++ sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/adapter/AbstractShardingPreparedStatementAdapter.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.shardingjdbc.jdbc.adapter;
 
 import lombok.Getter;
 import lombok.SneakyThrows;
-import org.apache.shardingsphere.core.exception.ShardingException;
 import org.apache.shardingsphere.shardingjdbc.jdbc.adapter.invocation.SetParameterMethodInvocation;
 import org.apache.shardingsphere.shardingjdbc.jdbc.unsupported.AbstractUnsupportedOperationPreparedStatement;
 
@@ -55,236 +54,197 @@ public abstract class AbstractShardingPreparedStatementAdapter extends AbstractU
     @Override
     public final void setNull(final int parameterIndex, final int sqlType) {
         setParameter(parameterIndex, null);
-        recordSetParameterForNull(new Class[]{int.class, int.class}, parameterIndex, sqlType);
     }
     
     @Override
     public final void setNull(final int parameterIndex, final int sqlType, final String typeName) {
         setParameter(parameterIndex, null);
-        recordSetParameterForNull(new Class[]{int.class, int.class, String.class}, parameterIndex, sqlType, typeName);
     }
     
     @Override
     public final void setBoolean(final int parameterIndex, final boolean x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBoolean", new Class[]{int.class, boolean.class}, parameterIndex, x);
     }
     
     @Override
     public final void setByte(final int parameterIndex, final byte x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setByte", new Class[]{int.class, byte.class}, parameterIndex, x);
     }
     
     @Override
     public final void setShort(final int parameterIndex, final short x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setShort", new Class[]{int.class, short.class}, parameterIndex, x);
     }
     
     @Override
     public final void setInt(final int parameterIndex, final int x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setInt", new Class[]{int.class, int.class}, parameterIndex, x);
     }
     
     @Override
     public final void setLong(final int parameterIndex, final long x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setLong", new Class[]{int.class, long.class}, parameterIndex, x);
     }
     
     @Override
     public final void setFloat(final int parameterIndex, final float x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setFloat", new Class[]{int.class, float.class}, parameterIndex, x);
     }
     
     @Override
     public final void setDouble(final int parameterIndex, final double x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setDouble", new Class[]{int.class, double.class}, parameterIndex, x);
     }
     
     @Override
     public final void setString(final int parameterIndex, final String x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setString", new Class[]{int.class, String.class}, parameterIndex, x);
     }
     
     @Override
     public final void setBigDecimal(final int parameterIndex, final BigDecimal x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBigDecimal", new Class[]{int.class, BigDecimal.class}, parameterIndex, x);
     }
     
     @Override
     public final void setDate(final int parameterIndex, final Date x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setDate", new Class[]{int.class, Date.class}, parameterIndex, x);
     }
     
     @Override
     public final void setDate(final int parameterIndex, final Date x, final Calendar cal) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setDate", new Class[]{int.class, Date.class, Calendar.class}, parameterIndex, x, cal);
     }
     
     @Override
     public final void setTime(final int parameterIndex, final Time x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setTime", new Class[]{int.class, Time.class}, parameterIndex, x);
     }
     
     @Override
     public final void setTime(final int parameterIndex, final Time x, final Calendar cal) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setTime", new Class[]{int.class, Time.class, Calendar.class}, parameterIndex, x, cal);
     }
     
     @Override
     public final void setTimestamp(final int parameterIndex, final Timestamp x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setTimestamp", new Class[]{int.class, Timestamp.class}, parameterIndex, x);
     }
     
     @Override
     public final void setTimestamp(final int parameterIndex, final Timestamp x, final Calendar cal) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setTimestamp", new Class[]{int.class, Timestamp.class, Calendar.class}, parameterIndex, x, cal);
     }
     
     @Override
     public final void setBytes(final int parameterIndex, final byte[] x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBytes", new Class[]{int.class, byte[].class}, parameterIndex, x);
     }
     
     @Override
     public final void setBlob(final int parameterIndex, final Blob x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBlob", new Class[]{int.class, Blob.class}, parameterIndex, x);
     }
     
     @Override
     public final void setBlob(final int parameterIndex, final InputStream x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBlob", new Class[]{int.class, InputStream.class}, parameterIndex, x);
     }
     
     @Override
     public final void setBlob(final int parameterIndex, final InputStream x, final long length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBlob", new Class[]{int.class, InputStream.class, long.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setClob(final int parameterIndex, final Clob x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setClob", new Class[]{int.class, Clob.class}, parameterIndex, x);
     }
     
     @Override
     public final void setClob(final int parameterIndex, final Reader x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setClob", new Class[]{int.class, Reader.class}, parameterIndex, x);
     }
     
     @Override
     public final void setClob(final int parameterIndex, final Reader x, final long length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setClob", new Class[]{int.class, Reader.class, long.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setAsciiStream(final int parameterIndex, final InputStream x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setAsciiStream", new Class[]{int.class, InputStream.class}, parameterIndex, x);
     }
     
     @Override
     public final void setAsciiStream(final int parameterIndex, final InputStream x, final int length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setAsciiStream", new Class[]{int.class, InputStream.class, int.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setAsciiStream(final int parameterIndex, final InputStream x, final long length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setAsciiStream", new Class[]{int.class, InputStream.class, long.class}, parameterIndex, x, length);
     }
     
     @SuppressWarnings("deprecation")
     @Override
     public final void setUnicodeStream(final int parameterIndex, final InputStream x, final int length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setUnicodeStream", new Class[]{int.class, InputStream.class, int.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setBinaryStream(final int parameterIndex, final InputStream x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBinaryStream", new Class[]{int.class, InputStream.class}, parameterIndex, x);
     }
     
     @Override
     public final void setBinaryStream(final int parameterIndex, final InputStream x, final int length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBinaryStream", new Class[]{int.class, InputStream.class, int.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setBinaryStream(final int parameterIndex, final InputStream x, final long length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setBinaryStream", new Class[]{int.class, InputStream.class, long.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setCharacterStream(final int parameterIndex, final Reader x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setCharacterStream", new Class[]{int.class, Reader.class}, parameterIndex, x);
     }
     
     @Override
     public final void setCharacterStream(final int parameterIndex, final Reader x, final int length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setCharacterStream", new Class[]{int.class, Reader.class, int.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setCharacterStream(final int parameterIndex, final Reader x, final long length) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setCharacterStream", new Class[]{int.class, Reader.class, long.class}, parameterIndex, x, length);
     }
     
     @Override
     public final void setURL(final int parameterIndex, final URL x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setURL", new Class[]{int.class, URL.class}, parameterIndex, x);
     }
     
     @Override
     public final void setSQLXML(final int parameterIndex, final SQLXML x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setSQLXML", new Class[]{int.class, SQLXML.class}, parameterIndex, x);
     }
     
     @Override
     public final void setObject(final int parameterIndex, final Object x) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setObject", new Class[]{int.class, Object.class}, parameterIndex, x);
     }
     
     @Override
     public final void setObject(final int parameterIndex, final Object x, final int targetSqlType) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setObject", new Class[]{int.class, Object.class, int.class}, parameterIndex, x, targetSqlType);
     }
     
     @Override
     public final void setObject(final int parameterIndex, final Object x, final int targetSqlType, final int scaleOrLength) {
         setParameter(parameterIndex, x);
-        recordSetParameter("setObject", new Class[]{int.class, Object.class, int.class, int.class}, parameterIndex, x, targetSqlType, scaleOrLength);
     }
     
     private void setParameter(final int parameterIndex, final Object value) {
@@ -298,33 +258,18 @@ public abstract class AbstractShardingPreparedStatementAdapter extends AbstractU
         parameters.set(parameterIndex - 1, value);
     }
     
-    private void recordSetParameter(final String methodName, final Class[] argumentTypes, final Object... arguments) {
-        try {
-            setParameterMethodInvocations.add(new SetParameterMethodInvocation(PreparedStatement.class.getMethod(methodName, argumentTypes), arguments, arguments[1]));
-        } catch (final NoSuchMethodException ex) {
-            throw new ShardingException(ex);
-        }
-    }
-    
-    private void recordSetParameterForNull(final Class[] argumentTypes, final Object... arguments) {
-        try {
-            setParameterMethodInvocations.add(new SetParameterMethodInvocation(PreparedStatement.class.getMethod("setNull", argumentTypes), arguments, null));
-        } catch (final NoSuchMethodException ex) {
-            throw new ShardingException(ex);
-        }
-    }
-    
     protected final void replaySetParameter(final PreparedStatement preparedStatement, final List<Object> parameters) {
+        setParameterMethodInvocations.clear();
         addParameters(parameters);
         for (SetParameterMethodInvocation each : setParameterMethodInvocations) {
             each.invoke(preparedStatement);
         }
-        setParameterMethodInvocations.clear();
     }
     
     private void addParameters(final List<Object> parameters) {
-        for (int i = setParameterMethodInvocations.size(); i < parameters.size(); i++) {
-            setParameter(new Class[]{int.class, Object.class}, i + 1, parameters.get(i));
+        int i = 0;
+        for (Object each : parameters) {
+            setParameter(new Class[]{int.class, Object.class}, i++ + 1, each);
         }
     }
     
