34856bd0d3a054c6fd952aa0e694f73b5eb3fd19
cherrylzhao <zhaojunv3@126.com>
2019-05-16 16:06:02 +0800

    for #2335 add init SeataATShardingTransactionManager.

---
+++ sharding-transaction/sharding-transaction-base/sharding-transaction-base-seata-at/src/main/java/org/apache/shardingsphere/transaction/base/seata/at/SeataATShardingTransactionManager.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.transaction.base.seata.at;
+
+import org.apache.shardingsphere.core.constant.DatabaseType;
+import org.apache.shardingsphere.transaction.core.ResourceDataSource;
+import org.apache.shardingsphere.transaction.core.TransactionType;
+import org.apache.shardingsphere.transaction.spi.ShardingTransactionManager;
+
+import java.sql.Connection;
+import java.util.Collection;
+
+/**
+ * Seata AT sharding transaction manager.
+ *
+ * @author zhaojun
+ */
+public final class SeataATShardingTransactionManager implements ShardingTransactionManager {
+    
+    @Override
+    public void init(final DatabaseType databaseType, final Collection<ResourceDataSource> resourceDataSources) {
+    
+    }
+    
+    @Override
+    public TransactionType getTransactionType() {
+        return TransactionType.BASE;
+    }
+    
+    @Override
+    public boolean isInTransaction() {
+        return false;
+    }
+    
+    @Override
+    public Connection getConnection(final String dataSourceName) {
+        return null;
+    }
+    
+    @Override
+    public void begin() {
+    
+    }
+    
+    @Override
+    public void commit() {
+    
+    }
+    
+    @Override
+    public void rollback() {
+    
+    }
+    
+    @Override
+    public void close() {
+    
+    }
+}
