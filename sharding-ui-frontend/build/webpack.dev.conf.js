a2aa3abaf78013e77f6b66eebebfd2b39d47808f
wqzwh <wangziqihappy@163.com>
2019-05-20 13:17:22 +0800

    refactor:Delete redundant code

--- sharding-ui-frontend/build/webpack.dev.conf.js
+++ sharding-ui-frontend/build/webpack.dev.conf.js
@@ -62,8 +62,6 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     }),
     new VueLoaderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    // new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
-    // new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
