963b42519d18a20415123cd0ae4ee2754b863300
tristaZero <hannkenn@163.com>
2019-05-30 18:34:12 +0800

    add EncryptColumnTokenGenerator

---
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/generator/EncryptColumnTokenGenerator.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.rewrite.token.generator;
+
+import org.apache.shardingsphere.core.parse.sql.segment.SQLSegment;
+import org.apache.shardingsphere.core.parse.sql.segment.dml.predicate.PredicateSegment;
+import org.apache.shardingsphere.core.parse.sql.statement.SQLStatement;
+import org.apache.shardingsphere.core.rewrite.token.pojo.AggregationDistinctToken;
+import org.apache.shardingsphere.core.rule.ShardingRule;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * Encrypt column token generator.
+ *
+ * @author panjuan
+ */
+public final class EncryptColumnTokenGenerator implements CollectionSQLTokenGenerator<ShardingRule> {
+    
+    @Override
+    public Collection<AggregationDistinctToken> generateSQLTokens(final SQLStatement sqlStatement, final ShardingRule shardingRule) {
+        Collection<AggregationDistinctToken> result = new LinkedList<>();
+        if (sqlStatement.getEncryptCondition().getOrConditions().isEmpty()) {
+            return result;
+        }
+        for (SQLSegment each : sqlStatement.getSqlSegments()) {
+            if (each instanceof PredicateSegment) {
+            }
+        }
+        return result;
+    }
+    
+}
