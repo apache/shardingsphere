830f52ead21cc489f22f248917e7c6316aa38295
tristaZero <hannkenn@163.com>
2019-05-31 17:37:58 +0800

    add isSameColumnIndex()

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/sql/statement/AbstractSQLStatement.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/sql/statement/AbstractSQLStatement.java
@@ -24,11 +24,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.apache.shardingsphere.core.constant.SQLType;
-import org.apache.shardingsphere.core.exception.ShardingException;
 import org.apache.shardingsphere.core.parse.sql.context.condition.Condition;
 import org.apache.shardingsphere.core.parse.sql.context.condition.ParseCondition;
 import org.apache.shardingsphere.core.parse.sql.context.table.Tables;
 import org.apache.shardingsphere.core.parse.sql.segment.SQLSegment;
+import org.apache.shardingsphere.core.parse.sql.segment.dml.column.ColumnSegment;
 import org.apache.shardingsphere.core.parse.sql.token.SQLToken;
 import org.apache.shardingsphere.core.parse.sql.token.impl.EncryptColumnToken;
 
@@ -95,26 +95,16 @@ public abstract class AbstractSQLStatement implements SQLStatement {
      * @return encrypt condition
      */
     public Optional<Condition> getEncryptCondition(final EncryptColumnToken encryptColumnToken) {
-        List<Condition> conditions = encryptCondition.findConditions(encryptColumnToken.getColumn());
-        if (conditions.isEmpty()) {
-            return Optional.absent();
-        }
-        if (1 == conditions.size()) {
-            return Optional.of(conditions.iterator().next());
-        }
-        return Optional.of(conditions.get(getEncryptConditionIndex(encryptColumnToken)));
-    }
-    
-    private int getEncryptConditionIndex(final EncryptColumnToken encryptColumnToken) {
-        int result = 0;
-        for (SQLToken each : sqlTokens) {
-            if (each.equals(encryptColumnToken)) {
-                return result;
-            } else if (each instanceof EncryptColumnToken) {
-                result++;
+        for (Condition each : encryptCondition.findConditions(encryptColumnToken.getColumn())) {
+            if (isSameColumnIndex(each.getColumnSegment(), encryptColumnToken)) {
+                return Optional.of(each);
             }
         }
-        throw new ShardingException("Index Out Of Bounds For sqlTokens.");
+        return Optional.absent();
+    }
+    
+    private boolean isSameColumnIndex(final ColumnSegment columnSegment, final EncryptColumnToken encryptColumnToken) {
+        return columnSegment.getStartIndex() == encryptColumnToken.getStartIndex() && columnSegment.getStopIndex() == encryptColumnToken.getStopIndex();
     }
     
     @Override
