38f92c7b11d2eb29476b2a656b3c40311fd200c4
tristaZero <hannkenn@163.com>
2019-06-02 11:55:45 +0800

    limit.getRowCount()

--- sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/ParsingSQLRouter.java
+++ sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/ParsingSQLRouter.java
@@ -198,7 +198,7 @@ public final class ParsingSQLRouter implements ShardingRouter {
             result.setOffset(new LimitValue(limit.getOffset().getValue(), limit.getOffset().getIndex(), limit.getOffset().getSqlSegment(), limit.getOffset().isBoundOpened()));
         }
         if (null != limit.getRowCount()) {
-            result.setRowCount(new LimitValue(limit.getRowCount().getValue(), limit.getRowCount().getIndex(), limit.getOffset().getSqlSegment(), limit.getRowCount().isBoundOpened()));
+            result.setRowCount(new LimitValue(limit.getRowCount().getValue(), limit.getRowCount().getIndex(), limit.getRowCount().getSqlSegment(), limit.getRowCount().isBoundOpened()));
         }
         return result; 
     }
