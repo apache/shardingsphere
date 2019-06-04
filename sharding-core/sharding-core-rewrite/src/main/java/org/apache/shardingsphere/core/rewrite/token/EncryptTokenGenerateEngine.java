2da67fd2207a07fdf8c9aea66250802bc97c5717
tristaZero <hannkenn@163.com>
2019-06-04 16:54:07 +0800

    use InsertSetEncryptValueTokenGenerator

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/EncryptTokenGenerateEngine.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/EncryptTokenGenerateEngine.java
@@ -19,6 +19,7 @@ package org.apache.shardingsphere.core.rewrite.token;
 
 import org.apache.shardingsphere.core.rewrite.token.generator.EncryptColumnTokenGenerator;
 import org.apache.shardingsphere.core.rewrite.token.generator.InsertAssistedColumnsTokenGenerator;
+import org.apache.shardingsphere.core.rewrite.token.generator.InsertSetEncryptValueTokenGenerator;
 import org.apache.shardingsphere.core.rewrite.token.generator.SQLTokenGenerator;
 import org.apache.shardingsphere.core.rule.EncryptRule;
 
@@ -37,6 +38,7 @@ public final class EncryptTokenGenerateEngine extends SQLTokenGenerateEngine<Enc
     static {
         SQL_TOKEN_GENERATORS.add(new EncryptColumnTokenGenerator());
         SQL_TOKEN_GENERATORS.add(new InsertAssistedColumnsTokenGenerator());
+        SQL_TOKEN_GENERATORS.add(new InsertSetEncryptValueTokenGenerator());
     }
     
     @Override
