8e2bb1a0e2cc3b5f40f96ce7cbb9c4d7fcd8712c
tristaZero <hannkenn@163.com>
2019-06-04 15:12:47 +0800

    add InsertGeneratedKeyToken

---
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/sql/token/impl/InsertGeneratedKeyToken.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.core.parse.sql.token.impl;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.apache.shardingsphere.core.parse.sql.token.Attachable;
+import org.apache.shardingsphere.core.parse.sql.token.SQLToken;
+
+/**
+ * Insert generated key columns token.
+ *
+ * @author panjuan
+ */
+@Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public final class InsertGeneratedKeyToken extends SQLToken implements Attachable {
+    
+    private final String column;
+    
+    private final boolean isToAddCloseParenthesis;
+    
+    public InsertGeneratedKeyToken(final int startIndex, final String column, final boolean isToAddCloseParenthesis) {
+        super(startIndex);
+        this.column = column;
+        this.isToAddCloseParenthesis = isToAddCloseParenthesis;
+    }
+}
