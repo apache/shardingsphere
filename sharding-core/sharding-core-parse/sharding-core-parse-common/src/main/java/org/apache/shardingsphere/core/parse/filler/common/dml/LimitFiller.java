aee680502fd5c6a9162cfdbac64db782853dabca
terrymanu <terrymanu@163.com>
2019-06-06 15:40:44 +0800

    for #2441, PaginationSegment => LimitSegment

--- sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/filler/common/dml/LimitFiller.java
+++ sharding-core/sharding-core-parse/sharding-core-parse-common/src/main/java/org/apache/shardingsphere/core/parse/filler/common/dml/LimitFiller.java
@@ -18,7 +18,7 @@
 package org.apache.shardingsphere.core.parse.filler.common.dml;
 
 import org.apache.shardingsphere.core.parse.filler.api.SQLSegmentFiller;
-import org.apache.shardingsphere.core.parse.sql.segment.dml.pagination.PaginationSegment;
+import org.apache.shardingsphere.core.parse.sql.segment.dml.pagination.limit.LimitSegment;
 import org.apache.shardingsphere.core.parse.sql.statement.SQLStatement;
 import org.apache.shardingsphere.core.parse.sql.statement.dml.SelectStatement;
 
@@ -27,10 +27,10 @@ import org.apache.shardingsphere.core.parse.sql.statement.dml.SelectStatement;
  *
  * @author duhongjun
  */
-public final class LimitFiller implements SQLSegmentFiller<PaginationSegment> {
+public final class LimitFiller implements SQLSegmentFiller<LimitSegment> {
     
     @Override
-    public void fill(final PaginationSegment sqlSegment, final SQLStatement sqlStatement) {
+    public void fill(final LimitSegment sqlSegment, final SQLStatement sqlStatement) {
         if (sqlSegment.getOffset().isPresent()) {
             ((SelectStatement) sqlStatement).setOffset(sqlSegment.getOffset().get());
         }
