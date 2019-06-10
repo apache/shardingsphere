33f54475614db78b6310de9e3e362c37d40b86ea
terrymanu <terrymanu@163.com>
2019-06-10 11:47:07 +0800

    for #2082, remove  DatabaseType.productName

--- sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/constant/DatabaseType.java
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/constant/DatabaseType.java
@@ -17,12 +17,6 @@
 
 package org.apache.shardingsphere.core.constant;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterators;
-
-import java.util.Arrays;
-
 /**
  * Supported database.
  * 
@@ -30,31 +24,5 @@ import java.util.Arrays;
  */
 public enum DatabaseType {
     
-    H2("H2"), MySQL("MySQL"), PostgreSQL("PostgreSQL"), Oracle("Oracle"), SQLServer("Microsoft SQL Server");
-    
-    private final String productName;
-    
-    DatabaseType(final String productName) {
-        this.productName = productName;
-    }
-    
-    /**
-     * Get database type enum via database name string.
-     * 
-     * @param databaseProductName database name string
-     * @return database enum
-     */
-    public static DatabaseType valueFrom(final String databaseProductName) {
-        Optional<DatabaseType> databaseTypeOptional = Iterators.tryFind(Arrays.asList(DatabaseType.values()).iterator(), new Predicate<DatabaseType>() {
-            
-            @Override
-            public boolean apply(final DatabaseType input) {
-                return input.productName.equals(databaseProductName);
-            }
-        });
-        if (databaseTypeOptional.isPresent()) {
-            return databaseTypeOptional.get();
-        }
-        throw new UnsupportedOperationException(String.format("Can not support database type [%s].", databaseProductName)); 
-    }
+    H2, MySQL, PostgreSQL, Oracle, SQLServer
 }
