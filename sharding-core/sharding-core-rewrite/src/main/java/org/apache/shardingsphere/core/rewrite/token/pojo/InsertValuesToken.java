5dc9ac3ccfb5e2dd1fd757f79435aec6076abdc6
tristaZero <hannkenn@163.com>
2019-07-02 17:09:45 +0800

    add insertValues

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/pojo/InsertValuesToken.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/pojo/InsertValuesToken.java
@@ -18,6 +18,14 @@
 package org.apache.shardingsphere.core.rewrite.token.pojo;
 
 import lombok.Getter;
+import org.apache.shardingsphere.core.rewrite.placeholder.Alterable;
+import org.apache.shardingsphere.core.rewrite.placeholder.InsertValuePlaceholder;
+import org.apache.shardingsphere.core.route.type.RoutingUnit;
+import org.apache.shardingsphere.core.rule.DataNode;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Insert values token.
@@ -26,12 +34,42 @@ import lombok.Getter;
  * @author panjuan
  */
 @Getter
-public final class InsertValuesToken extends SQLToken implements Substitutable {
+public final class InsertValuesToken extends SQLToken implements Substitutable, Alterable {
     
     private final int stopIndex;
+    
+    private final List<InsertValuePlaceholder> insertValues = new LinkedList<>();
     
     public InsertValuesToken(final int startIndex, final int stopIndex) {
         super(startIndex);
         this.stopIndex = stopIndex;
+    }
+    
+    @Override
+    public String toString(final RoutingUnit routingUnit, final Map<String, String> logicAndActualTables) {
+        StringBuilder result = new StringBuilder();
+        appendUnits(routingUnit, result);
+        result.delete(result.length() - 2, result.length());
+        return result.toString();
+    }
+    
+    private void appendUnits(final RoutingUnit routingUnit, final StringBuilder result) {
+        for (InsertValuePlaceholder each : insertValues) {
+            if (isToAppendInsertOptimizeResult(routingUnit, each)) {
+                result.append(each).append(", ");
+            }
+        }
+    }
+    
+    private boolean isToAppendInsertOptimizeResult(final RoutingUnit routingUnit, final InsertValuePlaceholder unit) {
+        if (unit.getDataNodes().isEmpty() || null == routingUnit) {
+            return true;
+        }
+        for (DataNode each : unit.getDataNodes()) {
+            if (routingUnit.getTableUnit(each.getDataSourceName(), each.getTableName()).isPresent()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
