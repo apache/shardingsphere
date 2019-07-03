1e6315e7194a43539124a8bf3c8b527ae2bcd0b2
tristaZero <hannkenn@163.com>
2019-07-03 14:08:26 +0800

    delete optimizedStatement

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/rewriter/sql/EncryptSQLRewriter.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/rewriter/sql/EncryptSQLRewriter.java
@@ -18,16 +18,11 @@
 package org.apache.shardingsphere.core.rewrite.rewriter.sql;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.shardingsphere.core.optimize.statement.InsertOptimizedStatement;
-import org.apache.shardingsphere.core.optimize.statement.OptimizedStatement;
-import org.apache.shardingsphere.core.optimize.statement.sharding.dml.insert.InsertOptimizeResultUnit;
-import org.apache.shardingsphere.core.parse.sql.segment.dml.expr.ExpressionSegment;
 import org.apache.shardingsphere.core.rewrite.builder.ParameterBuilder;
 import org.apache.shardingsphere.core.rewrite.builder.SQLBuilder;
 import org.apache.shardingsphere.core.rewrite.placeholder.InsertAssistedColumnsPlaceholder;
 import org.apache.shardingsphere.core.rewrite.placeholder.InsertSetAddItemsPlaceholder;
 import org.apache.shardingsphere.core.rewrite.placeholder.InsertSetEncryptValuePlaceholder;
-import org.apache.shardingsphere.core.rewrite.placeholder.InsertValuePlaceholder;
 import org.apache.shardingsphere.core.rewrite.placeholder.InsertValuesPlaceholder;
 import org.apache.shardingsphere.core.rewrite.placeholder.UpdateEncryptAssistedItemPlaceholder;
 import org.apache.shardingsphere.core.rewrite.placeholder.UpdateEncryptItemPlaceholder;
@@ -41,11 +36,6 @@ import org.apache.shardingsphere.core.rewrite.token.pojo.UpdateEncryptAssistedIt
 import org.apache.shardingsphere.core.rewrite.token.pojo.UpdateEncryptItemToken;
 import org.apache.shardingsphere.core.rewrite.token.pojo.WhereEncryptColumnToken;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * SQL rewriter encrypt.
  * 
@@ -54,16 +44,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public final class EncryptSQLRewriter implements SQLRewriter {
     
-    private final OptimizedStatement optimizedStatement;
-    
     @Override
     public void rewrite(final SQLBuilder sqlBuilder, final ParameterBuilder parameterBuilder, final SQLToken sqlToken) {
         if (sqlToken instanceof InsertValuesToken) {
-            appendInsertValuesPlaceholder(sqlBuilder, (InsertOptimizedStatement) optimizedStatement);
+            appendInsertValuesPlaceholder(sqlBuilder, (InsertValuesToken) sqlToken);
         } else if (sqlToken instanceof InsertSetEncryptValueToken) {
             appendInsertSetEncryptValuePlaceholder(sqlBuilder, (InsertSetEncryptValueToken) sqlToken);
         } else if (sqlToken instanceof InsertSetAddAssistedColumnsToken) {
-            appendInsertSetAddItemsPlaceholder(sqlBuilder, (InsertSetAddAssistedColumnsToken) sqlToken, (InsertOptimizedStatement) optimizedStatement);
+            appendInsertSetAddItemsPlaceholder(sqlBuilder, (InsertSetAddAssistedColumnsToken) sqlToken);
         } else if (sqlToken instanceof InsertAssistedColumnsToken) {
             appendInsertAssistedColumnsPlaceholder(sqlBuilder, (InsertAssistedColumnsToken) sqlToken);
         } else if (sqlToken instanceof WhereEncryptColumnToken) {
@@ -75,25 +63,16 @@ public final class EncryptSQLRewriter implements SQLRewriter {
         }
     }
     
-    private void appendInsertValuesPlaceholder(final SQLBuilder sqlBuilder, final InsertOptimizedStatement insertOptimizeResult) {
-        List<InsertValuePlaceholder> insertValues = new LinkedList<>();
-        for (InsertOptimizeResultUnit each : insertOptimizeResult.getUnits()) {
-            insertValues.add(new InsertValuePlaceholder(new ArrayList<>(each.getColumnNames()), Arrays.asList(each.getValues()), each.getDataNodes()));
-        }
-        sqlBuilder.appendPlaceholder(new InsertValuesPlaceholder(insertValues));
+    private void appendInsertValuesPlaceholder(final SQLBuilder sqlBuilder, final InsertValuesToken insertValuesToken) {
+        sqlBuilder.appendPlaceholder(new InsertValuesPlaceholder(insertValuesToken.getInsertValues()));
     }
     
     private void appendInsertSetEncryptValuePlaceholder(final SQLBuilder sqlBuilder, final InsertSetEncryptValueToken insertSetEncryptValueToken) {
         sqlBuilder.appendPlaceholder(new InsertSetEncryptValuePlaceholder(insertSetEncryptValueToken.getEncryptColumnValue()));
     }
     
-    private void appendInsertSetAddItemsPlaceholder(
-            final SQLBuilder sqlBuilder, final InsertSetAddAssistedColumnsToken insertSetAddAssistedColumnsToken, final InsertOptimizedStatement optimizedStatement) {
-        List<ExpressionSegment> columnValues = new LinkedList<>();
-        for (String each : insertSetAddAssistedColumnsToken.getColumnNames()) {
-            columnValues.add(optimizedStatement.getUnits().get(0).getColumnSQLExpression(each));
-        }
-        sqlBuilder.appendPlaceholder(new InsertSetAddItemsPlaceholder(new LinkedList<>(insertSetAddAssistedColumnsToken.getColumnNames()), columnValues));
+    private void appendInsertSetAddItemsPlaceholder(final SQLBuilder sqlBuilder, final InsertSetAddAssistedColumnsToken insertSetAddAssistedColumnsToken) {
+        sqlBuilder.appendPlaceholder(new InsertSetAddItemsPlaceholder(insertSetAddAssistedColumnsToken.getColumnNames(), insertSetAddAssistedColumnsToken.getColumnValues()));
     }
     
     private void appendInsertAssistedColumnsPlaceholder(final SQLBuilder sqlBuilder, final InsertAssistedColumnsToken insertAssistedColumnsToken) {
