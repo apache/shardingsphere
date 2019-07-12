f006aa540a6a513736de7cb4e201c88ceecaacf5
beckhampu <beckhampu@163.com>
2019-07-12 20:53:07 +0800

    adjust package name to apache

---
+++ sharding-ui/sharding-ui-backend/src/main/java/org/apache/shardingsphere/ui/web/controller/ProxyAuthenticationController.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.ui.web.controller;
+
+import org.apache.shardingsphere.ui.servcie.ProxyAuthenticationService;
+import org.apache.shardingsphere.ui.web.response.ResponseResult;
+import org.apache.shardingsphere.ui.web.response.ResponseResultUtil;
+import org.apache.shardingsphere.core.rule.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * RESTful API of sharding proxy authentication.
+ * 
+ * @author chenqingyang
+ */
+@RestController
+@RequestMapping("/api/authentication")
+public final class ProxyAuthenticationController {
+    
+    @Autowired
+    private ProxyAuthenticationService proxyAuthenticationService;
+    
+    /**
+     * Load authentication.
+     * 
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseResult<Authentication> loadAuthentication() {
+        return ResponseResultUtil.build(proxyAuthenticationService.getAuthentication());
+    }
+    
+    /**
+     * Update authentication.
+     * 
+     * @param authentication authentication
+     * @return response result
+     */
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public ResponseResult updateAuthentication(@RequestBody final Authentication authentication) {
+        proxyAuthenticationService.updateAuthentication(authentication);
+        return ResponseResultUtil.success();
+    }
+}
