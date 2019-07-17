964ea66e3580d61ea01e78c99b2099423e74b7c1
tristaZero <hannkenn@163.com>
2019-07-17 19:27:37 +0800

    add @Getter

--- sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/strategy/encrypt/EncryptColumn.java
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/strategy/encrypt/EncryptColumn.java
@@ -17,6 +17,7 @@
 
 package org.apache.shardingsphere.core.strategy.encrypt;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -25,6 +26,7 @@ import lombok.RequiredArgsConstructor;
  * @author panjuan
  */
 @RequiredArgsConstructor
+@Getter
 public final class EncryptColumn {
     
     private final String plainColumn;
