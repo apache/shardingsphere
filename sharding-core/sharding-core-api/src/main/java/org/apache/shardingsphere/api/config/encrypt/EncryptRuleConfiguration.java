514533a8e32845143bab2d1ef4e19ac16237f351
tristaZero <hannkenn@163.com>
2019-07-19 11:39:37 +0800

    delete isValidEncryptRuleConfig()

--- sharding-core/sharding-core-api/src/main/java/org/apache/shardingsphere/api/config/encrypt/EncryptRuleConfiguration.java
+++ sharding-core/sharding-core-api/src/main/java/org/apache/shardingsphere/api/config/encrypt/EncryptRuleConfiguration.java
@@ -17,13 +17,10 @@
 
 package org.apache.shardingsphere.api.config.encrypt;
 
-import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Collections2;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.apache.shardingsphere.api.config.RuleConfiguration;
 
-import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,6 +29,7 @@ import java.util.Map;
  *
  * @author panjuan
  */
+@RequiredArgsConstructor
 @Getter
 public final class EncryptRuleConfiguration implements RuleConfiguration {
     
@@ -41,31 +39,5 @@ public final class EncryptRuleConfiguration implements RuleConfiguration {
     
     public EncryptRuleConfiguration() {
         this(new LinkedHashMap<String, EncryptorRuleConfiguration>(), new LinkedHashMap<String, EncryptTableRuleConfiguration>());
-    }
-    
-    public EncryptRuleConfiguration(final Map<String, EncryptorRuleConfiguration> encryptors, final Map<String, EncryptTableRuleConfiguration> tables) {
-        this.encryptors = encryptors;
-        this.tables = tables;
-        Preconditions.checkArgument(isValidConfigurations(), "Invalid encryptorNames are used in EncryptTableRuleConfigurations.");
-    }
-    
-    private boolean isValidConfigurations() {
-        return (encryptors.isEmpty() && tables.isEmpty()) || isUsingValidEncryptNames();
-    }
-    
-    private boolean isUsingValidEncryptNames() {
-        for (EncryptTableRuleConfiguration each : tables.values()) {
-            Collection<String> encryptors = Collections2.transform(each.getColumns().values(), new Function<EncryptColumnRuleConfiguration, String>() {
-                
-                @Override
-                public String apply(final EncryptColumnRuleConfiguration input) {
-                    return input.getEncryptor();
-                }
-            });
-            if (!this.encryptors.keySet().containsAll(encryptors)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
