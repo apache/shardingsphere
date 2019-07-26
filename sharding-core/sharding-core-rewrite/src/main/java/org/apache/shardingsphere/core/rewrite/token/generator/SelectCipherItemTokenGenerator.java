6dee676b0d18ffb588b6ce29ff348511af88b723
tristaZero <hannkenn@163.com>
2019-07-26 15:38:59 +0800

    createSelectCipherItemTokens

--- sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/generator/SelectCipherItemTokenGenerator.java
+++ sharding-core/sharding-core-rewrite/src/main/java/org/apache/shardingsphere/core/rewrite/token/generator/SelectCipherItemTokenGenerator.java
@@ -19,34 +19,70 @@ package org.apache.shardingsphere.core.rewrite.token.generator;
 
 import com.google.common.base.Optional;
 import org.apache.shardingsphere.core.optimize.api.statement.OptimizedStatement;
-import org.apache.shardingsphere.core.parse.sql.segment.dml.item.AggregationDistinctSelectItemSegment;
+import org.apache.shardingsphere.core.optimize.sharding.statement.dml.ShardingSelectOptimizedStatement;
+import org.apache.shardingsphere.core.parse.sql.segment.dml.item.ColumnSelectItemSegment;
+import org.apache.shardingsphere.core.parse.sql.segment.dml.item.SelectItemSegment;
 import org.apache.shardingsphere.core.parse.sql.segment.dml.item.SelectItemsSegment;
-import org.apache.shardingsphere.core.parse.sql.statement.dml.SelectStatement;
 import org.apache.shardingsphere.core.rewrite.builder.ParameterBuilder;
-import org.apache.shardingsphere.core.rewrite.token.pojo.SelectItemPrefixToken;
-import org.apache.shardingsphere.core.rule.ShardingRule;
+import org.apache.shardingsphere.core.rewrite.token.pojo.SelectCipherItemToken;
+import org.apache.shardingsphere.core.rule.EncryptRule;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 
 /**
  * Select cipher item token generator.
  *
  * @author panjuan
  */
-public final class SelectCipherItemTokenGenerator implements OptionalSQLTokenGenerator<ShardingRule>, IgnoreForSingleRoute {
+public final class SelectCipherItemTokenGenerator implements CollectionSQLTokenGenerator<EncryptRule> {
+    
+    private EncryptRule encryptRule;
+    
+    private OptimizedStatement optimizedStatement;
     
     @Override
-    public Optional<SelectItemPrefixToken> generateSQLToken(
-            final OptimizedStatement optimizedStatement, final ParameterBuilder parameterBuilder, final ShardingRule shardingRule, final boolean isQueryWithCipherColumn) {
-        if (!(optimizedStatement.getSQLStatement() instanceof SelectStatement)) {
-            return Optional.absent();
+    public Collection<SelectCipherItemToken> generateSQLTokens(final OptimizedStatement optimizedStatement, 
+                                                               final ParameterBuilder parameterBuilder, final EncryptRule rule, final boolean isQueryWithCipherColumn) {
+        if (!isNeedToGenerateSQLToken(optimizedStatement)) {
+            return Collections.emptyList();
         }
-        Optional<SelectItemsSegment> selectItemsSegment = optimizedStatement.getSQLStatement().findSQLSegment(SelectItemsSegment.class);
-        if (selectItemsSegment.isPresent() && isContainAggregationDistinctSelectItemSegment(selectItemsSegment.get())) {
-            return Optional.of(new SelectItemPrefixToken(selectItemsSegment.get().getStartIndex()));
-        }
-        return Optional.absent();
+        initParameters(rule, optimizedStatement);
+        return createSelectCipherItemTokens();
     }
     
-    private boolean isContainAggregationDistinctSelectItemSegment(final SelectItemsSegment selectItemsSegment) {
-        return !selectItemsSegment.findSelectItemSegments(AggregationDistinctSelectItemSegment.class).isEmpty();
+    private boolean isNeedToGenerateSQLToken(final OptimizedStatement optimizedStatement) {
+        if (!(optimizedStatement instanceof ShardingSelectOptimizedStatement)) {
+            return false;
+        }
+        Optional<SelectItemsSegment> selectItemsSegment = optimizedStatement.getSQLStatement().findSQLSegment(SelectItemsSegment.class);
+        return selectItemsSegment.isPresent() && !selectItemsSegment.get().getSelectItems().isEmpty();
+    }
+    
+    private void initParameters(final EncryptRule rule, final OptimizedStatement optimizedStatement) {
+        encryptRule = rule;
+        this.optimizedStatement = optimizedStatement;
+    }
+    
+    private Collection<SelectCipherItemToken> createSelectCipherItemTokens() {
+        Collection<SelectCipherItemToken> result = new LinkedList<>();
+        SelectItemsSegment selectItemsSegment = optimizedStatement.getSQLStatement().findSQLSegment(SelectItemsSegment.class).get();
+        Collection<String> logicColumns = encryptRule.getCipherColumns(optimizedStatement.getTables().getSingleTableName());
+        for (SelectItemSegment each : selectItemsSegment.getSelectItems()) {
+            if (isLogicColumn(each, logicColumns)) {
+                result.add(new SelectCipherItemToken(each.getStartIndex(), each.getStopIndex(), ((ColumnSelectItemSegment) each).getName(), getColumnAlias(each)));
+            }
+        }
+        return result;
+    }
+    
+    private boolean isLogicColumn(final SelectItemSegment each, final Collection<String> logicColumns) {
+        return each instanceof ColumnSelectItemSegment && logicColumns.contains(((ColumnSelectItemSegment) each).getName());
+    }
+    
+    private String getColumnAlias(final SelectItemSegment selectItemSegment) {
+        Optional<String> alias = ((ColumnSelectItemSegment) selectItemSegment).getAlias();
+        return alias.isPresent() ? alias.get() : "";
     }
 }
