1a1b925d9f85cd329e6facd4cc21d18e979b230a
sunhaisheng <sunhaisheng@guazi.com>
2019-09-05 13:17:56 +0800

    make parameter to final.

--- sharding-core/sharding-core-optimize/src/main/java/org/apache/shardingsphere/core/optimize/api/segment/Tables.java
+++ sharding-core/sharding-core-optimize/src/main/java/org/apache/shardingsphere/core/optimize/api/segment/Tables.java
@@ -66,7 +66,7 @@ public final class Tables {
         }
     }
     
-    private Optional<String> getAlias(TableAvailable table) {
+    private Optional<String> getAlias(final TableAvailable table) {
         return table instanceof AliasAvailable ? ((AliasAvailable) table).getAlias() : Optional.<String>absent();
     }
     
