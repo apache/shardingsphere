080632f37c3143a6bcebbe8138245c0b1851f556
wqzwh <wangziqihappy@163.com>
2019-09-15 09:59:47 +0800

    feat[sharding-ui-frontend]:add Menu Componet unit test

---
+++ sharding-ui/sharding-ui-frontend/test/specs/Menu.spec.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai'
+import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import VueI18n from 'vue-i18n'
+import Vuex from 'vuex'
+import Menu from '../../src/components/Menu/index.vue'
+import Language from '../../src/lang/index'
+import store from '../../src/store'
+import router from '../../src/router'
+
+const localVue = createLocalVue()
+
+localVue.use(VueI18n)
+localVue.use(Vuex)
+
+// language setting init
+const navLang = navigator.language
+const localLang = navLang === 'zh-CN' || navLang === 'en-US' ? navLang : false
+const lang = window.localStorage.getItem('language') || localLang || 'zh-CN'
+localVue.config.lang = lang
+
+// language setting
+const locales = Language
+const mergeZH = locales['zh-CN']
+const mergeEN = locales['en-US']
+
+const i18n = new VueI18n({
+  locale: 'en-US',
+  messages: {
+    'zh-CN': mergeZH,
+    'en-US': mergeEN
+  }
+})
+
+describe('Menu/index.vue', () => {
+  it('Menu Does the component exist？', () => {
+    const wrapper = shallowMount(Menu, {
+      localVue,
+      i18n,
+      store,
+      router,
+      propsData: {
+        isCollapse: false
+      }
+    })
+    expect(wrapper.isVueInstance()).to.be.true
+  })
+
+  it('setData()', () => {
+    const wrapper = shallowMount(Menu, {
+      localVue,
+      i18n,
+      store,
+      router
+    })
+    wrapper.setProps({ isCollapse: true })
+    expect(wrapper.vm.isCollapse).to.equal(true)
+  })
+})
