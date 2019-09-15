980aef3f8023c60644625fa43f1b727bbf437d35
wqzwh <wangziqihappy@163.com>
2019-09-15 10:52:13 +0800

    feat[sharding-ui-frontend]:add login index views unit test

---
+++ sharding-ui/sharding-ui-frontend/test/specs/views/index.spec.js
@@ -0,0 +1,56 @@
+import { expect } from 'chai'
+import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import VueI18n from 'vue-i18n'
+import Vuex from 'vuex'
+import registConfig from '../../../src/views/index/module/registConfig.vue'
+import Language from '../../../src/lang/index'
+import store from '../../../src/store'
+import router from '../../../src/router'
+
+const localVue = createLocalVue()
+
+localVue.use(VueI18n)
+localVue.use(Vuex)
+
+// language setting init
+const navLang = navigator.language
+const localLang = navLang === 'zh-CN' || navLang === 'en-US' ? navLang : false
+const lang = window.localStorage.getItem('language') || localLang || 'zh-CN'
+localVue.config.lang = lang
+
+// language setting
+const locales = Language
+const mergeZH = locales['zh-CN']
+const mergeEN = locales['en-US']
+
+const i18n = new VueI18n({
+  locale: 'en-US',
+  messages: {
+    'zh-CN': mergeZH,
+    'en-US': mergeEN
+  }
+})
+
+describe('index/registConfig.vue', () => {
+  it('registConfig Does the pages exist？', () => {
+    const wrapper = shallowMount(registConfig, {
+      localVue,
+      i18n,
+      store,
+      router
+    })
+    expect(wrapper.isVueInstance()).to.be.true
+  })
+
+  it('add()', () => {
+    const wrapper = shallowMount(registConfig, {
+      localVue,
+      i18n,
+      store,
+      router
+    })
+    const btnPlus = wrapper.find('.btn-plus')
+    btnPlus.trigger('click')
+    expect(wrapper.vm.regustDialogVisible).to.equal(true)
+  })
+})
