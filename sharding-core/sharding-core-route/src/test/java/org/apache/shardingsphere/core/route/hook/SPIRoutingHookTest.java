8d82c255642fa4e9929d142c2a0345f7219a822d
KomachiSion <263976490@qq.com>
2019-09-17 15:22:20 +0800

    For #3047, Add unit test for RoutingUnit

--- sharding-core/sharding-core-route/src/test/java/org/apache/shardingsphere/core/route/hook/SPIRoutingHookTest.java
+++ sharding-core/sharding-core-route/src/test/java/org/apache/shardingsphere/core/route/hook/SPIRoutingHookTest.java
@@ -49,7 +49,7 @@ public class SPIRoutingHookTest {
     private Exception exception;
     
     @Test
-    public void start() {
+    public void assertStart() {
         spiRoutingHook.start(sql);
         RoutingHook routingHook = getFixtureHook();
         assertThat(routingHook, instanceOf(RoutingHookFixture.class));
@@ -57,7 +57,7 @@ public class SPIRoutingHookTest {
     }
     
     @Test
-    public void finishSuccess() {
+    public void assertFinishSuccess() {
         spiRoutingHook.finishSuccess(sqlRouteResult, tableMetas);
         RoutingHook routingHook = getFixtureHook();
         assertThat(routingHook, instanceOf(RoutingHookFixture.class));
@@ -66,7 +66,7 @@ public class SPIRoutingHookTest {
     }
     
     @Test
-    public void finishFailure() {
+    public void assertFinishFailure() {
         spiRoutingHook.finishFailure(exception);
         RoutingHook routingHook = getFixtureHook();
         assertThat(routingHook, instanceOf(RoutingHookFixture.class));
