7d84bb54ba87a8be8bf64ceaa85144af0eb43d1a
guangyuan <coconason@gmail.com>
2019-09-17 11:04:07 +0800

    Add Config Center (#3020)
    * Initialize config server module.
    
    * Initialize config server module.
    
    * Add TypeBasedSPI dependency.
    
    * Modify Config Center related.
    
    * Add Config Center Zookeeper related.
    
    * Modify CuratorZookeeperConfigCenter and format pom.xml.
    
    * Add Integration Test for Registry Center and Config Center.
    
    * Modify Integration Test for Registry Center.
    
    * Modify Config Center related functions.
    
    * Modify Config Center related functions.
    
    * Modify pom.xml.
    
    * Modify services org.apache.shardingsphere.orchestration.config.api.ConfigCenter
    
    * Modify pom.xml file.
    
    * Add Unit test cases.
    
    * format java file.
    
    * Add Unit Test for ConfigCenterServiceLoader.
    
    * Modify ConfigCenter.

---
+++ sharding-orchestration/sharding-orchestration-config/sharding-orchestration-config-api/src/main/java/org/apache/shardingsphere/orchestration/config/api/ConfigCenter.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.orchestration.config.api;
+
+import org.apache.shardingsphere.orchestration.config.listener.DataChangedEventListener;
+import org.apache.shardingsphere.spi.TypeBasedSPI;
+
+import java.util.List;
+
+/**
+ * Config center.
+ * 
+ * @author wangguangyuan
+ */
+public interface ConfigCenter extends TypeBasedSPI {
+    
+    /**
+     * Initialize config center.
+     * 
+     * @param config config center configuration
+     */
+    void init(ConfigCenterConfiguration config);
+    
+    /**
+     * Get data from config center.
+     * 
+     * <p>Maybe use cache if existed.</p>
+     * 
+     * @param key key of data
+     * @return value of data
+     */
+    String get(String key);
+    
+    /**
+     * Get data from config center directly.
+     * 
+     * <p>Cannot use cache.</p>
+     *
+     * @param key key of data
+     * @return value of data
+     */
+    String getDirectly(String key);
+    
+    /**
+     * Judge data is existed or not.
+     * 
+     * @param key key of data
+     * @return data is existed or not
+     */
+    boolean isExisted(String key);
+    
+    /**
+     * Get node's sub-nodes list.
+     *
+     * @param key key of data
+     * @return sub-nodes name list
+     */
+    List<String> getChildrenKeys(String key);
+    
+    /**
+     * Persist data.
+     * 
+     * @param key key of data
+     * @param value value of data
+     */
+    void persist(String key, String value);
+    
+    /**
+     * Update data.
+     *
+     * @param key key of data
+     * @param value value of data
+     */
+    void update(String key, String value);
+    
+    /**
+     * Persist ephemeral data.
+     *
+     * @param key key of data
+     * @param value value of data
+     */
+    void persistEphemeral(String key, String value);
+    
+    /**
+     * Watch key or path of the config server.
+     *
+     * @param key key of data
+     * @param dataChangedEventListener data changed event listener
+     */
+    void watch(String key, DataChangedEventListener dataChangedEventListener);
+    
+    /**
+     * Close.
+      */
+    void close();
+}
