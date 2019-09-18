b1209d8b57a25c7dc123c8c25caa4f472c2c63f9
terrymanu <terrymanu@163.com>
2019-09-18 20:18:20 +0800

    refactor sequence of ParsingSQLRoutingResultChecker.getAbsentRoutingUnit()

--- sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/ParsingSQLRoutingResultChecker.java
+++ sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/core/route/router/sharding/ParsingSQLRoutingResultChecker.java
@@ -93,6 +93,41 @@ public final class ParsingSQLRoutingResultChecker implements RoutingResultChecke
         }
     }
     
+    private Multimap<RoutingUnit, TableUnit> getAbsentRoutingUnit(final Collection<RoutingUnit> routingUnits) {
+        Multimap<RoutingUnit, TableUnit> result = HashMultimap.create();
+        for (RoutingUnit each : routingUnits) {
+            String dataSourceName = each.getDataSourceName();
+            result.putAll(each, getAbsentTableUnit(each, dataSourceName));
+        }
+        return result;
+    }
+    
+    private Collection<TableUnit> getAbsentTableUnit(final RoutingUnit routingUnit, final String dataSourceName) {
+        Collection<TableUnit> result = new LinkedList<>();
+        for (TableUnit each : routingUnit.getTableUnits()) {
+            if (containsInMetaData(dataSourceName, each.getActualTableName()) || containsInShardingRule(dataSourceName, each)) {
+                continue;
+            }
+            result.add(each);
+        }
+        return result;
+    }
+    
+    private boolean containsInShardingRule(final String dataSourceName, final TableUnit tableUnit) {
+        Optional<TableRule> tableRule = shardingRule.findTableRule(tableUnit.getLogicTableName());
+        return tableRule.isPresent() && tableRule.get().getActualTableNames(dataSourceName).contains(tableUnit.getActualTableName());
+    }
+    
+    private boolean containsInMetaData(final String dataSourceName, final String actualTableName) {
+        if (checkDataSource && (null == metaData.getDataSources() || null == metaData.getDataSources().getDataSourceMetaData(dataSourceName))) {
+            return false;
+        }
+        if (checkTable && (null == metaData.getTables() || !metaData.getTables().containsTable(actualTableName))) {
+            return false;
+        }
+        return true;
+    }
+    
     private void throwOthersRoutingResultException(final Multimap<RoutingUnit, TableUnit> absentRoutingUnitMap) {
         RoutingUnit routingUnit = absentRoutingUnitMap.keySet().iterator().next();
         Collection<String> absentDataNodes = Lists.newArrayListWithExpectedSize(absentRoutingUnitMap.get(routingUnit).size());
@@ -134,41 +169,6 @@ public final class ParsingSQLRoutingResultChecker implements RoutingResultChecke
             }
             throwExceptionForAbsentDataNode(absentDataNodes, detail);
         }
-    }
-    
-    private Multimap<RoutingUnit, TableUnit> getAbsentRoutingUnit(final Collection<RoutingUnit> routingUnits) {
-        Multimap<RoutingUnit, TableUnit> result = HashMultimap.create();
-        for (RoutingUnit each : routingUnits) {
-            String dataSourceName = each.getDataSourceName();
-            result.putAll(each, getAbsentTableUnit(each, dataSourceName));
-        }
-        return result;
-    }
-    
-    private Collection<TableUnit> getAbsentTableUnit(final RoutingUnit routingUnit, final String dataSourceName) {
-        Collection<TableUnit> result = new LinkedList<>();
-        for (TableUnit each : routingUnit.getTableUnits()) {
-            if (containsInMetaData(dataSourceName, each.getActualTableName()) || containsInShardingRule(dataSourceName, each)) {
-                continue;
-            }
-            result.add(each);
-        }
-        return result;
-    }
-    
-    private boolean containsInShardingRule(final String dataSourceName, final TableUnit tableUnit) {
-        Optional<TableRule> tableRule = shardingRule.findTableRule(tableUnit.getLogicTableName());
-        return tableRule.isPresent() && tableRule.get().getActualTableNames(dataSourceName).contains(tableUnit.getActualTableName());
-    }
-    
-    private boolean containsInMetaData(final String dataSourceName, final String actualTableName) {
-        if (checkDataSource && (null == metaData.getDataSources() || null == metaData.getDataSources().getDataSourceMetaData(dataSourceName))) {
-            return false;
-        }
-        if (checkTable && (null == metaData.getTables() || !metaData.getTables().containsTable(actualTableName))) {
-            return false;
-        }
-        return true;
     }
     
     private void throwExceptionForAbsentDataNode(final Collection<String> absentDataNodes, final CharSequence detail) {
