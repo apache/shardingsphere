3738293e703e8be9942f98ea81a1bf196746cac7
sunbufu <sunyoubufu@163.com>
2019-09-21 22:05:33 +0800

    try CI check again.

--- sharding-proxy/sharding-proxy-frontend/sharding-proxy-frontend-mysql/src/main/java/org/apache/shardingsphere/shardingproxy/frontend/mysql/auth/MySQLAuthenticationEngine.java
+++ sharding-proxy/sharding-proxy-frontend/sharding-proxy-frontend-mysql/src/main/java/org/apache/shardingsphere/shardingproxy/frontend/mysql/auth/MySQLAuthenticationEngine.java
@@ -41,6 +41,7 @@ import java.net.SocketAddress;
  * Authentication engine for MySQL.
  *
  * @author zhangliang
+ * @author sunbufu
  */
 public final class MySQLAuthenticationEngine implements AuthenticationEngine {
     
