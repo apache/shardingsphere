0db06f7932c36066c6d911c5214be05304094a9c
chentiyu <chentiyu.cd@ccbft.com>
2019-09-22 10:29:38 +0800

    TableMetaDataLoader  Test

---
+++ sharding-core/sharding-core-execute/src/test/java/org/apache/shardingsphere/core/execute/metadata/TableMetaDataLoaderTest.java
@@ -0,0 +1,98 @@
+package org.apache.shardingsphere.core.execute.metadata;
+
+import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
+import org.apache.shardingsphere.api.config.sharding.TableRuleConfiguration;
+import org.apache.shardingsphere.core.database.DatabaseTypes;
+import org.apache.shardingsphere.core.execute.ShardingExecuteEngine;
+import org.apache.shardingsphere.core.metadata.datasource.DataSourceMetas;
+import org.apache.shardingsphere.core.metadata.table.TableMetaData;
+import org.apache.shardingsphere.core.rule.ShardingRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TableMetaDataLoaderTest {
+
+    private TableMetaDataLoader tableMetaDataLoader;
+
+    private ShardingRule shardingRule;
+
+    @Before
+    public void setUp() {
+        shardingRule = createShardingRule();
+        ShardingExecuteEngine executeEngine = new ShardingExecuteEngine(10);
+        DataSourceMetas dataSourceMetas = buildDataSourceMetas();
+        tableMetaDataLoader = new TableMetaDataLoader(dataSourceMetas, executeEngine, getConnectionManager(), 2, false);
+    }
+
+    private TableMetaDataConnectionManager getConnectionManager() {
+        Connection connection = mock(Connection.class);
+        DatabaseMetaData databaseMetaData = mock(DatabaseMetaData.class);
+        TableMetaDataConnectionManager connectionManager = mock(TableMetaDataConnectionManager.class);
+        try {
+            ResultSet tableResultSet = mock(ResultSet.class);
+            ResultSet indexResultSet = mock(ResultSet.class);
+            ResultSet cloumnResultSet = mock(ResultSet.class);
+            ResultSet primaryKeyResultSet = mock(ResultSet.class);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any())).thenReturn(connection);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any()).getMetaData()).thenReturn(databaseMetaData);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any()).getMetaData().getTables(ArgumentMatchers.<String>any(), ArgumentMatchers.<String>any(),
+                    ArgumentMatchers.<String>any(), ArgumentMatchers.<String[]>any())).thenReturn(tableResultSet);
+            when(tableResultSet.next()).thenReturn(true);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any()).getMetaData().getPrimaryKeys(ArgumentMatchers.<String>any(),
+                    ArgumentMatchers.<String>any(), ArgumentMatchers.<String>any())).thenReturn(primaryKeyResultSet);
+            when(primaryKeyResultSet.next()).thenReturn(false);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any()).getMetaData().getColumns(ArgumentMatchers.<String>any(),
+                    ArgumentMatchers.<String>any(), ArgumentMatchers.<String>any(), ArgumentMatchers.<String>any())).thenReturn(cloumnResultSet);
+            when(cloumnResultSet.next()).thenReturn(false);
+            when(connectionManager.getConnection(ArgumentMatchers.<String>any()).getMetaData().getPrimaryKeys(ArgumentMatchers.<String>any(),
+                    ArgumentMatchers.<String>any(), ArgumentMatchers.<String>any())).thenReturn(indexResultSet);
+            when(indexResultSet.next()).thenReturn(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connectionManager;
+    }
+
+    private DataSourceMetas buildDataSourceMetas() {
+        Map<String, String> shardingDataSourceURLs = new LinkedHashMap<>();
+        shardingDataSourceURLs.put("ds_0", "jdbc:mysql://localhost:3306/ds_0");
+        shardingDataSourceURLs.put("ds_1", "jdbc:mysql://localhost:3306/ds_1");
+        return new DataSourceMetas(shardingDataSourceURLs, DatabaseTypes.getActualDatabaseType("MySQL"));
+    }
+
+    private ShardingRule createShardingRule() {
+        ShardingRuleConfiguration shardingRuleConfiguration = new ShardingRuleConfiguration();
+        TableRuleConfiguration tableRuleConfiguration = createTableRuleConfiguration("TEST_TABLE", "ds_${0..1}.test_table_${0..2}");
+        shardingRuleConfiguration.getTableRuleConfigs().add(tableRuleConfiguration);
+        return new ShardingRule(shardingRuleConfiguration, createDataSourceNames());
+    }
+
+    private TableRuleConfiguration createTableRuleConfiguration(final String logicTableName, final String actualDataNodes) {
+        return new TableRuleConfiguration(logicTableName, actualDataNodes);
+    }
+
+    private Collection<String> createDataSourceNames() {
+        return Arrays.asList("ds_0", "ds_1");
+    }
+
+    @Test
+    public void AssertLogicIndexes() throws SQLException {
+        TableMetaData tableMetaData = tableMetaDataLoader.load("TEST_TABLE", shardingRule);
+        assertThat(tableMetaData.getIndexes().size(), is(0));
+    }
+}
