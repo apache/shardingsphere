6cda735e35206af43c4d1245922b8c1923da1bad
wangxichun <wangxichun@jd.com>
2019-09-27 22:45:22 +0800

    1. remove powermock-api-mockito2 2. Apache copyright added, and several code style corrected 3. replace assertEquals with assertThat

--- sharding-proxy/sharding-proxy-backend/src/test/java/org/apache/shardingsphere/shardingproxy/backend/schema/LogicSchemaTest.java
+++ sharding-proxy/sharding-proxy-backend/src/test/java/org/apache/shardingsphere/shardingproxy/backend/schema/LogicSchemaTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.shardingproxy.backend.schema;
 
 import com.google.common.collect.Maps;
@@ -5,36 +22,35 @@ import org.apache.shardingsphere.shardingproxy.backend.communication.jdbc.dataso
 import org.apache.shardingsphere.shardingproxy.config.yaml.YamlDataSourceParameter;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.powermock.reflect.Whitebox;
+import java.lang.reflect.Field;
 import java.util.Map;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static junit.framework.TestCase.assertNotNull;
+import static org.mockito.Mockito.when;
 
-
-
-public class LogicSchemaTest {
+public final class LogicSchemaTest {
 
     @Test
-    public void checkDataSourcesReturned() {
-        //Given
-        LogicSchema absSchema = Mockito.mock(
-                LogicSchema.class,
-                Mockito.CALLS_REAL_METHODS);
+    public void assertDataSourcesReturned() throws NoSuchFieldException, IllegalAccessException {
+        LogicSchema absSchema = Mockito.mock(LogicSchema.class, Mockito.CALLS_REAL_METHODS);
 
         JDBCBackendDataSource jdbcBackendDataSource = Mockito.mock(JDBCBackendDataSource.class);
-        Whitebox.setInternalState(absSchema, "backendDataSource", jdbcBackendDataSource);
-        Map<String, YamlDataSourceParameter> expectedMap = Maps.newHashMap();
-        Mockito.when(jdbcBackendDataSource.getDataSourceParameters())
-                .thenReturn(expectedMap);
 
-        // when
+        Field backendDataSourceField = LogicSchema.class.getDeclaredField("backendDataSource");
+        backendDataSourceField.setAccessible(true);
+        backendDataSourceField.set(absSchema, jdbcBackendDataSource);
+
+
+        Map<String, YamlDataSourceParameter> expectedMap = Maps.newHashMap();
+        when(jdbcBackendDataSource.getDataSourceParameters()).thenReturn(expectedMap);
+
         Map<String, YamlDataSourceParameter> dataSourceParameterMap = absSchema.getDataSources();
 
 
-        // then
         assertNotNull(dataSourceParameterMap);
-        assertEquals(expectedMap, dataSourceParameterMap);
+        assertThat(dataSourceParameterMap, is(expectedMap));
     }
 }
 
