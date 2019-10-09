67c0f51d2d1e7c6d558dbff35ec97950fed646fa
wangxichun <wangxichun@jd.com>
2019-10-09 22:47:02 +0800

    final added

--- sharding-transaction/sharding-transaction-core/src/test/java/org/apache/shardingsphere/transaction/core/ResourceIDGeneratorTest.java
+++ sharding-transaction/sharding-transaction-core/src/test/java/org/apache/shardingsphere/transaction/core/ResourceIDGeneratorTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class ResourceIDGeneratorTest {
+public final class ResourceIDGeneratorTest {
     
     @Test
     public void assertNextIdProperly() {
