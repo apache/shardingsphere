18dab7b443309d6a6799d795e0c90c560f7ff785
KomachiSion <263976490@qq.com>
2019-10-11 15:16:15 +0800

    remove var in DataTypesCodec

--- src/main/java/info/avalon566/shardingscaling/sync/mysql/binlog/codec/DataTypesCodec.java
+++ src/main/java/info/avalon566/shardingscaling/sync/mysql/binlog/codec/DataTypesCodec.java
@@ -18,7 +18,6 @@
 package info.avalon566.shardingscaling.sync.mysql.binlog.codec;
 
 import io.netty.buffer.ByteBuf;
-import lombok.var;
 
 import java.util.BitSet;
 
@@ -78,7 +77,7 @@ public final class DataTypesCodec {
      * @return bitset value
      */
     public static BitSet readBitmap(final int length, final ByteBuf in) {
-        var bitSet = new BitSet(length);
+        BitSet bitSet = new BitSet(length);
         for (int bit = 0; bit < length; bit += 8) {
             int flag = ((int) in.readByte()) & 0xff;
             if (flag != 0) {
@@ -250,7 +249,7 @@ public final class DataTypesCodec {
      * @return byte array
      */
     public static byte[] readBytes(final int length, final ByteBuf in) {
-        var buffer = new byte[length];
+        byte[] buffer = new byte[length];
         in.readBytes(buffer, 0, length);
         return buffer;
     }
@@ -263,7 +262,7 @@ public final class DataTypesCodec {
      * @return string value
      */
     public static String readFixedLengthString(final int length, final ByteBuf in) {
-        var buffer = new byte[length];
+        byte[] buffer = new byte[length];
         in.readBytes(buffer, 0, length);
         return new String(buffer);
     }
@@ -275,7 +274,7 @@ public final class DataTypesCodec {
      * @return string value
      */
     public static String readLengthCodedString(final ByteBuf in) {
-        var length = (int) readLengthCodedIntLE(in);
+        int length = (int) readLengthCodedIntLE(in);
         return readFixedLengthString(length, in);
     }
 
@@ -286,8 +285,8 @@ public final class DataTypesCodec {
      * @return string value
      */
     public static String readNulTerminatedString(final ByteBuf in) {
-        var length = in.bytesBefore((byte) 0x00);
-        var str = readFixedLengthString(length, in);
+        int length = in.bytesBefore((byte) 0x00);
+        String str = readFixedLengthString(length, in);
         readNul(in);
         return str;
     }
