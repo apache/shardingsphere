ff7b01a55c9afd1a4c5c1a00401f471339d16bbb
avalon566 <avalon566@163.com>
2019-10-14 14:42:17 +0800

    Refactored ScalingJob

--- src/main/java/info/avalon566/shardingscaling/Bootstrap.java
+++ src/main/java/info/avalon566/shardingscaling/Bootstrap.java
@@ -18,13 +18,28 @@
 package info.avalon566.shardingscaling;
 
 import info.avalon566.shardingscaling.job.ScalingJob;
+import info.avalon566.shardingscaling.job.config.RuleConfiguration;
+import info.avalon566.shardingscaling.job.config.SyncConfiguration;
+import info.avalon566.shardingscaling.job.config.SyncType;
 import info.avalon566.shardingscaling.job.schedule.standalone.InProcessScheduler;
+import info.avalon566.shardingscaling.sync.core.DataSourceConfiguration;
+import info.avalon566.shardingscaling.sync.core.JdbcDataSourceConfiguration;
+import info.avalon566.shardingscaling.sync.core.RdbmsConfiguration;
 import info.avalon566.shardingscaling.utils.RuntimeUtil;
+import info.avalon566.shardingscaling.utils.YamlUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.File;
-import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Bootstrap of ShardingScaling.
@@ -37,6 +52,14 @@ public class Bootstrap {
     static {
         PropertyConfigurator.configure(RuntimeUtil.getBasePath() + "conf" + File.separator + "log4j.properties");
     }
+
+    private static final String INPUT_SHARDING_CONFIG = "input-sharding-config";
+
+    private static final String OUTPUT_JDBC_URL = "output-jdbc-url";
+
+    private static final String OUTPUT_JDBC_USERNAME = "output-jdbc-username";
+
+    private static final String OUTPUT_JDBC_PASSWORD = "output-jdbc-password";
     
     /**
      * Main entry.
@@ -47,7 +70,52 @@ public class Bootstrap {
         log.info("ShardingScaling Startup");
         InProcessScheduler scheduler = new InProcessScheduler();
         if ("scaling".equals(args[0])) {
-            new ScalingJob(Arrays.copyOfRange(args, 1, args.length), scheduler).run();
+            try {
+                CommandLine commandLine = parseCommand(args);
+                List<SyncConfiguration> syncConfigurations = toSyncConfigurations(commandLine);
+                new ScalingJob(syncConfigurations, scheduler).run();
+            } catch (FileNotFoundException | ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static CommandLine parseCommand(final String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption(Option.builder().required(true).hasArg(true).longOpt(INPUT_SHARDING_CONFIG).desc("shardingsphere 路由配置文件").build());
+        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_URL).desc("输出 jdbc url").build());
+        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_USERNAME).desc("输出 jdbc 用户名").build());
+        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_PASSWORD).desc("输出 jdbc 密码").build());
+        return new DefaultParser().parse(options, args);
+    }
+
+    private static List<SyncConfiguration> toSyncConfigurations(final CommandLine commandLine) throws FileNotFoundException {
+        RuleConfiguration ruleConfig = loadRuleConfiguration(commandLine.getOptionValue(INPUT_SHARDING_CONFIG));
+        List<SyncConfiguration> syncConfigurations = new ArrayList<SyncConfiguration>(ruleConfig.getDataSources().size());
+        for (Map.Entry<String, RuleConfiguration.YamlDataSourceParameter> entry : ruleConfig.getDataSources().entrySet()) {
+            RdbmsConfiguration readerConfiguration = new RdbmsConfiguration();
+            DataSourceConfiguration readerDataSourceConfiguration = new JdbcDataSourceConfiguration(
+                    entry.getValue().getUrl(),
+                    entry.getValue().getUsername(),
+                    entry.getValue().getPassword());
+            readerConfiguration.setDataSourceConfiguration(readerDataSourceConfiguration);
+            RdbmsConfiguration writerConfiguration = new RdbmsConfiguration();
+            DataSourceConfiguration writerDataSourceConfiguration = new JdbcDataSourceConfiguration(
+                    commandLine.getOptionValue(OUTPUT_JDBC_URL),
+                    commandLine.getOptionValue(OUTPUT_JDBC_USERNAME),
+                    commandLine.getOptionValue(OUTPUT_JDBC_PASSWORD));
+            writerConfiguration.setDataSourceConfiguration(writerDataSourceConfiguration);
+            syncConfigurations.add(new SyncConfiguration(SyncType.Database, 3, readerConfiguration, writerConfiguration));
+        }
+        return syncConfigurations;
+    }
+
+    private static RuleConfiguration loadRuleConfiguration(final String fileName) throws FileNotFoundException {
+        if (fileName.startsWith("/")) {
+            return YamlUtil.parse(fileName, RuleConfiguration.class);
+        } else {
+            String fullFileName = RuntimeUtil.getBasePath() + File.separator + fileName;
+            return YamlUtil.parse(fullFileName, RuleConfiguration.class);
         }
     }
 }
--- src/main/java/info/avalon566/shardingscaling/job/ScalingJob.java
+++ src/main/java/info/avalon566/shardingscaling/job/ScalingJob.java
@@ -17,27 +17,11 @@
 
 package info.avalon566.shardingscaling.job;
 
-import info.avalon566.shardingscaling.job.config.RuleConfiguration;
 import info.avalon566.shardingscaling.job.config.SyncConfiguration;
-import info.avalon566.shardingscaling.job.config.SyncType;
 import info.avalon566.shardingscaling.job.schedule.Reporter;
 import info.avalon566.shardingscaling.job.schedule.Scheduler;
-import info.avalon566.shardingscaling.sync.core.DataSourceConfiguration;
-import info.avalon566.shardingscaling.sync.core.JdbcDataSourceConfiguration;
-import info.avalon566.shardingscaling.sync.core.RdbmsConfiguration;
-import info.avalon566.shardingscaling.utils.RuntimeUtil;
-import info.avalon566.shardingscaling.utils.YamlUtil;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Scaling job.
@@ -46,20 +30,12 @@ import java.util.Map;
  */
 public class ScalingJob {
 
-    private static final String INPUT_SHARDING_CONFIG = "input-sharding-config";
-
-    private static final String OUTPUT_JDBC_URL = "output-jdbc-url";
-
-    private static final String OUTPUT_JDBC_USERNAME = "output-jdbc-username";
-
-    private static final String OUTPUT_JDBC_PASSWORD = "output-jdbc-password";
-
-    private final String[] args;
+    private final List<SyncConfiguration> syncConfigurations;
 
     private final Scheduler scheduler;
 
-    public ScalingJob(final String[] args, final Scheduler scheduler) {
-        this.args = args;
+    public ScalingJob(final List<SyncConfiguration> syncConfigurations, final Scheduler scheduler) {
+        this.syncConfigurations = syncConfigurations;
         this.scheduler = scheduler;
     }
 
@@ -67,51 +43,6 @@ public class ScalingJob {
      * Run.
      */
     public void run() {
-        try {
-            CommandLine commandLine = parseCommand(args);
-            List<SyncConfiguration> syncConfigurations = toSyncConfigurations(commandLine);
-            Reporter reporter = scheduler.schedule(syncConfigurations);
-        } catch (FileNotFoundException | ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private CommandLine parseCommand(final String[] args) throws ParseException {
-        Options options = new Options();
-        options.addOption(Option.builder().required(true).hasArg(true).longOpt(INPUT_SHARDING_CONFIG).desc("shardingsphere 路由配置文件").build());
-        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_URL).desc("输出 jdbc url").build());
-        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_USERNAME).desc("输出 jdbc 用户名").build());
-        options.addOption(Option.builder().required(true).hasArg(true).longOpt(OUTPUT_JDBC_PASSWORD).desc("输出 jdbc 密码").build());
-        return new DefaultParser().parse(options, args);
-    }
-
-    private List<SyncConfiguration> toSyncConfigurations(final CommandLine commandLine) throws FileNotFoundException {
-        RuleConfiguration ruleConfig = loadRuleConfiguration(commandLine.getOptionValue(INPUT_SHARDING_CONFIG));
-        List<SyncConfiguration> syncConfigurations = new ArrayList<SyncConfiguration>(ruleConfig.getDataSources().size());
-        for (Map.Entry<String, RuleConfiguration.YamlDataSourceParameter> entry : ruleConfig.getDataSources().entrySet()) {
-            RdbmsConfiguration readerConfiguration = new RdbmsConfiguration();
-            DataSourceConfiguration readerDataSourceConfiguration = new JdbcDataSourceConfiguration(
-                    entry.getValue().getUrl(),
-                    entry.getValue().getUsername(),
-                    entry.getValue().getPassword());
-            readerConfiguration.setDataSourceConfiguration(readerDataSourceConfiguration);
-            RdbmsConfiguration writerConfiguration = new RdbmsConfiguration();
-            DataSourceConfiguration writerDataSourceConfiguration = new JdbcDataSourceConfiguration(
-                    commandLine.getOptionValue(OUTPUT_JDBC_URL),
-                    commandLine.getOptionValue(OUTPUT_JDBC_USERNAME),
-                    commandLine.getOptionValue(OUTPUT_JDBC_PASSWORD));
-            writerConfiguration.setDataSourceConfiguration(writerDataSourceConfiguration);
-            syncConfigurations.add(new SyncConfiguration(SyncType.Database, 3, readerConfiguration, writerConfiguration));
-        }
-        return syncConfigurations;
-    }
-
-    private RuleConfiguration loadRuleConfiguration(final String fileName) throws FileNotFoundException {
-        if (fileName.startsWith("/")) {
-            return YamlUtil.parse(fileName, RuleConfiguration.class);
-        } else {
-            String fullFileName = RuntimeUtil.getBasePath() + File.separator + fileName;
-            return YamlUtil.parse(fullFileName, RuleConfiguration.class);
-        }
+        Reporter reporter = scheduler.schedule(syncConfigurations);
     }
 }
