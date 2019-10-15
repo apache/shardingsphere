d5ad2249b120b6542024bc92b58cc7c446cbd7f9
wangxichun <wangxichun@jd.com>
2019-10-15 23:18:26 +0800

    revert to the original version

--- sharding-core/sharding-core-preprocessor/src/test/java/org/apache/shardingsphere/core/preprocessor/segment/select/projection/ProjectionsContextTest.java
+++ sharding-core/sharding-core-preprocessor/src/test/java/org/apache/shardingsphere/core/preprocessor/segment/select/projection/ProjectionsContextTest.java
@@ -42,8 +42,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public final class ProjectionsContextTest {
     
