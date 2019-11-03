39d50b1f274953d527d74a031866ec21e61d5922
terrymanu <terrymanu@163.com>
2019-11-03 10:06:54 +0800

    decouple SQLParsingException & ShardingException

--- sql-parser/sql-parser-common/src/main/java/org/apache/shardingsphere/sql/parser/exception/SQLParsingException.java
+++ sql-parser/sql-parser-common/src/main/java/org/apache/shardingsphere/sql/parser/exception/SQLParsingException.java
@@ -17,18 +17,16 @@
 
 package org.apache.shardingsphere.sql.parser.exception;
 
-import org.apache.shardingsphere.core.exception.ShardingException;
-
 /**
  * Throw exception when SQL parsing error.
  * 
  * @author zhangliang 
  */
-public final class SQLParsingException extends ShardingException {
+public final class SQLParsingException extends RuntimeException {
     
     private static final long serialVersionUID = -6408790652103666096L;
     
     public SQLParsingException(final String message, final Object... args) {
-        super(message, args);
+        super(String.format(message, args));
     }
 }
