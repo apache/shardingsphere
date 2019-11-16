2783bc86ff65eb553e009107ed01b097afb67391
dongzl <dzllikelsw@163.com>
2019-11-16 21:45:26 +0800

    Apollo as a Config Center.

---
+++ sharding-orchestration/sharding-orchestration-center/src/main/java/org/apache/shardingsphere/orchestration/center/instance/ApolloInstance.java
@@ -0,0 +1,96 @@
+package org.apache.shardingsphere.orchestration.center.instance;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.core.ConfigConsts;
+import com.ctrip.framework.apollo.enums.PropertyChangeType;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.orchestration.center.api.ConfigCenter;
+import org.apache.shardingsphere.orchestration.center.configuration.InstanceConfiguration;
+import org.apache.shardingsphere.orchestration.center.listener.DataChangedEvent;
+import org.apache.shardingsphere.orchestration.center.listener.DataChangedEventListener;
+
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Registry center for Apollo.
+ *
+ * @author dongzonglei
+ */
+@Slf4j
+public final class ApolloInstance implements ConfigCenter {
+    
+    private Config apolloConfig;
+    
+    @Getter
+    @Setter
+    private Properties properties = new Properties();
+    
+    @Override
+    public void init(final InstanceConfiguration config) {
+        System.setProperty("app.id", properties.getProperty("appId", "APOLLO_SHARDING_SPHERE"));
+        System.setProperty("env", properties.getProperty("env", "DEV"));
+        System.setProperty(ConfigConsts.APOLLO_CLUSTER_KEY, properties.getProperty("clusterName", ConfigConsts.CLUSTER_NAME_DEFAULT));
+        System.setProperty(ConfigConsts.APOLLO_META_KEY, config.getServerLists());
+        apolloConfig = ConfigService.getConfig(config.getNamespace());
+    }
+    
+    @Override
+    public String get(final String key) {
+        return apolloConfig.getProperty(key.replace("/", "."), "");
+    }
+    
+    @Override
+    public List<String> getChildrenKeys(final String key) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void persist(final String key, final String value) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public void watch(final String key, final DataChangedEventListener dataChangedEventListener) {
+        apolloConfig.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(final ConfigChangeEvent changeEvent) {
+                for (String key : changeEvent.changedKeys()) {
+                    ConfigChange change = changeEvent.getChange(key);
+                    DataChangedEvent.ChangedType changedType = getChangedType(change.getChangeType());
+                    if (DataChangedEvent.ChangedType.IGNORED != changedType) {
+                        dataChangedEventListener.onChange(new DataChangedEvent(key, change.getNewValue(), changedType));
+                    }
+                }
+            }
+        }, Sets.newHashSet(key));
+    }
+    
+    private DataChangedEvent.ChangedType getChangedType(final PropertyChangeType changeType) {
+        switch (changeType) {
+            case MODIFIED:
+                return DataChangedEvent.ChangedType.UPDATED;
+            case DELETED:
+                return DataChangedEvent.ChangedType.DELETED;
+            default:
+                return DataChangedEvent.ChangedType.IGNORED;
+        }
+    }
+    
+    @Override
+    public void close() {
+
+    }
+    
+    @Override
+    public String getType() {
+        return "apollo";
+    }
+}
