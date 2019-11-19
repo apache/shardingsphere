be4f8dfe7cb22d32f88660be6fa34b1c01d1d1b1
dongzl <dzllikelsw@163.com>
2019-11-19 18:31:36 +0800

    add apollo-openapi, implement update & getChildrenKeys methods.

--- sharding-orchestration/sharding-orchestration-center/src/main/java/org/apache/shardingsphere/orchestration/center/instance/ApolloInstance.java
+++ sharding-orchestration/sharding-orchestration-center/src/main/java/org/apache/shardingsphere/orchestration/center/instance/ApolloInstance.java
@@ -24,7 +24,12 @@ import com.ctrip.framework.apollo.core.ConfigConsts;
 import com.ctrip.framework.apollo.enums.PropertyChangeType;
 import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.openapi.client.ApolloOpenApiClient;
+import com.ctrip.framework.apollo.openapi.client.constant.ApolloOpenApiConstants;
+import com.ctrip.framework.apollo.openapi.dto.NamespaceReleaseDTO;
+import com.ctrip.framework.apollo.openapi.dto.OpenItemDTO;
 import com.google.common.collect.Sets;
+import com.google.common.primitives.Ints;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -33,8 +38,14 @@ import org.apache.shardingsphere.orchestration.center.configuration.InstanceConf
 import org.apache.shardingsphere.orchestration.center.listener.DataChangedEvent;
 import org.apache.shardingsphere.orchestration.center.listener.DataChangedEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * Registry center for Apollo.
@@ -44,7 +55,27 @@ import java.util.Properties;
 @Slf4j
 public final class ApolloInstance implements ConfigCenter {
     
+    private static final String SHARDING_SPHERE_KEY_ROOT = "/";
+    
+    private static final String SHARDING_SPHERE_KEY_SEPARATOR = "/";
+    
+    private static final String APOLLO_KEY_SEPARATOR = ".";
+    
+    private String namespace;
+    
+    private String appId;
+    
+    private String env;
+    
+    private String clusterName;
+    
+    private String modifiyDataUserId;
+    
     private Config apolloConfig;
+    
+    private ApolloOpenApiClient client;
+    
+    private Map<String, Set<String>> keyAndChildrenMap = new HashMap<>();
     
     @Getter
     @Setter
@@ -52,26 +83,108 @@ public final class ApolloInstance implements ConfigCenter {
     
     @Override
     public void init(final InstanceConfiguration config) {
-        System.setProperty("app.id", properties.getProperty("appId", "APOLLO_SHARDING_SPHERE"));
-        System.setProperty("env", properties.getProperty("env", "DEV"));
-        System.setProperty(ConfigConsts.APOLLO_CLUSTER_KEY, properties.getProperty("clusterName", ConfigConsts.CLUSTER_NAME_DEFAULT));
+        initApolloConfig(config);
+        initApolloOpenApiClient();
+        initKeysRelationship();
+    }
+    
+    private void initApolloConfig(final InstanceConfiguration config) {
+        namespace = config.getNamespace();
+        appId = properties.getProperty("appId", "APOLLO_SHARDING_SPHERE");
+        env = properties.getProperty("env", "DEV");
+        clusterName = properties.getProperty("clusterName", ConfigConsts.CLUSTER_NAME_DEFAULT);
+        System.setProperty("app.id", appId);
+        System.setProperty("env", env);
+        System.setProperty(ConfigConsts.APOLLO_CLUSTER_KEY, clusterName);
         System.setProperty(ConfigConsts.APOLLO_META_KEY, config.getServerLists());
-        apolloConfig = ConfigService.getConfig(config.getNamespace());
+        apolloConfig = ConfigService.getConfig(namespace);
+    }
+    
+    private void initApolloOpenApiClient() {
+        modifiyDataUserId = properties.getProperty("modifiyDataUserId");
+        String apolloToken = properties.getProperty("token");
+        String portalUrl = properties.getProperty("portalUrl");
+        String connectTimeout = properties.getProperty("connectTimeout", Objects.toString(ApolloOpenApiConstants.DEFAULT_CONNECT_TIMEOUT));
+        String readTimeout = properties.getProperty("readTimeout", Objects.toString(ApolloOpenApiConstants.DEFAULT_READ_TIMEOUT));
+        client = ApolloOpenApiClient.newBuilder().withPortalUrl(portalUrl)
+                .withConnectTimeout(Ints.tryParse(connectTimeout)).withReadTimeout(Ints.tryParse(readTimeout))
+                .withToken(apolloToken).build();
+    }
+    
+    private void initKeysRelationship() {
+        List<OpenItemDTO> items = client.getNamespace(appId, env, clusterName, namespace).getItems();
+        for (OpenItemDTO each : items) {
+            if (!each.getKey().contains(APOLLO_KEY_SEPARATOR)) {
+                addRelationship(SHARDING_SPHERE_KEY_ROOT, SHARDING_SPHERE_KEY_ROOT + each.getKey());
+                continue;
+            }
+            initKeysRelationship(each.getKey());
+        }
+    }
+    
+    private void initKeysRelationship(final String apolloKey) {
+        String parentKey = SHARDING_SPHERE_KEY_ROOT;
+        String shardingSphereKey = deConvertKey(apolloKey);
+        for (int i = 1; i <= shardingSphereKey.lastIndexOf(SHARDING_SPHERE_KEY_SEPARATOR); i = shardingSphereKey.indexOf(SHARDING_SPHERE_KEY_SEPARATOR, i) + 1) {
+            String childrenKey = shardingSphereKey.substring(0, shardingSphereKey.indexOf(SHARDING_SPHERE_KEY_SEPARATOR, i));
+            addRelationship(parentKey, childrenKey);
+            parentKey = childrenKey;
+        }
+        addRelationship(parentKey, shardingSphereKey);
+    }
+    
+    private void addRelationship(final String parentKey, final String childrenKey) {
+        Set<String> childrenKeys = keyAndChildrenMap.containsKey(parentKey) ? keyAndChildrenMap.get(parentKey) : new HashSet<String>();
+        childrenKeys.add(childrenKey);
+        keyAndChildrenMap.put(parentKey, childrenKeys);
     }
     
     @Override
     public String get(final String key) {
-        return apolloConfig.getProperty(key.replace("/", "."), "");
+        return apolloConfig.getProperty(convertKey(key), "");
+    }
+    
+    private String convertKey(final String shardingSphereKey) {
+        return shardingSphereKey.replace(SHARDING_SPHERE_KEY_SEPARATOR, APOLLO_KEY_SEPARATOR).substring(1);
+    }
+    
+    private String deConvertKey(final String apolloKey) {
+        return new StringBuilder(SHARDING_SPHERE_KEY_ROOT).append(apolloKey.replace(APOLLO_KEY_SEPARATOR, SHARDING_SPHERE_KEY_SEPARATOR)).toString();
     }
     
     @Override
     public List<String> getChildrenKeys(final String key) {
-        throw new UnsupportedOperationException();
+        return new ArrayList<>(keyAndChildrenMap.get(key));
     }
     
     @Override
     public void persist(final String key, final String value) {
-        throw new UnsupportedOperationException();
+        String apolloKey = convertKey(key);
+        updateKey(apolloKey, value);
+        publishNamespace();
+        if (!apolloKey.contains(".")) {
+            addRelationship(SHARDING_SPHERE_KEY_ROOT, SHARDING_SPHERE_KEY_ROOT + apolloKey);
+            return;
+        }
+        initKeysRelationship(apolloKey);
+    }
+    
+    private void updateKey(final String key, final String value) {
+        OpenItemDTO openItem = new OpenItemDTO();
+        openItem.setKey(key);
+        openItem.setValue(value);
+        openItem.setComment("ShardingSphere create or update config");
+        openItem.setDataChangeCreatedBy(modifiyDataUserId);
+        client.createOrUpdateItem(appId, env, clusterName, namespace, openItem);
+    }
+    
+    private void publishNamespace() {
+        NamespaceReleaseDTO release = new NamespaceReleaseDTO();
+        release.setReleaseTitle("ShardingSphere namespace release");
+        release.setReleaseComment("ShardingSphere namespace release");
+        release.setReleasedBy(modifiyDataUserId);
+        release.setEmergencyPublish(true);
+        client.publishNamespace(appId, env, clusterName, namespace, release);
     }
     
     @Override
@@ -83,7 +196,7 @@ public final class ApolloInstance implements ConfigCenter {
                     ConfigChange change = changeEvent.getChange(key);
                     DataChangedEvent.ChangedType changedType = getChangedType(change.getChangeType());
                     if (DataChangedEvent.ChangedType.IGNORED != changedType) {
-                        dataChangedEventListener.onChange(new DataChangedEvent(key, change.getNewValue(), changedType));
+                        dataChangedEventListener.onChange(new DataChangedEvent(deConvertKey(key), change.getNewValue(), changedType));
                     }
                 }
             }
