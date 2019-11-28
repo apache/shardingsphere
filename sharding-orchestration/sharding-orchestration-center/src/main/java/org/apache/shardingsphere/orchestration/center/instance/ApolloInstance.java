16387ce164bde8dc6dee708516bc0cff0e443e6b
dongzl <dzllikelsw@163.com>
2019-11-28 12:37:53 +0800

    add Apollo as ConfigCenter.

---
+++ sharding-orchestration/sharding-orchestration-center/src/main/java/org/apache/shardingsphere/orchestration/center/instance/ApolloInstance.java
@@ -0,0 +1,209 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.orchestration.center.instance;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.core.ConfigConsts;
+import com.ctrip.framework.apollo.enums.PropertyChangeType;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.openapi.client.ApolloOpenApiClient;
+import com.ctrip.framework.apollo.openapi.client.constant.ApolloOpenApiConstants;
+import com.ctrip.framework.apollo.openapi.dto.NamespaceReleaseDTO;
+import com.ctrip.framework.apollo.openapi.dto.OpenItemDTO;
+import com.google.common.collect.Sets;
+import com.google.common.primitives.Ints;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.orchestration.center.api.ConfigCenter;
+import org.apache.shardingsphere.orchestration.center.configuration.InstanceConfiguration;
+import org.apache.shardingsphere.orchestration.center.instance.node.ConfigTreeNode;
+import org.apache.shardingsphere.orchestration.center.listener.DataChangedEvent;
+import org.apache.shardingsphere.orchestration.center.listener.DataChangedEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Registry center for Apollo.
+ *
+ * @author dongzonglei
+ */
+@Slf4j
+public final class ApolloInstance implements ConfigCenter {
+    
+    private static final String SHARDING_SPHERE_KEY_ROOT = "/";
+    
+    private static final String SHARDING_SPHERE_KEY_SEPARATOR = "/";
+    
+    private static final String APOLLO_KEY_SEPARATOR = ".";
+    
+    private static final String APOLLO_KEY_APP_ID = "app.id";
+    
+    private static final String APOLLO_KEY_ENV = "env";
+    
+    private static final String APOLLO_KEY_CLUSTER = ConfigConsts.APOLLO_CLUSTER_KEY;
+    
+    private static final String APOLLO_KEY_META = ConfigConsts.APOLLO_META_KEY;
+    
+    private String namespace;
+    
+    private String appId;
+    
+    private String env;
+    
+    private String clusterName;
+    
+    private String administrator;
+    
+    private Config apolloConfig;
+    
+    private ApolloOpenApiClient client;
+    
+    private ConfigTreeNode tree = new ConfigTreeNode(null, "/", Sets.<ConfigTreeNode>newHashSet());
+    
+    @Getter
+    @Setter
+    private Properties properties = new Properties();
+    
+    @Override
+    public void init(final InstanceConfiguration config) {
+        initApolloConfig(config);
+        initApolloOpenApiClient();
+        initKeysRelationship();
+    }
+    
+    private void initApolloConfig(final InstanceConfiguration config) {
+        namespace = config.getNamespace();
+        appId = properties.getProperty("appId", "APOLLO_SHARDING_SPHERE");
+        env = properties.getProperty("env", "DEV");
+        clusterName = properties.getProperty("clusterName", ConfigConsts.CLUSTER_NAME_DEFAULT);
+        System.setProperty(APOLLO_KEY_APP_ID, appId);
+        System.setProperty(APOLLO_KEY_ENV, env);
+        System.setProperty(APOLLO_KEY_CLUSTER, clusterName);
+        System.setProperty(APOLLO_KEY_META, config.getServerLists());
+        apolloConfig = ConfigService.getConfig(namespace);
+    }
+    
+    private void initApolloOpenApiClient() {
+        administrator = properties.getProperty("administrator");
+        String apolloToken = properties.getProperty("token");
+        String portalUrl = properties.getProperty("portalUrl");
+        Integer connectTimeout = Ints.tryParse(properties.getProperty("connectTimeout"));
+        Integer readTimeout = Ints.tryParse(properties.getProperty("readTimeout"));
+        client = ApolloOpenApiClient.newBuilder().withPortalUrl(portalUrl)
+                .withConnectTimeout(connectTimeout == null ? ApolloOpenApiConstants.DEFAULT_CONNECT_TIMEOUT : connectTimeout)
+                .withReadTimeout(readTimeout == null ? ApolloOpenApiConstants.DEFAULT_READ_TIMEOUT : readTimeout)
+                .withToken(apolloToken).build();
+    }
+    
+    private void initKeysRelationship() {
+        List<OpenItemDTO> items = client.getNamespace(appId, env, clusterName, namespace).getItems();
+        Set<String> keys = Sets.newHashSet();
+        for (OpenItemDTO each : items) {
+            keys.add(each.getKey());
+        }
+        tree.initTree(keys, APOLLO_KEY_SEPARATOR);
+    }
+    
+    @Override
+    public String get(final String key) {
+        return apolloConfig.getProperty(convertKey(key), "");
+    }
+    
+    private String convertKey(final String shardingSphereKey) {
+        return shardingSphereKey.replace(SHARDING_SPHERE_KEY_SEPARATOR, APOLLO_KEY_SEPARATOR).substring(1);
+    }
+    
+    private String deConvertKey(final String apolloKey) {
+        return new StringBuilder(SHARDING_SPHERE_KEY_ROOT).append(apolloKey.replace(APOLLO_KEY_SEPARATOR, SHARDING_SPHERE_KEY_SEPARATOR)).toString();
+    }
+    
+    @Override
+    public List<String> getChildrenKeys(final String key) {
+        return new ArrayList<>(tree.getChildrenKeys(key));
+    }
+    
+    @Override
+    public void persist(final String key, final String value) {
+        String apolloKey = convertKey(key);
+        updateKey(apolloKey, value);
+        publishNamespace();
+        tree.refresh(apolloKey, APOLLO_KEY_SEPARATOR);
+    }
+    
+    private void updateKey(final String key, final String value) {
+        OpenItemDTO openItem = new OpenItemDTO();
+        openItem.setKey(key);
+        openItem.setValue(value);
+        openItem.setComment("ShardingSphere create or update config");
+        openItem.setDataChangeCreatedBy(administrator);
+        client.createOrUpdateItem(appId, env, clusterName, namespace, openItem);
+    }
+    
+    private void publishNamespace() {
+        NamespaceReleaseDTO release = new NamespaceReleaseDTO();
+        release.setReleaseTitle("ShardingSphere namespace release");
+        release.setReleaseComment("ShardingSphere namespace release");
+        release.setReleasedBy(administrator);
+        release.setEmergencyPublish(true);
+        client.publishNamespace(appId, env, clusterName, namespace, release);
+    }
+    
+    @Override
+    public void watch(final String key, final DataChangedEventListener dataChangedEventListener) {
+        apolloConfig.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(final ConfigChangeEvent changeEvent) {
+                for (String key : changeEvent.changedKeys()) {
+                    ConfigChange change = changeEvent.getChange(key);
+                    DataChangedEvent.ChangedType changedType = getChangedType(change.getChangeType());
+                    if (DataChangedEvent.ChangedType.IGNORED != changedType) {
+                        dataChangedEventListener.onChange(new DataChangedEvent(deConvertKey(key), change.getNewValue(), changedType));
+                    }
+                }
+            }
+        }, Sets.newHashSet(key));
+    }
+    
+    private DataChangedEvent.ChangedType getChangedType(final PropertyChangeType changeType) {
+        switch (changeType) {
+            case MODIFIED:
+                return DataChangedEvent.ChangedType.UPDATED;
+            case DELETED:
+                return DataChangedEvent.ChangedType.DELETED;
+            default:
+                return DataChangedEvent.ChangedType.IGNORED;
+        }
+    }
+    
+    @Override
+    public void close() {
+
+    }
+    
+    @Override
+    public String getType() {
+        return "apollo";
+    }
+}
---
+++ sharding-orchestration/sharding-orchestration-center/src/main/java/org/apache/shardingsphere/orchestration/center/instance/node/ConfigTreeNode.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.orchestration.center.instance.node;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Config Tree Node.
+ *
+ * @author dongzonglei
+ * @author wangguangyuan
+ * @author sunbufu
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+public final class ConfigTreeNode {
+    
+    private static final String SHARDING_SPHERE_KEY_ROOT = "/";
+    
+    private static final String SHARDING_SPHERE_KEY_SEPARATOR = "/";
+    
+    private static Map<String, Set<String>> childrenKeyMap = new ConcurrentHashMap<>();
+    
+    private ConfigTreeNode parentNode;
+    
+    private String absolutePath;
+    
+    private Set<ConfigTreeNode> childrenNodes;
+    
+    /**
+     * init tree.
+     * 
+     * @param instanceKeys instance Key set
+     * @param keySeparator key separator
+     */
+    public void initTree(final Set<String> instanceKeys, final String keySeparator) {
+        initKeysRelationship(instanceKeys, keySeparator);
+        initTree(this, SHARDING_SPHERE_KEY_ROOT);
+    }
+    
+    private void initTree(final ConfigTreeNode parentNode, final String shardingSphereKey) {
+        Set<String> childrenKeys = childrenKeyMap.get(shardingSphereKey);
+        if (childrenKeys == null || childrenKeys.isEmpty()) {
+            return;
+        }
+        for (String each : childrenKeys) {
+            ConfigTreeNode child = new ConfigTreeNode(parentNode, each, Sets.<ConfigTreeNode>newHashSet());
+            parentNode.getChildrenNodes().add(child);
+            initTree(child, each);
+        }
+    }
+    
+    private void initKeysRelationship(final Set<String> instanceKeys, final String keySeparator) {
+        for (String each : instanceKeys) {
+            initKeysRelationship(each, keySeparator);
+        }
+    }
+    
+    private void initKeysRelationship(final String instanceKey, final String keySeparator) {
+        if (!instanceKey.contains(keySeparator)) {
+            addRelationship(SHARDING_SPHERE_KEY_ROOT, Joiner.on(SHARDING_SPHERE_KEY_SEPARATOR).join(instanceKey, ""));
+            return;
+        }
+        String parentKey = "/";
+        String shardingSphereKey = deConvertKey(instanceKey, keySeparator);
+        for (int i = 1; i <= shardingSphereKey.lastIndexOf(SHARDING_SPHERE_KEY_SEPARATOR); i = shardingSphereKey.indexOf(SHARDING_SPHERE_KEY_SEPARATOR, i) + 1) {
+            String childrenKey = shardingSphereKey.substring(0, shardingSphereKey.indexOf(SHARDING_SPHERE_KEY_SEPARATOR, i));
+            addRelationship(parentKey, childrenKey);
+            parentKey = childrenKey;
+        }
+        addRelationship(parentKey, shardingSphereKey);
+    }
+    
+    private void addRelationship(final String parentKey, final String childrenKey) {
+        Set<String> childrenKeys = childrenKeyMap.containsKey(parentKey) ? childrenKeyMap.get(parentKey) : new HashSet<String>();
+        childrenKeys.add(childrenKey);
+        childrenKeyMap.put(parentKey, childrenKeys);
+    }
+    
+    private String deConvertKey(final String instanceKey, final String keySeparator) {
+        return new StringBuilder(SHARDING_SPHERE_KEY_ROOT).append(instanceKey.replace(keySeparator, SHARDING_SPHERE_KEY_SEPARATOR)).toString();
+    }
+    
+    /**
+     * get children key set.
+     * 
+     * @param shardingSphereKey Sharding Sphere Key
+     * @return children key set
+     */
+    public Set<String> getChildrenKeys(final String shardingSphereKey) {
+        return childrenKeyMap.get(shardingSphereKey);
+    }
+    
+    /**
+     * refresh tree.
+     * 
+     * @param instanceKey new instance key
+     * @param keySeparator key separator
+     */
+    public void refresh(final String instanceKey, final String keySeparator) {
+        initKeysRelationship(instanceKey, keySeparator);
+        this.getChildrenNodes().clear();
+        initTree(this, SHARDING_SPHERE_KEY_ROOT);
+    }
+}
