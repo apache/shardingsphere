721a781c790194781ecbbd70f33c980496dbeeb6
terrymanu <terrymanu@163.com>
2019-12-07 17:29:38 +0800

    refactor AbstractDataSourceAdapter.close()

--- sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/adapter/AbstractDataSourceAdapter.java
+++ sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/adapter/AbstractDataSourceAdapter.java
@@ -21,6 +21,7 @@ import com.google.common.base.Preconditions;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.shardingsphere.core.database.DatabaseTypes;
+import org.apache.shardingsphere.shardingjdbc.jdbc.core.context.RuntimeContext;
 import org.apache.shardingsphere.shardingjdbc.jdbc.unsupported.AbstractUnsupportedOperationDataSource;
 import org.apache.shardingsphere.spi.database.DatabaseType;
 
@@ -92,7 +93,7 @@ public abstract class AbstractDataSourceAdapter extends AbstractUnsupportedOpera
     }
     
     @Override
-    public void close() throws Exception {
+    public final void close() throws Exception {
         close(dataSourceMap.keySet());
     }
     
@@ -106,6 +107,7 @@ public abstract class AbstractDataSourceAdapter extends AbstractUnsupportedOpera
         for (String each : dataSourceNames) {
             close(dataSourceMap.get(each));
         }
+        getRuntimeContext().close();
     }
     
     private void close(final DataSource dataSource) {
@@ -116,4 +118,6 @@ public abstract class AbstractDataSourceAdapter extends AbstractUnsupportedOpera
         } catch (final ReflectiveOperationException ignored) {
         }
     }
+    
+    protected abstract RuntimeContext getRuntimeContext();
 }
