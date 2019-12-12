47bc1e99ae55df2f97e2476961b8a6453e5737b7
avalon566 <avalon566@163.com>
2019-12-12 19:07:02 +0800

    Load source configuration from sharding proxy yaml configuration

--- sharding-scaling/sharding-scaling-core/src/main/java/org/apache/shardingsphere/shardingscaling/core/web/util/SyncConfigurationUtil.java
+++ sharding-scaling/sharding-scaling-core/src/main/java/org/apache/shardingsphere/shardingscaling/core/web/util/SyncConfigurationUtil.java
@@ -17,16 +17,23 @@
 
 package org.apache.shardingsphere.shardingscaling.core.web.util;
 
+import org.apache.shardingsphere.core.rule.ShardingRule;
+import org.apache.shardingsphere.core.rule.TableRule;
+import org.apache.shardingsphere.core.yaml.engine.YamlEngine;
+import org.apache.shardingsphere.core.yaml.swapper.impl.ShardingRuleConfigurationYamlSwapper;
 import org.apache.shardingsphere.shardingscaling.core.config.DataSourceConfiguration;
 import org.apache.shardingsphere.shardingscaling.core.config.JdbcDataSourceConfiguration;
 import org.apache.shardingsphere.shardingscaling.core.config.RdbmsConfiguration;
-import org.apache.shardingsphere.shardingscaling.core.config.RuleConfiguration;
 import org.apache.shardingsphere.shardingscaling.core.config.ScalingConfiguration;
 import org.apache.shardingsphere.shardingscaling.core.config.SyncConfiguration;
+import org.apache.shardingsphere.shardingscaling.core.config.yaml.YamlDataSourceParameter;
+import org.apache.shardingsphere.shardingscaling.core.config.yaml.YamlProxyRuleConfiguration;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -35,32 +42,106 @@ import java.util.Map;
  * @author ssxlulu
  */
 public class SyncConfigurationUtil {
-
+    
     /**
      * Split ScalingConfiguration to SyncConfigurations.
      *
      * @param scalingConfiguration ScalingConfiguration
      * @return List of SyncConfigurations
      */
-    public static List<SyncConfiguration> toSyncConfigurations(final ScalingConfiguration scalingConfiguration) {
-        RuleConfiguration ruleConfig = scalingConfiguration.getRuleConfiguration();
-        List<SyncConfiguration> syncConfigurations = new ArrayList<SyncConfiguration>(ruleConfig.getDataSources().size());
-        for (RuleConfiguration.YamlDataSourceParameter entry : ruleConfig.getDataSources()) {
-            RdbmsConfiguration readerConfiguration = new RdbmsConfiguration();
-            DataSourceConfiguration readerDataSourceConfiguration = new JdbcDataSourceConfiguration(
-                    entry.getUrl(),
-                    entry.getUsername(),
-                    entry.getPassword());
-            readerConfiguration.setDataSourceConfiguration(readerDataSourceConfiguration);
-            RdbmsConfiguration writerConfiguration = new RdbmsConfiguration();
-            DataSourceConfiguration writerDataSourceConfiguration = new JdbcDataSourceConfiguration(
-                    ruleConfig.getDestinationDataSources().getUrl(),
-                    ruleConfig.getDestinationDataSources().getUsername(),
-                    ruleConfig.getDestinationDataSources().getPassword());
-            writerConfiguration.setDataSourceConfiguration(writerDataSourceConfiguration);
-            syncConfigurations.add(new SyncConfiguration(scalingConfiguration.getJobConfiguration().getConcurrency(), new HashMap<String, String>(),
+    public static Collection<SyncConfiguration> toSyncConfigurations(final ScalingConfiguration scalingConfiguration) {
+        Collection<SyncConfiguration> result = new LinkedList<>();
+        YamlProxyRuleConfiguration ruleConfiguration = loadTestYamlProxyRuleConfiguration();
+        Map<String, YamlDataSourceParameter> dataSources = getDataSources(ruleConfiguration);
+        Map<String, Map<String, String>> dataSourceTableNameMap = toDataSourceTableNameMap(ruleConfiguration);
+        for (String each : dataSourceTableNameMap.keySet()) {
+            RdbmsConfiguration readerConfiguration = createReaderConfiguration(dataSources.get(each));
+            RdbmsConfiguration writerConfiguration = createWriterConfiguration(scalingConfiguration);
+            Map<String, String> tableNameMap = dataSourceTableNameMap.get(each);
+            if (null == tableNameMap) {
+                tableNameMap = new HashMap<>();
+            }
+            result.add(new SyncConfiguration(scalingConfiguration.getJobConfiguration().getConcurrency(), tableNameMap,
                     readerConfiguration, writerConfiguration));
         }
-        return syncConfigurations;
+        return result;
+    }
+    
+    private static RdbmsConfiguration createReaderConfiguration(final YamlDataSourceParameter dataSource) {
+        RdbmsConfiguration result = new RdbmsConfiguration();
+        DataSourceConfiguration readerDataSourceConfiguration = new JdbcDataSourceConfiguration(
+                dataSource.getUrl(),
+                dataSource.getUsername(),
+                dataSource.getPassword());
+        result.setDataSourceConfiguration(readerDataSourceConfiguration);
+        return result;
+    }
+    
+    private static RdbmsConfiguration createWriterConfiguration(final ScalingConfiguration scalingConfiguration) {
+        RdbmsConfiguration writerConfiguration = new RdbmsConfiguration();
+        DataSourceConfiguration writerDataSourceConfiguration = new JdbcDataSourceConfiguration(
+                scalingConfiguration.getRuleConfiguration().getDestinationDataSources().getUrl(),
+                scalingConfiguration.getRuleConfiguration().getDestinationDataSources().getUsername(),
+                scalingConfiguration.getRuleConfiguration().getDestinationDataSources().getPassword());
+        writerConfiguration.setDataSourceConfiguration(writerDataSourceConfiguration);
+        return writerConfiguration;
+    }
+    
+    private static YamlProxyRuleConfiguration loadTestYamlProxyRuleConfiguration() {
+        try {
+            return YamlEngine.unmarshal(new File(SyncConfigurationUtil.class.getResource("/conf/config-sharding.yaml").getFile()),
+                    YamlProxyRuleConfiguration.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private static Map<String, YamlDataSourceParameter> getDataSources(final YamlProxyRuleConfiguration ruleConfiguration) {
+        return ruleConfiguration.getDataSources();
+    }
+    
+    private static ShardingRule getShardingRule(final YamlProxyRuleConfiguration ruleConfiguration) {
+        return new ShardingRule(new ShardingRuleConfigurationYamlSwapper().swap(ruleConfiguration.getShardingRule()), getDataSources(ruleConfiguration).keySet());
+    }
+    
+    private static Map<String, Map<String, String>> toDataSourceTableNameMap(final YamlProxyRuleConfiguration ruleConfiguration) {
+        ShardingRule shardingRule = getShardingRule(ruleConfiguration);
+        Map<String, Map<String, String>> result = new HashMap<>();
+        for (TableRule each : shardingRule.getTableRules()) {
+            mergeDataSourceTableNameMap(result, toDataSourceTableNameMap(each));
+        }
+        return result;
+    }
+    
+    private static Map<String, Map<String, String>> toDataSourceTableNameMap(final TableRule tableRule) {
+        Map<String, Map<String, String>> result = new HashMap<>();
+        for (Map.Entry<String, Collection<String>> each : tableRule.getDatasourceToTablesMap().entrySet()) {
+            Map<String, String> tableNameMap = result.get(each.getKey());
+            if (null == tableNameMap) {
+                result.put(each.getKey(), toTableNameMap(tableRule.getLogicTable(), each.getValue()));
+            } else {
+                tableNameMap.putAll(toTableNameMap(tableRule.getLogicTable(), each.getValue()));
+            }
+        }
+        return result;
+    }
+    
+    private static void mergeDataSourceTableNameMap(final Map<String, Map<String, String>> o, final Map<String, Map<String, String>> n) {
+        for (Map.Entry<String, Map<String, String>> each : n.entrySet()) {
+            Map<String, String> tableNameMap = o.get(each.getKey());
+            if (null == tableNameMap) {
+                o.put(each.getKey(), each.getValue());
+            } else {
+                tableNameMap.putAll(each.getValue());
+            }
+        }
+    }
+    
+    private static Map<String, String> toTableNameMap(final String logicalTable, final Collection<String> actualTables) {
+        Map<String, String> result = new HashMap<>();
+        for (String each : actualTables) {
+            result.put(each, logicalTable);
+        }
+        return result;
     }
 }
