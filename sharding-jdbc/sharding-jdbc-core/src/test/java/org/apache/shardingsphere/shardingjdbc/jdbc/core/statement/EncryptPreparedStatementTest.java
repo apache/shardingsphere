d143bd49c72123f5fb6015d4867aed9bf156f432
Nicholas Jiang <programgeek@163.com>
2019-12-13 12:00:21 +0800

    encrypt statement in operator

--- sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptPreparedStatementTest.java
+++ sharding-jdbc/sharding-jdbc-core/src/test/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/statement/EncryptPreparedStatementTest.java
@@ -53,7 +53,7 @@ public final class EncryptPreparedStatementTest extends AbstractEncryptJDBCDatab
     private static final String SELECT_ALL_SQL = "select id, cipher_pwd, assist_pwd from t_query_encrypt";
     
     private static final String SELECT_FULL_SQL = "select id, cipher_pwd, plain_pwd, assist_pwd from t_query_and_plain_encrypt";
-
+    
     private static final String SELECT_SQL_WITH_IN_OPERATOR = "select * from t_query_encrypt where pwd IN (?)";
 
     @Test
