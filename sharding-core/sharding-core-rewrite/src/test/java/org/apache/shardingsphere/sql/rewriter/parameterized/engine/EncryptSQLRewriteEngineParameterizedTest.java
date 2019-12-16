dbac398a9418b8912e130ec4f3be790b4c0418a0
terrymanu <terrymanu@163.com>
2019-12-16 22:37:15 +0800

    add SQLRewriteEngineTestParameters

--- sharding-core/sharding-core-rewrite/src/test/java/org/apache/shardingsphere/sql/rewriter/parameterized/engine/EncryptSQLRewriteEngineParameterizedTest.java
+++ sharding-core/sharding-core-rewrite/src/test/java/org/apache/shardingsphere/sql/rewriter/parameterized/engine/EncryptSQLRewriteEngineParameterizedTest.java
@@ -17,11 +17,7 @@
 
 package org.apache.shardingsphere.sql.rewriter.parameterized.engine;
 
-import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.google.common.primitives.Ints;
 import lombok.RequiredArgsConstructor;
 import org.apache.shardingsphere.core.metadata.table.TableMetas;
 import org.apache.shardingsphere.core.rule.EncryptRule;
@@ -37,10 +33,7 @@ import org.apache.shardingsphere.sql.rewriter.context.SQLRewriteContext;
 import org.apache.shardingsphere.sql.rewriter.engine.SQLRewriteResult;
 import org.apache.shardingsphere.sql.rewriter.engine.impl.DefaultSQLRewriteEngine;
 import org.apache.shardingsphere.sql.rewriter.feature.encrypt.context.EncryptSQLRewriteContextDecorator;
-import org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.entity.RewriteAssertionEntity;
-import org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.entity.RewriteAssertionsRootEntity;
-import org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.entity.RewriteOutputEntity;
-import org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.loader.RewriteAssertionsRootEntityLoader;
+import org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.engine.SQLRewriteEngineTestParametersBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -49,16 +42,10 @@ import org.junit.runners.Parameterized.Parameters;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -89,61 +76,7 @@ public final class EncryptSQLRewriteEngineParameterizedTest {
     
     @Parameters(name = "ENCRYPT: {2} -> {0}")
     public static Collection<Object[]> getTestParameters() {
-        Collection<Object[]> result = new LinkedList<>();
-        for (Entry<String, RewriteAssertionsRootEntity> entry : getAllRewriteAssertionsRootEntities().entrySet()) {
-            result.addAll(getTestParameters(entry.getKey(), entry.getValue()));
-        }
-        return result;
-    }
-    
-    private static Collection<Object[]> getTestParameters(final String fileName, final RewriteAssertionsRootEntity rootAssertions) {
-        Collection<Object[]> result = new LinkedList<>();
-        for (RewriteAssertionEntity each : rootAssertions.getAssertions()) {
-            result.add(getTestParameter(fileName, rootAssertions, each));
-        }
-        return result;
-    }
-    
-    private static Object[] getTestParameter(final String fileName, final RewriteAssertionsRootEntity rootAssertions, final RewriteAssertionEntity assertion) {
-        Object[] result = new Object[8];
-        result[0] = fileName;
-        result[1] = rootAssertions.getYamlRule();
-        result[2] = assertion.getId();
-        result[3] = assertion.getInput().getSql();
-        if (null == assertion.getInput().getParameters()) {
-            result[4] = Collections.emptyList();
-        } else {
-            result[4] = Lists.transform(Splitter.on(",").trimResults().splitToList(assertion.getInput().getParameters()), new Function<String, Object>() {
-            
-                @Override
-                public Object apply(final String input) {
-                    Object result = Ints.tryParse(input);
-                    return result == null ? input : result;
-                }
-            });
-        }
-        List<RewriteOutputEntity> outputs = assertion.getOutputs();
-        List<String> outputSQLs = new ArrayList<>(outputs.size());
-        List<Object> outputGroupedParameters = new ArrayList<>(outputs.size());
-        for (RewriteOutputEntity each : outputs) {
-            outputSQLs.add(each.getSql());
-            outputGroupedParameters.add(null == each.getParameters() ? Collections.emptyList() : Splitter.on(",").trimResults().splitToList(each.getParameters()));
-        }
-        result[5] = outputSQLs;
-        result[6] = outputGroupedParameters;
-        result[7] = assertion.getDatabaseType();
-        return result;
-    }
-    
-    private static Map<String, RewriteAssertionsRootEntity> getAllRewriteAssertionsRootEntities() {
-        Map<String, RewriteAssertionsRootEntity> result = new LinkedHashMap<>();
-        File file = new File(EncryptSQLRewriteEngineParameterizedTest.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "/" + PATH);
-        for (File each : Objects.requireNonNull(file.listFiles())) {
-            if (each.getName().endsWith(".xml")) {
-                result.put(each.getName(), new RewriteAssertionsRootEntityLoader().load(PATH + "/" + each.getName()));
-            }
-        }
-        return result;
+        return SQLRewriteEngineTestParametersBuilder.loadTestParameters(PATH, EncryptSQLRewriteEngineParameterizedTest.class);
     }
     
     @Test
---
+++ shardingsphere-sql-rewriter/shardingsphere-sql-rewriter-test/src/main/java/org/apache/shardingsphere/sql/rewriter/parameterized/jaxb/engine/SQLRewriteEngineTestParameters.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.rewriter.parameterized.jaxb.engine;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Test parameters for SQL rewrite engine.
+ *
+ * @author zhangliang
+ */
+@RequiredArgsConstructor
+public final class SQLRewriteEngineTestParameters {
+    
+    private final String fileName;
+    
+    private final String ruleFile;
+    
+    private final String name;
+    
+    private final String inputSQL;
+    
+    private final List<Object> inputParameters;
+    
+    private final List<String> outputSQLs;
+    
+    private final List<List<String>> outputGroupedParameters;
+    
+    private final String databaseType;
+    
+    /**
+     * To array.
+     * 
+     * @return array value of test parameters
+     */
+    public Object[] toArray() {
+        Object[] result = new Object[8];
+        result[0] = fileName;
+        result[1] = ruleFile;
+        result[2] = name;
+        result[3] = inputSQL;
+        result[4] = inputParameters;
+        result[5] = outputSQLs;
+        result[6] = outputGroupedParameters;
+        result[7] = databaseType;
+        return result;
+    }
+}
