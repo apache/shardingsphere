ff17d684d36a222ba4eab613c5e452a4b1aed430
terrymanu <terrymanu@163.com>
2020-01-18 02:26:38 +0800

    refactor ExpectedProjections's field name

--- shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/jaxb/projection/ExpectedProjections.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/jaxb/projection/ExpectedProjections.java
@@ -42,22 +42,22 @@ public final class ExpectedProjections {
     private boolean distinctRow;
     
     @XmlElement(name = "shorthand-projection")
-    private Collection<ExpectedShorthandProjection> expectedShorthandProjections = new LinkedList<>();
+    private Collection<ExpectedShorthandProjection> shorthandProjections = new LinkedList<>();
     
     @XmlElement(name = "aggregation-projection")
-    private Collection<ExpectedAggregationProjection> expectedAggregationProjections = new LinkedList<>();
+    private Collection<ExpectedAggregationProjection> aggregationProjections = new LinkedList<>();
     
     @XmlElement(name = "aggregation-distinct-projection")
-    private Collection<ExpectedAggregationDistinctProjection> expectedAggregationDistinctProjections = new LinkedList<>();
+    private Collection<ExpectedAggregationDistinctProjection> aggregationDistinctProjections = new LinkedList<>();
     
     @XmlElement(name = "column-projection")
-    private Collection<ExpectedColumnProjection> expectedColumnProjections = new LinkedList<>();
+    private Collection<ExpectedColumnProjection> columnProjections = new LinkedList<>();
     
     @XmlElement(name = "expression-projection")
-    private Collection<ExpectedExpressionProjection> expectedExpressionProjections = new LinkedList<>();
+    private Collection<ExpectedExpressionProjection> expressionProjections = new LinkedList<>();
     
     @XmlElement(name = "top-projection")
-    private Collection<ExpectedTopProjection> expectedTopProjections = new LinkedList<>();
+    private Collection<ExpectedTopProjection> topProjections = new LinkedList<>();
     
     /**
      * Get size.
@@ -65,9 +65,7 @@ public final class ExpectedProjections {
      * @return size
      */
     public int getSize() {
-        return expectedAggregationProjections.size() + expectedShorthandProjections.size()
-                + expectedAggregationDistinctProjections.size() + expectedColumnProjections.size()
-                + expectedExpressionProjections.size() + expectedTopProjections.size();
+        return aggregationProjections.size() + shorthandProjections.size() + aggregationDistinctProjections.size() + columnProjections.size() + expressionProjections.size() + topProjections.size();
     }
     
     /**
@@ -80,22 +78,22 @@ public final class ExpectedProjections {
     public <T extends ExpectedProjection> Collection<ExpectedProjection> findExpectedProjections(final Class<T> expectedProjectionType) {
         Collection<ExpectedProjection> result = new LinkedList<>();
         if (expectedProjectionType.equals(ExpectedShorthandProjection.class)) {
-            result.addAll(expectedShorthandProjections);
+            result.addAll(shorthandProjections);
         }
         if (expectedProjectionType.equals(ExpectedAggregationProjection.class)) {
-            result.addAll(expectedAggregationProjections);
+            result.addAll(aggregationProjections);
         }
         if (expectedProjectionType.equals(ExpectedColumnProjection.class)) {
-            result.addAll(expectedColumnProjections);
+            result.addAll(columnProjections);
         }
         if (expectedProjectionType.equals(ExpectedAggregationDistinctProjection.class)) {
-            result.addAll(expectedAggregationDistinctProjections);
+            result.addAll(aggregationDistinctProjections);
         }
         if (expectedProjectionType.equals(ExpectedExpressionProjection.class)) {
-            result.addAll(expectedExpressionProjections);
+            result.addAll(expressionProjections);
         }
         if (expectedProjectionType.equals(ExpectedTopProjection.class)) {
-            result.addAll(expectedExpressionProjections);
+            result.addAll(expressionProjections);
         }
         return result;
     }
