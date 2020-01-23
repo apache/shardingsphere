7cf157e8b744799f651ce6a62acf866f2b1e6696
Liang Zhang <terrymanu@163.com>
2020-01-23 17:55:17 +0800

    Add assert column right value for predicate (#4070)
    * add shardingsphere-ui-bin-distribution module
    
    * add shardingsphere-ui-src-distribution module
    
    * add final name for shardingsphere-ui
    
    * add column-left-value
    
    * fix case select_count_with_binding_tables_without_join
    
    * fix case select_distinct_with_owner_star
    
    * fix case select_escape_with_double_quota
    
    * fix case select_order_by_desc_and_index_asc
    
    * fix case select_order_by_with_date

--- shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/jaxb/impl/predicate/ExpectedPredicate.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/jaxb/impl/predicate/ExpectedPredicate.java
@@ -31,17 +31,20 @@ import javax.xml.bind.annotation.XmlElement;
 @Setter
 public final class ExpectedPredicate extends AbstractExpectedSQLSegment {
     
-    @XmlElement
-    private ExpectedColumn column;
+    @XmlElement(name = "column-left-value")
+    private ExpectedColumn columnLeftValue;
     
-    @XmlElement(name = "predicate-between-right-value")
-    private ExpectedPredicateBetweenRightValue betweenRightValue = new ExpectedPredicateBetweenRightValue();
+    @XmlElement(name = "column-right-value")
+    private ExpectedColumn columnRightValue;
+    
+    @XmlElement(name = "predicate-compare-right-value")
+    private ExpectedPredicateCompareRightValue compareRightValue = new ExpectedPredicateCompareRightValue();
     
     @XmlElement(name = "predicate-in-right-value")
     private ExpectedPredicateInRightValue inRightValue = new ExpectedPredicateInRightValue();
     
-    @XmlElement(name = "predicate-compare-right-value")
-    private ExpectedPredicateCompareRightValue compareRightValue = new ExpectedPredicateCompareRightValue();
+    @XmlElement(name = "predicate-between-right-value")
+    private ExpectedPredicateBetweenRightValue betweenRightValue = new ExpectedPredicateBetweenRightValue();
     
     /**
      * Find expected right value type.
