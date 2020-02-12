5d54909296a5aca8c55ad57c64db54df4b093cb6
tristaZero <hannkenn@163.com>
2020-02-12 14:23:54 +0800

    move docs files and modify build script

--- docs/build.sh
+++ docs/build.sh
@@ -2,20 +2,11 @@
 cd `dirname $0`
 
 rm -rf target
-mkdir target
+mkdir -p target/document
 
 cp -rf homepage/. target
 
-mkdir -p target/document/legacy/1.x
-mkdir -p target/document/legacy/2.x
-mkdir -p target/document/legacy/3.x
-
-cp -rf document/legacy/1.x/cn target/document/legacy/1.x/cn
-cp -rf document/legacy/2.x/cn target/document/legacy/2.x/cn
-cp -rf document/legacy/2.x/en target/document/legacy/2.x/en
-cp -rf document/legacy/3.x/* target/document/legacy/3.x/
-
-cd document/current
+cd document
 hugo
 
 find ./ -name '*.html' -exec sed -i -e 's|[[:space:]]*<option id="\([a-zA-Z]\+\)" value="|<option id="\1" value="/document/current|g' {} \;
@@ -24,14 +15,12 @@ cd public/en
 sed -i -e 's/cn/en/g' index.html
 cd ../..
 
-
 cd ..
-cd ..
-mv document/current/public target/document/current
+mv document/public target/document/current
 
 mkdir target/community
 
-cd community/
+cd community
 hugo
 
 find ./ -name '*.html' -exec sed -i -e 's|[[:space:]]*<option id="\([a-zA-Z]\+\)" value="|<option id="\1" value="/community|g' {} \;
