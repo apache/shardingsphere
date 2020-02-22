bfe5f54e5164bca3f2b16b36cc4dc37f398d3f9a
Liang Zhang <terrymanu@163.com>
2020-02-22 13:46:13 +0800

    Switch SQL parser engine to new one (#4408)
    * Filter alias's owner of join table
    
    * fix test case
    
    * Transfer SQL parser engine to new one

--- shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/core/SQLParseKernel.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/core/SQLParseKernel.java
@@ -17,17 +17,11 @@
 
 package org.apache.shardingsphere.sql.parser.core;
 
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.apache.shardingsphere.sql.parser.core.extractor.SQLSegmentsExtractorEngine;
-import org.apache.shardingsphere.sql.parser.core.filler.SQLStatementFillerEngine;
 import org.apache.shardingsphere.sql.parser.core.parser.SQLAST;
 import org.apache.shardingsphere.sql.parser.core.parser.SQLParserEngine;
 import org.apache.shardingsphere.sql.parser.core.rule.registry.ParseRuleRegistry;
-import org.apache.shardingsphere.sql.parser.sql.segment.SQLSegment;
+import org.apache.shardingsphere.sql.parser.core.visitor.ParseTreeVisitorFactory;
 import org.apache.shardingsphere.sql.parser.sql.statement.SQLStatement;
-
-import java.util.Collection;
-import java.util.Map;
 
 /**
  * SQL parse kernel.
@@ -36,16 +30,10 @@ public final class SQLParseKernel {
     
     private final SQLParserEngine parserEngine;
     
-    private final SQLSegmentsExtractorEngine extractorEngine;
-    
-    private final SQLStatementFillerEngine fillerEngine;
-    
     private final String databaseTypeName;
     
     public SQLParseKernel(final ParseRuleRegistry parseRuleRegistry, final String databaseTypeName, final String sql) {
         parserEngine = new SQLParserEngine(parseRuleRegistry, databaseTypeName, sql);
-        extractorEngine = new SQLSegmentsExtractorEngine();
-        fillerEngine = new SQLStatementFillerEngine(parseRuleRegistry, databaseTypeName);
         this.databaseTypeName = databaseTypeName;
     }
     
@@ -56,10 +44,6 @@ public final class SQLParseKernel {
      */
     public SQLStatement parse() {
         SQLAST ast = parserEngine.parse();
-//        NEW PARSER
-//        return (SQLStatement) ParseTreeVisitorFactory.newInstance(databaseTypeName, ast.getParserRuleContext()).visit(ast.getParserRuleContext());
-        Collection<SQLSegment> sqlSegments = extractorEngine.extract(ast);
-        Map<ParserRuleContext, Integer> parameterMarkerIndexes = ast.getParameterMarkerIndexes();
-        return fillerEngine.fill(sqlSegments, parameterMarkerIndexes.size(), ast.getSqlStatementRule());
+        return (SQLStatement) ParseTreeVisitorFactory.newInstance(databaseTypeName, ast.getParserRuleContext()).visit(ast.getParserRuleContext());
     }
 }
