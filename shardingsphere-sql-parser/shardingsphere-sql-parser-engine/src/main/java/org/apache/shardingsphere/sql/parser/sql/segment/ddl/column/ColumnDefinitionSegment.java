d0472e8ab3ea2946358d1b40f15ad4b5cf759507
terrymanu <terrymanu@163.com>
2020-02-25 23:30:53 +0800

    add CreateDefinitionSegment

--- shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/sql/segment/ddl/column/ColumnDefinitionSegment.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/sql/segment/ddl/column/ColumnDefinitionSegment.java
@@ -19,7 +19,7 @@ package org.apache.shardingsphere.sql.parser.sql.segment.ddl.column;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.shardingsphere.sql.parser.sql.segment.SQLSegment;
+import org.apache.shardingsphere.sql.parser.sql.segment.ddl.CreateDefinitionSegment;
 import org.apache.shardingsphere.sql.parser.util.SQLUtil;
 
 /**
@@ -27,7 +27,7 @@ import org.apache.shardingsphere.sql.parser.util.SQLUtil;
  */
 @Getter
 @Setter
-public final class ColumnDefinitionSegment implements SQLSegment {
+public final class ColumnDefinitionSegment implements CreateDefinitionSegment {
     
     private final int startIndex;
     
