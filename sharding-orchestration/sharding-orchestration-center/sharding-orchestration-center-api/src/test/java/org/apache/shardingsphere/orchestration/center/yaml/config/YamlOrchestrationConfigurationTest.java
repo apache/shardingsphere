8cf8dc813b2ddef28a6a7208c236438cd36dd119
likai.lu <likai.lu@jiangrongxin.com>
2020-02-29 17:36:31 +0800

    add unit test for YamlOrchestrationConfiguration

---
+++ sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-api/src/test/java/org/apache/shardingsphere/orchestration/center/yaml/config/YamlOrchestrationConfigurationTest.java
@@ -0,0 +1,20 @@
+package org.apache.shardingsphere.orchestration.center.yaml.config;
+
+
+
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class YamlOrchestrationConfigurationTest {
+
+    @Test
+    public void assertInstanceConfigurationMap() {
+        Map<String, YamlInstanceConfiguration> instanceConfigurationMap = new HashMap<>();
+        YamlOrchestrationConfiguration yamlOrchestrationConfiguration = new YamlOrchestrationConfiguration(instanceConfigurationMap);
+        assertThat(yamlOrchestrationConfiguration.getInstanceConfigurationMap(), is(instanceConfigurationMap));
+    }
+}
