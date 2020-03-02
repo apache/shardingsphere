87b7113a7f97a8e7215c768fa07e9c767e6a1e26
likai.lu <likai.lu@jiangrongxin.com>
2020-03-02 12:00:02 +0800

    unified code style  YamlOrchestrationConfigurationTest

--- sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-api/src/test/java/org/apache/shardingsphere/orchestration/center/yaml/config/YamlOrchestrationConfigurationTest.java
+++ sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-api/src/test/java/org/apache/shardingsphere/orchestration/center/yaml/config/YamlOrchestrationConfigurationTest.java
@@ -26,7 +26,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class YamlOrchestrationConfigurationTest {
-
     @Test
     public void assertInstanceConfigurationMap() {
         Map<String, YamlInstanceConfiguration> instanceConfigurationMap = new HashMap<>();
