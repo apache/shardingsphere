6baba6fef3f2f0c9e0751bcd90be4a0afff7e941
zhaojinchao <zhaojinchao@mobimagic.com>
2020-03-16 15:09:32 +0800

    modify the variable name.

--- sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/constant/SQLExceptionConstant.java
+++ sharding-jdbc/sharding-jdbc-core/src/main/java/org/apache/shardingsphere/shardingjdbc/jdbc/core/constant/SQLExceptionConstant.java
@@ -28,7 +28,7 @@ public final class SQLExceptionConstant {
     
     public static final String SQL_STRING_NULL_OR_EMPTY = "SQL String can not be NULL or empty.";
     
-    public static final String SQL_STATE = "S1002";
+    public static final String SQL_STATE_S1002 = "S1002";
     
-    public static final int SQL_VENDOR_CODE = 0;
+    public static final int SQL_VENDOR_CODE_ZERO = 0;
 }
