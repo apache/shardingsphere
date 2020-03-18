75905b0b2cf7228218e07bfc647e0063120a5a01
JasonKing168 <80904001@qq.com>
2020-03-18 22:59:05 +0800

    issue-4785: Add final for class and its fields; remove inner class accessLevel, etc.

--- sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/metadata/ShardingMetaDataLoader.java
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/metadata/ShardingMetaDataLoader.java
@@ -17,8 +17,6 @@
 
 package org.apache.shardingsphere.core.metadata;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
