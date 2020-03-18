7fd62a8d1d5075a7c985892a120c0c83cfe2515f
JasonKing168 <80904001@qq.com>
2020-03-18 17:41:47 +0800

    issue-4785: Check uniformed with all actual tables' meta data in once when loading them.

--- sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/metadata/ShardingMetaDataLoader.java
+++ sharding-core/sharding-core-common/src/main/java/org/apache/shardingsphere/core/metadata/ShardingMetaDataLoader.java
@@ -113,7 +113,7 @@ public final class ShardingMetaDataLoader {
 
         if (!errorMetaDataList.isEmpty()) {
             StringBuilder exceptionMessageBuilder = new StringBuilder("Cannot get uniformed table structure for logic table `%s`"
-                    + ", it has different meta data of actual tables are as follows:");
+                    + ",it has different meta data of actual tables are as follows:");
             for (String[] each : errorMetaDataList) {
                 exceptionMessageBuilder.append("\nactual table:").append(each[0]).append(", meta data:").append(each[1]);
             }
