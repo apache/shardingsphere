3311eb49c08c919666d1236333bc59be1cd856a2
lujingshang1 <lujingshang1@jd.com>
2020-03-23 19:35:33 +0800

    add JoinTableSegment

---
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/segment/dml/TableReferenceSegment.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.parser.sql.segment.dml;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.shardingsphere.sql.parser.sql.segment.SQLSegment;
+import org.apache.shardingsphere.sql.parser.sql.segment.generic.table.SimpleTableSegment;
+import org.apache.shardingsphere.sql.parser.sql.segment.generic.table.TableSegment;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+
+@Getter
+@Setter
+public class TableReferenceSegment implements SQLSegment {
+    
+    private int startIndex;
+    
+    private int stopIndex;
+    
+    private TableSegment table;
+    
+    private JoinedTableSegment joinedTable;
+    
+    private Collection<SimpleTableSegment> tables;
+    
+    public Collection<SimpleTableSegment> getTables() {
+        Collection<SimpleTableSegment> tables = new LinkedList<>();
+        if (null != table) {
+            tables.add((SimpleTableSegment) table);
+        }
+        
+        if (null != joinedTable) {
+            tables.add((SimpleTableSegment) joinedTable.getTable());
+        }
+        return tables;
+    }
+}
