4a6aa0e9ef4bc8c122d54749c2458321e350e0e0
kimmking <kimmking@163.com>
2020-03-23 14:13:45 +0800

    Opt curator-client in unit test  (#4894)
    * optimize zk unittest
    
    * remove testingserver restart
    
    * ensure curatorClient is active
    
    * Polish code style

--- sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-zookeeper-curator/src/test/java/org/apache/shardingsphere/orchestration/center/instance/CuratorZookeeperCenterRepositoryTest.java
+++ sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-zookeeper-curator/src/test/java/org/apache/shardingsphere/orchestration/center/instance/CuratorZookeeperCenterRepositoryTest.java
@@ -18,15 +18,16 @@
 package org.apache.shardingsphere.orchestration.center.instance;
 
 import com.google.common.util.concurrent.SettableFuture;
+import lombok.SneakyThrows;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import org.apache.shardingsphere.orchestration.center.listener.DataChangedEvent;
 import org.apache.shardingsphere.orchestration.center.util.EmbedTestingServer;
 import org.apache.shardingsphere.orchestration.center.config.CenterConfiguration;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -40,19 +41,17 @@ import static org.junit.Assert.assertThat;
 public final class CuratorZookeeperCenterRepositoryTest {
     
     private static CuratorZookeeperCenterRepository centerRepository = new CuratorZookeeperCenterRepository();
-    
-    private static CuratorFramework client;
-    
-    private static final String SERVER_LISTS = "127.0.0.1:3181";
+
+    private static String serverLists;
     
     @BeforeClass
+    @SneakyThrows
     public static void init() {
         EmbedTestingServer.start();
+        serverLists = EmbedTestingServer.getTestingServerConnectionString();
         CenterConfiguration configuration = new CenterConfiguration(centerRepository.getType(), new Properties());
-        configuration.setServerLists(SERVER_LISTS);
+        configuration.setServerLists(serverLists);
         centerRepository.init(configuration);
-        client = CuratorFrameworkFactory.newClient(SERVER_LISTS, new ExponentialBackoffRetry(1000, 3));
-        client.start();
     }
     
     @Test
@@ -101,6 +100,9 @@ public final class CuratorZookeeperCenterRepositoryTest {
         centerRepository.persist("/test/children_deleted/5", "value5");
         SettableFuture<DataChangedEvent> future = SettableFuture.create();
         centerRepository.watch("/test/children_deleted/5", future::set);
+        Field field = CuratorZookeeperCenterRepository.class.getDeclaredField("client");
+        field.setAccessible(true);
+        CuratorFramework client = (CuratorFramework) field.get(centerRepository);
         client.delete().forPath("/test/children_deleted/5");
         DataChangedEvent dataChangedEvent = future.get(5, TimeUnit.SECONDS);
         assertNotNull(dataChangedEvent);
@@ -114,7 +116,7 @@ public final class CuratorZookeeperCenterRepositoryTest {
         centerRepository.persist("/test/children_added/4", "value4");
         AtomicReference<DataChangedEvent> actualDataChangedEvent = new AtomicReference<>();
         centerRepository.watch("/test/children_added", actualDataChangedEvent::set);
-        Thread.sleep(2000L);
+        Thread.sleep(50L);
         assertNull(actualDataChangedEvent.get());
     }
     
@@ -131,9 +133,8 @@ public final class CuratorZookeeperCenterRepositoryTest {
         properties.setProperty(ZookeeperPropertyKey.MAX_RETRIES.getKey(), "1");
         properties.setProperty(ZookeeperPropertyKey.TIME_TO_LIVE_SECONDS.getKey(), "100");
         properties.setProperty(ZookeeperPropertyKey.OPERATION_TIMEOUT_MILLISECONDS.getKey(), "1000");
-        EmbedTestingServer.start();
         CenterConfiguration configuration = new CenterConfiguration(customCenterRepository.getType(), new Properties());
-        configuration.setServerLists(SERVER_LISTS);
+        configuration.setServerLists(serverLists);
         customCenterRepository.setProperties(properties);
         customCenterRepository.init(configuration);
         assertThat(customCenterRepository.getProperties().getProperty(ZookeeperPropertyKey.RETRY_INTERVAL_MILLISECONDS.getKey()), is("1000"));
@@ -149,9 +150,8 @@ public final class CuratorZookeeperCenterRepositoryTest {
         final CuratorZookeeperCenterRepository customCenterRepository = new CuratorZookeeperCenterRepository();
         Properties properties = new Properties();
         properties.setProperty(ZookeeperPropertyKey.TIME_TO_LIVE_SECONDS.getKey(), "0");
-        EmbedTestingServer.start();
         CenterConfiguration configuration = new CenterConfiguration(customCenterRepository.getType(), new Properties());
-        configuration.setServerLists(SERVER_LISTS);
+        configuration.setServerLists(serverLists);
         customCenterRepository.setProperties(properties);
         customCenterRepository.init(configuration);
         assertThat(customCenterRepository.getProperties().getProperty(ZookeeperPropertyKey.TIME_TO_LIVE_SECONDS.getKey()), is("0"));
@@ -164,9 +164,8 @@ public final class CuratorZookeeperCenterRepositoryTest {
         final CuratorZookeeperCenterRepository customCenterRepository = new CuratorZookeeperCenterRepository();
         Properties properties = new Properties();
         properties.setProperty(ZookeeperPropertyKey.OPERATION_TIMEOUT_MILLISECONDS.getKey(), "0");
-        EmbedTestingServer.start();
         CenterConfiguration configuration = new CenterConfiguration(customCenterRepository.getType(), new Properties());
-        configuration.setServerLists(SERVER_LISTS);
+        configuration.setServerLists(serverLists);
         customCenterRepository.setProperties(properties);
         customCenterRepository.init(configuration);
         assertThat(customCenterRepository.getProperties().getProperty(ZookeeperPropertyKey.OPERATION_TIMEOUT_MILLISECONDS.getKey()), is("0"));
@@ -179,9 +178,8 @@ public final class CuratorZookeeperCenterRepositoryTest {
         final CuratorZookeeperCenterRepository customCenterRepository = new CuratorZookeeperCenterRepository();
         Properties properties = new Properties();
         properties.setProperty(ZookeeperPropertyKey.DIGEST.getKey(), "any");
-        EmbedTestingServer.start();
         CenterConfiguration configuration = new CenterConfiguration(customCenterRepository.getType(), new Properties());
-        configuration.setServerLists(SERVER_LISTS);
+        configuration.setServerLists(serverLists);
         customCenterRepository.setProperties(properties);
         customCenterRepository.init(configuration);
         assertThat(customCenterRepository.getProperties().getProperty(ZookeeperPropertyKey.DIGEST.getKey()), is("any"));
