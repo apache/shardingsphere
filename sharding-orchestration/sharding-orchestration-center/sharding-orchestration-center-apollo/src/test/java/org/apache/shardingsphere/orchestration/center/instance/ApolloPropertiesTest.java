cb93b9640df0b4655450faa6ee78127f33fb11af
kimmking <kimmking@163.com>
2020-03-23 14:06:16 +0800

    Polish ApolloProperty (#4891)

--- sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-apollo/src/test/java/org/apache/shardingsphere/orchestration/center/instance/ApolloPropertiesTest.java
+++ sharding-orchestration/sharding-orchestration-center/sharding-orchestration-center-apollo/src/test/java/org/apache/shardingsphere/orchestration/center/instance/ApolloPropertiesTest.java
@@ -27,20 +27,20 @@ public final class ApolloPropertiesTest {
     @Test
     public void assertGetValue() {
         Properties props = new Properties();
-        props.setProperty(ApolloPropertyKey.APP_ID.getKey(), "APOLLO_SHARDINGSPHERE1");
+        props.setProperty(ApolloPropertyKey.APP_ID.getKey(), "APOLLO_SHARDINGSPHERE_TEST");
         props.setProperty(ApolloPropertyKey.ENV.getKey(), "PROD");
         props.setProperty(ApolloPropertyKey.CLUSTER_NAME.getKey(), "sit");
         props.setProperty(ApolloPropertyKey.ADMINISTRATOR.getKey(), "guest");
-        props.setProperty(ApolloPropertyKey.TOKEN.getKey(), "a6e6bbec-1eed-4ddc-bb10-6791beb982d9");
+        props.setProperty(ApolloPropertyKey.TOKEN.getKey(), "this-is-a-test-token");
         props.setProperty(ApolloPropertyKey.PORTAL_URL.getKey(), "http://config-service-url");
         props.setProperty(ApolloPropertyKey.CONNECT_TIMEOUT.getKey(), "3000");
         props.setProperty(ApolloPropertyKey.READ_TIMEOUT.getKey(), "6000");
         ApolloProperties actual = new ApolloProperties(props);
-        assertThat(actual.getValue(ApolloPropertyKey.APP_ID), is("APOLLO_SHARDINGSPHERE1"));
+        assertThat(actual.getValue(ApolloPropertyKey.APP_ID), is("APOLLO_SHARDINGSPHERE_TEST"));
         assertThat(actual.getValue(ApolloPropertyKey.ENV), is("PROD"));
         assertThat(actual.getValue(ApolloPropertyKey.CLUSTER_NAME), is("sit"));
         assertThat(actual.getValue(ApolloPropertyKey.ADMINISTRATOR), is("guest"));
-        assertThat(actual.getValue(ApolloPropertyKey.TOKEN), is("a6e6bbec-1eed-4ddc-bb10-6791beb982d9"));
+        assertThat(actual.getValue(ApolloPropertyKey.TOKEN), is("this-is-a-test-token"));
         assertThat(actual.getValue(ApolloPropertyKey.PORTAL_URL), is("http://config-service-url"));
         assertThat(actual.getValue(ApolloPropertyKey.CONNECT_TIMEOUT), is(3000));
         assertThat(actual.getValue(ApolloPropertyKey.READ_TIMEOUT), is(6000));
@@ -49,13 +49,13 @@ public final class ApolloPropertiesTest {
     @Test
     public void assertGetDefaultValue() {
         ApolloProperties actual = new ApolloProperties(new Properties());
-        assertThat(actual.getValue(ApolloPropertyKey.APP_ID), is("APOLLO_SHARDING_SPHERE"));
-        assertThat(actual.getValue(ApolloPropertyKey.ENV), is("DEV"));
-        assertThat(actual.getValue(ApolloPropertyKey.CLUSTER_NAME), is("default"));
-        assertThat(actual.getValue(ApolloPropertyKey.ADMINISTRATOR), is(""));
-        assertThat(actual.getValue(ApolloPropertyKey.TOKEN), is(""));
-        assertThat(actual.getValue(ApolloPropertyKey.PORTAL_URL), is(""));
-        assertThat(actual.getValue(ApolloPropertyKey.CONNECT_TIMEOUT), is(1000));
-        assertThat(actual.getValue(ApolloPropertyKey.READ_TIMEOUT), is(5000));
+        assertThat(actual.getValue(ApolloPropertyKey.APP_ID), is(ApolloPropertyKey.APP_ID.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.ENV), is(ApolloPropertyKey.ENV.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.CLUSTER_NAME), is(ApolloPropertyKey.CLUSTER_NAME.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.ADMINISTRATOR), is(ApolloPropertyKey.ADMINISTRATOR.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.TOKEN), is(ApolloPropertyKey.TOKEN.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.PORTAL_URL), is(ApolloPropertyKey.PORTAL_URL.getDefaultValue()));
+        assertThat(actual.getValue(ApolloPropertyKey.CONNECT_TIMEOUT), is(Integer.parseInt(ApolloPropertyKey.CONNECT_TIMEOUT.getDefaultValue())));
+        assertThat(actual.getValue(ApolloPropertyKey.READ_TIMEOUT), is(Integer.parseInt(ApolloPropertyKey.READ_TIMEOUT.getDefaultValue())));
     }
 }
