ab450d53fb02744f74ad4727062c9563d61a5427
tristaZero <hannkenn@163.com>
2020-04-12 13:03:17 +0800

    fix bug for merging sharding value

--- sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/sharding/route/engine/condition/engine/WhereClauseShardingConditionEngine.java
+++ sharding-core/sharding-core-route/src/main/java/org/apache/shardingsphere/sharding/route/engine/condition/engine/WhereClauseShardingConditionEngine.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -160,11 +161,13 @@ public final class WhereClauseShardingConditionEngine {
     }
     
     private Collection<Comparable<?>> mergeListRouteValues(final Collection<Comparable<?>> value1, final Collection<Comparable<?>> value2) {
+        Collection<Comparable<?>> result = new LinkedHashSet<>();
         if (null == value2) {
             return value1;
         }
-        value1.retainAll(value2);
-        return value1;
+        result.addAll(value1);
+        result.addAll(value2);
+        return result;
     }
     
     private Range<Comparable<?>> mergeRangeRouteValues(final Range<Comparable<?>> value1, final Range<Comparable<?>> value2) {
