5798d27b7874d8b1601d113719f8108d7d53690a
yanyzy <1181862765@qq.com>
2020-04-13 16:38:04 +0800

    add shadow-raw-jdbc-example

---
+++ examples/sharding-jdbc-example/other-feature-example/shadow-example/shadow-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/shadow/table/raw/jdbc/config/ShadowDatabasesConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.example.shadow.table.raw.jdbc.config;
+
+import org.apache.shardingsphere.api.config.shadow.ShadowRuleConfiguration;
+import org.apache.shardingsphere.example.config.ExampleConfiguration;
+import org.apache.shardingsphere.example.core.api.DataSourceUtil;
+import org.apache.shardingsphere.shardingjdbc.api.ShadowDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShadowDatabasesConfiguration implements ExampleConfiguration {
+    
+    @Override
+    public DataSource getDataSource() {
+        ShadowRuleConfiguration shadowRuleConfiguration = new ShadowRuleConfiguration();
+        shadowRuleConfiguration.setColumn("shadow");
+        shadowRuleConfiguration.setShadowMappings(Collections.singletonMap("ds", "ds_0"));
+        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("ds", DataSourceUtil.createDataSource("demo_ds"));
+        dataSourceMap.put("ds_0", DataSourceUtil.createDataSource("shadow_demo_ds"));
+        try {
+            return ShadowDataSourceFactory.createDataSource(dataSourceMap, shadowRuleConfiguration, null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
