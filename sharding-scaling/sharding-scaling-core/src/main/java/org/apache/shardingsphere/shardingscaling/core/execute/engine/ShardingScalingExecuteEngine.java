0a8b92d516ae468253ce02a152f15a355b60afd7
KomachiSion <263976490@qq.com>
2020-04-20 14:59:05 +0800

    Add ShardingScalingExecuteEngine

--- sharding-scaling/sharding-scaling-core/src/main/java/org/apache/shardingsphere/shardingscaling/core/execute/engine/ShardingScalingExecuteEngine.java
+++ sharding-scaling/sharding-scaling-core/src/main/java/org/apache/shardingsphere/shardingscaling/core/execute/engine/ShardingScalingExecuteEngine.java
@@ -17,10 +17,58 @@
 
 package org.apache.shardingsphere.shardingscaling.core.execute.engine;
 
+import org.apache.shardingsphere.shardingscaling.core.execute.executor.ShardingScalingExecutor;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Sharding scaling executor engine.
  */
-public interface ShardingScalingExecuteEngine {
-
+public class ShardingScalingExecuteEngine {
     
+    private final ListeningExecutorService executorService;
+    
+    public ShardingScalingExecuteEngine(final int maxWorkerNumber) {
+        this.executorService = MoreExecutors.listeningDecorator(
+            new ThreadPoolExecutor(maxWorkerNumber, maxWorkerNumber, 0, TimeUnit.SECONDS, new SynchronousQueue<>(), new ThreadPoolExecutor.AbortPolicy()));
+    }
+    
+    /**
+     * Submit a {@code ShardingScalingExecutor} without callback to execute.
+     *
+     * @param shardingScalingExecutor sharding scaling executor
+     */
+    public void submit(final ShardingScalingExecutor shardingScalingExecutor) {
+        executorService.submit(shardingScalingExecutor);
+    }
+    
+    /**
+     * Submit a {@code ShardingScalingExecutor} with callback {@code ExecuteCallback} to execute.
+     *
+     * @param shardingScalingExecutor sharding scaling executor
+     * @param executeCallback execute callback
+     */
+    public void submit(final ShardingScalingExecutor shardingScalingExecutor, final ExecuteCallback executeCallback) {
+        ListenableFuture future = executorService.submit(shardingScalingExecutor);
+        Futures.addCallback(future, new FutureCallback<Object>() {
+            
+            @Override
+            public void onSuccess(final Object result) {
+                executeCallback.onSuccess();
+            }
+    
+            @Override
+            public void onFailure(final Throwable t) {
+                executeCallback.onFailure(t);
+            }
+        });
+    }
 }
