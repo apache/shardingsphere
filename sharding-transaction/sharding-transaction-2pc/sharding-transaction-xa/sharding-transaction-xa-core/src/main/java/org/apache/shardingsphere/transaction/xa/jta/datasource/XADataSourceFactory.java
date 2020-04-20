a368eea812f58a661aa6a721ba0953ea56b7ad59
cherrylzhao <zhaojunv3@126.com>
2020-04-20 17:07:52 +0800

    refactor DataSourceSwapper

--- sharding-transaction/sharding-transaction-2pc/sharding-transaction-xa/sharding-transaction-xa-core/src/main/java/org/apache/shardingsphere/transaction/xa/jta/datasource/XADataSourceFactory.java
+++ sharding-transaction/sharding-transaction-2pc/sharding-transaction-xa/sharding-transaction-xa-core/src/main/java/org/apache/shardingsphere/transaction/xa/jta/datasource/XADataSourceFactory.java
@@ -17,39 +17,21 @@
 
 package org.apache.shardingsphere.transaction.xa.jta.datasource;
 
-import com.atomikos.beans.PropertyUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.shardingsphere.underlying.common.exception.ShardingSphereException;
-import org.apache.shardingsphere.underlying.common.database.type.DatabaseType;
-import org.apache.shardingsphere.transaction.xa.jta.datasource.properties.XADataSourceDefinition;
 import org.apache.shardingsphere.transaction.xa.jta.datasource.properties.XADataSourceDefinitionFactory;
 import org.apache.shardingsphere.transaction.xa.jta.datasource.swapper.DataSourceSwapper;
+import org.apache.shardingsphere.underlying.common.database.type.DatabaseType;
 
 import javax.sql.DataSource;
 import javax.sql.XADataSource;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Properties;
 
 /**
  * XA data source factory.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class XADataSourceFactory {
-    
-    private static final DataSourceSwapper SWAPPER = new DataSourceSwapper();
-    
-    /**
-     * Create XA DataSource instance.
-     *
-     * @param databaseType database type
-     * @return XA DataSource instance
-     */
-    public static XADataSource build(final DatabaseType databaseType) {
-        return createXADataSource(XADataSourceDefinitionFactory.getXADataSourceDefinition(databaseType));
-    }
     
     /**
      * Create XA data source through general data source.
@@ -60,48 +42,6 @@ public final class XADataSourceFactory {
      */
     @SneakyThrows
     public static XADataSource build(final DatabaseType databaseType, final DataSource dataSource) {
-        XADataSourceDefinition xaDataSourceDefinition = XADataSourceDefinitionFactory.getXADataSourceDefinition(databaseType);
-        XADataSource result = createXADataSource(xaDataSourceDefinition);
-        Properties xaProperties = xaDataSourceDefinition.getXAProperties(SWAPPER.swap(dataSource));
-        PropertyUtils.setProperties(result, xaProperties);
-        return result;
-    }
-    
-    private static XADataSource createXADataSource(final XADataSourceDefinition xaDataSourceDefinition) {
-        XADataSource result = null;
-        List<ShardingSphereException> exceptions = new LinkedList<>();
-        for (String each : xaDataSourceDefinition.getXADriverClassName()) {
-            try {
-                result = loadXADataSource(each);
-            } catch (final ShardingSphereException ex) {
-                exceptions.add(ex);
-            }
-        }
-        if (null == result && !exceptions.isEmpty()) {
-            if (exceptions.size() > 1) {
-                throw new ShardingSphereException("Failed to create [%s] XA DataSource", xaDataSourceDefinition);
-            } else {
-                throw exceptions.iterator().next();
-            }
-        }
-        return result;
-    }
-    
-    private static XADataSource loadXADataSource(final String xaDataSourceClassName) {
-        Class xaDataSourceClass;
-        try {
-            xaDataSourceClass = Thread.currentThread().getContextClassLoader().loadClass(xaDataSourceClassName);
-        } catch (final ClassNotFoundException ignored) {
-            try {
-                xaDataSourceClass = Class.forName(xaDataSourceClassName);
-            } catch (final ClassNotFoundException ex) {
-                throw new ShardingSphereException("Failed to load [%s]", xaDataSourceClassName);
-            }
-        }
-        try {
-            return (XADataSource) xaDataSourceClass.newInstance();
-        } catch (final InstantiationException | IllegalAccessException ex) {
-            throw new ShardingSphereException("Failed to instance [%s]", xaDataSourceClassName);
-        }
+        return new DataSourceSwapper(XADataSourceDefinitionFactory.getXADataSourceDefinition(databaseType)).swap(dataSource);
     }
 }
