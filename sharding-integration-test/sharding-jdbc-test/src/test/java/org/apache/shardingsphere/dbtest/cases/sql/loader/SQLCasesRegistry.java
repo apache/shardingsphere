df0ee21fa3c784fdf1b8fd16818a0d3f36a68a8c
cherrylzhao <zhaojunv3@126.com>
2020-04-27 10:30:58 +0800

    fix integrate sql cases loader

--- sharding-integration-test/sharding-jdbc-test/src/test/java/org/apache/shardingsphere/dbtest/cases/sql/loader/SQLCasesRegistry.java
+++ sharding-integration-test/sharding-jdbc-test/src/test/java/org/apache/shardingsphere/dbtest/cases/sql/loader/SQLCasesRegistry.java
@@ -30,7 +30,7 @@ public final class SQLCasesRegistry {
     private SQLCasesLoader sqlCasesLoader;
     
     private SQLCasesRegistry() {
-        sqlCasesLoader = new SQLCasesLoader("sql/supported");
+        sqlCasesLoader = new SQLCasesLoader("integrate/sql/supported");
     }
     
     /**
