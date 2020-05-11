d8979f0eeebb6acf766f137f1399f4dd97d136b0
terrymanu <terrymanu@163.com>
2020-05-11 11:52:40 +0800

    refactor LogicSchemaFactory

--- sharding-proxy/sharding-proxy-backend/src/main/java/org/apache/shardingsphere/shardingproxy/backend/schema/LogicSchemaFactory.java
+++ sharding-proxy/sharding-proxy-backend/src/main/java/org/apache/shardingsphere/shardingproxy/backend/schema/LogicSchemaFactory.java
@@ -19,10 +19,7 @@ package org.apache.shardingsphere.shardingproxy.backend.schema;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.shardingsphere.api.config.masterslave.MasterSlaveRuleConfiguration;
 import org.apache.shardingsphere.api.config.shadow.ShadowRuleConfiguration;
-import org.apache.shardingsphere.api.config.sharding.ShardingRuleConfiguration;
-import org.apache.shardingsphere.encrypt.api.EncryptRuleConfiguration;
 import org.apache.shardingsphere.shardingproxy.backend.schema.impl.ShadowSchema;
 import org.apache.shardingsphere.shardingproxy.backend.schema.impl.ShardingSphereSchema;
 import org.apache.shardingsphere.shardingproxy.backend.schema.impl.TransparentSchema;
@@ -31,7 +28,6 @@ import org.apache.shardingsphere.underlying.common.config.RuleConfiguration;
 
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -51,25 +47,14 @@ public final class LogicSchemaFactory {
      */
     public static LogicSchema newInstance(final String schemaName, final Map<String, Map<String, YamlDataSourceParameter>> schemaDataSources,
                                           final Collection<RuleConfiguration> ruleConfigurations) throws SQLException {
-        if (ruleConfigurations.size() > 1) {
-            return new ShardingSphereSchema(schemaName, schemaDataSources.get(schemaName), ruleConfigurations);
-        }
         if (ruleConfigurations.isEmpty()) {
             return new TransparentSchema(schemaName, schemaDataSources.get(schemaName));
         }
+        // TODO process shadow rule
         RuleConfiguration ruleConfiguration = ruleConfigurations.iterator().next();
-        if (ruleConfiguration instanceof ShardingRuleConfiguration) {
-            return new ShardingSphereSchema(schemaName, schemaDataSources.get(schemaName), Collections.singleton(ruleConfiguration));
-        }
-        if (ruleConfiguration instanceof MasterSlaveRuleConfiguration) {
-            return new ShardingSphereSchema(schemaName, schemaDataSources.get(schemaName), Collections.singleton(ruleConfiguration));
-        }
-        if (ruleConfiguration instanceof EncryptRuleConfiguration) {
-            return new ShardingSphereSchema(schemaName, schemaDataSources.get(schemaName), Collections.singleton(ruleConfiguration));
-        }
         if (ruleConfiguration instanceof ShadowRuleConfiguration) {
             return new ShadowSchema(schemaName, schemaDataSources.get(schemaName), (ShadowRuleConfiguration) ruleConfiguration);
         }
-        return new TransparentSchema(schemaName, schemaDataSources.get(schemaName));
+        return new ShardingSphereSchema(schemaName, schemaDataSources.get(schemaName), ruleConfigurations);
     }
 }
