65fcfe0f72b7e6292bd13073349235929a900394
zhaojun <zhaojunv3@126.com>
2020-05-22 10:32:36 +0800

    add docker-compose-maven-plugin for integration test (#5742)
    * add docker-compose maven plugin
    
    * for checkstyle

--- shardingsphere-integration-test/shardingsphere-proxy-docker-build/src/main/resources/bin/stop.sh
+++ shardingsphere-integration-test/shardingsphere-proxy-docker-build/src/main/resources/bin/stop.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -15,5 +16,36 @@
 # limitations under the License.
 #
 
-org.apache.shardingsphere.dbtest.fixture.ConstantKeyGenerateAlgorithm
+SERVER_NAME=ShardingSphere-Proxy
 
+cd `dirname $0`
+cd ..
+DEPLOY_DIR=`pwd`
+
+PIDS=`ps -ef | grep java | grep "$DEPLOY_DIR" | grep -v grep |awk '{print $2}'`
+if [ -z "$PIDS" ]; then
+    echo "ERROR: The $SERVER_NAME does not started!"
+    exit 1
+fi
+
+echo -e "Stopping the $SERVER_NAME ...\c"
+for PID in ${PIDS} ; do
+    kill ${PID} > /dev/null 2>&1
+done
+
+COUNT=0
+while [ ${COUNT} -lt 1 ]; do
+    echo -e ".\c"
+    sleep 1
+    COUNT=1
+    for PID in ${PIDS} ; do
+        PID_EXIST=`ps -f -p ${PID} | grep java`
+        if [ -n "$PID_EXIST" ]; then
+            COUNT=0
+            break
+        fi
+    done
+done
+
+echo "OK!"
+echo "PID: $PIDS"
