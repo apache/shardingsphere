8eeba64186dfa96471890dbbc892ec0aabb9a6e3
JingShang Lu <jingshanglu1@gmail.com>
2020-05-28 19:01:08 +0800

    simple code

--- shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/engine/SQLParserParameterizedTest.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/test/java/org/apache/shardingsphere/sql/parser/integrate/engine/SQLParserParameterizedTest.java
@@ -127,10 +127,6 @@ public final class SQLParserParameterizedTest {
     
     @Test
     public void assertSupportedSQL() {
-        if ("update_with_column_equal_column".equals(sqlCaseId)) {
-            System.out.println("fasfasfasd");
-    
-        }
         SQLParserTestCase expected = SQL_PARSER_TEST_CASES_REGISTRY.get(sqlCaseId);
         String databaseType = "H2".equals(this.databaseType) ? "MySQL" : this.databaseType;
         String sql = SQL_CASES_LOADER.getSQL(sqlCaseId, sqlCaseType, SQL_PARSER_TEST_CASES_REGISTRY.get(sqlCaseId).getParameters());
