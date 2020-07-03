4f9f24299c61fabe7d29f56fe29c0dbf02d1900e
xbkaishui <xbkaishui@126.com>
2020-07-03 15:21:15 +0800

    change code style

--- shardingsphere-sql-parser/shardingsphere-sql-parser-binder/src/main/java/org/apache/shardingsphere/sql/parser/binder/metadata/schema/SchemaMetaDataLoader.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-binder/src/main/java/org/apache/shardingsphere/sql/parser/binder/metadata/schema/SchemaMetaDataLoader.java
@@ -17,23 +17,18 @@
 
 package org.apache.shardingsphere.sql.parser.binder.metadata.schema;
 
-import java.util.HashMap;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.sql.parser.binder.metadata.MetaDataConnection;
 import org.apache.shardingsphere.sql.parser.binder.metadata.table.TableMetaData;
+import org.apache.shardingsphere.sql.parser.binder.metadata.util.JdbcUtil;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import org.apache.shardingsphere.sql.parser.binder.metadata.util.JdbcUtil;
+import java.util.*;
 
 /**
  * Schema meta data loader.
@@ -41,15 +36,15 @@ import org.apache.shardingsphere.sql.parser.binder.metadata.util.JdbcUtil;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j(topic = "ShardingSphere-metadata")
 public final class SchemaMetaDataLoader {
-
+    
     private static final String TABLE_TYPE = "TABLE";
-
+    
     private static final String TABLE_NAME = "TABLE_NAME";
-
+    
     /**
      * Load schema meta data.
      *
-     * @param dataSource data source
+     * @param dataSource   data source
      * @param databaseType database type
      * @return schema meta data
      * @throws SQLException SQL exception
@@ -57,12 +52,12 @@ public final class SchemaMetaDataLoader {
     public static SchemaMetaData load(final DataSource dataSource, final String databaseType) throws SQLException {
         return load(dataSource, databaseType, Collections.emptyList());
     }
-
+    
     /**
      * Load schema meta data.
      *
-     * @param dataSource data source
-     * @param databaseType database type
+     * @param dataSource         data source
+     * @param databaseType       database type
      * @param excludedTableNames excluded table names
      * @return schema meta data
      * @throws SQLException SQL exception
@@ -83,7 +78,7 @@ public final class SchemaMetaDataLoader {
         });
         return new SchemaMetaData(tableMetaDataMap);
     }
-
+    
     private static List<String> loadAllTableNames(final Connection connection, final String databaseType) throws SQLException {
         List<String> result = new LinkedList<>();
         try (ResultSet resultSet = connection.getMetaData().getTables(connection.getCatalog(), JdbcUtil.getSchema(connection, databaseType), null, new String[]{TABLE_TYPE})) {
@@ -96,9 +91,9 @@ public final class SchemaMetaDataLoader {
         }
         return result;
     }
-
+    
     private static boolean isSystemTable(final String table) {
         return table.contains("$") || table.contains("/");
     }
-
+    
 }
