fa59df0d06238d0488fd00224577ca19f6365ec9
xiaoyu <549477611@qq.com>
2020-07-07 16:08:14 +0800

    jdbc and proxy metadata center refresh. (#6278)
    * sharding-proxy sharding-jdbc-spring-boot use common data source configuration。
    
    * jdbc and proxy metadata center refresh.
    
    * jdbc and proxy metadata center refresh.
    
    * modify doc.

--- shardingsphere-control-panel/shardingsphere-orchestration/shardingsphere-orchestration-core/shardingsphere-orchestration-core-metadatacenter/src/main/java/org/apache/shardingsphere/orchestration/core/metadatacenter/MetaDataCenter.java
+++ shardingsphere-control-panel/shardingsphere-orchestration/shardingsphere-orchestration-core/shardingsphere-orchestration-core-metadatacenter/src/main/java/org/apache/shardingsphere/orchestration/core/metadatacenter/MetaDataCenter.java
@@ -18,6 +18,7 @@
 package org.apache.shardingsphere.orchestration.core.metadatacenter;
 
 import com.google.common.base.Strings;
+import org.apache.shardingsphere.infra.metadata.callback.MetaDataCallback;
 import org.apache.shardingsphere.orchestration.center.CenterRepository;
 import org.apache.shardingsphere.orchestration.core.metadatacenter.yaml.RuleSchemaMetaDataYamlSwapper;
 import org.apache.shardingsphere.orchestration.core.metadatacenter.yaml.YamlRuleSchemaMetaData;
@@ -38,6 +39,7 @@ public final class MetaDataCenter {
     public MetaDataCenter(final String name, final CenterRepository centerRepository) {
         this.node = new MetaDataCenterNode(name);
         this.repository = centerRepository;
+        MetaDataCallback.INSTANCE.register(this::persistMetaDataCenterNode);
     }
     
     /**
