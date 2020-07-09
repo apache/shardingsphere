74ed2caf60a2909fb5c40e7841847478148da62d
tristaZero <hannkenn@163.com>
2020-07-09 15:34:54 +0800

    create custom sql statments

---
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-binder/src/main/java/org/apache/shardingsphere/sql/parser/binder/statement/ddl/CreateDataSourceStatementContext.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.parser.binder.statement.ddl;
+
+import org.apache.shardingsphere.sql.parser.binder.statement.CommonSQLStatementContext;
+import org.apache.shardingsphere.sql.parser.sql.statement.ddl.CreateDataSourceStatement;
+
+/**
+ * Create dataSource statement context.
+ */
+public class CreateDataSourceStatementContext extends CommonSQLStatementContext<CreateDataSourceStatement> {
+    
+    public CreateDataSourceStatementContext(final CreateDataSourceStatement sqlStatement) {
+        super(sqlStatement);
+    }
+    
+    /**
+     * Get url.
+     *
+     * @return datasource url
+     */
+    public String getUrl() {
+        return "";
+    }
+    
+    /**
+     * Get username.
+     *
+     * @return username
+     */
+    public String getUsername() {
+        return "";
+    }
+    
+    /**
+     * Get password.
+     *
+     * @return password
+     */
+    public String getPassword() {
+        return "";
+    }
+}
