9166f8ae2802a762547836a452a0f1f2b8fd86a4
feiweihy <36758723+feiweihy@users.noreply.github.com>
2020-07-20 10:49:15 +0800

    Update SchemaMetaDataLoaderTest.java

--- shardingsphere-sql-parser/shardingsphere-sql-parser-binder/src/test/java/org/apache/shardingsphere/sql/parser/binder/metadata/table/SchemaMetaDataLoaderTest.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-binder/src/test/java/org/apache/shardingsphere/sql/parser/binder/metadata/table/SchemaMetaDataLoaderTest.java
@@ -1,5 +1,6 @@
 package org.apache.shardingsphere.sql.parser.binder.metadata.table;
 
+import org.apache.shardingsphere.sql.parser.binder.metadata.schema.SchemaMetaData;
 import org.apache.shardingsphere.sql.parser.binder.metadata.schema.SchemaMetaDataLoader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,9 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -40,7 +44,10 @@ public final class SchemaMetaDataLoaderTest {
         when(databaseMetaData.getURL()).thenReturn("jdbc:oracle:thin:@127.0.0.1:1521/orcl");
         when(databaseMetaData.getUserName()).thenReturn(null);
         when(databaseMetaData.getTables(TEST_CATALOG, null, null, new String[]{TABLE_TYPE})).thenReturn(tableExistResultSet);
-        SchemaMetaDataLoader.load(dataSource, 5, "Oracle");
+        SchemaMetaData actual = SchemaMetaDataLoader.load(dataSource, 5, "Oracle");
+        TableMetaData tableMetaData = mock(TableMetaData.class);
+        actual.put("tbl", tableMetaData);
+        assertThat(actual.get("tbl"), is(tableMetaData));
     }
 
 }
