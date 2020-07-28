a87c8e764c57fefb170efa01344a9b4ac48b8115
terrymanu <terrymanu@163.com>
2020-07-28 15:02:16 +0800

    Add more test cases for OrchestrationConfigurationYamlSwapperTest

--- shardingsphere-control-panel/shardingsphere-orchestration/shardingsphere-orchestration-repository/shardingsphere-orchestration-repository-common/src/test/java/org/apache/shardingsphere/orchestration/repository/common/yaml/swapper/OrchestrationConfigurationYamlSwapperTest.java
+++ shardingsphere-control-panel/shardingsphere-orchestration/shardingsphere-orchestration-repository/shardingsphere-orchestration-repository-common/src/test/java/org/apache/shardingsphere/orchestration/repository/common/yaml/swapper/OrchestrationConfigurationYamlSwapperTest.java
@@ -17,51 +17,108 @@
 
 package org.apache.shardingsphere.orchestration.repository.common.yaml.swapper;
 
-import org.apache.shardingsphere.orchestration.repository.api.config.OrchestrationConfiguration;
 import org.apache.shardingsphere.orchestration.repository.api.config.OrchestrationCenterConfiguration;
-import org.apache.shardingsphere.orchestration.repository.common.yaml.config.YamlOrchestrationConfiguration;
+import org.apache.shardingsphere.orchestration.repository.api.config.OrchestrationConfiguration;
 import org.apache.shardingsphere.orchestration.repository.common.yaml.config.YamlOrchestrationCenterConfiguration;
+import org.apache.shardingsphere.orchestration.repository.common.yaml.config.YamlOrchestrationConfiguration;
 import org.junit.Test;
 
 import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public final class OrchestrationConfigurationYamlSwapperTest {
     
-    private static final String LOGIC_SCHEMA = "logic_schema";
+    @Test
+    public void assertSwapToYamlOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration() {
+        OrchestrationConfiguration expected = createOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration();
+        YamlOrchestrationConfiguration actual = new OrchestrationConfigurationYamlSwapper().swapToYamlConfiguration(expected);
+        assertThat(actual.getNamespace(), is("logic_schema"));
+        assertFalse(actual.isOverwrite());
+        assertThat(actual.getRegistryCenter().getType(), is(expected.getRegistryCenterConfiguration().getType()));
+        assertThat(actual.getRegistryCenter().getServerLists(), is(expected.getRegistryCenterConfiguration().getServerLists()));
+        assertThat(actual.getRegistryCenter().getProps(), is(expected.getRegistryCenterConfiguration().getProps()));
+        assertFalse(expected.getAdditionalConfigCenterConfiguration().isPresent());
+    }
+    
+    private OrchestrationConfiguration createOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration() {
+        return new OrchestrationConfiguration("logic_schema", new OrchestrationCenterConfiguration("TEST", "127.0.0.1:2181", new Properties()), false);
+    }
     
     @Test
-    public void assertSwapToYamlOrchestrationConfiguration() {
-        OrchestrationConfiguration data = getOrchestrationConfiguration();
-        YamlOrchestrationConfiguration result = new OrchestrationConfigurationYamlSwapper().swapToYamlConfiguration(data);
-        assertThat(result.getRegistryCenter().getType(), is(data.getRegistryCenterConfiguration().getType()));
-        assertThat(result.getRegistryCenter().getServerLists(), is(data.getRegistryCenterConfiguration().getServerLists()));
-        assertThat(result.getRegistryCenter().getProps(), is(data.getRegistryCenterConfiguration().getProps()));
+    public void assertSwapToYamlOrchestrationConfigurationWithAdditionalConfigCenterConfiguration() {
+        OrchestrationConfiguration expected = createOrchestrationConfigurationWithAdditionalConfigCenterConfiguration();
+        YamlOrchestrationConfiguration actual = new OrchestrationConfigurationYamlSwapper().swapToYamlConfiguration(expected);
+        assertThat(actual.getNamespace(), is("logic_schema"));
+        assertFalse(actual.isOverwrite());
+        assertThat(actual.getRegistryCenter().getType(), is(expected.getRegistryCenterConfiguration().getType()));
+        assertThat(actual.getRegistryCenter().getServerLists(), is(expected.getRegistryCenterConfiguration().getServerLists()));
+        assertThat(actual.getRegistryCenter().getProps(), is(expected.getRegistryCenterConfiguration().getProps()));
+        assertTrue(expected.getAdditionalConfigCenterConfiguration().isPresent());
+        assertThat(actual.getAdditionalConfigCenter().getType(), is(expected.getAdditionalConfigCenterConfiguration().get().getType()));
+        assertThat(actual.getAdditionalConfigCenter().getServerLists(), is(expected.getAdditionalConfigCenterConfiguration().get().getServerLists()));
+        assertThat(actual.getAdditionalConfigCenter().getProps(), is(expected.getAdditionalConfigCenterConfiguration().get().getProps()));
     }
     
-    private OrchestrationConfiguration getOrchestrationConfiguration() {
-        return new OrchestrationConfiguration(LOGIC_SCHEMA, new OrchestrationCenterConfiguration("ZooKeeper", "127.0.0.1:2181,127.0.0.1:2182", new Properties()), false);
+    private OrchestrationConfiguration createOrchestrationConfigurationWithAdditionalConfigCenterConfiguration() {
+        return new OrchestrationConfiguration("logic_schema", 
+                new OrchestrationCenterConfiguration("TEST", "127.0.0.1:2181", new Properties()), new OrchestrationCenterConfiguration("ADDITIONAL", "127.0.0.1:2181", new Properties()), false);
     }
     
     @Test
-    public void assertSwapToOrchestrationConfiguration() {
-        YamlOrchestrationConfiguration data = getYamlOrchestrationConfiguration();
-        OrchestrationConfiguration result = new OrchestrationConfigurationYamlSwapper().swapToObject(data);
-        assertThat(result.getRegistryCenterConfiguration().getType(), is(data.getRegistryCenter().getType()));
-        assertThat(result.getRegistryCenterConfiguration().getServerLists(), is(data.getRegistryCenter().getServerLists()));
-        assertThat(result.getRegistryCenterConfiguration().getProps(), is(data.getRegistryCenter().getProps()));
+    public void assertSwapToOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration() {
+        YamlOrchestrationConfiguration expected = createYamlOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration();
+        OrchestrationConfiguration actual = new OrchestrationConfigurationYamlSwapper().swapToObject(expected);
+        assertThat(actual.getRegistryCenterConfiguration().getType(), is(expected.getRegistryCenter().getType()));
+        assertThat(actual.getRegistryCenterConfiguration().getServerLists(), is(expected.getRegistryCenter().getServerLists()));
+        assertThat(actual.getRegistryCenterConfiguration().getProps(), is(expected.getRegistryCenter().getProps()));
+        assertFalse(actual.getAdditionalConfigCenterConfiguration().isPresent());
     }
     
-    private YamlOrchestrationConfiguration getYamlOrchestrationConfiguration() {
-        YamlOrchestrationCenterConfiguration registryCenterConfig = new YamlOrchestrationCenterConfiguration();
-        registryCenterConfig.setType("ZooKeeper");
-        registryCenterConfig.setProps(new Properties());
-        registryCenterConfig.setServerLists("127.0.0.1:2181,127.0.0.1:2182");
+    private YamlOrchestrationConfiguration createYamlOrchestrationConfigurationWithoutAdditionalConfigCenterConfiguration() {
         YamlOrchestrationConfiguration result = new YamlOrchestrationConfiguration();
-        result.setNamespace(LOGIC_SCHEMA);
-        result.setRegistryCenter(registryCenterConfig);
+        result.setNamespace("logic_schema");
+        result.setRegistryCenter(createYamlRegistryCenterConfiguration());
+        return result;
+    }
+    
+    @Test
+    public void assertSwapToOrchestrationConfigurationWithAdditionalConfigCenterConfiguration() {
+        YamlOrchestrationConfiguration expected = createYamlOrchestrationConfigurationWithAdditionalConfigCenterConfiguration();
+        OrchestrationConfiguration actual = new OrchestrationConfigurationYamlSwapper().swapToObject(expected);
+        assertThat(actual.getRegistryCenterConfiguration().getType(), is(expected.getRegistryCenter().getType()));
+        assertThat(actual.getRegistryCenterConfiguration().getServerLists(), is(expected.getRegistryCenter().getServerLists()));
+        assertThat(actual.getRegistryCenterConfiguration().getProps(), is(expected.getRegistryCenter().getProps()));
+        assertTrue(actual.getAdditionalConfigCenterConfiguration().isPresent());
+        assertThat(actual.getAdditionalConfigCenterConfiguration().get().getType(), is(expected.getAdditionalConfigCenter().getType()));
+        assertThat(actual.getAdditionalConfigCenterConfiguration().get().getServerLists(), is(expected.getAdditionalConfigCenter().getServerLists()));
+        assertThat(actual.getAdditionalConfigCenterConfiguration().get().getProps(), is(expected.getAdditionalConfigCenter().getProps()));
+    }
+    
+    private YamlOrchestrationConfiguration createYamlOrchestrationConfigurationWithAdditionalConfigCenterConfiguration() {
+        YamlOrchestrationConfiguration result = new YamlOrchestrationConfiguration();
+        result.setNamespace("logic_schema");
+        result.setRegistryCenter(createYamlRegistryCenterConfiguration());
+        result.setAdditionalConfigCenter(createYamlAdditionalConfigCenterConfiguration());
+        return result;
+    }
+    
+    private YamlOrchestrationCenterConfiguration createYamlRegistryCenterConfiguration() {
+        YamlOrchestrationCenterConfiguration result = new YamlOrchestrationCenterConfiguration();
+        result.setType("TEST");
+        result.setProps(new Properties());
+        result.setServerLists("127.0.0.1:2181");
+        return result;
+    }
+    
+    private YamlOrchestrationCenterConfiguration createYamlAdditionalConfigCenterConfiguration() {
+        YamlOrchestrationCenterConfiguration result = new YamlOrchestrationCenterConfiguration();
+        result.setType("ADDITIONAL");
+        result.setProps(new Properties());
+        result.setServerLists("127.0.0.1:2182");
         return result;
     }
 }
