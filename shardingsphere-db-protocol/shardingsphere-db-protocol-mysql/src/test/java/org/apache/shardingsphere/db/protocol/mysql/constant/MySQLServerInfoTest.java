8d1eb54d36aa62b0b23b9ce641fc0c00fed36fb8
sunnianjun <sunnianjun@huobi.com>
2020-08-04 15:30:52 +0800

    Refactor: removed the fileds

--- shardingsphere-db-protocol/shardingsphere-db-protocol-mysql/src/test/java/org/apache/shardingsphere/db/protocol/mysql/constant/MySQLServerInfoTest.java
+++ shardingsphere-db-protocol/shardingsphere-db-protocol-mysql/src/test/java/org/apache/shardingsphere/db/protocol/mysql/constant/MySQLServerInfoTest.java
@@ -7,21 +7,15 @@ import static org.junit.Assert.assertThat;
 
 public class MySQLServerInfoTest {
 
-    private String SPECIFIC_VERSION = "5.1.47";
-
-    private String EXPECTED_VERSION = "5.1.47-ShardingSphere-Proxy 5.0.0-RC1";
-
-    private String EXPECTED_NULL_VERSION = "null-ShardingSphere-Proxy 5.0.0-RC1";
-
     @Test
     public void assertSetServerVersion() {
-        MySQLServerInfo.setServerVersion(SPECIFIC_VERSION);
-        assertThat(MySQLServerInfo.getServerVersion(), is(EXPECTED_VERSION));
+        MySQLServerInfo.setServerVersion("5.1.47");
+        assertThat(MySQLServerInfo.getServerVersion(), is("5.1.47-ShardingSphere-Proxy 5.0.0-RC1"));
     }
 
     @Test
     public void assertSetServerVersionForNull() {
         MySQLServerInfo.setServerVersion(null);
-        assertThat(MySQLServerInfo.getServerVersion(), is(EXPECTED_NULL_VERSION));
+        assertThat(MySQLServerInfo.getServerVersion(), is("null-ShardingSphere-Proxy 5.0.0-RC1"));
     }
 }
