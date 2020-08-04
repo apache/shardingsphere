d20c4d130e7f0fb10928f3d6ed1fafd58c70bcac
sunnianjun <sunnianjun@huobi.com>
2020-08-04 16:44:24 +0800

    Refactor: add final modifier to class sinace there is no need for inheritance

--- shardingsphere-db-protocol/shardingsphere-db-protocol-mysql/src/test/java/org/apache/shardingsphere/db/protocol/mysql/constant/MySQLServerInfoTest.java
+++ shardingsphere-db-protocol/shardingsphere-db-protocol-mysql/src/test/java/org/apache/shardingsphere/db/protocol/mysql/constant/MySQLServerInfoTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class MySQLServerInfoTest {
+public final class MySQLServerInfoTest {
 
     @Test
     public void assertSetServerVersion() {
