2d042990c6ddab870d833389e2b0f91281145eb0
terrymanu <terrymanu@163.com>
2020-08-08 16:06:17 +0800

    Add more test case for AuthenticationYamlSwapper

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/auth/yaml/swapper/AuthenticationYamlSwapperTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/auth/yaml/swapper/AuthenticationYamlSwapperTest.java
@@ -24,23 +24,26 @@ import org.apache.shardingsphere.infra.auth.yaml.config.YamlProxyUserConfigurati
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public final class AuthenticationYamlSwapperTest {
-        
+    
     @Test
     public void assertSwapToYaml() {
-        ProxyUser user1 = new ProxyUser("pwd1", Collections.singleton("db1"));
-        ProxyUser user2 = new ProxyUser("pwd2", Collections.singleton("db2"));
         Authentication authentication = new Authentication();
-        authentication.getUsers().put("user1", user1);
-        authentication.getUsers().put("user2", user2);
+        authentication.getUsers().put("user1", new ProxyUser("pwd1", Collections.singleton("db1")));
+        authentication.getUsers().put("user2", new ProxyUser("pwd2", Collections.singleton("db2")));
         YamlAuthenticationConfiguration actual = new AuthenticationYamlSwapper().swapToYamlConfiguration(authentication);
         assertThat(actual.getUsers().size(), is(2));
+        assertThat(actual.getUsers().get("user1").getPassword(), is("pwd1"));
+        assertThat(actual.getUsers().get("user1").getAuthorizedSchemas(), is("db1"));
         assertThat(actual.getUsers().get("user2").getPassword(), is("pwd2"));
+        assertThat(actual.getUsers().get("user2").getAuthorizedSchemas(), is("db2"));
     }
     
     @Test
@@ -51,11 +54,14 @@ public final class AuthenticationYamlSwapperTest {
         YamlProxyUserConfiguration user2 = new YamlProxyUserConfiguration();
         user2.setPassword("pwd2");
         user2.setAuthorizedSchemas("db2,db1");
-        YamlAuthenticationConfiguration configuration = new YamlAuthenticationConfiguration();
-        configuration.getUsers().put("user1", user1);
-        configuration.getUsers().put("user2", user2);
-        Authentication actual = new AuthenticationYamlSwapper().swapToObject(configuration);
+        Map<String, YamlProxyUserConfiguration> users = new HashMap<>(2, 1);
+        users.put("user1", user1);
+        users.put("user2", user2);
+        YamlAuthenticationConfiguration yamlConfig = new YamlAuthenticationConfiguration();
+        yamlConfig.setUsers(users);
+        Authentication actual = new AuthenticationYamlSwapper().swapToObject(yamlConfig);
         assertThat(actual.getUsers().size(), is(2));
+        assertThat(actual.getUsers().get("user1").getAuthorizedSchemas().size(), is(1));
         assertThat(actual.getUsers().get("user2").getAuthorizedSchemas().size(), is(2));
     }
     
