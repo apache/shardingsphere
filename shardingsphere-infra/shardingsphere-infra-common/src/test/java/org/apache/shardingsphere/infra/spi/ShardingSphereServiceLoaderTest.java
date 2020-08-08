cb35493336ee8ec12e94809b6339480ba64b26ec
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-08-08 16:03:39 +0800

    add test cases for infra.spi
    * add test case for TypedSPIRegistry
    
    * add test case for ShardingSphereServiceLoader

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/spi/ShardingSphereServiceLoaderTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/spi/ShardingSphereServiceLoaderTest.java
@@ -17,10 +17,16 @@
 
 package org.apache.shardingsphere.infra.spi;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lombok.SneakyThrows;
+import org.apache.shardingsphere.infra.spi.exception.ServiceLoaderInstantiationException;
 import org.apache.shardingsphere.infra.spi.fixture.TypedSPIFixture;
 import org.junit.Test;
-
-import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -40,5 +46,30 @@ public final class ShardingSphereServiceLoaderTest {
         ShardingSphereServiceLoader.register(TypedSPIFixture.class);
         Collection collection = ShardingSphereServiceLoader.newServiceInstances(TypedSPIFixture.class);
         assertThat(collection.size(), is(1));
+    }
+    
+    @Test
+    public void assertRegisterTwice() {
+        ShardingSphereServiceLoader.register(TypedSPIFixture.class);
+        Collection actualFirstRegister = ShardingSphereServiceLoader.newServiceInstances(TypedSPIFixture.class);
+        assertThat(actualFirstRegister.size(), is(1));
+        ShardingSphereServiceLoader.register(TypedSPIFixture.class);
+        Collection actualSecondRegister = ShardingSphereServiceLoader.newServiceInstances(TypedSPIFixture.class);
+        assertThat(actualSecondRegister.size(), is(actualFirstRegister.size()));
+    }
+    
+    @SneakyThrows({NoSuchFieldException.class, IllegalAccessException.class})
+    @Test(expected = ServiceLoaderInstantiationException.class)
+    public void assertNewInstanceError() {
+        ShardingSphereServiceLoader.register(TypedSPIFixture.class);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        Field actualField = ShardingSphereServiceLoader.class.getDeclaredField("SERVICE_MAP");
+        actualField.setAccessible(true);
+        modifiersField.setInt(actualField, actualField.getModifiers() & ~Modifier.FINAL);
+        Map<Class<?>, Collection<Class<?>>> actualServiceMap = new ConcurrentHashMap<>();
+        actualServiceMap.put(TypedSPIFixture.class, Collections.singleton(TypedSPIFixture.class));
+        actualField.set(null, actualServiceMap);
+        ShardingSphereServiceLoader.newServiceInstances(TypedSPIFixture.class);
     }
 }
