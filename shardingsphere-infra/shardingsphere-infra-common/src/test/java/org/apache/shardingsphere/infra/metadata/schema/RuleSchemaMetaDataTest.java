035eb25f2adc46d85633b0d5cb4a068d2e6cb149
胡宏韬 <huht@gddxit.com>
2020-08-12 23:56:01 +0800

    Add test case for RuleSchemaMetaData(apache#6731)

---
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/RuleSchemaMetaDataTest.java
@@ -0,0 +1,14 @@
+package org.apache.shardingsphere.infra.metadata.schema;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RuleSchemaMetaDataTest {
+    @Test
+    public void assertNewInstance()
+    {
+        RuleSchemaMetaData obj = new RuleSchemaMetaData(null, null);
+        assertNotNull("RuleSchemaMetaData is null", obj);
+    }
+}
