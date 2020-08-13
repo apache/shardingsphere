cb1dc446522e9088a48bb32241485d22a0c0d230
胡宏韬 <huht@gddxit.com>
2020-08-13 21:09:01 +0800

    Standard codes.

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/RuleSchemaMetaDataTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/RuleSchemaMetaDataTest.java
@@ -2,12 +2,11 @@ package org.apache.shardingsphere.infra.metadata.schema;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class RuleSchemaMetaDataTest {
     @Test
-    public void assertNewInstance()
-    {
+    public void assertNewInstance() {
         RuleSchemaMetaData obj = new RuleSchemaMetaData(null, null);
         assertNotNull("RuleSchemaMetaData is null", obj);
     }
