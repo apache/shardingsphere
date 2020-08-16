76a9b520c0d6a2e053e9199e32720ce362e060d4
sunnianjun <sunnianjun@huobi.com>
2020-08-16 12:04:13 +0800

    Refactor: refactor the test fixture

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-merge/src/test/java/org/apache/shardingsphere/encrypt/merge/dql/fixture/TableAvailableAndSqlStatementContextFixture.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-merge/src/test/java/org/apache/shardingsphere/encrypt/merge/dql/fixture/TableAvailableAndSqlStatementContextFixture.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.encrypt.merge.dql.fixture;
 
 import org.apache.shardingsphere.sql.parser.binder.segment.table.TablesContext;
@@ -8,10 +25,7 @@ import org.apache.shardingsphere.sql.parser.sql.statement.SQLStatement;
 
 import java.util.Collection;
 
-/**
- * @author Nianjun Sun
- */
-public class TableAvailableAndSqlStatementContextFixture implements TableAvailable, SQLStatementContext {
+public final class TableAvailableAndSqlStatementContextFixture implements TableAvailable, SQLStatementContext {
     
     @Override
     public SQLStatement getSqlStatement() {
