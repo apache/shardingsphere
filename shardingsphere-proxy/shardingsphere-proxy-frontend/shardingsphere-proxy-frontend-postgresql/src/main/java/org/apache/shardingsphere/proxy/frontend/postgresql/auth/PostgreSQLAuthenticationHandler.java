ca220d0b447db4a994e4972b397b589b1df1c1c9
sandynz <42492540+sandynz@users.noreply.github.com>
2020-08-17 17:01:39 +0800

    Implement PostgreSQL authentication for proxy (#5978) (#6860)
    * Implement PostgreSQL authentication for proxy (#5978)
    
    * Fix rat
    
    * Implement PostgreSQL authentication for proxy
    
    * Implement PostgreSQL authentication for proxy - follow review suggestion

---
+++ shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/main/java/org/apache/shardingsphere/proxy/frontend/postgresql/auth/PostgreSQLAuthenticationHandler.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.proxy.frontend.postgresql.auth;
+
+import java.security.MessageDigest;
+import java.util.Map;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.shardingsphere.db.protocol.postgresql.constant.PostgreSQLErrorCode;
+import org.apache.shardingsphere.db.protocol.postgresql.packet.handshake.PostgreSQLPasswordMessagePacket;
+import org.apache.shardingsphere.infra.auth.ProxyUser;
+import org.apache.shardingsphere.proxy.backend.schema.ProxySchemaContexts;
+
+/**
+ * Authentication handler for PostgreSQL.
+ */
+public class PostgreSQLAuthenticationHandler {
+    
+    /**
+     * Login.
+     *
+     * @param userName              user name
+     * @param databaseName          database name
+     * @param md5Salt               md5 salt
+     * @param passwordMessagePacket password message packet
+     * @return PostgreSQLLoginResult
+     */
+    public static PostgreSQLLoginResult loginWithMd5Password(final String userName, final String databaseName, final byte[] md5Salt, final PostgreSQLPasswordMessagePacket passwordMessagePacket) {
+        ProxyUser proxyUser = null;
+        for (Map.Entry<String, ProxyUser> entry : ProxySchemaContexts.getInstance().getSchemaContexts().getAuthentication().getUsers().entrySet()) {
+            if (entry.getKey().equals(userName)) {
+                proxyUser = entry.getValue();
+                break;
+            }
+        }
+        if (null == proxyUser) {
+            return new PostgreSQLLoginResult(PostgreSQLErrorCode.INVALID_AUTHORIZATION_SPECIFICATION, "unknown userName: " + userName);
+        }
+        
+        String md5Digest = passwordMessagePacket.getMd5Digest();
+        String expectedMd5Digest = md5Encode(userName, proxyUser.getPassword(), md5Salt);
+        if (!expectedMd5Digest.equals(md5Digest)) {
+            return new PostgreSQLLoginResult(PostgreSQLErrorCode.INVALID_PASSWORD, "password authentication failed for user \"" + userName + "\"");
+        }
+        
+        if (!proxyUser.getAuthorizedSchemas().contains(databaseName)) {
+            return new PostgreSQLLoginResult(PostgreSQLErrorCode.PRIVILEGE_NOT_GRANTED, String.format("%s has no configured %s in authorizedSchemas", userName, databaseName));
+        }
+        
+        return new PostgreSQLLoginResult(PostgreSQLErrorCode.SUCCESSFUL_COMPLETION, null);
+    }
+    
+    private static String md5Encode(final String userName, final String password, final byte[] md5Salt) {
+        String passwordHash = new String(Hex.encodeHex(DigestUtils.md5(password + userName), true));
+        MessageDigest messageDigest = DigestUtils.getMd5Digest();
+        messageDigest.update(passwordHash.getBytes());
+        messageDigest.update(md5Salt);
+        return "md5" + new String(Hex.encodeHex(messageDigest.digest(), true));
+    }
+    
+}
