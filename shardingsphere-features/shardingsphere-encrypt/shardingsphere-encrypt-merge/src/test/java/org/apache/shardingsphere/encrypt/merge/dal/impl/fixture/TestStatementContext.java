8fac2f996cc7c501a9a6dfbf5e653d28083d63d0
zhouyanjie <zhouyanjie@aliyun.com>
2020-08-17 10:16:01 +0800

    Add test case for DecoratedEncryptColumnsMergedResult

---
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-merge/src/test/java/org/apache/shardingsphere/encrypt/merge/dal/impl/fixture/TestStatementContext.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.encrypt.merge.dal.impl.fixture;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.sql.parser.binder.segment.table.TablesContext;
+import org.apache.shardingsphere.sql.parser.binder.statement.SQLStatementContext;
+import org.apache.shardingsphere.sql.parser.binder.type.TableAvailable;
+import org.apache.shardingsphere.sql.parser.sql.segment.generic.table.SimpleTableSegment;
+import org.apache.shardingsphere.sql.parser.sql.statement.SQLStatement;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class TestStatementContext implements SQLStatementContext, TableAvailable {
+    
+    private final SQLStatement sqlStatement;
+    
+    private final Collection<SimpleTableSegment> allTables;
+    
+    private final TablesContext tablesContext;
+    
+    @Override
+    public SQLStatement getSqlStatement() {
+        return sqlStatement;
+    }
+    
+    @Override
+    public Collection<SimpleTableSegment> getAllTables() {
+        return allTables;
+    }
+    
+    @Override
+    public TablesContext getTablesContext() {
+        return tablesContext;
+    }
+}
