fde9269f7441b6fe1c6e65c4f832f64c825613c9
jian365066744 <365066744@qq.com>
2020-08-20 15:36:22 +0800

    Add more assertions of MasterSlaveSpringBootStarterTest #6926

--- shardingsphere-features/shardingsphere-master-slave/shardingsphere-master-slave-spring/shardingsphere-master-slave-spring-boot-starter/src/test/java/org/apache/shardingsphere/masterslave/spring/boot/MasterSlaveSpringBootStarterTest.java
+++ shardingsphere-features/shardingsphere-master-slave/shardingsphere-master-slave-spring/shardingsphere-master-slave-spring-boot-starter/src/test/java/org/apache/shardingsphere/masterslave/spring/boot/MasterSlaveSpringBootStarterTest.java
@@ -19,6 +19,8 @@ package org.apache.shardingsphere.masterslave.spring.boot;
 
 import org.apache.shardingsphere.masterslave.algorithm.RandomMasterSlaveLoadBalanceAlgorithm;
 import org.apache.shardingsphere.masterslave.algorithm.config.AlgorithmProvidedMasterSlaveRuleConfiguration;
+import org.apache.shardingsphere.masterslave.api.config.rule.MasterSlaveDataSourceRuleConfiguration;
+import org.apache.shardingsphere.masterslave.spi.MasterSlaveLoadBalanceAlgorithm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,8 +29,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MasterSlaveSpringBootStarterTest.class)
@@ -49,6 +58,14 @@ public class MasterSlaveSpringBootStarterTest {
     
     @Test
     public void assertMasterSlaveRuleConfiguration() {
-        // TODO assert MasterSlave Rule Configuration
+        assertTrue(masterSlaveRuleConfiguration.getDataSources().isEmpty());
+        assertTrue(masterSlaveRuleConfiguration.getLoadBalanceAlgorithms().isEmpty());
+        Collection<MasterSlaveDataSourceRuleConfiguration> dataSources = new LinkedList<>();
+        dataSources.add(mock(MasterSlaveDataSourceRuleConfiguration.class));
+        Map<String, MasterSlaveLoadBalanceAlgorithm> loadBalanceAlgorithms = new LinkedHashMap<>();
+        loadBalanceAlgorithms.put("name", mock(MasterSlaveLoadBalanceAlgorithm.class));
+        masterSlaveRuleConfiguration = new AlgorithmProvidedMasterSlaveRuleConfiguration(dataSources, loadBalanceAlgorithms);
+        assertThat(masterSlaveRuleConfiguration.getDataSources().size(), is(1));
+        assertTrue(masterSlaveRuleConfiguration.getLoadBalanceAlgorithms().containsKey("name"));
     }
 }
