87361775530b8e67bfa3e0489e78af4aab68f968
zhouyanjie <zhouyanjie@aliyun.com>
2020-08-24 13:42:52 +0800

    Add sharding rule assertion

--- shardingsphere-jdbc/shardingsphere-jdbc-spring/shardingsphere-jdbc-core-spring/shardingsphere-jdbc-core-spring-boot-starter/src/test/java/org/apache/shardingsphere/spring/boot/SpringBootStarterTest.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-spring/shardingsphere-jdbc-core-spring/shardingsphere-jdbc-core-spring-boot-starter/src/test/java/org/apache/shardingsphere/spring/boot/SpringBootStarterTest.java
@@ -17,13 +17,21 @@
 
 package org.apache.shardingsphere.spring.boot;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
 import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.apache.shardingsphere.infra.config.properties.ConfigurationPropertyKey;
+import org.apache.shardingsphere.infra.datanode.DataNode;
+import org.apache.shardingsphere.infra.datanode.DataNodeUtil;
 import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
 import org.apache.shardingsphere.masterslave.rule.MasterSlaveRule;
 import org.apache.shardingsphere.shadow.rule.ShadowRule;
+import org.apache.shardingsphere.sharding.algorithm.sharding.inline.InlineShardingAlgorithm;
 import org.apache.shardingsphere.sharding.rule.ShardingRule;
+import org.apache.shardingsphere.sharding.rule.TableRule;
+import org.apache.shardingsphere.sharding.spi.ShardingAlgorithm;
+import org.apache.shardingsphere.sharding.strategy.ShardingStrategy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,9 +40,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -72,7 +86,33 @@ public class SpringBootStarterTest {
     }
     
     private void assertShardingRule(final ShardingRule rule) {
-        // TODO
+        assertThat(rule.getDataSourceNames(), is(Sets.newHashSet("ds_0", "ds_1")));
+        Map<String, ShardingAlgorithm> shardingAlgorithmMap = rule.getShardingAlgorithms();
+        assertNotNull(shardingAlgorithmMap);
+        InlineShardingAlgorithm databaseShardingAlgorithm = InlineShardingAlgorithm.class.cast(shardingAlgorithmMap.get("databaseShardingAlgorithm"));
+        assertThat(databaseShardingAlgorithm.getProps().getProperty("algorithm.expression"), is("ds_$->{user_id % 2}"));
+        InlineShardingAlgorithm orderTableShardingAlgorithm = InlineShardingAlgorithm.class.cast(shardingAlgorithmMap.get("orderTableShardingAlgorithm"));
+        assertThat(orderTableShardingAlgorithm.getProps().getProperty("algorithm.expression"), is("t_order_$->{order_id % 2}"));
+        Collection<TableRule> tableRules = rule.getTableRules();
+        assertNotNull(tableRules);
+        assertThat(tableRules.size(), equalTo(1));
+        TableRule tableRule = tableRules.iterator().next();
+        assertThat(tableRule.getLogicTable(), is("t_order"));
+        List<DataNode> dataNodes = Arrays.asList(new DataNode("ds_0.t_order_0"),
+                new DataNode("ds_0.t_order_1"), new DataNode("ds_1.t_order_0"), new DataNode("ds_1.t_order_1"));
+        assertThat(tableRule.getActualDataNodes(), is(dataNodes));
+        assertThat(tableRule.getActualDatasourceNames(), is(Sets.newHashSet("ds_0", "ds_1")));
+        assertThat(tableRule.getDataNodeGroups(), is(DataNodeUtil.getDataNodeGroups(dataNodes)));
+        assertThat(tableRule.getDatasourceToTablesMap(), is(ImmutableMap.of("ds_1",
+                Sets.newHashSet("t_order_0", "t_order_1"), "ds_0", Sets.newHashSet("t_order_0", "t_order_1"))));
+        ShardingStrategy databaseShardingStrategy = tableRule.getDatabaseShardingStrategy();
+        assertNotNull(databaseShardingStrategy);
+        assertThat(databaseShardingStrategy.getShardingColumns(), is(Sets.newTreeSet(Collections.singleton("user_id"))));
+        assertThat(databaseShardingStrategy.getShardingAlgorithm().getProps().getProperty("algorithm.expression"), is("ds_$->{user_id % 2}"));
+        ShardingStrategy tableShardingStrategy = tableRule.getTableShardingStrategy();
+        assertNotNull(tableShardingStrategy);
+        assertThat(tableShardingStrategy.getShardingColumns(), is(Sets.newTreeSet(Collections.singleton("order_id"))));
+        assertThat(tableShardingStrategy.getShardingAlgorithm().getProps().getProperty("algorithm.expression"), is("t_order_$->{order_id % 2}"));
     }
     
     private void assertMasterSlaveRule(final MasterSlaveRule rule) {
