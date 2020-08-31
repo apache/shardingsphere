335069e0cffb4dad9718d7d1ad34ef885f411a1c
kimmking <kimmking@apache.org>
2020-08-31 15:57:53 +0800

    rename governance

---
+++ examples/shardingsphere-jdbc-example/governance-example/governance-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/governance/raw/jdbc/JavaConfigurationExampleMain.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.example.governance.raw.jdbc;
+
+import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
+import org.apache.shardingsphere.example.config.ExampleConfiguration;
+import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
+import org.apache.shardingsphere.example.core.api.service.ExampleService;
+import org.apache.shardingsphere.example.core.jdbc.service.OrderServiceImpl;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.GovernanceRepositoryConfigurationUtil;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.cloud.CloudEncryptConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.cloud.CloudMasterSlaveConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.cloud.CloudShadowConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.cloud.CloudShardingDatabasesAndTablesConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.local.LocalEncryptConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.local.LocalMasterSlaveConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.local.LocalShadowConfiguration;
+import org.apache.shardingsphere.example.governance.raw.jdbc.config.local.LocalShardingDatabasesAndTablesConfiguration;
+import org.apache.shardingsphere.example.type.RegistryCenterType;
+import org.apache.shardingsphere.example.type.ShardingType;
+import org.apache.shardingsphere.governance.repository.api.config.GovernanceConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/*
+ * 1. Please make sure master-slave data sync on MySQL is running correctly. Otherwise this example will query empty data from slave.
+ * 2. Please make sure sharding-governance-center-zookeeper-curator in your pom if registryCenterType = RegistryCenterType.ZOOKEEPER.
+ * 3. Please make sure sharding-governance-center-nacos in your pom if registryCenterType = RegistryCenterType.NACOS.
+ */
+public final class JavaConfigurationExampleMain {
+    
+    private static ShardingType shardingType = ShardingType.SHARDING_DATABASES_AND_TABLES;
+//    private static ShardingType shardingType = ShardingType.MASTER_SLAVE;
+//    private static ShardingType shardingType = ShardingType.ENCRYPT;
+//    private static ShardingType shardingType = ShardingType.SHADOW;
+    
+    private static boolean loadConfigFromRegCenter = false;
+//    private static boolean loadConfigFromRegCenter = true;
+    
+    private static RegistryCenterType registryCenterType = RegistryCenterType.ZOOKEEPER;
+//    private static RegistryCenterType registryCenterType = RegistryCenterType.NACOS;
+    
+    public static void main(final String[] args) throws Exception {
+        DataSource dataSource = getDataSource(shardingType, loadConfigFromRegCenter);
+        try {
+            ExampleExecuteTemplate.run(getExampleService(dataSource));
+        } finally {
+            closeDataSource(dataSource);
+        }
+    }
+    
+    private static DataSource getDataSource(final ShardingType shardingType, final boolean loadConfigFromRegCenter) throws SQLException {
+        GovernanceConfiguration governanceConfiguration = getGovernanceConfiguration(registryCenterType, shardingType);
+        ExampleConfiguration configuration;
+        switch (shardingType) {
+            case SHARDING_DATABASES_AND_TABLES:
+                configuration = loadConfigFromRegCenter 
+                        ? new CloudShardingDatabasesAndTablesConfiguration(governanceConfiguration) : new LocalShardingDatabasesAndTablesConfiguration(governanceConfiguration);
+                break;
+            case MASTER_SLAVE:
+                configuration = loadConfigFromRegCenter ? new CloudMasterSlaveConfiguration(governanceConfiguration) : new LocalMasterSlaveConfiguration(governanceConfiguration);
+                break;
+            case ENCRYPT:
+                configuration = loadConfigFromRegCenter ? new CloudEncryptConfiguration(governanceConfiguration) : new LocalEncryptConfiguration(governanceConfiguration);
+                break;
+            case SHADOW:
+                configuration = loadConfigFromRegCenter ? new CloudShadowConfiguration(governanceConfiguration) : new LocalShadowConfiguration(governanceConfiguration);
+                break;
+            default:
+                throw new UnsupportedOperationException(shardingType.name());
+        }
+        return configuration.getDataSource();
+    }
+    
+    private static GovernanceConfiguration getGovernanceConfiguration(final RegistryCenterType registryCenterType, final ShardingType shardingType) {
+        return RegistryCenterType.ZOOKEEPER == registryCenterType
+                ? GovernanceRepositoryConfigurationUtil.getZooKeeperConfiguration(!loadConfigFromRegCenter, shardingType)
+                : GovernanceRepositoryConfigurationUtil.getNacosConfiguration(!loadConfigFromRegCenter, shardingType);
+    }
+    
+    private static ExampleService getExampleService(final DataSource dataSource) {
+        return new OrderServiceImpl(dataSource);
+    }
+    
+    private static void closeDataSource(final DataSource dataSource) throws Exception {
+        if (dataSource instanceof ShardingSphereDataSource) {
+            ((ShardingSphereDataSource) dataSource).close();
+        }
+    }
+}
---
+++ examples/shardingsphere-jdbc-example/governance-example/governance-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/governance/raw/jdbc/config/GovernanceRepositoryConfigurationUtil.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.example.governance.raw.jdbc.config;
+
+import org.apache.shardingsphere.example.type.ShardingType;
+import org.apache.shardingsphere.governance.repository.api.config.GovernanceConfiguration;
+import org.apache.shardingsphere.governance.repository.api.config.GovernanceCenterConfiguration;
+
+import java.util.Properties;
+
+public final class GovernanceRepositoryConfigurationUtil {
+    
+    private static final String ZOOKEEPER_CONNECTION_STRING = "localhost:2181";
+    
+    private static final String NACOS_CONNECTION_STRING = "localhost:8848";
+    
+    public static GovernanceConfiguration getZooKeeperConfiguration(final boolean overwrite, final ShardingType shardingType) {
+        GovernanceCenterConfiguration governanceCenterConfig = new GovernanceCenterConfiguration("ZooKeeper", ZOOKEEPER_CONNECTION_STRING, new Properties());
+        switch (shardingType) {
+            case SHARDING_DATABASES_AND_TABLES:
+                return new GovernanceConfiguration("governance-sharding-data-source", governanceCenterConfig, overwrite);
+            case MASTER_SLAVE:
+                return new GovernanceConfiguration("governance-ms-data-source", governanceCenterConfig, overwrite);
+            case ENCRYPT:
+                return new GovernanceConfiguration("governance-encrypt-data-source", governanceCenterConfig, overwrite);
+            case SHADOW:
+                return new GovernanceConfiguration("governance-shadow-data-source", governanceCenterConfig, overwrite);
+            default:
+                throw new UnsupportedOperationException(shardingType.toString());
+        }
+    }
+    
+    public static GovernanceConfiguration getNacosConfiguration(final boolean overwrite, final ShardingType shardingType) {
+        Properties nacosProperties = new Properties();
+        nacosProperties.setProperty("group", "SHARDING_SPHERE_DEFAULT_GROUP");
+        nacosProperties.setProperty("timeout", "3000");
+        GovernanceCenterConfiguration nacosConfig = new GovernanceCenterConfiguration("Nacos", NACOS_CONNECTION_STRING, nacosProperties);
+        Properties zookeeperProperties = new Properties();
+        GovernanceCenterConfiguration zookeeperConfig = new GovernanceCenterConfiguration("ZooKeeper", ZOOKEEPER_CONNECTION_STRING, zookeeperProperties);
+        switch (shardingType) {
+            case SHARDING_DATABASES_AND_TABLES:
+                return new GovernanceConfiguration("governance-zookeeper-sharding-data-source", zookeeperConfig, nacosConfig, overwrite);
+            case MASTER_SLAVE:
+                return new GovernanceConfiguration("governance-zookeeper-ms-data-source", zookeeperConfig, nacosConfig, overwrite);
+            case ENCRYPT:
+                return new GovernanceConfiguration("governance-zookeeper-encrypt-data-source", zookeeperConfig, nacosConfig, overwrite);
+            case SHADOW:
+                return new GovernanceConfiguration("governance-zookeeper-shadow-data-source", zookeeperConfig, nacosConfig, overwrite);
+            default:
+                throw new UnsupportedOperationException(shardingType.toString());
+        }
+    }
+}
