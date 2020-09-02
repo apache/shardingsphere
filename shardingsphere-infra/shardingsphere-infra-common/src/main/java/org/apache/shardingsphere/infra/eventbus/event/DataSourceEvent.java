be8aca7857f9cebb9ccb2f2042e17ed7a7ab3d07
terrymanu <terrymanu@163.com>
2020-09-02 10:32:53 +0800

    Fix compile error

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/eventbus/event/DataSourceEvent.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/eventbus/event/DataSourceEvent.java
@@ -19,7 +19,7 @@ package org.apache.shardingsphere.infra.eventbus.event;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.apache.shardingsphere.infra.config.DataSourceConfiguration;
+import org.apache.shardingsphere.infra.config.datasource.DataSourceConfiguration;
 
 import java.util.Map;
 
