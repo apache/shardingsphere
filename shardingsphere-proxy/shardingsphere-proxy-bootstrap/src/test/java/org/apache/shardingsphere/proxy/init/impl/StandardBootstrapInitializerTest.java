3514581903d55bf6c0077c5d127487c31f55d6a7
sunnianjun <sunnianjun@huobi.com>
2020-09-07 18:19:53 +0800

    Add: add more asserts for test case (#7285)

--- shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/init/impl/StandardBootstrapInitializerTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/init/impl/StandardBootstrapInitializerTest.java
@@ -18,6 +18,9 @@
 package org.apache.shardingsphere.proxy.init.impl;
 
 import com.google.common.collect.Lists;
+import org.apache.shardingsphere.infra.auth.yaml.config.YamlAuthenticationConfiguration;
+import org.apache.shardingsphere.infra.auth.yaml.config.YamlProxyUserConfiguration;
+import org.apache.shardingsphere.infra.context.schema.DataSourceParameter;
 import org.apache.shardingsphere.infra.spi.ShardingSphereServiceLoader;
 import org.apache.shardingsphere.infra.yaml.config.YamlRuleConfiguration;
 import org.apache.shardingsphere.infra.yaml.swapper.YamlRuleConfigurationSwapper;
@@ -25,14 +28,20 @@ import org.apache.shardingsphere.proxy.config.ProxyConfiguration;
 import org.apache.shardingsphere.proxy.config.YamlProxyConfiguration;
 import org.apache.shardingsphere.proxy.config.yaml.YamlDataSourceParameter;
 import org.apache.shardingsphere.proxy.config.yaml.YamlProxyRuleConfiguration;
+import org.apache.shardingsphere.proxy.config.yaml.YamlProxyServerConfiguration;
 import org.apache.shardingsphere.proxy.fixture.FixtureYamlRuleConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -46,17 +55,25 @@ public final class StandardBootstrapInitializerTest {
     @Test
     public void assertGetProxyConfiguration() {
         Map<String, YamlProxyRuleConfiguration> ruleConfigurations = generateYamlProxyRuleConfiguration();
+        YamlProxyServerConfiguration serverConfiguration = generateYamlProxyServerConfiguration();
         YamlProxyConfiguration yamlConfig = mock(YamlProxyConfiguration.class);
         when(yamlConfig.getRuleConfigurations()).thenReturn(ruleConfigurations);
-        StandardBootstrapInitializer standardBootstrapInitializer = new StandardBootstrapInitializer();
-        ProxyConfiguration proxyConfiguration = standardBootstrapInitializer.getProxyConfiguration(yamlConfig);
+        when(yamlConfig.getServerConfiguration()).thenReturn(serverConfiguration);
+        ProxyConfiguration proxyConfiguration = new StandardBootstrapInitializer().getProxyConfiguration(yamlConfig);
+        assertSchemaDataSources(proxyConfiguration.getSchemaDataSources());
     }
     
     private Map<String, YamlProxyRuleConfiguration> generateYamlProxyRuleConfiguration() {
         YamlDataSourceParameter yamlDataSourceParameter = new YamlDataSourceParameter();
+        yamlDataSourceParameter.setUrl("jdbc:mysql://localhost:3306/demo_ds");
         yamlDataSourceParameter.setUsername("root");
         yamlDataSourceParameter.setPassword("root");
         yamlDataSourceParameter.setReadOnly(false);
+        yamlDataSourceParameter.setConnectionTimeoutMilliseconds(1000L);
+        yamlDataSourceParameter.setIdleTimeoutMilliseconds(2000L);
+        yamlDataSourceParameter.setMaxLifetimeMilliseconds(4000L);
+        yamlDataSourceParameter.setMaxPoolSize(20);
+        yamlDataSourceParameter.setMinPoolSize(10);
         Map<String, YamlDataSourceParameter> dataSources = new HashMap<>();
         dataSources.put("hikari", yamlDataSourceParameter);
         YamlProxyRuleConfiguration yamlProxyRuleConfiguration = new YamlProxyRuleConfiguration();
@@ -66,5 +83,40 @@ public final class StandardBootstrapInitializerTest {
         Map<String, YamlProxyRuleConfiguration> ruleConfigurations = new HashMap<>();
         ruleConfigurations.put("datasource-0", yamlProxyRuleConfiguration);
         return ruleConfigurations;
+    }
+    
+    private YamlProxyServerConfiguration generateYamlProxyServerConfiguration() {
+        YamlProxyServerConfiguration serverConfiguration = new YamlProxyServerConfiguration();
+        YamlAuthenticationConfiguration authentication = new YamlAuthenticationConfiguration();
+        YamlProxyUserConfiguration yamlProxyUserConfiguration = new YamlProxyUserConfiguration();
+        yamlProxyUserConfiguration.setPassword("root");
+        yamlProxyUserConfiguration.setAuthorizedSchemas("ds-1,ds-2");
+        Map<String, YamlProxyUserConfiguration> users = new HashMap<>();
+        users.put("root", yamlProxyUserConfiguration);
+        authentication.setUsers(users);
+        serverConfiguration.setAuthentication(authentication);
+        Properties props = new Properties();
+        props.setProperty("alpha-1", "alpha-A");
+        props.setProperty("beta-2", "beta-B");
+        serverConfiguration.setProps(props);
+        return serverConfiguration;
+    }
+    
+    private void assertSchemaDataSources(Map<String, Map<String, DataSourceParameter>> schemaDataSources) {
+        assertThat(schemaDataSources.size(), is(1));
+        assertTrue("there is no such key !", schemaDataSources.containsKey("datasource-0"));
+        Map<String, DataSourceParameter> dataSourceParameterMap = schemaDataSources.get("datasource-0");
+        assertThat(dataSourceParameterMap.size(), is(1));
+        assertTrue("there is no such key !", dataSourceParameterMap.containsKey("hikari"));
+        DataSourceParameter dataSourceParameter = dataSourceParameterMap.get("hikari");
+        assertThat(dataSourceParameter.getUrl(), is("jdbc:mysql://localhost:3306/demo_ds"));
+        assertThat(dataSourceParameter.getUsername(), is("root"));
+        assertThat(dataSourceParameter.getPassword(), is("root"));
+        assertThat(dataSourceParameter.isReadOnly(), is(false));
+        assertThat(dataSourceParameter.getConnectionTimeoutMilliseconds(), is(1000L));
+        assertThat(dataSourceParameter.getIdleTimeoutMilliseconds(), is(2000L));
+        assertThat(dataSourceParameter.getMaxLifetimeMilliseconds(), is(4000L));
+        assertThat(dataSourceParameter.getMaxPoolSize(), is(20));
+        assertThat(dataSourceParameter.getMinPoolSize(), is(10));
     }
 }
