39767cf70525d693bd8577003516e0b1c73e297a
flycoolman <flycoolman@gmail.com>
2020-09-10 16:47:26 -0400

    changes in ConfigCenter.java

--- shardingsphere-governance/shardingsphere-governance-core/shardingsphere-governance-core-config/src/main/java/org/apache/shardingsphere/governance/core/config/ConfigCenter.java
+++ shardingsphere-governance/shardingsphere-governance-core/shardingsphere-governance-core-config/src/main/java/org/apache/shardingsphere/governance/core/config/ConfigCenter.java
@@ -40,8 +40,8 @@ import org.apache.shardingsphere.governance.core.event.persist.SchemaNamePersist
 import org.apache.shardingsphere.infra.yaml.config.YamlRootRuleConfigurations;
 import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
 import org.apache.shardingsphere.infra.yaml.swapper.YamlRuleConfigurationSwapperEngine;
-import org.apache.shardingsphere.masterslave.algorithm.config.AlgorithmProvidedMasterSlaveRuleConfiguration;
-import org.apache.shardingsphere.masterslave.api.config.MasterSlaveRuleConfiguration;
+import org.apache.shardingsphere.primaryreplica.algorithm.config.AlgorithmProvidedPrimaryReplicaRuleConfiguration;
+import org.apache.shardingsphere.primaryreplica.api.config.PrimaryReplicaRuleConfiguration;
 import org.apache.shardingsphere.shadow.api.config.ShadowRuleConfiguration;
 import org.apache.shardingsphere.sharding.algorithm.config.AlgorithmProvidedShardingRuleConfiguration;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
@@ -163,19 +163,19 @@ public final class ConfigCenter {
                 Preconditions.checkState(hasAvailableTableConfigurations(config),
                         "No available rule configurations in `%s` for governance.", schemaName);
                 configurations.add(each);
-            } else if (each instanceof AlgorithmProvidedMasterSlaveRuleConfiguration) {
-                AlgorithmProvidedMasterSlaveRuleConfiguration config = (AlgorithmProvidedMasterSlaveRuleConfiguration) each;
+            } else if (each instanceof AlgorithmProvidedPrimaryReplicaRuleConfiguration) {
+                AlgorithmProvidedPrimaryReplicaRuleConfiguration config = (AlgorithmProvidedPrimaryReplicaRuleConfiguration) each;
                 config.getDataSources().forEach(group -> Preconditions.checkState(
-                        !group.getMasterDataSourceName().isEmpty(), "No available master-slave rule configuration in `%s` for governance.", schemaName));
+                        !group.getPrimaryDataSourceName().isEmpty(), "No available primary-replica rule configuration in `%s` for governance.", schemaName));
                 configurations.add(each);
             } else if (each instanceof AlgorithmProvidedEncryptRuleConfiguration) {
                 AlgorithmProvidedEncryptRuleConfiguration config = (AlgorithmProvidedEncryptRuleConfiguration) each;
                 Preconditions.checkState(!config.getEncryptors().isEmpty(), "No available encrypt rule configuration in `%s` for governance.", schemaName);
                 configurations.add(each);
-            } else if (each instanceof MasterSlaveRuleConfiguration) {
-                MasterSlaveRuleConfiguration config = (MasterSlaveRuleConfiguration) each;
+            } else if (each instanceof PrimaryReplicaRuleConfiguration) {
+                PrimaryReplicaRuleConfiguration config = (PrimaryReplicaRuleConfiguration) each;
                 config.getDataSources().forEach(group -> Preconditions.checkState(
-                        !group.getMasterDataSourceName().isEmpty(), "No available master-slave rule configuration in `%s` for governance.", schemaName));
+                        !group.getPrimaryDataSourceName().isEmpty(), "No available primary-replica rule configuration in `%s` for governance.", schemaName));
                 configurations.add(each);
             } else if (each instanceof EncryptRuleConfiguration) {
                 EncryptRuleConfiguration config = (EncryptRuleConfiguration) each;
