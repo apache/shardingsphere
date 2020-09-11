e034e810965a321d3ba5838046cbffc3d6b08484
flycoolman <flycoolman@gmail.com>
2020-09-11 16:27:26 -0400

    changes in shardingsphere/dbtest/engine/dml/BatchDMLIT.java

--- shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/dbtest/engine/dml/BatchDMLIT.java
+++ shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/dbtest/engine/dml/BatchDMLIT.java
@@ -55,8 +55,8 @@ public final class BatchDMLIT extends BatchIT {
     
     @Test
     public void assertExecuteBatch() throws JAXBException, IOException, SQLException, ParseException {
-        // TODO fix masterslave
-        if ("masterslave".equals(getRuleType())) {
+        // TODO fix primaryreplica
+        if ("primaryreplica".equals(getRuleType())) {
             return;
         }
         // TODO fix shadow
@@ -88,8 +88,8 @@ public final class BatchDMLIT extends BatchIT {
     
     @Test
     public void assertClearBatch() throws SQLException, ParseException {
-        // TODO fix masterslave
-        if ("masterslave".equals(getRuleType())) {
+        // TODO fix primaryreplica
+        if ("primaryreplica".equals(getRuleType())) {
             return;
         }
         // TODO fix shadow
