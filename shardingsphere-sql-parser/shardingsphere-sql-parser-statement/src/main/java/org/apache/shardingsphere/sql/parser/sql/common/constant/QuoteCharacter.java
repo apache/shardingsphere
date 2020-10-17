54b330fc64a7a9aacf296294775f56640bae959f
terrymanu <terrymanu@163.com>
2020-10-17 15:46:20 +0800

    Add test case

--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/constant/QuoteCharacter.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/constant/QuoteCharacter.java
@@ -64,6 +64,6 @@ public enum QuoteCharacter {
      * @return wrapped value
      */
     public String wrap(final String value) {
-        return String.join(startDelimiter, value, endDelimiter);
+        return String.format("%s%s%s", startDelimiter, value, endDelimiter);
     }
 }
