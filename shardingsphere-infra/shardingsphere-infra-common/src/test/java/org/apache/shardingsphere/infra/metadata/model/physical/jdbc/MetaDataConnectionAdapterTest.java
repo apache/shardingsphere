aa82b5f99a419174b48e81bf97f7faf26326262c
sunnianjun <sunnianjun@huobi.com>
2020-10-29 09:27:56 +0800

    Format: format the code (#7761)

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/model/physical/jdbc/MetaDataConnectionAdapterTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/model/physical/jdbc/MetaDataConnectionAdapterTest.java
@@ -70,7 +70,7 @@ public final class MetaDataConnectionAdapterTest {
     }
     
     @Test
-    public void  assertGetSchemaByOracleSPI() throws SQLException {
+    public void assertGetSchemaByOracleSPI() throws SQLException {
         DatabaseMetaData databaseMetaData = mock(DatabaseMetaData.class);
         when(connection.getMetaData()).thenReturn(databaseMetaData);
         when(databaseMetaData.getUserName()).thenReturn(TEST_SCHEMA);
@@ -79,7 +79,7 @@ public final class MetaDataConnectionAdapterTest {
     }
     
     @Test
-    public void  assertGetSchemaByMySQLSPI() throws SQLException {
+    public void assertGetSchemaByMySQLSPI() throws SQLException {
         when(connection.getSchema()).thenReturn(TEST_SCHEMA);
         MetaDataConnectionAdapter connectionAdapter = new MetaDataConnectionAdapter(databaseType, connection);
         assertThat(connectionAdapter.getSchema(), is(TEST_SCHEMA));
