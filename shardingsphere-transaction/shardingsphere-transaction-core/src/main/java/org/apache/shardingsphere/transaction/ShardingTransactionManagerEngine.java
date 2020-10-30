e420640379ee8b14393c876100ba8eb2359d95db
yu199195 <549477611@qq.com>
2020-10-30 15:17:25 +0800

    fix checkstyle

--- shardingsphere-transaction/shardingsphere-transaction-core/src/main/java/org/apache/shardingsphere/transaction/ShardingTransactionManagerEngine.java
+++ shardingsphere-transaction/shardingsphere-transaction-core/src/main/java/org/apache/shardingsphere/transaction/ShardingTransactionManagerEngine.java
@@ -29,7 +29,6 @@ import javax.sql.DataSource;
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.ServiceLoader;
