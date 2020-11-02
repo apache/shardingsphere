e0715c37ba033046566a40956dad0eb974ffc0de
terrymanu <terrymanu@163.com>
2020-11-02 18:28:54 +0800

    Use SQLRouteExecutor to refactor SQL route engine

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/main/java/org/apache/shardingsphere/sharding/metadata/ShardingTableAddressingMetaDataDecorator.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/main/java/org/apache/shardingsphere/sharding/metadata/ShardingTableAddressingMetaDataDecorator.java
@@ -38,10 +38,13 @@ public final class ShardingTableAddressingMetaDataDecorator implements TableAddr
     }
     
     private void decorate(final TableRule tableRule, final TableAddressingMetaData metaData) {
+        boolean found = false;
         for (String each : tableRule.getActualDataNodes().stream().map(DataNode::getTableName).collect(Collectors.toSet())) {
-            metaData.getTableDataSourceNamesMapper().remove(each);
+            found = null != metaData.getTableDataSourceNamesMapper().remove(each) || found;
         }
-        metaData.getTableDataSourceNamesMapper().put(tableRule.getLogicTable(), new LinkedList<>(tableRule.getActualDatasourceNames()));
+        if (found) {
+            metaData.getTableDataSourceNamesMapper().put(tableRule.getLogicTable(), new LinkedList<>(tableRule.getActualDatasourceNames()));
+        }
     }
     
     @Override
