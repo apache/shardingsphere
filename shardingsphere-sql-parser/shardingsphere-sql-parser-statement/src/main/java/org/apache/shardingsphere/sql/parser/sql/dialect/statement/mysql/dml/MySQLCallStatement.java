5fa9b9aa3c5e482759bf1bb4d2080a91a46d50d0
wenweibin <weibinwen@gmail.com>
2020-11-06 09:59:00 +0800

    Improve mysql Call Statement definition and Visitor #7791

--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/dml/MySQLCallStatement.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/dml/MySQLCallStatement.java
@@ -19,6 +19,7 @@ package org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ExpressionSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.CallStatement;
@@ -31,6 +32,7 @@ import java.util.List;
  */
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public final class MySQLCallStatement extends CallStatement implements MySQLStatement {
 
