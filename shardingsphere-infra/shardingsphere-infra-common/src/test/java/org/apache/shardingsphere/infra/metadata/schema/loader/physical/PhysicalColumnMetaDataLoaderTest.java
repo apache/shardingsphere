2693b6b2710adc05332cff1467aa9cddf00d4886
terrymanu <terrymanu@163.com>
2020-11-09 22:17:28 +0800

    Refactor PhysicalColumnMetaDataLoaderTest

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/loader/physical/PhysicalColumnMetaDataLoaderTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/loader/physical/PhysicalColumnMetaDataLoaderTest.java
@@ -18,20 +18,18 @@
 package org.apache.shardingsphere.infra.metadata.schema.loader.physical;
 
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
-import org.apache.shardingsphere.infra.database.type.DatabaseTypeRegistry;
 import org.apache.shardingsphere.infra.metadata.schema.model.ColumnMetaData;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Types;
 import java.util.Collection;
 import java.util.Iterator;
@@ -39,31 +37,20 @@ import java.util.Iterator;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class PhysicalColumnMetaDataLoaderTest {
     
-    private static final String TEST_CATALOG = "catalog";
-    
-    private static final String TEST_TABLE = "table";
-    
-    private final DatabaseType databaseType = DatabaseTypeRegistry.getActualDatabaseType("MySQL");
-    
-    @Mock
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private Connection connection;
-    
-    @Mock
-    private DatabaseMetaData databaseMetaData;
     
     @Mock
     private ResultSet primaryResultSet;
     
     @Mock
     private ResultSet columnResultSet;
-    
-    @Mock
-    private Statement statement;
     
     @Mock
     private ResultSet caseSensitivesResultSet;
@@ -73,19 +60,17 @@ public final class PhysicalColumnMetaDataLoaderTest {
     
     @Before
     public void setUp() throws SQLException {
-        when(connection.getCatalog()).thenReturn(TEST_CATALOG);
-        when(connection.getMetaData()).thenReturn(databaseMetaData);
-        when(databaseMetaData.getPrimaryKeys(TEST_CATALOG, null, TEST_TABLE)).thenReturn(primaryResultSet);
+        when(connection.getCatalog()).thenReturn("catalog");
+        when(connection.getMetaData().getPrimaryKeys("catalog", null, "tbl")).thenReturn(primaryResultSet);
         when(primaryResultSet.next()).thenReturn(true, false);
         when(primaryResultSet.getString("COLUMN_NAME")).thenReturn("pk_col");
-        when(databaseMetaData.getColumns(TEST_CATALOG, null, TEST_TABLE, "%")).thenReturn(columnResultSet);
+        when(connection.getMetaData().getColumns("catalog", null, "tbl", "%")).thenReturn(columnResultSet);
         when(columnResultSet.next()).thenReturn(true, true, false);
-        when(columnResultSet.getString("TABLE_NAME")).thenReturn(TEST_TABLE);
+        when(columnResultSet.getString("TABLE_NAME")).thenReturn("tbl");
         when(columnResultSet.getString("COLUMN_NAME")).thenReturn("pk_col", "col");
         when(columnResultSet.getInt("DATA_TYPE")).thenReturn(Types.INTEGER, Types.VARCHAR);
         when(columnResultSet.getString("TYPE_NAME")).thenReturn("INT", "VARCHAR");
-        when(connection.createStatement()).thenReturn(statement);
-        when(statement.executeQuery(anyString())).thenReturn(caseSensitivesResultSet);
+        when(connection.createStatement().executeQuery(anyString())).thenReturn(caseSensitivesResultSet);
         when(caseSensitivesResultSet.findColumn("pk_col")).thenReturn(1);
         when(caseSensitivesResultSet.findColumn("col")).thenReturn(2);
         when(caseSensitivesResultSet.getMetaData()).thenReturn(resultSetMetaData);
@@ -94,7 +79,7 @@ public final class PhysicalColumnMetaDataLoaderTest {
     
     @Test
     public void assertLoad() throws SQLException {
-        Collection<ColumnMetaData> actual = PhysicalColumnMetaDataLoader.load(connection, TEST_TABLE, databaseType);
+        Collection<ColumnMetaData> actual = PhysicalColumnMetaDataLoader.load(connection, "tbl", mock(DatabaseType.class));
         assertThat(actual.size(), is(2));
         Iterator<ColumnMetaData> columnMetaDataIterator = actual.iterator();
         assertColumnMetaData(columnMetaDataIterator.next(), "pk_col", Types.INTEGER, "INT", true, true);
