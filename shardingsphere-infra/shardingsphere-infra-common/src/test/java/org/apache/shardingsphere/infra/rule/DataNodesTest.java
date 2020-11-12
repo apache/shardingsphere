bd5e1aeadaa834346612c3e4dc2e8ca7ba2340f8
wenweibin <weibinwen@gmail.com>
2020-11-12 09:42:25 +0800

    Use mock to instead of TestShardingRule and TestTableRule in DataNodesTest class

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/rule/DataNodesTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/rule/DataNodesTest.java
@@ -19,15 +19,14 @@ package org.apache.shardingsphere.infra.rule;
 
 import org.apache.shardingsphere.infra.datanode.DataNode;
 import org.apache.shardingsphere.infra.datanode.DataNodes;
-import org.apache.shardingsphere.infra.rule.fixture.TestShardingRule;
-import org.apache.shardingsphere.infra.rule.fixture.TestShardingSphereRule;
-import org.apache.shardingsphere.infra.rule.fixture.TestTableRule;
+import org.apache.shardingsphere.infra.rule.type.DataNodeContainedRule;
 import org.apache.shardingsphere.infra.rule.type.DataSourceContainedRule;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -40,59 +39,60 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public final class DataNodesTest {
-    
+
     private final String logicTableName1 = "user";
-    
+
     private final String logicTableName2 = "dept";
-    
+
     private final Collection<String> dataSourceNames1 = Arrays.asList("primary_db_1", "primary_db_2", "replica_db_1", "replica_db_2");
-    
+
     private final Collection<String> dataSourceNames2 = Arrays.asList("primary_db_3", "replica_db_3");
-    
+
     private final String logicDataSourceName = "primary_db_1";
-    
+
     private final Collection<String> replicaDataSourceNames = Arrays.asList("route_db_1", "route_db_2");
-    
+
     @Test(expected = NullPointerException.class)
     public void assertWrongTable() {
         DataNodes dataNodes = getRoutedRuleDataNodes();
         dataNodes.getDataNodes("wrongTableName");
     }
-    
+
     @Test
     public void assertGetEmpty() {
         assertThat(getNonRoutedRuleDataNodes().getDataNodes("tableName"), is(Collections.emptyList()));
     }
-    
+
     @Test
     public void assertGetDataNodes() {
         DataNodes dataNodes = getRoutedRuleDataNodes();
         assertTrue(dataNodes.getDataNodes(logicTableName1).containsAll(getExpectedDataNodes(dataSourceNames1, logicTableName1)));
         assertTrue(dataNodes.getDataNodes(logicTableName2).containsAll(getExpectedDataNodes(dataSourceNames2, logicTableName2)));
     }
-    
+
     @Test
     public void assertGetDataNodeGroups() {
         DataNodes dataNodes = getRoutedRuleDataNodes();
         assertThat(dataNodes.getDataNodeGroups(logicTableName1), is(getExpectedDataNodeGroups(dataSourceNames1, logicTableName1)));
         assertThat(dataNodes.getDataNodeGroups(logicTableName2), is(getExpectedDataNodeGroups(dataSourceNames2, logicTableName2)));
     }
-    
+
     private DataNodes getRoutedRuleDataNodes() {
-        TestTableRule tableRule1 = new TestTableRule(dataSourceNames1, logicTableName1);
-        TestTableRule tableRule2 = new TestTableRule(dataSourceNames2, logicTableName2);
-        List<TestTableRule> tableRules = Arrays.asList(tableRule1, tableRule2);
-        ShardingSphereRule rule1 = new TestShardingRule(tableRules);
+        Map<String, Collection<DataNode>> nodeMap = new HashMap<>();
+        nodeMap.put(logicTableName1, getExpectedDataNodes(dataSourceNames1, logicTableName1));
+        nodeMap.put(logicTableName2, getExpectedDataNodes(dataSourceNames2, logicTableName2));
+        DataNodeContainedRule rule1 = mock(DataNodeContainedRule.class);
+        when(rule1.getAllDataNodes()).thenReturn(nodeMap);
         Map<String, Collection<String>> dataSourceMapper = Collections.singletonMap(logicDataSourceName, replicaDataSourceNames);
         DataSourceContainedRule rule2 = mock(DataSourceContainedRule.class);
         when(rule2.getDataSourceMapper()).thenReturn(dataSourceMapper);
         return new DataNodes(Arrays.asList(rule1, rule2));
     }
-    
+
     private DataNodes getNonRoutedRuleDataNodes() {
-        return new DataNodes(Collections.singleton(new TestShardingSphereRule()));
+        return new DataNodes(Collections.singleton(mock(ShardingSphereRule.class)));
     }
-    
+
     private Collection<DataNode> getExpectedDataNodes(final Collection<String> dataSourceNames, final String logicTableName) {
         Collection<DataNode> result = new LinkedList<>();
         for (String each : dataSourceNames) {
@@ -104,7 +104,7 @@ public final class DataNodesTest {
         }
         return result;
     }
-    
+
     private Map<String, List<DataNode>> getExpectedDataNodeGroups(final Collection<String> dataSourceNames, final String logicTableName) {
         return getExpectedDataNodes(dataSourceNames, logicTableName).stream().collect(Collectors.groupingBy(DataNode::getDataSourceName));
     }
