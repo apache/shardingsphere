c50e3da55c676b11edbef07dbd2b81b442422df1
Liang Zhang <terrymanu@163.com>
2020-11-12 16:38:45 +0800

    Simplify encrypt rule (#8146)
    * Refactor EncryptTableMetaDataBuilder
    
    * Fix test case
    
    * Remove useless EncryptRule.getEncryptTableNames
    
    * Remove useless EncryptRule.isCipherColumn
    
    * Remove useless EncryptRule.getLogicColumnOfCipher
    
    * Remove useless EncryptRule.getAssistedQueryAndPlainColumns
    
    * For code style

--- shardingsphere-jdbc/shardingsphere-jdbc-spring/shardingsphere-jdbc-core-spring/shardingsphere-jdbc-core-spring-boot-starter/src/test/java/org/apache/shardingsphere/spring/boot/SpringBootStarterTest.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-spring/shardingsphere-jdbc-core-spring/shardingsphere-jdbc-core-spring-boot-starter/src/test/java/org/apache/shardingsphere/spring/boot/SpringBootStarterTest.java
@@ -21,6 +21,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
+import org.apache.shardingsphere.encrypt.rule.EncryptTable;
 import org.apache.shardingsphere.infra.config.properties.ConfigurationPropertyKey;
 import org.apache.shardingsphere.infra.datanode.DataNode;
 import org.apache.shardingsphere.infra.datanode.DataNodeUtil;
@@ -118,13 +119,15 @@ public class SpringBootStarterTest {
     }
     
     private void assertEncryptRule(final EncryptRule rule) {
-        assertThat(rule.getEncryptTableNames(), is(Sets.newLinkedHashSet(Collections.singletonList("t_order"))));
+        assertTrue(rule.findEncryptTable("t_order").isPresent());
+        EncryptTable table = rule.findEncryptTable("t_order").get();
+        assertThat(table.getLogicColumn("pwd_cipher"), is("pwd"));
+        assertThat(table.getPlainColumns(), is(Collections.singletonList("pwd_plain")));
+        assertThat(table.getAssistedQueryColumns(), is(Collections.singletonList("pwd_assisted_query_cipher")));
         assertThat(rule.getCipherColumn("t_order", "pwd"), is("pwd_cipher"));
         assertThat(rule.getAssistedQueryColumns("t_order"), is(Collections.singletonList("pwd_assisted_query_cipher")));
         assertThat(rule.getLogicAndCipherColumns("t_order"), is(Collections.singletonMap("pwd", "pwd_cipher")));
-        assertThat(rule.getLogicColumnOfCipher("t_order", "pwd_cipher"), is("pwd"));
         assertThat(rule.getEncryptValues("t_order", "pwd", Collections.singletonList("pwd_plain")), is(Collections.singletonList("V/RkV1+dVv80Y3csT3cR4g==")));
-        assertThat(rule.getAssistedQueryAndPlainColumns("t_order"), is(Arrays.asList("pwd_assisted_query_cipher", "pwd_plain")));
     }
     
     private void assertShadowRule(final ShadowRule rule) {
