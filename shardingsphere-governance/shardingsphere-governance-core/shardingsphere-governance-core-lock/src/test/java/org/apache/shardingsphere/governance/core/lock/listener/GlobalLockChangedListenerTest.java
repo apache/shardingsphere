0e46ddf0a42aa7ff9a36f6ce8c4ef6fd2aea3ccc
menghaoranss <localcc@163.com>
2020-11-24 18:55:48 +0800

    Initialize distributed lock module

--- shardingsphere-governance/shardingsphere-governance-core/shardingsphere-governance-core-lock/src/test/java/org/apache/shardingsphere/governance/core/lock/listener/GlobalLockChangedListenerTest.java
+++ shardingsphere-governance/shardingsphere-governance-core/shardingsphere-governance-core-lock/src/test/java/org/apache/shardingsphere/governance/core/lock/listener/GlobalLockChangedListenerTest.java
@@ -24,12 +24,15 @@ import org.apache.shardingsphere.governance.repository.api.listener.DataChangedE
 import org.apache.shardingsphere.governance.repository.api.listener.DataChangedEvent.Type;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
 
+@RunWith(MockitoJUnitRunner.class)
 public final class GlobalLockChangedListenerTest {
     
     private GlobalLockChangedListener globalLockChangedListener;
