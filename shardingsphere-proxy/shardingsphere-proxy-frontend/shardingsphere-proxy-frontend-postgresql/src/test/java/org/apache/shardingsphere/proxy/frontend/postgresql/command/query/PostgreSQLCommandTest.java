c890c6ec31025b6405179e2744578ba7fa62c4b6
huanghao-jk <huanghao-jk@360jinrong.net>
2020-12-01 18:44:01 +0800

    Add test case for PostgreSQLCommand #8439
    (cherry picked from commit 2f8e254524546d32ab1c497f0cf3a466b88ba5e9)
    
    # Conflicts:
    #	shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/PostgreSQLCommandTest.java

--- shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/PostgreSQLCommandTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/PostgreSQLCommandTest.java
@@ -34,6 +34,12 @@ import static org.mockito.Mockito.mock;
 public final class PostgreSQLCommandTest {
 
     @Test
+    public void assertPostgreSQLCommandWhenSQLStatementInstanceOfUnEnumeration() {
+        PostgreSQLCommand postgreSQLCommand = new PostgreSQLCommand(mock(SQLStatement.class));
+        assertThat(postgreSQLCommand.getSQLCommand(), is(""));
+    }
+
+    @Test
     public void assertPostgreSQLCommandWhenSQLStatementInstanceOfInsertStatement() {
         PostgreSQLCommand postgreSQLCommand = new PostgreSQLCommand(mock(InsertStatement.class));
         assertThat(postgreSQLCommand.getSQLCommand(), is("INSERT"));
@@ -73,11 +79,5 @@ public final class PostgreSQLCommandTest {
     public void assertPostgreSQLCommandWhenSQLStatementInstanceOfCreateShardingRuleStatement() {
         PostgreSQLCommand postgreSQLCommand = new PostgreSQLCommand(mock(CreateShardingRuleStatement.class));
         assertThat(postgreSQLCommand.getSQLCommand(), is("CREATE"));
-    }
-
-    @Test
-    public void assertPostgreSQLCommandWhenSQLStatementInstanceOfUnEnumeration() {
-        PostgreSQLCommand postgreSQLCommand = new PostgreSQLCommand(mock(SQLStatement.class));
-        assertThat(postgreSQLCommand.getSQLCommand(), is(""));
     }
 }
