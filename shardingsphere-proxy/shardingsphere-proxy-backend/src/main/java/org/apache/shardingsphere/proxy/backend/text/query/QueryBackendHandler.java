4d325a7c95f042afb5ae4010f870964410bed16a
terrymanu <terrymanu@163.com>
2020-12-03 14:17:49 +0800

    Merge QueryBackendHandler and UnicastBackendHandler

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/query/QueryBackendHandler.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/query/QueryBackendHandler.java
@@ -18,10 +18,12 @@
 package org.apache.shardingsphere.proxy.backend.text.query;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
 import org.apache.shardingsphere.proxy.backend.communication.DatabaseCommunicationEngine;
 import org.apache.shardingsphere.proxy.backend.communication.DatabaseCommunicationEngineFactory;
 import org.apache.shardingsphere.proxy.backend.communication.jdbc.connection.BackendConnection;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
+import org.apache.shardingsphere.proxy.backend.exception.NoDatabaseSelectedException;
 import org.apache.shardingsphere.proxy.backend.exception.RuleNotExistsException;
 import org.apache.shardingsphere.proxy.backend.response.header.ResponseHeader;
 import org.apache.shardingsphere.proxy.backend.text.TextProtocolBackendHandler;
@@ -29,6 +31,7 @@ import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * Backend handler with query.
@@ -44,15 +47,29 @@ public final class QueryBackendHandler implements TextProtocolBackendHandler {
     
     private final BackendConnection backendConnection;
     
+    private final boolean schemaSelectedRequired;
+    
     private DatabaseCommunicationEngine databaseCommunicationEngine;
     
     @Override
     public ResponseHeader execute() throws SQLException {
+        if (!schemaSelectedRequired && null == backendConnection.getSchemaName()) {
+            // TODO should remove set default ShardingSphere schema after parser can recognize all DAL broadcast SQL.
+            backendConnection.setCurrentSchema(getFirstSchemaName());
+        }
         if (!ProxyContext.getInstance().getMetaData(backendConnection.getSchemaName()).isComplete()) {
             throw new RuleNotExistsException();
         }
         databaseCommunicationEngine = databaseCommunicationEngineFactory.newTextProtocolInstance(sqlStatement, sql, backendConnection);
         return databaseCommunicationEngine.execute();
+    }
+    
+    private String getFirstSchemaName() {
+        Map<String, ShardingSphereMetaData> metaDataMap = ProxyContext.getInstance().getMetaDataContexts().getMetaDataMap();
+        if (metaDataMap.isEmpty()) {
+            throw new NoDatabaseSelectedException();
+        }
+        return metaDataMap.keySet().iterator().next();
     }
     
     @Override
