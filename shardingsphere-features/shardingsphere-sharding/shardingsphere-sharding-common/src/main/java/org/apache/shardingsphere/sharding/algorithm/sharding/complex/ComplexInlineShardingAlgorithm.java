49480c746ef5465d5aee7c6cc39328ad444f9ed9
likly <569553290@qq.com>
2020-12-11 21:20:07 +0800

    Add Complex And Hint Inline Sharding Algorithm. (#8576)
    * Add Complex and Hint Inline sharding algorithm.
    
    * Update Complex and Hint Inline sharding algorithm docs
    
    * Add Complex and Hint Inline sharding algorithm license
    
    * Fix not config sharding-columns checkout sharing values keys size bug
    
    * fix checkstyle
    
    * remove file header info
    
    Co-authored-by: likly <likly@ilikly.com>

---
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/main/java/org/apache/shardingsphere/sharding/algorithm/sharding/complex/ComplexInlineShardingAlgorithm.java
@@ -0,0 +1,154 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sharding.algorithm.sharding.complex;
+
+import com.google.common.base.Preconditions;
+import groovy.lang.Closure;
+import groovy.util.Expando;
+import org.apache.shardingsphere.sharding.algorithm.sharding.inline.InlineExpressionParser;
+import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingAlgorithm;
+import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+/**
+ * Complex Inline sharding algorithm.
+ */
+public class ComplexInlineShardingAlgorithm implements ComplexKeysShardingAlgorithm<Comparable<?>> {
+
+    private static final String ALGORITHM_EXPRESSION_KEY = "algorithm-expression";
+
+    private static final String SHARING_COLUMNS_KEY = "sharding-columns";
+
+    private static final String ALLOW_RANGE_QUERY_KEY = "allow-range-query-with-inline-sharding";
+
+    private boolean allowRangeQuery;
+
+    private String[] shardingColumns;
+
+    private String algorithmExpression;
+
+    private final Properties props = new Properties();
+
+    @Override
+    public Collection<String> doSharding(final Collection<String> availableTargetNames, final ComplexKeysShardingValue<Comparable<?>> shardingValue) {
+
+        if (!shardingValue.getColumnNameAndRangeValuesMap().isEmpty()) {
+            if (isAllowRangeQuery()) {
+                return availableTargetNames;
+            }
+
+            throw new UnsupportedOperationException("Since the property of `" + ALLOW_RANGE_QUERY_KEY + "` is false, inline sharding algorithm can not tackle with range query.");
+        }
+
+        Map<String, Collection<Comparable<?>>> columnNameAndShardingValuesMap = shardingValue.getColumnNameAndShardingValuesMap();
+
+        if (shardingColumns.length > 0 && shardingColumns.length != columnNameAndShardingValuesMap.size()) {
+            throw new IllegalArgumentException("complex inline need " + shardingColumns.length + " sharing columns, but only found " + columnNameAndShardingValuesMap.size());
+        }
+
+        Collection<Map<String, Comparable<?>>> combine = combine(columnNameAndShardingValuesMap);
+
+        return combine.stream()
+                .map(this::doSharding)
+                .collect(Collectors.toList());
+
+    }
+
+    private String doSharding(final Map<String, Comparable<?>> shardingValues) {
+        Closure<?> closure = createClosure();
+        for (Map.Entry<String, Comparable<?>> entry : shardingValues.entrySet()) {
+            closure.setProperty(entry.getKey(), entry.getValue());
+        }
+        return closure.call().toString();
+    }
+
+    @Override
+    public void init() {
+        String expression = props.getProperty(ALGORITHM_EXPRESSION_KEY);
+        Preconditions.checkNotNull(expression, "Inline sharding algorithm expression cannot be null.");
+        algorithmExpression = InlineExpressionParser.handlePlaceHolder(expression.trim());
+        initShardingColumns(props.getProperty(SHARING_COLUMNS_KEY, ""));
+        allowRangeQuery = Boolean.parseBoolean(props.getOrDefault(ALLOW_RANGE_QUERY_KEY, Boolean.FALSE.toString()).toString());
+    }
+
+    private void initShardingColumns(final String shardingColumns) {
+        if (shardingColumns.length() == 0) {
+            this.shardingColumns = new String[0];
+            return;
+        }
+        this.shardingColumns = shardingColumns.split(",");
+    }
+
+    private boolean isAllowRangeQuery() {
+        return allowRangeQuery;
+    }
+
+    private Closure<?> createClosure() {
+        Closure<?> result = new InlineExpressionParser(algorithmExpression).evaluateClosure().rehydrate(new Expando(), null, null);
+        result.setResolveStrategy(Closure.DELEGATE_ONLY);
+        return result;
+    }
+
+    @Override
+    public String getType() {
+        return "COMPLEX_INLINE";
+    }
+
+    @Override
+    public Properties getProps() {
+        return props;
+    }
+
+    @Override
+    public void setProps(final Properties props) {
+        this.props.clear();
+        this.props.putAll(props);
+    }
+
+    private static <K, V> Collection<Map<K, V>> combine(final Map<K, Collection<V>> map) {
+        Collection<Map<K, V>> result = new ArrayList<>();
+        for (Map.Entry<K, Collection<V>> entry : map.entrySet()) {
+            if (result.isEmpty()) {
+                for (V value : entry.getValue()) {
+                    Map<K, V> item = new HashMap<>();
+                    item.put(entry.getKey(), value);
+                    result.add(item);
+                }
+            } else {
+                Collection<Map<K, V>> list = new ArrayList<>();
+                for (Map<K, V> loop : result) {
+                    for (V value : entry.getValue()) {
+                        Map<K, V> item = new HashMap<>();
+                        item.put(entry.getKey(), value);
+                        item.putAll(loop);
+                        list.add(item);
+                    }
+                }
+                result = list;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/test/java/org/apache/shardingsphere/sharding/algorithm/sharding/complex/ComplexInlineShardingAlgorithmTest.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sharding.algorithm.sharding.complex;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Range;
+import org.apache.shardingsphere.sharding.api.sharding.complex.ComplexKeysShardingValue;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Complex Inline sharding algorithm test.
+ */
+public class ComplexInlineShardingAlgorithmTest {
+
+    private ComplexInlineShardingAlgorithm complexInlineShardingAlgorithm;
+
+    private ComplexInlineShardingAlgorithm complexInlineShardingAlgorithmAllowRangeQuery;
+
+    @Before
+    public void setUp() throws Exception {
+        initComplexInlineShardingAlgorithm();
+        initComplexInlineShardingAlgorithmAllowRangeQuery();
+    }
+
+    private void initComplexInlineShardingAlgorithm() {
+        complexInlineShardingAlgorithm = new ComplexInlineShardingAlgorithm();
+        complexInlineShardingAlgorithm.getProps().setProperty("algorithm-expression", "t_order_${type % 2}_${order_id % 2}");
+        complexInlineShardingAlgorithm.getProps().setProperty("sharding-columns", "type,order_id");
+        complexInlineShardingAlgorithm.init();
+    }
+
+    private void initComplexInlineShardingAlgorithmAllowRangeQuery() {
+        complexInlineShardingAlgorithmAllowRangeQuery = new ComplexInlineShardingAlgorithm();
+        complexInlineShardingAlgorithmAllowRangeQuery.getProps().setProperty("algorithm-expression", "t_order_${type % 2}_${order_id % 2}");
+        complexInlineShardingAlgorithmAllowRangeQuery.getProps().setProperty("sharding-columns", "type,order_id");
+        complexInlineShardingAlgorithmAllowRangeQuery.getProps().setProperty("allow-range-query-with-inline-sharding", "true");
+        complexInlineShardingAlgorithmAllowRangeQuery.init();
+    }
+
+    @Test
+    public void assertDoSharding() {
+        List<String> availableTargetNames = Lists.newArrayList("t_order_0_0", "t_order_0_1", "t_order_1_0", "t_order_1_1");
+        Map<String, Collection<Comparable<?>>> sharingValueMap = new HashMap<>();
+        sharingValueMap.put("type", Collections.singletonList(2));
+        sharingValueMap.put("order_id", Collections.singletonList(2));
+        Map<String, Range<Comparable<?>>> rangeShardingValueMap = new HashMap<>();
+        ComplexKeysShardingValue<Comparable<?>> shardingValue = new ComplexKeysShardingValue<>("t_order", sharingValueMap, rangeShardingValueMap);
+        Collection<String> actual = complexInlineShardingAlgorithm.doSharding(availableTargetNames, shardingValue);
+        assertTrue(actual.size() == 1 && actual.contains("t_order_0_0"));
+    }
+
+    @Test
+    public void assertDoShardingWithMultiValue() {
+        List<String> availableTargetNames = Lists.newArrayList("t_order_0_0", "t_order_0_1", "t_order_1_0", "t_order_1_1");
+        Map<String, Collection<Comparable<?>>> sharingValueMap = new HashMap<>();
+        sharingValueMap.put("type", Arrays.asList(1, 2));
+        sharingValueMap.put("order_id", Arrays.asList(1, 2));
+        Map<String, Range<Comparable<?>>> rangeShardingValueMap = new HashMap<>();
+        ComplexKeysShardingValue<Comparable<?>> shardingValue = new ComplexKeysShardingValue<>("t_order", sharingValueMap, rangeShardingValueMap);
+        Collection<String> actual = complexInlineShardingAlgorithm.doSharding(availableTargetNames, shardingValue);
+        assertTrue(actual.containsAll(availableTargetNames));
+    }
+
+    @Test
+    public void assertDoShardingWithRangeValue() {
+        List<String> availableTargetNames = Lists.newArrayList("t_order_0_0", "t_order_0_1", "t_order_1_0", "t_order_1_1");
+        Map<String, Collection<Comparable<?>>> sharingValueMap = new HashMap<>();
+        Map<String, Range<Comparable<?>>> rangeShardingValueMap = new HashMap<>();
+        rangeShardingValueMap.put("type", Range.all());
+        ComplexKeysShardingValue<Comparable<?>> shardingValue = new ComplexKeysShardingValue<>("t_order", sharingValueMap, rangeShardingValueMap);
+        Collection<String> actual = complexInlineShardingAlgorithmAllowRangeQuery.doSharding(availableTargetNames, shardingValue);
+        assertTrue(actual.containsAll(availableTargetNames));
+    }
+
+}
