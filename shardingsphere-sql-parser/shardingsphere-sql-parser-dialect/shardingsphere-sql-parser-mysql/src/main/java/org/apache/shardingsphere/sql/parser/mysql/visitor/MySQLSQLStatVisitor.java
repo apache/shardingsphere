50ccd683ed3a0e7e590bce625c13162a4c31a3d4
JingShang Lu <jingshanglu1@gmail.com>
2020-12-17 14:56:45 +0800

    fix

--- shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/MySQLSQLStatVisitor.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/MySQLSQLStatVisitor.java
@@ -19,47 +19,154 @@ package org.apache.shardingsphere.sql.parser.mysql.visitor;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementBaseVisitor;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AliasContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ColumnDefinitionContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ColumnRefContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.IdentifierContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.InsertContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.OwnerContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableFactorContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableNameContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableWildContext;
-import org.apache.shardingsphere.sql.parser.sql.common.util.SQLUtil;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.ColumnSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.AliasSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.OwnerSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.SimpleTableSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.TableNameSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
- * MySQL Format SQL visitor for MySQL.
+ * MySQL SQL Stats visitor for MySQL.
  */
 @Getter
 @Setter
-public final class MySQLSQLStatVisitor extends MySQLStatementBaseVisitor<Set<String>> {
+public final class MySQLSQLStatVisitor extends MySQLStatementBaseVisitor<Boolean> {
 
-    private Map<String, String> tables = new HashMap();
+    private Map<String, SimpleTableSegment> tables = new LinkedHashMap<>();
 
-    @Override
-    public Set<String> visitTableName(final TableNameContext ctx) {
-        String tableName = getTableNameFromIden(ctx.name().identifier());
-        tables.put(tableName, tableName);
-        return tables.keySet();
-    }
+    private Map<Integer, ColumnSegment> columns = new LinkedHashMap<>();
 
     @Override
-    public Set<String> visitTableWild(final TableWildContext ctx) {
-        return tables.keySet();
-    }
-
-    private String getTableNameFromIden(final IdentifierContext ctx) {
-        if (null != ctx.DOUBLE_QUOTED_TEXT() || null != ctx.IDENTIFIER_()) {
-            return SQLUtil.getExactlyValue(ctx.getText());
+    public Boolean visitTableFactor(final TableFactorContext ctx) {
+        if (null != ctx.tableName()) {
+            SimpleTableSegment tableSegment = getTableName(ctx.tableName());
+            if (null != ctx.alias()) {
+                tableSegment.setAlias(getAlias(ctx.alias()));
+            }
+            addTable(tableSegment);
+            return true;
         }
-        return ctx.getText();
+        return super.visitTableFactor(ctx);
     }
 
     @Override
-    public Set<String> visitTerminal(final TerminalNode node) {
-        return tables.keySet();
+    public Boolean visitInsert(final InsertContext ctx) {
+        SimpleTableSegment tableSegment = getTableName(ctx.tableName());
+        addTable(tableSegment);
+        if (null != ctx.insertValuesClause()) {
+            visit(ctx.insertValuesClause());
+        } else if (null != ctx.insertSelectClause()) {
+            visit(ctx.insertSelectClause());
+        } else {
+            visit(ctx.setAssignmentsClause());
+        }
+        return true;
+    }
+
+    @Override
+    public Boolean visitColumnRef(final ColumnRefContext ctx) {
+        ColumnSegment column = getColumn(ctx);
+        addColumn(column);
+        return true;
+    }
+
+    @Override
+    public Boolean visitColumnDefinition(final ColumnDefinitionContext ctx) {
+        ColumnSegment column = getColumn(ctx.column_name);
+        addColumn(column);
+        return true;
+    }
+
+    private ColumnSegment getColumn(ColumnRefContext ctx) {
+        IdentifierValue name;
+        OwnerSegment owner = null;
+        if (2 == ctx.identifier().size()) {
+            name = new IdentifierValue(ctx.identifier(1).getText());
+            owner = new OwnerSegment(ctx.identifier(0).start.getStartIndex(), ctx.identifier(0).stop.getStopIndex(), new IdentifierValue(ctx.identifier(0).getText()));
+        } else if (3 == ctx.identifier().size()) {
+            name = new IdentifierValue(ctx.identifier(2).getText());
+            owner = new OwnerSegment(ctx.identifier(1).start.getStartIndex(), ctx.identifier(1).stop.getStopIndex(), new IdentifierValue(ctx.identifier(1).getText()));
+        } else {
+            name = new IdentifierValue(ctx.identifier(0).getText());
+        }
+        ColumnSegment column = new ColumnSegment(ctx.start.getStartIndex(), ctx.stop.getStopIndex(), name);
+        column.setOwner(owner);
+        return column;
+    }
+
+    private ColumnSegment getColumn(IdentifierContext ctx) {
+        ColumnSegment column = new ColumnSegment(ctx.start.getStartIndex(), ctx.stop.getStopIndex(), new IdentifierValue(ctx.getText()));
+        return column;
+    }
+
+    private AliasSegment getAlias(final AliasContext ctx) {
+        return new AliasSegment(ctx.start.getStartIndex(), ctx.stop.getStopIndex(), new IdentifierValue(ctx.textOrIdentifier().getText()));
+    }
+
+    private SimpleTableSegment getTableName(final TableNameContext ctx) {
+        SimpleTableSegment result = new SimpleTableSegment(new TableNameSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), getTableFromIden(ctx.name().identifier())));
+        OwnerContext owner = ctx.owner();
+        if (null != owner) {
+            result.setOwner(new OwnerSegment(owner.getStart().getStartIndex(), owner.getStop().getStopIndex(), new IdentifierValue(owner.identifier().getText())));
+        }
+        return result;
+    }
+
+    @Override
+    public Boolean visitTableName(final TableNameContext ctx) {
+        SimpleTableSegment tableSegment = getTableName(ctx);
+        addTable(tableSegment);
+        return true;
+    }
+
+    @Override
+    public Boolean visitTableWild(final TableWildContext ctx) {
+        ColumnSegment column = new ColumnSegment(ctx.start.getStartIndex(), ctx.stop.getStopIndex(), new IdentifierValue("*"));
+        IdentifierContext owner = ctx.identifier().get(ctx.identifier().size() - 1);
+        column.setOwner(new OwnerSegment(owner.start.getStartIndex(), owner.stop.getStopIndex(), new IdentifierValue(owner.getText())));
+        addColumn(column);
+        return true;
+    }
+
+    private int hashCode(ColumnSegment column) {
+        StringBuilder columString = new StringBuilder();
+        if (column.getOwner().isPresent()) {
+            columString.append(column.getOwner().get().getIdentifier().getValue());
+        }
+        columString.append(column.getIdentifier().getValue());
+        return columString.toString().hashCode();
+    }
+
+    private Boolean addTable(final SimpleTableSegment tableSegment) {
+        if (!tables.containsKey(tableSegment.getTableName().getIdentifier().getValue())) {
+            tables.put(tableSegment.getTableName().getIdentifier().getValue(), tableSegment);
+        }
+        return true;
+    }
+
+    private Boolean addColumn(final ColumnSegment column) {
+        int columnHashcode = hashCode(column);
+        if (!columns.containsKey(columnHashcode)) {
+            columns.put(columnHashcode, column);
+        }
+        return true;
+    }
+
+    private IdentifierValue getTableFromIden(final IdentifierContext ctx) {
+        return new IdentifierValue(ctx.getText());
     }
 }
--- shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/test/java/org/apache/shardingsphere/sql/parser/mysql/MySQLTableVisterTest.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/test/java/org/apache/shardingsphere/sql/parser/mysql/MySQLTableVisterTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.sql.parser.mysql;
 
 import org.antlr.v4.runtime.CodePointBuffer;
@@ -15,101 +32,59 @@ import org.junit.runners.Parameterized;
 import java.nio.CharBuffer;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Set;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
-public class MySQLTableVisterTest {
-    private static Collection<String[]> testUnits = new LinkedList();
+public final class MySQLTableVisterTest {
+    private static Collection<Object[]> testUnits = new LinkedList();
 
     static {
-        testUnits.add(new String[]{"select_with_union", "select a+1 as b, name n from table1 join table2 where id=1 and name='lu';", "SELECT a + 1 AS b, name n\n"
-                + "FROM table1 JOIN table2\n"
-                + "WHERE \n"
-                + "\tid = 1\n"
-                + "\tand name = 'lu';"});
-        testUnits.add(new String[]{"select_item_nums", "select id, name, age, sex, ss, yy from table1 where id=1", "SELECT id , name , age , \n"
-                + "\tsex , ss , yy \n"
-                + "FROM table1\n"
-                + "WHERE \n"
-                + "\tid = 1;"});
-        testUnits.add(new String[]{"select_with_subquery", "select id, name, age, count(*) as n, (select id, name, age, sex from table2 where id=2) as sid, yyyy from table1 where id=1", "SELECT id ,"
-                + " name , age , \n"
-                + "\tCOUNT ( * ) AS n, \n"
-                + "\t(\n"
-                + "\t\tSELECT id , name , age , \n"
-                + "\t\t\tsex \n"
-                + "\t\tFROM table2\n"
-                + "\t\tWHERE \n"
-                + "\t\t\tid = 2\n"
-                + "\t) AS sid, yyyy \n"
-                + "FROM table1\n"
-                + "WHERE \n"
-                + "\tid = 1;"});
-        testUnits.add(new String[]{"select_where_num", "select id, name, age, sex, ss, yy from table1 where id=1 and name=1 and a=1 and b=2 and c=4 and d=3", "SELECT id , name , age , \n"
-                + "\tsex , ss , yy \n"
-                + "FROM table1\n"
-                + "WHERE \n"
-                + "\tid = 1\n"
-                + "\tand name = 1\n"
-                + "\tand a = 1\n"
-                + "\tand b = 2\n"
-                + "\tand c = 4\n"
-                + "\tand d = 3;"});
-        testUnits.add(new String[]{"alter_table", "ALTER TABLE t_order ADD column4 DATE, ADD column5 DATETIME, engine ss max_rows 10,min_rows 2, ADD column6 TIMESTAMP, ADD column7 TIME;", ""
-                + "ALTER TABLE t_order\n"
-                + "\tADD column4 DATE,\n"
-                + "\tADD column5 DATETIME,\n"
-                + "\tENGINE ss\n"
-                + "\tMAX_ROWS 10,\n"
-                + "\tMIN_ROWS 2,\n"
-                + "\tADD column6 TIMESTAMP,\n"
-                + "\tADD column7 TIME"});
-        testUnits.add(new String[]{"create_table", "CREATE TABLE IF NOT EXISTS `runoob_tbl`(\n"
+        testUnits.add(new Object[]{"select_with_union", "select a+1 as b, name n from table1 join table2 where id=1 and name='lu';", 2, 3});
+        testUnits.add(new Object[]{"select_item_nums", "select id, name, age, sex, ss, yy from table1 where id=1", 1, 6});
+        testUnits.add(new Object[]{"select_with_subquery", "select id, name, age, count(*) as n, (select id, name, age, sex from table2 where id=2) as sid, yyyy from table1 where id=1", 2, 5});
+        testUnits.add(new Object[]{"select_where_num", "select id, name, age, sex, ss, yy from table1 where id=1 and name=1 and a=1 and b=2 and c=4 and d=3", 1, 10});
+        testUnits.add(new Object[]{"alter_table", "ALTER TABLE t_order ADD column4 DATE, ADD column5 DATETIME, engine ss max_rows 10,min_rows 2, ADD column6 TIMESTAMP, ADD column7 TIME;", 1, 4});
+        testUnits.add(new Object[]{"create_table", "CREATE TABLE IF NOT EXISTS `runoob_tbl`(\n"
                 + "`runoob_id` INT UNSIGNED AUTO_INCREMENT,\n"
                 + "`runoob_title` VARCHAR(100) NOT NULL,\n"
                 + "`runoob_author` VARCHAR(40) NOT NULL,\n"
                 + "`runoob_test` NATIONAL CHAR(40),\n"
                 + "`submission_date` DATE,\n"
                 + "PRIMARY KEY ( `runoob_id` )\n"
-                + ")ENGINE=InnoDB DEFAULT CHARSET=utf8;", "CREATE TABLE IF NOT EXISTS `runoob_tbl` (\n"
-                + "\t`runoob_id` INT UNSIGNED AUTO_INCREMENT,\n"
-                + "\t`runoob_title` VARCHAR(100) NOT NULL,\n"
-                + "\t`runoob_author` VARCHAR(40) NOT NULL,\n"
-                + "\t`runoob_test` NATIONAL CHAR(40),\n"
-                + "\t`submission_date` DATE,\n"
-                + "\tPRIMARY KEY ( `runoob_id` )\n"
-                + ") ENGINE = InnoDB DEFAULT CHARSET = utf8"});
+                + ")ENGINE=InnoDB DEFAULT CHARSET=utf8;", 1, 5});
     }
 
     private final String caseId;
 
     private final String inputSql;
 
-    private final String expectFormartedSql;
+    private final int tableNum;
 
-    public MySQLTableVisterTest(final String caseId, final String inputSql, final String expectFormartedSql) {
+    private final int columnNum;
+
+    public MySQLTableVisterTest(final String caseId, final String inputSql, final int tableNum, final int columnNum) {
         this.caseId = caseId;
         this.inputSql = inputSql;
-        this.expectFormartedSql = expectFormartedSql;
+        this.tableNum = tableNum;
+        this.columnNum = columnNum;
     }
 
     @Parameterized.Parameters(name = "{0}")
-    public static Collection<String[]> getTestParameters() {
+    public static Collection<Object[]> getTestParameters() {
         return testUnits;
     }
 
     @Test
-    public void assertSqlFormat() {
+    public void assertSqlStats() {
         CodePointBuffer buffer = CodePointBuffer.withChars(CharBuffer.wrap(inputSql.toCharArray()));
         MySQLLexer lexer = new MySQLLexer(CodePointCharStream.fromBuffer(buffer));
         MySQLParser parser = new MySQLParser(new CommonTokenStream(lexer));
         ParseTree tree = ((ParseASTNode) parser.parse()).getRootNode();
-//        MySQLFormatSQLVisitor visitor = new MySQLDMLFormatSQLVisitor();
         MySQLSQLStatVisitor visitor = new MySQLSQLStatVisitor();
-        Set<String> result = visitor.visit(tree);
-
-        assertTrue("SQL format error", expectFormartedSql.equals(result));
+        visitor.visit(tree);
+        assertThat("table assert error", visitor.getTables().keySet().size(), is(tableNum));
+        assertThat("column assert error", visitor.getColumns().keySet().size(), is(columnNum));
     }
 }
