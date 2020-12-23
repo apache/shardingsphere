ef9132056d36dcfcead6305ffc5a9077316fc6b5
JingShang Lu <jingshanglu1@gmail.com>
2020-12-23 14:36:02 +0800

    fix

--- shardingsphere-distsql-parser/shardingsphere-distsql-parser-statement/src/main/java/org/apache/shardingsphere/distsql/parser/segment/rdl/ReplicaQueryRuleSegment.java
+++ shardingsphere-distsql-parser/shardingsphere-distsql-parser-statement/src/main/java/org/apache/shardingsphere/distsql/parser/segment/rdl/ReplicaQueryRuleSegment.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.distsql.parser.segment.rdl;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.apache.shardingsphere.sql.parser.api.visitor.ASTNode;
 
