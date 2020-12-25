86ac8205950143778989e9ae2cc44456ca52c7b2
xiaoyu <549477611@qq.com>
2020-12-25 15:43:23 +0800

    agent add logging plugin (#8776)

---
+++ shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-logging/shardingsphere-agent-logging-base/src/main/java/org/apache/shardingsphere/agent/plugin/logging/base/advice/SchemaMetaDataLoaderAdvice.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.agent.plugin.logging.base.advice;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.agent.core.plugin.advice.MethodAroundAdvice;
+import org.apache.shardingsphere.agent.core.plugin.advice.MethodInvocationResult;
+import org.apache.shardingsphere.agent.core.plugin.advice.TargetObject;
+
+/**
+ * Schema meta data loader advice.
+ */
+@Slf4j
+public final class SchemaMetaDataLoaderAdvice implements MethodAroundAdvice {
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public void afterMethod(final TargetObject target, final Method method, final Object[] args, final MethodInvocationResult result) {
+        Collection<String> results = (Collection<String>) result.getResult();
+        log.info("Loading {} tables' meta data for unconfigured tables.", results.size());
+    }
+}
