6240ea7852cb2ab1849a4f6ab7fdf023c9176419
terrymanu <terrymanu@163.com>
2020-12-29 20:07:41 +0800

    Revise javadoc

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/env/datasource/builder/JdbcDataSourceBuilder.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/env/datasource/builder/JdbcDataSourceBuilder.java
@@ -43,7 +43,7 @@ public final class JdbcDataSourceBuilder {
     private static final Map<DataSourceCacheKey, DataSource> CACHE = new HashMap<>();
     
     /**
-     * Build data source.
+     * Build jdbc data source.
      *
      * @param name data source name
      * @param databaseType database type
