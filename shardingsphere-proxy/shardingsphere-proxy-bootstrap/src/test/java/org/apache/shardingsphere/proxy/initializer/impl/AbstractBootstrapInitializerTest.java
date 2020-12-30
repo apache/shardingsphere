c044cecbef9e6b77617bee88e126f00f424a5815
Haoran Meng <menghaoranss@gmail.com>
2020-12-30 22:18:57 +0800

    Init scaling worker in proxy (#8834)

--- shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/initializer/impl/AbstractBootstrapInitializerTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/initializer/impl/AbstractBootstrapInitializerTest.java
@@ -24,6 +24,7 @@ import org.apache.shardingsphere.infra.config.properties.ConfigurationPropertyKe
 import org.apache.shardingsphere.infra.context.metadata.MetaDataContexts;
 import org.apache.shardingsphere.proxy.config.ProxyConfiguration;
 import org.apache.shardingsphere.proxy.config.YamlProxyConfiguration;
+import org.apache.shardingsphere.proxy.config.yaml.YamlProxyServerConfiguration;
 import org.apache.shardingsphere.proxy.frontend.ShardingSphereProxy;
 import org.apache.shardingsphere.transaction.context.TransactionContexts;
 import org.junit.Before;
@@ -84,6 +85,7 @@ public abstract class AbstractBootstrapInitializerTest {
         doReturn(metaDataContexts).when(abstractBootstrapInitializer).decorateMetaDataContexts(any());
         doReturn(mock(TransactionContexts.class)).when(abstractBootstrapInitializer).decorateTransactionContexts(any(), any());
         YamlProxyConfiguration yamlConfig = mock(YamlProxyConfiguration.class);
+        when(yamlConfig.getServerConfiguration()).thenReturn(mock(YamlProxyServerConfiguration.class));
         abstractBootstrapInitializer.init(yamlConfig, eq(anyInt()));
         verify(shardingSphereProxy).start(anyInt());
     }
