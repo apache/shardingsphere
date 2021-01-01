88be24dbd60d47320d3887bf0f50c0df13f52cbc
terrymanu <terrymanu@163.com>
2021-01-01 15:01:32 +0800

    Move IntegrateTestCaseAssertionInitialSQL

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/cases/assertion/root/IntegrateTestCaseAssertion.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/cases/assertion/root/IntegrateTestCaseAssertion.java
@@ -20,7 +20,6 @@ package org.apache.shardingsphere.test.integration.cases.assertion.root;
 import com.google.common.base.Splitter;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.shardingsphere.test.integration.cases.assertion.ddl.IntegrateTestCaseAssertionInitialSQL;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/cases/assertion/root/IntegrateTestCaseAssertionInitialSQL.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-test-suite/src/test/java/org/apache/shardingsphere/test/integration/cases/assertion/root/IntegrateTestCaseAssertionInitialSQL.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.test.integration.cases.assertion.ddl;
+package org.apache.shardingsphere.test.integration.cases.assertion.root;
 
 import lombok.Getter;
 import lombok.Setter;
