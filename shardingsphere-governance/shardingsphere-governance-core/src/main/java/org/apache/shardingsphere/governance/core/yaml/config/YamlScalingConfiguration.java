9594ddd14cfd888f444915181213ca4f7448b26b
menghaoranss <localcc@163.com>
2021-01-06 16:09:36 +0800

    Add yaml scaling configuration for proxy

--- shardingsphere-governance/shardingsphere-governance-core/src/main/java/org/apache/shardingsphere/governance/core/yaml/config/YamlScalingConfiguration.java
+++ shardingsphere-governance/shardingsphere-governance-core/src/main/java/org/apache/shardingsphere/governance/core/yaml/config/YamlScalingConfiguration.java
@@ -15,29 +15,20 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.proxy.config.yaml;
+package org.apache.shardingsphere.governance.core.yaml.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.shardingsphere.governance.core.yaml.config.YamlGovernanceConfiguration;
-import org.apache.shardingsphere.governance.core.yaml.config.YamlScalingConfiguration;
-import org.apache.shardingsphere.infra.auth.builtin.yaml.config.YamlAuthenticationConfiguration;
 import org.apache.shardingsphere.infra.yaml.config.YamlConfiguration;
 
-import java.util.Properties;
-
 /**
- * Server configuration for YAML.
+ * Scaling configuration for Yaml.
  */
 @Getter
 @Setter
-public final class YamlProxyServerConfiguration implements YamlConfiguration {
+public final class YamlScalingConfiguration implements YamlConfiguration {
     
-    private YamlAuthenticationConfiguration authentication;
+    private int blockQueueSize;
     
-    private YamlGovernanceConfiguration governance;
-    
-    private YamlScalingConfiguration scaling;
-    
-    private Properties props = new Properties();
+    private int workerThread;
 }
