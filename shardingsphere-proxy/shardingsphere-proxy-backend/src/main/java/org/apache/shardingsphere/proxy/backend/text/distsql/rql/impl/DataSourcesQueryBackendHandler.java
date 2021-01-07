39fcbfbdaacfa16aa65aa65778c0a62066588cbc
JingShang Lu <jingshanglu1@gmail.com>
2021-01-07 17:46:15 +0800

    format the result of 'show resources'

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/distsql/rql/impl/DataSourcesQueryBackendHandler.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/distsql/rql/impl/DataSourcesQueryBackendHandler.java
@@ -17,10 +17,11 @@
 
 package org.apache.shardingsphere.proxy.backend.text.distsql.rql.impl;
 
+import com.google.gson.Gson;
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.ShowResourcesStatement;
 import org.apache.shardingsphere.infra.config.datasource.DataSourceConverter;
 import org.apache.shardingsphere.infra.config.datasource.DataSourceParameter;
-import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
+import org.apache.shardingsphere.infra.database.metadata.DataSourceMetaData;
 import org.apache.shardingsphere.proxy.backend.communication.jdbc.connection.BackendConnection;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.proxy.backend.response.header.ResponseHeader;
@@ -32,40 +33,73 @@ import org.apache.shardingsphere.proxy.config.util.DataSourceParameterConverter;
 import java.sql.Types;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Backend handler for show resources.
  */
 public final class DataSourcesQueryBackendHandler extends SchemaRequiredBackendHandler<ShowResourcesStatement> {
-    
+
     private Map<String, DataSourceParameter> dataSourceParameterMap;
-    
+
+    private String schemaName;
+
     private Iterator<String> dataSourceNames;
-    
+
     public DataSourcesQueryBackendHandler(final ShowResourcesStatement sqlStatement, final BackendConnection backendConnection) {
         super(sqlStatement, backendConnection);
     }
     
     @Override
     public ResponseHeader execute(final String schemaName, final ShowResourcesStatement sqlStatement) {
-        QueryHeader nameQueryHeader = new QueryHeader(schemaName, "", "name", "name", Types.CHAR, "CHAR", 255, 0, false, false, false, false);
-        QueryHeader contentQueryHeader = new QueryHeader(schemaName, "", "data source", "data source", Types.CHAR, "CHAR", 255, 0, false, false, false, false);
+        if (sqlStatement.getSchema().isPresent()) {
+            this.schemaName = sqlStatement.getSchema().get().getIdentifier().getValue();
+        } else {
+            this.schemaName = schemaName;
+        }
+
         dataSourceParameterMap = DataSourceParameterConverter.getDataSourceParameterMap(
-                DataSourceConverter.getDataSourceConfigurationMap(ProxyContext.getInstance().getMetaData(schemaName).getResource().getDataSources()));
+                DataSourceConverter.getDataSourceConfigurationMap(ProxyContext.getInstance().getMetaData(this.schemaName).getResource().getDataSources()));
         dataSourceNames = dataSourceParameterMap.keySet().iterator();
-        return new QueryResponseHeader(Arrays.asList(nameQueryHeader, contentQueryHeader));
+        return new QueryResponseHeader(generateResponseHeader());
+    }
+
+    private List<QueryHeader> generateResponseHeader() {
+        List<QueryHeader> result = new LinkedList();
+        result.add(new QueryHeader(schemaName, "", "name", "name", Types.CHAR, "CHAR", 255, 0, false, false, false, false));
+        result.add(new QueryHeader(schemaName, "", "type", "type", Types.CHAR, "CHAR", 255, 0, false, false, false, false));
+        result.add(new QueryHeader(schemaName, "", "host", "host", Types.CHAR, "CHAR", 255, 0, false, false, false, false));
+        result.add(new QueryHeader(schemaName, "", "port", "port", Types.BIGINT, "BIGINT", 255, 0, false, false, false, false));
+        result.add(new QueryHeader(schemaName, "", "db", "db", Types.CHAR, "CHAR", 255, 0, false, false, false, false));
+        result.add(new QueryHeader(schemaName, "", "attribute", "attribute", Types.CHAR, "CHAR", 255, 0, false, false, false, false));
+        return result;
     }
     
     @Override
     public boolean next() {
         return dataSourceNames.hasNext();
     }
-    
+
     @Override
     public Collection<Object> getRowData() {
         String dataSourceName = dataSourceNames.next();
-        return Arrays.asList(dataSourceName, YamlEngine.marshal(dataSourceParameterMap.get(dataSourceName)));
+        DataSourceMetaData dataSourceMetaData = ProxyContext.getInstance().getMetaData(schemaName).getResource().getDataSourcesMetaData().getDataSourceMetaData(dataSourceName);
+        Map<Object, Object> attributeMap = new HashMap();
+        attributeMap.put("connectionTimeoutMilliseconds", dataSourceParameterMap.get(dataSourceName).getConnectionTimeoutMilliseconds());
+        attributeMap.put("idleTimeoutMilliseconds", dataSourceParameterMap.get(dataSourceName).getIdleTimeoutMilliseconds());
+        attributeMap.put("maxLifetimeMilliseconds", dataSourceParameterMap.get(dataSourceName).getMaxLifetimeMilliseconds());
+        attributeMap.put("maxPoolSize", dataSourceParameterMap.get(dataSourceName).getMaxPoolSize());
+        attributeMap.put("minPoolSize", dataSourceParameterMap.get(dataSourceName).getMinPoolSize());
+        attributeMap.put("maintenanceIntervalMilliseconds", dataSourceParameterMap.get(dataSourceName).getMaintenanceIntervalMilliseconds());
+        attributeMap.put("readOnly", dataSourceParameterMap.get(dataSourceName).isReadOnly());
+        String type = ProxyContext.getInstance().getMetaData(schemaName).getResource().getDatabaseType().getName();
+        String host = dataSourceMetaData.getHostName();
+        int port = dataSourceMetaData.getPort();
+        String db = dataSourceMetaData.getCatalog();
+        return Arrays.asList(dataSourceName, type, host, port, db, (new Gson()).toJson(attributeMap));
     }
 }
