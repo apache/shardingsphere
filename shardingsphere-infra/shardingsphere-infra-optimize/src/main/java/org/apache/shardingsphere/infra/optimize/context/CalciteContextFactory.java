064e4867c8fb55da9761dc9c9784e51452c9046d
Jieker <jieker_mail@163.com>
2021-01-07 22:10:53 +0800

    Support different Database types for Query optimization and fixed some bugs (#8918)
    * Support different Database types for Query optimization and fix some bugs
    
    * Support different Database types for Query optimization and fix some bugs
    
    * fix some problem
    
    * fixed some related problems
    
    * fixed some related problems
    
    * fixed some related problems
    
    * maked more clear
    
    * fixed some problems
    
    * fixed some problems
    
    * fixed some problems

--- shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/context/CalciteContextFactory.java
+++ shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/context/CalciteContextFactory.java
@@ -39,6 +39,15 @@ import org.apache.calcite.sql.validate.SqlValidator;
 import org.apache.calcite.sql.validate.SqlValidatorUtil;
 import org.apache.calcite.sql2rel.SqlToRelConverter;
 import org.apache.calcite.sql2rel.StandardConvertletTable;
+import org.apache.shardingsphere.infra.database.type.DatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.H2DatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.MariaDBDatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.MySQLDatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.OracleDatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.PostgreSQLDatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.SQL92DatabaseType;
+import org.apache.shardingsphere.infra.database.type.dialect.SQLServerDatabaseType;
+import org.apache.shardingsphere.infra.exception.ShardingSphereException;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
 import org.apache.shardingsphere.infra.optimize.execute.raw.plan.PlannerInitializer;
 import org.apache.shardingsphere.infra.optimize.schema.CalciteLogicSchema;
@@ -53,8 +62,12 @@ import java.util.Properties;
  * Calcite context factory.
  */
 public final class CalciteContextFactory {
-
-    private final Properties properties;
+    
+    private static final String LEX_CAMEL_NAME = CalciteConnectionProperty.LEX.camelName();
+    
+    private static final String CONFORMANCE_CAMEL_NAME = CalciteConnectionProperty.CONFORMANCE.camelName();
+    
+    private final Properties properties = new Properties();
     
     private final CalciteConnectionConfig connectionConfig;
     
@@ -67,24 +80,60 @@ public final class CalciteContextFactory {
     private final RelOptCluster cluster;
     
     public CalciteContextFactory(final Map<String, ShardingSphereMetaData> metaDataMap) {
-        properties = createProperties();
+        DatabaseType databaseType = metaDataMap.isEmpty() ? null : metaDataMap.values().iterator().next().getResource().getDatabaseType();
+        initProperties(databaseType);
+        typeFactory = new JavaTypeFactoryImpl();
+        cluster = newCluster();
+        factory = new CalciteLogicSchemaFactory(metaDataMap);
         connectionConfig = new CalciteConnectionConfigImpl(properties);
         parserConfig = SqlParser.config()
                 .withLex(connectionConfig.lex())
                 .withIdentifierMaxLength(SqlParser.DEFAULT_IDENTIFIER_MAX_LENGTH)
                 .withConformance(connectionConfig.conformance())
                 .withParserFactory(SqlParserImpl.FACTORY);
-        typeFactory = new JavaTypeFactoryImpl();
-        factory = new CalciteLogicSchemaFactory(metaDataMap);
-        cluster = newCluster();
     }
     
-    private Properties createProperties() {
-        // TODO Not only MySQL here.
-        Properties result = new Properties();
-        result.setProperty(CalciteConnectionProperty.LEX.camelName(), Lex.MYSQL.name());
-        result.setProperty(CalciteConnectionProperty.CONFORMANCE.camelName(), SqlConformanceEnum.MYSQL_5.name());
-        return result;
+    private void initProperties(final DatabaseType databaseType) {
+        // TODO Logic could be improved.
+        if (databaseType instanceof MySQLDatabaseType || databaseType == null) {
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.MYSQL.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.MYSQL_5.name());
+            return;
+        }
+        if (databaseType instanceof H2DatabaseType) {
+            // TODO No suitable type of Lex
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.MYSQL.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.DEFAULT.name());
+            return;
+        }
+        if (databaseType instanceof MariaDBDatabaseType) {
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.MYSQL.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.MYSQL_5.name());
+            return;
+        }
+        if (databaseType instanceof OracleDatabaseType) {
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.ORACLE.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.ORACLE_12.name());
+            return;
+        }
+        if (databaseType instanceof PostgreSQLDatabaseType) {
+            // TODO No suitable type of Lex and conformance
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.MYSQL.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.DEFAULT.name());
+            return;
+        }
+        if (databaseType instanceof SQL92DatabaseType) {
+            // TODO No suitable type of Lex
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.MYSQL.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.STRICT_92.name());
+            return;
+        }
+        if (databaseType instanceof SQLServerDatabaseType) {
+            this.properties.setProperty(LEX_CAMEL_NAME, Lex.SQL_SERVER.name());
+            this.properties.setProperty(CONFORMANCE_CAMEL_NAME, SqlConformanceEnum.SQL_SERVER_2008.name());
+            return;
+        }
+        throw new ShardingSphereException("No matching DatabaseType found");
     }
     
     private RelOptCluster newCluster() {
@@ -101,23 +150,20 @@ public final class CalciteContextFactory {
      * @return calcite context
      */
     public CalciteContext create(final String schema, final CalciteRowExecutor executor) {
-        return create(connectionConfig, parserConfig, typeFactory, cluster, factory.create(schema, executor));
-    }
-
-    private CalciteContext create(final CalciteConnectionConfig config,
-                                  final SqlParser.Config parserConfig, final RelDataTypeFactory typeFactory, final RelOptCluster cluster, final CalciteLogicSchema calciteLogicSchema) {
-        CalciteCatalogReader catalogReader = createCalciteCatalogReader(config, typeFactory, calciteLogicSchema);
-        SqlValidator validator = createSqlValidator(config, typeFactory, catalogReader);
+        CalciteLogicSchema calciteLogicSchema = factory.create(schema, executor);
+        CalciteCatalogReader catalogReader = createCalciteCatalogReader(schema, connectionConfig, typeFactory, calciteLogicSchema);
+        SqlValidator validator = createSqlValidator(connectionConfig, typeFactory, catalogReader);
         SqlToRelConverter relConverter = createSqlToRelConverter(cluster, validator, catalogReader);
         return new CalciteContext(properties, calciteLogicSchema, parserConfig, validator, relConverter);
     }
-
-    private CalciteCatalogReader createCalciteCatalogReader(final CalciteConnectionConfig config, final RelDataTypeFactory typeFactory, final CalciteLogicSchema calciteLogicSchema) {
+    
+    private CalciteCatalogReader createCalciteCatalogReader(final String schema, final CalciteConnectionConfig config,
+                                                            final RelDataTypeFactory typeFactory, final CalciteLogicSchema calciteLogicSchema) {
         CalciteSchema rootSchema = CalciteSchema.createRootSchema(true);
         rootSchema.add(calciteLogicSchema.getName(), calciteLogicSchema);
-        return new CalciteCatalogReader(rootSchema, Collections.singletonList(config.schema()), typeFactory, config);
+        return new CalciteCatalogReader(rootSchema, Collections.singletonList(schema), typeFactory, config);
     }
-
+    
     private SqlValidator createSqlValidator(final CalciteConnectionConfig config, final RelDataTypeFactory typeFactory, final CalciteCatalogReader catalogReader) {
         return SqlValidatorUtil.newValidator(SqlStdOperatorTable.instance(), catalogReader, typeFactory, SqlValidator.Config.DEFAULT
                 .withLenientOperatorLookup(config.lenientOperatorLookup())
@@ -125,7 +171,7 @@ public final class CalciteContextFactory {
                 .withDefaultNullCollation(config.defaultNullCollation())
                 .withIdentifierExpansion(true));
     }
-
+    
     private SqlToRelConverter createSqlToRelConverter(final RelOptCluster cluster, final SqlValidator validator, final CalciteCatalogReader catalogReader) {
         SqlToRelConverter.Config config = SqlToRelConverter.config().withTrimUnusedFields(true);
         RelOptTable.ViewExpander expander = (rowType, queryString, schemaPath, viewPath) -> null;
