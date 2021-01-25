145528dd14dce29d5c1a798952e9b00726896fd3
lmh <lmhlmhtju@163.com>
2021-01-25 13:40:47 +0800

    Test Calcite doing query across single and sharding tables.

--- shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/statement/CalcitePrepareStatementTest.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/statement/CalcitePrepareStatementTest.java
@@ -30,12 +30,29 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public final class CalcitePrepareStatementTest extends AbstractShardingSphereDataSourceForCalciteTest {
-    
-    private static final String SELECT_SQL_BY_ID = "select o.*, i.* from t_order_calcite o, t_order_item_calcite i where o.order_id = ? and i.item_id = ?";
-    
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_TABLES = "select o.*, i.* from t_order_calcite o, t_order_item_calcite i where o.order_id = ? and i.item_id = ?";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES =
+            "select t_order_calcite.*, t_order_item_calcite_sharding.* "
+                    + "from t_order_calcite, t_order_item_calcite_sharding "
+                    + "where t_order_calcite.order_id = t_order_item_calcite_sharding.item_id "
+                    + "AND t_order_item_calcite_sharding.order_id = ?";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ALIAS =
+            "select o.*, i.* from t_order_calcite o, t_order_item_calcite_sharding i "
+                    + "where o.order_id = i.item_id AND i.order_id = ?";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_REWRITE =
+            "select t_order_calcite.*, t_order_item_calcite_sharding.* "
+                    + "from t_order_calcite, t_order_item_calcite_sharding "
+                    + "where t_order_calcite.order_id = t_order_item_calcite_sharding.item_id "
+                    + "AND t_order_item_calcite_sharding.remarks = 't_order_item_calcite_sharding' "
+                    + "AND t_order_item_calcite_sharding.user_id = ?";
+
     @Test
     public void assertQueryWithCalciteInSingleTables() throws SQLException {
-        ShardingSpherePreparedStatement preparedStatement = (ShardingSpherePreparedStatement) getShardingSphereDataSource().getConnection().prepareStatement(SELECT_SQL_BY_ID);
+        ShardingSpherePreparedStatement preparedStatement = (ShardingSpherePreparedStatement) getShardingSphereDataSource().getConnection().prepareStatement(SELECT_SQL_BY_ID_ACROSS_SINGLE_TABLES);
         preparedStatement.setInt(1, 1000);
         preparedStatement.setInt(2, 100000);
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -49,5 +66,73 @@ public final class CalcitePrepareStatementTest extends AbstractShardingSphereDat
         assertThat(resultSet.getInt(6), is(10));
         assertThat(resultSet.getString(7), is("init"));
         assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTable() throws SQLException {
+        ShardingSpherePreparedStatement preparedStatement = (ShardingSpherePreparedStatement) getShardingSphereDataSource()
+                .getConnection().prepareStatement(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES);
+        preparedStatement.setInt(1, 10001);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTableWithAlias() throws SQLException {
+        ShardingSpherePreparedStatement preparedStatement = (ShardingSpherePreparedStatement) getShardingSphereDataSource()
+                .getConnection().prepareStatement(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ALIAS);
+        preparedStatement.setInt(1, 10001);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTableRewrite() throws SQLException {
+        ShardingSpherePreparedStatement preparedStatement = (ShardingSpherePreparedStatement) getShardingSphereDataSource()
+                .getConnection().prepareStatement(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_REWRITE);
+        preparedStatement.setInt(1, 11);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(8), is("t_order_item_calcite_sharding"));
+        assertFalse(resultSet.next());
+        preparedStatement.setInt(1, 10);
+        ResultSet resultSet1 = preparedStatement.executeQuery();
+        assertNotNull(resultSet1);
+        assertTrue(resultSet1.next());
+        assertThat(resultSet1.getInt(1), is(1000));
+        assertThat(resultSet1.getInt(2), is(10));
+        assertThat(resultSet1.getString(3), is("init"));
+        assertThat(resultSet1.getInt(4), is(1000));
+        assertThat(resultSet1.getInt(5), is(10000));
+        assertThat(resultSet1.getInt(6), is(10));
+        assertThat(resultSet1.getString(7), is("init"));
+        assertThat(resultSet1.getString(8), is("t_order_item_calcite_sharding"));
+        assertFalse(resultSet1.next());
     }
 }
--- shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/statement/CalciteStatementTest.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/statement/CalciteStatementTest.java
@@ -30,13 +30,33 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public final class CalciteStatementTest extends AbstractShardingSphereDataSourceForCalciteTest {
-    
-    private static final String SELECT_SQL_BY_ID = "select o.*, i.* from t_order_calcite o, t_order_item_calcite i where o.order_id = 1000 and i.item_id = 100000";
-    
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_TABLES = "select o.*, i.* from t_order_calcite o, t_order_item_calcite i "
+            + "where o.order_id = 1000 and i.item_id = 100000";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES =
+            "select t_order_calcite.*, t_order_item_calcite_sharding.* from t_order_calcite, "
+                    + "t_order_item_calcite_sharding where t_order_calcite.order_id = "
+                    + "t_order_item_calcite_sharding.item_id";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ALIAS = "select o.*, i.* from"
+            + " t_order_calcite o, t_order_item_calcite_sharding i where o.order_id = i.item_id";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_REWRITE =
+            "select t_order_calcite.*, t_order_item_calcite_sharding.* from "
+                    + "t_order_calcite, t_order_item_calcite_sharding "
+                    + "where t_order_calcite.order_id = t_order_item_calcite_sharding.item_id "
+                    + "AND t_order_item_calcite_sharding.remarks = 't_order_item_calcite_sharding'";
+
+    private static final String SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ORDERBY =
+            "select t_order_calcite.* from t_order_calcite, t_order_item_calcite_sharding "
+                    + "where t_order_calcite.order_id = t_order_item_calcite_sharding.item_id "
+                    + "ORDER BY t_order_item_calcite_sharding.user_id";
+
     @Test
     public void assertQueryWithCalciteInSingleTables() throws SQLException {
         ShardingSphereStatement preparedStatement = (ShardingSphereStatement) getShardingSphereDataSource().getConnection().createStatement();
-        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID);
+        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID_ACROSS_SINGLE_TABLES);
         assertNotNull(resultSet);
         assertTrue(resultSet.next());
         assertThat(resultSet.getInt(1), is(1000));
@@ -46,6 +66,99 @@ public final class CalciteStatementTest extends AbstractShardingSphereDataSource
         assertThat(resultSet.getInt(5), is(1000));
         assertThat(resultSet.getInt(6), is(10));
         assertThat(resultSet.getString(7), is("init"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTable() throws SQLException {
+        ShardingSphereStatement preparedStatement = (ShardingSphereStatement) getShardingSphereDataSource().getConnection().createStatement();
+        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES);
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1000));
+        assertThat(resultSet.getInt(2), is(10));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1000));
+        assertThat(resultSet.getInt(5), is(10000));
+        assertThat(resultSet.getInt(6), is(10));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(7), is("init"));
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTableWithAlias() throws SQLException {
+        ShardingSphereStatement preparedStatement = (ShardingSphereStatement) getShardingSphereDataSource().getConnection().createStatement();
+        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ALIAS);
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1000));
+        assertThat(resultSet.getInt(2), is(10));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1000));
+        assertThat(resultSet.getInt(5), is(10000));
+        assertThat(resultSet.getInt(6), is(10));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(8), is("t_order_item_calcite_sharding"));
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(8), is("t_order_item_calcite_sharding"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTableRewrite() throws SQLException {
+        ShardingSphereStatement preparedStatement = (ShardingSphereStatement) getShardingSphereDataSource().getConnection().createStatement();
+        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_REWRITE);
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1000));
+        assertThat(resultSet.getInt(2), is(10));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1000));
+        assertThat(resultSet.getInt(5), is(10000));
+        assertThat(resultSet.getInt(6), is(10));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(8), is("t_order_item_calcite_sharding"));
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
+        assertThat(resultSet.getInt(4), is(1001));
+        assertThat(resultSet.getInt(5), is(10001));
+        assertThat(resultSet.getInt(6), is(11));
+        assertThat(resultSet.getString(7), is("init"));
+        assertThat(resultSet.getString(8), is("t_order_item_calcite_sharding"));
+        assertFalse(resultSet.next());
+    }
+
+    @Test
+    public void assertQueryWithCalciteInSingleAndShardingTableOrderBy() throws SQLException {
+        ShardingSphereStatement preparedStatement = (ShardingSphereStatement) getShardingSphereDataSource().getConnection().createStatement();
+        ResultSet resultSet = preparedStatement.executeQuery(SELECT_SQL_BY_ID_ACROSS_SINGLE_AND_SHARDING_TABLES_ORDERBY);
+        assertNotNull(resultSet);
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1000));
+        assertThat(resultSet.getInt(2), is(10));
+        assertThat(resultSet.getString(3), is("init"));
+        assertTrue(resultSet.next());
+        assertThat(resultSet.getInt(1), is(1001));
+        assertThat(resultSet.getInt(2), is(11));
+        assertThat(resultSet.getString(3), is("init"));
         assertFalse(resultSet.next());
     }
 }
