67bd86cdf360ef5d608a6bba771652a823a5bd8c
tristaZero <panjuan@apache.org>
2021-01-31 22:09:09 +0800

    fill SchemaPrivilege

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/auth/SchemaPrivilege.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/auth/SchemaPrivilege.java
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, softwarea
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,9 +17,34 @@
 
 package org.apache.shardingsphere.infra.auth;
 
+import lombok.Getter;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Schema privilege.
  */
+@Getter
 public final class SchemaPrivilege {
-
+    
+    private final Map<String, TablePrivilege> tablePrivileges = new LinkedHashMap<>();
+    
+    /**
+     * Has privileges.
+     *
+     * @param table table
+     * @param privileges privileges
+     * @return has privileges or not
+     */
+    public boolean hasPrivileges(final String table, final Collection<PrivilegeType> privileges) {
+        if (!tablePrivileges.containsKey(table)) {
+            return false;
+        }
+        if (tablePrivileges.containsKey("*")) {
+            return tablePrivileges.get("*").hasPrivileges(privileges);
+        }
+        return tablePrivileges.get(table).hasPrivileges(privileges);
+    }
 }
