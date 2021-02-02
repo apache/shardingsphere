3fc412277c120f90d9b5021d9d73821b78fb842e
qiulu3 <Lucas209910>
2021-02-02 11:00:02 +0800

    remove useless function

--- shardingsphere-scaling/shardingsphere-scaling-core/src/main/java/org/apache/shardingsphere/scaling/core/util/ReflectionUtil.java
+++ shardingsphere-scaling/shardingsphere-scaling-core/src/main/java/org/apache/shardingsphere/scaling/core/util/ReflectionUtil.java
@@ -33,25 +33,6 @@ import java.util.Map;
 public final class ReflectionUtil {
     
     /**
-     * Get field map.
-     *
-     * @param object object
-     * @return field map
-     * @throws IllegalAccessException illegal access exception
-     */
-    public static Map<String, Object> getFieldMap(final Object object) throws IllegalAccessException {
-        Map<String, Object> result = new HashMap<>();
-        for (Field field : object.getClass().getDeclaredFields()) {
-            field.setAccessible(true);
-            Object value = field.get(object);
-            if (null != value) {
-                result.put(field.getName(), value);
-            }
-        }
-        return result;
-    }
-    
-    /**
      * Set value into target object field.
      *
      * @param target target object
@@ -63,37 +44,6 @@ public final class ReflectionUtil {
     public static void setFieldValue(final Object target, final String fieldName, final Object value) throws NoSuchFieldException, IllegalAccessException {
         Field field = getField(target.getClass(), fieldName, true);
         field.set(target, value);
-    }
-    
-    /**
-     * Set field value into target object.
-     *
-     * @param targetClass target class
-     * @param targetObject target object
-     * @param fieldName field name
-     * @param value target filed value
-     * @throws NoSuchFieldException no such field exception
-     * @throws IllegalAccessException illegal access exception
-     */
-    public static void setFieldValue(final Class<?> targetClass, final Object targetObject, final String fieldName, final Object value) throws NoSuchFieldException, IllegalAccessException {
-        Field field = getField(targetClass, fieldName, true);
-        field.setAccessible(true);
-        field.set(targetObject, value);
-    }
-    
-    /**
-     * Set static field value.
-     *
-     * @param targetClass target class
-     * @param fieldName field name
-     * @param value new value
-     * @throws NoSuchFieldException no such field exception
-     * @throws IllegalAccessException illegal access exception
-     */
-    public static void setStaticFieldValue(final Class<?> targetClass, final String fieldName, final Object value) throws NoSuchFieldException, IllegalAccessException {
-        Field field = getField(targetClass, fieldName, true);
-        field.setAccessible(true);
-        field.set(null, value);
     }
     
     /**
