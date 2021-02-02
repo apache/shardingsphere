a0e4cb2095e791760a85c3825bed5ac6bd937d50
glbb <unbeatablegl@gmail.com>
2021-02-02 21:34:58 +0800

    code review

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/test/java/org/apache/shardingsphere/sharding/rule/single/SingleTableRuleLoaderTest.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-common/src/test/java/org/apache/shardingsphere/sharding/rule/single/SingleTableRuleLoaderTest.java
@@ -3,9 +3,7 @@ package org.apache.shardingsphere.sharding.rule.single;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import lombok.val;
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +19,8 @@ import java.sql.SQLException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -42,33 +42,33 @@ public class SingleTableRuleLoaderTest {
         if (Strings.isNullOrEmpty(dataSourceName) || tables == null) {
             throw new IllegalArgumentException("dataSourceNam is empty or tables is null");
         }
-        val dataSource = mock(DataSource.class);
-        val conn = mock(Connection.class);
+        DataSource dataSource = mock(DataSource.class);
+        Connection conn = mock(Connection.class);
         when(dataSource.getConnection()).thenReturn(conn);
         when(conn.getCatalog()).thenReturn(dataSourceName);
 
-        val resultSet = mock(ResultSet.class);
-        val tableList = Lists.newArrayList(tables);
+        ResultSet resultSet = mock(ResultSet.class);
+        List<String> tableList = Lists.newArrayList(tables);
         if (tableList.size() == 0) {
             when(resultSet.next()).thenReturn(false);
         } else if (tableList.size() == 1) {
             when(resultSet.next()).thenReturn(true, false);
             when(resultSet.getString(TABLE_NAME)).thenReturn(tableList.get(0));
         } else {
-            val subTableList = tableList.subList(1, tables.size());
-            val subNextList = subTableList.stream()
+            List<String> subTableList = tableList.subList(1, tables.size());
+            List<Boolean> subNextList = subTableList.stream()
                 .map(item -> true)
                 .collect(Collectors.toList());
             subNextList.add(false);
-            val subTableArray = new String[subTableList.size()];
-            val subNextArray = new Boolean[subNextList.size()];
+            String[] subTableArray = new String[subTableList.size()];
+            Boolean[] subNextArray = new Boolean[subNextList.size()];
             subTableList.toArray(subTableArray);
             subNextList.toArray(subNextArray);
             when(resultSet.next()).thenReturn(true, subNextArray);
             when(resultSet.getString(TABLE_NAME)).thenReturn(tableList.get(0), subTableArray);
         }
 
-        val metaData = mock(DatabaseMetaData.class);
+        DatabaseMetaData metaData = mock(DatabaseMetaData.class);
         when(conn.getMetaData()).thenReturn(metaData);
 
         when(
@@ -109,6 +109,4 @@ public class SingleTableRuleLoaderTest {
         assertTrue(tableSet.contains("teacher"));
         assertTrue(tableSet.contains("class"));
     }
-
-
 }
