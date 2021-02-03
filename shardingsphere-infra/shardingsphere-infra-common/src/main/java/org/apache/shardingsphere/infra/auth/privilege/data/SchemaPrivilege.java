e28d4e89d970c62afd115295f607eaf08cb48389
tristaZero <panjuan@apache.org>
2021-02-03 15:55:06 +0800

    Create DataPrivilege and refactor ShardingSpherePrivilege

---
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/auth/privilege/data/SchemaPrivilege.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.auth.privilege.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.infra.auth.privilege.PrivilegeType;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Schema privilege.
+ */
+@RequiredArgsConstructor
+@Getter
+public final class SchemaPrivilege {
+    
+    private final String name;
+    
+    private final Collection<PrivilegeType> globalPrivileges = new LinkedHashSet<>();
+    
+    private final Map<String, TablePrivilege> specificPrivileges = new LinkedHashMap<>();
+    
+    /**
+     * Has privileges.
+     *
+     * @param table table
+     * @param privileges privileges
+     * @return has privileges or not
+     */
+    public boolean hasPrivileges(final String table, final Collection<PrivilegeType> privileges) {
+        return globalPrivileges.contains(PrivilegeType.ALL) || globalPrivileges.containsAll(privileges)
+                || hasPrivileges0(table, privileges);
+    }
+    
+    private boolean hasPrivileges0(final String table, final Collection<PrivilegeType> privileges) {
+        Collection<PrivilegeType> targets = privileges.stream().filter(each -> !globalPrivileges.contains(each)).collect(Collectors.toList());
+        return specificPrivileges.containsKey(table) && specificPrivileges.get(table).hasPrivileges(targets);
+    }
+}
