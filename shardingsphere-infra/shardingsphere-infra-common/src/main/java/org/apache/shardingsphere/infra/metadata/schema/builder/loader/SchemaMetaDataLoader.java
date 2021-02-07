2b3ae524f1e97a51de3632c1c121910abe5f5ec5
Liang Zhang <terrymanu@163.com>
2021-02-07 13:17:10 +0800

    Remove useless log for SchemaMetaDataLoader.loadAllTableNames() (#9366)
    * Remove useless log
    
    * Remove useless log for SchemaMetaDataLoader.loadAllTableNames()

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/schema/builder/loader/SchemaMetaDataLoader.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/schema/builder/loader/SchemaMetaDataLoader.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.infra.metadata.schema.builder.loader;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
 import org.apache.shardingsphere.infra.metadata.schema.builder.loader.adapter.MetaDataLoaderConnectionAdapter;
 
@@ -28,7 +27,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,7 +34,6 @@ import java.util.List;
  * Schema meta data loader.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@Slf4j(topic = "ShardingSphere-metadata")
 public final class SchemaMetaDataLoader {
     
     private static final String TABLE_TYPE = "TABLE";
@@ -54,15 +51,9 @@ public final class SchemaMetaDataLoader {
      * @throws SQLException SQL exception
      */
     public static Collection<String> loadAllTableNames(final DataSource dataSource, final DatabaseType databaseType) throws SQLException {
-        Collection<String> result;
         try (MetaDataLoaderConnectionAdapter connectionAdapter = new MetaDataLoaderConnectionAdapter(databaseType, dataSource.getConnection())) {
-            result = loadAllTableNames(connectionAdapter);
+            return loadAllTableNames(connectionAdapter);
         }
-        log.info("Loading {} tables' meta data for unconfigured tables.", result.size());
-        if (result.isEmpty()) {
-            return Collections.emptyList();
-        }
-        return result;
     }
     
     private static Collection<String> loadAllTableNames(final Connection connection) throws SQLException {
