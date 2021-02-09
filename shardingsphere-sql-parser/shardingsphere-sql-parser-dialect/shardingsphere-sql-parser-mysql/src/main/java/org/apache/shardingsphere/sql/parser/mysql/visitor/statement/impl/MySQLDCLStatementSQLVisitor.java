806194001c61b94f6682a03601e67cefa3f3255c
JingShang Lu <lujingshang@apache.org>
2021-02-08 23:58:53 -0800

    fix the create user syntax of MySQL (#9395)
    * fix the create user syntax of MySQL and add the definition of MySQL create user statement
    
    * code format
    
    * fix
    
    * code format
    
    * code format

--- shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/statement/impl/MySQLDCLStatementSQLVisitor.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/statement/impl/MySQLDCLStatementSQLVisitor.java
@@ -18,23 +18,37 @@
 package org.apache.shardingsphere.sql.parser.mysql.visitor.statement.impl;
 
 import lombok.NoArgsConstructor;
-import org.apache.shardingsphere.sql.parser.api.visitor.operation.SQLStatementVisitor;
 import org.apache.shardingsphere.sql.parser.api.visitor.ASTNode;
+import org.apache.shardingsphere.sql.parser.api.visitor.operation.SQLStatementVisitor;
 import org.apache.shardingsphere.sql.parser.api.visitor.type.DCLSQLVisitor;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AccountLockPasswordExpireOptionContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AccountLockPasswordExpireOptionsContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.AlterUserContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ConnectOptionContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.ConnectOptionsContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateRoleContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateUserContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateUserEntryContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateUserEntryIdentifiedByContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateUserEntryIdentifiedWithContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.CreateUserEntryNoOptionContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.DropRoleContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.DropUserContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.GrantContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.PrivilegeClauseContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.RenameUserContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.RequireClauseContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.RevokeContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.RoleNameContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SetDefaultRoleContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SetPasswordContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.SetRoleContext;
 import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TableNameContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.TlsOptionContext;
+import org.apache.shardingsphere.sql.parser.autogen.MySQLStatementParser.UserNameContext;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.SimpleTableSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
+import org.apache.shardingsphere.sql.parser.sql.common.value.literal.impl.NumberLiteralValue;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLAlterUserStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLCreateRoleStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLCreateUserStatement;
@@ -46,6 +60,13 @@ import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQ
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLSetDefaultRoleStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLSetPasswordStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dcl.MySQLSetRoleStatement;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.ACLAttributeEnum;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.PasswordOrLockOptionSegment;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.SSLTypeEnum;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.TLSOptionSegment;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.UserResourceSegment;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.UserResourceSpecifiedLimitEnum;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment.UserSegment;
 
 import java.util.Optional;
 import java.util.Properties;
@@ -88,7 +109,255 @@ public final class MySQLDCLStatementSQLVisitor extends MySQLStatementSQLVisitor 
     
     @Override
     public ASTNode visitCreateUser(final CreateUserContext ctx) {
-        return new MySQLCreateUserStatement();
+        MySQLCreateUserStatement result = new MySQLCreateUserStatement();
+        for (CreateUserEntryContext each : ctx.createUserList().createUserEntry()) {
+            result.getUsers().add((UserSegment) visit(each));
+        }
+        if (null != ctx.defaultRoleClause()) {
+            for (RoleNameContext each : ctx.defaultRoleClause().roleName()) {
+                result.getDefaultRoles().add(each.getText());
+            }
+        }
+        if (null != ctx.requireClause()) {
+            result.setTlsOptionSegment((TLSOptionSegment) visit(ctx.requireClause()));
+        }
+        if (null != ctx.connectOptions()) {
+            result.setUserResource((UserResourceSegment) visit(ctx.connectOptions()));
+        }
+        if (null != ctx.accountLockPasswordExpireOptions()) {
+            result.setPasswordOrLockOption((PasswordOrLockOptionSegment) visit(ctx.accountLockPasswordExpireOptions()));
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitRequireClause(final RequireClauseContext ctx) {
+        TLSOptionSegment result = new TLSOptionSegment();
+        if (null != ctx.NONE()) {
+            result.setType(SSLTypeEnum.SSL_TYPE_NONE);
+        } else if (null != ctx.X509()) {
+            result.setType(SSLTypeEnum.SSL_TYPE_X509);
+        } else if (null != ctx.SSL()) {
+            result.setType(SSLTypeEnum.SSL_TYPE_ANY);
+        } else {
+            result.setType(SSLTypeEnum.SSL_TYPE_SPECIFIED);
+            for (TlsOptionContext each : ctx.tlsOption()) {
+                if (null != each.SUBJECT()) {
+                    result.setX509Subject(each.string_().getText());
+                } else if (null != each.ISSUER()) {
+                    result.setX509Issuer(each.string_().getText());
+                } else if (null != each.CIPHER()) {
+                    result.setX509Cipher(each.string_().getText());
+                }
+            }
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitConnectOptions(final ConnectOptionsContext ctx) {
+        UserResourceSegment result = new UserResourceSegment();
+        result.setStartIndex(ctx.start.getStartIndex());
+        result.setStopIndex(ctx.stop.getStopIndex());
+        for (ConnectOptionContext each : ctx.connectOption()) {
+            if (null != each.MAX_QUERIES_PER_HOUR()) {
+                result.setSpecifiedLimits(UserResourceSpecifiedLimitEnum.QUERIES_PER_HOUR);
+                result.setQuestions(new NumberLiteralValue(each.NUMBER_().getText()).getValue().intValue());
+            }
+            if (null != each.MAX_UPDATES_PER_HOUR()) {
+                result.setSpecifiedLimits(UserResourceSpecifiedLimitEnum.UPDATES_PER_HOUR);
+                result.setUpdates(new NumberLiteralValue(each.NUMBER_().getText()).getValue().intValue());
+            }
+            if (null != each.MAX_CONNECTIONS_PER_HOUR()) {
+                result.setSpecifiedLimits(UserResourceSpecifiedLimitEnum.CONNECTIONS_PER_HOUR);
+                result.setConnPerHour(new NumberLiteralValue(each.NUMBER_().getText()).getValue().intValue());
+            }
+            if (null != each.MAX_USER_CONNECTIONS()) {
+                result.setSpecifiedLimits(UserResourceSpecifiedLimitEnum.USER_CONNECTIONS);
+                result.setUserConn(new NumberLiteralValue(each.NUMBER_().getText()).getValue().intValue());
+            }
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitCreateUserEntryNoOption(final CreateUserEntryNoOptionContext ctx) {
+        UserSegment result = (UserSegment) visit(ctx.userName());
+        result.setStartIndex(ctx.start.getStartIndex());
+        result.setStopIndex(ctx.stop.getStopIndex());
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitCreateUserEntryIdentifiedBy(final CreateUserEntryIdentifiedByContext ctx) {
+        UserSegment result = (UserSegment) visit(ctx.userName());
+        result.setStartIndex(ctx.start.getStartIndex());
+        result.setStopIndex(ctx.stop.getStopIndex());
+        if (null != ctx.string_()) {
+            result.setAuth(ctx.string_().getText());
+            result.setHasPasswordGenerator(false);
+            result.setUsesIdentifiedByClause(true);
+            result.setDiscardOldPassword(false);
+        } else {
+            result.setHasPasswordGenerator(true);
+            result.setUsesIdentifiedByClause(true);
+            result.setDiscardOldPassword(false);
+            result.setUsesIdentifiedWithClause(false);
+        }
+        result.setRetainCurrentPassword(false);
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitCreateUserEntryIdentifiedWith(final CreateUserEntryIdentifiedWithContext ctx) {
+        UserSegment result = (UserSegment) visit(ctx.userName());
+        result.setStartIndex(ctx.start.getStartIndex());
+        result.setStopIndex(ctx.stop.getStopIndex());
+        if (null != ctx.textOrIdentifier()) {
+            result.setPlugin(ctx.textOrIdentifier().getText());
+            result.setHasPasswordGenerator(false);
+            result.setUsesIdentifiedByClause(true);
+            result.setDiscardOldPassword(false);
+            result.setRetainCurrentPassword(false);
+        } else if (null != ctx.AS()) {
+            result.setPlugin(ctx.textOrIdentifier().getText());
+            result.setAuth(ctx.string_().getText());
+            result.setHasPasswordGenerator(false);
+            result.setUsesIdentifiedByClause(true);
+            result.setDiscardOldPassword(false);
+            result.setRetainCurrentPassword(false);
+        } else if (null != ctx.BY() && null != ctx.string_()) {
+            result.setPlugin(ctx.textOrIdentifier().getText());
+            result.setAuth(ctx.string_().getText());
+            result.setHasPasswordGenerator(false);
+            result.setUsesIdentifiedByClause(true);
+            result.setUsesIdentifiedWithClause(true);
+            result.setDiscardOldPassword(false);
+            result.setRetainCurrentPassword(false);
+        } else {
+            result.setPlugin(ctx.textOrIdentifier().getText());
+            result.setAuth(ctx.string_().getText());
+            result.setHasPasswordGenerator(true);
+            result.setUsesIdentifiedByClause(true);
+            result.setUsesIdentifiedWithClause(true);
+            result.setDiscardOldPassword(false);
+            result.setRetainCurrentPassword(false);
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitAccountLockPasswordExpireOptions(final AccountLockPasswordExpireOptionsContext ctx) {
+        PasswordOrLockOptionSegment result = new PasswordOrLockOptionSegment();
+        for (AccountLockPasswordExpireOptionContext each : ctx.accountLockPasswordExpireOption()) {
+            fillAccountLockPasswordExpireOption(result, each);
+        }
+        return result;
+    }
+    
+    private void fillAccountLockPasswordExpireOption(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.ACCOUNT()) {
+            fillAccountLock(segment, ctx);
+        } else if (null != ctx.PASSWORD() && null != ctx.EXPIRE()) {
+            fillPasswordExpire(segment, ctx);
+        } else if (null != ctx.PASSWORD() && null != ctx.HISTORY()) {
+            fillPasswordHistory(segment, ctx);
+        } else if (null != ctx.PASSWORD() && null != ctx.REUSE()) {
+            fillPasswordReuse(segment, ctx);
+        } else if (null != ctx.PASSWORD() && null != ctx.REQUIRE()) {
+            fillPasswordRequire(segment, ctx);
+        } else if (null != ctx.FAILED_LOGIN_ATTEMPTS()) {
+            segment.setUpdateFailedLoginAttempts(true);
+            segment.setFailedLoginAttempts(new NumberLiteralValue(ctx.NUMBER_().getText()).getValue().intValue());
+        } else {
+            if (null != ctx.UNBOUNDED()) {
+                segment.setUpdatePasswordLockTime(true);
+                segment.setPasswordLockTime(-1);
+            } else {
+                segment.setUpdatePasswordLockTime(true);
+                segment.setPasswordLockTime(new NumberLiteralValue(ctx.NUMBER_().getText()).getValue().intValue());
+            }
+        }
+    }
+    
+    private void fillAccountLock(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.LOCK()) {
+            segment.setUpdateAccountLockedColumn(true);
+            segment.setAccountLocked(true);
+        } else {
+            segment.setUpdateAccountLockedColumn(true);
+            segment.setAccountLocked(false);
+        }
+    }
+    
+    private void fillPasswordExpire(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.INTERVAL()) {
+            segment.setExpireAfterDays(new NumberLiteralValue(ctx.NUMBER_().getText()).getValue().intValue());
+            segment.setUpdatePasswordExpiredColumn(false);
+            segment.setUpdatePasswordExpiredFields(true);
+            segment.setUseDefaultPasswordLifeTime(false);
+        } else if (null != ctx.NEVER()) {
+            segment.setExpireAfterDays(0);
+            segment.setUpdatePasswordExpiredColumn(false);
+            segment.setUpdatePasswordExpiredFields(true);
+            segment.setUseDefaultPasswordLifeTime(false);
+        } else if (null != ctx.DEFAULT()) {
+            segment.setExpireAfterDays(0);
+            segment.setUpdatePasswordExpiredColumn(false);
+            segment.setUpdatePasswordExpiredFields(true);
+            segment.setUseDefaultPasswordLifeTime(true);
+        } else {
+            segment.setExpireAfterDays(0);
+            segment.setUpdatePasswordExpiredColumn(true);
+            segment.setUpdatePasswordExpiredFields(true);
+            segment.setUseDefaultPasswordLifeTime(true);
+        }
+    }
+    
+    private void fillPasswordHistory(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.DEFAULT()) {
+            segment.setPasswordHistoryLength(0);
+            segment.setUpdatePasswordHistory(true);
+            segment.setUseDefaultPasswordHistory(true);
+        } else {
+            segment.setPasswordHistoryLength(new NumberLiteralValue(ctx.NUMBER_().getText()).getValue().intValue());
+            segment.setUpdatePasswordHistory(true);
+            segment.setUseDefaultPasswordHistory(false);
+        }
+    }
+    
+    private void fillPasswordReuse(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.DEFAULT()) {
+            segment.setPasswordReuseInterval(0);
+            segment.setUpdatePasswordReuseInterval(true);
+            segment.setUseDefaultPasswordReuseInterval(true);
+        } else {
+            segment.setPasswordReuseInterval(new NumberLiteralValue(ctx.NUMBER_().getText()).getValue().intValue());
+            segment.setUpdatePasswordReuseInterval(true);
+            segment.setUseDefaultPasswordReuseInterval(false);
+        }
+    }
+    
+    private void fillPasswordRequire(final PasswordOrLockOptionSegment segment, final AccountLockPasswordExpireOptionContext ctx) {
+        if (null != ctx.DEFAULT()) {
+            segment.setUpdatePasswordRequireCurrent(ACLAttributeEnum.DEFAULT);
+        } else if (null != ctx.OPTIONAL()) {
+            segment.setUpdatePasswordRequireCurrent(ACLAttributeEnum.NO);
+        } else {
+            segment.setUpdatePasswordRequireCurrent(ACLAttributeEnum.YES);
+        }
+    }
+    
+    @Override
+    public ASTNode visitUserName(final UserNameContext ctx) {
+        UserSegment result = new UserSegment();
+        if (null != ctx.userIdentifierOrText()) {
+            result.setUser(new IdentifierValue(ctx.userIdentifierOrText().textOrIdentifier(0).getText()).getValue());
+            if (null != ctx.userIdentifierOrText().AT_()) {
+                result.setHost(new IdentifierValue(ctx.userIdentifierOrText().textOrIdentifier(1).getText()).getValue());
+            }
+        }
+        return result;
     }
     
     @Override
