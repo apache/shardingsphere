442c97b1d4e518c77bc71b17b3973091d0dc2712
yu199195 <549477611@qq.com>
2021-03-01 21:24:00 +0800

    fix collector sql

--- shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-tracing/shardingsphere-agent-tracing-jaeger/src/main/java/org/apache/shardingsphere/agent/plugin/tracing/jaeger/advice/JDBCExecutorCallbackAdvice.java
+++ shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-tracing/shardingsphere-agent-tracing-jaeger/src/main/java/org/apache/shardingsphere/agent/plugin/tracing/jaeger/advice/JDBCExecutorCallbackAdvice.java
@@ -49,22 +49,21 @@ public final class JDBCExecutorCallbackAdvice implements InstanceMethodAroundAdv
     public void beforeMethod(final AdviceTargetObject target, final Method method, final Object[] args, final MethodInvocationResult result) {
         Span root = (Span) ((Map<String, Object>) args[2]).get(JaegerConstants.ROOT_SPAN);
         Tracer.SpanBuilder builder = GlobalTracer.get().buildSpan(OPERATION_NAME);
-        if ((boolean) args[1]) {
-            builder.asChildOf(root);
-        } else {
-            JDBCExecutionUnit executionUnit = (JDBCExecutionUnit) args[0];
-            Method getMetadataMethod = JDBCExecutorCallback.class.getDeclaredMethod("getDataSourceMetaData", DatabaseMetaData.class);
-            getMetadataMethod.setAccessible(true);
-            DataSourceMetaData metaData = (DataSourceMetaData) getMetadataMethod.invoke(target, new Object[]{executionUnit.getStorageResource().getConnection().getMetaData()});
-            builder.withTag(Tags.COMPONENT.getKey(), JaegerConstants.COMPONENT_NAME)
-                    .withTag(Tags.DB_TYPE.getKey(), JaegerConstants.DB_TYPE_VALUE)
-                    .withTag(Tags.DB_INSTANCE.getKey(), executionUnit.getExecutionUnit().getDataSourceName())
-                    .withTag(Tags.PEER_HOSTNAME.getKey(), metaData.getHostName())
-                    .withTag(Tags.PEER_PORT.getKey(), metaData.getPort())
-                    .withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT)
-                    .withTag(Tags.DB_STATEMENT.getKey(), executionUnit.getExecutionUnit().getSqlUnit().getSql())
-                    .withTag(JaegerConstants.ShardingSphereTags.DB_BIND_VARIABLES.getKey(), executionUnit.getExecutionUnit().getSqlUnit().getParameters().toString());
+        if (null != root) {
+            builder = builder.asChildOf(root);
         }
+        JDBCExecutionUnit executionUnit = (JDBCExecutionUnit) args[0];
+        Method getMetadataMethod = JDBCExecutorCallback.class.getDeclaredMethod("getDataSourceMetaData", DatabaseMetaData.class);
+        getMetadataMethod.setAccessible(true);
+        DataSourceMetaData metaData = (DataSourceMetaData) getMetadataMethod.invoke(target, new Object[]{executionUnit.getStorageResource().getConnection().getMetaData()});
+        builder.withTag(Tags.COMPONENT.getKey(), JaegerConstants.COMPONENT_NAME)
+                .withTag(Tags.DB_TYPE.getKey(), JaegerConstants.DB_TYPE_VALUE)
+                .withTag(Tags.DB_INSTANCE.getKey(), executionUnit.getExecutionUnit().getDataSourceName())
+                .withTag(Tags.PEER_HOSTNAME.getKey(), metaData.getHostName())
+                .withTag(Tags.PEER_PORT.getKey(), metaData.getPort())
+                .withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CLIENT)
+                .withTag(Tags.DB_STATEMENT.getKey(), executionUnit.getExecutionUnit().getSqlUnit().getSql())
+                .withTag(JaegerConstants.ShardingSphereTags.DB_BIND_VARIABLES.getKey(), executionUnit.getExecutionUnit().getSqlUnit().getParameters().toString());
         target.setAttachment(builder.startActive(true));
     }
     
