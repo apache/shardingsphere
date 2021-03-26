49369cfb04f2a0aaa9fa7769287e201eeffd643d
JingShang Lu <jingshanglu1@gmail.com>
2021-03-26 16:54:46 +0800

    fix

--- shardingsphere-governance/shardingsphere-governance-context/src/main/java/org/apache/shardingsphere/governance/context/metadata/GovernanceMetaDataContexts.java
+++ shardingsphere-governance/shardingsphere-governance-context/src/main/java/org/apache/shardingsphere/governance/context/metadata/GovernanceMetaDataContexts.java
@@ -419,12 +419,12 @@ public final class GovernanceMetaDataContexts implements MetaDataContexts {
             return;
         }
         Map<ShardingSphereUser, ShardingSpherePrivilege> result = PrivilegeBuilder.build(metaDataContexts.getMetaDataMap().values(), users, metaDataContexts.getProps());
-        for (Entry<ShardingSphereUser, ShardingSpherePrivilege> each : result.entrySet()) {
-            Optional<ShardingSphereUser> user = metaDataContexts.getAuthentication().getAuthentication().keySet().stream().filter(t -> t.getGrantee().equals(each.getKey().getGrantee())).findFirst();
-            if (user.isPresent() && null != result.get(each.getKey())) {
-                metaDataContexts.getAuthentication().getAuthentication().put(user.get(), each.getValue());
-            } else if (!user.isPresent() && null != result.get(each.getKey())) {
-                metaDataContexts.getAuthentication().getAuthentication().put(each.getKey(), each.getValue());
+        for (Entry<ShardingSphereUser, ShardingSpherePrivilege> entry : result.entrySet()) {
+            Optional<ShardingSphereUser> user = metaDataContexts.getAuthentication().getAuthentication().keySet().stream().filter(t -> t.getGrantee().equals(entry.getKey().getGrantee())).findFirst();
+            if (user.isPresent() && null != result.get(entry.getKey())) {
+                metaDataContexts.getAuthentication().getAuthentication().put(user.get(), entry.getValue());
+            } else if (!user.isPresent() && null != result.get(entry.getKey())) {
+                metaDataContexts.getAuthentication().getAuthentication().put(entry.getKey(), entry.getValue());
             }
         }
     }
