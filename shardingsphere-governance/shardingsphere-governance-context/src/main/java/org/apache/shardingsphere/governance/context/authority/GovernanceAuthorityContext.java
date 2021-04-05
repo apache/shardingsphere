91af750cca944602be00e14aa015bd2f9dfe3664
terrymanu <terrymanu@163.com>
2021-04-05 22:37:53 +0800

    Rename package

--- shardingsphere-governance/shardingsphere-governance-context/src/main/java/org/apache/shardingsphere/governance/context/authority/GovernanceAuthorityContext.java
+++ shardingsphere-governance/shardingsphere-governance-context/src/main/java/org/apache/shardingsphere/governance/context/authority/GovernanceAuthorityContext.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.governance.context.auth;
+package org.apache.shardingsphere.governance.context.authority;
 
 import com.google.common.base.Preconditions;
 import com.google.common.eventbus.Subscribe;
