062518c3ee6f4ad9e016ccf8aa259b6389546560
totalo <totalo@qq.com>
2021-04-08 14:46:41 +0800

    feat:add SQLServerPrivilegeLoader

--- shardingsphere-features/shardingsphere-authority/shardingsphere-authority-common/src/main/java/org/apache/shardingsphere/authority/loader/storage/impl/dialect/SQLServerPrivilegeLoader.java
+++ shardingsphere-features/shardingsphere-authority/shardingsphere-authority-common/src/main/java/org/apache/shardingsphere/authority/loader/storage/impl/dialect/SQLServerPrivilegeLoader.java
@@ -17,13 +17,11 @@
 
 package org.apache.shardingsphere.authority.loader.storage.impl.dialect;
 
-import com.google.common.base.Joiner;
 import org.apache.shardingsphere.authority.loader.storage.impl.StoragePrivilegeLoader;
 import org.apache.shardingsphere.authority.model.PrivilegeType;
 import org.apache.shardingsphere.authority.model.ShardingSpherePrivileges;
 import org.apache.shardingsphere.authority.model.database.SchemaPrivileges;
 import org.apache.shardingsphere.authority.model.database.TablePrivileges;
-import org.apache.shardingsphere.infra.metadata.user.Grantee;
 import org.apache.shardingsphere.infra.metadata.user.ShardingSphereUser;
 
 import javax.sql.DataSource;
@@ -38,187 +36,102 @@ import java.util.stream.Collectors;
  * SQLServer privilege loader.
  */
 public final class SQLServerPrivilegeLoader implements StoragePrivilegeLoader {
-    
+
+    private static final String TABLE_PRIVILEGE_SQL =
+            "SELECT GRANTOR, GRANTEE, TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, PRIVILEGE_TYPE, IS_GRANTABLE from INFORMATION_SCHEMA.TABLE_PRIVILEGES WHERE GRANTEE IN (%s)";
+
     @Override
     public Map<ShardingSphereUser, ShardingSpherePrivileges> load(final Collection<ShardingSphereUser> users, final DataSource dataSource) throws SQLException {
         Map<ShardingSphereUser, ShardingSpherePrivileges> result = new LinkedHashMap<>();
-        fillGlobalPrivilege(result, dataSource, users);
-        fillSchemaPrivilege(result, dataSource, users);
-        fillTablePrivilege(result, dataSource, users);
+        users.forEach(user -> result.put(user, new ShardingSpherePrivileges()));
+        fillTablePrivileges(result, dataSource, users);
         return result;
     }
-    
-    private void fillGlobalPrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final DataSource dataSource, final Collection<ShardingSphereUser> users) throws SQLException {
+
+    private void fillTablePrivileges(final Map<ShardingSphereUser, ShardingSpherePrivileges> userPrivilegeMap,
+                                     final DataSource dataSource, final Collection<ShardingSphereUser> users) throws SQLException {
+        Map<ShardingSphereUser, Map<String, Map<String, List<PrivilegeType>>>> privilegeCache = new HashMap<>();
         try (Connection connection = dataSource.getConnection()) {
             Statement statement = connection.createStatement();
-            try (ResultSet resultSet = statement.executeQuery(getGlobalPrivilegeSQL(users))) {
+            try (ResultSet resultSet = statement.executeQuery(getTablePrivilegesSQL(users))) {
                 while (resultSet.next()) {
-                    fillGlobalPrivilege(privileges, resultSet);
+                    collectPrivileges(privilegeCache, resultSet);
+                }
+            }
+        }
+        fillTablePrivileges(privilegeCache, userPrivilegeMap);
+    }
+
+    private void fillTablePrivileges(final Map<ShardingSphereUser, Map<String, Map<String, List<PrivilegeType>>>> privilegeCache,
+                                     final Map<ShardingSphereUser, ShardingSpherePrivileges> userPrivilegeMap) {
+        for (Map.Entry<ShardingSphereUser, Map<String, Map<String, List<PrivilegeType>>>> entry : privilegeCache.entrySet()) {
+            for (String db : entry.getValue().keySet()) {
+                for (String tableName : entry.getValue().get(db).keySet()) {
+                    TablePrivileges tablePrivileges = new TablePrivileges(tableName, entry.getValue().get(db).get(tableName));
+                    ShardingSpherePrivileges privileges = userPrivilegeMap.get(entry.getKey());
+                    if (!privileges.getDatabasePrivileges().getSpecificPrivileges().containsKey(db)) {
+                        privileges.getDatabasePrivileges().getSpecificPrivileges().put(db, new SchemaPrivileges(db));
+                    }
+                    privileges.getDatabasePrivileges().getSpecificPrivileges().get(db).getSpecificPrivileges().put(tableName, tablePrivileges);
                 }
             }
         }
     }
-    
-    private void fillGlobalPrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final ResultSet resultSet) throws SQLException {
-        Optional<ShardingSphereUser> user = getShardingSphereUser(privileges, resultSet);
-        if (user.isPresent()) {
-            privileges.get(user.get()).getAdministrativePrivileges().getPrivileges().addAll(loadAdministrativePrivileges(resultSet));
-            privileges.get(user.get()).getDatabasePrivileges().getGlobalPrivileges().addAll(loadDatabaseGlobalPrivileges(resultSet));
+
+    private void collectPrivileges(final Map<ShardingSphereUser, Map<String, Map<String, List<PrivilegeType>>>> privilegeCache, final ResultSet resultSet) throws SQLException {
+        String db = resultSet.getString("TABLE_CATALOG");
+        String tableName = resultSet.getString("TABLE_NAME");
+        String privilegeType = resultSet.getString("PRIVILEGE_TYPE");
+        boolean hasPrivilege = resultSet.getString("IS_GRANTABLE").equalsIgnoreCase("YES");
+        String grantee = resultSet.getString("GRANTEE");
+        if (hasPrivilege) {
+            privilegeCache
+                    .computeIfAbsent(new ShardingSphereUser(grantee, "", ""), k -> new HashMap<>())
+                    .computeIfAbsent(db, k -> new HashMap<>())
+                    .computeIfAbsent(tableName, k -> new ArrayList<>())
+                    .add(getPrivilegeType(privilegeType));
         }
     }
-    
-    private void fillSchemaPrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final DataSource dataSource, final Collection<ShardingSphereUser> users) throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement statement = connection.createStatement();
-            try (ResultSet resultSet = statement.executeQuery(getSchemaPrivilegeSQL(users))) {
-                while (resultSet.next()) {
-                    fillSchemaPrivilege(privileges, resultSet);
-                }
-            }
-        }
+
+    private String getTablePrivilegesSQL(final Collection<ShardingSphereUser> users) {
+        String userList = users.stream().map(each -> String.format("'%s'", each.getGrantee().getUsername())).collect(Collectors.joining(", "));
+        return String.format(TABLE_PRIVILEGE_SQL, userList);
     }
-    
-    private void fillSchemaPrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final ResultSet resultSet) throws SQLException {
-        Optional<ShardingSphereUser> user = getShardingSphereUser(privileges, resultSet);
-        if (user.isPresent()) {
-            String db = resultSet.getString("Db");
-            SchemaPrivileges schemaPrivilege = new SchemaPrivileges(db);
-            schemaPrivilege.getGlobalPrivileges().addAll(loadDatabaseGlobalPrivileges(resultSet));
-            privileges.get(user.get()).getDatabasePrivileges().getSpecificPrivileges().put(db, schemaPrivilege);
-        }
-    }
-    
-    private void fillTablePrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final DataSource dataSource, final Collection<ShardingSphereUser> users) throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement statement = connection.createStatement();
-            try (ResultSet resultSet = statement.executeQuery(getTablePrivilegeSQL(users))) {
-                while (resultSet.next()) {
-                    fillTablePrivilege(privileges, resultSet);
-                }
-            }
-        }
-    }
-    
-    private void fillTablePrivilege(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final ResultSet resultSet) throws SQLException {
-        Optional<ShardingSphereUser> user = getShardingSphereUser(privileges, resultSet);
-        if (user.isPresent()) {
-            String db = resultSet.getString("Db");
-            String tableName = resultSet.getString("Table_name");
-            String[] tablePrivileges = (String[]) resultSet.getArray("Table_priv").getArray();
-            TablePrivileges tablePrivilege = new TablePrivileges(tableName, getPrivileges(tablePrivileges));
-            ShardingSpherePrivileges privilege = privileges.get(user.get());
-            if (!privilege.getDatabasePrivileges().getSpecificPrivileges().containsKey(db)) {
-                privilege.getDatabasePrivileges().getSpecificPrivileges().put(db, new SchemaPrivileges(db));
-            }
-            privilege.getDatabasePrivileges().getSpecificPrivileges().get(db).getSpecificPrivileges().put(tableName, tablePrivilege);
-        }
-    }
-    
-    private String getGlobalPrivilegeSQL(final Collection<ShardingSphereUser> users) {
-        Collection<String> result = new LinkedList<>();
-        StringBuilder builder = new StringBuilder("SELECT * FROM mysql.user WHERE (user, host) in ( ");
-        users.forEach(each -> builder.append("(").append(each.getGrantee().getUsername()).append(", ").append(each.getGrantee().getHostname()).append(")"));
-        return builder.append(Joiner.on(", ").join(result)).append(" )").toString();
-    }
-    
-    private String getSchemaPrivilegeSQL(final Collection<ShardingSphereUser> users) {
-        Collection<String> result = new LinkedList<>();
-        StringBuilder builder = new StringBuilder("SELECT * FROM mysql.db WHERE (user, host) in ( ");
-        users.forEach(each -> builder.append("(").append(each.getGrantee().getUsername()).append(", ").append(each.getGrantee().getHostname()).append(")"));
-        return builder.append(Joiner.on(", ").join(result)).append(" )").toString();
-    }
-    
-    private String getTablePrivilegeSQL(final Collection<ShardingSphereUser> users) {
-        Collection<String> result = new LinkedList<>();
-        StringBuilder builder = new StringBuilder("SELECT Db, Table_name, Table_priv FROM mysql.tables_priv WHERE (user, host) in ( ");
-        users.forEach(each -> builder.append("(").append(each.getGrantee().getUsername()).append(", ").append(each.getGrantee().getHostname()).append(")"));
-        return builder.append(Joiner.on(", ").join(result)).append(" )").toString();
-    }
-    
-    private Optional<ShardingSphereUser> getShardingSphereUser(final Map<ShardingSphereUser, ShardingSpherePrivileges> privileges, final ResultSet resultSet) throws SQLException {
-        Grantee grantee = new Grantee(resultSet.getString("user"), resultSet.getString("host"));
-        return privileges.keySet().stream().filter(each -> each.getGrantee().equals(grantee)).findFirst();
-    }
-    
-    private Collection<PrivilegeType> getPrivileges(final String[] privileges) {
-        return Arrays.stream(privileges).map(this::getPrivilegeType).collect(Collectors.toSet());
-    }
-    
+
     private PrivilegeType getPrivilegeType(final String privilege) {
         switch (privilege) {
-            case "Select":
+            case "SELECT":
                 return PrivilegeType.SELECT;
-            case "Insert":
+            case "INSERT":
                 return PrivilegeType.INSERT;
-            case "Update":
+            case "UPDATE":
                 return PrivilegeType.UPDATE;
-            case "Delete":
+            case "DELETE":
                 return PrivilegeType.DELETE;
-            case "Create":
-                return PrivilegeType.CREATE;
-            case "Alter":
-                return PrivilegeType.ALTER;
-            case "Drop":
-                return PrivilegeType.DROP;
-            case "Grant":
-                return PrivilegeType.GRANT;
-            case "Index":
-                return PrivilegeType.INDEX;
-            case "References":
+            case "CREATE FUNCTION":
+                return PrivilegeType.CREATE_FUNCTION;
+            case "REFERENCES":
                 return PrivilegeType.REFERENCES;
-            case "Create View":
+            case "CREATE TABLE":
+                return PrivilegeType.CREATE_TABLE;
+            case "CREATE DATABASE":
+                return PrivilegeType.CREATE_DATABASE;
+            case "EXECUTE":
+                return PrivilegeType.EXECUTE;
+            case "CREATE VIEW":
                 return PrivilegeType.CREATE_VIEW;
-            case "Show view":
-                return PrivilegeType.SHOW_VIEW;
-            case "Trigger":
-                return PrivilegeType.TRIGGER;
+            case "CREATE PROCEDURE":
+                return PrivilegeType.CREATE_PROC;
+            case "BACKUP DATABASE":
+                return PrivilegeType.BACKUP_DATABASE;
+            case "CREATE DEFAULT":
+                return PrivilegeType.CREATE_DEFAULT;
+            case "BACKUP LOG":
+                return PrivilegeType.BACKUP_LOG;
+            case "CREATE RULE":
+                return PrivilegeType.CREATE_RULE;
             default:
                 throw new UnsupportedOperationException(privilege);
-        }
-    }
-    
-    private Collection<PrivilegeType> loadAdministrativePrivileges(final ResultSet resultSet) throws SQLException {
-        Collection<PrivilegeType> result = new LinkedList<>();
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Super_priv"), PrivilegeType.SUPER, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Reload_priv"), PrivilegeType.RELOAD, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Shutdown_priv"), PrivilegeType.SHUTDOWN, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Process_priv"), PrivilegeType.PROCESS, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("File_priv"), PrivilegeType.FILE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Show_db_priv"), PrivilegeType.SHOW_DB, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Repl_slave_priv"), PrivilegeType.REPL_SLAVE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Repl_client_priv"), PrivilegeType.REPL_CLIENT, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_user_priv"), PrivilegeType.CREATE_USER, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_tablespace_priv"), PrivilegeType.CREATE_TABLESPACE, result);
-        return result;
-    }
-    
-    private Collection<PrivilegeType> loadDatabaseGlobalPrivileges(final ResultSet resultSet) throws SQLException {
-        Collection<PrivilegeType> result = new LinkedList<>();
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Select_priv"), PrivilegeType.SELECT, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Insert_priv"), PrivilegeType.INSERT, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Update_priv"), PrivilegeType.UPDATE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Delete_priv"), PrivilegeType.DELETE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_priv"), PrivilegeType.CREATE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Alter_priv"), PrivilegeType.ALTER, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Drop_priv"), PrivilegeType.DROP, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Grant_priv"), PrivilegeType.GRANT, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Index_priv"), PrivilegeType.INDEX, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("References_priv"), PrivilegeType.REFERENCES, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_tmp_table_priv"), PrivilegeType.CREATE_TMP, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Lock_tables_priv"), PrivilegeType.LOCK_TABLES, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Execute_priv"), PrivilegeType.EXECUTE, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_view_priv"), PrivilegeType.CREATE_VIEW, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Show_view_priv"), PrivilegeType.SHOW_VIEW, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Create_routine_priv"), PrivilegeType.CREATE_PROC, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Alter_routine_priv"), PrivilegeType.ALTER_PROC, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Event_priv"), PrivilegeType.EVENT, result);
-        addToPrivilegeTypesIfPresent(resultSet.getBoolean("Trigger_priv"), PrivilegeType.TRIGGER, result);
-        return result;
-    }
-    
-    private void addToPrivilegeTypesIfPresent(final boolean hasPrivilege, final PrivilegeType privilegeType, final Collection<PrivilegeType> target) {
-        if (hasPrivilege) {
-            target.add(privilegeType);
         }
     }
 
---
+++ shardingsphere-features/shardingsphere-authority/shardingsphere-authority-common/src/test/java/org/apache/shardingsphere/authority/loader/storage/impl/dialect/SQLServerPrivilegeLoaderTest.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.authority.loader.storage.impl.dialect;
+
+import org.apache.shardingsphere.authority.loader.storage.impl.StoragePrivilegeLoader;
+import org.apache.shardingsphere.authority.model.PrivilegeType;
+import org.apache.shardingsphere.authority.model.ShardingSpherePrivileges;
+import org.apache.shardingsphere.authority.model.database.SchemaPrivileges;
+import org.apache.shardingsphere.infra.metadata.user.ShardingSphereUser;
+import org.apache.shardingsphere.infra.spi.ShardingSphereServiceLoader;
+import org.apache.shardingsphere.infra.spi.typed.TypedSPIRegistry;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class SQLServerPrivilegeLoaderTest {
+
+    @BeforeClass
+    public static void setUp() {
+        ShardingSphereServiceLoader.register(StoragePrivilegeLoader.class);
+    }
+
+    @Test
+    public void assertLoad() throws SQLException {
+        Collection<ShardingSphereUser> users = createUsers();
+        DataSource dataSource = mockDataSource(users);
+        assertPrivileges(TypedSPIRegistry.getRegisteredService(StoragePrivilegeLoader.class, "SQLServer", new Properties()).load(users, dataSource));
+    }
+
+    private void assertPrivileges(final Map<ShardingSphereUser, ShardingSpherePrivileges> actual) {
+        assertThat(actual.size(), is(1));
+        ShardingSphereUser user = new ShardingSphereUser("dbo", "", "");
+        assertThat(actual.get(user).getDatabasePrivileges().getGlobalPrivileges().size(), is(0));
+        assertThat(actual.get(user).getDatabasePrivileges().getSpecificPrivileges().size(), is(1));
+        Collection<PrivilegeType> expectedSpecificPrivilege = new CopyOnWriteArraySet(Arrays.asList(PrivilegeType.INSERT, PrivilegeType.SELECT, PrivilegeType.UPDATE,
+                PrivilegeType.DELETE));
+        SchemaPrivileges schemaPrivileges = actual.get(user).getDatabasePrivileges().getSpecificPrivileges().get("db0");
+        assertThat(schemaPrivileges.getSpecificPrivileges().get("t_order").hasPrivileges(expectedSpecificPrivilege), is(true));
+    }
+
+    private Collection<ShardingSphereUser> createUsers() {
+        LinkedList<ShardingSphereUser> result = new LinkedList<>();
+        result.add(new ShardingSphereUser("dbo", "", ""));
+        return result;
+    }
+
+    private DataSource mockDataSource(final Collection<ShardingSphereUser> users) throws SQLException {
+        ResultSet tablePrivilegeResultSet = mockTablePrivilegeResultSet();
+        DataSource result = mock(DataSource.class, RETURNS_DEEP_STUBS);
+        String tablePrivilegeSql = "SELECT GRANTOR, GRANTEE, TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, PRIVILEGE_TYPE, IS_GRANTABLE from INFORMATION_SCHEMA.TABLE_PRIVILEGES WHERE GRANTEE IN (%s)";
+        String userList = users.stream().map(item -> String.format("'%s'", item.getGrantee().getUsername())).collect(Collectors.joining(", "));
+        when(result.getConnection().createStatement().executeQuery(String.format(tablePrivilegeSql, userList))).thenReturn(tablePrivilegeResultSet);
+        return result;
+    }
+
+    private ResultSet mockTablePrivilegeResultSet() throws SQLException {
+        ResultSet result = mock(ResultSet.class, RETURNS_DEEP_STUBS);
+        when(result.next()).thenReturn(true, true, true, true, true, true, true, false);
+        when(result.getString("TABLE_CATALOG")).thenReturn("db0");
+        when(result.getString("TABLE_NAME")).thenReturn("t_order");
+        when(result.getString("PRIVILEGE_TYPE")).thenReturn("INSERT", "SELECT", "UPDATE", "DELETE", "REFERENCES");
+        when(result.getString("IS_GRANTABLE")).thenReturn("YES", "YES", "YES", "YES", "YES", "YES", "YES");
+        when(result.getString("GRANTEE")).thenReturn("dbo");
+        return result;
+    }
+}
