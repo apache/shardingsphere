57954ac3824aff61ba758debb805c55baaaa764b
huanghao495430759 <34728144+huanghao495430759@users.noreply.github.com>
2021-04-13 07:45:50 -0500

    add combinational sql token and owner token (#10071)
    * add OwnerToken.
    
    * add CombinationalSQLToken.
    
    * add CombinationalSQLTokenTest and OwnerTokenTest.
    
    * fix OwnerTokenTest.
    
    Co-authored-by: huanghao <huanghao-jk@360shuke.com>

---
+++ shardingsphere-infra/shardingsphere-infra-rewrite/shardingsphere-infra-rewrite-engine/src/test/java/org/apache/shardingsphere/infra/rewrite/sql/token/pojo/generic/CombinationalSQLTokenTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.rewrite.sql.token.pojo.generic;
+
+import org.apache.shardingsphere.infra.rewrite.sql.token.pojo.SQLToken;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class CombinationalSQLTokenTest {
+
+    @Test
+    public void assertCombinationalSQLToken() {
+        CombinationalSQLToken combinationalSQLToken = new CombinationalSQLToken(0, 1);
+        SQLToken sqlToken = mock(SQLToken.class);
+        when(sqlToken.getStartIndex()).thenReturn(2);
+        when(sqlToken.compareTo(any(SQLToken.class))).thenReturn(0);
+        combinationalSQLToken.addSQLToken(sqlToken);
+        assertThat(combinationalSQLToken.getStartIndex(), is(0));
+        assertThat(combinationalSQLToken.getStopIndex(), is(1));
+        assertThat(combinationalSQLToken.getSQLTokens().size(), is(1));
+        assertThat(combinationalSQLToken.getSQLTokens().iterator().next().compareTo(sqlToken), is(0));
+    }
+}
