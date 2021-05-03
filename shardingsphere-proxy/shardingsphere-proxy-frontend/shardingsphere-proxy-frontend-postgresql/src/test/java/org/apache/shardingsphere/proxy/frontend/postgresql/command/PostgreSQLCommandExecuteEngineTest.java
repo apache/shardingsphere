3ba2be4d3ffccfb807228ddc60a2cd1810691df7
吴伟杰 <wuweijie@apache.org>
2021-05-03 16:47:12 +0800

    Complete testcases for PostgreSQLCommandExecuteEngineTest

--- shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/PostgreSQLCommandExecuteEngineTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/PostgreSQLCommandExecuteEngineTest.java
@@ -17,10 +17,18 @@
 
 package org.apache.shardingsphere.proxy.frontend.postgresql.command;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import org.apache.shardingsphere.db.protocol.postgresql.packet.PostgreSQLPacket;
+import org.apache.shardingsphere.db.protocol.postgresql.packet.generic.PostgreSQLCommandCompletePacket;
 import org.apache.shardingsphere.db.protocol.postgresql.packet.generic.PostgreSQLReadyForQueryPacket;
+import org.apache.shardingsphere.proxy.backend.communication.jdbc.connection.BackendConnection;
+import org.apache.shardingsphere.proxy.backend.communication.jdbc.connection.ResourceLock;
 import org.apache.shardingsphere.proxy.frontend.command.executor.QueryCommandExecutor;
 import org.apache.shardingsphere.proxy.frontend.command.executor.ResponseType;
+import org.apache.shardingsphere.proxy.frontend.postgresql.command.query.binary.sync.PostgreSQLComSyncExecutor;
+import org.apache.shardingsphere.proxy.frontend.postgresql.command.query.text.PostgreSQLComQueryExecutor;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -28,14 +36,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.SQLException;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-/**
- * TODO Complete testcases.
- */
 @RunWith(MockitoJUnitRunner.class)
 public final class PostgreSQLCommandExecuteEngineTest {
     
@@ -43,7 +51,18 @@ public final class PostgreSQLCommandExecuteEngineTest {
     private ChannelHandlerContext channelHandlerContext;
     
     @Mock
+    private Channel channel;
+    
+    @Mock
     private QueryCommandExecutor queryCommandExecutor;
+    
+    @Mock
+    private BackendConnection backendConnection;
+    
+    @Before
+    public void setUp() {
+        when(channelHandlerContext.channel()).thenReturn(channel);
+    }
     
     @Test
     public void assertWriteQueryDataWithUpdate() throws SQLException {
@@ -51,5 +70,41 @@ public final class PostgreSQLCommandExecuteEngineTest {
         when(queryCommandExecutor.getResponseType()).thenReturn(ResponseType.UPDATE);
         commandExecuteEngine.writeQueryData(channelHandlerContext, null, queryCommandExecutor, 0);
         verify(channelHandlerContext, times(1)).write(isA(PostgreSQLReadyForQueryPacket.class));
+    }
+    
+    @Test
+    public void assertWriteQueryDataWithComSync() throws SQLException {
+        PostgreSQLCommandExecuteEngine commandExecuteEngine = new PostgreSQLCommandExecuteEngine();
+        commandExecuteEngine.writeQueryData(channelHandlerContext, backendConnection, new PostgreSQLComSyncExecutor(backendConnection), 0);
+        verify(channelHandlerContext, never()).write(any(Object.class));
+    }
+    
+    @Test
+    public void assertWriteQueryDataWithInactiveChannel() throws SQLException {
+        PostgreSQLCommandExecuteEngine commandExecuteEngine = new PostgreSQLCommandExecuteEngine();
+        when(queryCommandExecutor.getResponseType()).thenReturn(ResponseType.QUERY);
+        when(channel.isActive()).thenReturn(false);
+        commandExecuteEngine.writeQueryData(channelHandlerContext, backendConnection, queryCommandExecutor, 0);
+        verify(channelHandlerContext).write(isA(PostgreSQLCommandCompletePacket.class));
+    }
+    
+    @Test
+    public void assertWriteQueryDataWithHasNextResult() throws SQLException {
+        PostgreSQLComQueryExecutor queryCommandExecutor = mock(PostgreSQLComQueryExecutor.class);
+        when(queryCommandExecutor.getResponseType()).thenReturn(ResponseType.QUERY);
+        when(channel.isActive()).thenReturn(true);
+        when(queryCommandExecutor.next()).thenReturn(true, false);
+        when(channel.isWritable()).thenReturn(false, true);
+        ResourceLock resourceLock = mock(ResourceLock.class);
+        when(backendConnection.getResourceLock()).thenReturn(resourceLock);
+        PostgreSQLPacket packet = mock(PostgreSQLPacket.class);
+        when(queryCommandExecutor.getQueryRowPacket()).thenReturn(packet);
+        PostgreSQLCommandExecuteEngine commandExecuteEngine = new PostgreSQLCommandExecuteEngine();
+        commandExecuteEngine.writeQueryData(channelHandlerContext, backendConnection, queryCommandExecutor, 0);
+        verify(resourceLock).doAwait();
+        verify(channelHandlerContext).write(packet);
+        verify(channelHandlerContext).write(isA(PostgreSQLCommandCompletePacket.class));
+        verify(channelHandlerContext).flush();
+        verify(channelHandlerContext).write(isA(PostgreSQLReadyForQueryPacket.class));
     }
 }
