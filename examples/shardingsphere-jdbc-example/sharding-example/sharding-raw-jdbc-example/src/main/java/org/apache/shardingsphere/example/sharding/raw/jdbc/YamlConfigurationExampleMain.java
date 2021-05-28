c228b73acc718176af9b165d3c754890059e7788
totalo <486515671@qq.com>
2021-05-28 20:55:51 +0800

    fix example error (#10532)
    * fix example error

--- examples/shardingsphere-jdbc-example/sharding-example/sharding-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/sharding/raw/jdbc/YamlConfigurationExampleMain.java
+++ examples/shardingsphere-jdbc-example/sharding-example/sharding-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/sharding/raw/jdbc/YamlConfigurationExampleMain.java
@@ -32,11 +32,11 @@ import java.sql.SQLException;
  */
 public final class YamlConfigurationExampleMain {
     
-    private static ShardingType shardingType = ShardingType.SHARDING_DATABASES;
+    private static ShardingType shardingType = ShardingType.SHARDING_READWRITE_SPLITTING;
 //    private static ShardingType shardingType = ShardingType.SHARDING_TABLES;
 //    private static ShardingType shardingType = ShardingType.SHARDING_DATABASES_AND_TABLES;
-//    private static ShardingType shardingType = ShardingType.REPLICA_QUERY;
-//    private static ShardingType shardingType = ShardingType.SHARDING_REPLICA_QUERY;
+//    private static ShardingType shardingType = ShardingType.READWRITE_SPLITTING;
+//    private static ShardingType shardingType = ShardingType.SHARDING_READWRITE_SPLITTING;
     
     public static void main(final String[] args) throws SQLException, IOException {
         DataSource dataSource = YamlDataSourceFactory.newInstance(shardingType);
