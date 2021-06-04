d48a0085bdc50e1678a8350ac1d07062e6bb9106
totalo <totalo@qq.com>
2021-06-04 21:07:12 +0800

    add xml

---
+++ shardingsphere-distsql-parser/shardingsphere-distsql-parser-engine/src/test/java/org/apache/shardingsphere/distsql/parser/api/DistSQLStatementParserTest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.distsql.parser.api;
+
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.SQLParserTestCasesRegistry;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.SQLParserTestCasesRegistryFactory;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.domain.statement.SQLParserTestCase;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.sql.SQLCaseType;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.sql.loader.SQLCasesLoader;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.sql.loader.SQLCasesRegistry;
+import org.apache.shardingsphere.sql.parser.api.SQLParserEngine;
+import org.apache.shardingsphere.sql.parser.api.SQLVisitorEngine;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.*;
+
+import static org.junit.Assert.fail;
+
+@AllArgsConstructor
+@RunWith(Parameterized.class)
+public final class DistSQLStatementParserTest {
+
+    private static final SQLCasesLoader SQL_CASES_LOADER = SQLCasesRegistry.getInstance().getSqlCasesLoader();
+
+    private static final SQLParserTestCasesRegistry SQL_PARSER_TEST_CASES_REGISTRY = SQLParserTestCasesRegistryFactory.getInstance().getRegistry();
+
+    private final String sqlCaseId;
+
+    private static void checkTestCases() {
+        Collection<String> allSQLCaseIDs = new HashSet<>(SQL_CASES_LOADER.getAllSQLCaseIDs());
+        if (allSQLCaseIDs.size() != SQL_PARSER_TEST_CASES_REGISTRY.getAllSQLCaseIDs().size()) {
+            allSQLCaseIDs.removeAll(SQL_PARSER_TEST_CASES_REGISTRY.getAllSQLCaseIDs());
+            fail(String.format("The count of SQL cases and SQL parser cases are mismatched, missing cases are: %s", allSQLCaseIDs));
+        }
+    }
+
+    @Test
+    public final void assertSupportedSQL() {
+        checkTestCases();
+        SQLParserTestCase expected = SQL_PARSER_TEST_CASES_REGISTRY.get(sqlCaseId);
+        String sql = SQL_CASES_LOADER.getSQL(sqlCaseId);
+        SQLStatement actual = parseSQLStatement(sql);
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> getTestParameters() {
+        return SQL_CASES_LOADER.getSQLTestParameters();
+    }
+
+    private SQLStatement parseSQLStatement(final String sql) {
+        return new DistSQLStatementParserEngine().parse(sql);
+    }
+}
---
+++ shardingsphere-distsql-parser/shardingsphere-distsql-parser-engine/src/test/java/org/apache/shardingsphere/distsql/parser/api/sql/jaxb/cases/domain/SQLParserTestCases.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.domain;
+
+import com.google.common.base.Preconditions;
+import lombok.Getter;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.domain.statement.AddResourceStatementTestCase;
+import org.apache.shardingsphere.distsql.parser.api.sql.jaxb.cases.domain.statement.SQLParserTestCase;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.*;
+
+/**
+ * SQL parser test cases.
+ */
+@XmlRootElement(name = "sql-parser-test-cases")
+@Getter
+public final class SQLParserTestCases {
+    
+    @XmlElement(name = "add-resource")
+    private final List<AddResourceStatementTestCase> addResourceTestCase = new LinkedList<>();
+    
+    /**
+     * Get all SQL parser test cases.
+     * 
+     * @return all SQL parser test cases
+     */
+    public Map<String, SQLParserTestCase> getAllSQLParserTestCases() {
+        Map<String, SQLParserTestCase> result = new HashMap<>();
+        putAll(addResourceTestCase, result);
+        return result;
+    }
+    
+    private void putAll(final List<? extends SQLParserTestCase> sqlParserTestCases, final Map<String, SQLParserTestCase> target) {
+        Map<String, SQLParserTestCase> sqlParserTestCaseMap = getSQLParserTestCases(sqlParserTestCases);
+        Collection<String> sqlParserTestCaseIds = new HashSet<>(sqlParserTestCaseMap.keySet());
+        sqlParserTestCaseIds.retainAll(target.keySet());
+        Preconditions.checkState(sqlParserTestCaseIds.isEmpty(), "Find duplicated SQL Case IDs: %s", sqlParserTestCaseIds);
+        target.putAll(sqlParserTestCaseMap);
+    }
+    
+    private Map<String, SQLParserTestCase> getSQLParserTestCases(final List<? extends SQLParserTestCase> sqlParserTestCases) {
+        Map<String, SQLParserTestCase> result = new HashMap<>(sqlParserTestCases.size(), 1);
+        for (SQLParserTestCase each : sqlParserTestCases) {
+            Preconditions.checkState(!result.containsKey(each.getSqlCaseId()), "Find duplicated SQL Case ID: %s", each.getSqlCaseId());
+            result.put(each.getSqlCaseId(), each);
+        }
+        return result;
+    }
+}
