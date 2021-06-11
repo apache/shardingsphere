82773a58f6dfffdd9945eab49ae7423b8d45a52e
Da Xiang Huang <localhost80@foxmail.com>
2021-06-11 20:43:59 +0800

    Add more assertions in MetaDataChangedWatcherTest (#10765)

--- shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/watcher/MetaDataChangedWatcherTest.java
+++ shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/watcher/MetaDataChangedWatcherTest.java
@@ -25,13 +25,83 @@ import org.junit.Test;
 import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public final class MetaDataChangedWatcherTest {
     
     @Test
     public void assertCreateEventWithInvalidPath() {
-        DataChangedEvent dataChangedEvent = new DataChangedEvent("/metadata_invalid/sharding_db", "encrypt_db", Type.UPDATED);
-        Optional<GovernanceEvent> actual = new MetaDataChangedWatcher().createGovernanceEvent(dataChangedEvent);
+        String key = "/metadata_invalid/sharding_db";
+        String value = "encrypt_db";
+        Optional<GovernanceEvent> actual = createEvent(key, value, Type.UPDATED);
         assertFalse(actual.isPresent());
+        actual = createEvent(key, value, Type.ADDED);
+        assertFalse(actual.isPresent());
+        actual = createEvent(key, value, Type.DELETED);
+        assertFalse(actual.isPresent());
+        actual = createEvent(key, value, Type.IGNORED);
+        assertFalse(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateAddedEvent() {
+        String key = "/metadata/sharding_db";
+        String value = "encrypt_db";
+        Optional<GovernanceEvent> actual = createEvent(key, value, Type.UPDATED);
+        assertTrue(actual.isPresent());
+        actual = createEvent(key, value, Type.ADDED);
+        assertTrue(actual.isPresent());
+        actual = createEvent(key, value, Type.IGNORED);
+        assertFalse(actual.isPresent());
+    }
+    
+    @Test
+    public void assertEmptyValue() {
+        String key = "/metadata/sharding_db/dataSources";
+        Optional<GovernanceEvent> actual = createEvent(key, null, Type.UPDATED);
+        assertFalse(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateDeletedEvent() {
+        String key = "/metadata/sharding_db";
+        String value = "encrypt_db";
+        Optional<GovernanceEvent> actual = createEvent(key, value, Type.DELETED);
+        assertTrue(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateDataSourceChangedEvent() {
+        String key = "/metadata/sharding_db/dataSources";
+        String value = "{}";
+        Optional<GovernanceEvent> actual = createEvent(key, value, Type.UPDATED);
+        assertTrue(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateRuleChangedEvent() {
+        String key = "/metadata/sharding_db/rules";
+        Optional<GovernanceEvent> actual = createEvent(key, "[]", Type.UPDATED);
+        assertTrue(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateCachedEvent() {
+        String key = "/metadata/sharding_db/rules/cache";
+        Optional<GovernanceEvent> actual = createEvent(key, "[]", Type.UPDATED);
+        assertTrue(actual.isPresent());
+    }
+    
+    @Test
+    public void assertCreateSchemaChangedEvent() {
+        String key = "/metadata/sharding_db/schema";
+        Optional<GovernanceEvent> actual = createEvent(key, "{}", Type.UPDATED);
+        assertTrue(actual.isPresent());
+    }
+    
+    private Optional<GovernanceEvent> createEvent(final String key, final String value, final Type type) {
+        DataChangedEvent dataChangedEvent = new DataChangedEvent(key, value, type);
+        Optional<GovernanceEvent> actual = new MetaDataChangedWatcher().createGovernanceEvent(dataChangedEvent);
+        return actual;
     }
 }
