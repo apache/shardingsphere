9b45044020755bc86678f10fe434c86ddae3f780
totalo <totalo@qq.com>
2021-06-20 17:49:37 +0800

    update test case

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/builder/SchemaBuilderTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/builder/SchemaBuilderTest.java
@@ -17,8 +17,10 @@
 
 package org.apache.shardingsphere.infra.metadata.schema.builder;
 
+import lombok.SneakyThrows;
 import org.apache.shardingsphere.infra.config.properties.ConfigurationProperties;
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
+import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
 import org.apache.shardingsphere.infra.metadata.schema.fixture.rule.CommonFixtureRule;
 import org.apache.shardingsphere.infra.metadata.schema.fixture.rule.DataNodeContainedFixtureRule;
 import org.apache.shardingsphere.infra.metadata.schema.model.TableMetaData;
@@ -30,6 +32,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -38,9 +43,24 @@ import java.util.Map;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class SchemaBuilderTest {
+
+    private static final String TEST_CATALOG = "catalog";
+
+    private static final String TEST_SCHEMA = "schema";
+
+    private static final String TABLE_TYPE = "TABLE";
+
+    private static final String VIEW_TYPE = "VIEW";
+
+    private static final String TABLE_NAME = "TABLE_NAME";
+
+    private final String[] singleTableNames = {"single_table1", "single_table2"};
 
     private SchemaBuilderMaterials schemaBuilderMaterials;
     
@@ -66,13 +86,41 @@ public final class SchemaBuilderTest {
         Map<String, TableMetaData> logicTables = actual.values().iterator().next();
         assertThat(actualTables.size(), is(0));
         assertThat(logicTables.size(), is(2));
-        //assertSchemaOfShardingTables(actualTables);
+        assertSchemaOfShardingTables(logicTables);
     }
     
     private void assertSchemaOfShardingTables(final Map<String, TableMetaData> actual) {
         assertTrue(actual.containsKey("data_node_routed_table1"));
-        assertTrue(actual.get("data_node_routed_table1").getColumns().containsKey("id"));
         assertTrue(actual.containsKey("data_node_routed_table2"));
-        assertTrue(actual.get("data_node_routed_table2").getColumns().containsKey("id"));
+    }
+
+    @Test
+    @SneakyThrows(SQLException.class)
+    public void assertBuildOfShardingTablesAndSingleTables() {
+        ResultSet resultSet = mock(ResultSet.class, Answers.RETURNS_DEEP_STUBS);
+        DatabaseMetaData databaseMetaData = mock(DatabaseMetaData.class, Answers.RETURNS_DEEP_STUBS);
+        Connection connection = mock(Connection.class, RETURNS_DEEP_STUBS);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.getMetaData()).thenReturn(databaseMetaData);
+        when(connection.getCatalog()).thenReturn(TEST_CATALOG);
+        when(connection.getSchema()).thenReturn(TEST_SCHEMA);
+        when(databaseType.getSchema(connection)).thenReturn(TEST_SCHEMA);
+        when(databaseMetaData.getTables(connection.getCatalog(), connection.getSchema(), null, new String[]{TABLE_TYPE, VIEW_TYPE})).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, true, true, true, true, false);
+        String[] mockReturnTables = {singleTableNames[1], "data_node_routed_table1_0", "data_node_routed_table1_1", "data_node_routed_table2_0", "data_node_routed_table2_1"};
+        when(resultSet.getString(TABLE_NAME)).thenReturn(singleTableNames[0], mockReturnTables);
+        Map<Map<String, TableMetaData>, Map<String, TableMetaData>> actual = SchemaBuilder.build(schemaBuilderMaterials);
+        Map<String, TableMetaData> actualTables = actual.keySet().iterator().next();
+        Map<String, TableMetaData> logicTables = actual.values().iterator().next();
+        assertThat(actualTables.size(), is(2));
+        assertThat(logicTables.size(), is(2));
+        assertActualOfShardingTablesAndSingleTables(actualTables);
+    }
+
+    private void assertActualOfShardingTablesAndSingleTables(final Map<String, TableMetaData> actual) {
+        assertTrue(actual.containsKey(singleTableNames[0]));
+        assertThat(actual.get(singleTableNames[0]).getColumns().size(), is(0));
+        assertTrue(actual.containsKey(singleTableNames[1]));
+        assertThat(actual.get(singleTableNames[1]).getColumns().size(), is(0));
     }
 }
