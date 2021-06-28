c6ab17c04400a3c475e0a272735e082185ed1527
totalo <totalo@qq.com>
2021-06-28 20:28:23 +0800

    Canonical label name

--- shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/segment/impl/distsql/rdl/ExpectedShardingBindingTableRule.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/segment/impl/distsql/rdl/ExpectedShardingBindingTableRule.java
@@ -30,6 +30,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 @Setter
 public final class ExpectedShardingBindingTableRule extends AbstractExpectedIdentifierSQLSegment {
     
-    @XmlAttribute
-    private String tables;
+    @XmlAttribute(name = "table-groups")
+    private String tableGroups;
 }
