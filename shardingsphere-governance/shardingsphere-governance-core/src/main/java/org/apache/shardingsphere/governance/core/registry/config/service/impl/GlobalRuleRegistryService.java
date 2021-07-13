e3f9d488d847ecd6b25d33caca542b9cd4e56a2c
zhangliang <zhangliang@apache.org>
2021-07-13 14:06:24 +0800

    Remove duplicated methods

--- shardingsphere-governance/shardingsphere-governance-core/src/main/java/org/apache/shardingsphere/governance/core/registry/config/service/impl/GlobalRuleRegistryService.java
+++ shardingsphere-governance/shardingsphere-governance-core/src/main/java/org/apache/shardingsphere/governance/core/registry/config/service/impl/GlobalRuleRegistryService.java
@@ -17,25 +17,17 @@
 
 package org.apache.shardingsphere.governance.core.registry.config.service.impl;
 
-import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
-import com.google.common.eventbus.Subscribe;
-import org.apache.shardingsphere.authority.api.config.AuthorityRuleConfiguration;
 import org.apache.shardingsphere.governance.core.registry.config.node.GlobalNode;
 import org.apache.shardingsphere.governance.core.registry.config.service.GlobalRegistryService;
-import org.apache.shardingsphere.governance.core.registry.state.node.StatesNode;
 import org.apache.shardingsphere.governance.repository.spi.RegistryCenterRepository;
 import org.apache.shardingsphere.infra.config.RuleConfiguration;
 import org.apache.shardingsphere.infra.eventbus.ShardingSphereEventBus;
-import org.apache.shardingsphere.infra.metadata.mapper.event.dcl.impl.CreateUserStatementEvent;
-import org.apache.shardingsphere.infra.metadata.mapper.event.dcl.impl.GrantStatementEvent;
-import org.apache.shardingsphere.infra.metadata.user.yaml.config.YamlUsersConfigurationConverter;
 import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
 import org.apache.shardingsphere.infra.yaml.swapper.YamlRuleConfigurationSwapperEngine;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Optional;
 
 /**
  * Global rule registry service.
@@ -66,32 +58,5 @@ public final class GlobalRuleRegistryService implements GlobalRegistryService<Co
     
     private boolean isExisted() {
         return !Strings.isNullOrEmpty(repository.get(GlobalNode.getGlobalRuleNode()));
-    }
-    
-    /**
-     * Update when user created.
-     *
-     * @param event create user statement event
-     */
-    @Subscribe
-    public void update(final CreateUserStatementEvent event) {
-        Collection<RuleConfiguration> globalRuleConfigs = load();
-        Optional<AuthorityRuleConfiguration> authorityRuleConfig = globalRuleConfigs.stream().filter(each -> each instanceof AuthorityRuleConfiguration)
-                .findAny().map(each -> (AuthorityRuleConfiguration) each);
-        Preconditions.checkState(authorityRuleConfig.isPresent(), "No available authority rules for governance.");
-        authorityRuleConfig.get().getUsers().addAll(event.getUsers());
-        persist(globalRuleConfigs, true);
-    }
-    
-    /**
-     * Update when granted.
-     *
-     * @param event grant statement event
-     */
-    @Subscribe
-    public void update(final GrantStatementEvent event) {
-        if (!event.getUsers().isEmpty()) {
-            repository.persist(StatesNode.getPrivilegeNodePath(), YamlEngine.marshal(YamlUsersConfigurationConverter.convertYamlUserConfigurations(event.getUsers())));
-        }
     }
 }
