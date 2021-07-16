0006d81f78d9f4f2bf76bd481594009bc575ba0a
吴伟杰 <wuweijie@apache.org>
2021-07-16 21:56:24 +0800

    Refactor Proxy PacketCodec (#11370)
    * Refactor PacketCodec
    
    * Complete test cases

--- shardingsphere-db-protocol/shardingsphere-db-protocol-postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/codec/PostgreSQLPacketCodecEngineTest.java
+++ shardingsphere-db-protocol/shardingsphere-db-protocol-postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/codec/PostgreSQLPacketCodecEngineTest.java
@@ -61,18 +61,17 @@ public final class PostgreSQLPacketCodecEngineTest {
     @Test
     public void assertDecode() {
         when(byteBuf.markReaderIndex()).thenReturn(byteBuf);
+        when(byteBuf.readableBytes()).thenReturn(51, 47, 0);
         when(byteBuf.readInt()).thenReturn(50);
         List<Object> out = new LinkedList<>();
-        new PostgreSQLPacketCodecEngine().decode(context, byteBuf, out, 54);
+        new PostgreSQLPacketCodecEngine().decode(context, byteBuf, out);
         assertThat(out.size(), is(1));
     }
     
     @Test
     public void assertDecodeWithStickyPacket() {
-        when(byteBuf.markReaderIndex()).thenReturn(byteBuf);
-        when(byteBuf.readInt()).thenReturn(50);
         List<Object> out = new LinkedList<>();
-        new PostgreSQLPacketCodecEngine().decode(context, byteBuf, out, 40);
+        new PostgreSQLPacketCodecEngine().decode(context, byteBuf, out);
         assertTrue(out.isEmpty());
     }
     
