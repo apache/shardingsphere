ce3bb936316af7b09d426aebaeba05df889c32cd
Liang Zhang <terrymanu@163.com>
2021-07-18 19:47:56 +0800

    Add SchemaMetaDataRegistrySubscriber (#11373)

--- shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/service/SchemaMetaDataPersistServiceTest.java
+++ shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/service/SchemaMetaDataPersistServiceTest.java
@@ -18,22 +18,17 @@
 package org.apache.shardingsphere.governance.core.registry.metadata.service;
 
 import lombok.SneakyThrows;
-import org.apache.shardingsphere.governance.core.registry.metadata.event.DatabaseCreatedSQLNotificationEvent;
-import org.apache.shardingsphere.governance.core.registry.metadata.event.DatabaseDroppedSQLNotificationEvent;
 import org.apache.shardingsphere.governance.core.yaml.schema.pojo.YamlSchema;
 import org.apache.shardingsphere.governance.core.yaml.schema.swapper.SchemaYamlSwapper;
 import org.apache.shardingsphere.governance.repository.spi.RegistryCenterRepository;
 import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
-import org.apache.shardingsphere.infra.metadata.schema.refresher.event.SchemaAlteredEvent;
 import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -53,43 +48,34 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public final class SchemaRegistryServiceTest {
+public final class SchemaMetaDataPersistServiceTest {
     
     @Mock
-    private RegistryCenterRepository registryCenterRepository;
-    
-    private SchemaRegistryService schemaRegistryService;
-    
-    @Before
-    public void setUp() throws ReflectiveOperationException {
-        schemaRegistryService = new SchemaRegistryService(registryCenterRepository);
-        Field field = schemaRegistryService.getClass().getDeclaredField("repository");
-        field.setAccessible(true);
-        field.set(schemaRegistryService, registryCenterRepository);
-    }
+    private RegistryCenterRepository repository;
     
     @Test
     public void assertPersist() {
         ShardingSphereSchema schema = new SchemaYamlSwapper().swapToObject(YamlEngine.unmarshal(readYAML(), YamlSchema.class));
-        schemaRegistryService.persist("foo_db", schema);
-        verify(registryCenterRepository).persist(eq("/metadata/foo_db/schema"), anyString());
+        new SchemaMetaDataPersistService(repository).persist("foo_db", schema);
+        verify(repository).persist(eq("/metadata/foo_db/schema"), anyString());
     }
     
     @Test
     public void assertDelete() {
-        schemaRegistryService.delete("foo_db");
-        verify(registryCenterRepository).delete(eq("/metadata/foo_db"));
+        new SchemaMetaDataPersistService(repository).delete("foo_db");
+        verify(repository).delete("/metadata/foo_db");
     }
     
     @Test
     public void assertLoad() {
-        when(registryCenterRepository.get("/metadata/foo_db/schema")).thenReturn(readYAML());
-        Optional<ShardingSphereSchema> schemaOptional = schemaRegistryService.load("foo_db");
+        SchemaMetaDataPersistService schemaMetaDataPersistService = new SchemaMetaDataPersistService(repository);
+        when(repository.get("/metadata/foo_db/schema")).thenReturn(readYAML());
+        Optional<ShardingSphereSchema> schemaOptional = schemaMetaDataPersistService.load("foo_db");
         assertTrue(schemaOptional.isPresent());
-        Optional<ShardingSphereSchema> empty = schemaRegistryService.load("test");
+        Optional<ShardingSphereSchema> empty = schemaMetaDataPersistService.load("test");
         assertThat(empty, is(Optional.empty()));
         ShardingSphereSchema schema = schemaOptional.get();
-        verify(registryCenterRepository).get(eq("/metadata/foo_db/schema"));
+        verify(repository).get(eq("/metadata/foo_db/schema"));
         assertThat(schema.getAllTableNames(), is(Collections.singleton("t_order")));
         assertThat(schema.get("t_order").getIndexes().keySet(), is(Collections.singleton("primary")));
         assertThat(schema.getAllColumnNames("t_order").size(), is(1));
@@ -98,32 +84,11 @@ public final class SchemaRegistryServiceTest {
     
     @Test
     public void assertLoadAllNames() {
-        when(registryCenterRepository.getChildrenKeys("/metadata")).thenReturn(Arrays.asList("foo_db", "bar_db"));
-        Collection<String> actual = schemaRegistryService.loadAllNames();
+        when(repository.getChildrenKeys("/metadata")).thenReturn(Arrays.asList("foo_db", "bar_db"));
+        Collection<String> actual = new SchemaMetaDataPersistService(repository).loadAllNames();
         assertThat(actual.size(), is(2));
         assertThat(actual, hasItems("foo_db"));
         assertThat(actual, hasItems("bar_db"));
-    }
-    
-    @Test
-    public void assertUpdateWithDatabaseCreatedSQLNotificationEvent() {
-        DatabaseCreatedSQLNotificationEvent event = new DatabaseCreatedSQLNotificationEvent("bar_db");
-        schemaRegistryService.update(event);
-        verify(registryCenterRepository).persist("/metadata/bar_db", "");
-    }
-    
-    @Test
-    public void assertUpdateWithMetaDataAlteredEvent() {
-        SchemaAlteredEvent event = new SchemaAlteredEvent("foo_db", new SchemaYamlSwapper().swapToObject(YamlEngine.unmarshal(readYAML(), YamlSchema.class)));
-        schemaRegistryService.update(event);
-        verify(registryCenterRepository).persist(eq("/metadata/foo_db/schema"), anyString());
-    }
-    
-    @Test
-    public void assertUpdateWithDatabaseDroppedSQLNotificationEvent() {
-        DatabaseDroppedSQLNotificationEvent event = new DatabaseDroppedSQLNotificationEvent("foo_db");
-        schemaRegistryService.update(event);
-        verify(registryCenterRepository).delete("/metadata/foo_db");
     }
     
     @SneakyThrows({IOException.class, URISyntaxException.class})
