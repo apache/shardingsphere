f903fe40dbdc3549ebe26faf8f1960059edcb816
RaigorJiang <raigor.jiang@gmail.com>
2021-07-23 15:02:53 +0800

    Fixes #10673, execute DCL without use schema.

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/test/java/org/apache/shardingsphere/proxy/backend/text/data/DatabaseBackendHandlerFactoryTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/test/java/org/apache/shardingsphere/proxy/backend/text/data/DatabaseBackendHandlerFactoryTest.java
@@ -25,6 +25,7 @@ import org.apache.shardingsphere.proxy.backend.text.data.impl.UnicastDatabaseBac
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.SetStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dcl.DCLStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
 import org.junit.Test;
 
@@ -69,5 +70,14 @@ public final class DatabaseBackendHandlerFactoryTest {
         when(context.getSqlStatement()).thenReturn(mock(SQLStatement.class));
         DatabaseBackendHandler actual = DatabaseBackendHandlerFactory.newInstance(context, sql, mock(BackendConnection.class));
         assertThat(actual, instanceOf(SchemaAssignedDatabaseBackendHandler.class));
+    }
+    
+    @Test
+    public void assertNewInstanceWithDCLStatement() {
+        String sql = "CREATE USER test IDENTIFIED BY '123456'";
+        SQLStatementContext<DCLStatement> context = mock(SQLStatementContext.class);
+        when(context.getSqlStatement()).thenReturn(mock(DCLStatement.class));
+        DatabaseBackendHandler actual = DatabaseBackendHandlerFactory.newInstance(context, sql, mock(BackendConnection.class));
+        assertThat(actual, instanceOf(BroadcastDatabaseBackendHandler.class));
     }
 }
