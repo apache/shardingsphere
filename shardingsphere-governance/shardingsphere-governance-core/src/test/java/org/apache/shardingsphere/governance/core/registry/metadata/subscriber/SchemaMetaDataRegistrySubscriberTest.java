e03782c339de27baf2f519ca952356c20b99184f
zhangliang <zhangliang@apache.org>
2021-07-25 14:54:02 +0800

    Use direct call to instead of event post on CreateDatabaseBackendHandler

--- shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/subscriber/SchemaMetaDataRegistrySubscriberTest.java
+++ shardingsphere-governance/shardingsphere-governance-core/src/test/java/org/apache/shardingsphere/governance/core/registry/metadata/subscriber/SchemaMetaDataRegistrySubscriberTest.java
@@ -17,10 +17,9 @@
 
 package org.apache.shardingsphere.governance.core.registry.metadata.subscriber;
 
-import org.apache.shardingsphere.governance.core.registry.metadata.event.DatabaseCreatedSQLNotificationEvent;
 import org.apache.shardingsphere.governance.core.registry.metadata.event.DatabaseDroppedSQLNotificationEvent;
-import org.apache.shardingsphere.infra.config.persist.service.SchemaMetaDataPersistService;
 import org.apache.shardingsphere.governance.repository.spi.RegistryCenterRepository;
+import org.apache.shardingsphere.infra.config.persist.service.SchemaMetaDataPersistService;
 import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
 import org.apache.shardingsphere.infra.metadata.schema.refresher.event.SchemaAlteredEvent;
 import org.junit.Before;
@@ -48,13 +47,6 @@ public final class SchemaMetaDataRegistrySubscriberTest {
         Field field = schemaMetaDataRegistrySubscriber.getClass().getDeclaredField("persistService");
         field.setAccessible(true);
         field.set(schemaMetaDataRegistrySubscriber, persistService);
-    }
-    
-    @Test
-    public void assertUpdateWithDatabaseCreatedSQLNotificationEvent() {
-        DatabaseCreatedSQLNotificationEvent event = new DatabaseCreatedSQLNotificationEvent("bar_db");
-        schemaMetaDataRegistrySubscriber.update(event);
-        verify(persistService).persist("bar_db", null);
     }
     
     @Test
