b6d3713bd4a44abfa754fc24b77ac0205a3351db
Dachuan J <jpc00939@aliyun.com>
2021-07-28 16:43:38 +0800

    Add proxy state, uptime and more sql or dist sql metrics

--- shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-metrics/shardingsphere-agent-metrics-api/src/main/java/org/apache/shardingsphere/agent/metrics/api/advice/SQLRouteEngineAdvice.java
+++ shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-metrics/shardingsphere-agent-metrics-api/src/main/java/org/apache/shardingsphere/agent/metrics/api/advice/SQLRouteEngineAdvice.java
@@ -17,44 +17,73 @@
 
 package org.apache.shardingsphere.agent.metrics.api.advice;
 
-import java.lang.reflect.Method;
-import java.util.Collection;
+import org.apache.shardingsphere.agent.api.advice.AdviceTargetObject;
 import org.apache.shardingsphere.agent.api.advice.InstanceMethodAroundAdvice;
 import org.apache.shardingsphere.agent.api.result.MethodInvocationResult;
-import org.apache.shardingsphere.agent.api.advice.AdviceTargetObject;
 import org.apache.shardingsphere.agent.metrics.api.reporter.MetricsReporter;
+import org.apache.shardingsphere.distsql.parser.statement.ral.RALStatement;
+import org.apache.shardingsphere.distsql.parser.statement.rdl.RDLStatement;
+import org.apache.shardingsphere.distsql.parser.statement.rql.RQLStatement;
 import org.apache.shardingsphere.infra.binder.LogicSQL;
 import org.apache.shardingsphere.infra.route.context.RouteContext;
 import org.apache.shardingsphere.infra.route.context.RouteMapper;
 import org.apache.shardingsphere.infra.route.context.RouteUnit;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dcl.DCLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.ddl.DDLStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.DeleteStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.InsertStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.UpdateStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.tcl.TCLStatement;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
 
 /**
  * SQL route engine advice.
  */
 public final class SQLRouteEngineAdvice implements InstanceMethodAroundAdvice {
     
-    private static final String SELECT = "sql_select_total";
+    private static final String SELECT = "sql_dml_select_total";
     
-    private static final String UPDATE = "sql_update_total";
+    private static final String UPDATE = "sql_dml_update_total";
     
-    private static final String DELETE = "sql_delete_total";
+    private static final String DELETE = "sql_dml_delete_total";
     
-    private static final String INSERT = "sql_insert_total";
+    private static final String INSERT = "sql_dml_insert_total";
+    
+    private static final String SQL_DDL = "sql_ddl_total";
+    
+    private static final String SQL_DCL = "sql_dcl_total";
+    
+    private static final String SQL_DAL = "sql_dal_total";
+    
+    private static final String SQL_TCL = "sql_tcl_total";
+    
+    private static final String DIST_SQL_RQL = "dist_sql_rql_total";
+    
+    private static final String DIST_SQL_RDL = "dist_sql_rdl_total";
+    
+    private static final String DIST_SQL_RAL = "dist_sql_ral_total";
     
     private static final String ROUTE_DATASOURCE = "route_datasource";
     
     private static final String ROUTE_TABLE = "route_table";
     
     static {
-        MetricsReporter.registerCounter(SELECT, "the shardingsphere proxy executor select sql total");
-        MetricsReporter.registerCounter(UPDATE, "the shardingsphere proxy executor update sql total");
-        MetricsReporter.registerCounter(DELETE, "the shardingsphere proxy executor delete sql total");
         MetricsReporter.registerCounter(INSERT, "the shardingsphere proxy executor insert sql total");
+        MetricsReporter.registerCounter(DELETE, "the shardingsphere proxy executor delete sql total");
+        MetricsReporter.registerCounter(UPDATE, "the shardingsphere proxy executor update sql total");
+        MetricsReporter.registerCounter(SELECT, "the shardingsphere proxy executor select sql total");
+        MetricsReporter.registerCounter(SQL_DDL, "the shardingsphere proxy executor ddl sql total");
+        MetricsReporter.registerCounter(SQL_DCL, "the shardingsphere proxy executor dcl sql total");
+        MetricsReporter.registerCounter(SQL_DAL, "the shardingsphere proxy executor dal sql total");
+        MetricsReporter.registerCounter(SQL_TCL, "the shardingsphere proxy executor tcl sql total");
+        MetricsReporter.registerCounter(DIST_SQL_RQL, "the shardingsphere proxy executor rql dist sql total");
+        MetricsReporter.registerCounter(DIST_SQL_RDL, "the shardingsphere proxy executor rdl dist total");
+        MetricsReporter.registerCounter(DIST_SQL_RAL, "the shardingsphere proxy executor ral dist sql total");
         MetricsReporter.registerCounter(ROUTE_DATASOURCE, new String[] {"name"}, "the shardingsphere proxy route datasource");
         MetricsReporter.registerCounter(ROUTE_TABLE, new String[] {"name"}, "the shardingsphere proxy route table");
     }
@@ -71,6 +100,20 @@ public final class SQLRouteEngineAdvice implements InstanceMethodAroundAdvice {
             MetricsReporter.counterIncrement(UPDATE);
         } else if (sqlStatement instanceof SelectStatement) {
             MetricsReporter.counterIncrement(SELECT);
+        } else if (sqlStatement instanceof DDLStatement) {
+            MetricsReporter.counterIncrement(SQL_DDL);
+        } else if (sqlStatement instanceof DCLStatement) {
+            MetricsReporter.counterIncrement(SQL_DCL);
+        } else if (sqlStatement instanceof DALStatement) {
+            MetricsReporter.counterIncrement(SQL_DAL);
+        } else if (sqlStatement instanceof TCLStatement) {
+            MetricsReporter.counterIncrement(SQL_TCL);
+        } else if (sqlStatement instanceof RQLStatement) {
+            MetricsReporter.counterIncrement(DIST_SQL_RQL);
+        } else if (sqlStatement instanceof RDLStatement) {
+            MetricsReporter.counterIncrement(DIST_SQL_RDL);
+        } else if (sqlStatement instanceof RALStatement) {
+            MetricsReporter.counterIncrement(DIST_SQL_RAL);
         }
     }
 
---
+++ shardingsphere-agent/shardingsphere-agent-plugins/shardingsphere-agent-plugin-metrics/shardingsphere-agent-metrics-prometheus/src/main/java/org/apache/shardingsphere/agent/metrics/prometheus/collector/ProxyInfoCollector.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.agent.metrics.prometheus.collector;
+
+import io.prometheus.client.Collector;
+import io.prometheus.client.GaugeMetricFamily;
+import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
+import org.apache.shardingsphere.proxy.frontend.ShardingSphereProxy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Proxy info collector.
+ */
+public final class ProxyInfoCollector extends Collector {
+    
+    private static final String PROXY_STATE = "state";
+    
+    private static final String PROXY_UP_TIME = "uptime";
+    
+    @Override
+    public List<Collector.MetricFamilySamples> collect() {
+        List<Collector.MetricFamilySamples> result = new ArrayList<>(1);
+        if (!checkProxy()) {
+            return result;
+        }
+        GaugeMetricFamily proxyInfo = new GaugeMetricFamily(
+                "proxy_info",
+                "Proxy information.",
+                Arrays.asList("name"));
+        proxyInfo.addMetric(Arrays.asList(PROXY_STATE), ProxyContext.getInstance().getStateContext().getCurrentState().ordinal());
+        Date now = new Date();
+        proxyInfo.addMetric(Arrays.asList(PROXY_UP_TIME), now.getTime() - ShardingSphereProxy.getStartTime());
+        result.add(proxyInfo);
+        return result;
+    }
+    
+    private boolean checkProxy() {
+        try {
+            Class.forName("org.apache.shardingsphere.proxy.frontend.ShardingSphereProxy");
+        } catch (ClassNotFoundException ex) {
+            return false;
+        }
+        return true;
+    }
+}
