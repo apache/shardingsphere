ceb4eee90a4410ed91942b3681cb6f49a33404c4
fwhdzh <1064039735@qq.com>
2021-08-06 10:53:45 +0800

    Modify the test code format of ExecuteProcessEngine. Add a test for ExecuteProcessEngine.finish(final String executionID, final SQLExecutionUnit executionUnit).

--- shardingsphere-infra/shardingsphere-infra-executor/src/test/java/org/apache/shardingsphere/infra/executor/sql/process/ExecuteProcessEngineTest.java
+++ shardingsphere-infra/shardingsphere-infra-executor/src/test/java/org/apache/shardingsphere/infra/executor/sql/process/ExecuteProcessEngineTest.java
@@ -29,58 +29,53 @@ import org.apache.shardingsphere.infra.executor.kernel.model.ExecutorDataMap;
 import org.apache.shardingsphere.infra.executor.sql.context.ExecutionUnit;
 import org.apache.shardingsphere.infra.executor.sql.context.SQLUnit;
 import org.apache.shardingsphere.infra.executor.sql.execute.engine.SQLExecutionUnit;
+import org.apache.shardingsphere.infra.executor.sql.execute.engine.raw.RawSQLExecutionUnit;
 import org.apache.shardingsphere.infra.executor.sql.process.fixture.ExecuteProcessReporterFixture;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.item.ProjectionsSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.pagination.limit.LimitSegment;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml.MySQLSelectStatement;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 import java.util.List;
 import java.util.LinkedList;
-import java.util.ArrayList;
 
+import org.apache.shardingsphere.infra.executor.sql.execute.engine.ConnectionMode;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 
 public final class ExecuteProcessEngineTest {
 
-    private static final String SQL = "SELECT * FROM t_user";
-
     private ExecutionGroupContext<? extends SQLExecutionUnit> executionGroupContext;
 
-    @Test
-    public void assertExecuteIDCanBePutAndRemoved() {
-
-        Map<String, Object> map = ExecutorDataMap.getValue();
-
+    @Before
+    public void setUp() {
         LogicSQL logicSQL = createLogicSQL();
         executionGroupContext = createMockedExecutionGroups(2, 2);
         ConfigurationProperties actual = createConfigurationProperties();
-
         ExecuteProcessEngine.initialize(logicSQL, executionGroupContext, actual);
         assertTrue(executionGroupContext.getExecutionID().equals(ExecutorDataMap.getValue().get("EXECUTE_ID")));
         assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(0).equals("Report the summary of this task."));
+    }
+
+    @Test
+    public void assertFinish() {
+        ExecutionUnit executionUnit = new ExecutionUnit("actualName1", new SQLUnit("sql1", Collections.singletonList("parameter1")));
+        RawSQLExecutionUnit rawExecutionUnit = new RawSQLExecutionUnit(executionUnit, ConnectionMode.MEMORY_STRICTLY);
+        ExecuteProcessEngine.finish(executionGroupContext.getExecutionID(), rawExecutionUnit);
+        assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(1).equals("Report a unit of this task."));
         ExecuteProcessEngine.finish(executionGroupContext.getExecutionID());
-        assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(1).equals("Report this task on completion."));
-        ExecuteProcessEngine.clean();
-        assertTrue(ExecutorDataMap.getValue().size() == 0);
+        assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(2).equals("Report this task on completion."));
+    }
 
-        LogicSQL logicSQL2 = createLogicSQL();
-        ExecutionGroupContext<? extends SQLExecutionUnit> executionGroupContext2 = createMockedExecutionGroups(2, 2);
-        ConfigurationProperties actual2 = createConfigurationProperties();
-
-        ExecuteProcessEngine.initialize(logicSQL2, executionGroupContext2, actual2);
-        assertTrue(executionGroupContext2.getExecutionID().equals(ExecutorDataMap.getValue().get("EXECUTE_ID")));
-        assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(2).equals("Report the summary of this task."));
-        ExecuteProcessEngine.finish(executionGroupContext2.getExecutionID());
-        assertTrue(ExecuteProcessReporterFixture.ACTIONS.get(3).equals("Report this task on completion."));
+    @Test
+    public void assertClean() {
         ExecuteProcessEngine.clean();
         assertTrue(ExecutorDataMap.getValue().size() == 0);
     }
@@ -90,7 +85,6 @@ public final class ExecuteProcessEngineTest {
         MySQLSelectStatement selectStatement = new MySQLSelectStatement();
         selectStatement.setLimit(new LimitSegment(0, 10, null, null));
         selectStatement.setProjections(projectionsSegment);
-
         SQLStatementContext<?> sqlStatementContext = SQLStatementContextFactory.newInstance(mockMetaDataMap(), Collections.emptyList(), selectStatement, DefaultSchema.LOGIC_NAME);
         LogicSQL logicSQL = new LogicSQL(sqlStatementContext, null, null);
         return logicSQL;
@@ -106,10 +100,6 @@ public final class ExecuteProcessEngineTest {
 
     private Map<String, ShardingSphereMetaData> mockMetaDataMap() {
         return Collections.singletonMap(DefaultSchema.LOGIC_NAME, mock(ShardingSphereMetaData.class, RETURNS_DEEP_STUBS));
-    }
-
-    private Collection<ExecutionUnit> mockExecutionUnits(final Collection<String> dataSourceNames, final String sql) {
-        return dataSourceNames.stream().map(each -> new ExecutionUnit(each, new SQLUnit(sql, new ArrayList<>()))).collect(Collectors.toList());
     }
 
     private ExecutionGroupContext<? extends SQLExecutionUnit> createMockedExecutionGroups(final int groupSize, final int unitSize) {
