913d25633cd2d2b62cd39fe95706e1e15ed9f009
carl.che <carl.che@voyageone.cn>
2021-08-11 05:02:07 +0800

    format code

--- shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-distsql/shardingsphere-readwrite-splitting-distsql-handler/src/test/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/converter/ReadwriteSplittingRuleStatementConverterTest.java
+++ shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-distsql/shardingsphere-readwrite-splitting-distsql-handler/src/test/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/converter/ReadwriteSplittingRuleStatementConverterTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.readwritesplitting.distsql.handler.converter;
 
 import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmConfiguration;
@@ -6,13 +23,19 @@ import org.apache.shardingsphere.readwritesplitting.api.rule.ReadwriteSplittingD
 import org.apache.shardingsphere.readwritesplitting.distsql.parser.segment.ReadwriteSplittingRuleSegment;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public final class ReadwriteSplittingRuleStatementConverterTest {
-
 
     @Test
     public void convertTest() {
@@ -32,13 +55,13 @@ public final class ReadwriteSplittingRuleStatementConverterTest {
         Properties properties4 = new Properties();
         properties4.setProperty("ping3", "pong3");
         properties4.setProperty("ping4", "pong4");
-        List<ReadwriteSplittingRuleSegment> multipleReadwriteSplittingRuleSegments = Arrays.asList(createReadwriteSplittingRuleSegment("name2", "write_ds_02", Arrays.asList("read_ds_02", "read_ds_03"), "lb02", properties2),
+        List<ReadwriteSplittingRuleSegment> multipleReadwriteSplittingRuleSegments = Arrays.asList(
+                createReadwriteSplittingRuleSegment("name2", "write_ds_02", Arrays.asList("read_ds_02", "read_ds_03"), "lb02", properties2),
                 createReadwriteSplittingRuleSegment("name3", "write_ds_03", Arrays.asList("read_ds_04", "read_ds_05"), "lb03", properties3),
                 createReadwriteSplittingRuleSegment("name4", "write_ds_04", Arrays.asList("read_ds_06", "read_ds_07"), "lb04", properties4));
         ReadwriteSplittingRuleConfiguration multipleRuleSegmentConvertResult = ReadwriteSplittingRuleStatementConverter.convert(multipleReadwriteSplittingRuleSegments);
         assertMultipleReadwriteSplittingRuleSegments(multipleReadwriteSplittingRuleSegments, multipleRuleSegmentConvertResult);
     }
-
 
     private ReadwriteSplittingRuleSegment createReadwriteSplittingRuleSegment(final String name,
                                                                               final String writeDataSource,
@@ -48,31 +71,32 @@ public final class ReadwriteSplittingRuleStatementConverterTest {
         return new ReadwriteSplittingRuleSegment(name, writeDataSource, readDataSourceList, loadBalancerTypeName, properties);
     }
 
-    private void assertEmptyRuleSegmentConvertResult(ReadwriteSplittingRuleConfiguration emptyRuleSegmentConvertResult) {
+    private void assertEmptyRuleSegmentConvertResult(final ReadwriteSplittingRuleConfiguration emptyRuleSegmentConvertResult) {
         assertTrue(emptyRuleSegmentConvertResult.getDataSources().isEmpty());
         assertTrue(emptyRuleSegmentConvertResult.getLoadBalancers().isEmpty());
     }
 
-    private void assertSingleRuleSegmentConvertResult(ReadwriteSplittingRuleSegment readwriteSplittingRuleSegment, ReadwriteSplittingRuleConfiguration singleRuleSegmentConvertResult) {
+    private void assertSingleRuleSegmentConvertResult(final ReadwriteSplittingRuleSegment readwriteSplittingRuleSegment,
+                                                      final ReadwriteSplittingRuleConfiguration singleRuleSegmentConvertResult) {
         Collection<ReadwriteSplittingDataSourceRuleConfiguration> singleRuleSegmentConvertResultDataSources = singleRuleSegmentConvertResult.getDataSources();
         Map<String, ShardingSphereAlgorithmConfiguration> singleRuleSegmentConvertResultLoadBalancers = singleRuleSegmentConvertResult.getLoadBalancers();
         assertEquals(1, singleRuleSegmentConvertResultDataSources.size());
         assertEquals(1, singleRuleSegmentConvertResultLoadBalancers.size());
         Object[] dataSources = singleRuleSegmentConvertResultDataSources.toArray();
         ReadwriteSplittingDataSourceRuleConfiguration ruleConfiguration = (ReadwriteSplittingDataSourceRuleConfiguration) dataSources[0];
-        String loadBalancerName = singleRuleSegmentConvertResultLoadBalancers.keySet().toArray()[0].toString();
-        ShardingSphereAlgorithmConfiguration sphereAlgorithmConfiguration = (ShardingSphereAlgorithmConfiguration) singleRuleSegmentConvertResultLoadBalancers.values().toArray()[0];
         assertEquals(ruleConfiguration.getName(), readwriteSplittingRuleSegment.getName());
         assertThat(ruleConfiguration.getLoadBalancerName(), org.hamcrest.Matchers.containsString(readwriteSplittingRuleSegment.getLoadBalancer()));
         assertEquals(ruleConfiguration.getWriteDataSourceName(), readwriteSplittingRuleSegment.getWriteDataSource());
         assertEquals(ruleConfiguration.getReadDataSourceNames(), readwriteSplittingRuleSegment.getReadDataSources());
+        String loadBalancerName = singleRuleSegmentConvertResultLoadBalancers.keySet().toArray()[0].toString();
         assertThat(loadBalancerName, org.hamcrest.Matchers.containsString(readwriteSplittingRuleSegment.getLoadBalancer()));
+        ShardingSphereAlgorithmConfiguration sphereAlgorithmConfiguration = (ShardingSphereAlgorithmConfiguration) singleRuleSegmentConvertResultLoadBalancers.values().toArray()[0];
         assertThat(sphereAlgorithmConfiguration.getType(), org.hamcrest.Matchers.containsString(readwriteSplittingRuleSegment.getLoadBalancer()));
         assertEquals(sphereAlgorithmConfiguration.getProps(), readwriteSplittingRuleSegment.getProps());
     }
 
-
-    private void assertMultipleReadwriteSplittingRuleSegments(List<ReadwriteSplittingRuleSegment> readwriteSplittingRuleSegmentList, ReadwriteSplittingRuleConfiguration multipleReadwriteSplittingRuleSegments) {
+    private void assertMultipleReadwriteSplittingRuleSegments(final List<ReadwriteSplittingRuleSegment> readwriteSplittingRuleSegmentList,
+                                                              final ReadwriteSplittingRuleConfiguration multipleReadwriteSplittingRuleSegments) {
         Collection<ReadwriteSplittingDataSourceRuleConfiguration> singleRuleSegmentConvertResultDataSources = multipleReadwriteSplittingRuleSegments.getDataSources();
         Map<String, ShardingSphereAlgorithmConfiguration> singleRuleSegmentConvertResultLoadBalancers = multipleReadwriteSplittingRuleSegments.getLoadBalancers();
         assertEquals(readwriteSplittingRuleSegmentList.size(), singleRuleSegmentConvertResultDataSources.size());
@@ -94,6 +118,4 @@ public final class ReadwriteSplittingRuleStatementConverterTest {
                     assertEquals(sphereAlgorithmConfiguration.getProps(), readwriteSplittingRuleSegment.getProps());
                 });
     }
-
-
 }
