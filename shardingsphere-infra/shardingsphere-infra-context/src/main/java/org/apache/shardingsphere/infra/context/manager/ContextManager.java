954e1eb49153407d90f9885c17b905231aa1610e
zhangliang <zhangliang@apache.org>
2021-08-14 21:37:06 +0800

    Add ContextManager

---
+++ shardingsphere-infra/shardingsphere-infra-context/src/main/java/org/apache/shardingsphere/infra/context/manager/ContextManager.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.context.manager;
+
+import org.apache.shardingsphere.infra.context.metadata.impl.StandardMetaDataContexts;
+import org.apache.shardingsphere.infra.lock.ShardingSphereLock;
+import org.apache.shardingsphere.transaction.context.impl.StandardTransactionContexts;
+
+import java.util.Optional;
+
+/**
+ * Context manager.
+ */
+public interface ContextManager {
+    
+    /**
+     * Initialize context manager.
+     * 
+     * @param metaDataContexts meta data contexts
+     * @param transactionContexts transaction contexts
+     */
+    void init(StandardMetaDataContexts metaDataContexts, StandardTransactionContexts transactionContexts);
+    
+    /**
+     * Get meta data contexts.
+     * 
+     * @return meta data contexts
+     */
+    StandardMetaDataContexts getMetaDataContexts();
+    
+    /**
+     * Renew meta data contexts.
+     *
+     * @param metaDataContexts meta data contexts
+     */
+    void renewMetaDataContexts(StandardMetaDataContexts metaDataContexts);
+    
+    /**
+     * Get transaction contexts.
+     *
+     * @return transaction contexts
+     */
+    StandardTransactionContexts getTransactionContexts();
+    
+    /**
+     * Renew transaction contexts.
+     *
+     * @param transactionContexts transaction contexts
+     */
+    void renewTransactionContexts(StandardTransactionContexts transactionContexts);
+    
+    /**
+     * Get lock.
+     *
+     * @return lock
+     */
+    Optional<ShardingSphereLock> getLock();
+}
