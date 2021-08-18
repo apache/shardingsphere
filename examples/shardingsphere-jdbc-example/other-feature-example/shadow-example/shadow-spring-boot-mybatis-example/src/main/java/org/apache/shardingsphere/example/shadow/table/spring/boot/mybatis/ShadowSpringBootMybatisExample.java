f7b217a426726cbe7ca5404e0963fb1762906063
yx9o <yangx_soft@163.com>
2021-08-18 13:45:40 +0800

    Refactor shadow-spring-boot-example

--- examples/shardingsphere-jdbc-example/other-feature-example/shadow-example/shadow-spring-boot-mybatis-example/src/main/java/org/apache/shardingsphere/example/shadow/table/spring/boot/mybatis/ShadowSpringBootMybatisExample.java
+++ examples/shardingsphere-jdbc-example/other-feature-example/shadow-example/shadow-spring-boot-mybatis-example/src/main/java/org/apache/shardingsphere/example/shadow/table/spring/boot/mybatis/ShadowSpringBootMybatisExample.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.example.shadow.table.spring.boot;
+package org.apache.shardingsphere.example.shadow.table.spring.boot.mybatis;
 
 import org.apache.shardingsphere.example.core.api.ExampleExecuteTemplate;
 import org.apache.shardingsphere.example.core.api.service.ExampleService;
@@ -31,10 +31,10 @@ import java.sql.SQLException;
 @ComponentScan("org.apache.shardingsphere.example.core.mybatis")
 @MapperScan(basePackages = "org.apache.shardingsphere.example.core.mybatis.repository")
 @SpringBootApplication(exclude = JtaAutoConfiguration.class)
-public class ShadowSpringBootExample {
+public class ShadowSpringBootMybatisExample {
     
     public static void main(final String[] args) throws SQLException {
-        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(ShadowSpringBootExample.class, args)) {
+        try (ConfigurableApplicationContext applicationContext = SpringApplication.run(ShadowSpringBootMybatisExample.class, args)) {
             ExampleExecuteTemplate.run(applicationContext.getBean("shadow", ExampleService.class));
         }
     }
