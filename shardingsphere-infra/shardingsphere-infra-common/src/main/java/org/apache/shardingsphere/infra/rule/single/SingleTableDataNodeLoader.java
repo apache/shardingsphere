9220faec58a654eb135e482fb4b53516cc1b3f2e
strongduanmu <duanzhengqiang@apache.org>
2021-08-18 10:54:03 +0800

    rename properties key

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/rule/single/SingleTableDataNodeLoader.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/rule/single/SingleTableDataNodeLoader.java
@@ -52,12 +52,12 @@ public final class SingleTableDataNodeLoader {
     public static Map<String, SingleTableDataNode> load(final DatabaseType databaseType, final Map<String, DataSource> dataSourceMap, 
                                                         final Collection<String> excludedTables, final ConfigurationProperties props) {
         Map<String, SingleTableDataNode> result = new HashMap<>();
-        boolean isCheckingSingleTable = props.getValue(ConfigurationPropertyKey.CHECK_SINGLE_TABLE_ENABLED);
+        boolean checkSingleTableDuplicate = props.getValue(ConfigurationPropertyKey.CHECK_SINGLE_TABLE_DUPLICATE_ENABLED);
         for (Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
             Map<String, SingleTableDataNode> dataNodeMap = load(databaseType, entry.getKey(), entry.getValue(), excludedTables);
             for (String each : dataNodeMap.keySet()) {
                 SingleTableDataNode existDataNode = result.putIfAbsent(each, dataNodeMap.get(each));
-                if (isCheckingSingleTable) {
+                if (checkSingleTableDuplicate) {
                     Preconditions.checkState(null == existDataNode, "Single table conflict, there are multiple tables `%s` existed.", each);
                 }
             }
