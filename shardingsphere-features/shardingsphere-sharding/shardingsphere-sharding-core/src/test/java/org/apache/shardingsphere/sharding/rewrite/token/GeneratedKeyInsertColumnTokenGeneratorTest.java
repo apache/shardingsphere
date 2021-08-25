8cb515e72c6bb78dcbd94ff6aa9b0411aa829888
fwhdzh <43179325+fwhdzh@users.noreply.github.com>
2021-08-25 10:08:11 +0800

    Issue10968 (#11957)
    * Migrate YAML Configuration change history from
    4.1.2.Configuration Manual->YAML Configuration->Change History to
    7.2.API Change History->7.2.1.ShardingSphere-JDBC->YAML Configuration;
    
    * Add content of change history
    
    * add unit test of ShardingGeneratedKeyInsertValueParameterRewriter.
    
    * define the constant as static final.
    
    * Add test code of ShardingPaginationParameterRewriter and GeneratedKeyAssignmentTokenGenerator.
    remove ShardingGeneratedKeyInsertValueParameterRewriterTest from *.rewrite to *.rewrite.parameter.
    
    * add final.
    
    * Add unit test of GeneratedKeyForUseDefaultInsertColumnsTokenGenerator, GeneratedKeyInsertColumnTokenGenerator and GeneratedKeyInsertValuesTokenGenerator.
    
    * Add unit test of AggregationDistinctTokenGenerator.

---
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/rewrite/token/GeneratedKeyInsertColumnTokenGeneratorTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sharding.rewrite.token;
+
+import org.apache.shardingsphere.infra.binder.segment.insert.keygen.GeneratedKeyContext;
+import org.apache.shardingsphere.infra.binder.statement.dml.InsertStatementContext;
+import org.apache.shardingsphere.sharding.rewrite.token.generator.impl.keygen.GeneratedKeyInsertColumnTokenGenerator;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.InsertColumnsSegment;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class GeneratedKeyInsertColumnTokenGeneratorTest {
+
+    private static final int TEST_STOP_INDEX = 4;
+
+    private static final String TEST_COLUMN_NAME = "TEST_COLUMN_NAME";
+
+    @Test
+    public void assertGenerateSQLToken() {
+        GeneratedKeyContext generatedKeyContext = mock(GeneratedKeyContext.class);
+        when(generatedKeyContext.getColumnName()).thenReturn(TEST_COLUMN_NAME);
+        InsertStatementContext insertStatementContext = mock(InsertStatementContext.class, RETURNS_DEEP_STUBS);
+        when(insertStatementContext.getGeneratedKeyContext()).thenReturn(Optional.of(generatedKeyContext));
+        InsertColumnsSegment insertColumnsSegment = mock(InsertColumnsSegment.class);
+        when(insertColumnsSegment.getStopIndex()).thenReturn(TEST_STOP_INDEX);
+        when(insertStatementContext.getSqlStatement().getInsertColumns()).thenReturn(Optional.of(insertColumnsSegment));
+        GeneratedKeyInsertColumnTokenGenerator generatedKeyInsertColumnTokenGenerator = new GeneratedKeyInsertColumnTokenGenerator();
+        assertThat(generatedKeyInsertColumnTokenGenerator.generateSQLToken(insertStatementContext).toString(), is(", " + TEST_COLUMN_NAME));
+    }
+}
