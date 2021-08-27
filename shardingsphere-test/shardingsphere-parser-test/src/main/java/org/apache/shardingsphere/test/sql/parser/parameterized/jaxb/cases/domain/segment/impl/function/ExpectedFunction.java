0a9fb000feb46c7661d8665393592752466c947e
yx9o <yangx_soft@163.com>
2021-08-27 10:49:29 +0800

    Remove blank lines

--- shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/segment/impl/function/ExpectedFunction.java
+++ shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/segment/impl/function/ExpectedFunction.java
@@ -30,8 +30,7 @@ import javax.xml.bind.annotation.XmlElement;
 @Getter
 @Setter
 public final class ExpectedFunction extends AbstractExpectedIdentifierSQLSegment {
-
+    
     @XmlElement
     private ExpectedOwner owner;
 }
-
