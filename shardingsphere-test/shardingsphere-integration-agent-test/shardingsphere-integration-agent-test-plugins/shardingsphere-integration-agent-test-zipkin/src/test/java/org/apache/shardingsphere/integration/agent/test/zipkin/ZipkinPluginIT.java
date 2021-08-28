3d687a22ada89b9a11a73bf45744b2ee7a4eb0e5
totalo <totalo@qq.com>
2021-08-28 17:52:03 +0800

    Refactor plugin integration test.

--- shardingsphere-test/shardingsphere-integration-agent-test/shardingsphere-integration-agent-test-plugins/shardingsphere-integration-agent-test-zipkin/src/test/java/org/apache/shardingsphere/integration/agent/test/zipkin/ZipkinPluginIT.java
+++ shardingsphere-test/shardingsphere-integration-agent-test/shardingsphere-integration-agent-test-plugins/shardingsphere-integration-agent-test-zipkin/src/test/java/org/apache/shardingsphere/integration/agent/test/zipkin/ZipkinPluginIT.java
@@ -17,36 +17,15 @@
 
 package org.apache.shardingsphere.integration.agent.test.zipkin;
 
-import org.apache.shardingsphere.integration.agent.test.common.entity.OrderEntity;
-import org.apache.shardingsphere.integration.agent.test.common.env.IntegrationTestEnvironment;
-import org.apache.shardingsphere.integration.agent.test.common.util.JDBCAgentTestUtils;
+import org.apache.shardingsphere.integration.agent.test.common.BasePluginIT;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
-
-public final class ZipkinPluginIT {
+public final class ZipkinPluginIT extends BasePluginIT {
     
     @Test
     @Ignore
     public void assertProxyWithAgent() {
-        if (IntegrationTestEnvironment.getInstance().isEnvironmentPrepared()) {
-            DataSource dataSource = IntegrationTestEnvironment.getInstance().getDataSource();
-            List<Long> results = new ArrayList<>(10);
-            for (int i = 1; i <= 10; i++) {
-                OrderEntity orderEntity = new OrderEntity(i, i, "INSERT_TEST");
-                JDBCAgentTestUtils.insertOrder(orderEntity, dataSource);
-                results.add(orderEntity.getOrderId());
-            }
-            OrderEntity orderEntity = new OrderEntity(1000, 1000, "ROLL_BACK");
-            JDBCAgentTestUtils.insertOrderRollback(orderEntity, dataSource);
-            JDBCAgentTestUtils.updateOrderStatus(orderEntity, dataSource);
-            JDBCAgentTestUtils.selectAllOrders(dataSource);
-            for (Long each : results) {
-                JDBCAgentTestUtils.deleteOrderByOrderId(each, dataSource);
-            }
-        }
+        super.assertProxyWithAgent();
     }
 }
