8eb5dbca9494c3834dcc85fe778aa39d05b5392b
JiekerTime <jieker_mail@163.com>
2021-08-31 17:54:06 +0800

    Fixing connection leaks

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-fixture/src/test/java/org/apache/shardingsphere/test/integration/env/DataSourceEnvironment.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-fixture/src/test/java/org/apache/shardingsphere/test/integration/env/DataSourceEnvironment.java
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.test.integration.env.datasource;
+package org.apache.shardingsphere.test.integration.env;
 
 /**
  * Data source environment.
  */
-public final class DataSourceEnvironmentUtil {
+public final class DataSourceEnvironment {
 
     /**
      * Get driver class name.
