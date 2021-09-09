49ca5374854aed6d76711c785c127747f2149f54
tuichenchuxin <chuxinchen.x@qq.com>
2021-09-09 18:45:35 +0800

    fix review suggestion.

--- shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/SQLParserTestCase.java
+++ shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/SQLParserTestCase.java
@@ -44,7 +44,7 @@ public abstract class SQLParserTestCase {
     @XmlAttribute
     private String parameters;
     
-    @XmlElement(name = "expected-comment")
+    @XmlElement(name = "comment")
     private List<ExpectedComment> comments = new LinkedList<>();
     
     /**
