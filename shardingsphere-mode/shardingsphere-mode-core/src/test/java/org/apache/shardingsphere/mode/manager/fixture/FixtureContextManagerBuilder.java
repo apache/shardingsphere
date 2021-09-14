d9eabd219359473bf289c1573af49faecd4e9d32
Haoran Meng <menghaoranss@gmail.com>
2021-09-14 18:41:25 +0800

    Use port instead of pid in ClusterInstance for proxy (#12423)
    * Use port instead of pid in ClusterInstance for proxy
    
    * Use port instead of pid in ClusterInstance for proxy

--- shardingsphere-mode/shardingsphere-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/fixture/FixtureContextManagerBuilder.java
+++ shardingsphere-mode/shardingsphere-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/fixture/FixtureContextManagerBuilder.java
@@ -17,21 +17,22 @@
 
 package org.apache.shardingsphere.mode.manager.fixture;
 
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Properties;
-import javax.sql.DataSource;
 import org.apache.shardingsphere.infra.config.RuleConfiguration;
 import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.mode.manager.ContextManagerBuilder;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
+
 public final class FixtureContextManagerBuilder implements ContextManagerBuilder {
 
     @Override
     public ContextManager build(final ModeConfiguration modeConfig, final Map<String, Map<String, DataSource>> dataSourcesMap, final Map<String, Collection<RuleConfiguration>> schemaRuleConfigs,
-                                final Collection<RuleConfiguration> globalRuleConfigs, final Properties props, final boolean isOverwrite) throws SQLException {
+                                final Collection<RuleConfiguration> globalRuleConfigs, final Properties props, final boolean isOverwrite, final Integer port) throws SQLException {
         return null;
     }
 
