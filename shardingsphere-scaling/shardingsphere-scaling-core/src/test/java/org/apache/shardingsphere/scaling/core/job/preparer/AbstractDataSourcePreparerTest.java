e793f6f0991c84db5b7a6375b35d4462df167203
sandynz <sandynz@126.com>
2021-09-17 16:53:15 +0800

    Unit test

--- shardingsphere-scaling/shardingsphere-scaling-core/src/test/java/org/apache/shardingsphere/scaling/core/job/preparer/AbstractDataSourcePreparerTest.java
+++ shardingsphere-scaling/shardingsphere-scaling-core/src/test/java/org/apache/shardingsphere/scaling/core/job/preparer/AbstractDataSourcePreparerTest.java
@@ -15,39 +15,59 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.scaling.core.preparer;
+package org.apache.shardingsphere.scaling.core.job.preparer;
 
-import lombok.SneakyThrows;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.shardingsphere.scaling.core.config.JobConfiguration;
-import org.apache.shardingsphere.scaling.core.job.preparer.AbstractDataSourcePreparer;
 import org.junit.Test;
 
-import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public final class AbstractDataSourcePreparerTest {
     
     private static final Pattern PATTERN_CREATE_TABLE_IF_NOT_EXISTS = Pattern.compile("CREATE\\s+TABLE\\s+IF\\s+NOT\\s+EXISTS\\s+", Pattern.CASE_INSENSITIVE);
     
+    private final AbstractDataSourcePreparer preparer = new AbstractDataSourcePreparer() {
+        @Override
+        public void prepareTargetTables(final JobConfiguration jobConfig) {
+        }
+    };
+    
     @Test
-    @SneakyThrows(ReflectiveOperationException.class)
+    public void assertGetTableDefinitionSQLType() {
+        Collection<Pair<String, TableDefinitionSQLType>> pairs = new ArrayList<>();
+        pairs.add(Pair.of("SET search_path = public", TableDefinitionSQLType.UNKNOWN));
+        pairs.add(Pair.of("CREATE TABLE t1_0 (id int NOT NULL)", TableDefinitionSQLType.CREATE_TABLE));
+        pairs.add(Pair.of("ALTER TABLE t1_0 ADD CONSTRAINT t1_0_pkey PRIMARY KEY (id)", TableDefinitionSQLType.ALTER_TABLE));
+        for (Pair<String, TableDefinitionSQLType> each : pairs) {
+            TableDefinitionSQLType sqlType = preparer.getTableDefinitionSQLType(each.getKey());
+            assertThat(sqlType, is(each.getValue()));
+        }
+    }
+    
+    @Test
     public void assertAddIfNotExistsForCreateTableSQL() {
-        Method method = AbstractDataSourcePreparer.class.getDeclaredMethod("addIfNotExistsForCreateTableSQL", String.class);
-        method.setAccessible(true);
-        AbstractDataSourcePreparer preparer = new AbstractDataSourcePreparer() {
-            @Override
-            public void prepareTargetTables(final JobConfiguration jobConfig) {
-            }
-        };
         List<String> createTableSQLs = Arrays.asList("CREATE TABLE IF NOT EXISTS t (id int)", "CREATE TABLE t (id int)",
                 "CREATE  TABLE IF \nNOT \tEXISTS t (id int)", "CREATE \tTABLE t (id int)");
         for (String createTableSQL : createTableSQLs) {
-            String sql = (String) method.invoke(preparer, createTableSQL);
+            String sql = preparer.addIfNotExistsForCreateTableSQL(createTableSQL);
             assertTrue(PATTERN_CREATE_TABLE_IF_NOT_EXISTS.matcher(sql).find());
         }
+    }
+    
+    @Test
+    public void assertReplaceActualTableNameToLogicTableName() {
+        String sql = "ALTER TABLE t_order_0 ADD CONSTRAINT t_order_0_uniq UNIQUE (order_id)";
+        String expected = "ALTER TABLE t_order ADD CONSTRAINT t_order_0_uniq UNIQUE (order_id)";
+        String actual = preparer.replaceActualTableNameToLogicTableName(sql, "t_order_0", "t_order");
+        assertThat(actual, is(expected));
     }
 }
