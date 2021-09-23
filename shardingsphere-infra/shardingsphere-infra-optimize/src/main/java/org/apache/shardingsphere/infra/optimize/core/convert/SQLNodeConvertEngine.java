2542ccfbbbcd154fe3414a9e4764c2cf79460c87
zhangliang <zhangliang@apache.org>
2021-09-23 22:22:56 +0800

    Rename SQLNodeConvertEngine

--- shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/core/convert/SQLNodeConvertEngine.java
+++ shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/core/convert/SQLNodeConvertEngine.java
@@ -35,7 +35,7 @@ public final class SQLNodeConvertEngine {
     /**
      * Convert.
      * 
-     * @param statement statement
+     * @param statement SQL statement
      * @return sqlNode converted SQL node
      */
     public static SqlNode convert(final SQLStatement statement) {
