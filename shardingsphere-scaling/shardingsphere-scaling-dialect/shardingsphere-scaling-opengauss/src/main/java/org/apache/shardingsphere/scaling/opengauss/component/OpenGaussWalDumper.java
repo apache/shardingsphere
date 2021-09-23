ad2b8292da79d500c156ee112052267f5a60170b
justbk2015 <249296768@qq.com>
2021-09-23 17:37:51 +0800

    avoid parallel create table throw already exists except to continue scaling and add mppdb decoding.

--- shardingsphere-scaling/shardingsphere-scaling-dialect/shardingsphere-scaling-opengauss/src/main/java/org/apache/shardingsphere/scaling/opengauss/component/OpenGaussWalDumper.java
+++ shardingsphere-scaling/shardingsphere-scaling-dialect/shardingsphere-scaling-opengauss/src/main/java/org/apache/shardingsphere/scaling/opengauss/component/OpenGaussWalDumper.java
@@ -20,7 +20,10 @@ package org.apache.shardingsphere.scaling.opengauss.component;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.scaling.core.common.channel.Channel;
+import org.apache.shardingsphere.scaling.core.common.constant.ScalingConstant;
 import org.apache.shardingsphere.scaling.core.common.exception.ScalingTaskExecuteException;
+import org.apache.shardingsphere.scaling.core.common.record.Column;
+import org.apache.shardingsphere.scaling.core.common.record.DataRecord;
 import org.apache.shardingsphere.scaling.core.common.record.Record;
 import org.apache.shardingsphere.scaling.core.config.DumperConfiguration;
 import org.apache.shardingsphere.scaling.core.config.datasource.StandardJDBCDataSourceConfiguration;
@@ -29,11 +32,12 @@ import org.apache.shardingsphere.scaling.core.executor.dumper.IncrementalDumper;
 import org.apache.shardingsphere.scaling.core.job.position.ScalingPosition;
 import org.apache.shardingsphere.scaling.core.util.ThreadUtil;
 import org.apache.shardingsphere.scaling.opengauss.wal.OpenGaussLogicalReplication;
+import org.apache.shardingsphere.scaling.opengauss.wal.decode.MppdbDecodingPlugin;
 import org.apache.shardingsphere.scaling.opengauss.wal.decode.OpenGaussTimestampUtils;
 import org.apache.shardingsphere.scaling.opengauss.wal.decode.OpenGaussLogSequenceNumber;
 import org.apache.shardingsphere.scaling.postgresql.wal.WalEventConverter;
 import org.apache.shardingsphere.scaling.postgresql.wal.WalPosition;
-import org.apache.shardingsphere.scaling.postgresql.wal.decode.TestDecodingPlugin;
+import org.apache.shardingsphere.scaling.postgresql.wal.decode.DecodingPlugin;
 import org.apache.shardingsphere.scaling.postgresql.wal.event.AbstractWalEvent;
 import org.apache.shardingsphere.scaling.postgresql.wal.event.PlaceholderEvent;
 import org.opengauss.jdbc.PgConnection;
@@ -57,6 +61,8 @@ public final class OpenGaussWalDumper extends AbstractScalingExecutor implements
     private final OpenGaussLogicalReplication logicalReplication = new OpenGaussLogicalReplication();
 
     private final WalEventConverter walEventConverter;
+    
+    private String slotName = OpenGaussLogicalReplication.SLOT_NAME_PREFIX;
 
     @Setter
     private Channel channel;
@@ -82,14 +88,15 @@ public final class OpenGaussWalDumper extends AbstractScalingExecutor implements
                 .unwrap(PgConnection.class);
     }
 
-    private TestDecodingPlugin initReplication() {
-        TestDecodingPlugin plugin = null;
+    private MppdbDecodingPlugin initReplication() {
+        MppdbDecodingPlugin plugin = null;
         try {
             DataSource dataSource = dumperConfig.getDataSourceConfig().toDataSource();
             try (Connection conn = dataSource.getConnection()) {
+                slotName = OpenGaussLogicalReplication.getUniqueSlotName(conn);
                 OpenGaussLogicalReplication.createIfNotExists(conn);
                 OpenGaussTimestampUtils utils = new OpenGaussTimestampUtils(conn.unwrap(PgConnection.class).getTimestampUtils());
-                plugin = new TestDecodingPlugin(utils);
+                plugin = new MppdbDecodingPlugin(utils);
             }
         } catch (SQLException sqlExp) {
             log.warn("create replication slot failed!");
@@ -98,9 +105,9 @@ public final class OpenGaussWalDumper extends AbstractScalingExecutor implements
     }
 
     private void dump() {
-        TestDecodingPlugin decodingPlugin = initReplication();
+        DecodingPlugin decodingPlugin = initReplication();
         try (PgConnection pgConnection = getReplicationConn()) {
-            PGReplicationStream stream = logicalReplication.createReplicationStream(pgConnection, walPosition.getLogSequenceNumber());
+            PGReplicationStream stream = logicalReplication.createReplicationStream(pgConnection, walPosition.getLogSequenceNumber(), slotName);
             while (isRunning()) {
                 ByteBuffer message = stream.readPending();
                 if (null == message) {
@@ -113,6 +120,7 @@ public final class OpenGaussWalDumper extends AbstractScalingExecutor implements
                 if (!(event instanceof PlaceholderEvent) && log.isDebugEnabled()) {
                     log.debug("dump, event={}, record={}", event, record);
                 }
+                updateRecordOldValue(record);
                 pushRecord(record);
             }
         } catch (final SQLException ex) {
@@ -123,6 +131,21 @@ public final class OpenGaussWalDumper extends AbstractScalingExecutor implements
         }
     }
 
+    private void updateRecordOldValue(final Record record) {
+        if (!(record instanceof DataRecord)) {
+            return;
+        }
+        DataRecord dataRecord = (DataRecord) record;
+        if (!ScalingConstant.UPDATE.equals(dataRecord.getType())) {
+            return;
+        }
+        for (Column col: dataRecord.getColumns()) {
+            if (col.isPrimaryKey() && col.isUpdated()) {
+                col.setOldValue(col.getValue());
+            }
+        }
+    }
+    
     private void pushRecord(final Record record) {
         try {
             channel.pushRecord(record);
