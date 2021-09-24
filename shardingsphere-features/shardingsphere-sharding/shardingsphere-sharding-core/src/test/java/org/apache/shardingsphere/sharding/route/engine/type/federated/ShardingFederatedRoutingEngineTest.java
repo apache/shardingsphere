ffd857620152f33ee2935d06c90ec3cc1bbb3578
Zhengqiang Duan <duanzhengqiang@apache.org>
2021-09-24 12:46:24 +0800

    fix select subquery wrong result (#12658)
    * fix select subquery wrong result
    
    * fix unit test
    
    * fix pagination route logic
    
    * fix unit test
    
    * fix unit test
    
    * add federate route test
    
    * optimize route logic
    
    * add todo
    
    * add todo

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/route/engine/type/federated/ShardingFederatedRoutingEngineTest.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/route/engine/type/federated/ShardingFederatedRoutingEngineTest.java
@@ -17,12 +17,9 @@
 
 package org.apache.shardingsphere.sharding.route.engine.type.federated;
 
-import org.apache.shardingsphere.infra.binder.statement.SQLStatementContext;
-import org.apache.shardingsphere.infra.config.properties.ConfigurationProperties;
 import org.apache.shardingsphere.infra.route.context.RouteContext;
 import org.apache.shardingsphere.infra.route.context.RouteMapper;
 import org.apache.shardingsphere.infra.route.context.RouteUnit;
-import org.apache.shardingsphere.sharding.route.engine.condition.ShardingConditions;
 import org.apache.shardingsphere.sharding.route.engine.fixture.AbstractRoutingEngineTest;
 import org.apache.shardingsphere.sharding.rule.ShardingRule;
 import org.junit.Test;
@@ -33,7 +30,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -46,11 +42,10 @@ public final class ShardingFederatedRoutingEngineTest extends AbstractRoutingEng
     
     @Test
     public void assertRouteByNonConditions() {
-        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Arrays.asList("t_order"),
-                new ShardingConditions(Collections.emptyList(), mock(SQLStatementContext.class), mock(ShardingRule.class)));
-        RouteContext routeContext = federatedRoutingEngine.route(createBasedShardingRule());
-        List<RouteUnit> routeUnits = new ArrayList<>(routeContext.getRouteUnits());
-        assertThat(routeContext.getRouteUnits().size(), is(2));
+        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Collections.singletonList("t_order"));
+        RouteContext actual = federatedRoutingEngine.route(createBasedShardingRule());
+        List<RouteUnit> routeUnits = new ArrayList<>(actual.getRouteUnits());
+        assertThat(actual.getRouteUnits().size(), is(2));
         RouteUnit routeUnit1 = routeUnits.get(0);
         assertThat(routeUnit1.getDataSourceMapper().getActualName(), is("ds_0"));
         assertThat(routeUnit1.getTableMappers().size(), is(2));
@@ -77,58 +72,91 @@ public final class ShardingFederatedRoutingEngineTest extends AbstractRoutingEng
     
     @Test
     public void assertRouteByShardingConditions() {
-        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Arrays.asList("t_order"), createShardingConditions("t_order"));
-        RouteContext routeContext = federatedRoutingEngine.route(createBasedShardingRule());
-        List<RouteUnit> routeUnits = new ArrayList<>(routeContext.getRouteUnits());
-        assertThat(routeContext.getRouteUnits().size(), is(1));
-        assertThat(routeUnits.get(0).getDataSourceMapper().getActualName(), is("ds_1"));
-        assertThat(routeUnits.get(0).getTableMappers().size(), is(1));
-        assertThat(routeUnits.get(0).getTableMappers().iterator().next().getActualName(), is("t_order_1"));
-        assertThat(routeUnits.get(0).getTableMappers().iterator().next().getLogicName(), is("t_order"));
+        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Collections.singletonList("t_order"));
+        RouteContext actual = federatedRoutingEngine.route(createBasedShardingRule());
+        assertThat(actual.getRouteUnits().size(), is(2));
+        List<RouteUnit> routeUnits = new ArrayList<>(actual.getRouteUnits());
+        assertThat(routeUnits.get(0).getDataSourceMapper().getActualName(), is("ds_0"));
+        assertThat(routeUnits.get(0).getDataSourceMapper().getLogicName(), is("ds_0"));
+        assertThat(routeUnits.get(0).getTableMappers().size(), is(2));
+        List<RouteMapper> firstRouteMappers = new ArrayList<>(routeUnits.get(0).getTableMappers());
+        assertThat(firstRouteMappers.get(0).getActualName(), is("t_order_0"));
+        assertThat(firstRouteMappers.get(0).getLogicName(), is("t_order"));
+        assertThat(firstRouteMappers.get(1).getActualName(), is("t_order_1"));
+        assertThat(firstRouteMappers.get(1).getLogicName(), is("t_order"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getActualName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getLogicName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getTableMappers().size(), is(2));
+        List<RouteMapper> secondRouteMappers = new ArrayList<>(routeUnits.get(1).getTableMappers());
+        assertThat(secondRouteMappers.get(0).getActualName(), is("t_order_0"));
+        assertThat(secondRouteMappers.get(0).getLogicName(), is("t_order"));
+        assertThat(secondRouteMappers.get(1).getActualName(), is("t_order_1"));
+        assertThat(secondRouteMappers.get(1).getLogicName(), is("t_order"));
     }
     
     @Test
     public void assertRoutingForBindingTables() {
-        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Arrays.asList("t_order", "t_order_item"), createShardingConditions("t_order"));
-        RouteContext routeContext = federatedRoutingEngine.route(createBindingShardingRule());
-        List<RouteUnit> routeUnits = new ArrayList<>(routeContext.getRouteUnits());
-        assertThat(routeContext.getRouteUnits().size(), is(1));
-        RouteUnit routeUnit = routeUnits.get(0);
-        assertThat(routeUnit.getDataSourceMapper().getActualName(), is("ds_1"));
-        assertThat(routeUnit.getDataSourceMapper().getLogicName(), is("ds_1"));
-        assertThat(routeUnit.getTableMappers().size(), is(2));
-        Collection<RouteMapper> tableMappers = routeUnit.getTableMappers();
-        Iterator<RouteMapper> iterator = tableMappers.iterator();
-        RouteMapper tableMapper1 = iterator.next();
-        assertThat(tableMapper1.getActualName(), is("t_order_1"));
-        assertThat(tableMapper1.getLogicName(), is("t_order"));
-        RouteMapper tableMapper2 = iterator.next();
-        assertThat(tableMapper2.getActualName(), is("t_order_item_1"));
-        assertThat(tableMapper2.getLogicName(), is("t_order_item"));
+        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Arrays.asList("t_order", "t_order_item"));
+        RouteContext actual = federatedRoutingEngine.route(createBindingShardingRule());
+        assertThat(actual.getRouteUnits().size(), is(2));
+        List<RouteUnit> routeUnits = new ArrayList<>(actual.getRouteUnits());
+        assertThat(routeUnits.get(0).getDataSourceMapper().getActualName(), is("ds_0"));
+        assertThat(routeUnits.get(0).getDataSourceMapper().getLogicName(), is("ds_0"));
+        assertThat(routeUnits.get(0).getTableMappers().size(), is(4));
+        List<RouteMapper> firstRouteMappers = new ArrayList<>(routeUnits.get(0).getTableMappers());
+        assertThat(firstRouteMappers.get(0).getActualName(), is("t_order_0"));
+        assertThat(firstRouteMappers.get(0).getLogicName(), is("t_order"));
+        assertThat(firstRouteMappers.get(1).getActualName(), is("t_order_1"));
+        assertThat(firstRouteMappers.get(1).getLogicName(), is("t_order"));
+        assertThat(firstRouteMappers.get(2).getActualName(), is("t_order_item_0"));
+        assertThat(firstRouteMappers.get(2).getLogicName(), is("t_order_item"));
+        assertThat(firstRouteMappers.get(3).getActualName(), is("t_order_item_1"));
+        assertThat(firstRouteMappers.get(3).getLogicName(), is("t_order_item"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getActualName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getLogicName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getTableMappers().size(), is(4));
+        List<RouteMapper> secondRouteMappers = new ArrayList<>(routeUnits.get(1).getTableMappers());
+        assertThat(secondRouteMappers.get(0).getActualName(), is("t_order_0"));
+        assertThat(secondRouteMappers.get(0).getLogicName(), is("t_order"));
+        assertThat(secondRouteMappers.get(1).getActualName(), is("t_order_1"));
+        assertThat(secondRouteMappers.get(1).getLogicName(), is("t_order"));
+        assertThat(secondRouteMappers.get(2).getActualName(), is("t_order_item_0"));
+        assertThat(secondRouteMappers.get(2).getLogicName(), is("t_order_item"));
+        assertThat(secondRouteMappers.get(3).getActualName(), is("t_order_item_1"));
+        assertThat(secondRouteMappers.get(3).getLogicName(), is("t_order_item"));
     }
     
     @Test
     public void assertRoutingForNonLogicTable() {
-        ShardingFederatedRoutingEngine complexRoutingEngine = createShardingFederatedRoutingEngine(Collections.emptyList(), createShardingConditions("t_order"));
-        RouteContext routeContext = complexRoutingEngine.route(mock(ShardingRule.class));
-        assertThat(routeContext.getOriginalDataNodes().size(), is(0));
-        assertThat(routeContext.getRouteUnits().size(), is(0));
-        assertThat(routeContext.getRouteStageContexts().size(), is(0));
-        assertThat(routeContext.isFederated(), is(true));
+        ShardingFederatedRoutingEngine complexRoutingEngine = createShardingFederatedRoutingEngine(Collections.emptyList());
+        RouteContext actual = complexRoutingEngine.route(mock(ShardingRule.class));
+        assertThat(actual.getOriginalDataNodes().size(), is(0));
+        assertThat(actual.getRouteUnits().size(), is(0));
+        assertThat(actual.getRouteStageContexts().size(), is(0));
+        assertThat(actual.isFederated(), is(true));
     }
     
     @Test
     public void assertRoutingForShardingTableJoinBroadcastTable() {
-        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Arrays.asList("t_config"), mock(ShardingConditions.class));
-        RouteContext routeContext = federatedRoutingEngine.route(createBroadcastShardingRule());
-        List<RouteUnit> routeUnits = new ArrayList<>(routeContext.getRouteUnits());
-        assertThat(routeContext.getRouteUnits().size(), is(1));
+        ShardingFederatedRoutingEngine federatedRoutingEngine = createShardingFederatedRoutingEngine(Collections.singletonList("t_config"));
+        RouteContext actual = federatedRoutingEngine.route(createBroadcastShardingRule());
+        List<RouteUnit> routeUnits = new ArrayList<>(actual.getRouteUnits());
+        assertThat(actual.getRouteUnits().size(), is(2));
+        assertThat(routeUnits.get(0).getDataSourceMapper().getActualName(), is("ds_0"));
+        assertThat(routeUnits.get(0).getDataSourceMapper().getLogicName(), is("ds_0"));
         assertThat(routeUnits.get(0).getTableMappers().size(), is(1));
-        assertThat(routeUnits.get(0).getTableMappers().iterator().next().getActualName(), is("t_config"));
-        assertThat(routeUnits.get(0).getTableMappers().iterator().next().getLogicName(), is("t_config"));
+        List<RouteMapper> firstRouteMappers = new ArrayList<>(routeUnits.get(0).getTableMappers());
+        assertThat(firstRouteMappers.get(0).getActualName(), is("t_config"));
+        assertThat(firstRouteMappers.get(0).getLogicName(), is("t_config"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getActualName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getDataSourceMapper().getLogicName(), is("ds_1"));
+        assertThat(routeUnits.get(1).getTableMappers().size(), is(1));
+        List<RouteMapper> secondRouteMappers = new ArrayList<>(routeUnits.get(0).getTableMappers());
+        assertThat(secondRouteMappers.get(0).getActualName(), is("t_config"));
+        assertThat(secondRouteMappers.get(0).getLogicName(), is("t_config"));
     }
     
-    private ShardingFederatedRoutingEngine createShardingFederatedRoutingEngine(final Collection<String> logicTables, final ShardingConditions shardingConditions) {
-        return new ShardingFederatedRoutingEngine(logicTables, shardingConditions, new ConfigurationProperties(new Properties()));
+    private ShardingFederatedRoutingEngine createShardingFederatedRoutingEngine(final Collection<String> logicTables) {
+        return new ShardingFederatedRoutingEngine(logicTables);
     }
 }
