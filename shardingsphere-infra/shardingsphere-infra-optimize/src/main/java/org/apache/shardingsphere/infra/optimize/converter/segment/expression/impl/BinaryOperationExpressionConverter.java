c28b5425bd34c6c053f9437df102d56cd229573d
Liang Zhang <terrymanu@163.com>
2021-09-25 15:02:06 +0800

    Remove BinarySQLOperator (#12706)

--- shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/converter/segment/expression/impl/BinaryOperationExpressionConverter.java
+++ shardingsphere-infra/shardingsphere-infra-optimize/src/main/java/org/apache/shardingsphere/infra/optimize/converter/segment/expression/impl/BinaryOperationExpressionConverter.java
@@ -19,27 +19,50 @@ package org.apache.shardingsphere.infra.optimize.converter.segment.expression.im
 
 import com.google.common.base.Preconditions;
 import org.apache.calcite.sql.SqlBasicCall;
+import org.apache.calcite.sql.SqlBinaryOperator;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.shardingsphere.infra.optimize.converter.segment.SQLSegmentConverter;
 import org.apache.shardingsphere.infra.optimize.converter.segment.expression.ExpressionConverter;
-import org.apache.shardingsphere.infra.optimize.operator.BinarySQLOperator;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.BinaryOperationExpression;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ExpressionSegment;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 /**
  * Binary operation expression converter.
  */
 public final class BinaryOperationExpressionConverter implements SQLSegmentConverter<BinaryOperationExpression, SqlNode> {
     
+    private static final Map<String, SqlBinaryOperator> REGISTRY = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    
+    static {
+        register(SqlStdOperatorTable.EQUALS);
+        register(SqlStdOperatorTable.GREATER_THAN);
+        register(SqlStdOperatorTable.GREATER_THAN_OR_EQUAL);
+        register(SqlStdOperatorTable.LESS_THAN);
+        register(SqlStdOperatorTable.LESS_THAN_OR_EQUAL);
+        register(SqlStdOperatorTable.AND);
+    }
+    
+    private static void register(final SqlBinaryOperator sqlBinaryOperator) {
+        REGISTRY.put(sqlBinaryOperator.getName(), sqlBinaryOperator);
+    }
+    
     @Override
     public Optional<SqlNode> convert(final BinaryOperationExpression segment) {
-        BinarySQLOperator operator = BinarySQLOperator.value(segment.getOperator());
+        SqlBinaryOperator operator = getOperator(segment.getOperator());
         SqlNode left = convertExpression(segment.getLeft());
         SqlNode right = convertExpression(segment.getRight());
-        return Optional.of(new SqlBasicCall(operator.getSqlBinaryOperator(), new SqlNode[] {left, right}, SqlParserPos.ZERO));
+        return Optional.of(new SqlBasicCall(operator, new SqlNode[] {left, right}, SqlParserPos.ZERO));
+    }
+    
+    private SqlBinaryOperator getOperator(final String operator) {
+        Preconditions.checkState(REGISTRY.containsKey(operator), "Unsupported SQL operator: `%s`", operator);
+        return REGISTRY.get(operator);
     }
     
     private SqlNode convertExpression(final ExpressionSegment segment) {
