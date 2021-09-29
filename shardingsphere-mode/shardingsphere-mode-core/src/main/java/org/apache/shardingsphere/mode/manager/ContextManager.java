f41e9415cc6b61b14affdcfa9a5e260ca322b77b
menghaoranss <menghaoranss@gmail.com>
2021-09-29 17:16:56 +0800

    Refresh memory meta data for create and drop database

--- shardingsphere-mode/shardingsphere-mode-core/src/main/java/org/apache/shardingsphere/mode/manager/ContextManager.java
+++ shardingsphere-mode/shardingsphere-mode-core/src/main/java/org/apache/shardingsphere/mode/manager/ContextManager.java
@@ -18,13 +18,29 @@
 package org.apache.shardingsphere.mode.manager;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.infra.config.RuleConfiguration;
+import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
+import org.apache.shardingsphere.infra.metadata.resource.ShardingSphereResource;
+import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
 import org.apache.shardingsphere.infra.state.StateContext;
 import org.apache.shardingsphere.mode.metadata.MetaDataContexts;
+import org.apache.shardingsphere.mode.metadata.MetaDataContextsBuilder;
+import org.apache.shardingsphere.transaction.ShardingSphereTransactionManagerEngine;
 import org.apache.shardingsphere.transaction.context.TransactionContexts;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Context manager.
  */
+@Slf4j
 @Getter
 public final class ContextManager implements AutoCloseable {
     
@@ -61,6 +77,74 @@ public final class ContextManager implements AutoCloseable {
      */
     public synchronized void renewTransactionContexts(final TransactionContexts transactionContexts) {
         this.transactionContexts = transactionContexts;
+    }
+    
+    /**
+     * Add schema.
+     * 
+     * @param schemaName schema name
+     * @throws SQLException SQL exception                  
+     */
+    public void addSchema(final String schemaName) throws SQLException {
+        if (metaDataContexts.getMetaDataMap().containsKey(schemaName)) {
+            return;
+        }
+        MetaDataContexts newMetaDataContexts = buildNewMetaDataContext(schemaName);
+        metaDataContexts.getOptimizerContext().getMetaData().getSchemas().put(schemaName,
+                newMetaDataContexts.getOptimizerContext().getMetaData().getSchemas().get(schemaName));
+        metaDataContexts.getMetaDataMap().put(schemaName, newMetaDataContexts.getMetaData(schemaName));
+    }
+    
+    /**
+     * Delete schema.
+     * 
+     * @param schemaName schema name
+     */
+    public void deleteSchema(final String schemaName) {
+        if (metaDataContexts.getMetaDataMap().containsKey(schemaName)) {
+            metaDataContexts.getOptimizerContext().getMetaData().getSchemas().remove(schemaName);
+            metaDataContexts.getOptimizerContext().getParserContexts().remove(schemaName);
+            metaDataContexts.getOptimizerContext().getPlannerContexts().remove(schemaName);
+            ShardingSphereMetaData removeMetaData = metaDataContexts.getMetaDataMap().remove(schemaName);
+            closeDataSources(removeMetaData);
+            closeTransactionEngine(schemaName);
+        }
+    }
+    
+    private MetaDataContexts buildNewMetaDataContext(final String schemaName) throws SQLException {
+        Map<String, Map<String, DataSource>> dataSourcesMap = Collections.singletonMap(schemaName, new HashMap<>());
+        Map<String, Collection<RuleConfiguration>> schemaRuleConfigs = Collections.singletonMap(schemaName, Collections.emptyList());
+        Properties props = metaDataContexts.getProps().getProps();
+        Map<String, ShardingSphereSchema> schemas = Collections.singletonMap(schemaName, new ShardingSphereSchema());
+        return new MetaDataContextsBuilder(dataSourcesMap, schemaRuleConfigs, metaDataContexts.getGlobalRuleMetaData().getConfigurations(), schemas, props)
+                .build(metaDataContexts.getMetaDataPersistService().orElse(null));
+    }
+    
+    private void closeDataSources(final ShardingSphereMetaData removeMetaData) {
+        if (null != removeMetaData.getResource()) {
+            removeMetaData.getResource().getDataSources().values().forEach(each -> closeDataSource(removeMetaData.getResource(), each));
+        }
+    }
+    
+    private void closeDataSource(final ShardingSphereResource resource, final DataSource dataSource) {
+        try {
+            resource.close(dataSource);
+        } catch (final SQLException ex) {
+            log.error("Close data source failed", ex);
+        }
+    }
+    
+    private void closeTransactionEngine(final String schemaName) {
+        ShardingSphereTransactionManagerEngine staleEngine = transactionContexts.getEngines().remove(schemaName);
+        if (null != staleEngine) {
+            try {
+                staleEngine.close();
+                // CHECKSTYLE:OFF
+            } catch (final Exception ex) {
+                // CHECKSTYLE:ON
+                log.error("Close transaction engine failed", ex);
+            }
+        }
     }
     
     @Override
