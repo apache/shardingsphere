5259487b753e9e85eb260fa2e999c1f5317963f2
cunha <cunha.gabriel919@gmail.com>
2021-10-05 15:36:11 -0300

    Making test class final

--- shardingsphere-kernel/shardingsphere-transaction/shardingsphere-transaction-core/src/test/java/org/apache/shardingsphere/transaction/ConnectionTransactionTest.java
+++ shardingsphere-kernel/shardingsphere-transaction/shardingsphere-transaction-core/src/test/java/org/apache/shardingsphere/transaction/ConnectionTransactionTest.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ConnectionTransactionTest {
+public final class ConnectionTransactionTest {
 
     private ConnectionTransaction connectionTransaction;
 
