585f1cc5cdaa46de24b965b73487274edc537535
Ali Kaan BACI <kaan.baci@obss.com.tr>
2021-10-05 16:57:23 +0300

    code style fix

--- shardingsphere-scaling/shardingsphere-scaling-core/src/test/java/org/apache/shardingsphere/scaling/core/common/datasource/DataSourceWrapperTest.java
+++ shardingsphere-scaling/shardingsphere-scaling-core/src/test/java/org/apache/shardingsphere/scaling/core/common/datasource/DataSourceWrapperTest.java
@@ -35,78 +35,78 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public final class DataSourceWrapperTest  {
+public final class DataSourceWrapperTest {
 
-  private static final String CLIENT_USERNAME = "username";
+    private static final String CLIENT_USERNAME = "username";
 
-  private static final String CLIENT_PASSWORD = "password";
+    private static final String CLIENT_PASSWORD = "password";
 
-  private static final int LOGIN_TIMEOUT = 15;
+    private static final int LOGIN_TIMEOUT = 15;
 
-  @Mock(extraInterfaces = AutoCloseable.class)
-  private DataSource dataSource;
+    @Mock(extraInterfaces = AutoCloseable.class)
+    private DataSource dataSource;
 
-  @Mock
-  private Connection connection;
+    @Mock
+    private Connection connection;
 
-  @Mock
-  private PrintWriter printWriter;
+    @Mock
+    private PrintWriter printWriter;
 
-  @Mock
-  private Logger parentLogger;
+    @Mock
+    private Logger parentLogger;
 
-  @Before
-  public void setUp() throws SQLException {
-    when(dataSource.getConnection()).thenReturn(connection);
-    when(dataSource.getConnection(CLIENT_USERNAME, CLIENT_PASSWORD)).thenReturn(connection);
-    when(dataSource.getLogWriter()).thenReturn(printWriter);
-    when(dataSource.getLoginTimeout()).thenReturn(LOGIN_TIMEOUT);
-    when(dataSource.isWrapperFor(any())).thenReturn(Boolean.TRUE);
-    when(dataSource.getParentLogger()).thenReturn(parentLogger);
-  }
+    @Before
+    public void setUp() throws SQLException {
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(dataSource.getConnection(CLIENT_USERNAME, CLIENT_PASSWORD)).thenReturn(connection);
+        when(dataSource.getLogWriter()).thenReturn(printWriter);
+        when(dataSource.getLoginTimeout()).thenReturn(LOGIN_TIMEOUT);
+        when(dataSource.isWrapperFor(any())).thenReturn(Boolean.TRUE);
+        when(dataSource.getParentLogger()).thenReturn(parentLogger);
+    }
 
-  @Test
-  public void assertGetConnection() throws SQLException {
-    DataSourceWrapper dataSourceWrapper = new DataSourceWrapper(dataSource);
-    assertThat(dataSourceWrapper.getConnection(), is(connection));
-    assertThat(dataSourceWrapper.getConnection(CLIENT_USERNAME, CLIENT_PASSWORD), is(connection));
-    assertGetLogWriter(dataSourceWrapper.getLogWriter());
-    assertGetLoginTimeout(dataSourceWrapper.getLoginTimeout());
-    assertIsWrappedFor(dataSourceWrapper.isWrapperFor(any()));
-    assertGetParentLogger(dataSourceWrapper.getParentLogger());
-  }
+    @Test
+    public void assertGetConnection() throws SQLException {
+        DataSourceWrapper dataSourceWrapper = new DataSourceWrapper(dataSource);
+        assertThat(dataSourceWrapper.getConnection(), is(connection));
+        assertThat(dataSourceWrapper.getConnection(CLIENT_USERNAME, CLIENT_PASSWORD), is(connection));
+        assertGetLogWriter(dataSourceWrapper.getLogWriter());
+        assertGetLoginTimeout(dataSourceWrapper.getLoginTimeout());
+        assertIsWrappedFor(dataSourceWrapper.isWrapperFor(any()));
+        assertGetParentLogger(dataSourceWrapper.getParentLogger());
+    }
 
-  private void assertGetLogWriter(final PrintWriter actual) {
-    assertThat(actual, is(printWriter));
-  }
+    private void assertGetLogWriter(final PrintWriter actual) {
+        assertThat(actual, is(printWriter));
+    }
 
-  private void assertGetLoginTimeout(final int actual) {
-    assertThat(actual, is(LOGIN_TIMEOUT));
-  }
+    private void assertGetLoginTimeout(final int actual) {
+        assertThat(actual, is(LOGIN_TIMEOUT));
+    }
 
-  private void assertIsWrappedFor(final boolean actual) {
-    assertThat(actual, is(Boolean.TRUE));
-  }
+    private void assertIsWrappedFor(final boolean actual) {
+        assertThat(actual, is(Boolean.TRUE));
+    }
 
-  private void assertGetParentLogger(final Logger actual) {
-    assertThat(actual, is(parentLogger));
-  }
+    private void assertGetParentLogger(final Logger actual) {
+        assertThat(actual, is(parentLogger));
+    }
 
-  @Test(expected = SQLException.class)
-  public void assertSetLoginTimeoutFailure() throws SQLException {
-    doThrow(new SQLException("")).when(dataSource).setLoginTimeout(LOGIN_TIMEOUT);
-    new DataSourceWrapper(dataSource).setLoginTimeout(LOGIN_TIMEOUT);
-  }
+    @Test(expected = SQLException.class)
+    public void assertSetLoginTimeoutFailure() throws SQLException {
+        doThrow(new SQLException("")).when(dataSource).setLoginTimeout(LOGIN_TIMEOUT);
+        new DataSourceWrapper(dataSource).setLoginTimeout(LOGIN_TIMEOUT);
+    }
 
-  @Test(expected = SQLException.class)
-  public void assertSetLogWriterFailure() throws SQLException {
-    doThrow(new SQLException("")).when(dataSource).setLogWriter(printWriter);
-    new DataSourceWrapper(dataSource).setLogWriter(printWriter);
-  }
+    @Test(expected = SQLException.class)
+    public void assertSetLogWriterFailure() throws SQLException {
+        doThrow(new SQLException("")).when(dataSource).setLogWriter(printWriter);
+        new DataSourceWrapper(dataSource).setLogWriter(printWriter);
+    }
 
-  @Test(expected = SQLException.class)
-  public void assertCloseFailure() throws Exception {
-    doThrow(new Exception("")).when((AutoCloseable) dataSource).close();
-    new DataSourceWrapper(dataSource).close();
-  }
+    @Test(expected = SQLException.class)
+    public void assertCloseFailure() throws Exception {
+        doThrow(new Exception("")).when((AutoCloseable) dataSource).close();
+        new DataSourceWrapper(dataSource).close();
+    }
 }
