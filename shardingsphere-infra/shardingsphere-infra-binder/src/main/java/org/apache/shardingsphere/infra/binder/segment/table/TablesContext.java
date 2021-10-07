38e398ce7cd3345bea3114401cb8e77593bf044d
cheese8 <yinwerhong@163.com>
2021-10-07 22:07:46 +0800

    Update TablesContext.java

--- shardingsphere-infra/shardingsphere-infra-binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/table/TablesContext.java
+++ shardingsphere-infra/shardingsphere-infra-binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/table/TablesContext.java
@@ -152,7 +152,6 @@ public final class TablesContext {
             if (tableColumnNames.isEmpty()) {
                 continue;
             }
-            //tableColumnNames.retainAll(columnNames);
             List<String> retainAllTableColumnNames = tableColumnNames.stream().filter(item -> columnNames.contains(item)).collect(Collectors.toList());
             for (String columnName : retainAllTableColumnNames) {
                 result.put(columnName, each);
