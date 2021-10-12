687f98d3cd96fc3ee02c8335e40b2937e834b94d
Liang Zhang <terrymanu@163.com>
2021-10-12 11:32:12 +0800

    Revise #12997 (#12998)

--- shardingsphere-mode/shardingsphere-mode-type/shardingsphere-cluster-mode/shardingsphere-cluster-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/process/GovernanceExecuteProcessReporterTest.java
+++ shardingsphere-mode/shardingsphere-mode-type/shardingsphere-cluster-mode/shardingsphere-cluster-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/process/GovernanceExecuteProcessReporterTest.java
@@ -17,104 +17,40 @@
 
 package org.apache.shardingsphere.mode.manager.cluster.process;
 
-import com.google.common.eventbus.Subscribe;
 import org.apache.shardingsphere.infra.binder.LogicSQL;
-import org.apache.shardingsphere.infra.eventbus.ShardingSphereEventBus;
 import org.apache.shardingsphere.infra.executor.kernel.model.ExecutionGroupContext;
-import org.apache.shardingsphere.infra.executor.sql.context.ExecutionUnit;
-import org.apache.shardingsphere.infra.executor.sql.execute.engine.ConnectionMode;
 import org.apache.shardingsphere.infra.executor.sql.execute.engine.SQLExecutionUnit;
 import org.apache.shardingsphere.infra.executor.sql.process.model.ExecuteProcessConstants;
 import org.apache.shardingsphere.infra.executor.sql.process.model.ExecuteProcessContext;
-import org.apache.shardingsphere.mode.manager.cluster.coordinator.registry.process.event.ExecuteProcessReportEvent;
-import org.apache.shardingsphere.mode.manager.cluster.coordinator.registry.process.event.ExecuteProcessSummaryReportEvent;
-import org.apache.shardingsphere.mode.manager.cluster.coordinator.registry.process.event.ExecuteProcessUnitReportEvent;
-
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.UUID;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public final class GovernanceExecuteProcessReporterTest {
-
-    private final GovernanceExecuteProcessReporter governanceExecuteProcessReporter = new GovernanceExecuteProcessReporter();
-
-    private String str = "";
-
-    private ExecutionGroupContext<? extends SQLExecutionUnit> executionGroupContext;
-
-    private SQLExecutionUnit executionUnit = new SQLExecutionUnit() {
-        @Override
-        public ExecutionUnit getExecutionUnit() {
-            return new ExecutionUnit("", null);
-        }
-
-        @Override
-        public ConnectionMode getConnectionMode() {
-            return null;
-        }
-    };
-
-    @Before
-    public void setUp() {
-        ShardingSphereEventBus.getInstance().register(this);
-        executionGroupContext = createMockedExecutionGroups();
-    }
-
-    /**
-     * onExecuteProcessSummaryReportEvent.
-     * @param executeProcessSummaryReportEvent An event.
-     */
-    @Subscribe
-    public void onExecuteProcessSummaryReportEvent(final ExecuteProcessSummaryReportEvent executeProcessSummaryReportEvent) {
-        ExecuteProcessContext executeProcessContext = executeProcessSummaryReportEvent.getExecuteProcessContext();
-        str = executeProcessContext.getExecutionID();
-    }
-
-    /**
-     * onExecuteProcessUnitReportEvent.
-     * @param executeProcessUnitReportEvent An event.
-     */
-    @Subscribe
-    public void onExecuteProcessUnitReportEvent(final ExecuteProcessUnitReportEvent executeProcessUnitReportEvent) {
-        str = executeProcessUnitReportEvent.getExecutionID();
-    }
-
-    /**
-     * onExecuteProcessReportEvent.
-     * @param executeProcessReportEvent An event.
-     */
-    @Subscribe
-    public void onExecuteProcessReportEvent(final ExecuteProcessReportEvent executeProcessReportEvent) {
-        str = executeProcessReportEvent.getExecutionID();
-    }
-
+    
     @Test
     public void assertReport() {
-        ExecuteProcessConstants constants = ExecuteProcessConstants.EXECUTE_ID;
+        GovernanceExecuteProcessReporterSubscriberFixture subscriber = new GovernanceExecuteProcessReporterSubscriberFixture();
         LogicSQL logicSQL = new LogicSQL(null, null, null);
-        ExecuteProcessContext executeProcessContext = new ExecuteProcessContext(logicSQL.getSql(), executionGroupContext, constants);
-
-        governanceExecuteProcessReporter.report(logicSQL, executionGroupContext, constants);
-        assertTrue(str.equals(executeProcessContext.getExecutionID()));
-        governanceExecuteProcessReporter.report("TEST", executionUnit, constants);
-        assertFalse(str.equals(executeProcessContext.getExecutionID()));
-        assertTrue("TEST".equals(str));
-        governanceExecuteProcessReporter.report("TEST1", constants);
-        assertFalse(str.equals(executeProcessContext.getExecutionID()));
-        assertFalse("TEST".equals(str));
-        assertTrue("TEST1".equals(str));
+        ExecutionGroupContext<? extends SQLExecutionUnit> executionGroupContext = mockExecutionGroupContext();
+        ExecuteProcessContext executeProcessContext = new ExecuteProcessContext(logicSQL.getSql(), executionGroupContext, ExecuteProcessConstants.EXECUTE_ID);
+        GovernanceExecuteProcessReporter reporter = new GovernanceExecuteProcessReporter();
+        reporter.report(logicSQL, executionGroupContext, ExecuteProcessConstants.EXECUTE_ID);
+        assertThat(subscriber.getValue(), is(executeProcessContext.getExecutionID()));
+        reporter.report("TEST0", mock(SQLExecutionUnit.class, RETURNS_DEEP_STUBS), ExecuteProcessConstants.EXECUTE_ID);
+        assertThat(subscriber.getValue(), is("TEST0"));
+        reporter.report("TEST1", ExecuteProcessConstants.EXECUTE_ID);
+        assertThat(subscriber.getValue(), is("TEST1"));
     }
-
-    private ExecutionGroupContext<? extends SQLExecutionUnit> createMockedExecutionGroups() {
+    
+    @SuppressWarnings("unchecked")
+    private ExecutionGroupContext<? extends SQLExecutionUnit> mockExecutionGroupContext() {
         ExecutionGroupContext<? extends SQLExecutionUnit> result = mock(ExecutionGroupContext.class);
         when(result.getExecutionID()).thenReturn(UUID.randomUUID().toString());
         return result;
