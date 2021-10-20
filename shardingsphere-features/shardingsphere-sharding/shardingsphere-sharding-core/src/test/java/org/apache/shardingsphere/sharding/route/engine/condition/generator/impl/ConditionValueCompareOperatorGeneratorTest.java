8dbdc9ff4f14bdd7f03f28af14dee3bb09f019c7
Zhengqiang Duan <duanzhengqiang@apache.org>
2021-10-20 18:51:26 +0800

    add parameter check when execute sql without parameter (#13175)

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/route/engine/condition/generator/impl/ConditionValueCompareOperatorGeneratorTest.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/route/engine/condition/generator/impl/ConditionValueCompareOperatorGeneratorTest.java
@@ -26,12 +26,18 @@ import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.Column
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.BinaryOperationExpression;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.complex.CommonExpressionSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.simple.LiteralExpressionSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.simple.ParameterMarkerExpressionSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
@@ -106,5 +112,28 @@ public final class ConditionValueCompareOperatorGeneratorTest {
         Optional<ShardingConditionValue> shardingConditionValue = generator.generate(rightValue, column, new LinkedList<>());
         assertTrue(shardingConditionValue.isPresent());
         assertFalse(((ListShardingConditionValue<Integer>) shardingConditionValue.get()).getValues().isEmpty());
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void assertGenerateConditionValueWithParameter() {
+        ColumnSegment left = new ColumnSegment(0, 0, new IdentifierValue("id"));
+        ParameterMarkerExpressionSegment right = new ParameterMarkerExpressionSegment(0, 0, 0);
+        BinaryOperationExpression predicate = new BinaryOperationExpression(0, 0, left, right, "=", "id = ?");
+        Optional<ShardingConditionValue> actual = generator.generate(predicate, column, Collections.singletonList(1));
+        assertTrue(actual.isPresent());
+        assertThat(actual.get(), instanceOf(ListShardingConditionValue.class));
+        ListShardingConditionValue<Integer> conditionValue = (ListShardingConditionValue<Integer>) actual.get();
+        assertThat(conditionValue.getTableName(), is("tbl"));
+        assertThat(conditionValue.getColumnName(), is("id"));
+        assertThat(conditionValue.getValues(), is(Collections.singletonList(1)));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void assertGenerateConditionValueWithoutParameter() {
+        ColumnSegment left = new ColumnSegment(0, 0, new IdentifierValue("order_id"));
+        ParameterMarkerExpressionSegment right = new ParameterMarkerExpressionSegment(0, 0, 0);
+        BinaryOperationExpression predicate = new BinaryOperationExpression(0, 0, left, right, "=", "order_id = ?");
+        generator.generate(predicate, column, new LinkedList<>());
     }
 }
