da4abba25e043a5a2177f86c00ad52c7f8c8bfd3
sunhangda <lwclover@126.com>
2021-11-01 19:16:49 +0800

    Exception occurs when Changing /meta/scheamName/dataSources/ config in registry. (#13227)
    * proxy conntion limit Issue[#13132]
    
    * shardingshere proxy connection limit Issue[#13132] fix checkstyle
    
    * shardingshere proxy connection limit Issue[#13132] add lisence header
    
    * shardingshere proxy connection limit [#13132] return database message
    
    * shardingshere proxy connection limit Issue[apache#13132] fix code style
    
    * shardingshere proxy connection limit [#13132] add mysql error code
    
    * shardingshere proxy connection limit Issue[#13132] modiy exception msg
    
    * Exception occurs when Changing /meta/scheamName/dataSources/ config in
    registry #13213
    
    * Exception occurs when Changing /meta/scheamName/dataSources/ config in
    registry Fixes [#13213] codesytle.
    
    * Exception occurs when Changing /meta/scheamName/dataSources/ config in
    registry Fixes #13213 compile Hikari dependency
    
    * fix #13213 modiy code style
    
    * code style fix
    
    * fix code style2
    
    * Modify the Java doc
    
    * Modify the Java doc comment

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/resource/ShardingSphereResource.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/resource/ShardingSphereResource.java
@@ -17,16 +17,20 @@
 
 package org.apache.shardingsphere.infra.metadata.resource;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.apache.shardingsphere.infra.database.type.DatabaseType;
-
-import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+
+import javax.sql.DataSource;
+
+import org.apache.shardingsphere.infra.config.datasource.killer.DataSourceKiller;
+import org.apache.shardingsphere.infra.config.datasource.killer.DataSourceKillerFactory;
+import org.apache.shardingsphere.infra.database.type.DatabaseType;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * ShardingSphere resource.
@@ -81,14 +85,7 @@ public final class ShardingSphereResource {
      * @throws SQLException exception
      */
     public void close(final DataSource dataSource) throws SQLException {
-        if (dataSource instanceof AutoCloseable) {
-            try {
-                ((AutoCloseable) dataSource).close();
-                // CHECKSTYLE:OFF
-            } catch (final Exception ex) {
-                // CHECKSTYLE:ON
-                throw new SQLException(ex);
-            }
-        }
+        DataSourceKiller dataSourceKiller = DataSourceKillerFactory.getDataSourceKiller(dataSource.getClass().getName());
+        dataSourceKiller.closeDataSource(dataSource);
     }
 }
