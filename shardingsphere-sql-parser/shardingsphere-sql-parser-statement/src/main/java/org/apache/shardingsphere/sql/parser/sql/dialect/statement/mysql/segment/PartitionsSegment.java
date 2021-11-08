e57c7bf85d54ad212b45b35978c28e774363255a
Zonglei Dong <dongzonglei@apache.org>
2021-11-08 17:01:53 +0800

    Refactor cache index DAL SQL parser class for MySQL.

--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/segment/PartitionsSegment.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/segment/PartitionsSegment.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.SQLSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.SimpleTableSegment;
 
@@ -28,7 +27,6 @@ import java.util.LinkedList;
 
 @RequiredArgsConstructor
 @Getter
-@Setter
 public final class PartitionsSegment implements SQLSegment {
     
     private final int startIndex;
--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/segment/TableIndexSegment.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/mysql/segment/TableIndexSegment.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.segment
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.SQLSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.ddl.index.IndexSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.SimpleTableSegment;
@@ -28,7 +27,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 @RequiredArgsConstructor
-@Setter
 @Getter
 public final class TableIndexSegment implements SQLSegment {
     
