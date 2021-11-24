8531cd6f487985737a07c09e6775c846194157a3
刘伟 <llzx373@gmail.com>
2021-11-24 19:39:31 +0800

    Og discovery (#13755)
    * add Discovery support for openGuass
    
    1. if openGauss Primary changed, switch primary in Shardingsphere.
    2. DAL could add Datasource to openGauss Discovery Datasorce, and active
       for read loadbalance.
    3. if one of standbys failed,it will be disabled, read loadbalance continue
     without failed.
    
    * fix project version
    
    * fix style problem
    
    * change package name from shardingsphere-db-discovery-og to shardingsphere-db-discovery-opengauss
    
    * change openGaussDiscovery getType
    
    * fix comment
    
    * fix comment

---
+++ shardingsphere-features/shardingsphere-db-discovery/shardingsphere-db-discovery-provider/shardingsphere-db-discovery-opengauss/src/main/java/org/apache/shardingsphere/dbdiscovery/opengauss/OpenGaussDatabaseDiscoveryType.java
@@ -0,0 +1,194 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.dbdiscovery.opengauss;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.dbdiscovery.spi.DatabaseDiscoveryType;
+import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
+import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.ScheduleJobBootstrap;
+import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperConfiguration;
+import org.apache.shardingsphere.elasticjob.reg.zookeeper.ZookeeperRegistryCenter;
+import org.apache.shardingsphere.elasticjob.infra.pojo.JobConfigurationPOJO;
+import org.apache.shardingsphere.infra.config.exception.ShardingSphereConfigurationException;
+import org.apache.shardingsphere.infra.eventbus.ShardingSphereEventBus;
+import org.apache.shardingsphere.infra.rule.event.impl.DataSourceDisabledEvent;
+import org.apache.shardingsphere.infra.rule.event.impl.PrimaryDataSourceChangedEvent;
+import org.apache.shardingsphere.elasticjob.lite.lifecycle.internal.settings.JobConfigurationAPIImpl;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+/**
+ * OpenGauss data base discovery type.
+ */
+@Slf4j
+public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryType {
+
+    private static final String DB_ROLE = "select local_role,db_state from pg_stat_get_stream_replications()";
+
+    private static CoordinatorRegistryCenter coordinatorRegistryCenter;
+
+    private static final Map<String, ScheduleJobBootstrap> SCHEDULE_JOB_BOOTSTRAP_MAP = new HashMap<>(16, 1);
+
+    private String oldPrimaryDataSource;
+
+    @Getter
+    @Setter
+    private Properties props = new Properties();
+
+    @Override
+    public void checkDatabaseDiscoveryConfiguration(final String schemaName,
+            final Map<String, DataSource> dataSourceMap) throws SQLException {
+        try (Connection connection = dataSourceMap.get(oldPrimaryDataSource).getConnection();
+                Statement statement = connection.createStatement()) {
+            checkRolePrimary(statement);
+        }
+    }
+
+    private void checkRolePrimary(final Statement statement) throws SQLException {
+        try (ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
+            while (resultSet.next()) {
+                if (!"Primary".equals(resultSet.getString("local_role"))) {
+                    throw new ShardingSphereConfigurationException("Instance is not Primary.");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void updatePrimaryDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames, final String groupName) {
+        Map<String, DataSource> activeDataSourceMap = new HashMap<>(dataSourceMap);
+        if (!disabledDataSourceNames.isEmpty()) {
+            activeDataSourceMap.entrySet().removeIf(each -> disabledDataSourceNames.contains(each.getKey()));
+        }
+        String newPrimaryDataSource = determinePrimaryDataSource(activeDataSourceMap);
+        if (newPrimaryDataSource.isEmpty()) {
+            return;
+        }
+        if (!newPrimaryDataSource.equals(oldPrimaryDataSource)) {
+            oldPrimaryDataSource = newPrimaryDataSource;
+            ShardingSphereEventBus.getInstance()
+                    .post(new PrimaryDataSourceChangedEvent(schemaName, groupName, newPrimaryDataSource));
+        }
+    }
+
+    private String determinePrimaryDataSource(final Map<String, DataSource> dataSourceMap) {
+        String result = "";
+        for (Map.Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
+            try (Connection connection = entry.getValue().getConnection();
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
+                if (resultSet.next()) {
+                    if (resultSet.getString("local_role").equals("Primary")
+                            && resultSet.getString("db_state").equals("Normal")) {
+                        return entry.getKey();
+                    }
+                }
+            } catch (final SQLException ex) {
+                log.error("An exception occurred while find primary data source url", ex);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void updateMemberState(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames) {
+        Map<String, DataSource> activeDataSourceMap = new HashMap<>(dataSourceMap);
+        if (!disabledDataSourceNames.isEmpty()) {
+            activeDataSourceMap.entrySet().removeIf(each -> disabledDataSourceNames.contains(each.getKey()));
+        }
+        determineDisabledDataSource(schemaName, activeDataSourceMap);
+    }
+
+    private void determineDisabledDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap) {
+        for (Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
+            boolean disable = true;
+            try (Connection connection = entry.getValue().getConnection();
+                Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
+                if (resultSet.next()) {
+                    if (resultSet.getString("local_role").equals("Standby") && resultSet.getString("db_state").equals("Normal")) {
+                        disable = false;
+                    }
+                }
+            } catch (final SQLException ex) {
+                log.error("An exception occurred while find data source urls", ex);
+            }
+            if (disable) {
+                ShardingSphereEventBus.getInstance().post(new DataSourceDisabledEvent(schemaName, entry.getKey(), true));
+            } else {
+                ShardingSphereEventBus.getInstance().post(new DataSourceDisabledEvent(schemaName, entry.getKey(), false));
+            }
+        }
+    }
+
+    @Override
+    public void startPeriodicalUpdate(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames, final String groupName) {
+        if (null == coordinatorRegistryCenter) {
+            ZookeeperConfiguration zkConfig = new ZookeeperConfiguration(props.getProperty("zkServerLists"),
+                    "opengauss-elasticjob");
+            coordinatorRegistryCenter = new ZookeeperRegistryCenter(zkConfig);
+            coordinatorRegistryCenter.init();
+        }
+        if (null != SCHEDULE_JOB_BOOTSTRAP_MAP.get(groupName)) {
+            SCHEDULE_JOB_BOOTSTRAP_MAP.get(groupName).shutdown();
+        }
+        SCHEDULE_JOB_BOOTSTRAP_MAP.put(groupName, new ScheduleJobBootstrap(coordinatorRegistryCenter,
+                new OpenGaussHeartbeatJob(this, schemaName, dataSourceMap, disabledDataSourceNames, groupName),
+                JobConfiguration.newBuilder("opengauss-" + groupName, 1).cron(props.getProperty("keepAliveCron")).build()));
+        SCHEDULE_JOB_BOOTSTRAP_MAP.get(groupName).schedule();
+    }
+
+    @Override
+    public String getPrimaryDataSource() {
+        return oldPrimaryDataSource;
+    }
+
+    @Override
+    public void updateProperties(final String groupName, final Properties props) {
+        new JobConfigurationAPIImpl(coordinatorRegistryCenter)
+                .updateJobConfiguration(createJobConfiguration("opengauss-" + groupName, props.getProperty("keepAliveCron")));
+    }
+
+    private JobConfigurationPOJO createJobConfiguration(final String jobName, final String cron) {
+        JobConfigurationPOJO result = new JobConfigurationPOJO();
+        result.setJobName(jobName);
+        result.setCron(cron);
+        result.setShardingTotalCount(1);
+        return result;
+    }
+
+    @Override
+    public String getType() {
+        return "openGauss";
+    }
+}
---
+++ shardingsphere-features/shardingsphere-db-discovery/shardingsphere-db-discovery-provider/shardingsphere-db-discovery-opengauss/src/test/java/org/apache/shardingsphere/dbdiscovery/opengauss/OpenGaussDatabaseDiscoveryTypeTest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.dbdiscovery.opengauss;
+
+import org.apache.shardingsphere.infra.exception.ShardingSphereException;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class OpenGaussDatabaseDiscoveryTypeTest {
+    
+    private static final String DB_ROLE = "select local_role,db_state from pg_stat_get_stream_replications()";
+    
+    private static final String STANDBYS = "select client_addr,sync_state from pg_stat_replication";
+
+    private final OpenGaussDatabaseDiscoveryType ogHaType = new OpenGaussDatabaseDiscoveryType();
+    
+    @Test
+    public void checkHAConfig() {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        Statement statement = mock(Statement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        try {
+            when(dataSource.getConnection()).thenReturn(connection);
+            when(connection.createStatement()).thenReturn(statement);
+            when(statement.executeQuery(DB_ROLE)).thenReturn(resultSet);
+            when(statement.executeQuery(STANDBYS)).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, false, true, false, true, false, true, false);
+            when(resultSet.getString("local_role")).thenReturn("Primary");
+            when(resultSet.getString("db_state")).thenReturn("Normal");
+            when(resultSet.getString("db_state")).thenReturn("Sync");
+        } catch (final SQLException ex) {
+            throw new ShardingSphereException(ex);
+        }
+        Map<String, DataSource> dataSourceMap = mock(HashMap.class);
+        when(dataSourceMap.get(null)).thenReturn(dataSource);
+        try {
+            ogHaType.getProps().setProperty("groupName", "group_name");
+            ogHaType.checkDatabaseDiscoveryConfiguration("discovery_db", dataSourceMap);
+        } catch (final SQLException ex) {
+            throw new ShardingSphereException(ex);
+        }
+    }
+    
+    @Test
+    public void updatePrimaryDataSource() {
+        List<DataSource> dataSources = new LinkedList<>();
+        List<Connection> connections = new LinkedList<>();
+        List<Statement> statements = new LinkedList<>();
+        List<ResultSet> resultSets = new LinkedList<>();
+        List<DatabaseMetaData> databaseMetaData = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            dataSources.add(mock(DataSource.class));
+            connections.add(mock(Connection.class));
+            statements.add(mock(Statement.class));
+            resultSets.add(mock(ResultSet.class));
+            databaseMetaData.add(mock(DatabaseMetaData.class));
+        }
+        try {
+            for (int i = 0; i < 3; i++) {
+                when(dataSources.get(i).getConnection()).thenReturn(connections.get(i));
+                when(connections.get(i).createStatement()).thenReturn(statements.get(i));
+                when(statements.get(i).executeQuery(DB_ROLE)).thenReturn(resultSets.get(i));
+                when(resultSets.get(i).next()).thenReturn(true, false);
+                when(resultSets.get(i).getString("local_role")).thenReturn("Primary");
+                when(resultSets.get(i).getString("db_state")).thenReturn("Normal");
+                when(connections.get(i).getMetaData()).thenReturn(databaseMetaData.get(i));
+                when(databaseMetaData.get(i).getURL()).thenReturn("jdbc:postgres://127.0.0.1:" + (3306 + i) + "/ds_0");
+            }
+        } catch (final SQLException ex) {
+            throw new ShardingSphereException(ex);
+        }
+        Map<String, DataSource> dataSourceMap = new HashMap<>(3, 1);
+        for (int i = 0; i < 3; i++) {
+            dataSourceMap.put(String.format("ds_%s", i), dataSources.get(i));
+        }
+        ogHaType.getProps().setProperty("groupName", "group_name");
+        ogHaType.updatePrimaryDataSource("discovery_db", dataSourceMap, Collections.emptySet(), "group_name");
+        assertThat(ogHaType.getPrimaryDataSource(), is("ds_2"));
+    }
+}
