59250e382ca7a7d3e1899e3fa2770dd2d02487dd
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 13:21:10 -0800

    Update ComposeClassStaticMethodAroundAdviceTest.java

--- shardingsphere-agent/shardingsphere-agent-core/src/test/java/org/apache/shardingsphere/agent/core/bytebuddy/transformer/advice/ComposeClassStaticMethodAroundAdviceTest.java
+++ shardingsphere-agent/shardingsphere-agent-core/src/test/java/org/apache/shardingsphere/agent/core/bytebuddy/transformer/advice/ComposeClassStaticMethodAroundAdviceTest.java
@@ -21,7 +21,6 @@ import org.apache.shardingsphere.agent.api.advice.ClassStaticMethodAroundAdvice;
 import org.apache.shardingsphere.agent.api.result.MethodInvocationResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Method;
@@ -29,52 +28,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ComposeClassStaticMethodAroundAdviceTest {
 
     private ComposeClassStaticMethodAroundAdvice composeClassStaticMethodAroundAdvice;
-    
+
     @Test
     public void beforeMethodTest() {
-        try {
-            ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
-            Method method = mock(Method.class);
-            MethodInvocationResult methodInvocationResult = mock(MethodInvocationResult.class);
-            List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
-            composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
-            composeClassStaticMethodAroundAdvice.beforeMethod(String.class, method, new Object[2], methodInvocationResult);
-        } catch (Exception e) {
-            fail();
-        }
+        ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
+        Method method = mock(Method.class);
+        MethodInvocationResult methodInvocationResult = mock(MethodInvocationResult.class);
+        List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
+        composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
+        composeClassStaticMethodAroundAdvice.beforeMethod(String.class, method, new Object[2], methodInvocationResult);
     }
-    
+
     @Test
     public void afterMethodTest() {
-        try {
-            ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
-            Method method = mock(Method.class);
-            MethodInvocationResult methodInvocationResult = mock(MethodInvocationResult.class);
-            List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
-            composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
-            composeClassStaticMethodAroundAdvice.afterMethod(String.class, method, new Object[2], methodInvocationResult);
-        } catch (Exception e) {
-            fail();
-        }
+        ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
+        Method method = mock(Method.class);
+        MethodInvocationResult methodInvocationResult = mock(MethodInvocationResult.class);
+        List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
+        composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
+        composeClassStaticMethodAroundAdvice.afterMethod(String.class, method, new Object[2], methodInvocationResult);
     }
-    
+
     @Test
     public void onThrowingTest() {
-        try {
-            ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
-            Method method = mock(Method.class);
-            List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
-            composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
-            composeClassStaticMethodAroundAdvice.onThrowing(String.class, method, new Object[2], new NullPointerException("Null Pointer"));
-        } catch (Exception e) {
-            fail();
-        }
+        ClassStaticMethodAroundAdvice advice = mock(ClassStaticMethodAroundAdvice.class);
+        Method method = mock(Method.class);
+        List<ClassStaticMethodAroundAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
+        composeClassStaticMethodAroundAdvice = new ComposeClassStaticMethodAroundAdvice(adviceList);
+        composeClassStaticMethodAroundAdvice.onThrowing(String.class, method, new Object[2], new NullPointerException("Null Pointer"));
     }
 }
