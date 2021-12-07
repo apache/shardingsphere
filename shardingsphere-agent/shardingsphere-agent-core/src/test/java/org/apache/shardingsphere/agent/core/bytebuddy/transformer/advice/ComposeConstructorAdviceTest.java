a59481b3141f98843b44c9b74a3de425140d01bd
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 13:21:46 -0800

    Update ComposeConstructorAdviceTest.java

--- shardingsphere-agent/shardingsphere-agent-core/src/test/java/org/apache/shardingsphere/agent/core/bytebuddy/transformer/advice/ComposeConstructorAdviceTest.java
+++ shardingsphere-agent/shardingsphere-agent-core/src/test/java/org/apache/shardingsphere/agent/core/bytebuddy/transformer/advice/ComposeConstructorAdviceTest.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.agent.core.bytebuddy.transformer.advice;
 
 import org.apache.shardingsphere.agent.api.advice.AdviceTargetObject;
-import org.apache.shardingsphere.agent.api.advice.ClassStaticMethodAroundAdvice;
 import org.apache.shardingsphere.agent.api.advice.ConstructorAdvice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,24 +27,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ComposeConstructorAdviceTest {
 
     private ComposeConstructorAdvice composeConstructorAdvice;
-    
+
     @Test
     public void onConstructorTest() {
-        try {
-            ConstructorAdvice advice = mock(ConstructorAdvice.class);
-            AdviceTargetObject adviceTargetObject = mock(AdviceTargetObject.class);
-            List<ConstructorAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
-            composeConstructorAdvice = new ComposeConstructorAdvice(adviceList);
-            composeConstructorAdvice.onConstructor(adviceTargetObject, new Object[2]);
-        } catch (Exception e) {
-            fail();
-        }
+        ConstructorAdvice advice = mock(ConstructorAdvice.class);
+        AdviceTargetObject adviceTargetObject = mock(AdviceTargetObject.class);
+        List<ConstructorAdvice> adviceList = new ArrayList<>(Arrays.asList(advice));
+        composeConstructorAdvice = new ComposeConstructorAdvice(adviceList);
+        composeConstructorAdvice.onConstructor(adviceTargetObject, new Object[2]);
     }
 }
