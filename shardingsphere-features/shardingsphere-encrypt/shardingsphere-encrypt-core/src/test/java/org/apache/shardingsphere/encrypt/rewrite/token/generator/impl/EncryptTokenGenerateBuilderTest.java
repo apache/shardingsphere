c69079e7e1a8bd6c738f15e5d89e15f76f079916
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 13:17:24 -0800

    Update EncryptTokenGenerateBuilderTest.java

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/impl/EncryptTokenGenerateBuilderTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/impl/EncryptTokenGenerateBuilderTest.java
@@ -17,24 +17,22 @@
 
 package org.apache.shardingsphere.encrypt.rewrite.token;
 
-import org.apache.shardingsphere.encrypt.rewrite.token.generator.impl.EncryptPredicateColumnTokenGenerator;
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.apache.shardingsphere.infra.rewrite.sql.token.generator.SQLTokenGenerator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EncryptTokenGenerateBuilderTest {
 
     private EncryptTokenGenerateBuilder builder;
-    
+
     @Test
     public void getSQLTokenGeneratorsTest() {
         final EncryptRule encryptRule = mock(EncryptRule.class);
