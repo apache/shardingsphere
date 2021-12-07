ef895873f6039fdf57a46a51dcb2cb25e9e90983
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 13:10:47 -0800

    Update AssistQueryAndPlainInsertColumnsTokenGeneratorTest.java

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/impl/AssistQueryAndPlainInsertColumnsTokenGeneratorTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/impl/AssistQueryAndPlainInsertColumnsTokenGeneratorTest.java
@@ -37,7 +37,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
