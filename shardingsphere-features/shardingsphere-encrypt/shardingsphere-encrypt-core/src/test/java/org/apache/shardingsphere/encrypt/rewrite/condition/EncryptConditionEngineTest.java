fe33e41e3ccd685e088039bfbf22bbbaa79ac63f
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 13:03:55 -0800

    Update EncryptConditionEngineTest.java

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/condition/EncryptConditionEngineTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/condition/EncryptConditionEngineTest.java
@@ -17,21 +17,14 @@
 
 package org.apache.shardingsphere.encrypt.rewrite.condition;
 
-import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.apache.shardingsphere.infra.binder.segment.insert.values.InsertSelectContext;
 import org.apache.shardingsphere.infra.binder.segment.table.TablesContext;
 import org.apache.shardingsphere.infra.binder.statement.SQLStatementContext;
 import org.apache.shardingsphere.infra.binder.statement.dml.InsertStatementContext;
 import org.apache.shardingsphere.infra.binder.statement.dml.SelectStatementContext;
-import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
-import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
-import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.BinaryOperationExpression;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ExpressionSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.InExpression;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.predicate.WhereSegment;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
-import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml.MySQLSelectStatement;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -43,7 +36,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -51,12 +46,6 @@ public class EncryptConditionEngineTest {
 
     @InjectMocks
     private EncryptConditionEngine encryptConditionEngine;
-
-    @Mock
-    private EncryptRule encryptRule;
-
-    @Mock
-    private ShardingSphereSchema shardingSphereSchema;
 
     @Mock
     private SQLStatementContext sqlStatementContext;
@@ -73,20 +62,17 @@ public class EncryptConditionEngineTest {
     @Mock
     private TablesContext tablesContext;
 
-    @Test
     public void createEncryptConditionsWithEmptyContextTest() {
         final Collection<EncryptCondition> encryptConditions = encryptConditionEngine.createEncryptConditions(sqlStatementContext);
         assertEquals(0, encryptConditions.size());
     }
 
-    @Test
     public void createEncryptConditionsFromInsertStatementContextTest() {
         when(insertStatementContext.getInsertSelectContext()).thenReturn(insertSelectContext);
         final Collection<EncryptCondition> encryptConditions = encryptConditionEngine.createEncryptConditions(insertStatementContext);
         assertEquals(0, encryptConditions.size());
     }
 
-    @Test
     public void createEncryptConditionsFromInsertIncludingSelectWithoutWhereTest() {
         when(insertStatementContext.getInsertSelectContext()).thenReturn(insertSelectContext);
         when(insertSelectContext.getSelectStatementContext()).thenReturn(selectStatementContext);
@@ -94,13 +80,29 @@ public class EncryptConditionEngineTest {
         assertEquals(0, encryptConditions.size());
     }
 
-    @Test
     public void createEncryptConditionsFromInsertIncludingSelectWhereTest() {
         when(insertStatementContext.getInsertSelectContext()).thenReturn(insertSelectContext);
         when(insertSelectContext.getSelectStatementContext()).thenReturn(selectStatementContext);
         ExpressionSegment expressionSegment = new InExpression(0, 0, null, null, false);
         WhereSegment whereSegment = new WhereSegment(0, 1, expressionSegment);
         when(selectStatementContext.getWhere()).thenReturn(Optional.of(whereSegment));
+        when(selectStatementContext.getTablesContext()).thenReturn(tablesContext);
+        final Collection<EncryptCondition> encryptConditions = encryptConditionEngine.createEncryptConditions(insertStatementContext);
+        assertEquals(0, encryptConditions.size());
+    }
+
+    public void createEncryptConditionsFromInsertTest() {
+        when(insertStatementContext.getInsertSelectContext()).thenReturn(insertSelectContext);
+        when(selectStatementContext.getSchemaName()).thenReturn("schema");
+        when(insertSelectContext.getSelectStatementContext()).thenReturn(selectStatementContext);
+        ExpressionSegment expressionSegment = new InExpression(0, 0, null, null, false);
+        WhereSegment whereSegment = new WhereSegment(0, 1, expressionSegment);
+        when(selectStatementContext.getWhere()).thenReturn(Optional.of(whereSegment));
+
+        Map<String, String> columnTablesNames = new HashMap<>();
+        columnTablesNames.put("table1", "column1");
+
+        when(tablesContext.findTableName(anyList(), any())).thenReturn(columnTablesNames);
         when(selectStatementContext.getTablesContext()).thenReturn(tablesContext);
         final Collection<EncryptCondition> encryptConditions = encryptConditionEngine.createEncryptConditions(insertStatementContext);
         assertEquals(0, encryptConditions.size());
