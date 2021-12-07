e990ee4195d51a9708c93be42ba253fac0061946
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-06 18:45:03 -0800

    Update DefaultAuthorityRuleConfigurationBuilderTest.java

--- shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/builder/DefaultAuthorityRuleConfigurationBuilderTest.java
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/builder/DefaultAuthorityRuleConfigurationBuilderTest.java
@@ -15,13 +15,15 @@
  * limitations under the License.
  */
 
-
 package org.apache.shardingsphere.authority.rule.builder;
 
 import org.apache.shardingsphere.authority.config.AuthorityRuleConfiguration;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class DefaultAuthorityRuleConfigurationBuilderTest {
 
