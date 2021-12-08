74ea32a77d5d1fd97ac0f2de9667cc930e84deee
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-07 19:55:58 -0800

    Update EncryptTokenGenerateBuilderTest.java

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/EncryptTokenGenerateBuilderTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/EncryptTokenGenerateBuilderTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.encrypt.rewrite.token;
+package org.apache.shardingsphere.encrypt.rewrite.token.generator;
 
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.apache.shardingsphere.infra.rewrite.sql.token.generator.SQLTokenGenerator;
