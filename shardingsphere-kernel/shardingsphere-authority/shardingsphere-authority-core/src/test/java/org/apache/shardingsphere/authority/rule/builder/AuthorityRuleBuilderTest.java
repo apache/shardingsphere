d9c2a263c84d54b024e6954ebe826ce2b5ce624e
Franco-H <84954135+Franco-H@users.noreply.github.com>
2021-12-09 22:54:44 -0800

    Add unit test for shardingsphere-infra-authority (Issue #10970) (#13961)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Rename shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/AuthorityRuleConfigurationYamlSwapperTest.java to shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/yaml/swapper/AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Rename shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/DefaultAuthorityRuleConfigurationBuilderTest.java to shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/builder/DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update and rename shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/AuthorityRuleBuilderTest.java to shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/builder/AuthorityRuleBuilderTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update AuthorityRuleBuilderTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update AuthorityRuleBuilderTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update AuthorityRuleConfigurationYamlSwapperTest.java
    
    * Update DefaultAuthorityRuleConfigurationBuilderTest.java
    
    * Update AuthorityRuleBuilderTest.java

---
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/rule/builder/AuthorityRuleBuilderTest.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.authority.rule.builder;
+
+import org.apache.shardingsphere.authority.config.AuthorityRuleConfiguration;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public final class AuthorityRuleBuilderTest {
+
+    private final AuthorityRuleBuilder ruleBuilder = new AuthorityRuleBuilder();
+
+    @Test
+    public void getOrderTest() {
+        int order = ruleBuilder.getOrder();
+        assertThat(500, is(order));
+    }
+    
+    @Test
+    public void getTypeClassTest() {
+        assertThat(AuthorityRuleConfiguration.class, equalTo(ruleBuilder.getTypeClass()));
+    }
+}
+
---
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/yaml/swapper/AuthorityRuleConfigurationYamlSwapperTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.authority.yaml.swapper;
+
+import org.apache.shardingsphere.authority.config.AuthorityRuleConfiguration;
+import org.apache.shardingsphere.authority.yaml.config.YamlAuthorityRuleConfiguration;
+import org.apache.shardingsphere.infra.config.algorithm.ShardingSphereAlgorithmConfiguration;
+import org.apache.shardingsphere.infra.yaml.config.pojo.algorithm.YamlShardingSphereAlgorithmConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class AuthorityRuleConfigurationYamlSwapperTest {
+
+    private final AuthorityRuleConfigurationYamlSwapper swapper = new AuthorityRuleConfigurationYamlSwapper();
+
+    @Test
+    public void swapToYamlConfigurationTest() {
+        AuthorityRuleConfiguration authorityRuleConfiguration = mock(AuthorityRuleConfiguration.class);
+        ShardingSphereAlgorithmConfiguration configuration = mock(ShardingSphereAlgorithmConfiguration.class);
+
+        when(authorityRuleConfiguration.getProvider()).thenReturn(configuration);
+        when(configuration.getType()).thenReturn("type");
+        when(configuration.getProps()).thenReturn(new Properties());
+
+        YamlAuthorityRuleConfiguration result = swapper.swapToYamlConfiguration(authorityRuleConfiguration);
+        assertNotNull(result);
+        assertThat(0, is(result.getUsers().size()));
+        assertNotNull(result.getProvider());
+    }
+
+    @Test
+    public void swapToObjectTest() {
+        YamlAuthorityRuleConfiguration authorityRuleConfiguration = mock(YamlAuthorityRuleConfiguration.class);
+        YamlShardingSphereAlgorithmConfiguration configuration = mock(YamlShardingSphereAlgorithmConfiguration.class);
+
+        when(authorityRuleConfiguration.getUsers()).thenReturn(Collections.singletonList("root@localhost:pass"));
+        when(authorityRuleConfiguration.getProvider()).thenReturn(configuration);
+        when(authorityRuleConfiguration.getProvider()).thenReturn(configuration);
+        when(configuration.getType()).thenReturn("type");
+        when(configuration.getProps()).thenReturn(new Properties());
+
+        AuthorityRuleConfiguration resultConfig = swapper.swapToObject(authorityRuleConfiguration);
+
+        assertNotNull(resultConfig);
+        assertNotNull(resultConfig.getUsers());
+        assertThat(1, is(resultConfig.getUsers().size()));
+        assertNotNull(resultConfig.getProvider());
+    }
+
+    @Test
+    public void getTypeClass() {
+        assertThat(AuthorityRuleConfiguration.class, equalTo(swapper.getTypeClass()));
+    }
+
+    @Test
+    public void getRuleTagName() {
+        assertThat("AUTHORITY", equalTo(swapper.getRuleTagName()));
+    }
+
+    @Test
+    public void getOrder() {
+        assertThat(500, is(swapper.getOrder()));
+    }
+}
+
