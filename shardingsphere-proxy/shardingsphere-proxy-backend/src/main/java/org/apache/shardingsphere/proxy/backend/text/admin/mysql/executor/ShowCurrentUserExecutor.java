8257ff4cb38d1db6d15696e05d7c433cea746c3e
yx9o <yangx_soft@163.com>
2021-12-20 11:17:00 +0800

    Update

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowCurrentUserExecutor.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowCurrentUserExecutor.java
@@ -42,13 +42,11 @@ import java.util.Optional;
 @Getter
 public final class ShowCurrentUserExecutor implements DatabaseAdminQueryExecutor {
     
+    public static final String FUNCTION_NAME = "current_user()";
+    
+    public static final String FUNCTION_NAME_ALIAS = "current_user";
+    
     private MergedResult mergedResult;
-    
-    private String showName;
-    
-    public ShowCurrentUserExecutor(final String showName) {
-        this.showName = showName;
-    }
     
     @Override
     public void execute(final ConnectionSession connectionSession) {
@@ -61,6 +59,6 @@ public final class ShowCurrentUserExecutor implements DatabaseAdminQueryExecutor
     
     @Override
     public QueryResultMetaData getQueryResultMetaData() {
-        return new RawQueryResultMetaData(Collections.singletonList(new RawQueryResultColumnMetaData("", showName, showName, Types.VARCHAR, "VARCHAR", 100, 0)));
+        return new RawQueryResultMetaData(Collections.singletonList(new RawQueryResultColumnMetaData("", FUNCTION_NAME, FUNCTION_NAME, Types.VARCHAR, "VARCHAR", 100, 0)));
     }
 }
--- shardingsphere-proxy/shardingsphere-proxy-backend/src/test/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowCurrentUserExecutorTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/test/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowCurrentUserExecutorTest.java
@@ -29,7 +29,6 @@ import org.apache.shardingsphere.mode.metadata.MetaDataContexts;
 import org.apache.shardingsphere.mode.metadata.persist.MetaDataPersistService;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
-import org.apache.shardingsphere.proxy.backend.util.CurrentUserEnum;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -75,7 +74,7 @@ public final class ShowCurrentUserExecutorTest {
     
     @Test
     public void assertExecute() throws SQLException {
-        ShowCurrentUserExecutor executor = new ShowCurrentUserExecutor(CurrentUserEnum.CURRENT_USER_BRACKETS.getValue());
+        ShowCurrentUserExecutor executor = new ShowCurrentUserExecutor();
         executor.execute(mockConnectionSession());
         assertThat(executor.getQueryResultMetaData().getColumnCount(), is(1));
         while (executor.getMergedResult().next()) {
