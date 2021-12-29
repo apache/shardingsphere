f0c96bc526ab6ace8c7a7a13f3dc2e81c8457c21
刘伟 <llzx373@gmail.com>
2021-12-29 16:10:59 +0800

    openGauss primary not need disable when discovery
    openGauss primary not need disable when discovery

--- shardingsphere-features/shardingsphere-db-discovery/shardingsphere-db-discovery-provider/shardingsphere-db-discovery-opengauss/src/main/java/org/apache/shardingsphere/dbdiscovery/opengauss/OpenGaussDatabaseDiscoveryType.java
+++ shardingsphere-features/shardingsphere-db-discovery/shardingsphere-db-discovery-provider/shardingsphere-db-discovery-opengauss/src/main/java/org/apache/shardingsphere/dbdiscovery/opengauss/OpenGaussDatabaseDiscoveryType.java
@@ -42,23 +42,24 @@ import java.util.Properties;
  */
 @Slf4j
 public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryType {
-    
+
     private static final String DB_ROLE = "select local_role,db_state from pg_stat_get_stream_replications()";
-    
+
     private String oldPrimaryDataSource;
-    
+
     @Getter
     @Setter
     private Properties props = new Properties();
-    
+
     @Override
-    public void checkDatabaseDiscoveryConfiguration(final String schemaName, final Map<String, DataSource> dataSourceMap) throws SQLException {
+    public void checkDatabaseDiscoveryConfiguration(final String schemaName,
+            final Map<String, DataSource> dataSourceMap) throws SQLException {
         try (Connection connection = dataSourceMap.get(oldPrimaryDataSource).getConnection();
                 Statement statement = connection.createStatement()) {
             checkRolePrimary(statement);
         }
     }
-    
+
     private void checkRolePrimary(final Statement statement) throws SQLException {
         try (ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
             while (resultSet.next()) {
@@ -68,9 +69,10 @@ public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryTy
             }
         }
     }
-    
+
     @Override
-    public void updatePrimaryDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap, final Collection<String> disabledDataSourceNames, final String groupName) {
+    public void updatePrimaryDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames, final String groupName) {
         Map<String, DataSource> activeDataSourceMap = new HashMap<>(dataSourceMap);
         if (!disabledDataSourceNames.isEmpty()) {
             activeDataSourceMap.entrySet().removeIf(each -> disabledDataSourceNames.contains(each.getKey()));
@@ -81,10 +83,11 @@ public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryTy
         }
         if (!newPrimaryDataSource.equals(oldPrimaryDataSource)) {
             oldPrimaryDataSource = newPrimaryDataSource;
-            ShardingSphereEventBus.getInstance().post(new PrimaryDataSourceChangedEvent(schemaName, groupName, newPrimaryDataSource));
+            ShardingSphereEventBus.getInstance()
+                    .post(new PrimaryDataSourceChangedEvent(schemaName, groupName, newPrimaryDataSource));
         }
     }
-    
+
     private String determinePrimaryDataSource(final Map<String, DataSource> dataSourceMap) {
         String result = "";
         for (Map.Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
@@ -92,7 +95,8 @@ public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryTy
                     Statement statement = connection.createStatement();
                     ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
                 if (resultSet.next()) {
-                    if (resultSet.getString("local_role").equals("Primary") && resultSet.getString("db_state").equals("Normal")) {
+                    if (resultSet.getString("local_role").equals("Primary")
+                            && resultSet.getString("db_state").equals("Normal")) {
                         return entry.getKey();
                     }
                 }
@@ -102,36 +106,50 @@ public final class OpenGaussDatabaseDiscoveryType implements DatabaseDiscoveryTy
         }
         return result;
     }
-    
+
     @Override
-    public void updateMemberState(final String schemaName, final Map<String, DataSource> dataSourceMap, final Collection<String> disabledDataSourceNames) {
+    public void updateMemberState(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames) {
         Map<String, DataSource> activeDataSourceMap = new HashMap<>(dataSourceMap);
-        determineDisabledDataSource(schemaName, activeDataSourceMap);
+        determineDisabledDataSource(schemaName, activeDataSourceMap, disabledDataSourceNames);
     }
-    
-    private void determineDisabledDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap) {
+
+    private void determineDisabledDataSource(final String schemaName, final Map<String, DataSource> dataSourceMap,
+            final Collection<String> disabledDataSourceNames) {
         for (Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
             boolean disable = true;
             try (Connection connection = entry.getValue().getConnection();
-                Statement statement = connection.createStatement();
-                 ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery(DB_ROLE)) {
                 if (resultSet.next()) {
-                    if (resultSet.getString("local_role").equals("Standby") && resultSet.getString("db_state").equals("Normal")) {
+                    String role = resultSet.getString("local_role");
+                    if (("Standby".equals(role) || "Primary".equals(role))
+                            && resultSet.getString("db_state").equals("Normal")) {
                         disable = false;
                     }
                 }
             } catch (final SQLException ex) {
                 log.error("An exception occurred while find data source urls", ex);
             }
-            ShardingSphereEventBus.getInstance().post(new DataSourceDisabledEvent(schemaName, entry.getKey(), disable));
+            String dataSourceName = entry.getKey();
+            if (disabledDataSourceNames.contains(dataSourceName) && (!disable)) {
+                ShardingSphereEventBus.getInstance()
+                        .post(new DataSourceDisabledEvent(schemaName, dataSourceName, false));
+                log.error("DBDiscovery Enable datasource:" + dataSourceName);
+            }
+            if ((!disabledDataSourceNames.contains(dataSourceName)) && disable) {
+                ShardingSphereEventBus.getInstance()
+                        .post(new DataSourceDisabledEvent(schemaName, dataSourceName, true));
+                log.error("DBDiscovery Disable datasource:" + dataSourceName);
+            }
         }
     }
-    
+
     @Override
     public String getPrimaryDataSource() {
         return oldPrimaryDataSource;
     }
-    
+
     @Override
     public String getType() {
         return "openGauss";
