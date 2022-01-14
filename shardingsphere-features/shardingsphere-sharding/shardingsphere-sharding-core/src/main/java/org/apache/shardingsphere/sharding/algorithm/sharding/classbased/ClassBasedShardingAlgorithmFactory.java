d999a859750ae3917233cbf83b05c5e9122b6697
strongduanmu <duanzhengqiang@apache.org>
2022-01-14 19:32:21 +0800

    support custom props for ClassBasedShardingAlgorithm

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/algorithm/sharding/classbased/ClassBasedShardingAlgorithmFactory.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/algorithm/sharding/classbased/ClassBasedShardingAlgorithmFactory.java
@@ -22,6 +22,9 @@ import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.shardingsphere.infra.exception.ShardingSphereException;
 import org.apache.shardingsphere.sharding.spi.ShardingAlgorithm;
+import org.apache.shardingsphere.spi.typed.TypedSPI;
+
+import java.util.Properties;
 
 /**
  * ShardingSphere class based algorithm factory.
@@ -32,20 +35,31 @@ public final class ClassBasedShardingAlgorithmFactory {
     /**
      * Create sharding algorithm.
      *
+     * @param <T> class generic type
      * @param shardingAlgorithmClassName sharding algorithm class name
      * @param superShardingAlgorithmClass sharding algorithm super class
-     * @param <T> class generic type
+     * @param props properties
      * @return sharding algorithm instance
      */
     @SneakyThrows
     @SuppressWarnings("unchecked")
-    public static <T extends ShardingAlgorithm> T newInstance(final String shardingAlgorithmClassName, final Class<T> superShardingAlgorithmClass) {
+    public static <T extends ShardingAlgorithm> T newInstance(final String shardingAlgorithmClassName, final Class<T> superShardingAlgorithmClass, final Properties props) {
         Class<?> result = Class.forName(shardingAlgorithmClassName);
         if (!superShardingAlgorithmClass.isAssignableFrom(result)) {
             throw new ShardingSphereException("Class %s should be implement %s", shardingAlgorithmClassName, superShardingAlgorithmClass.getName());
         }
         T instance = (T) result.newInstance();
+        setProperties(instance, props);
         instance.init();
         return instance;
+    }
+    
+    private static <T extends TypedSPI> void setProperties(final T instance, final Properties props) {
+        if (null == props) {
+            return;
+        }
+        Properties newProps = new Properties();
+        props.forEach((key, value) -> newProps.setProperty(key.toString(), null == value ? null : value.toString()));
+        instance.setProps(newProps);
     }
 }
