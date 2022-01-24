e3471f2e221a172286e536326be4136e45281514
RaigorJiang <raigor.jiang@gmail.com>
2022-01-24 10:52:34 +0800

    Refactor & fix IT test.

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/metadata/impl/HikariDataSourcePoolMetaData.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/metadata/impl/HikariDataSourcePoolMetaData.java
@@ -71,8 +71,6 @@ public final class HikariDataSourcePoolMetaData implements DataSourcePoolMetaDat
     }
     
     private void buildTransientFieldNames() {
-        transientFieldNames.add("username");
-        transientFieldNames.add("password");
         transientFieldNames.add("running");
         transientFieldNames.add("poolName");
         transientFieldNames.add("registerMbeans");
--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/distsql/rql/resource/DataSourceQueryResultSet.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/distsql/rql/resource/DataSourceQueryResultSet.java
@@ -20,28 +20,18 @@ package org.apache.shardingsphere.proxy.backend.text.distsql.rql.resource;
 import com.google.gson.Gson;
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.ShowResourcesStatement;
 import org.apache.shardingsphere.infra.database.metadata.DataSourceMetaData;
-import org.apache.shardingsphere.infra.datasource.pool.creator.DataSourceReflection;
-import org.apache.shardingsphere.infra.datasource.pool.metadata.DataSourcePoolMetaData;
-import org.apache.shardingsphere.infra.datasource.pool.metadata.DataSourcePoolMetaDataFactory;
 import org.apache.shardingsphere.infra.datasource.props.DataSourceProperties;
 import org.apache.shardingsphere.infra.datasource.props.DataSourcePropertiesCreator;
 import org.apache.shardingsphere.infra.distsql.query.DistSQLResultSet;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
 import org.apache.shardingsphere.infra.metadata.resource.ShardingSphereResource;
-import org.apache.shardingsphere.mode.metadata.persist.MetaDataPersistService;
-import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
 
 /**
  * Result set for show data source.
@@ -69,10 +59,7 @@ public final class DataSourceQueryResultSet implements DistSQLResultSet {
     @Override
     public void init(final ShardingSphereMetaData metaData, final SQLStatement sqlStatement) {
         resource = metaData.getResource();
-        Optional<MetaDataPersistService> persistService = ProxyContext.getInstance().getContextManager().getMetaDataContexts().getMetaDataPersistService();
-        dataSourcePropsMap = persistService.isPresent()
-                ? persistService.get().getDataSourceService().load(metaData.getName())
-                : DataSourcePropertiesCreator.create(metaData.getResource().getDataSources());
+        dataSourcePropsMap = DataSourcePropertiesCreator.create(metaData.getResource().getDataSources());
         dataSourceNames = dataSourcePropsMap.keySet().iterator();
     }
     
@@ -90,14 +77,6 @@ public final class DataSourceQueryResultSet implements DistSQLResultSet {
     @Override
     public Collection<Object> getRowData() {
         String dataSourceName = dataSourceNames.next();
-        DataSourceProperties dataSourceProperties = dataSourcePropsMap.get(dataSourceName);
-        DataSourcePoolMetaData poolMetaData = DataSourcePoolMetaDataFactory.newInstance(dataSourceProperties.getDataSourceClassName());
-        Map<String, Object> allProperties = getAllProperties(resource.getDataSources().get(dataSourceName));
-        Map<String, Object> standardProps = getStandardProperties(dataSourceProperties, poolMetaData, allProperties);
-        return getRowData(dataSourceName, poolMetaData, allProperties, standardProps);
-    }
-    
-    private Collection<Object> getRowData(final String dataSourceName, final DataSourcePoolMetaData poolMetaData, final Map<String, Object> allProperties, final Map<String, Object> standardProps) {
         DataSourceMetaData metaData = resource.getDataSourcesMetaData().getDataSourceMetaData(dataSourceName);
         Collection<Object> result = new LinkedList<>();
         result.add(dataSourceName);
@@ -105,52 +84,24 @@ public final class DataSourceQueryResultSet implements DistSQLResultSet {
         result.add(metaData.getHostname());
         result.add(metaData.getPort());
         result.add(metaData.getCatalog());
-        result.add(getStandardProperty(standardProps, CONNECTION_TIMEOUT_MILLISECONDS));
-        result.add(getStandardProperty(standardProps, IDLE_TIMEOUT_MILLISECONDS));
-        result.add(getStandardProperty(standardProps, MAX_LIFETIME_MILLISECONDS));
-        result.add(getStandardProperty(standardProps, MAX_POOL_SIZE));
-        result.add(getStandardProperty(standardProps, MIN_POOL_SIZE));
-        result.add(getStandardProperty(standardProps, READ_ONLY));
-        result.add(new Gson().toJson(getFilteredUndisplayedProperties(poolMetaData, allProperties, standardProps)));
+        DataSourceProperties dataSourceProperties = dataSourcePropsMap.get(dataSourceName);
+        Map<String, Object> standardProperties = dataSourceProperties.getPoolPropertySynonyms().getStandardProperties();
+        result.add(getStandardProperty(standardProperties, CONNECTION_TIMEOUT_MILLISECONDS));
+        result.add(getStandardProperty(standardProperties, IDLE_TIMEOUT_MILLISECONDS));
+        result.add(getStandardProperty(standardProperties, MAX_LIFETIME_MILLISECONDS));
+        result.add(getStandardProperty(standardProperties, MAX_POOL_SIZE));
+        result.add(getStandardProperty(standardProperties, MIN_POOL_SIZE));
+        result.add(getStandardProperty(standardProperties, READ_ONLY));
+        Map<String, Object> otherProperties = dataSourceProperties.getCustomDataSourceProperties().getProperties();
+        result.add(otherProperties.isEmpty() ? "" : new Gson().toJson(otherProperties));
         return result;
     }
     
-    private Map<String, Object> getAllProperties(final DataSource dataSource) {
-        DataSourceReflection dataSourceReflection = new DataSourceReflection(dataSource);
-        Map<String, Object> result = dataSourceReflection.convertToProperties();
-        return result;
-    }
-    
-    private Map<String, Object> getStandardProperties(final DataSourceProperties dataSourceProperties, final DataSourcePoolMetaData poolMetaData, final Map<String, Object> allProperties) {
-        Collection<String> standardPropertyKeys = dataSourceProperties.getPoolPropertySynonyms().getStandardPropertyKeys();
-        Map<String, Object> result = new HashMap<>(standardPropertyKeys.size(), 1);
-        Map<String, String> propertySynonyms = poolMetaData.getPropertySynonyms();
-        for (String each : standardPropertyKeys) {
-            if (propertySynonyms.containsKey(each)) {
-                result.put(each, allProperties.get(propertySynonyms.get(each)));
-            } else {
-                result.put(each, allProperties.get(each));
-            }
-        }
-        return result;
-    }
-    
-    private String getStandardProperty(final Map<String, Object> standardProps, final String key) {
-        if (standardProps.containsKey(key) && null != standardProps.get(key)) {
-            return standardProps.get(key).toString();
+    private String getStandardProperty(final Map<String, Object> standardProperties, final String key) {
+        if (standardProperties.containsKey(key) && null != standardProperties.get(key)) {
+            return standardProperties.get(key).toString();
         }
         return "";
-    }
-    
-    private Map<String, Object> getFilteredUndisplayedProperties(final DataSourcePoolMetaData poolMetaData, final Map<String, Object> allProperties, final Map<String, Object> standardProps) {
-        Map<String, String> propertySynonyms = poolMetaData.getPropertySynonyms();
-        Map<String, Object> result = new LinkedHashMap<>(allProperties);
-        standardProps.forEach(result::remove);
-        propertySynonyms.keySet().forEach(result::remove);
-        propertySynonyms.values().forEach(result::remove);
-        poolMetaData.getTransientFieldNames().forEach(result::remove);
-        result.remove(poolMetaData.getJdbcUrlPropertiesFieldName());
-        return new TreeMap<>(result);
     }
     
     @Override
