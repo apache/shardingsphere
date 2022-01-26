9732526268fc81df501a9d3845dae44ec0276168
Hongsheng Zhong <sandynz@126.com>
2022-01-26 16:33:54 +0800

    Reuse PipelineDataSourceManager to reduce connection creation and destruction (#15087)
    * Reuse PipelineDataSourceManager to reduce connection creation and destruction
    
    * Refactor dataSourceManager cleanup

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/task/IncrementalTask.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/task/IncrementalTask.java
@@ -67,9 +67,9 @@ public final class IncrementalTask extends AbstractLifecycleExecutor implements 
     private final IncrementalTaskProgress progress;
     
     public IncrementalTask(final int concurrency, final DumperConfiguration dumperConfig, final ImporterConfiguration importerConfig,
-            final PipelineChannelFactory pipelineChannelFactory, final ExecuteEngine incrementalDumperExecuteEngine) {
+                           final PipelineChannelFactory pipelineChannelFactory, final PipelineDataSourceManager dataSourceManager,
+                           final ExecuteEngine incrementalDumperExecuteEngine) {
         this.incrementalDumperExecuteEngine = incrementalDumperExecuteEngine;
-        PipelineDataSourceManager dataSourceManager = new PipelineDataSourceManager();
         this.dataSourceManager = dataSourceManager;
         taskId = dumperConfig.getDataSourceName();
         progress = new IncrementalTaskProgress();
@@ -86,7 +86,6 @@ public final class IncrementalTask extends AbstractLifecycleExecutor implements 
         Future<?> future = incrementalDumperExecuteEngine.submitAll(importers, getExecuteCallback());
         dumper.start();
         waitForResult(future);
-        dataSourceManager.close();
     }
     
     private Collection<Importer> createImporters(final int concurrency, final ImporterConfiguration importerConfig, final PipelineDataSourceManager dataSourceManager, final PipelineChannel channel) {
@@ -140,6 +139,5 @@ public final class IncrementalTask extends AbstractLifecycleExecutor implements 
             each.stop();
         }
         channel.close();
-        dataSourceManager.close();
     }
 }
