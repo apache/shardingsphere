40309a48cc3295ba5f42c84d5aeb0a425b7ead0d
strongduanmu <duanzhengqiang@apache.org>
2022-01-28 14:19:13 +0800

    fix unit test

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/main/java/org/apache/shardingsphere/encrypt/context/EncryptContextBuilder.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/main/java/org/apache/shardingsphere/encrypt/context/EncryptContextBuilder.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.encrypt.context;
 
 import lombok.AccessLevel;
@@ -14,6 +31,15 @@ import java.util.Optional;
 @NoArgsConstructor(access = AccessLevel.NONE)
 public final class EncryptContextBuilder {
     
+    /**
+     * Build encrypt context.
+     * 
+     * @param schemaName schema name
+     * @param tableName table name
+     * @param columnName column name
+     * @param encryptRule encrypt rule
+     * @return encrypt context
+     */
     public static EncryptContext build(final String schemaName, final String tableName, final String columnName, final EncryptRule encryptRule) {
         EncryptContext result = new EncryptContext(schemaName, tableName, columnName);
         Optional<EncryptColumn> encryptColumn = encryptRule.findEncryptTable(tableName).flatMap(optional -> optional.findEncryptColumn(columnName));
