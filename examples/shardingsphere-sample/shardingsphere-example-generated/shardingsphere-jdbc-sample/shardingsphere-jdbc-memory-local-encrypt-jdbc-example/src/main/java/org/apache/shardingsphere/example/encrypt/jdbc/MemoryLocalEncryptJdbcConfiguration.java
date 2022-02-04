7f37e999055a7bb81b0ab1150d4ec00d78d444a2
zhangliang <zhangliang@apache.org>
2022-02-04 16:53:27 +0800

    For code format

--- examples/shardingsphere-sample/shardingsphere-example-generated/shardingsphere-jdbc-sample/shardingsphere-jdbc-memory-local-encrypt-jdbc-example/src/main/java/org/apache/shardingsphere/example/encrypt/jdbc/MemoryLocalEncryptJdbcConfiguration.java
+++ examples/shardingsphere-sample/shardingsphere-example-generated/shardingsphere-jdbc-sample/shardingsphere-jdbc-memory-local-encrypt-jdbc-example/src/main/java/org/apache/shardingsphere/example/encrypt/jdbc/MemoryLocalEncryptJdbcConfiguration.java
@@ -45,8 +45,9 @@ public final class MemoryLocalEncryptJdbcConfiguration {
     /**
      * Create a DataSource object, which is an object rewritten by ShardingSphere itself 
      * and contains various rules for rewriting the original data storage. When in use, you only need to use this object.
-     * @return
-     * @throws SQLException
+     * 
+     * @return data source
+     * @throws SQLException SQL exception
     */
     public DataSource getDataSource() throws SQLException {
         return ShardingSphereDataSourceFactory.createDataSource(createDataSource("demo_ds_0"), Collections.singleton(createEncryptRuleConfiguration()), new Properties());
