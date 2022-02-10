213ccab184700ef20f24a0d84c20febc24b53f50
liguoping <xdliguoping@163.com>
2022-02-10 18:04:51 +0800

    queryResult spi

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-distsql/shardingsphere-sharding-distsql-handler/src/main/java/org/apache/shardingsphere/sharding/distsql/handler/query/ShardingTableRulesUsedKeyGeneratorQueryResult.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-distsql/shardingsphere-sharding-distsql-handler/src/main/java/org/apache/shardingsphere/sharding/distsql/handler/query/ShardingTableRulesUsedKeyGeneratorQueryResult.java
@@ -1,10 +1,8 @@
-package org.apache.shardingsphere.proxy.backend.text.distsql.rql.rule;
+package org.apache.shardingsphere.sharding.distsql.handler.query;
 
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.ShowShardingTableRulesUsedKeyGeneratorStatement;
 import org.apache.shardingsphere.infra.distsql.query.DistSQLResultSet;
-import org.apache.shardingsphere.infra.exception.SchemaNotExistedException;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
-import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 
@@ -15,37 +13,37 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Result set for show sharding table rules.
+ */
 public final class ShardingTableRulesUsedKeyGeneratorQueryResult implements DistSQLResultSet {
-    
-    private static final String SHARDING = "sharding";
     
     private Iterator<Collection<Object>> data = Collections.emptyIterator();
     
     @Override
     public void init(final ShardingSphereMetaData metaData, final SQLStatement sqlStatement) {
-        ShardingSphereMetaData tempMetaData = metaData;
         ShowShardingTableRulesUsedKeyGeneratorStatement statement = (ShowShardingTableRulesUsedKeyGeneratorStatement) sqlStatement;
-        String schemaName = "";
-        if (statement.getSchema().isPresent()) {
-            schemaName = statement.getSchema().get().getIdentifier().getValue();
-            if (ProxyContext.getInstance().getAllSchemaNames().contains(schemaName)) {
-                throw new SchemaNotExistedException(schemaName);
-            }
-            tempMetaData = ProxyContext.getInstance().getMetaData(schemaName);
-        }
         List<Collection<Object>> result = new ArrayList<>();
-        Collection<ShardingRuleConfiguration> shardingTableRules = tempMetaData.getRuleMetaData().findRuleConfiguration(ShardingRuleConfiguration.class);
-        if (statement.getKeyGeneratorName().isPresent()) {
-            shardingTableRules.forEach(shardingTableRule -> shardingTableRule.getTables().stream()
-                    .filter(table -> null != table.getKeyGenerateStrategy() && statement.getKeyGeneratorName().get().equals(table.getKeyGenerateStrategy().getKeyGeneratorName()))
-                    .forEach(table -> result.add(Arrays.asList(SHARDING, schemaName, table.getLogicTable()))));
-        }
+        Collection<ShardingRuleConfiguration> shardingTableRules = metaData.getRuleMetaData().findRuleConfiguration(ShardingRuleConfiguration.class);
+        shardingTableRules.forEach(each -> requireResult(statement, metaData.getName(), result, each));
         data = result.iterator();
+    }
+    
+    private void requireResult(final ShowShardingTableRulesUsedKeyGeneratorStatement statement, final String schemaName, final List<Collection<Object>> result,
+                               final ShardingRuleConfiguration shardingRuleConfiguration) {
+        if (!statement.getKeyGeneratorName().isPresent()) {
+            return;
+        }
+        shardingRuleConfiguration.getTables().forEach(each -> {
+            if (null != each.getKeyGenerateStrategy() && statement.getKeyGeneratorName().get().equals(each.getKeyGenerateStrategy().getKeyGeneratorName())) {
+                result.add(Arrays.asList(schemaName, each.getLogicTable()));
+            }
+        });
     }
     
     @Override
     public Collection<String> getColumnNames() {
-        return Arrays.asList("type", "schema", "name");
+        return Arrays.asList("schema", "name");
     }
     
     @Override
