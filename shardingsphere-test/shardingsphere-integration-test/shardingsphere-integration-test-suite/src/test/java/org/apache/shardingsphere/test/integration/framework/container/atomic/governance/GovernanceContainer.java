cc72e1237b40db9565a6b8e2a42243d458a84c39
zhangliang <zhangliang@apache.org>
2022-02-15 22:37:30 +0800

    Refactor GovernanceContainer

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/container/atomic/governance/GovernanceContainer.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/container/atomic/governance/GovernanceContainer.java
@@ -18,16 +18,14 @@
 package org.apache.shardingsphere.test.integration.framework.container.atomic.governance;
 
 import org.apache.shardingsphere.test.integration.framework.container.atomic.DockerITContainer;
-import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 
 /**
  * Governance container.
  */
 public abstract class GovernanceContainer extends DockerITContainer {
     
-    public GovernanceContainer(final String name, final String dockerImageName, final boolean isFakedContainer) {
-        super(name, dockerImageName, isFakedContainer);
-        setWaitStrategy(new LogMessageWaitStrategy().withRegEx(".*PrepRequestProcessor \\(sid:[0-9]+\\) started.*"));
+    public GovernanceContainer(final String name, final String dockerImageName) {
+        super(name, dockerImageName, false);
     }
     
     /**
