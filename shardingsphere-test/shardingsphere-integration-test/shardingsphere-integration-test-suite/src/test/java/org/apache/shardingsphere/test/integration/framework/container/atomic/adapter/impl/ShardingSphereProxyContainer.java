a32bd255c3bf88e81532ac3ae4c847d807d9e8a0
zhangliang <zhangliang@apache.org>
2022-02-19 18:30:25 +0800

    Refactor ShardingSphereProxyContainer

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/container/atomic/adapter/impl/ShardingSphereProxyContainer.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/container/atomic/adapter/impl/ShardingSphereProxyContainer.java
@@ -17,16 +17,9 @@
 
 package org.apache.shardingsphere.test.integration.framework.container.atomic.adapter.impl;
 
-import com.google.common.io.ByteStreams;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.authority.yaml.config.YamlAuthorityRuleConfiguration;
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
-import org.apache.shardingsphere.infra.metadata.user.yaml.config.YamlUserConfiguration;
-import org.apache.shardingsphere.infra.metadata.user.yaml.config.YamlUsersConfigurationConverter;
-import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
-import org.apache.shardingsphere.proxy.config.yaml.YamlProxyServerConfiguration;
 import org.apache.shardingsphere.test.integration.env.DataSourceEnvironment;
 import org.apache.shardingsphere.test.integration.framework.container.atomic.DockerITContainer;
 import org.apache.shardingsphere.test.integration.framework.container.atomic.adapter.AdapterContainer;
@@ -35,9 +28,6 @@ import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.utility.MountableFile;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -64,18 +54,6 @@ public final class ShardingSphereProxyContainer extends DockerITContainer implem
     }
     
     /**
-     * Mount path into container from classpath.
-     *
-     * @param classPathResource resource path in classpath
-     * @param containerPath path in container
-     * @return self
-     */
-    public ShardingSphereProxyContainer withClassPathResourceMapping(final String classPathResource, final String containerPath) {
-        withCopyFileToContainer(MountableFile.forClasspathResource(classPathResource), containerPath);
-        return this;
-    }
-    
-    /**
      * Mount the agent into container.
      *
      * @param agentHome agent home
@@ -89,7 +67,7 @@ public final class ShardingSphereProxyContainer extends DockerITContainer implem
     
     @Override
     protected void configure() {
-        withClassPathResourceMapping("/env/" + scenario + "/docker/proxy/conf/" + databaseType.getName().toLowerCase(), "/opt/shardingsphere-proxy/conf");
+        withCopyFileToContainer(MountableFile.forClasspathResource("/env/" + scenario + "/docker/proxy/conf/" + databaseType.getName().toLowerCase()), "/opt/shardingsphere-proxy/conf");
         setWaitStrategy(new LogMessageWaitStrategy().withRegEx(".*ShardingSphere-Proxy .* mode started successfully.*"));
         super.configure();
     }
@@ -107,24 +85,10 @@ public final class ShardingSphereProxyContainer extends DockerITContainer implem
         HikariDataSource result = new HikariDataSource();
         result.setDriverClassName(DataSourceEnvironment.getDriverClassName(databaseType));
         result.setJdbcUrl(DataSourceEnvironment.getURL(databaseType, getHost(), getMappedPort(3307), scenario));
-        YamlUserConfiguration userConfig = loadUserConfiguration();
-        result.setUsername(userConfig.getUsername());
-        result.setPassword(userConfig.getPassword());
+        result.setUsername("root");
+        result.setPassword("root");
         result.setMaximumPoolSize(2);
         result.setTransactionIsolation("TRANSACTION_READ_COMMITTED");
         return result;
-    }
-    
-    @SneakyThrows(IOException.class)
-    private YamlUserConfiguration loadUserConfiguration() {
-        String serverFile = "/env/" + scenario + "/docker/proxy/conf/" + databaseType.getName().toLowerCase() + "/server.yaml";
-        YamlProxyServerConfiguration serverConfig = YamlEngine.unmarshal(
-                ByteStreams.toByteArray(Objects.requireNonNull(this.getClass().getResourceAsStream(serverFile))), YamlProxyServerConfiguration.class);
-        return YamlUsersConfigurationConverter.convertYamlUserConfiguration(getProxyUsers(serverConfig)).stream().findFirst().orElse(new YamlUserConfiguration());
-    }
-    
-    private Collection<String> getProxyUsers(final YamlProxyServerConfiguration serverConfig) {
-        return serverConfig.getRules()
-                .stream().filter(each -> each instanceof YamlAuthorityRuleConfiguration).findFirst().map(each -> ((YamlAuthorityRuleConfiguration) each).getUsers()).orElse(Collections.emptyList());
     }
 }
