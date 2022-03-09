0b41214dc72a9eedbc4adb523383673ecafbcae7
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-03-09 21:07:15 +0530

    Space added after ,

--- shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-api/src/main/java/org/apache/shardingsphere/readwritesplitting/api/rule/ReadWriteSplittingDataSourceRuleTest.java
+++ shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-api/src/main/java/org/apache/shardingsphere/readwritesplitting/api/rule/ReadWriteSplittingDataSourceRuleTest.java
@@ -18,25 +18,25 @@ public final class ReadWriteSplittingDataSourceRuleTest {
 
     @Before
     public void setup(){
-        readwriteSplittingDataSourceRuleConfig = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Static", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
-        readwriteSplittingDataSourceRuleConfigDynamic = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Dynamic", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
+        readwriteSplittingDataSourceRuleConfig = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Static", getProperties("write_ds", "read_ds_0,read_ds_1"), "");
+        readwriteSplittingDataSourceRuleConfigDynamic = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Dynamic", getProperties("write_ds", "read_ds_0,read_ds_1"), "");
     }
 
     @Test
     public void assertGetAutoAwareDataSourceName(){
         String actual = readwriteSplittingDataSourceRuleConfigDynamic.getProps().getProperty("auto-aware-data-source-name");
-        assertThat(actual,is("ds"));
+        assertThat(actual, is("ds"));
     }
 
     @Test
     public void assertGetWriteDataSourceName(){
         String actual = readwriteSplittingDataSourceRuleConfig.getProps().getProperty("write-data-source-name");
-        assertThat(actual,is("write_ds"));
+        assertThat(actual, is("write_ds"));
     }
 
     @Test
     public void assertGetReadDataSourceNames(){
-        assertThat(readwriteSplittingDataSourceRuleConfig.getProps().getProperty("read-data-source-names"),is(Arrays.asList("read_ds_0", "read_ds_1")));
+        assertThat(readwriteSplittingDataSourceRuleConfig.getProps().getProperty("read-data-source-names"), is(Arrays.asList("read_ds_0", "read_ds_1")));
     }
 
     private Properties getProperties(final String writeDataSource, final String readDataSources) {
