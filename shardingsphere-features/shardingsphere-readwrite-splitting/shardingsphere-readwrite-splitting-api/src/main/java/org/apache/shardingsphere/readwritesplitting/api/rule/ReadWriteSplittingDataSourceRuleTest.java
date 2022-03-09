7e71cc53a9c52aa8b4df86e3543e8c32b53598f2
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-03-09 20:33:31 +0530

    Spaces added

--- shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-api/src/main/java/org/apache/shardingsphere/readwritesplitting/api/rule/ReadWriteSplittingDataSourceRuleTest.java
+++ shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-api/src/main/java/org/apache/shardingsphere/readwritesplitting/api/rule/ReadWriteSplittingDataSourceRuleTest.java
@@ -20,30 +20,30 @@ public class ReadWriteSplittingDataSourceRuleTest {
 
     @Before
     public void setup(){
-        readwriteSplittingDataSourceRuleConfig=new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Static", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
-        readwriteSplittingDataSourceRuleConfigDynamic=new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Dynamic", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
+        readwriteSplittingDataSourceRuleConfig = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Static", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
+        readwriteSplittingDataSourceRuleConfigDynamic = new ReadwriteSplittingDataSourceRuleConfiguration("ds", "Dynamic", getProperties("write_ds", "read_ds_0,read_ds_1"),"");
     }
 
     @Test
     public void assertGetAutoAwareDataSourceName(){
 
-        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance=new ReadwriteSplittingDataSourceRuleConfiguration("ds","Dynamic",getProperties("write_ds","read_ds_0,read_ds_1"),"");
-        String actual= readwriteSplittingDataSourceRuleConfigDynamic.getProps().getProperty("auto-aware-data-source-name");
-        String testing= anotherInstance.getProps().getProperty("auto-aware-data-source-name");
+        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance = new ReadwriteSplittingDataSourceRuleConfiguration("ds","Dynamic",getProperties("write_ds","read_ds_0,read_ds_1"),"");
+        String actual = readwriteSplittingDataSourceRuleConfigDynamic.getProps().getProperty("auto-aware-data-source-name");
+        String testing = anotherInstance.getProps().getProperty("auto-aware-data-source-name");
         assertThat(testing,is(actual));
     }
 
     @Test
     public void assertGetWriteDataSourceName(){
-        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance=new ReadwriteSplittingDataSourceRuleConfiguration("ds","Static",getProperties("write_ds","read_ds_0,read_ds_1"),"");
-        String actual= readwriteSplittingDataSourceRuleConfig.getProps().getProperty("write-data-source-name");
-        String testing= anotherInstance.getProps().getProperty("write-data-source-name");
+        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance = new ReadwriteSplittingDataSourceRuleConfiguration("ds","Static",getProperties("write_ds","read_ds_0,read_ds_1"),"");
+        String actual = readwriteSplittingDataSourceRuleConfig.getProps().getProperty("write-data-source-name");
+        String testing = anotherInstance.getProps().getProperty("write-data-source-name");
         assertThat(testing,is(actual));
     }
 
     @Test
     public void assertGetReadDataSourceNames(){
-        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance=new ReadwriteSplittingDataSourceRuleConfiguration("ds","Static",getProperties("write_ds","read_ds_0,read_ds_1"),"");
+        ReadwriteSplittingDataSourceRuleConfiguration anotherInstance = new ReadwriteSplittingDataSourceRuleConfiguration("ds","Static",getProperties("write_ds","read_ds_0,read_ds_1"),"");
         assertThat(anotherInstance.getProps().getProperty("read-data-source-names"),is(Arrays.asList("read_ds_0", "read_ds_1")));
     }
 
