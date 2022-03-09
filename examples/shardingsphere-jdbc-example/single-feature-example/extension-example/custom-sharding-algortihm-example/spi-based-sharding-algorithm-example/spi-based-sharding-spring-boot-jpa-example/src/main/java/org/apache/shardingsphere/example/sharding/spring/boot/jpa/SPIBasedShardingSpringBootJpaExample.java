1d61e4f32e8b11c488794ad037d730cbf748e364
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2022-03-09 10:30:53 +0530

    Add proposed changes

--- examples/shardingsphere-jdbc-example/single-feature-example/extension-example/custom-sharding-algortihm-example/spi-based-sharding-algorithm-example/spi-based-sharding-spring-boot-jpa-example/src/main/java/org/apache/shardingsphere/example/sharding/spring/boot/jpa/SPIBasedShardingSpringBootJpaExample.java
+++ examples/shardingsphere-jdbc-example/single-feature-example/extension-example/custom-sharding-algortihm-example/spi-based-sharding-algorithm-example/spi-based-sharding-spring-boot-jpa-example/src/main/java/org/apache/shardingsphere/example/sharding/spring/boot/jpa/SPIBasedShardingSpringBootJpaExample.java
@@ -33,7 +33,7 @@ import java.sql.SQLException;
 @SpringBootApplication(exclude = JtaAutoConfiguration.class)
 public class ShardingSpringBootJpaExample {
     
-    public static void main(String[] args) throws SQLException {
+    public static void main(final String[] args) throws SQLException {
         try (ConfigurableApplicationContext applicationContext = SpringApplication.run(ShardingSpringBootJpaExample.class, args)) {
             ExampleExecuteTemplate.run(applicationContext.getBean(ExampleService.class));
         }
