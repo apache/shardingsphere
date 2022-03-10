1d9668281b313218c30b8a3c18ed82229f492f1e
BashCache <shruthi.harmini2001@gmail.com>
2022-03-10 20:22:34 +0530

    Made changes according to review

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/record/RecordUtilTest.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/record/RecordUtilTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.data.pipeline.core.record;
 
 import org.apache.shardingsphere.data.pipeline.api.ingest.position.PlaceholderPosition;
@@ -6,31 +23,31 @@ import org.apache.shardingsphere.data.pipeline.api.ingest.record.DataRecord;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public final class RecordUtilTest {
+
     @Test
     public void assertExtractPrimaryColumns() {
         DataRecord dataRecord = mockDataRecord("t2");
-        List<Column> actual = RecordUtil.extractPrimaryColumns(dataRecord);
+        Collection<Column> actual = RecordUtil.extractPrimaryColumns(dataRecord);
         assertThat(actual.size(), is(2));
-    }
-
-    @Test
-    public void assertExtractUpdatedColumns() {
-        DataRecord dataRecord = mockDataRecord("t2");
-        List<Column> actual = RecordUtil.extractUpdatedColumns(dataRecord);
-        assertThat(actual.size(), is(3));
     }
 
     @Test
     public void assertExtractConditionalColumns() {
         DataRecord dataRecord = mockDataRecord("t2");
-        List<Column> actual = RecordUtil.extractConditionColumns(dataRecord, Collections.singleton("c1"));
+        Collection<Column> actual = RecordUtil.extractConditionColumns(dataRecord, Collections.singleton("c1"));
+        assertThat(actual.size(), is(3));
+    }
+
+    @Test
+    public void assertExtractUpdatedColumns() {
+        DataRecord dataRecord = mockDataRecord("t2");
+        Collection<Column> actual = RecordUtil.extractUpdatedColumns(dataRecord);
         assertThat(actual.size(), is(3));
     }
 
