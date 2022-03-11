52c8e34216389e2f171441403838beb9740f1192
zhangliang <zhangliang@apache.org>
2022-03-12 01:10:25 +0800

    Refactor ResourceUtil

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/util/ResourceUtil.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/util/ResourceUtil.java
@@ -53,12 +53,11 @@ public final class ResourceUtil {
      */
     public static JobConfiguration createJobConfiguration() {
         JobConfiguration result = new JobConfiguration();
-        WorkflowConfiguration workflowConfig = new WorkflowConfiguration("logic_db", Collections.singletonList(YamlShardingRuleConfiguration.class.getName()), "0");
-        result.setWorkflowConfig(workflowConfig);
+        result.setWorkflowConfig(new WorkflowConfiguration("logic_db", Collections.singletonList(YamlShardingRuleConfiguration.class.getName()), "0"));
         PipelineConfiguration pipelineConfig = new PipelineConfiguration();
-        result.setPipelineConfig(pipelineConfig);
         pipelineConfig.setSource(createYamlPipelineDataSourceConfiguration(new ShardingSpherePipelineDataSourceConfiguration(readFileToString("/config_sharding_sphere_jdbc_source.yaml"))));
         pipelineConfig.setTarget(createYamlPipelineDataSourceConfiguration(new StandardPipelineDataSourceConfiguration(readFileToString("/config_standard_jdbc_target.yaml"))));
+        result.setPipelineConfig(pipelineConfig);
         result.buildHandleConfig();
         return result;
     }
