97a3068de39b6560793bd2d469e19911d3e5e050
BashCache <shruthi.harmini2001@gmail.com>
2022-03-15 19:15:05 +0530

    Added test cases for JobProgreeYamlSwapper

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/job/progress/yaml/JobProgressYamlSwapperTest.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/core/job/progress/yaml/JobProgressYamlSwapperTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;      
+import static org.junit.Assert.assertNull;
 
 
 public final class JobProgressYamlSwapperTest {
@@ -24,30 +24,30 @@ public final class JobProgressYamlSwapperTest {
     @Test
     public void assertFullSwapToYaml() {
         JobProgress jobProgress = getJobProgress(ConfigurationFileUtil.readFile("job-progress.yaml"));
-        YamlJobProgress result = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
-        assertThat(result.getStatus(), is("RUNNING"));
-        assertThat(result.getSourceDatabaseType(), is("H2"));
-        assertThat(result.getInventory().getFinished().length, is(2));
-        assertArrayEquals(result.getInventory().getFinished(), new String[]{"ds0.t_2", "ds0.t_1"});
-        assertThat(result.getInventory().getUnfinished().size(), is(2));
-        assertThat(result.getInventory().getUnfinished().get("ds1.t_2"), is("1,2"));
-        assertThat(result.getInventory().getUnfinished().get("ds1.t_1"), is(""));
-        assertThat(result.getIncremental().size(), is(1));
-        assertTrue(result.getIncremental().containsKey("ds0"));
-        assertNull(result.getIncremental().get("position"));
+        YamlJobProgress actual = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
+        assertThat(actual.getStatus(), is("RUNNING"));
+        assertThat(actual.getSourceDatabaseType(), is("H2"));
+        assertThat(actual.getInventory().getFinished().length, is(2));
+        assertArrayEquals(actual.getInventory().getFinished(), new String[]{"ds0.t_2", "ds0.t_1"});
+        assertThat(actual.getInventory().getUnfinished().size(), is(2));
+        assertThat(actual.getInventory().getUnfinished().get("ds1.t_2"), is("1,2"));
+        assertThat(actual.getInventory().getUnfinished().get("ds1.t_1"), is(""));
+        assertThat(actual.getIncremental().size(), is(1));
+        assertTrue(actual.getIncremental().containsKey("ds0"));
+        assertNull(actual.getIncremental().get("position"));
     }
 
     @Test
     public void assertNullIncremental() {
         JobProgress jobProgress = getJobProgress(ConfigurationFileUtil.readFile("job-progress-no-finished.yaml"));
-        YamlJobProgress result = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
-        assertTrue(result.getIncremental().isEmpty());
+        YamlJobProgress actual = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
+        assertTrue(actual.getIncremental().isEmpty());
     }
 
     @Test
     public void assertNullInventory() {
         JobProgress jobProgress = getJobProgress(ConfigurationFileUtil.readFile("job-progress-no-inventory.yaml"));
-        YamlJobProgress result = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
-        assertThat(result.getInventory().getFinished().length, is(0));
+        YamlJobProgress actual = JOB_PROGRESS_YAML_SWAPPER.swapToYaml(jobProgress);
+        assertThat(actual.getInventory().getFinished().length, is(0));
     }
 }
