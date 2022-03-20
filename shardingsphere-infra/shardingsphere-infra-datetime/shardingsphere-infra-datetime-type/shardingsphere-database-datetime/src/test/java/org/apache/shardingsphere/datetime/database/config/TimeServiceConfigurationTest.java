b4ab4cb6765ea5adcb262ebd6ca8b8f652d826b8
zhangliang <zhangliang@apache.org>
2022-03-20 23:57:16 +0800

    Fix checkstyle

--- shardingsphere-infra/shardingsphere-infra-datetime/shardingsphere-infra-datetime-type/shardingsphere-database-datetime/src/test/java/org/apache/shardingsphere/datetime/database/config/TimeServiceConfigurationTest.java
+++ shardingsphere-infra/shardingsphere-infra-datetime/shardingsphere-infra-datetime-type/shardingsphere-database-datetime/src/test/java/org/apache/shardingsphere/datetime/database/config/TimeServiceConfigurationTest.java
@@ -21,7 +21,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public final class TimeServiceConfigurationTest {
