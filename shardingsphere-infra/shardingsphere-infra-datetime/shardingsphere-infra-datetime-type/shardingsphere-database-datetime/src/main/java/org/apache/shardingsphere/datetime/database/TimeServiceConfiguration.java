f0a5d721ca3e41c085debe308c347a6e4aa3eeeb
zhangliang <zhangliang@apache.org>
2022-03-20 23:24:41 +0800

    Refactor TimeServiceConfiguration

--- shardingsphere-infra/shardingsphere-infra-datetime/shardingsphere-infra-datetime-type/shardingsphere-database-datetime/src/main/java/org/apache/shardingsphere/datetime/database/TimeServiceConfiguration.java
+++ shardingsphere-infra/shardingsphere-infra-datetime/shardingsphere-infra-datetime-type/shardingsphere-database-datetime/src/main/java/org/apache/shardingsphere/datetime/database/TimeServiceConfiguration.java
@@ -55,7 +55,7 @@ public final class TimeServiceConfiguration {
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE)) {
             return new Yaml().loadAs(inputStream, Map.class);
         } catch (final IOException ex) {
-            throw new ShardingSphereException("please check your time-service.properties", ex);
+            throw new ShardingSphereException("please check your " + CONFIG_FILE, ex);
         }
     }
     
