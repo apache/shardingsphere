a52cb1cf103b1c6791102a974abc0369025b61c1
ntsompa <t8180141@aueb.gr>
2022-03-28 21:42:33 +0300

    Fixed errors

--- shardingsphere-mode/shardingsphere-mode-type/shardingsphere-cluster-mode/shardingsphere-cluster-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/coordinator/future/lock/service/GlobalLockNodeTest.java
+++ shardingsphere-mode/shardingsphere-mode-type/shardingsphere-cluster-mode/shardingsphere-cluster-mode-core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/coordinator/future/lock/service/GlobalLockNodeTest.java
@@ -42,10 +42,12 @@ public final class GlobalLockNodeTest {
     }
 
     @Test
-    public void assertGenerateSchemaLockName() {
-        assertThat(GlobalLockNode.generateSchemaLockName("schema", "instanceId"), is("/lock/global/locks/schema-instanceId"));
-
     public void assertGetGlobalAckNodePath() {
         assertThat(GlobalLockNode.getGlobalAckNodePath(), is("/lock/global/ack"));
+    }
+
+    @Test
+    public void assertGenerateSchemaLockName() {
+        assertThat(GlobalLockNode.generateSchemaLockName("schema", "instanceId"), is("/lock/global/locks/schema-instanceId"));
     }
 }
