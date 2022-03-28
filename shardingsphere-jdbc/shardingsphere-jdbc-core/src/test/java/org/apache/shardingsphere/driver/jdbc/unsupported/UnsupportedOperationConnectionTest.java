740ca6f9d026a4b6c9a93058e30985d873082efa
JingShang Lu <jingshanglu1@gmail.com>
2022-03-28 17:44:07 +0800

    fix

--- shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/unsupported/UnsupportedOperationConnectionTest.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-core/src/test/java/org/apache/shardingsphere/driver/jdbc/unsupported/UnsupportedOperationConnectionTest.java
@@ -62,26 +62,6 @@ public final class UnsupportedOperationConnectionTest {
         shardingSphereConnection.nativeSQL("");
     }
     
-//    @Test(expected = SQLException.class)
-//    public void assertSetSavepoint() throws SQLException {
-//        shardingSphereConnection.setSavepoint();
-//    }
-    
-//    @Test(expected = SQLException.class)
-//    public void assertSetSavepointWithName() throws SQLException {
-//        shardingSphereConnection.setSavepoint("");
-//    }
-    
-//    @Test(expected = SQLException.class)
-//    public void assertReleaseSavepoint() throws SQLException {
-//        shardingSphereConnection.releaseSavepoint(null);
-//    }
-    
-//    @Test(expected = SQLException.class)
-//    public void assertRollback() throws SQLException {
-//        shardingSphereConnection.rollback(null);
-//    }
-    
     @Test(expected = SQLFeatureNotSupportedException.class)
     public void assertAbort() throws SQLException {
         shardingSphereConnection.abort(null);
