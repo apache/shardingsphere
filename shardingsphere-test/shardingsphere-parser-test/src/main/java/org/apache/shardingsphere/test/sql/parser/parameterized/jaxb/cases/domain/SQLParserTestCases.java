590904c1af78c1be22c014ec915c9be187608ca4
Trydamere <304080325@qq.com>
2022-04-01 14:09:45 +0800

    support parsing Postgre sql DROP COLLATION METHOD (#16517)

--- shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/SQLParserTestCases.java
+++ shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/SQLParserTestCases.java
@@ -176,6 +176,7 @@ import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.ddl.DropMaterializedViewStatementTestCase;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.ddl.DropCastStatementTestCase;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.ddl.DropAggregateStatementTestCase;
+import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.ddl.DropCollationStatementTestCase;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.distsql.ral.AddShardingHintDatabaseValueStatementTestCase;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.distsql.ral.AddShardingHintTableValueStatementTestCase;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.distsql.ral.AlterInstanceStatementTestCase;
@@ -1215,6 +1216,9 @@ public final class SQLParserTestCases {
     
     @XmlElement(name = "drop-aggregate")
     private final List<DropAggregateStatementTestCase> dropAggregateStatementTestCases = new LinkedList<>();
+
+    @XmlElement(name = "drop-collation")
+    private final List<DropCollationStatementTestCase> dropCollationStatementTestCases = new LinkedList<>();
     
     /**
      * Get all SQL parser test cases.
@@ -1519,6 +1523,7 @@ public final class SQLParserTestCases {
         putAll(dropMaterializedViewStatementTestCases, result);
         putAll(dropCastStatementTestCases, result);
         putAll(dropAggregateStatementTestCases, result);
+        putAll(dropCollationStatementTestCases, result);
         return result;
     }
     // CHECKSTYLE:ON
