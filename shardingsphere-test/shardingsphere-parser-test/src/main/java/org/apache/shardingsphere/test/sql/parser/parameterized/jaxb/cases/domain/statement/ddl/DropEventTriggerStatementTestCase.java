fc2089c69ce095ac450e53d70632e5ef4f65d8ff
Thanoshan MV <48581379+ThanoshanMV@users.noreply.github.com>
2022-04-02 07:14:56 +0530

    small changes to PostgreSQL DROP EVENT TRIGGER statement (#16552)

--- shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/ddl/DropEventTriggerStatementTestCase.java
+++ shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/ddl/DropEventTriggerStatementTestCase.java
@@ -20,7 +20,7 @@ package org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domai
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.SQLParserTestCase;
 
 /**
- * Drop Event Trigger test case.
+ * Drop event trigger statement test case.
  */
 public class DropEventTriggerStatementTestCase extends SQLParserTestCase {
 }
