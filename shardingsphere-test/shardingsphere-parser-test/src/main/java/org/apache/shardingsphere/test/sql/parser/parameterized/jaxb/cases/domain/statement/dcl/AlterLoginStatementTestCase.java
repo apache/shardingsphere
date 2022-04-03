1dc7d3a4af10924f99d62269caae23a816e8873a
Thanoshan MV <48581379+ThanoshanMV@users.noreply.github.com>
2022-04-03 21:40:14 +0530

    Proofread SQLServer ALTER LOGIN Statement (#16575)

--- shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/dcl/AlterLoginStatementTestCase.java
+++ shardingsphere-test/shardingsphere-parser-test/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/jaxb/cases/domain/statement/dcl/AlterLoginStatementTestCase.java
@@ -17,10 +17,18 @@
 
 package org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.dcl;
 
+import lombok.Getter;
+import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.segment.impl.login.ExpectedLogin;
 import org.apache.shardingsphere.test.sql.parser.parameterized.jaxb.cases.domain.statement.SQLParserTestCase;
+
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  * Alter login statement test case.
  */
+@Getter
 public final class AlterLoginStatementTestCase extends SQLParserTestCase {
+    
+    @XmlElement(name = "login")
+    private ExpectedLogin login;
 }
