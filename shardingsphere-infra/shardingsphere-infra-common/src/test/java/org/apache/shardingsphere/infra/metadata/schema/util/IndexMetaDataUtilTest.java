90b4067630cf1a3e9308995401d29cc7a62cdfcc
strongduanmu <duanzhengqiang@apache.org>
2022-04-09 22:07:59 +0800

    fix unit test

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/util/IndexMetaDataUtilTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/schema/util/IndexMetaDataUtilTest.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.infra.metadata.schema.util;
 
 import com.google.common.collect.Lists;
-import lombok.SneakyThrows;
 import org.apache.shardingsphere.infra.metadata.schema.ShardingSphereSchema;
 import org.apache.shardingsphere.infra.metadata.schema.model.IndexMetaData;
 import org.apache.shardingsphere.infra.metadata.schema.model.TableMetaData;
@@ -26,14 +25,7 @@ import org.apache.shardingsphere.sql.parser.sql.common.segment.ddl.index.IndexSe
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.ColumnSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Answers;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -41,19 +33,12 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public final class IndexMetaDataUtilTest {
     
     private static final String TABLE_NAME = "t_order";
     
     private static final String INDEX_NAME = "user_id_idx";
-    
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private DataSource dataSource;
     
     @Test
     public void assertGetLogicIndexNameWithIndexNameSuffix() {
@@ -88,13 +73,9 @@ public final class IndexMetaDataUtilTest {
     }
     
     @Test
-    @SneakyThrows(SQLException.class)
     public void assertGetTableNamesFromMetaData() {
-        Connection connection = mock(Connection.class, RETURNS_DEEP_STUBS);
-        when(dataSource.getConnection()).thenReturn(connection);
-        ShardingSphereSchema schema = buildSchema();
-        IndexSegment indexSegment = new IndexSegment(0, 0, new IdentifierValue(TABLE_NAME));
-        assertThat(IndexMetaDataUtil.getTableNamesFromMetaData(schema, Lists.newArrayList(indexSegment)), is(Collections.singletonList(TABLE_NAME)));
+        IndexSegment indexSegment = new IndexSegment(0, 0, new IdentifierValue(INDEX_NAME));
+        assertThat(IndexMetaDataUtil.getTableNamesFromMetaData(buildSchema(), Lists.newArrayList(indexSegment)), is(Collections.singletonList(TABLE_NAME)));
     }
     
     private ShardingSphereSchema buildSchema() {
