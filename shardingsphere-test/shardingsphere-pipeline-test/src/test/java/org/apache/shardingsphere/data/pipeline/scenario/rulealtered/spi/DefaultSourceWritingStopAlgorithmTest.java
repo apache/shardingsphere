33084f97a10c593eec72df60b104ee26cd98d379
tianhao960 <tianhao960@163.com>
2022-04-12 20:52:28 +0800

    add unit test for DefaultSourceWritingStopAlgorithm

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/scenario/rulealtered/spi/DefaultSourceWritingStopAlgorithmTest.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/data/pipeline/scenario/rulealtered/spi/DefaultSourceWritingStopAlgorithmTest.java
@@ -1,14 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.data.pipeline.scenario.rulealtered.spi;
 
 import org.apache.shardingsphere.data.pipeline.core.util.PipelineContextUtil;
 import org.junit.Before;
 import org.junit.Test;
 
-public class DefaultSourceWritingStopAlgorithmTest {
+public final class DefaultSourceWritingStopAlgorithmTest {
 
-    private final  DefaultSourceWritingStopAlgorithm defaultSourceWritingStopAlgorithm = new DefaultSourceWritingStopAlgorithm();
+    private final DefaultSourceWritingStopAlgorithm defaultSourceWritingStopAlgorithm = new DefaultSourceWritingStopAlgorithm();
 
     private final String lockName = "lock1";
+
     private final String jobId = "jobId1";
 
     @Before
@@ -18,29 +36,26 @@ public class DefaultSourceWritingStopAlgorithmTest {
 
     @Test
     public void assertSuccessLockReleaseLock() {
-        defaultSourceWritingStopAlgorithm.lock(lockName,jobId);
-        defaultSourceWritingStopAlgorithm.releaseLock(lockName,jobId);
+        defaultSourceWritingStopAlgorithm.lock(lockName, jobId);
+        defaultSourceWritingStopAlgorithm.releaseLock(lockName, jobId);
     }
-
 
     @Test
     public void assertSuccessLockTwiceReleaseLock() {
-        defaultSourceWritingStopAlgorithm.lock(lockName,jobId);
-        defaultSourceWritingStopAlgorithm.lock(lockName,jobId);
-        defaultSourceWritingStopAlgorithm.releaseLock(lockName,jobId);
+        defaultSourceWritingStopAlgorithm.lock(lockName, jobId);
+        defaultSourceWritingStopAlgorithm.lock(lockName, jobId);
+        defaultSourceWritingStopAlgorithm.releaseLock(lockName, jobId);
     }
-
 
     @Test
     public void assertSuccessLockReleaseLockTwice() {
-        defaultSourceWritingStopAlgorithm.lock(lockName,jobId);
-        defaultSourceWritingStopAlgorithm.releaseLock(lockName,jobId);
-        defaultSourceWritingStopAlgorithm.releaseLock(lockName,jobId);
+        defaultSourceWritingStopAlgorithm.lock(lockName, jobId);
+        defaultSourceWritingStopAlgorithm.releaseLock(lockName, jobId);
+        defaultSourceWritingStopAlgorithm.releaseLock(lockName, jobId);
     }
 
     @Test
     public void assertSuccessReleaseNullLock() {
-        defaultSourceWritingStopAlgorithm.releaseLock(lockName,jobId);
+        defaultSourceWritingStopAlgorithm.releaseLock(lockName, jobId);
     }
-
 }
