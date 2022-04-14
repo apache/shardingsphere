264842fd31c62bace1fc036f171fca77c8a463f7
zhangliang <zhangliang@apache.org>
2022-04-14 15:27:00 +0800

    Revise ShardingRuleAlteredDetectorTest

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/sharding/ShardingRuleAlteredDetectorTest.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/sharding/ShardingRuleAlteredDetectorTest.java
@@ -17,17 +17,13 @@
 
 package org.apache.shardingsphere.sharding;
 
-import org.apache.shardingsphere.infra.config.rulealtered.OnRuleAlteredActionConfiguration;
 import org.apache.shardingsphere.infra.yaml.config.pojo.YamlRuleConfiguration;
 import org.apache.shardingsphere.infra.yaml.engine.YamlEngine;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
 import org.apache.shardingsphere.sharding.schedule.ShardingRuleAlteredDetector;
 import org.apache.shardingsphere.sharding.yaml.config.YamlShardingRuleConfiguration;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,47 +31,42 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
 public final class ShardingRuleAlteredDetectorTest {
     
     @Test
     public void assertGetOnRuleAlteredActionConfigSuccess() {
-        ShardingRuleAlteredDetector shardingRuleAlteredDetector = new ShardingRuleAlteredDetector();
-        ShardingRuleConfiguration result = new ShardingRuleConfiguration();
-        Optional<OnRuleAlteredActionConfiguration> config = shardingRuleAlteredDetector.getOnRuleAlteredActionConfig(result);
-        Assert.assertFalse(config.isPresent());
+        assertFalse(new ShardingRuleAlteredDetector().getOnRuleAlteredActionConfig(new ShardingRuleConfiguration()).isPresent());
     }
     
     @Test
     public void assertFindRuleAlteredLogicTablesSucceed() throws IOException {
         URL sourceUrl = getClass().getClassLoader().getResource("scaling/detector/source_rule_config.yaml");
-        Assert.assertNotNull(sourceUrl);
+        assertNotNull(sourceUrl);
         YamlRuleConfiguration sourceRuleConfig = YamlEngine.unmarshal(new File(sourceUrl.getFile()), YamlShardingRuleConfiguration.class);
         URL targetUrl = getClass().getClassLoader().getResource("scaling/detector/target_rule_config.yaml");
-        Assert.assertNotNull(targetUrl);
+        assertNotNull(targetUrl);
         YamlRuleConfiguration targetRuleConfig = YamlEngine.unmarshal(new File(targetUrl.getFile()), YamlShardingRuleConfiguration.class);
-        Map<String, Map<String, Object>> sameDatasource = new HashMap<>(5, 1);
+        Map<String, Map<String, Object>> sameDataSources = new HashMap<>(5, 1);
         for (int i = 0; i < 5; i++) {
-            Map<String, Object> prop = new HashMap<>(2, 1);
-            prop.put("dataSourceClassName", "org.apache.shardingsphere.test.mock.MockedDataSource");
-            prop.put("jdbcUrl", "jdbc:h2:mem:test_ds_" + i + ";DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;MODE=MySQL");
-            sameDatasource.put("ds_" + i, prop);
+            Map<String, Object> props = new HashMap<>(2, 1);
+            props.put("dataSourceClassName", "org.apache.shardingsphere.test.mock.MockedDataSource");
+            props.put("jdbcUrl", "jdbc:h2:mem:test_ds_" + i + ";DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;MODE=MySQL");
+            sameDataSources.put("ds_" + i, props);
         }
-        ShardingRuleAlteredDetector detector = new ShardingRuleAlteredDetector();
-        List<String> ruleAlteredLogicTables = detector.findRuleAlteredLogicTables(sourceRuleConfig, targetRuleConfig, sameDatasource, sameDatasource);
+        List<String> ruleAlteredLogicTables = new ShardingRuleAlteredDetector().findRuleAlteredLogicTables(sourceRuleConfig, targetRuleConfig, sameDataSources, sameDataSources);
         assertThat(ruleAlteredLogicTables.get(0), Matchers.is("t_order"));
     }
     
     @Test
     public void assertNoFindRuleAlteredLogicTables() throws IOException {
         URL sourceUrl = getClass().getClassLoader().getResource("scaling/detector/source_rule_config.yaml");
-        Assert.assertNotNull(sourceUrl);
+        assertNotNull(sourceUrl);
         YamlRuleConfiguration sourceRuleConfig = YamlEngine.unmarshal(new File(sourceUrl.getFile()), YamlShardingRuleConfiguration.class);
-        // target = source
         List<String> ruleAlteredLogicTables = new ShardingRuleAlteredDetector().findRuleAlteredLogicTables(sourceRuleConfig, sourceRuleConfig, null, null);
         assertThat("not table rule alter", ruleAlteredLogicTables.size(), Matchers.is(0));
     }
@@ -83,7 +74,7 @@ public final class ShardingRuleAlteredDetectorTest {
     @Test
     public void assertExtractAllLogicTables() throws IOException {
         URL sourceUrl = getClass().getClassLoader().getResource("scaling/detector/source_rule_config.yaml");
-        Assert.assertNotNull(sourceUrl);
+        assertNotNull(sourceUrl);
         YamlRuleConfiguration sourceRuleConfig = YamlEngine.unmarshal(new File(sourceUrl.getFile()), YamlShardingRuleConfiguration.class);
         List<String> ruleAlteredLogicTables = new ShardingRuleAlteredDetector().findRuleAlteredLogicTables(sourceRuleConfig, null, null, null);
         assertThat(ruleAlteredLogicTables.get(0), Matchers.is("t_order"));
