71c32362156ac1f8242d275bd91e9dfbd13656f6
cherubimLV <1178130627@qq.com>
2022-05-06 20:49:43 +0800

    Update WeightReplicaLoadBalanceAlgorithm.java
    Change static field WEIGHT_MAP to class level

--- shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-core/src/main/java/org/apache/shardingsphere/readwritesplitting/algorithm/WeightReplicaLoadBalanceAlgorithm.java
+++ shardingsphere-features/shardingsphere-readwrite-splitting/shardingsphere-readwrite-splitting-core/src/main/java/org/apache/shardingsphere/readwritesplitting/algorithm/WeightReplicaLoadBalanceAlgorithm.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.readwritesplitting.algorithm;
 
 import lombok.Getter;
@@ -16,7 +33,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Getter
 @Setter
 public class WeightReplicaLoadBalanceAlgorithm implements ReplicaLoadBalanceAlgorithm {
-    private static final ConcurrentHashMap<String, Double[]> WEIGHT_MAP = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Double[]> weightMap = new ConcurrentHashMap<>();
 
     private Properties props = new Properties();
 
@@ -37,8 +54,8 @@ public class WeightReplicaLoadBalanceAlgorithm implements ReplicaLoadBalanceAlgo
 
     @Override
     public String getDataSource(final String name, final String writeDataSourceName, final List<String> readDataSourceNames) {
-        Double[] weight = WEIGHT_MAP.containsKey(name) ? WEIGHT_MAP.get(name) : initWeight(readDataSourceNames);
-        WEIGHT_MAP.putIfAbsent(name, weight);
+        Double[] weight = weightMap.containsKey(name) ? weightMap.get(name) : initWeight(readDataSourceNames);
+        weightMap.putIfAbsent(name, weight);
         String dataSourceName = getDataSourceName(readDataSourceNames, weight);
         return dataSourceName;
     }
