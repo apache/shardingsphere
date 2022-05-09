1f9eee80c18d7e98cbf90badb33bdb136c2dc134
Liang Zhang <zhangliang@apache.org>
2022-05-09 16:21:07 +0800

    Refactor AuthorityProviderAlgorithm (#17476)

--- shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/main/java/org/apache/shardingsphere/authority/provider/schema/SchemaPrivilegesPermittedAuthorityProviderAlgorithm.java
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/main/java/org/apache/shardingsphere/authority/provider/schema/SchemaPrivilegesPermittedAuthorityProviderAlgorithm.java
@@ -18,18 +18,16 @@
 package org.apache.shardingsphere.authority.provider.schema;
 
 import lombok.Getter;
-import org.apache.shardingsphere.authority.model.ShardingSpherePrivileges;
+import org.apache.shardingsphere.authority.model.AuthorityRegistry;
 import org.apache.shardingsphere.authority.provider.schema.builder.SchemaPrivilegeBuilder;
+import org.apache.shardingsphere.authority.registry.UserPrivilegeMapAuthorityRegistry;
 import org.apache.shardingsphere.authority.spi.AuthorityProviderAlgorithm;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
-import org.apache.shardingsphere.infra.metadata.user.Grantee;
 import org.apache.shardingsphere.infra.metadata.user.ShardingSphereUser;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Schema privileges permitted authority provider algorithm.
@@ -41,26 +39,14 @@ public final class SchemaPrivilegesPermittedAuthorityProviderAlgorithm implement
     @Getter
     private Properties props;
     
-    private final Map<ShardingSphereUser, ShardingSpherePrivileges> userPrivilegeMap = new ConcurrentHashMap<>();
-    
     @Override
     public void init(final Properties props) {
         this.props = props;
     }
     
     @Override
-    public void init(final Map<String, ShardingSphereMetaData> metaDataMap, final Collection<ShardingSphereUser> users) {
-        userPrivilegeMap.putAll(SchemaPrivilegeBuilder.build(users, props));
-    }
-    
-    @Override
-    public void refresh(final Map<String, ShardingSphereMetaData> metaDataMap, final Collection<ShardingSphereUser> users) {
-        userPrivilegeMap.putAll(SchemaPrivilegeBuilder.build(users, props));
-    }
-    
-    @Override
-    public Optional<ShardingSpherePrivileges> findPrivileges(final Grantee grantee) {
-        return userPrivilegeMap.keySet().stream().filter(each -> each.getGrantee().equals(grantee)).findFirst().map(userPrivilegeMap::get);
+    public AuthorityRegistry buildAuthorityRegistry(final Map<String, ShardingSphereMetaData> metaDataMap, final Collection<ShardingSphereUser> users) {
+        return new UserPrivilegeMapAuthorityRegistry(SchemaPrivilegeBuilder.build(users, props));
     }
     
     @Override
--- shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/provider/natived/NativeAuthorityProviderAlgorithmTest.java
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-core/src/test/java/org/apache/shardingsphere/authority/provider/natived/NativeAuthorityProviderAlgorithmTest.java
@@ -17,6 +17,7 @@
 
 package org.apache.shardingsphere.authority.provider.natived;
 
+import org.apache.shardingsphere.authority.model.AuthorityRegistry;
 import org.apache.shardingsphere.authority.model.PrivilegeType;
 import org.apache.shardingsphere.authority.model.ShardingSpherePrivileges;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
@@ -47,24 +48,10 @@ public final class NativeAuthorityProviderAlgorithmTest {
         ShardingSphereUser root = new ShardingSphereUser("root", "", "localhost");
         users.add(root);
         ShardingSphereMetaData metaData = mockShardingSphereMetaData(users);
-        algorithm.init(Collections.singletonMap("db0", metaData), users);
-        Optional<ShardingSpherePrivileges> privileges = algorithm.findPrivileges(new Grantee("root", "localhost"));
+        AuthorityRegistry authorityRegistry = algorithm.buildAuthorityRegistry(Collections.singletonMap("db0", metaData), users);
+        Optional<ShardingSpherePrivileges> privileges = authorityRegistry.findPrivileges(new Grantee("root", "localhost"));
         assertTrue(privileges.isPresent());
         assertPrivilege(privileges.get());
-    }
-    
-    @Test
-    public void assertRefreshPrivileges() throws SQLException {
-        NativeAuthorityProviderAlgorithm algorithm = new NativeAuthorityProviderAlgorithm();
-        Collection<ShardingSphereUser> users = Collections.singletonList(new ShardingSphereUser("root", "", "localhost"));
-        algorithm.init(Collections.emptyMap(), users);
-        Optional<ShardingSpherePrivileges> privileges1 = algorithm.findPrivileges(new Grantee("root", "localhost"));
-        assertTrue(privileges1.isPresent());
-        assertTrue(privileges1.get().hasPrivileges(Collections.singletonList(PrivilegeType.SUPER)));
-        algorithm.refresh(Collections.singletonMap("db0", mockShardingSphereMetaData(users)), users);
-        Optional<ShardingSpherePrivileges> privileges2 = algorithm.findPrivileges(new Grantee("root", "localhost"));
-        assertTrue(privileges2.isPresent());
-        assertPrivilege(privileges2.get());
     }
     
     private void assertPrivilege(final ShardingSpherePrivileges privileges) {
