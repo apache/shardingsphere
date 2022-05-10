7c447ae5560f322806b40ae6a76cfc17fc551e17
cha-cha369 <1301826221@qq.com>
2022-05-10 15:39:29 +0800

    Support parsing ALTER OUTLINE in Oracle

--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/oracle/ddl/OracleAlterOutlineStatement.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/oracle/ddl/OracleAlterOutlineStatement.java
@@ -18,12 +18,12 @@
 package org.apache.shardingsphere.sql.parser.sql.dialect.statement.oracle.ddl;
 
 import lombok.ToString;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.ddl.AlterDatabaseLinkStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.ddl.AlterOutlineStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.oracle.OracleStatement;
 
 /**
  * Oracle alter outline statement.
  */
 @ToString
-public final class OracleAlterOutlineStatement extends AlterDatabaseLinkStatement implements OracleStatement {
+public final class OracleAlterOutlineStatement extends AlterOutlineStatement implements OracleStatement {
 }
