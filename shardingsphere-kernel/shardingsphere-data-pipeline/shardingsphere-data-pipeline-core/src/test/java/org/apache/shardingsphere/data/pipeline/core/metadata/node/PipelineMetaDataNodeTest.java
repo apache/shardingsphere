0e45e369552f47066e6a7ae1f016bec549cba7e9
azexcy <13588031592@qq.com>
2022-05-16 12:50:28 +0800

    Fix codestyle

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/test/java/org/apache/shardingsphere/data/pipeline/core/metadata/node/PipelineMetaDataNodeTest.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/test/java/org/apache/shardingsphere/data/pipeline/core/metadata/node/PipelineMetaDataNodeTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class PipelineMetaDataNodeTest {
+public final class PipelineMetaDataNodeTest {
     
     @Test
     public void assertGetJobConfigPath() {
