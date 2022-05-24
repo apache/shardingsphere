265e0bd6d53237edcfca7515b7ba029c26d8e934
Panagiotis Parthenis <pparthenhs@gmail.com>
2022-05-24 13:42:57 +0300

    add unit test for ShardingSphereRuleMetaData

---
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/rule/ShardingSphereRuleConfiguration.java
@@ -0,0 +1,7 @@
+package org.apache.shardingsphere.infra.metadata.rule;
+
+import org.apache.shardingsphere.infra.config.scope.SchemaRuleConfiguration;
+
+public class ShardingSphereRuleConfiguration implements SchemaRuleConfiguration {
+
+}
