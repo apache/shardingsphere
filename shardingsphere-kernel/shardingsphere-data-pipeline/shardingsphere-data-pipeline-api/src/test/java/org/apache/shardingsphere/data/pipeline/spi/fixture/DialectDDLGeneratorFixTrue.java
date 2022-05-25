e6524e982288795338cbfd2551f85aa0f4f8b66e
windwheel <yuluoxinsheng@gmail.com>
2022-05-25 19:33:03 +0800

    [issue-17552](https://github.com/apache/shardingsphere/issues/17552) fix bugs

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-api/src/test/java/org/apache/shardingsphere/data/pipeline/spi/fixture/DialectDDLGeneratorFixTrue.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-api/src/test/java/org/apache/shardingsphere/data/pipeline/spi/fixture/DialectDDLGeneratorFixTrue.java
@@ -1,26 +1,22 @@
 package org.apache.shardingsphere.data.pipeline.spi.fixture;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.sql.DataSource;
 import org.apache.shardingsphere.data.pipeline.spi.ddlgenerator.DialectDDLGenerator;
-import org.apache.shardingsphere.infra.exception.ShardingSphereException;
 
 public class DialectDDLGeneratorFixTrue implements DialectDDLGenerator {
     private static final String SHOW_CREATE_SQL = "SHOW CREATE TABLE %s";
 
-    private static final String COLUMN_LABEL = "create table";
-
     @Override
     public String generateDDLSQL(final String tableName, final String schemaName, final DataSource dataSource) throws SQLException {
-        try (
-                Statement statement = dataSource.getConnection().createStatement();
-                ResultSet resultSet = statement.executeQuery(String.format(SHOW_CREATE_SQL, tableName))) {
-            if (resultSet.next()) {
-                return resultSet.getString(COLUMN_LABEL);
-            }
-        }
-        throw new ShardingSphereException("Failed to get ddl sql for table %s", tableName);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format(SHOW_CREATE_SQL, tableName));
+        return sb.toString();
+    }
+
+    @Override
+    public String getType() {
+        return "MySQL";
     }
 }
