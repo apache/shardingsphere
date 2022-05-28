a7d5dc6152207564ca88f7e2376f28da94490d9f
windWheel <1817802738@qq.com>
2022-05-28 16:06:03 +0800

    add unit tests for DialectDDLGeneratorFactoryTest (#17910)
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    add unit tests for DialectDDLGeneratorFactoryTest
    
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    fix checkstyle
    
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    remove whitespace
    
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    refactor DialectDDLGeneratorFixTrue
    
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    export SPI
    
    * [issue-17586](https://github.com/apache/shardingsphere/issues/17586)
    fix checkstyle
    
    * [issue-17552](https://github.com/apache/shardingsphere/issues/17552)
    fix bugs
    
    * [issue-17552](https://github.com/apache/shardingsphere/issues/17555)
    remove fields
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    remove fields
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    add license and remove whitespace
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    add license
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    fix ci
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    fix ci
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    fix ci
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    repair review comment
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    repair review comment
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    repair review comment
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    fix checkstyle
    
    * [issue-17555](https://github.com/apache/shardingsphere/issues/17555)
    add maven scope

---
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-api/src/test/java/org/apache/shardingsphere/data/pipeline/spi/fixture/DialectDDLGeneratorFixture.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.data.pipeline.spi.fixture;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.apache.shardingsphere.data.pipeline.spi.ddlgenerator.DialectDDLGenerator;
+
+public final class DialectDDLGeneratorFixture implements DialectDDLGenerator {
+    
+    private static final String SHOW_CREATE_SQL = "SHOW CREATE TABLE %s";
+    
+    @Override
+    public String generateDDLSQL(final String tableName, final String schemaName, final DataSource dataSource) throws SQLException {
+        return String.format(SHOW_CREATE_SQL, tableName);
+    }
+    
+    @Override
+    public String getType() {
+        return "FIXTURE";
+    }
+}
