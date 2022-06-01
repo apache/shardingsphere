5b4010f1b1fc88dc5a33de3d55e53d153b3b76dc
Haoran Meng <menghaoranss@gmail.com>
2022-06-01 11:07:11 +0800

    refactor event for meta data refresher (#18110)

--- shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/database/schema/event/DropIndexEvent.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/main/java/org/apache/shardingsphere/infra/metadata/database/schema/event/DropIndexEvent.java
@@ -18,27 +18,15 @@
 package org.apache.shardingsphere.infra.metadata.database.schema.event;
 
 import lombok.Getter;
-import org.apache.shardingsphere.infra.metadata.database.schema.decorator.model.ShardingSphereTable;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * Schema altered event.
+ * Drop index event.
  */
 @Getter
-public final class SchemaAlteredEvent implements MetaDataRefreshedEvent {
+public final class DropIndexEvent implements MetaDataRefreshedEvent {
     
-    private final String databaseName;
-    
-    private final String schemaName;
-    
-    private final Collection<ShardingSphereTable> alteredTables = new ArrayList<>();
-    
-    private final Collection<String> droppedTables = new ArrayList<>();
-    
-    public SchemaAlteredEvent(final String databaseName, final String schemaName) {
-        this.databaseName = databaseName;
-        this.schemaName = schemaName;
-    }
+    private List<SchemaAlteredEvent> schemaAlteredEvents = new LinkedList<>();
 }
