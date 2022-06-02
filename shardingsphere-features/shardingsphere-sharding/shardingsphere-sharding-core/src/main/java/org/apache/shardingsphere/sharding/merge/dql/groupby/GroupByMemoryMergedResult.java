1beefb04e3e500c25486fd31cf96eb5a75f4270a
Zhoutzzz <925920668@qq.com>
2022-06-02 11:13:57 +0800

    checkstyle

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/groupby/GroupByMemoryMergedResult.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/groupby/GroupByMemoryMergedResult.java
@@ -39,7 +39,6 @@ import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.Sim
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
