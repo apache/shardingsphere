d5e8a37dd01e0d90f5e850fbf1dced35600cb5ad
windwheel <yuluoxinsheng@gmail.com>
2022-06-03 13:10:21 +0800

    fix checkstyle

--- shardingsphere-infra/shardingsphere-infra-federation/shardingsphere-infra-federation-optimizer/src/test/java/org/apache/shardingsphere/infra/federation/optimizer/context/parser/fixture/OptimizerSQLDialectBuilderFixture.java
+++ shardingsphere-infra/shardingsphere-infra-federation/shardingsphere-infra-federation-optimizer/src/test/java/org/apache/shardingsphere/infra/federation/optimizer/context/parser/fixture/OptimizerSQLDialectBuilderFixture.java
@@ -1,24 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.infra.federation.optimizer.context.parser.fixture;
 
 import java.util.Properties;
-import org.apache.shardingsphere.infra.database.type.DatabaseType;
 import org.apache.shardingsphere.infra.federation.optimizer.context.parser.dialect.OptimizerSQLDialectBuilder;
 
-public class OptimizerSQLDialectBuilderFixture implements OptimizerSQLDialectBuilder {
-
-    private final DatabaseType databaseType;
-
-    public OptimizerSQLDialectBuilderFixture(final DatabaseType databaseType) {
-        this.databaseType = databaseType;
-    }
-
+public final class OptimizerSQLDialectBuilderFixture implements OptimizerSQLDialectBuilder {
+    
     @Override
     public Properties build() {
         return new Properties();
     }
-
+    
     @Override
     public String getType() {
-        return databaseType.getType();
+        return "FIXTURE";
     }
 }
