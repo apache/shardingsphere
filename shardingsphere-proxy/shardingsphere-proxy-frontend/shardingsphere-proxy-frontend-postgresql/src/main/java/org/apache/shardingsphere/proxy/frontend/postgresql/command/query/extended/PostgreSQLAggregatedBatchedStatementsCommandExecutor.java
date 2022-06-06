9e297ca1b3e5b2eeb0e4b32b684a6e7016d1f7f8
JingShang Lu <jingshanglu1@gmail.com>
2022-06-06 08:59:06 +0800

    fix

--- shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/main/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/extended/PostgreSQLAggregatedBatchedStatementsCommandExecutor.java
+++ shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-postgresql/src/main/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/extended/PostgreSQLAggregatedBatchedStatementsCommandExecutor.java
@@ -51,7 +51,6 @@ public final class PostgreSQLAggregatedBatchedStatementsCommandExecutor implemen
     
     @Override
     public Collection<DatabasePacket<?>> execute() throws SQLException {
-        connectionSession.getBackendConnection().handleAutoCommit();
         PostgreSQLPreparedStatement preparedStatement = getPreparedStatement();
         PostgreSQLBatchedStatementsExecutor executor = new PostgreSQLBatchedStatementsExecutor(connectionSession, preparedStatement, readParameterSets(preparedStatement.getParameterTypes()));
         List<DatabasePacket<?>> result = new ArrayList<>(packets.size());
