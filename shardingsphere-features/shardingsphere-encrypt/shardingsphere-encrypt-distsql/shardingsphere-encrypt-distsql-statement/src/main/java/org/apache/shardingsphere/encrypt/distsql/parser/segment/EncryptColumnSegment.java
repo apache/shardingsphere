333eb0bac7a4188fda961c35824f23ccf6ca6f0c
cheese8 <yinwerhong@163.com>
2022-06-09 13:20:38 +0800

    Update EncryptColumnSegment.java

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-distsql/shardingsphere-encrypt-distsql-statement/src/main/java/org/apache/shardingsphere/encrypt/distsql/parser/segment/EncryptColumnSegment.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-distsql/shardingsphere-encrypt-distsql-statement/src/main/java/org/apache/shardingsphere/encrypt/distsql/parser/segment/EncryptColumnSegment.java
@@ -56,6 +56,11 @@ public final class EncryptColumnSegment implements ASTNode {
         this(name, cipherColumn, plainColumn, assistedQueryColumn, encryptor, null);
     }
     
+    public EncryptColumnSegment(final String name, final String cipherColumn, final String plainColumn, final String assistedQueryColumn, final String dataType, final String cipherDataType, 
+                                final String plainDataType, final String assistedQueryDataType, final AlgorithmSegment encryptor) {
+        this(name, cipherColumn, plainColumn, assistedQueryColumn, dataType, cipherDataType, plainDataType, assistedQueryDataType, encryptor, null);
+    }
+    
     /**
      * Is the data type correct.
      *
