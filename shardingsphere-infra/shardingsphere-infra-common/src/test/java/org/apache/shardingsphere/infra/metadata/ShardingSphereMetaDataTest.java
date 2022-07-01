8c450ea620a2f9b2dc59e04ec20380f0f9fd3da4
Ziyuan Han <hanziyuan08@outlook.com>
2022-07-02 00:37:19 +0800

    Add test case for ShardingSphereMetaData.dropDatabase

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/ShardingSphereMetaDataTest.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/ShardingSphereMetaDataTest.java
@@ -17,18 +17,69 @@
 
 package org.apache.shardingsphere.infra.metadata;
 
+import org.apache.shardingsphere.infra.config.props.ConfigurationProperties;
+import org.apache.shardingsphere.infra.database.DefaultDatabase;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
+import org.apache.shardingsphere.infra.metadata.database.resource.ShardingSphereResource;
+import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
+import org.apache.shardingsphere.infra.rule.identifier.type.ResourceHeldRule;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public final class ShardingSphereMetaDataTest {
-
+    @Mock
+    private ShardingSphereRuleMetaData globalRuleMetaData;
+    
+    @Mock
+    private ResourceHeldRule<?> shardingRuleMetaDataResourceHeldRule;
+    
+    @Mock
+    private ResourceHeldRule<?> globalRuleMedataResourceHeldRule;
+    
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private ShardingSphereDatabase shardingSphereDatabase;
+    
+    @Mock
+    private ShardingSphereResource shardingSphereResource;
+    
+    @Mock
+    private DataSource mockedDataSource;
+    
+    private ShardingSphereMetaData shardingSphereMetaData;
+    
+    @Before
+    public void setUp() throws SQLException {
+        when(globalRuleMetaData.findRules(ResourceHeldRule.class)).thenReturn(Collections.singletonList(globalRuleMedataResourceHeldRule));
+        when(shardingSphereDatabase.getName()).thenReturn(DefaultDatabase.LOGIC_NAME);
+        when(shardingSphereDatabase.getResource()).thenReturn(shardingSphereResource);
+        when(shardingSphereDatabase.getResource().getDataSources()).thenReturn(Collections.singletonMap(DefaultDatabase.LOGIC_NAME, mockedDataSource));
+        when(shardingSphereDatabase.getRuleMetaData().findRules(ResourceHeldRule.class)).thenReturn(Collections.singletonList(shardingRuleMetaDataResourceHeldRule));
+        shardingSphereMetaData = new ShardingSphereMetaData(new LinkedHashMap<>(), globalRuleMetaData, new ConfigurationProperties(new Properties()));
+    }
+    
     @Test
     public void assertDropDatabase() {
-        ShardingSphereMetaData shardingSphereMetaData = mock(ShardingSphereMetaData.class, RETURNS_DEEP_STUBS);
-        shardingSphereMetaData.dropDatabase("foo_db");
-        verify(shardingSphereMetaData.getDatabases().remove("foo_db"));
+        shardingSphereMetaData.getDatabases().put(DefaultDatabase.LOGIC_NAME, shardingSphereDatabase);
+        shardingSphereMetaData.dropDatabase(DefaultDatabase.LOGIC_NAME);
+        Assert.assertThat(shardingSphereMetaData.getDatabases(), is(Collections.emptyMap()));
+        verify(shardingSphereResource).close(mockedDataSource);
+        verify(shardingRuleMetaDataResourceHeldRule).closeStaleResource(DefaultDatabase.LOGIC_NAME);
+        verify(globalRuleMedataResourceHeldRule).closeStaleResource(DefaultDatabase.LOGIC_NAME);
     }
 }
