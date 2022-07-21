f928004d3827154f51aad78aa8021981453f068f
吴伟杰 <wuweijie@apache.org>
2022-07-21 10:17:35 +0800

    Make show tables from schema work in Proxy

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowTablesExecutor.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/text/admin/mysql/executor/ShowTablesExecutor.java
@@ -63,8 +63,17 @@ public final class ShowTablesExecutor implements DatabaseAdminQueryExecutor {
     
     @Override
     public void execute(final ConnectionSession connectionSession) {
-        queryResultMetaData = createQueryResultMetaData(connectionSession.getDatabaseName());
-        mergedResult = new TransparentMergedResult(getQueryResult(connectionSession.getDatabaseName()));
+        String databaseName = showTablesStatement.getFromSchema().map(schema -> schema.getSchema().getIdentifier().getValue()).orElseGet(connectionSession::getDatabaseName);
+        queryResultMetaData = createQueryResultMetaData(databaseName);
+        mergedResult = new TransparentMergedResult(getQueryResult(databaseName));
+    }
+    
+    private QueryResultMetaData createQueryResultMetaData(final String databaseName) {
+        List<RawQueryResultColumnMetaData> columnNames = new LinkedList<>();
+        String tableColumnName = String.format("Tables_in_%s", databaseName);
+        columnNames.add(new RawQueryResultColumnMetaData("", tableColumnName, tableColumnName, Types.VARCHAR, "VARCHAR", 255, 0));
+        columnNames.add(new RawQueryResultColumnMetaData("", "Table_type", "Table_type", Types.VARCHAR, "VARCHAR", 20, 0));
+        return new RawQueryResultMetaData(columnNames);
     }
     
     private QueryResult getQueryResult(final String databaseName) {
@@ -88,13 +97,5 @@ public final class ShowTablesExecutor implements DatabaseAdminQueryExecutor {
             return pattern.isPresent() ? result.stream().filter(each -> RegularUtil.matchesCaseInsensitive(pattern.get(), each)).collect(Collectors.toList()) : result;
         }
         return result;
-    }
-    
-    private QueryResultMetaData createQueryResultMetaData(final String databaseName) {
-        List<RawQueryResultColumnMetaData> columnNames = new LinkedList<>();
-        String tableColumnName = String.format("Tables_in_%s", databaseName);
-        columnNames.add(new RawQueryResultColumnMetaData("", tableColumnName, tableColumnName, Types.VARCHAR, "VARCHAR", 255, 0));
-        columnNames.add(new RawQueryResultColumnMetaData("", "Table_type", "Table_type", Types.VARCHAR, "VARCHAR", 20, 0));
-        return new RawQueryResultMetaData(columnNames);
     }
 }
