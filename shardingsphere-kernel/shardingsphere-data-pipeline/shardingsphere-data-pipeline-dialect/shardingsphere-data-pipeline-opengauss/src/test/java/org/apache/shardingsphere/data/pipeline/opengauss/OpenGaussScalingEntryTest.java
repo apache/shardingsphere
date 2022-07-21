f6cd380490bdcaa97dd048174a9092042507cb08
azexcy <101622833+azexcy@users.noreply.github.com>
2022-07-21 15:15:34 +0800

    Remove EnvironmentChecker of scaling (#19415)

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-opengauss/src/test/java/org/apache/shardingsphere/data/pipeline/opengauss/OpenGaussScalingEntryTest.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-opengauss/src/test/java/org/apache/shardingsphere/data/pipeline/opengauss/OpenGaussScalingEntryTest.java
@@ -34,7 +34,6 @@ public final class OpenGaussScalingEntryTest {
     public void assertGetScalingEntryByDatabaseType() {
         ScalingEntry actual = ScalingEntryFactory.getInstance("openGauss");
         assertThat(actual, instanceOf(OpenGaussScalingEntry.class));
-        assertThat(actual.getEnvironmentCheckerClass(), equalTo(OpenGaussEnvironmentChecker.class));
         assertThat(actual.getImporterClass(), equalTo(OpenGaussImporter.class));
         assertThat(actual.getInventoryDumperClass(), equalTo(PostgreSQLInventoryDumper.class));
         assertThat(actual.getIncrementalDumperClass(), equalTo(OpenGaussWalDumper.class));
