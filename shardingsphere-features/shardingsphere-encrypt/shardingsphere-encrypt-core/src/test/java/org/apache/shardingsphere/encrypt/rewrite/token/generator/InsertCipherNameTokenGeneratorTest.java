b8eaade664ba3880d3af76e5c2332c5435dfb1a6
Sushant Srivastava <sushant.srivastav@gmail.com>
2022-07-24 22:35:59 +0530

    Added Unit Tests

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/InsertCipherNameTokenGeneratorTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-core/src/test/java/org/apache/shardingsphere/encrypt/rewrite/token/generator/InsertCipherNameTokenGeneratorTest.java
@@ -18,22 +18,43 @@
 package org.apache.shardingsphere.encrypt.rewrite.token.generator;
 
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
+import org.apache.shardingsphere.infra.binder.statement.dml.SelectStatementContext;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
 
-public final class InsertCipherNameTokenGeneratorTest  {
+public final class InsertCipherNameTokenGeneratorTest extends EncryptGeneratorBaseTest {
 
     private InsertCipherNameTokenGenerator generator;
 
     @Before
     public void setup() {
         generator = new InsertCipherNameTokenGenerator();
-        generator.setEncryptRule(mockEncryptRule());
+        generator.setEncryptRule(new EncryptRule(createEncryptRuleConfiguration()));
     }
 
-    private EncryptRule mockEncryptRule() {
-        EncryptRule result = mock(EncryptRule.class);
-        return result;
+    @Test
+    public void assertIsNotGenerateSQLTokenWithNotInsertStatement() {
+        assertFalse(generator.isGenerateSQLToken(mock(SelectStatementContext.class)));
+    }
+
+    @Test
+    public void assertIsGenerateSQLTokenWithInsertStatementContext() {
+        assertTrue(generator.isGenerateSQLToken(createInsertStatementContext(Collections.emptyList())));
+    }
+
+    @Test
+    public void assertGenerateSQLTokensWithInsertStatementContext() {
+        Collection<?> tokens = generator.generateSQLTokens(createInsertStatementContext(Collections.emptyList()));
+        assertThat(tokens.size(), is(1));
     }
 }
