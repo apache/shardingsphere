3ded72c6dda1c74525cbf34a4479a9a62e6ba894
skai <skcai@topode.com>
2022-07-26 18:11:41 +0800

    add test for ExpressionExtractUtil#findNotContainsNullLiteralsExpression

--- shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/test/java/org/apache/shardingsphere/sql/parser/sql/common/util/ExpressionExtractUtilTest.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-statement/src/test/java/org/apache/shardingsphere/sql/parser/sql/common/util/ExpressionExtractUtilTest.java
@@ -25,6 +25,7 @@ import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.complex.
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.simple.LiteralExpressionSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.simple.ParameterMarkerExpressionSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.predicate.AndPredicate;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.OwnerSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
 import org.junit.Test;
 
@@ -32,9 +33,12 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public final class ExpressionExtractUtilTest {
     
@@ -117,5 +121,23 @@ public final class ExpressionExtractUtilTest {
         functionSegment.getParameters().add(param3);
         List<ParameterMarkerExpressionSegment> result = ExpressionExtractUtil.getParameterMarkerExpressions(Collections.singletonList(functionSegment));
         assertThat(result.size(), is(1));
+    }
+
+    @Test
+    public void assertFindNotContainsNullLiteralsExpression() {
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new LiteralExpressionSegment(0, 3, "NULL"), false);
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new LiteralExpressionSegment(0, 3, "null"), false);
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new LiteralExpressionSegment(0, 7, "NOT NULL"), false);
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new BinaryOperationExpression(0, 0, null, new LiteralExpressionSegment(0, 3, "NULL"), null, null), false);
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new ColumnSegment(0, 5, new IdentifierValue("number")), true);
+        assertFindNotContainsNullLiteralsExpressionIsPresent(new BinaryOperationExpression(0, 0, new ColumnSegment(0, 10, new IdentifierValue("number")), null, null, null), true);
+    }
+
+    private void assertFindNotContainsNullLiteralsExpressionIsPresent(final ExpressionSegment expression, final Boolean result) {
+        if (result) {
+            assertTrue(ExpressionExtractUtil.findNotContainsNullLiteralsExpression(expression).isPresent());
+        } else {
+            assertFalse(ExpressionExtractUtil.findNotContainsNullLiteralsExpression(expression).isPresent());
+        }
     }
 }
