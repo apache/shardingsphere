b792a7df28b0557618a04385abd0d7b069f80f14
Chuxin Chen <chuxinchen.x@qq.com>
2022-07-28 16:33:51 +0800

    Update DataTypeLoaderFixture.java

--- shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/database/schema/loader/dialect/DataTypeLoaderFixture.java
+++ shardingsphere-infra/shardingsphere-infra-common/src/test/java/org/apache/shardingsphere/infra/metadata/database/schema/loader/dialect/DataTypeLoaderFixture.java
@@ -24,7 +24,7 @@ import java.sql.SQLException;
 import java.util.Map;
 
 /**
- * MySQL data type loader.
+ * Data type loader fixture.
  */
 public final class DataTypeLoaderFixture extends AbstractDataTypeLoader {
     
