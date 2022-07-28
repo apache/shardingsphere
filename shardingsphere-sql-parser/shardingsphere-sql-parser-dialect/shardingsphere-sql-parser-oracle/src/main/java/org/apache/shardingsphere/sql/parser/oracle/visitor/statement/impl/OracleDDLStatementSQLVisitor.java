3e8c6e0c99b260fb5195b876c483fc1c0b94b974
cha-cha369 <1301826221@qq.com>
2022-07-28 11:58:08 +0800

    format code

--- shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-oracle/src/main/java/org/apache/shardingsphere/sql/parser/oracle/visitor/statement/impl/OracleDDLStatementSQLVisitor.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-dialect/shardingsphere-sql-parser-oracle/src/main/java/org/apache/shardingsphere/sql/parser/oracle/visitor/statement/impl/OracleDDLStatementSQLVisitor.java
@@ -956,22 +956,22 @@ public final class OracleDDLStatementSQLVisitor extends OracleStatementSQLVisito
     public ASTNode visitAlterMaterializedViewLog(final AlterMaterializedViewLogContext ctx) {
         return new OracleAlterMaterializedViewLogStatement();
     }
-
+    
     @Override
     public ASTNode visitAlterFunction(final AlterFunctionContext ctx) {
         return new OracleAlterFunctionStatement();
     }
-
+    
     @Override
     public ASTNode visitAlterHierarchy(final AlterHierarchyContext ctx) {
         return new OracleAlterHierarchyStatement();
     }
-
+    
     @Override
     public ASTNode visitAlterLockdownProfile(final AlterLockdownProfileContext ctx) {
         return new OracleAlterLockdownProfileStatement();
     }
-
+    
     @Override
     public ASTNode visitAlterPluggableDatabase(final AlterPluggableDatabaseContext ctx) {
         return new OracleAlterPluggableDatabaseStatement();
