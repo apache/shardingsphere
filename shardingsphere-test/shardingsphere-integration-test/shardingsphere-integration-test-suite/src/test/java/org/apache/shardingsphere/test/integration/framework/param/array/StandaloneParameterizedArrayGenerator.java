c2e89bd9989aa045aaee0ba7795b1ac44351c938
zhaojinchao <zjcnb0215@gmail.com>
2022-08-01 22:12:41 +0800

    adjument code

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/param/array/StandaloneParameterizedArrayGenerator.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-suite/src/test/java/org/apache/shardingsphere/test/integration/framework/param/array/StandaloneParameterizedArrayGenerator.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.test.integration.framework.param.array;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.shardingsphere.infra.database.type.DatabaseType;
 import org.apache.shardingsphere.infra.database.type.DatabaseTypeFactory;
 import org.apache.shardingsphere.test.integration.cases.SQLCommandType;
 import org.apache.shardingsphere.test.integration.env.runtime.IntegrationTestEnvironment;
@@ -35,10 +34,6 @@ import java.util.Collections;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class StandaloneParameterizedArrayGenerator {
     
-    private static final Collection<String> ADAPTERS = Collections.singleton("jdbc");
-    
-    private static final Collection<DatabaseType> DATABASE_TYPES = Collections.singleton(DatabaseTypeFactory.getInstance("H2"));
-    
     private static final IntegrationTestEnvironment ENV = IntegrationTestEnvironment.getInstance();
     
     /**
@@ -52,7 +47,8 @@ public final class StandaloneParameterizedArrayGenerator {
             return new ParameterizedArrayGenerator(Collections.singleton("proxy"), ENV.getScenarios(), "Standalone",
                     Collections.singleton(DatabaseTypeFactory.getInstance("MySQL"))).getAssertionParameterized(sqlCommandType);
         }
-        return new ParameterizedArrayGenerator(ADAPTERS, ENV.getScenarios(), "Standalone", DATABASE_TYPES).getAssertionParameterized(sqlCommandType);
+        return new ParameterizedArrayGenerator(Collections.singleton("jdbc"), ENV.getScenarios(), "Standalone",
+                Collections.singleton(DatabaseTypeFactory.getInstance("H2"))).getAssertionParameterized(sqlCommandType);
     }
     
     /**
@@ -66,6 +62,7 @@ public final class StandaloneParameterizedArrayGenerator {
             return new ParameterizedArrayGenerator(Collections.singleton("proxy"), ENV.getScenarios(), "Standalone",
                     Collections.singleton(DatabaseTypeFactory.getInstance("MySQL"))).getCaseParameterized(sqlCommandType);
         }
-        return new ParameterizedArrayGenerator(ADAPTERS, ENV.getScenarios(), "Standalone", DATABASE_TYPES).getCaseParameterized(sqlCommandType);
+        return new ParameterizedArrayGenerator(Collections.singleton("jdbc"), ENV.getScenarios(), "Standalone",
+                Collections.singleton(DatabaseTypeFactory.getInstance("H2"))).getCaseParameterized(sqlCommandType);
     }
 }
