571bfa8d4d57262f7be551d06754fa02d7a1c5a2
azexcy <13588031592@qq.com>
2022-08-02 14:31:16 +0800

    Improve scaling mysql increment synchronization

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-mysql/src/main/java/org/apache/shardingsphere/data/pipeline/mysql/ingest/client/MySQLClient.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-mysql/src/main/java/org/apache/shardingsphere/data/pipeline/mysql/ingest/client/MySQLClient.java
@@ -51,6 +51,7 @@ import java.util.Objects;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * MySQL Connector.
@@ -73,7 +74,7 @@ public final class MySQLClient {
     
     private volatile boolean running = true;
     
-    private volatile int reconnectTimes;
+    private final AtomicInteger reconnectTimes = new AtomicInteger();
     
     /**
      * Connect to MySQL.
@@ -150,6 +151,8 @@ public final class MySQLClient {
         initDumpConnectSession();
         registerSlave();
         dumpBinlog(binlogFileName, binlogPosition, queryChecksumLength());
+        log.info("subscribe binlog file: {}, position: {}", binlogFileName, binlogPosition);
+        reconnectTimes.set(0);
     }
     
     private void initDumpConnectSession() {
@@ -257,7 +260,7 @@ public final class MySQLClient {
         public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
             if (null != responseCallback) {
                 responseCallback.setFailure(cause);
-                log.error("protocol resolution error", cause);
+                log.error("MySQLCommandResponseHandler: protocol resolution error", cause);
             }
         }
     }
@@ -283,7 +286,7 @@ public final class MySQLClient {
             if (!running) {
                 return;
             }
-            if (reconnectTimes > 3) {
+            if (reconnectTimes.get() > 3) {
                 log.warn("exceeds the maximum number of retry times, lastBinlogEvent={}", lastBinlogEvent);
                 running = false;
                 return;
@@ -294,12 +297,11 @@ public final class MySQLClient {
         @Override
         public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
             running = false;
-            log.error("protocol resolution error", cause);
+            log.error("MySQLBinlogEventHandler: protocol resolution error", cause);
         }
         
         private void reconnect() {
-            reconnectTimes++;
-            log.info("reconnect mysql client, retryTimes={}", reconnectTimes);
+            log.info("reconnect mysql client, retryTimes={}", reconnectTimes.incrementAndGet());
             closeChannel();
             connect();
             subscribe(lastBinlogEvent.getFileName(), lastBinlogEvent.getPosition());
