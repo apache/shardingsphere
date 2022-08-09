9dad619269bd9eeb7a67aff91ef49049d3919890
huangdaxiang <localhost80@foxmail.com>
2022-08-10 00:35:14 +0800

    check style

--- shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/scaling/cor/job/importer/ImporterCreatorFactoryTest.java
+++ shardingsphere-test/shardingsphere-pipeline-test/src/test/java/org/apache/shardingsphere/scaling/cor/job/importer/ImporterCreatorFactoryTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.scaling.cor.job.importer;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -21,16 +38,16 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 public final class ImporterCreatorFactoryTest {
-
+    
     @Mock
     private PipelineDataSourceManager dataSourceManager;
-
+    
     @Mock
     private PipelineChannel channel;
-
+    
     private final PipelineDataSourceConfiguration dataSourceConfig = new StandardPipelineDataSourceConfiguration(
             "jdbc:h2:mem:test_db;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;MODE=MySQL;USER=root;PASSWORD=root", "root", "root");
-
+    
     @Test
     public void assertCreateImporterForMysql() {
         Importer importer = ImporterCreatorFactory.getInstance("MySQL")
@@ -38,7 +55,7 @@ public final class ImporterCreatorFactoryTest {
                         new FixturePipelineJobProgressListener());
         assertThat(importer, instanceOf(DefaultImporter.class));
     }
-
+    
     @Test
     public void assertCreateImporterForPostgreSQL() {
         Importer importer = ImporterCreatorFactory.getInstance("PostgreSQL")
@@ -46,7 +63,7 @@ public final class ImporterCreatorFactoryTest {
                         new FixturePipelineJobProgressListener());
         assertThat(importer, instanceOf(DefaultImporter.class));
     }
-
+    
     @Test
     public void assertCreateImporterForOpenGauss() {
         Importer importer = ImporterCreatorFactory.getInstance("openGauss")
@@ -54,7 +71,7 @@ public final class ImporterCreatorFactoryTest {
                         new FixturePipelineJobProgressListener());
         assertThat(importer, instanceOf(DefaultImporter.class));
     }
-
+    
     @Test
     public void assertCreateImporterForH2() {
         Importer importer = ImporterCreatorFactory.getInstance("H2")
@@ -62,10 +79,9 @@ public final class ImporterCreatorFactoryTest {
                         new FixturePipelineJobProgressListener());
         assertThat(importer, instanceOf(FixtureImporter.class));
     }
-
+    
     private ImporterConfiguration mockImporterConfiguration() {
-        Map<LogicTableName, Set<String>>
-                shardingColumnsMap = Collections.singletonMap(new LogicTableName("test_table"), Collections.singleton("user"));
+        Map<LogicTableName, Set<String>> shardingColumnsMap = Collections.singletonMap(new LogicTableName("test_table"), Collections.singleton("user"));
         return new ImporterConfiguration(dataSourceConfig, shardingColumnsMap, new TableNameSchemaNameMapping(Collections.emptyMap()), 1000, 3, 3);
     }
 }
