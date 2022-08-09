42b68a1dfd7bedc51f3840ccd3e574f44ed54729
yx9o <yangx_soft@163.com>
2022-08-09 14:59:14 +0800

    Add AuthorityRuleQueryResultSet.

---
+++ shardingsphere-kernel/shardingsphere-authority/shardingsphere-authority-distsql/shardingsphere-authority-distsql-handler/src/test/java/org/apache/shardingsphere/authority/distsql/handler/AuthorityRuleQueryResultSetTest.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.authority.distsql.handler;
+
+import org.apache.shardingsphere.authority.config.AuthorityRuleConfiguration;
+import org.apache.shardingsphere.authority.distsql.parser.statement.ShowAuthorityRuleStatement;
+import org.apache.shardingsphere.authority.rule.AuthorityRule;
+import org.apache.shardingsphere.infra.config.algorithm.AlgorithmConfiguration;
+import org.apache.shardingsphere.infra.distsql.query.DistSQLResultSet;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
+import org.apache.shardingsphere.infra.metadata.user.ShardingSphereUser;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class AuthorityRuleQueryResultSetTest {
+    
+    @Test
+    public void assertAuthorityRule() {
+        ShardingSphereDatabase database = mockDatabase();
+        DistSQLResultSet resultSet = new AuthorityRuleQueryResultSet();
+        resultSet.init(database, mock(ShowAuthorityRuleStatement.class));
+        Collection<Object> actual = resultSet.getRowData();
+        assertThat(actual.size(), is(3));
+        assertTrue(actual.contains("root@localhost"));
+        assertTrue(actual.contains("ALL_PERMITTED"));
+        assertTrue(actual.contains(""));
+    }
+    
+    private ShardingSphereDatabase mockDatabase() {
+        ShardingSphereDatabase result = mock(ShardingSphereDatabase.class, RETURNS_DEEP_STUBS);
+        AuthorityRule rule = mock(AuthorityRule.class);
+        when(rule.getConfiguration()).thenReturn(getAuthorityRuleConfiguration());
+        when(result.getRuleMetaData().findSingleRule(AuthorityRule.class)).thenReturn(Optional.of(rule));
+        return result;
+    }
+    
+    private AuthorityRuleConfiguration getAuthorityRuleConfiguration() {
+        ShardingSphereUser root = new ShardingSphereUser("root", "", "localhost");
+        return new AuthorityRuleConfiguration(Collections.singleton(root), new AlgorithmConfiguration("ALL_PERMITTED", new Properties()));
+    }
+}
