164b191b9314cd599310784354d075ff5f4f60e8
ThanoshanMV <mvthanoshan@apache.org>
2022-08-12 00:56:46 +0530

    Add empty line

--- shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/core/database/visitor/SQLVisitorRule.java
+++ shardingsphere-sql-parser/shardingsphere-sql-parser-engine/src/main/java/org/apache/shardingsphere/sql/parser/core/database/visitor/SQLVisitorRule.java
@@ -635,6 +635,7 @@ public enum SQLVisitorRule {
     CREATE_GROUP("CreateGroup", SQLStatementType.DCL),
     
     CREATE_MATERIALIZED_VIEW("CreateMaterializedView", SQLStatementType.DDL);
+    
     private final String name;
     
     @Getter
