529a3219c398ca890608c26a26e0ede171a0883f
skcai <sunkai.cai@gmail.com>
2022-08-13 23:38:58 +0800

    add test case of AlterEncryptRule

--- shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-distsql/shardingsphere-encrypt-distsql-parser/src/test/java/org/apache/shardingsphere/encrypt/distsql/parser/EncryptDistSqlTest.java
+++ shardingsphere-features/shardingsphere-encrypt/shardingsphere-encrypt-distsql/shardingsphere-encrypt-distsql-parser/src/test/java/org/apache/shardingsphere/encrypt/distsql/parser/EncryptDistSqlTest.java
@@ -24,6 +24,7 @@ import org.apache.shardingsphere.distsql.parser.statement.DistSQLStatement;
 import org.apache.shardingsphere.encrypt.distsql.parser.facade.EncryptDistSQLStatementParserFacade;
 import org.apache.shardingsphere.encrypt.distsql.parser.segment.EncryptColumnSegment;
 import org.apache.shardingsphere.encrypt.distsql.parser.segment.EncryptRuleSegment;
+import org.apache.shardingsphere.encrypt.distsql.parser.statement.AlterEncryptRuleStatement;
 import org.apache.shardingsphere.encrypt.distsql.parser.statement.CreateEncryptRuleStatement;
 import org.apache.shardingsphere.sql.parser.api.visitor.SQLVisitor;
 import org.apache.shardingsphere.sql.parser.core.ParseASTNode;
@@ -40,14 +41,28 @@ import static org.junit.Assert.assertThat;
 public final class EncryptDistSqlTest {
     
     @Test
-    public void createEncryptRuleTest() {
+    public void assertCreateEncryptRule() {
         String sql = "CREATE ENCRYPT RULE t_encrypt (COLUMNS("
-                + " (NAME=user_id,PLAIN=user_plain,CIPHER=user_cipher,TYPE(NAME=\"AES\",PROPERTIES('aes-key-value'='123456abc')))"
-                + ",(NAME=order_id, CIPHER =order_cipher,TYPE(NAME=\"MD5\")))"
+                + " (NAME=user_id,PLAIN=user_plain,CIPHER=user_cipher,TYPE(NAME='AES',PROPERTIES('aes-key-value'='123456abc')))"
+                + ",(NAME=order_id, CIPHER =order_cipher,TYPE(NAME='MD5')))"
                 + ",QUERY_WITH_CIPHER_COLUMN=true)";
         CreateEncryptRuleStatement createEncryptRuleStatement = (CreateEncryptRuleStatement) getEncryptDistSQLStatement(sql);
         assertThat(createEncryptRuleStatement.getRules().size(), is(1));
-        EncryptRuleSegment encryptRuleSegment = createEncryptRuleStatement.getRules().iterator().next();
+        assertEncryptRule(createEncryptRuleStatement.getRules().iterator().next());
+
+    }
+
+    @Test
+    public void assertAlterEncryptRule() {
+        String sql = "ALTER ENCRYPT RULE t_encrypt (COLUMNS("
+                + " (NAME=user_id,PLAIN=user_plain,CIPHER=user_cipher,TYPE(NAME='AES',PROPERTIES('aes-key-value'='123456abc'))),"
+                + " (NAME=order_id,CIPHER=order_cipher,TYPE(NAME='MD5'))), QUERY_WITH_CIPHER_COLUMN=TRUE)";
+        AlterEncryptRuleStatement alterEncryptRule = (AlterEncryptRuleStatement) getEncryptDistSQLStatement(sql);
+        assertThat(alterEncryptRule.getRules().size(), is(1));
+        assertEncryptRule(alterEncryptRule.getRules().iterator().next());
+    }
+
+    private void assertEncryptRule(EncryptRuleSegment encryptRuleSegment) {
         assertThat(encryptRuleSegment.getTableName(), is("t_encrypt"));
         assertThat(encryptRuleSegment.getColumns().size(), is(2));
         assertThat(encryptRuleSegment.getQueryWithCipherColumn(), is(true));
@@ -66,9 +81,8 @@ public final class EncryptDistSqlTest {
         assertThat(orderEncryptColumn.getCipherColumn(), is("order_cipher"));
         assertThat(orderEncryptColumn.getEncryptor().getName(), is("MD5"));
         assertThat(orderEncryptColumn.getEncryptor().getProps().size(), is(0));
-        
     }
-    
+
     @SneakyThrows(ReflectiveOperationException.class)
     @SuppressWarnings("rawtypes")
     private DistSQLStatement getEncryptDistSQLStatement(final String sql) {
