99566f706e1288579b4404d8596534d241176c63
孙念君 Nianjun Sun <sunnianjun@sphere-ex.com>
2022-08-15 14:17:53 +0800

    Remove : remove spelling error Java doc

--- shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-env/src/test/java/org/apache/shardingsphere/test/integration/env/container/atomic/util/DatabaseTypeUtil.java
+++ shardingsphere-test/shardingsphere-integration-test/shardingsphere-integration-test-env/src/test/java/org/apache/shardingsphere/test/integration/env/container/atomic/util/DatabaseTypeUtil.java
@@ -32,7 +32,7 @@ public final class DatabaseTypeUtil {
      * Check MySQL database type.
      *
      * @param databaseType database type
-     * @return true if database type isDatabaseTypeUtil.java MySQL, false otherwise
+     * @return true if database type MySQL, false otherwise
      */
     public static boolean isMySQL(final DatabaseType databaseType) {
         return databaseType instanceof MySQLDatabaseType;
