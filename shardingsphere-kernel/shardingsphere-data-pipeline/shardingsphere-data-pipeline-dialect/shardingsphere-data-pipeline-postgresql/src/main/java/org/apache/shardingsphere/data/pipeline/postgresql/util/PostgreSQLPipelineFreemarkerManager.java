906f7bef3b81112b46c1e7a2395a84b8ae89233e
Liang Zhang <zhangliang@apache.org>
2022-08-21 11:59:43 +0800

    Refactor PostgreSQLPipelineFreemarkerManager (#20330)

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-postgresql/src/main/java/org/apache/shardingsphere/data/pipeline/postgresql/util/PostgreSQLPipelineFreemarkerManager.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-dialect/shardingsphere-data-pipeline-postgresql/src/main/java/org/apache/shardingsphere/data/pipeline/postgresql/util/PostgreSQLPipelineFreemarkerManager.java
@@ -24,14 +24,12 @@ import freemarker.template.TemplateNotFoundException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.shardingsphere.infra.exception.ShardingSphereException;
 
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 
 /**
  * PostgreSQL pipeline freemarker manager.
@@ -39,22 +37,21 @@ import java.util.Optional;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class PostgreSQLPipelineFreemarkerManager {
     
-    private static final Map<Integer, String> PG_VERSION_PATHS = new LinkedHashMap<>();
+    private static final Map<Integer, String> VERSION_PATHS = new LinkedHashMap<>();
     
     private static final Configuration TEMPLATE_CONFIG = createTemplateConfiguration();
     
     static {
-        PG_VERSION_PATHS.put(120000, "12_plus");
-        PG_VERSION_PATHS.put(110000, "11_plus");
-        PG_VERSION_PATHS.put(100000, "10_plus");
-        PG_VERSION_PATHS.put(90600, "9.6_plus");
-        PG_VERSION_PATHS.put(90500, "9.5_plus");
-        PG_VERSION_PATHS.put(90400, "9.4_plus");
-        PG_VERSION_PATHS.put(90300, "9.3_plus");
-        PG_VERSION_PATHS.put(90200, "9.2_plus");
-        PG_VERSION_PATHS.put(90100, "9.1_plus");
-        PG_VERSION_PATHS.put(90000, "9.0_plus");
-        PG_VERSION_PATHS.put(0, "default");
+        VERSION_PATHS.put(120000, "12_plus");
+        VERSION_PATHS.put(110000, "11_plus");
+        VERSION_PATHS.put(100000, "10_plus");
+        VERSION_PATHS.put(90600, "9.6_plus");
+        VERSION_PATHS.put(90500, "9.5_plus");
+        VERSION_PATHS.put(90400, "9.4_plus");
+        VERSION_PATHS.put(90300, "9.3_plus");
+        VERSION_PATHS.put(90200, "9.2_plus");
+        VERSION_PATHS.put(90100, "9.1_plus");
+        VERSION_PATHS.put(90000, "9.0_plus");
     }
     
     private static Configuration createTemplateConfiguration() {
@@ -77,21 +74,21 @@ public final class PostgreSQLPipelineFreemarkerManager {
     public static String getSQLByVersion(final Map<String, Object> dataModel, final String pathFormat, final int majorVersion, final int minorVersion) {
         int version = majorVersion * 10000 + minorVersion;
         try (StringWriter result = new StringWriter()) {
-            findTemplate(pathFormat, version).orElseThrow(() -> new ShardingSphereException("Failed to get template, path:%s, version:%s", pathFormat, version)).process(dataModel, result);
+            findTemplate(pathFormat, version).process(dataModel, result);
             return result.toString();
         }
     }
     
-    private static Optional<Template> findTemplate(final String pathFormat, final int version) throws IOException {
-        for (Entry<Integer, String> entry : PG_VERSION_PATHS.entrySet()) {
+    private static Template findTemplate(final String pathFormat, final int version) throws IOException {
+        for (Entry<Integer, String> entry : VERSION_PATHS.entrySet()) {
             if (entry.getKey() > version) {
                 continue;
             }
             try {
-                return Optional.of(TEMPLATE_CONFIG.getTemplate(String.format(pathFormat, entry.getValue())));
+                return TEMPLATE_CONFIG.getTemplate(String.format(pathFormat, entry.getValue()));
             } catch (TemplateNotFoundException ignored) {
             }
         }
-        return Optional.empty();
+        return TEMPLATE_CONFIG.getTemplate(String.format(pathFormat, "default"));
     }
 }
