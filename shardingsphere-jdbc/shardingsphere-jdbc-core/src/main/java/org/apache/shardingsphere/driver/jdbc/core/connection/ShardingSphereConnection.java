54ceafeebdfec0a3aa18b8204595953df8f3173c
oking <xieqing1116@gmail.com>
2022-08-22 20:29:53 +0800

    Fixes #20414

--- shardingsphere-jdbc/shardingsphere-jdbc-core/src/main/java/org/apache/shardingsphere/driver/jdbc/core/connection/ShardingSphereConnection.java
+++ shardingsphere-jdbc/shardingsphere-jdbc-core/src/main/java/org/apache/shardingsphere/driver/jdbc/core/connection/ShardingSphereConnection.java
@@ -110,7 +110,7 @@ public final class ShardingSphereConnection extends AbstractConnectionAdapter {
     
     @Override
     public PreparedStatement prepareStatement(final String sql, final String[] columnNames) throws SQLException {
-        return new ShardingSpherePreparedStatement(this, sql, Statement.RETURN_GENERATED_KEYS);
+        return new ShardingSpherePreparedStatement(this, sql, columnNames);
     }
     
     @Override
