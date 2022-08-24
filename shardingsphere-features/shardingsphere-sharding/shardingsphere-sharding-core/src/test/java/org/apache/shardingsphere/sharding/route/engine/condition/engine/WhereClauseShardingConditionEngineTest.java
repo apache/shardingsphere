705c8a35fb8645256173669134e4902e788746d6
bjyflihongyu <lihongyuinfo@jd.com>
2022-08-24 19:25:45 +0800

    add UT for WhereClauseShardingConditionEngine

---
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-core/src/test/java/org/apache/shardingsphere/sharding/route/engine/condition/engine/WhereClauseShardingConditionEngineTest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sharding.route.engine.condition.engine;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.Mockito.when;
+
+import org.apache.groovy.util.Maps;
+import org.apache.shardingsphere.infra.binder.segment.table.TablesContext;
+import org.apache.shardingsphere.infra.binder.statement.dml.SelectStatementContext;
+import org.apache.shardingsphere.infra.database.type.DatabaseTypeEngine;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
+import org.apache.shardingsphere.sharding.route.engine.condition.ShardingCondition;
+import org.apache.shardingsphere.sharding.route.engine.condition.engine.impl.WhereClauseShardingConditionEngine;
+import org.apache.shardingsphere.sharding.route.engine.condition.value.ListShardingConditionValue;
+import org.apache.shardingsphere.sharding.route.engine.condition.value.RangeShardingConditionValue;
+import org.apache.shardingsphere.sharding.rule.ShardingRule;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.ColumnSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.BetweenExpression;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ExpressionSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.InExpression;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ListExpression;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.simple.LiteralExpressionSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.predicate.WhereSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.value.identifier.IdentifierValue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class WhereClauseShardingConditionEngineTest {
+
+    private WhereClauseShardingConditionEngine shardingConditionEngine;
+
+    @Mock
+    private ShardingRule shardingRule;
+
+    @Mock
+    private SelectStatementContext sqlStatementContext;
+
+    @Mock
+    private WhereSegment whereSegment;
+
+    @Mock
+    private TablesContext tablesContext;
+
+    @Before
+    public void setUp() throws SQLException {
+        ShardingSphereDatabase database = ShardingSphereDatabase.create("test_db", DatabaseTypeEngine.getDatabaseType("MySQL"));
+        shardingConditionEngine = new WhereClauseShardingConditionEngine(shardingRule, database);
+
+        when(sqlStatementContext.getWhereSegments()).thenReturn(Collections.singletonList(whereSegment));
+        when(sqlStatementContext.getTablesContext()).thenReturn(tablesContext);
+        when(tablesContext.findTableNamesByColumnSegment(anyCollection(), any())).thenReturn(Maps.of("foo_sharding_col", "table_1"));
+    }
+
+    @Test
+    public void assertCreateShardingConditionsSelectRangeStatement() {
+        int between = 1;
+        int and = 100;
+        ColumnSegment left = new ColumnSegment(0, 0, new IdentifierValue("foo_sharding_col"));
+        ExpressionSegment betweenSegment = new LiteralExpressionSegment(0, 0, between);
+        ExpressionSegment andSegment = new LiteralExpressionSegment(0, 0, and);
+        BetweenExpression betweenExpression = new BetweenExpression(0, 0, left, betweenSegment, andSegment, false);
+        when(whereSegment.getExpr()).thenReturn(betweenExpression);
+        when(shardingRule.findShardingColumn(any(), any())).thenReturn(Optional.of("foo_sharding_col"));
+        List<ShardingCondition> shardingConditions = shardingConditionEngine.createShardingConditions(sqlStatementContext, Collections.emptyList());
+        assertThat(shardingConditions.get(0).getStartIndex(), is(0));
+        assertTrue(shardingConditions.get(0).getValues().get(0) instanceof RangeShardingConditionValue);
+    }
+
+    @Test
+    public void assertCreateShardingConditionsSelectInStatement() {
+        ColumnSegment left = new ColumnSegment(0, 0, new IdentifierValue("foo_sharding_col"));
+        ListExpression right = new ListExpression(0, 0);
+        LiteralExpressionSegment literalExpressionSegment = new LiteralExpressionSegment(0, 0, 5);
+        right.getItems().add(literalExpressionSegment);
+        InExpression inExpression = new InExpression(0, 0, left, right, false);
+        when(whereSegment.getExpr()).thenReturn(inExpression);
+        when(shardingRule.findShardingColumn(any(), any())).thenReturn(Optional.of("foo_sharding_col"));
+        List<ShardingCondition> shardingConditions = shardingConditionEngine.createShardingConditions(sqlStatementContext, Collections.emptyList());
+        assertThat(shardingConditions.get(0).getStartIndex(), is(0));
+        assertTrue(shardingConditions.get(0).getValues().get(0) instanceof ListShardingConditionValue);
+    }
+
+}
