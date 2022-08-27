336f06fef8250e77187bc994d89c267b97c10561
zhangliang <zhangliang@apache.org>
2022-08-27 23:43:58 +0800

    Add DatabaseServerLoadingException

--- shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/database/DatabaseServerInfoTest.java
+++ shardingsphere-proxy/shardingsphere-proxy-bootstrap/src/test/java/org/apache/shardingsphere/proxy/database/DatabaseServerInfoTest.java
@@ -17,7 +17,7 @@
 
 package org.apache.shardingsphere.proxy.database;
 
-import org.apache.shardingsphere.infra.exception.ShardingSphereException;
+import org.apache.shardingsphere.proxy.exception.DatabaseServerLoadingException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
@@ -39,8 +39,7 @@ public final class DatabaseServerInfoTest {
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private DataSource dataSource;
     
-    @SuppressWarnings("ResultOfObjectAllocationIgnored")
-    @Test(expected = ShardingSphereException.class)
+    @Test(expected = DatabaseServerLoadingException.class)
     public void assertNewInstanceFailure() throws SQLException {
         when(dataSource.getConnection()).thenThrow(SQLException.class);
         new DatabaseServerInfo(dataSource);
