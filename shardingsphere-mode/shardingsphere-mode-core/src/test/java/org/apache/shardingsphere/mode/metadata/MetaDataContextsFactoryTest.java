774b38fa6bfa5ebf7b56f907a63e9c84952284d9
Zhengqiang Duan <duanzhengqiang@apache.org>
2022-09-05 09:04:04 +0800

    Refactor YamlSQLParserCacheOptionConfigurationSwapperTest (#20779)

--- shardingsphere-mode/shardingsphere-mode-core/src/test/java/org/apache/shardingsphere/mode/metadata/MetaDataContextsFactoryTest.java
+++ shardingsphere-mode/shardingsphere-mode-core/src/test/java/org/apache/shardingsphere/mode/metadata/MetaDataContextsFactoryTest.java
@@ -96,7 +96,7 @@ public final class MetaDataContextsFactoryTest {
     }
     
     private ContextManagerBuilderParameter createContextManagerBuilderParameter() {
-        return new ContextManagerBuilderParameter(null, 
+        return new ContextManagerBuilderParameter(null,
                 Collections.singletonMap("foo_db", mock(DataSourceGeneratedDatabaseConfiguration.class)), Collections.emptyList(), new Properties(), Collections.emptyList(), null);
     }
 }
