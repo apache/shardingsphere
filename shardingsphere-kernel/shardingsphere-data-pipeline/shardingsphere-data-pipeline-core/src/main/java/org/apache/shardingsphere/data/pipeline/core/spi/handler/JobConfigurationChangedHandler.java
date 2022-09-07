2e175ba5d780b97d9b0f3ec5e145274da9ce3b51
Xinze Guo <101622833+azexcy@users.noreply.github.com>
2022-09-07 21:30:43 +0800

    Extract job configuration change event handler SPI and impl in PipelineJobExecutor for common usage (#20782)
    * Extract job configuration change event handler SPI and impl in PipelineJobExecutor for common usage
    
    * Fix codestyle
    
    * Add processor
    
    * Improve comment

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/spi/handler/JobConfigurationChangedHandler.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/spi/handler/JobConfigurationChangedHandler.java
@@ -18,24 +18,18 @@
 package org.apache.shardingsphere.data.pipeline.core.spi.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.data.pipeline.api.config.job.MigrationJobConfiguration;
-import org.apache.shardingsphere.data.pipeline.api.config.job.yaml.YamlMigrationJobConfigurationSwapper;
-import org.apache.shardingsphere.data.pipeline.core.api.PipelineAPIFactory;
-import org.apache.shardingsphere.data.pipeline.core.context.PipelineContext;
-import org.apache.shardingsphere.data.pipeline.core.job.PipelineJobCenter;
+import org.apache.shardingsphere.data.pipeline.core.job.PipelineJobIdUtils;
 import org.apache.shardingsphere.data.pipeline.core.metadata.node.PipelineMetaDataNode;
-import org.apache.shardingsphere.data.pipeline.scenario.migration.MigrationJob;
-import org.apache.shardingsphere.data.pipeline.scenario.migration.MigrationJobPreparer;
+import org.apache.shardingsphere.data.pipeline.core.spi.process.PipelineJobConfigurationChangedEventProcessor;
+import org.apache.shardingsphere.data.pipeline.core.spi.process.PipelineJobConfigurationChangedEventProcessorFactory;
 import org.apache.shardingsphere.elasticjob.infra.pojo.JobConfigurationPOJO;
-import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.OneOffJobBootstrap;
 import org.apache.shardingsphere.infra.util.yaml.YamlEngine;
 import org.apache.shardingsphere.mode.repository.cluster.listener.DataChangedEvent;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.regex.Pattern;
 
 /**
- * Migration pipeline meta data handler.
+ * Job config changed handler.
  */
 @Slf4j
 public final class JobConfigurationChangedHandler implements PipelineMetaDataChangedHandler {
@@ -57,37 +51,7 @@ public final class JobConfigurationChangedHandler implements PipelineMetaDataCha
             log.error("analyze job config pojo failed.", ex);
             return;
         }
-        String jobId = jobConfigPOJO.getJobName();
-        if (jobConfigPOJO.isDisabled()) {
-            PipelineJobCenter.stop(jobId);
-            return;
-        }
-        switch (event.getType()) {
-            case ADDED:
-            case UPDATED:
-                if (PipelineJobCenter.isJobExisting(jobConfigPOJO.getJobName())) {
-                    log.info("{} added to executing jobs failed since it already exists", jobConfigPOJO.getJobName());
-                } else {
-                    log.info("{} executing jobs", jobConfigPOJO.getJobName());
-                    CompletableFuture.runAsync(() -> execute(jobConfigPOJO), PipelineContext.getEventListenerExecutor());
-                }
-                break;
-            case DELETED:
-                log.info("deleted jobId={}", jobId);
-                MigrationJobConfiguration jobConfig = YamlMigrationJobConfigurationSwapper.swapToObject(jobConfigPOJO.getJobParameter());
-                new MigrationJobPreparer().cleanup(jobConfig);
-                PipelineJobCenter.stop(jobConfigPOJO.getJobName());
-                break;
-            default:
-                break;
-        }
-    }
-    
-    private void execute(final JobConfigurationPOJO jobConfigPOJO) {
-        MigrationJob job = new MigrationJob();
-        PipelineJobCenter.addJob(jobConfigPOJO.getJobName(), job);
-        OneOffJobBootstrap oneOffJobBootstrap = new OneOffJobBootstrap(PipelineAPIFactory.getRegistryCenter(), job, jobConfigPOJO.toJobConfiguration());
-        oneOffJobBootstrap.execute();
-        job.setOneOffJobBootstrap(oneOffJobBootstrap);
+        PipelineJobConfigurationChangedEventProcessor processor = PipelineJobConfigurationChangedEventProcessorFactory.getInstance(PipelineJobIdUtils.parseJobType(jobConfigPOJO.getJobName()));
+        processor.process(event, jobConfigPOJO);
     }
 }
