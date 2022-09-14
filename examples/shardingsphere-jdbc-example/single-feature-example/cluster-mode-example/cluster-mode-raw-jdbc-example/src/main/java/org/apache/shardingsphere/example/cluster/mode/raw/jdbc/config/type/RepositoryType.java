7090b8fbfca98d7e2f163a464ef7c9306c213b9d
qiuhaoyang <69-qhyhs@users.noreply.gitlab.topviewclub.cn>
2022-09-14 20:12:17 +0800

    Refactor example

---
+++ examples/shardingsphere-jdbc-example/single-feature-example/cluster-mode-example/cluster-mode-raw-jdbc-example/src/main/java/org/apache/shardingsphere/example/cluster/mode/raw/jdbc/config/type/RepositoryType.java
@@ -0,0 +1,13 @@
+package org.apache.shardingsphere.example.cluster.mode.raw.jdbc.config.type;
+
+/**
+ * Repository type.
+ */
+public class RepositoryType {
+    
+    public static final String NACOS = "Nacos";
+    
+    public static final String ZOOKEEPER = "ZooKeeper";
+    
+    public static final String ETCD = "etcd";
+}
