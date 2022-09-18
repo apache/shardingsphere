e26901f19a80993a7a0412b4c55a0c9e07ff15ad
skcai <sunkai.cai@gmail.com>
2022-09-18 21:29:35 +0800

    Update ShardingTableRuleStatementChecker.java

--- shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-distsql/shardingsphere-sharding-distsql-handler/src/main/java/org/apache/shardingsphere/sharding/distsql/handler/checker/ShardingTableRuleStatementChecker.java
+++ shardingsphere-features/shardingsphere-sharding/shardingsphere-sharding-distsql/shardingsphere-sharding-distsql-handler/src/main/java/org/apache/shardingsphere/sharding/distsql/handler/checker/ShardingTableRuleStatementChecker.java
@@ -293,14 +293,16 @@ public final class ShardingTableRuleStatementChecker {
     private static void checkAlgorithm(final String databaseName, final ShardingRuleConfiguration currentRuleConfig, final Collection<TableRuleSegment> rules) throws DistSQLException {
         Map<String, AlgorithmConfiguration> currentAlgorithms = Optional.ofNullable(currentRuleConfig).map(ShardingRuleConfiguration::getShardingAlgorithms).orElse(Collections.emptyMap());
         Collection<String> invalidAlgorithms = rules.stream().map(each -> Arrays.asList(each.getDatabaseStrategySegment(), each.getTableStrategySegment())).flatMap(Collection::stream)
-                .filter(Objects::nonNull).map(each -> isInvalidAlgorithm(currentAlgorithms, each)).filter(Objects::nonNull).collect(Collectors.toList());
+                .filter(Objects::nonNull).map(each -> getInvalidAlgorithmName(currentAlgorithms, each)).filter(Objects::nonNull).collect(Collectors.toList());
         DistSQLException.predictionThrow(invalidAlgorithms.isEmpty(), () -> new InvalidAlgorithmConfigurationException(databaseName, invalidAlgorithms));
     }
     
-    private static String isInvalidAlgorithm(final Map<String, AlgorithmConfiguration> currentAlgorithms, final ShardingStrategySegment shardingStrategySegment) {
+    private static String getInvalidAlgorithmName(final Map<String, AlgorithmConfiguration> currentAlgorithms, final ShardingStrategySegment shardingStrategySegment) {
         ShardingAlgorithm shardingAlgorithm;
         String shardingAlgorithmName;
-        if (null == shardingStrategySegment.getShardingAlgorithmName() && null != shardingStrategySegment.getAlgorithmSegment()) {
+        if (null == shardingStrategySegment.getShardingAlgorithmName() && null == shardingStrategySegment.getAlgorithmSegment()) {
+            return "ShardingAlgorithm is not fount";
+        } else if (null != shardingStrategySegment.getAlgorithmSegment()) {
             shardingAlgorithmName = shardingStrategySegment.getAlgorithmSegment().getName();
             shardingAlgorithm = ShardingAlgorithmFactory.newInstance(new AlgorithmConfiguration(shardingAlgorithmName, shardingStrategySegment.getAlgorithmSegment().getProps()));
         } else {
