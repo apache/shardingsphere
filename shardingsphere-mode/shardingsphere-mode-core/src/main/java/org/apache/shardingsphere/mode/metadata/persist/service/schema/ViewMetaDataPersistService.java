9c83948cdb49af429851eb2eca9ce10da9dfb7a7
zhaojinchao <zhaojinchao@apache.org>
2022-09-19 08:13:14 +0800

    Refactor view metadata load and persist logic (#21037)
    * Refactor view metadata load and persist logic
    
    * Remove unless line
    
    * Rename to result

--- shardingsphere-mode/shardingsphere-mode-core/src/main/java/org/apache/shardingsphere/mode/metadata/persist/service/schema/ViewMetaDataPersistService.java
+++ shardingsphere-mode/shardingsphere-mode-core/src/main/java/org/apache/shardingsphere/mode/metadata/persist/service/schema/ViewMetaDataPersistService.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.mode.metadata.persist.service.schema;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
-import org.apache.shardingsphere.infra.metadata.database.schema.SchemaManager;
 import org.apache.shardingsphere.infra.metadata.database.schema.decorator.model.ShardingSphereView;
 import org.apache.shardingsphere.infra.util.yaml.YamlEngine;
 import org.apache.shardingsphere.infra.yaml.schema.pojo.YamlShardingSphereView;
@@ -42,9 +41,6 @@ public final class ViewMetaDataPersistService implements SchemaMetaDataPersistSe
     
     @Override
     public void compareAndPersist(final String databaseName, final String schemaName, final Map<String, ShardingSphereView> loadedViews) {
-        Map<String, ShardingSphereView> currentViews = load(databaseName, schemaName);
-        persist(databaseName, schemaName, SchemaManager.getToBeAddedViews(loadedViews, currentViews));
-        SchemaManager.getToBeDeletedViews(loadedViews, currentViews).forEach((key, value) -> delete(databaseName, schemaName, key));
     }
     
     @Override
