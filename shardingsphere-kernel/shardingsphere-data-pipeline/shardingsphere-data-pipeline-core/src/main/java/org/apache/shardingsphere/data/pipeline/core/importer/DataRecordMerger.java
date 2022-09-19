2801fc243af9d2d33a43776bb4b4d478623a25d6
Zhengqiang Duan <duanzhengqiang@apache.org>
2022-09-19 20:50:22 +0800

    Add more integration test case for sql federation (#21059)
    * Add more integration test case for sql federation
    
    * Add more integration test case for sql federation

--- shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/importer/DataRecordMerger.java
+++ shardingsphere-kernel/shardingsphere-data-pipeline/shardingsphere-data-pipeline-core/src/main/java/org/apache/shardingsphere/data/pipeline/core/importer/DataRecordMerger.java
@@ -88,8 +88,8 @@ public final class DataRecordMerger {
     
     private void mergeInsert(final DataRecord dataRecord, final Map<DataRecord.Key, DataRecord> dataRecords) {
         DataRecord beforeDataRecord = dataRecords.get(dataRecord.getKey());
-        ShardingSpherePreconditions.checkState(null == beforeDataRecord || IngestDataChangeType.DELETE.equals(beforeDataRecord.getType()),
-                () -> new PipelineUnexpectedDataRecordOrderException(beforeDataRecord, dataRecord));
+        ShardingSpherePreconditions.checkState(null == beforeDataRecord
+                || IngestDataChangeType.DELETE.equals(beforeDataRecord.getType()), () -> new PipelineUnexpectedDataRecordOrderException(beforeDataRecord, dataRecord));
         dataRecords.put(dataRecord.getKey(), dataRecord);
     }
     
@@ -120,8 +120,8 @@ public final class DataRecordMerger {
     
     private void mergeDelete(final DataRecord dataRecord, final Map<DataRecord.Key, DataRecord> dataRecords) {
         DataRecord beforeDataRecord = dataRecords.get(dataRecord.getKey());
-        ShardingSpherePreconditions.checkState(null == beforeDataRecord || (!IngestDataChangeType.DELETE.equals(beforeDataRecord.getType())),
-                () -> new PipelineUnexpectedDataRecordOrderException(beforeDataRecord, dataRecord));
+        ShardingSpherePreconditions.checkState(null == beforeDataRecord
+                || (!IngestDataChangeType.DELETE.equals(beforeDataRecord.getType())), () -> new PipelineUnexpectedDataRecordOrderException(beforeDataRecord, dataRecord));
         if (null != beforeDataRecord && IngestDataChangeType.UPDATE.equals(beforeDataRecord.getType()) && checkUpdatedPrimaryKey(beforeDataRecord)) {
             DataRecord mergedDataRecord = new DataRecord(dataRecord.getPosition(), dataRecord.getColumnCount());
             for (int i = 0; i < dataRecord.getColumnCount(); i++) {
