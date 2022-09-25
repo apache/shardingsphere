ba081e00d57a37a1b88bd624bbc612c840b02a5d
caqhy <75082574+caqhy@users.noreply.github.com>
2022-09-25 20:14:22 +0800

    Support Nacos to persist metadata in cluster mode (#20984)
    * Support ClusterPersistRepository: Nacos in cluster mode
    
    * Add unit tests for NacosRepository
    
    * Add unit tests for database based distributed lock
    
    * Refactor example
    
    * Fix NPE when etcd listener triggers
    
    * Add license
    
    * Revert commit
    
    * Refactor NacosRepository
    
    * Refactor NacosRepositoryTest
    
    * Update pom
    
    * Remove duplicate IP check
    
    * Remove SPI
    
    * Add NacosRepository SPI
    
    * Refactor example
    
    * Refactor MetadataUtil
    
    * Fix lombok
    
    * Add final
    
    * Refactor nacos initialization process and watcher logic
    
    * Refactor NacosRepositoryTest
    
    * Remove RegisterMetadata
    
    * Add ServiceController to manage service metadata
    
    * Update NacosRepositoryTest
    
    * Update clusterIp default value
    
    * Update NacosPropertiesTest
    
    * Remove Slf4j
    
    * Add license
    
    * Rename tryLock and unlock
    
    * Fix import
    
    Co-authored-by: qiuhaoyang <69-qhyhs@users.noreply.gitlab.topviewclub.cn>

---
+++ shardingsphere-mode/shardingsphere-mode-type/shardingsphere-cluster-mode/shardingsphere-cluster-mode-repository/shardingsphere-cluster-mode-repository-provider/shardingsphere-cluster-mode-repository-nacos/src/test/java/org/apache/shardingsphere/mode/repository/cluster/nacos/props/NacosPropertiesTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.mode.repository.cluster.nacos.props;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public final class NacosPropertiesTest {
+    
+    @Test
+    public void assertGetValue() {
+        NacosProperties actual = new NacosProperties(createProperties());
+        assertThat(actual.getValue(NacosPropertyKey.CLUSTER_IP), is("127.0.0.1"));
+        assertThat(actual.getValue(NacosPropertyKey.RETRY_INTERVAL_MILLISECONDS), is(1000L));
+        assertThat(actual.getValue(NacosPropertyKey.MAX_RETRIES), is(5));
+        assertThat(actual.getValue(NacosPropertyKey.TIME_TO_LIVE_SECONDS), is(60));
+    }
+    
+    private Properties createProperties() {
+        Properties result = new Properties();
+        result.setProperty(NacosPropertyKey.CLUSTER_IP.getKey(), "127.0.0.1");
+        result.setProperty(NacosPropertyKey.RETRY_INTERVAL_MILLISECONDS.getKey(), "1000");
+        result.setProperty(NacosPropertyKey.MAX_RETRIES.getKey(), "5");
+        result.setProperty(NacosPropertyKey.TIME_TO_LIVE_SECONDS.getKey(), "60");
+        return result;
+    }
+    
+    @Test
+    public void assertGetDefaultValue() {
+        NacosProperties actual = new NacosProperties(new Properties());
+        assertThat(actual.getValue(NacosPropertyKey.CLUSTER_IP), is(""));
+        assertThat(actual.getValue(NacosPropertyKey.RETRY_INTERVAL_MILLISECONDS), is(500L));
+        assertThat(actual.getValue(NacosPropertyKey.MAX_RETRIES), is(3));
+        assertThat(actual.getValue(NacosPropertyKey.TIME_TO_LIVE_SECONDS), is(30));
+    }
+}
