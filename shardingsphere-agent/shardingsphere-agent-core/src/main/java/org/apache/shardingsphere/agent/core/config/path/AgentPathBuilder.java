8360a5d456830029b54feae56c3843789852a0f8
Liang Zhang <zhangliang@apache.org>
2022-09-29 19:45:23 +0800

    Remove ShardingSphereAgentException (#21271)

--- shardingsphere-agent/shardingsphere-agent-core/src/main/java/org/apache/shardingsphere/agent/core/config/path/AgentPathBuilder.java
+++ shardingsphere-agent/shardingsphere-agent-core/src/main/java/org/apache/shardingsphere/agent/core/config/path/AgentPathBuilder.java
@@ -17,22 +17,21 @@
 
 package org.apache.shardingsphere.agent.core.config.path;
 
+import com.google.common.base.Preconditions;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.agent.core.exception.ShardingSphereAgentException;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Objects;
 
 /**
  * Agent path builder.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@Slf4j
 public final class AgentPathBuilder {
     
     @Getter
@@ -48,15 +47,22 @@ public final class AgentPathBuilder {
     
     private static File buildAgentPath() {
         String classResourcePath = String.join("", AgentPathBuilder.class.getName().replaceAll("\\.", "/"), ".class");
-        URL resource = ClassLoader.getSystemClassLoader().getResource(classResourcePath);
-        if (null != resource) {
-            String url = resource.toString();
-            log.debug("The beacon class location is {}", url);
-            int existFileInJarIndex = url.indexOf('!');
-            boolean isInJar = existFileInJarIndex > -1;
-            return isInJar ? getFileInJar(url, existFileInJarIndex) : getFileInResource(url, classResourcePath);
+        URL resource = Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource(classResourcePath), "Can not locate agent jar file");
+        String url = resource.toString();
+        int existFileInJarIndex = url.indexOf('!');
+        boolean isInJar = existFileInJarIndex > -1;
+        return isInJar ? getFileInJar(url, existFileInJarIndex) : getFileInResource(url, classResourcePath);
+    }
+    
+    private static File getFileInJar(final String url, final int fileInJarIndex) {
+        String realUrl = url.substring(url.indexOf("file:"), fileInJarIndex);
+        try {
+            File agentJarFile = new File(new URL(realUrl).toURI());
+            Preconditions.checkState(agentJarFile.exists(), "Can not locate agent jar file by URL `%s`", url);
+            return agentJarFile.getParentFile();
+        } catch (final MalformedURLException | URISyntaxException ex) {
+            throw new IllegalStateException(String.format("Can not locate agent jar file by URL `%s`", url));
         }
-        throw new ShardingSphereAgentException("Can not locate agent jar file.");
     }
     
     private static File getFileInResource(final String url, final String classResourcePath) {
@@ -65,18 +71,7 @@ public final class AgentPathBuilder {
         return new File(classLocation);
     }
     
-    private static File getFileInJar(final String url, final int fileInJarIndex) {
-        String realUrl = url.substring(url.indexOf("file:"), fileInJarIndex);
-        try {
-            File agentJarFile = new File(new URL(realUrl).toURI());
-            return agentJarFile.exists() ? agentJarFile.getParentFile() : null;
-        } catch (final MalformedURLException | URISyntaxException ex) {
-            log.error(String.format("Can not locate agent jar file by url %s", url), ex);
-            return null;
-        }
-    }
-    
     private static File buildAgentPluginPath() {
-        return new File(String.join("", agentPath.getPath(), "/plugins"));
+        return new File(String.join("/", agentPath.getPath(), "plugins"));
     }
 }
