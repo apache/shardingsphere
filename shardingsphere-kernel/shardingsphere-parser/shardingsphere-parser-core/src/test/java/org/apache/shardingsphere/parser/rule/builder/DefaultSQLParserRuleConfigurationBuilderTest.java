c4e2dfa6a7a6fb23b224c336a552aae23e773f07
abhinav-koppula <abhinav.koppula@gmail.com>
2022-09-29 14:57:23 +0530

    [Issue #20396]-Add unit test for DefaultSQLParserRuleConfigurationBuilder

---
+++ shardingsphere-kernel/shardingsphere-parser/shardingsphere-parser-core/src/test/java/org/apache/shardingsphere/parser/rule/builder/DefaultSQLParserRuleConfigurationBuilderTest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.parser.rule.builder;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.hamcrest.Matchers.is;
+
+import org.apache.shardingsphere.parser.config.SQLParserRuleConfiguration;
+import org.apache.shardingsphere.parser.constant.SQLParserOrder;
+import org.junit.Test;
+
+public final class DefaultSQLParserRuleConfigurationBuilderTest {
+    
+    @Test
+    public void assertBuild() {
+        SQLParserRuleConfiguration actualResult = new DefaultSQLParserRuleConfigurationBuilder().build();
+        assertFalse(actualResult.isSqlCommentParseEnabled());
+        assertThat(actualResult.getParseTreeCache().getInitialCapacity(), is(128));
+        assertThat(actualResult.getParseTreeCache().getMaximumSize(), is(1024L));
+        assertThat(actualResult.getSqlStatementCache().getInitialCapacity(), is(2000));
+        assertThat(actualResult.getSqlStatementCache().getMaximumSize(), is(65535L));
+    }
+    
+    @Test
+    public void assertGetOrder() {
+        assertThat(new DefaultSQLParserRuleConfigurationBuilder().getOrder(), is(SQLParserOrder.ORDER));
+    }
+    
+    @Test
+    public void assertGetTypeClass() {
+        assertThat(new DefaultSQLParserRuleConfigurationBuilder().getTypeClass().toString(), is(SQLParserRuleBuilder.class.toString()));
+    }
+}
