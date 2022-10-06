c43e66d3cb505b6909b91666568a3b4e53b8e8db
zhangliang <zhangliang@apache.org>
2022-10-07 00:54:19 +0800

    Refactor YamlPipelineProcessConfigurationSwapperTest

--- kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/yaml/process/YamlPipelineProcessConfigurationSwapperTest.java
+++ kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/yaml/process/YamlPipelineProcessConfigurationSwapperTest.java
@@ -18,28 +18,39 @@
 package org.apache.shardingsphere.data.pipeline.yaml.process;
 
 import org.apache.shardingsphere.data.pipeline.api.config.process.PipelineProcessConfiguration;
-import org.apache.shardingsphere.infra.util.yaml.YamlEngine;
+import org.apache.shardingsphere.data.pipeline.api.config.process.PipelineReadConfiguration;
+import org.apache.shardingsphere.data.pipeline.api.config.process.PipelineWriteConfiguration;
+import org.apache.shardingsphere.infra.config.algorithm.AlgorithmConfiguration;
 import org.apache.shardingsphere.infra.yaml.config.pojo.algorithm.YamlAlgorithmConfiguration;
 import org.junit.Test;
 
 import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNull;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNull;
 
 public final class YamlPipelineProcessConfigurationSwapperTest {
     
     @Test
-    public void assertSwap() {
-        YamlPipelineProcessConfiguration yamlConfig = createYamlPipelineProcessConfiguration();
-        YamlPipelineProcessConfigurationSwapper swapper = new YamlPipelineProcessConfigurationSwapper();
-        PipelineProcessConfiguration actualConfig = swapper.swapToObject(yamlConfig);
-        YamlPipelineProcessConfiguration actualYamlConfig = swapper.swapToYamlConfiguration(actualConfig);
-        assertThat(YamlEngine.marshal(actualYamlConfig), is(YamlEngine.marshal(yamlConfig)));
+    public void assertSwapToObject() {
+        PipelineProcessConfiguration actual = new YamlPipelineProcessConfigurationSwapper().swapToObject(createYamlConfiguration());
+        assertThat(actual.getRead().getWorkerThread(), is(40));
+        assertThat(actual.getRead().getBatchSize(), is(1000));
+        assertThat(actual.getRead().getShardingSize(), is(10000000));
+        assertThat(actual.getRead().getRateLimiter().getType(), is("INPUT"));
+        assertThat(actual.getRead().getRateLimiter().getProps().getProperty("batch-size"), is("1000"));
+        assertThat(actual.getRead().getRateLimiter().getProps().getProperty("qps"), is("50"));
+        assertThat(actual.getWrite().getWorkerThread(), is(40));
+        assertThat(actual.getWrite().getBatchSize(), is(1000));
+        assertThat(actual.getWrite().getRateLimiter().getType(), is("OUTPUT"));
+        assertThat(actual.getWrite().getRateLimiter().getProps().getProperty("batch-size"), is("1000"));
+        assertThat(actual.getWrite().getRateLimiter().getProps().getProperty("qps"), is("50"));
+        assertThat(actual.getStreamChannel().getType(), is("MEMORY"));
+        assertThat(actual.getStreamChannel().getProps().getProperty("block-queue-size"), is("10000"));
     }
     
-    private YamlPipelineProcessConfiguration createYamlPipelineProcessConfiguration() {
+    private YamlPipelineProcessConfiguration createYamlConfiguration() {
         YamlPipelineProcessConfiguration result = new YamlPipelineProcessConfiguration();
         Properties rateLimiterProps = new Properties();
         rateLimiterProps.setProperty("batch-size", "1000");
@@ -54,6 +65,32 @@ public final class YamlPipelineProcessConfigurationSwapperTest {
         streamChannelProps.setProperty("block-queue-size", "10000");
         result.setStreamChannel(new YamlAlgorithmConfiguration("MEMORY", streamChannelProps));
         return result;
+    }
+    
+    @Test
+    public void assertSwapToYamlConfiguration() {
+        Properties rateLimiterProps = new Properties();
+        rateLimiterProps.setProperty("batch-size", "1000");
+        rateLimiterProps.setProperty("qps", "50");
+        PipelineReadConfiguration readConfig = new PipelineReadConfiguration(40, 1000, 10000000, new AlgorithmConfiguration("INPUT", rateLimiterProps));
+        PipelineWriteConfiguration writeConfig = new PipelineWriteConfiguration(40, 1000, new AlgorithmConfiguration("OUTPUT", rateLimiterProps));
+        Properties streamChannelProps = new Properties();
+        streamChannelProps.setProperty("block-queue-size", "10000");
+        PipelineProcessConfiguration config = new PipelineProcessConfiguration(readConfig, writeConfig, new AlgorithmConfiguration("MEMORY", streamChannelProps));
+        YamlPipelineProcessConfiguration actual = new YamlPipelineProcessConfigurationSwapper().swapToYamlConfiguration(config);
+        assertThat(actual.getRead().getWorkerThread(), is(40));
+        assertThat(actual.getRead().getBatchSize(), is(1000));
+        assertThat(actual.getRead().getShardingSize(), is(10000000));
+        assertThat(actual.getRead().getRateLimiter().getType(), is("INPUT"));
+        assertThat(actual.getRead().getRateLimiter().getProps().getProperty("batch-size"), is("1000"));
+        assertThat(actual.getRead().getRateLimiter().getProps().getProperty("qps"), is("50"));
+        assertThat(actual.getWrite().getWorkerThread(), is(40));
+        assertThat(actual.getWrite().getBatchSize(), is(1000));
+        assertThat(actual.getWrite().getRateLimiter().getType(), is("OUTPUT"));
+        assertThat(actual.getWrite().getRateLimiter().getProps().getProperty("batch-size"), is("1000"));
+        assertThat(actual.getWrite().getRateLimiter().getProps().getProperty("qps"), is("50"));
+        assertThat(actual.getStreamChannel().getType(), is("MEMORY"));
+        assertThat(actual.getStreamChannel().getProps().getProperty("block-queue-size"), is("10000"));
     }
     
     @Test
