9047d93e72e80ced4290c57883294de75161853b
Gabriel Cunha <cunha.gabriel919@gmail.com>
2022-10-06 11:02:30 -0300

    Adding license

--- shardingsphere-kernel/shardingsphere-transaction/shardingsphere-transaction-core/src/test/java/org/apache/shardingsphere/transaction/yaml/swapper/YamlTransactionRuleConfigurationSwapperTest.java
+++ shardingsphere-kernel/shardingsphere-transaction/shardingsphere-transaction-core/src/test/java/org/apache/shardingsphere/transaction/yaml/swapper/YamlTransactionRuleConfigurationSwapperTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.transaction.yaml.swapper;
 
 import static org.junit.Assert.assertEquals;
@@ -8,9 +25,9 @@ import org.apache.shardingsphere.transaction.yaml.config.YamlTransactionRuleConf
 import org.junit.Test;
 
 public class YamlTransactionRuleConfigurationSwapperTest {
-
+    
     private final YamlTransactionRuleConfigurationSwapper swapper = new YamlTransactionRuleConfigurationSwapper();
-
+    
     @Test
     public void assertSwapToYamlConfiguration() {
         Properties props = new Properties();
@@ -20,5 +37,5 @@ public class YamlTransactionRuleConfigurationSwapperTest {
         assertEquals("provider", actual.getProviderType());
         assertEquals("default", actual.getDefaultType());
     }
-
+    
 }
