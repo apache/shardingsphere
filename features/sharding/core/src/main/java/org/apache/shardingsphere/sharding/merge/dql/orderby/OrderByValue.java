d8ca7ebce213dec77e1e597f908f0faeaf9a3116
ZhangCheng <flyingzc@outlook.com>
2022-10-11 12:23:10 +0800

    Fix

--- features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/orderby/OrderByValue.java
+++ features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/orderby/OrderByValue.java
@@ -117,9 +117,7 @@ public final class OrderByValue implements Comparable<OrderByValue> {
     public int compareTo(final OrderByValue o) {
         int i = 0;
         for (OrderByItem each : orderByItems) {
-            int result = CompareUtil.compareTo(i < orderValues.size() ? orderValues.get(i) : null,
-                    i < o.orderValues.size() ? o.orderValues.get(i) : null,
-                    each.getSegment().getOrderDirection(),
+            int result = CompareUtil.compareTo(orderValues.get(i), o.orderValues.get(i), each.getSegment().getOrderDirection(),
                     each.getSegment().getNullOrderDirection(), orderValuesCaseSensitive.get(i));
             if (0 != result) {
                 return result;
