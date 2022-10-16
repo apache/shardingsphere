01d59e8af4d4962e1402d3244b95e619d247ff87
Jacob953 <jacob953@csu.edu.cn>
2022-10-17 05:18:17 +0800

    fix: get test cases with github rest api

--- test/parser/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/engine/DynamicLoadingSQLParserParameterizedTest.java
+++ test/parser/src/main/java/org/apache/shardingsphere/test/sql/parser/parameterized/engine/DynamicLoadingSQLParserParameterizedTest.java
@@ -17,129 +17,106 @@
 
 package org.apache.shardingsphere.test.sql.parser.parameterized.engine;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.shardingsphere.sql.parser.api.CacheOption;
 import org.apache.shardingsphere.sql.parser.api.SQLParserEngine;
 import org.apache.shardingsphere.sql.parser.api.SQLVisitorEngine;
 import org.apache.shardingsphere.sql.parser.core.ParseASTNode;
-import org.apache.shardingsphere.sql.parser.exception.SQLParsingException;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.testng.asserts.IAssert;
-import org.testng.asserts.SoftAssert;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Arrays;
 import java.util.Properties;
 
 @RunWith(Parameterized.class)
-public final class DynamicLoadingSQLParserParameterizedTest extends SoftAssert {
+public class DynamicLoadingSQLParserParameterizedTest {
     
-    private static final String SQL_CASE_FILE_MARKER = "js-navigation-open Link--primary";
+    private static String sqlCasesOwner;
     
-    private static final String SQL_CASE_MARKER = "blob-code blob-code-inner js-file-line";
+    private static String sqlCasesRepo;
     
-    private static boolean inBracket;
+    private static String sqlCasesDirectory;
     
-    private static StringBuilder sqlCase;
+    private final String databaseType;
     
-    private static int sqlCaseId;
-    
-    private static String sqlCaseURL;
-    
-    private static String databaseType;
-
-    public DynamicLoadingSQLParserParameterizedTest() {
-        sqlCaseURL = System.getProperty("url");
-        databaseType = System.getProperty("database");
+    public DynamicLoadingSQLParserParameterizedTest(final String database, final String url) {
+        databaseType = database;
+        String[] patches = url.split("/", 8);
+        sqlCasesOwner = patches[3];
+        sqlCasesRepo = patches[4];
+        sqlCasesDirectory = patches[7];
     }
     
-    private static String[] getSqlCaseFiles(final String sqlCaseURL) throws IOException {
-        Document document = Jsoup.connect(sqlCaseURL).get();
-        return document.getElementsByClass(SQL_CASE_FILE_MARKER).text().split("\\s+");
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestCases() {
+        return Arrays.asList(new Object[][]{
+                {"MySQL", "https://github.com/mysql/mysql-server/tree/8.0/mysql-test/t"},
+                {"PostgreSQL", "https://github.com/postgres/postgres/tree/master/src/test/regress/sql"},
+        });
     }
     
-    private static Collection<Object[]> getTestParameters(final String databaseType) throws IOException {
-        Collection<Object[]> result = new LinkedList<>();
-        String[] sqlCaseFiles = getSqlCaseFiles(sqlCaseURL);
-        for (String each : sqlCaseFiles) {
-            sqlCaseId = 1;
-            inBracket = false;
-            sqlCase = new StringBuilder();
-            
-            Document document = Jsoup.connect(sqlCaseURL + "/" + each).get();
-            result.addAll(getSqlCases(document, each));
+    private static Collection<Object[]> getTestParameters() throws IOException, URISyntaxException {
+        Collection<Object[]> result = new ArrayList<>();
+        LinkedList<Map<String, String>> response = new RestTemplate().getForObject(
+                "https://api.github.com/repos/{owner}/{repo}/contents/{directory}", LinkedList.class,
+                sqlCasesOwner, sqlCasesRepo, sqlCasesDirectory);
+        for (Map<String, String> each : response) {
+            result.addAll(getSqlCases(each));
         }
         return result;
     }
     
-    private static Collection<Object[]> getSqlCases(final Document document, final String sqlCaseFile) {
-        Collection<Object[]> result = new LinkedList<>();
-        int fileLength = document.getElementsByClass(SQL_CASE_MARKER).size();
-        for (int line = 1; line <= fileLength; line++) {
-            StringBuilder curSqlCase = new StringBuilder().append(Objects.requireNonNull(document.getElementById("LC" + line)).text());
-            if (0 == curSqlCase.length() || '#' == curSqlCase.charAt(0) || '-' == curSqlCase.charAt(0)) {
+    private static Collection<Object[]> getSqlCases(final Map<String, String> elements) throws IOException, URISyntaxException {
+        Collection<Object[]> result = new ArrayList<>();
+        String sqlCaseFileName = elements.get("name");
+        String sqlCaseFileURL = elements.get("download_url");
+        String sqlCaseFileContent = IOUtils.toString(new URI(sqlCaseFileURL), Charset.defaultCharset());
+        String[] lines = sqlCaseFileContent.split("\n");
+        int sqlCaseId = 1;
+        StringBuilder sqlCase = new StringBuilder();
+        for (String each : lines) {
+            if (each.contains("--") || each.contains("#")) {
                 continue;
             }
-            char lastChar = curSqlCase.charAt(curSqlCase.length() - 1);
-            if (';' != lastChar && !inBracket) {
-                result.add(getCurSqlCase(sqlCaseFile, curSqlCase));
+            sqlCase.append(each);
+            if (each.contains(";") || each.contains("}")) {
+                result.add(new Object[]{
+                        sqlCaseFileName + sqlCaseId, sqlCase.toString(),
+                });
                 sqlCaseId++;
                 sqlCase = new StringBuilder();
-                continue;
-            }
-            if (0 != sqlCase.length()) {
-                sqlCase.append(" ");
-            }
-            sqlCase.append(curSqlCase);
-            if ('{' == lastChar) {
-                inBracket = true;
-            } else if ('}' == lastChar) {
-                result.add(getSqlCase(databaseType, sqlCaseFile, sqlCase));
-                sqlCase = new StringBuilder();
-                sqlCaseId++;
-                inBracket = false;
             }
         }
         return result;
-    }
-    
-    private static Object[] getCurSqlCase(final String sqlCaseFile, final StringBuilder curSqlCase) {
-        return sqlCase.length() > 0 ? getSqlCase(databaseType, sqlCaseFile, sqlCase) : getSqlCase(databaseType, sqlCaseFile, curSqlCase);
-    }
-    
-    private static Object[] getSqlCase(final String databaseType, final String sqlCaseFile, final StringBuilder sqlCase) {
-        Object[] parameters = new Object[3];
-        parameters[0] = sqlCaseFile.split("\\.")[0] + sqlCaseId;
-        parameters[1] = sqlCase.toString();
-        parameters[2] = databaseType;
-        return parameters;
     }
     
     @Test
-    public void assertDynamicLoadingSQL() throws IOException {
-        Collection<Object[]> testParameters = getTestParameters(databaseType);
-        DynamicLoadingSQLParserParameterizedTest softAssert = new DynamicLoadingSQLParserParameterizedTest();
+    public void assertDynamicLoadingSQL() throws IOException, URISyntaxException {
+        Collection<Object[]> testParameters = getTestParameters();
+        Collection<String> result = new LinkedList<>();
         CacheOption cacheOption = new CacheOption(128, 1024L);
+        String databaseType = "H2".equals(this.databaseType) ? "MySQL" : this.databaseType;
         for (Object[] each : testParameters) {
             String sql = each[1].toString();
-            try {
-                ParseASTNode parseContext = new SQLParserEngine(databaseType, cacheOption).parse(sql, false);
-                SQLStatement sqlStatement = new SQLVisitorEngine(databaseType, "STATEMENT", true, new Properties()).visit(parseContext);
-                softAssert.assertEquals(sqlStatement, parseContext, "<sql-case id=" + each[0] + " value=" + each[1] + " db-types=" + each[2] + " />");
-            } catch (SQLParsingException | ClassCastException ignored) {
-                softAssert.assertTrue(false, "<sql-case id=" + each[0] + " value=" + each[1] + " db-types=" + each[2] + " />");
+            ParseASTNode parseContext = new SQLParserEngine(databaseType, cacheOption).parse(sql, false);
+            SQLStatement sqlStatement = new SQLVisitorEngine(databaseType, "STATEMENT", true, new Properties()).visit(parseContext);
+            if (!parseContext.toString().equals(sqlStatement.toString())) {
+                result.add("<sql-case id=" + each[0] + " value=" + sql + " db-types=" + databaseType + " />");
             }
         }
-    }
-
-    @Override
-    public void onAssertFailure(final IAssert<?> ignored, final AssertionError ex) {
-        System.out.println(ex);
+        result.forEach(System.out::println);
+        Assert.assertFalse(result.isEmpty());
     }
 }
