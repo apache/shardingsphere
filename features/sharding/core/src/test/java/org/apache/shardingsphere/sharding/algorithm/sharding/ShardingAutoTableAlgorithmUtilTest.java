83c5b1a0ba8b13f1dd7bdf83d305562f25a6c3b1
Karthick B <karthick.b@tesco.com>
2022-10-18 15:46:49 +0530

    [ISSUE #21569] Fix checkstyle errors

--- features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/algorithm/sharding/ShardingAutoTableAlgorithmUtilTest.java
+++ features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/algorithm/sharding/ShardingAutoTableAlgorithmUtilTest.java
@@ -1,7 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.sharding.algorithm.sharding;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -11,42 +29,43 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ShardingAutoTableAlgorithmUtilTest {
-
+    
     private Collection<String> collection;
+    
     private DataNodeInfo dataNodeInfo;
-
+    
     @Before
-    public void setup(){
+    public void setup() {
         collection = new ArrayList<>();
         collection.add("PREFIX----SUFFIX");
         collection.add("PREFIXSUFFIXSTRING");
         collection.add("PREFIX----------");
-
+        
         final String prefix = "PREFIX";
         final int suffixMinLength = 10;
         final char paddingChar = '-';
         dataNodeInfo = new DataNodeInfo(prefix, suffixMinLength, paddingChar);
     }
-
+    
     @Test
-    public void assertFindMatchedTargetNameForValidInputs(){
-
-        Optional<String> output = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection,"SUFFIX",dataNodeInfo);
-        assert(output.isPresent());
+    public void assertFindMatchedTargetNameForValidInputs() {
+        
+        Optional<String> output = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection, "SUFFIX", dataNodeInfo);
+        assertTrue(output.isPresent());
         assertEquals("PREFIX----SUFFIX", output.get());
-
-        Optional<String> output1 = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection,"SUFFIXSTRING",dataNodeInfo);
-        assert(output1.isPresent());
+        
+        Optional<String> output1 = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection, "SUFFIXSTRING", dataNodeInfo);
+        assertTrue(output1.isPresent());
         assertEquals("PREFIXSUFFIXSTRING", output1.get());
-
-        Optional<String> output2 = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection,"",dataNodeInfo);
-        assert(output2.isPresent());
+        
+        Optional<String> output2 = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection, "", dataNodeInfo);
+        assertTrue(output2.isPresent());
         assertEquals("PREFIX----------", output2.get());
     }
-
+    
     @Test
-    public void assertFindMatchedTargetNameNonExistingInput(){
-        Optional<String> output = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection,"NONEXISTINGSUFFIX",dataNodeInfo);
+    public void assertFindMatchedTargetNameNonExistingInput() {
+        Optional<String> output = ShardingAutoTableAlgorithmUtil.findMatchedTargetName(collection, "NONEXISTINGSUFFIX", dataNodeInfo);
         assertFalse(output.isPresent());
     }
 }
