69ec8d70885914c6cfee0fd1ad2718dd020d882e
Jacob953 <jacob953@csu.edu.cn>
2022-10-22 20:25:40 +0800

    feat: add MySQL & Postgre dynamic loading sql test

---
+++ sql-parser/dialect/postgresql/src/test/java/org/apache/shardingsphere/sql/parser/postgresql/DynamicLoadingPostgreParserParameterizedTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.parser.postgresql;
+
+import org.apache.shardingsphere.test.runner.ShardingSphereParallelTestParameterized;
+import org.apache.shardingsphere.test.sql.parser.parameterized.engine.DynamicLoadingSQLParserParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+@RunWith(ShardingSphereParallelTestParameterized.class)
+public final class DynamicLoadingPostgreParserParameterizedTest extends DynamicLoadingSQLParserParameterizedTest {
+    
+    private static final String sqlCaseURL = "https://github.com/postgres/postgres/tree/master/src/test/regress/sql";
+    
+    public DynamicLoadingPostgreParserParameterizedTest(final String sqlCaseId, final String sqlCaseValue, final String databaseType) {
+        super(sqlCaseId, sqlCaseValue, databaseType);
+    }
+    
+    @Parameters(name = "{1} ({2}) -> {0}")
+    public static Collection<Object[]> getTestParameters() throws IOException, URISyntaxException, ParserConfigurationException, TransformerException {
+        return DynamicLoadingSQLParserParameterizedTest.getTestParameters("PostgreSQL", sqlCaseURL);
+    }
+}
