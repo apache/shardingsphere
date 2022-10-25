62cc6c63eeb1c7b0810bf9c152fd2e7e32a35db9
duanzhengqiang <duanzhengqiang@apache.org>
2022-10-25 19:41:48 +0800

    Remove databaseType in ShardingSphereResourceMetaData and modify unit test

--- proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/admin/postgresql/executor/SelectTableExecutor.java
+++ proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/admin/postgresql/executor/SelectTableExecutor.java
@@ -55,7 +55,7 @@ public final class SelectTableExecutor extends DefaultDatabaseMetadataExecutor {
     @Override
     protected void initDatabaseData(final String databaseName) {
         ShardingSphereDatabase database = ProxyContext.getInstance().getDatabase(databaseName);
-        String schemaName = DatabaseTypeEngine.getDefaultSchemaName(database.getResourceMetaData().getDatabaseType(), databaseName);
+        String schemaName = DatabaseTypeEngine.getDefaultSchemaName(database.getProtocolType(), databaseName);
         tableNames = new ArrayList<>(database.getSchema(schemaName).getAllTableNames());
     }
     
