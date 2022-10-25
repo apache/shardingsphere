d1a095fec13a5de16d4198fb3a080bd5b3d7b159
zhaojinchao <zhaojinchao@apache.org>
2022-10-26 00:04:23 +0800

    Refactor dataSource event listener logic and fix it (#21695)
    * Fix UNREGISTER STORAGE UNIT fail when has one STORAGE
    
    * Modify logic
    
    * Fix IT
    
    * Adjust xml style
    
    * Replace to LinkedHashMap

--- mode/core/src/test/java/org/apache/shardingsphere/mode/manager/switcher/ResourceSwitchManagerTest.java
+++ mode/core/src/test/java/org/apache/shardingsphere/mode/manager/switcher/ResourceSwitchManagerTest.java
@@ -43,6 +43,19 @@ public final class ResourceSwitchManagerTest {
         assertStaleDataSources(dataSourceMap);
     }
     
+    @Test
+    public void assertCreateByAlterDataSourceProps() throws InterruptedException {
+        Map<String, DataSource> dataSourceMap = new HashMap<>(3, 1);
+        dataSourceMap.put("ds_0", new MockedDataSource());
+        dataSourceMap.put("ds_1", new MockedDataSource());
+        SwitchingResource actual = new ResourceSwitchManager().createByAlterDataSourceProps(new ShardingSphereResourceMetaData("sharding_db", dataSourceMap), Collections.emptyMap());
+        assertThat(actual.getNewDataSources().size(), is(0));
+        assertThat(actual.getStaleDataSources().size(), is(2));
+        actual.closeStaleDataSources();
+        assertStaleDataSource((MockedDataSource) dataSourceMap.get("ds_0"));
+        assertStaleDataSource((MockedDataSource) dataSourceMap.get("ds_1"));
+    }
+    
     private Map<String, DataSource> createDataSourceMap() {
         Map<String, DataSource> result = new HashMap<>(3, 1);
         result.put("not_change", new MockedDataSource());
