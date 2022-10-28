789ab15b6c31475d1c532fd5b9d874ac7facc15a
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2022-10-28 16:32:58 +0530

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -2,19 +2,17 @@
 @REM Licensed to the Apache Software Foundation (ASF) under one or more
 @REM contributor license agreements.  See the NOTICE file distributed with
 @REM this work for additional information regarding copyright ownership.
-@REM The ASF licenses this file to you under the Apache License
-@REM , Version 2.0 (the "License"); you may not use this file
-@REM except in compliance with the License.
-@REM You may obtain a copy of the License at
+@REM The ASF licenses this file to You under the Apache License, Version 2.0
+@REM (the "License"); you may not use this file except in compliance with
+@REM the License.  You may obtain a copy of the License at
 @REM
-@REM    http://www.apache.org/licenses/LICENSE-2.0
+@REM     http://www.apache.org/licenses/LICENSE-2.0
 @REM
-@REM Unless required by applicable law or agreed to in writing,
-@REM software distributed under the License is distributed on an
-@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-@REM KIND, either express or implied.  See the License for the
-@REM specific language governing permissions and limitations
-@REM under the License.
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
 @REM ----------------------------------------------------------------------------
 
 @REM ----------------------------------------------------------------------------
