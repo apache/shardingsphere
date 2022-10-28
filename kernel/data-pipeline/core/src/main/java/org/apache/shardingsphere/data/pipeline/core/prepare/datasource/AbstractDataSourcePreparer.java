1c352d74d19a124b91cda07ee49735b14368a69b
Hongsheng Zhong <zhonghongsheng@apache.org>
2022-10-28 20:18:51 +0800

    Improve code style; Add TODO

--- kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/prepare/datasource/AbstractDataSourcePreparer.java
+++ kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/prepare/datasource/AbstractDataSourcePreparer.java
@@ -49,6 +49,7 @@ public abstract class AbstractDataSourcePreparer implements DataSourcePreparer {
     
     private static final Pattern PATTERN_CREATE_TABLE = Pattern.compile("CREATE\\s+TABLE\\s+", Pattern.CASE_INSENSITIVE);
     
+    // TODO it's just used for openGauss
     private static final String[] IGNORE_EXCEPTION_MESSAGE = {"multiple primary keys for table", "already exists"};
     
     @Override
@@ -83,7 +84,7 @@ public abstract class AbstractDataSourcePreparer implements DataSourcePreparer {
                 statement.execute(sql);
             }
         } catch (final SQLException ex) {
-            log.error("create schema failed, {}", ex.getMessage());
+            log.warn("create schema failed, error: {}", ex.getMessage());
         }
     }
     
