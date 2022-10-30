0963ce4a36761f3001044437c15feb9c7e4f767c
gavin.peng <gavin.peng@ximalaya.com>
2022-10-30 12:22:34 +0800

    one:remove the code with lock no use two:support use local consul agent and improves unit test performance

--- mode/type/cluster/repository/provider/consul/src/main/java/org/apache/shardingsphere/mode/repository/cluster/consul/ConsulRepository.java
+++ mode/type/cluster/repository/provider/consul/src/main/java/org/apache/shardingsphere/mode/repository/cluster/consul/ConsulRepository.java
@@ -17,6 +17,7 @@
 
 package org.apache.shardingsphere.mode.repository.cluster.consul;
 
+import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.ConsulRawClient;
 import com.ecwid.consul.v1.QueryParams;
 import com.ecwid.consul.v1.Response;
@@ -39,12 +40,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Registry repository of Consul.
  */
 public class ConsulRepository implements ClusterPersistRepository {
+    
+    private static final ScheduledThreadPoolExecutor SESSION_FLUSH_EXECUTOR = new ScheduledThreadPoolExecutor(2);
     
     private ShardingSphereConsulClient consulClient;
     
@@ -56,7 +61,14 @@ public class ConsulRepository implements ClusterPersistRepository {
     
     @Override
     public void init(final ClusterPersistRepositoryConfiguration config, final InstanceMetaData instanceMetaData) {
-        consulClient = new ShardingSphereConsulClient(new ConsulRawClient(config.getServerLists()));
+        ConsulRawClient rawClient;
+        String serverList = config.getServerLists();
+        if (serverList == null || serverList.length() <= 0) {
+            rawClient = new ConsulRawClient();
+        } else {
+            rawClient = new ConsulRawClient(serverList);
+        }
+        consulClient = new ShardingSphereConsulClient(rawClient);
         consulProps = new ConsulProperties(config.getProps());
         consulDistributedLockHolder = new ConsulDistributedLockHolder(consulClient, consulProps);
         watchKeyMap = new HashMap<>(6, 1);
@@ -76,7 +88,8 @@ public class ConsulRepository implements ClusterPersistRepository {
     
     @Override
     public boolean isExisted(final String key) {
-        return false;
+        Response<GetValue> response = consulClient.getKVValue(key);
+        return response.getValue() == null ? false : true;
     }
     
     @Override
@@ -86,7 +99,7 @@ public class ConsulRepository implements ClusterPersistRepository {
     
     @Override
     public void update(final String key, final String value) {
-        // TODO
+        consulClient.setKVValue(key, value);
     }
     
     @Override
@@ -106,7 +119,7 @@ public class ConsulRepository implements ClusterPersistRepository {
         PutParams putParams = new PutParams();
         putParams.setAcquireSession(sessionId);
         consulClient.setKVValue(key, value, putParams);
-        consulDistributedLockHolder.generatorFlushSessionTtlTask(consulClient, sessionId);
+        generatorFlushSessionTtlTask(consulClient, sessionId);
     }
     
     private NewSession createNewSession(final String key) {
@@ -185,6 +198,16 @@ public class ConsulRepository implements ClusterPersistRepository {
     
     private void fireDataChangeEvent(final GetValue getValue, final DataChangedEventListener listener, final DataChangedEvent.Type type) {
         listener.onChange(new DataChangedEvent(getValue.getKey(), getValue.getValue(), type));
+    }
+    
+    /**
+     * Flush session by update TTL.
+     *
+     * @param consulClient consul client
+     * @param sessionId session id
+     */
+    public void generatorFlushSessionTtlTask(final ConsulClient consulClient, final String sessionId) {
+        SESSION_FLUSH_EXECUTOR.scheduleAtFixedRate(() -> consulClient.renewSession(sessionId, QueryParams.DEFAULT), 5L, 10L, TimeUnit.SECONDS);
     }
     
     @Override
--- mode/type/cluster/repository/provider/consul/src/test/java/org/apache/shardingsphere/mode/repository/cluster/consul/ConsulRepositoryTest.java
+++ mode/type/cluster/repository/provider/consul/src/test/java/org/apache/shardingsphere/mode/repository/cluster/consul/ConsulRepositoryTest.java
@@ -26,10 +26,10 @@ import lombok.SneakyThrows;
 import org.apache.shardingsphere.mode.repository.cluster.consul.lock.ConsulDistributedLockHolder;
 import org.apache.shardingsphere.mode.repository.cluster.consul.props.ConsulProperties;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
 import org.mockito.internal.configuration.plugins.Plugins;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.plugins.MemberAccessor;
@@ -69,16 +69,16 @@ public final class ConsulRepositoryTest {
     @Mock
     private Response<Boolean> responseBoolean;
     
-    // @Mock
-    // private Response<String> sessionResponse;
+    @Mock
+    private Response<String> sessionResponse;
     
     @Mock
     private GetValue getValue;
     
-    // @Mock
-    // private List<GetValue> getValueList;
-    //
-    // private long index = 123456L;
+    @Mock
+    private List<GetValue> getValueList;
+    
+    private long index = 123456L;
     
     @Before
     public void setUp() {
@@ -90,12 +90,12 @@ public final class ConsulRepositoryTest {
     private void setClient() {
         when(client.getKVValue(any(String.class))).thenReturn(response);
         when(response.getValue()).thenReturn(getValue);
-        // when(client.getKVValues(any(String.class), any(QueryParams.class))).thenReturn(responseGetValueList);
+        when(client.getKVValues(any(String.class), any(QueryParams.class))).thenReturn(responseGetValueList);
         when(client.getKVKeysOnly(any(String.class))).thenReturn(responseList);
-        // when(client.sessionCreate(any(NewSession.class), any(QueryParams.class))).thenReturn(sessionResponse);
-        // when(sessionResponse.getValue()).thenReturn("12323ddsf3sss");
-        // when(responseGetValueList.getConsulIndex()).thenReturn(index++);
-        // when(responseGetValueList.getValue()).thenReturn(getValueList);
+        when(client.sessionCreate(any(NewSession.class), any(QueryParams.class))).thenReturn(sessionResponse);
+        when(sessionResponse.getValue()).thenReturn("12323ddsf3sss");
+        when(responseGetValueList.getConsulIndex()).thenReturn(index++);
+        when(responseGetValueList.getValue()).thenReturn(getValueList);
         when(client.setKVValue(any(String.class), any(String.class))).thenReturn(responseBoolean);
         Plugins.getMemberAccessor().set(repository.getClass().getDeclaredField("consulClient"), repository, client);
         Plugins.getMemberAccessor().set(repository.getClass().getDeclaredField("consulDistributedLockHolder"), repository, mock(ConsulDistributedLockHolder.class));
@@ -109,7 +109,7 @@ public final class ConsulRepositoryTest {
     }
     
     @Test
-    public void assertGetKey() {
+    public void assertDirectlyKey() {
         repository.getDirectly("key");
         verify(client).getKVValue("key");
         verify(response).getValue();
@@ -134,17 +134,13 @@ public final class ConsulRepositoryTest {
     }
     
     @Test
-    @Ignore
     public void assertPersistEphemeral() throws InterruptedException {
         repository.persistEphemeral("key1", "value1");
         verify(client).sessionCreate(any(NewSession.class), any(QueryParams.class));
         verify(client).setKVValue(any(String.class), any(String.class), any(PutParams.class));
-        Thread.sleep(6000L);
-        verify(client).renewSession(any(String.class), any(QueryParams.class));
     }
     
     @Test
-    @Ignore
     public void assertWatchUpdate() throws InterruptedException {
         final String key = "sharding/key";
         final String k1 = "sharding/key/key1";
@@ -157,12 +153,18 @@ public final class ConsulRepositoryTest {
         repository.watch(key, event -> {
         });
         client.setKVValue(k1, "value1-1");
-        verify(client, atLeastOnce()).getKVValues(any(String.class), any(QueryParams.class));
-        Thread.sleep(10000L);
+        while (true) {
+            Thread.sleep(100L);
+            try {
+                verify(client, atLeastOnce()).getKVValues(any(String.class), any(QueryParams.class));
+                break;
+            } catch (MockitoException e) {
+                continue;
+            }
+        }
     }
     
     @Test
-    @Ignore
     public void assertWatchDelete() throws InterruptedException {
         final String key = "sharding/key";
         final String k1 = "sharding/key/key1";
@@ -178,8 +180,15 @@ public final class ConsulRepositoryTest {
         repository.watch(key, event -> {
         });
         client.deleteKVValue(k2);
-        verify(client, atLeastOnce()).getKVValues(any(String.class), any(QueryParams.class));
-        Thread.sleep(10000L);
+        while (true) {
+            Thread.sleep(100L);
+            try {
+                verify(client, atLeastOnce()).getKVValues(any(String.class), any(QueryParams.class));
+                break;
+            } catch (MockitoException e) {
+                continue;
+            }
+        }
     }
     
     @Test
