5035a965f88f143b143a22fe90d51d232b0b8bc2
Jacob953 <jacob953@csu.edu.cn>
2022-11-09 11:57:34 +0800

    feat: add loader of GitHub

---
+++ test/integration-test/sql-parser/src/test/java/org/apache/shardingsphere/sql/parser/loader/DynamicLoadingSQLCaseGitHubLoaderIT.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.parser.loader;
+
+import org.apache.shardingsphere.sql.parser.base.DynamicLoadingSQLParserParameterizedTest;
+import org.apache.shardingsphere.sql.parser.result.CSVResultGenerator;
+
+import java.net.URI;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class DynamicLoadingSQLCaseGitHubLoaderIT extends DynamicLoadingSQLParserParameterizedTest implements DynamicLoadingSQLCaseLoaderStrategyIT {
+
+    public DynamicLoadingSQLCaseGitHubLoaderIT() {
+        super(null, null, null, new CSVResultGenerator(null));
+    }
+
+    /**
+     * Get test parameters.
+     *
+     * @param sqlCaseURI the URI of sql case
+     *
+     * @return Test cases from GitHub.
+     **/
+    public Collection<Object[]> getTestParameters(final URI sqlCaseURI) {
+        Collection<Object[]> result = new LinkedList<>();
+        for (Map<String, String> each : getResponse("https://api.github.com/repos/", sqlCaseURI)) {
+            String sqlCaseFileName = each.get("name").split("\\.")[0];
+            String sqlCaseFileContent = getContent(URI.create(each.get("download_url")));
+            result.addAll(getSQLCases(sqlCaseFileName, sqlCaseFileContent));
+        }
+        if (result.isEmpty()) {
+            result.add(new Object[]{null, null});
+        }
+        return result;
+    }
+}
