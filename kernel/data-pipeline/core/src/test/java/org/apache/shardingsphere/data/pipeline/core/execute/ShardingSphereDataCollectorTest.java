bddc5a42b11c9902bc1ee762a4d61d34bc888f54
tuichenchuxin <chuxinchen.x@qq.com>
2022-11-11 15:14:30 +0800

    Refactor ShardingSphereDataScheduleCollector.

--- kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/core/execute/ShardingSphereDataCollectorTest.java
+++ kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/core/execute/ShardingSphereDataCollectorTest.java
@@ -36,7 +36,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class ShardingSphereDataCollectorTest {
+public final class ShardingSphereDataCollectorTest {
     
     @Test
     public void assertCollect() throws InterruptedException {
