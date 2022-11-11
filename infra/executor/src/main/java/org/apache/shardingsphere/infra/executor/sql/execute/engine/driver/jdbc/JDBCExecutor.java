3a919712994f1568146bc7da4c27512f1d823641
ZhangCheng <flyingzc@outlook.com>
2022-11-11 16:35:58 +0800

    Fix

--- infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/execute/engine/driver/jdbc/JDBCExecutor.java
+++ infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/execute/engine/driver/jdbc/JDBCExecutor.java
@@ -35,6 +35,7 @@ public final class JDBCExecutor {
     
     private final ExecutorEngine executorEngine;
     
+    // TODO add transaction type to ConnectionContext
     private final ConnectionContext connectionContext;
     
     /**
