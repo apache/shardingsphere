85541320133e8049027e922557636fe32c58625c
Chuxin Chen <chuxinchen.x@qq.com>
2022-11-11 16:09:33 +0800

    Refactor ShardingSphereDataScheduleCollector. (#22076)
    * Refactor ShardingSphereDataScheduleCollector.
    
    * Refactor ShardingSphereDataScheduleCollector.
    
    * Refactor ShardingSphereDataScheduleCollector.

---
+++ kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/core/execute/ShardingSphereDataCollectorFixture.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.data.pipeline.core.execute;
+
+import org.apache.shardingsphere.data.pipeline.spi.data.collector.ShardingSphereDataCollector;
+import org.apache.shardingsphere.infra.metadata.data.ShardingSphereRowData;
+import org.apache.shardingsphere.infra.metadata.data.ShardingSphereTableData;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
+import org.apache.shardingsphere.infra.metadata.database.schema.decorator.model.ShardingSphereTable;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+
+/**
+ * ShardingSphere data collector fixture.
+ */
+public final class ShardingSphereDataCollectorFixture implements ShardingSphereDataCollector {
+    
+    @Override
+    public Optional<ShardingSphereTableData> collect(final ShardingSphereDatabase shardingSphereDatabase, final ShardingSphereTable table) throws SQLException {
+        ShardingSphereTableData shardingSphereTableData = new ShardingSphereTableData("test_table", Collections.emptyList());
+        shardingSphereTableData.getRows().add(new ShardingSphereRowData(Arrays.asList("1", "2")));
+        return Optional.of(shardingSphereTableData);
+    }
+    
+    @Override
+    public String getType() {
+        return "test_table";
+    }
+}
