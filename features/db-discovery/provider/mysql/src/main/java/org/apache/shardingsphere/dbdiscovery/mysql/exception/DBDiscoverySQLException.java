497757bb26285530a1e6a0e40bcb7c786861b4a5
Liang Zhang <zhangliang@apache.org>
2022-11-12 11:00:46 +0800

    Rename variable from arguments to args (#22095)

--- features/db-discovery/provider/mysql/src/main/java/org/apache/shardingsphere/dbdiscovery/mysql/exception/DBDiscoverySQLException.java
+++ features/db-discovery/provider/mysql/src/main/java/org/apache/shardingsphere/dbdiscovery/mysql/exception/DBDiscoverySQLException.java
@@ -29,7 +29,7 @@ public abstract class DBDiscoverySQLException extends FeatureSQLException {
     
     private static final int FEATURE_CODE = 3;
     
-    public DBDiscoverySQLException(final SQLState sqlState, final int errorCode, final String reason, final Object... messageArguments) {
-        super(sqlState, FEATURE_CODE, errorCode, reason, messageArguments);
+    public DBDiscoverySQLException(final SQLState sqlState, final int errorCode, final String reason, final Object... messageArgs) {
+        super(sqlState, FEATURE_CODE, errorCode, reason, messageArgs);
     }
 }
