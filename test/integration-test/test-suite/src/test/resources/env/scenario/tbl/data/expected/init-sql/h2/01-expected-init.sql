72ad987ea0ce0f140eaee803d4a874304e8248c9
吴伟杰 <wuweijie@apache.org>
2022-11-19 00:09:37 +0800

    Fix JDBC tests

--- test/integration-test/test-suite/src/test/resources/env/scenario/tbl/data/expected/init-sql/h2/01-expected-init.sql
+++ test/integration-test/test-suite/src/test/resources/env/scenario/tbl/data/expected/init-sql/h2/01-expected-init.sql
@@ -28,5 +28,6 @@ CREATE TABLE t_single_table (single_id INT NOT NULL, id INT NOT NULL, status VAR
 CREATE TABLE t_broadcast_table (id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (id));
 CREATE TABLE t_order_federate_sharding (order_id_sharding INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (order_id_sharding));
 CREATE TABLE t_order_item_federate_sharding (item_id INT NOT NULL, order_id INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, remarks VARCHAR(45) NULL, PRIMARY KEY (item_id));
+CREATE TABLE t_single_table_integer (col_bigint BIGINT NOT NULL, col_int INT NOT NULL, col_mediumint MEDIUMINT NOT NULL, col_smallint SMALLINT NOT NULL, col_tinyint TINYINT NOT NULL);
 
 CREATE INDEX order_index_t_order ON t_order (order_id);
