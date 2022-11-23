afb65a1b82be325a4e7907822254875efdb3627e
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2022-11-23 16:58:05 +0530

    Update MetadataContextsFactoryAdvice.java

--- agent/plugins/logging/base/src/main/java/org/apache/shardingsphere/agent/plugin/logging/base/advice/MetadataContextsFactoryAdvice.java
+++ agent/plugins/logging/base/src/main/java/org/apache/shardingsphere/agent/plugin/logging/base/advice/MetadataContextsFactoryAdvice.java
@@ -28,7 +28,7 @@ import java.lang.reflect.Method;
  * Schema metadata loader advice.
  */
 @Slf4j
-public final class MetaDataContextsFactoryAdvice implements ClassStaticMethodAroundAdvice {
+public final class MetadataContextsFactoryAdvice implements ClassStaticMethodAroundAdvice {
     
     @Override
     public void beforeMethod(final Class<?> clazz, final Method method, final Object[] args, final MethodInvocationResult result) {
