6d70c4c125fe6daafcdbb5071f9d146fe8b30033
吴伟杰 <wuweijie@apache.org>
2022-11-25 13:33:12 +0800

    Add new IT scenario passthrough for ShardingSphere-Proxy (#22401)
    * Add new IT scenario passthrough for ShardingSphere-Proxy
    
    * Update IT actions

---
+++ test/integration-test/test-suite/src/test/resources/env/scenario/passthrough/data/actual/init-sql/postgresql/01-actual-init.sql
@@ -0,0 +1,25 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+CREATE DATABASE passthrough;
+
+GRANT ALL PRIVILEGES ON DATABASE passthrough TO test_user;
+
+\c passthrough
+
+DROP TABLE IF EXISTS t_data_type_integer;
+CREATE TABLE t_data_type_integer (id INT PRIMARY KEY, col_bigint BIGINT NOT NULL, col_int INT NOT NULL, col_mediumint INT4 NOT NULL, col_smallint SMALLINT NOT NULL, col_tinyint INT2 NOT NULL);
