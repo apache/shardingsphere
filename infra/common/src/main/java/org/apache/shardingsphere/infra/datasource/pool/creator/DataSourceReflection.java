b192148dcd3661c6f07219f97657b732e0c28689
zhaojinchao <zjcnb0215@gmail.com>
2022-11-30 23:48:55 +0800

    Fix datasource properties comparison not right when properties are map type

--- infra/common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/creator/DataSourceReflection.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/creator/DataSourceReflection.java
@@ -90,12 +90,16 @@ public final class DataSourceReflection {
             String fieldName = getGetterFieldName(each, prefix);
             if (GENERAL_CLASS_TYPES.contains(each.getReturnType()) && !SKIPPED_PROPERTY_KEYS.contains(fieldName)) {
                 Object fieldValue = each.invoke(dataSource);
-                if (null != fieldValue) {
+                if (null != fieldValue && isEmptyProperties(fieldValue)) {
                     result.put(fieldName, fieldValue);
                 }
             }
         }
         return result;
+    }
+    
+    private boolean isEmptyProperties(final Object fieldValue) {
+        return !(fieldValue instanceof Properties) || !((Properties) fieldValue).isEmpty();
     }
     
     private Collection<Method> findAllGetterMethods(final String methodPrefix) {
