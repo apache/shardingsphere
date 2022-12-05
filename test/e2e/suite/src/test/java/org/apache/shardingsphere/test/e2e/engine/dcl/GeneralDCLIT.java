a8bbdcd530341a9f796548acece46502f0c03a43
Liang Zhang <zhangliang@apache.org>
2022-12-05 21:15:57 +0800

    Rename package name of suite test (#22678)

---
+++ test/e2e/suite/src/test/java/org/apache/shardingsphere/test/e2e/engine/dcl/GeneralDCLIT.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.test.e2e.engine.dcl;
+
+import org.apache.shardingsphere.test.e2e.cases.SQLCommandType;
+import org.apache.shardingsphere.test.e2e.cases.SQLExecuteType;
+import org.apache.shardingsphere.test.e2e.framework.param.array.ITTestParameterFactory;
+import org.apache.shardingsphere.test.e2e.framework.param.model.AssertionTestParameter;
+import org.apache.shardingsphere.test.e2e.framework.runner.ParallelRunningStrategy;
+import org.apache.shardingsphere.test.e2e.framework.runner.ParallelRunningStrategy.ParallelLevel;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.util.Collection;
+
+@ParallelRunningStrategy(ParallelLevel.SCENARIO)
+public final class GeneralDCLIT extends BaseDCLIT {
+    
+    public GeneralDCLIT(final AssertionTestParameter testParameter) {
+        super(testParameter);
+    }
+    
+    @Parameters(name = "{0}")
+    public static Collection<AssertionTestParameter> getTestParameters() {
+        return ITTestParameterFactory.getAssertionTestParameters(SQLCommandType.DCL);
+    }
+    
+    @Test
+    public void assertExecuteUpdate() throws SQLException, ParseException {
+        String sql = getSQL();
+        try (Connection connection = getTargetDataSource().getConnection()) {
+            if (SQLExecuteType.Literal == getSqlExecuteType()) {
+                try (Statement statement = connection.createStatement()) {
+                    statement.executeUpdate(sql);
+                }
+            } else {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void assertExecute() throws SQLException, ParseException {
+        String sql = getSQL();
+        try (Connection connection = getTargetDataSource().getConnection()) {
+            if (SQLExecuteType.Literal == getSqlExecuteType()) {
+                try (Statement statement = connection.createStatement()) {
+                    statement.execute(sql);
+                }
+            } else {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.execute();
+                }
+            }
+        }
+    }
+}
