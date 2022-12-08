dbb59cc5c0e097ccc4e0e3830d4ae0fca0454140
zhangliang <zhangliang@apache.org>
2022-12-08 23:46:44 +0800

    Refactor AgentTypedSPIRegistry

--- agent/core/src/main/java/org/apache/shardingsphere/agent/core/spi/AgentTypedSPIRegistry.java
+++ agent/core/src/main/java/org/apache/shardingsphere/agent/core/spi/AgentTypedSPIRegistry.java
@@ -17,12 +17,12 @@
 
 package org.apache.shardingsphere.agent.core.spi;
 
-import java.util.Collection;
-import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.shardingsphere.agent.core.exception.AgentServiceProviderNotFoundException;
 import org.apache.shardingsphere.agent.spi.type.AgentTypedSPI;
+
+import java.util.Collection;
+import java.util.Optional;
 
 /**
  *  Agent typed SPI registry.
@@ -32,40 +32,24 @@ public final class AgentTypedSPIRegistry {
     
     /**
      * Get registered service.
-     * 
-     * @param typedSPIClass typed SPI class
-     * @param type type
-     * @param <T> type
-     * @return registered service
-     */
-    public static <T extends AgentTypedSPI> T getRegisteredService(final Class<T> typedSPIClass, final String type) {
-        Optional<T> serviceInstance = AgentServiceLoader.getServiceLoader(typedSPIClass).newServiceInstances().stream().filter(each -> each.getType().equalsIgnoreCase(type)).findFirst();
-        if (serviceInstance.isPresent()) {
-            return serviceInstance.get();
-        }
-        throw new AgentServiceProviderNotFoundException(typedSPIClass, type);
-    }
-    
-    /**
-     * Get registered service.
      *
      * @param typedSPIClass typed SPI class
      * @param type type
-     * @param <T> type
+     * @param <T> type of agent typed SPI
      * @return registered service
      */
-    public static <T extends AgentTypedSPI> Optional<T> getRegisteredServiceOptional(final Class<T> typedSPIClass, final String type) {
+    public static <T extends AgentTypedSPI> Optional<T> getRegisteredService(final Class<T> typedSPIClass, final String type) {
         return AgentServiceLoader.getServiceLoader(typedSPIClass).newServiceInstances().stream().filter(each -> each.getType().equalsIgnoreCase(type)).findFirst();
     }
     
     /**
-     * Get all registered service.
+     * Get all registered services.
      *
      * @param typedSPIClass typed SPI class
-     * @param <T> type
-     * @return registered service
+     * @param <T> type of agent typed SPI
+     * @return registered services
      */
-    public static <T extends AgentTypedSPI> Collection<T> getAllRegisteredService(final Class<T> typedSPIClass) {
+    public static <T extends AgentTypedSPI> Collection<T> getAllRegisteredServices(final Class<T> typedSPIClass) {
         return AgentServiceLoader.getServiceLoader(typedSPIClass).newServiceInstances();
     }
 }
