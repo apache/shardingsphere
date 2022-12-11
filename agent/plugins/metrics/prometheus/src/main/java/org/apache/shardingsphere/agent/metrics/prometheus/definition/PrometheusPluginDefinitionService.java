9a8e84afff6a884ee9996d915f71104eb6900514
zhangliang <zhangliang@apache.org>
2022-12-11 15:53:05 +0800

    Add InterceptorsYamlSwapper

--- agent/plugins/metrics/prometheus/src/main/java/org/apache/shardingsphere/agent/metrics/prometheus/definition/PrometheusPluginDefinitionService.java
+++ agent/plugins/metrics/prometheus/src/main/java/org/apache/shardingsphere/agent/metrics/prometheus/definition/PrometheusPluginDefinitionService.java
@@ -19,10 +19,13 @@ package org.apache.shardingsphere.agent.metrics.prometheus.definition;
 
 import net.bytebuddy.matcher.ElementMatchers;
 import org.apache.shardingsphere.agent.api.point.PluginInterceptorPoint.Builder;
-import org.apache.shardingsphere.agent.core.definition.AbstractPluginDefinitionService;
 import org.apache.shardingsphere.agent.core.yaml.entity.Interceptor;
+import org.apache.shardingsphere.agent.core.yaml.entity.Interceptors;
 import org.apache.shardingsphere.agent.core.yaml.entity.TargetPoint;
-import org.apache.shardingsphere.agent.core.yaml.swapper.InterceptorsYamlSwapper;
+import org.apache.shardingsphere.agent.core.definition.AbstractPluginDefinitionService;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
 
 /**
  * Metrics plugin definition service.
@@ -31,13 +34,18 @@ public final class PrometheusPluginDefinitionService extends AbstractPluginDefin
     
     @Override
     protected void defineProxyInterceptors() {
-        for (Interceptor each : new InterceptorsYamlSwapper().unmarshal("/prometheus/interceptors.yaml").getInterceptors()) {
+        InputStream inputStream = getClass().getResourceAsStream("/prometheus/interceptors.yaml");
+        Interceptors interceptors = new Yaml().loadAs(inputStream, Interceptors.class);
+        for (Interceptor each : interceptors.getInterceptors()) {
             if (null == each.getTarget()) {
                 continue;
             }
             Builder builder = defineInterceptor(each.getTarget());
             if (null != each.getConstructAdvice() && !("".equals(each.getConstructAdvice()))) {
                 builder.onConstructor(ElementMatchers.isConstructor()).implement(each.getConstructAdvice()).build();
+            }
+            if (null == each.getPoints()) {
+                continue;
             }
             String[] instancePoints = each.getPoints().stream().filter(i -> "instance".equals(i.getType())).map(TargetPoint::getName).toArray(String[]::new);
             String[] staticPoints = each.getPoints().stream().filter(i -> "static".equals(i.getType())).map(TargetPoint::getName).toArray(String[]::new);
