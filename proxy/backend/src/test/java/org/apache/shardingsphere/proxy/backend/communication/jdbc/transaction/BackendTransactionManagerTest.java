9654ea009cf321ad7b076c9c9ad83fc8feda3d18
zhangliang <zhangliang@apache.org>
2022-12-22 22:11:10 +0800

    Refactor BackendTransactionManagerTest

--- proxy/backend/src/test/java/org/apache/shardingsphere/proxy/backend/communication/jdbc/transaction/BackendTransactionManagerTest.java
+++ proxy/backend/src/test/java/org/apache/shardingsphere/proxy/backend/communication/jdbc/transaction/BackendTransactionManagerTest.java
@@ -35,9 +35,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.internal.util.reflection.InstanceField;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.lang.reflect.Field;
 import java.sql.SQLException;
 
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
@@ -172,8 +172,6 @@ public final class BackendTransactionManagerTest extends ProxyContextRestorer {
     
     @SneakyThrows(ReflectiveOperationException.class)
     private void setLocalTransactionManager() {
-        Field field = BackendTransactionManager.class.getDeclaredField("localTransactionManager");
-        field.setAccessible(true);
-        field.set(backendTransactionManager, localTransactionManager);
+        new InstanceField(BackendTransactionManager.class.getDeclaredField("localTransactionManager"), backendTransactionManager).set(localTransactionManager);
     }
 }
