e4c5af7aa7b7c56209b3f1921f3700d301a48c28
孙念君 Nianjun Sun <sunnianjun@apache.org>
2022-12-27 15:54:29 +0800

    Add : support lib directory for agent classloader (#23118)

--- agent/bootstrap/src/main/java/org/apache/shardingsphere/agent/bootstrap/plugin/loader/AgentPluginLoader.java
+++ agent/bootstrap/src/main/java/org/apache/shardingsphere/agent/bootstrap/plugin/loader/AgentPluginLoader.java
@@ -26,9 +26,11 @@ import org.apache.shardingsphere.agent.bootstrap.plugin.PluginJar;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.jar.JarFile;
 
 /**
@@ -46,8 +48,15 @@ public final class AgentPluginLoader {
      * @throws IOException IO exception
      */
     public static Collection<PluginJar> load() throws IOException {
-        File[] jarFiles = AgentPathBuilder.getPluginPath().listFiles(each -> each.getName().endsWith(".jar"));
-        if (null == jarFiles) {
+        List<File> jarFiles = new LinkedList<>();
+        AgentPathBuilder.getPluginPaths().forEach(
+                path -> {
+                    File[] jarFileArray = path.listFiles(each -> each.getName().endsWith(".jar"));
+                    if (null != jarFileArray) {
+                        jarFiles.addAll(Arrays.asList(jarFileArray));
+                    }
+                });
+        if (jarFiles.isEmpty()) {
             return Collections.emptyList();
         }
         Collection<PluginJar> result = new LinkedList<>();
