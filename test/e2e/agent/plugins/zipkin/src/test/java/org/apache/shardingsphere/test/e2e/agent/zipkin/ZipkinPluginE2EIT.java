c480201058e4ff5eaf9de5612fb9137d46ab9f49
孙念君 Nianjun Sun <sunnianjun@apache.org>
2023-01-05 15:42:11 +0800

    Add : update the test comment (#23135)

--- test/e2e/agent/plugins/zipkin/src/test/java/org/apache/shardingsphere/test/e2e/agent/zipkin/ZipkinPluginE2EIT.java
+++ test/e2e/agent/plugins/zipkin/src/test/java/org/apache/shardingsphere/test/e2e/agent/zipkin/ZipkinPluginE2EIT.java
@@ -51,7 +51,7 @@ public final class ZipkinPluginE2EIT extends BasePluginE2EIT {
     public void assertProxyWithAgent() throws IOException {
         super.assertProxyWithAgent();
         try {
-            // TODO this needs to refactor, replace with Polling not Sleeping.
+            // TODO this needs to refactor, replace Sleeping with Polling not.
             Thread.sleep(Long.parseLong(props.getProperty("zipkin.waitMs", "60000")));
         } catch (final InterruptedException ignore) {
         }
