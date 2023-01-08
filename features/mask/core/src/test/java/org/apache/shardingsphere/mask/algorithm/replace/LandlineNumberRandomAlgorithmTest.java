afb2b905d7d21b4972ce7d72ca7f2bc91d016377
lushaorong <wwwwwlsr@163.com>
2023-01-08 18:32:30 +0800

    add final
    Signed-off-by: lushaorong <wwwwwlsr@163.com>

--- features/mask/core/src/test/java/org/apache/shardingsphere/mask/algorithm/replace/LandlineNumberRandomAlgorithmTest.java
+++ features/mask/core/src/test/java/org/apache/shardingsphere/mask/algorithm/replace/LandlineNumberRandomAlgorithmTest.java
@@ -26,7 +26,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class LandlineNumberRandomAlgorithmTest {
+public final class LandlineNumberRandomAlgorithmTest {
     
     private LandlineNumberRandomAlgorithm maskAlgorithm;
     
