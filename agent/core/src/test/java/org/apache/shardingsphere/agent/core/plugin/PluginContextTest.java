16b1f9fd74ebb560d80508c331350e1f803c0f61
Hzg030 <913628829@qq.com>
2023-01-12 10:52:15 +0800

    #23419 make PluginContextTest final

--- agent/core/src/test/java/org/apache/shardingsphere/agent/core/plugin/PluginContextTest.java
+++ agent/core/src/test/java/org/apache/shardingsphere/agent/core/plugin/PluginContextTest.java
@@ -25,7 +25,7 @@ import java.util.Random;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class PluginContextTest {
+public final class PluginContextTest {
 
     private static final String PLUGIN_ENABLED_KEY = "AGENT_PLUGINS_ENABLED";
 
