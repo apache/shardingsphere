8957e8f032bf5c8d2ddd6582b1d3e69e4000dff5
Hzg030 <913628829@qq.com>
2023-01-12 10:51:34 +0800

    #23424 make AgentServiceLoaderTest final

--- agent/core/src/test/java/org/apache/shardingsphere/agent/core/spi/AgentServiceLoaderTest.java
+++ agent/core/src/test/java/org/apache/shardingsphere/agent/core/spi/AgentServiceLoaderTest.java
@@ -27,7 +27,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-public class AgentServiceLoaderTest {
+public final class AgentServiceLoaderTest {
 
     @Test(expected = NullPointerException.class)
     public void assertGetServiceLoaderWithNullValue() {
