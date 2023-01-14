89dc070d8b2f2273f0715f06d117715e9f233410
zhangliang <zhangliang@apache.org>
2023-01-14 21:30:27 +0800

    Rename RouteCountAdvice and SQLParserEngineAdvice

---
+++ agent/plugins/metrics/core/src/main/java/org/apache/shardingsphere/agent/plugin/metrics/core/advice/SQLRouteCountAdvice.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.agent.plugin.metrics.core.advice;
+
+import org.apache.shardingsphere.agent.api.advice.TargetAdviceObject;
+import org.apache.shardingsphere.agent.api.advice.type.InstanceMethodAdvice;
+import org.apache.shardingsphere.agent.plugin.metrics.core.MetricsPool;
+import org.apache.shardingsphere.agent.plugin.metrics.core.MetricsWrapper;
+import org.apache.shardingsphere.agent.plugin.metrics.core.constant.MetricIds;
+import org.apache.shardingsphere.infra.binder.QueryContext;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.DeleteStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.InsertStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.UpdateStatement;
+
+import java.lang.reflect.Method;
+
+/**
+ * SQL route count advice.
+ */
+public final class SQLRouteCountAdvice implements InstanceMethodAdvice {
+    
+    static {
+        MetricsPool.create(MetricIds.ROUTED_INSERT_SQL);
+        MetricsPool.create(MetricIds.ROUTED_DELETE_SQL);
+        MetricsPool.create(MetricIds.ROUTED_UPDATE_SQL);
+        MetricsPool.create(MetricIds.ROUTED_SELECT_SQL);
+    }
+    
+    @Override
+    public void beforeMethod(final TargetAdviceObject target, final Method method, final Object[] args) {
+        QueryContext queryContext = (QueryContext) args[1];
+        SQLStatement sqlStatement = queryContext.getSqlStatementContext().getSqlStatement();
+        if (sqlStatement instanceof InsertStatement) {
+            MetricsPool.get(MetricIds.ROUTED_INSERT_SQL).ifPresent(MetricsWrapper::inc);
+        } else if (sqlStatement instanceof DeleteStatement) {
+            MetricsPool.get(MetricIds.ROUTED_DELETE_SQL).ifPresent(MetricsWrapper::inc);
+        } else if (sqlStatement instanceof UpdateStatement) {
+            MetricsPool.get(MetricIds.ROUTED_UPDATE_SQL).ifPresent(MetricsWrapper::inc);
+        } else if (sqlStatement instanceof SelectStatement) {
+            MetricsPool.get(MetricIds.ROUTED_SELECT_SQL).ifPresent(MetricsWrapper::inc);
+        }
+    }
+}
--- agent/plugins/metrics/core/src/test/java/org/apache/shardingsphere/agent/plugin/metrics/core/advice/SQLRouteCountAdviceTest.java
+++ agent/plugins/metrics/core/src/test/java/org/apache/shardingsphere/agent/plugin/metrics/core/advice/SQLRouteCountAdviceTest.java
@@ -23,9 +23,6 @@ import org.apache.shardingsphere.agent.plugin.metrics.core.fixture.FixtureWrappe
 import org.apache.shardingsphere.infra.binder.QueryContext;
 import org.apache.shardingsphere.infra.binder.statement.CommonSQLStatementContext;
 import org.apache.shardingsphere.infra.context.ConnectionContext;
-import org.apache.shardingsphere.infra.route.context.RouteContext;
-import org.apache.shardingsphere.infra.route.context.RouteMapper;
-import org.apache.shardingsphere.infra.route.context.RouteUnit;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml.MySQLDeleteStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml.MySQLInsertStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dml.MySQLSelectStatement;
@@ -40,9 +37,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-public final class RouteCountAdviceTest extends MetricsAdviceBaseTest {
+public final class SQLRouteCountAdviceTest extends MetricsAdviceBaseTest {
     
-    private final RouteCountAdvice advice = new RouteCountAdvice();
+    private final SQLRouteCountAdvice advice = new SQLRouteCountAdvice();
     
     @Test
     public void assertInsertRoute() {
@@ -69,24 +66,8 @@ public final class RouteCountAdviceTest extends MetricsAdviceBaseTest {
     }
     
     public void assertRoute(final String metricIds, final QueryContext queryContext) {
-        MockTargetAdviceObject targetObject = new MockTargetAdviceObject();
-        advice.beforeMethod(targetObject, mock(Method.class), new Object[]{new ConnectionContext(), queryContext});
+        advice.beforeMethod(new MockTargetAdviceObject(), mock(Method.class), new Object[]{new ConnectionContext(), queryContext});
         assertTrue(MetricsPool.get(metricIds).isPresent());
         assertThat(((FixtureWrapper) MetricsPool.get(metricIds).get()).getFixtureValue(), is(1d));
-    }
-    
-    @Test
-    public void assertRouteDataSourceAndTable() {
-        RouteContext routeContext = new RouteContext();
-        RouteMapper dataSourceMapper = new RouteMapper("logic_db", "ds_0");
-        RouteMapper tableMapper = new RouteMapper("t_order", "t_order_0");
-        routeContext.getRouteUnits().add(new RouteUnit(dataSourceMapper, Collections.singleton(tableMapper)));
-        advice.afterMethod(new MockTargetAdviceObject(), mock(Method.class), new Object[]{}, routeContext);
-        FixtureWrapper wrapper = (FixtureWrapper) MetricsPool.get(MetricIds.ROUTED_DATA_SOURCES).get();
-        assertTrue(MetricsPool.get(MetricIds.ROUTED_DATA_SOURCES).isPresent());
-        assertThat(wrapper.getFixtureValue(), is(1d));
-        wrapper = (FixtureWrapper) MetricsPool.get(MetricIds.ROUTED_TABLES).get();
-        assertTrue(MetricsPool.get(MetricIds.ROUTED_TABLES).isPresent());
-        assertThat(wrapper.getFixtureValue(), is(1d));
     }
 }
