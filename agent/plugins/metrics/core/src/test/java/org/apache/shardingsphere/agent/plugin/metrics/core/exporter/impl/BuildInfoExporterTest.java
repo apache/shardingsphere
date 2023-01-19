e28e2bb8070bd3cf144c0f90fd474126347731be
zhangliang <zhangliang@apache.org>
2023-01-19 20:59:00 +0800

    Refactor BuildInfoExporter

--- agent/plugins/metrics/core/src/test/java/org/apache/shardingsphere/agent/plugin/metrics/core/exporter/impl/BuildInfoExporterTest.java
+++ agent/plugins/metrics/core/src/test/java/org/apache/shardingsphere/agent/plugin/metrics/core/exporter/impl/BuildInfoExporterTest.java
@@ -45,6 +45,6 @@ public final class BuildInfoExporterTest {
     public void assertExport() {
         Optional<GaugeMetricFamilyMetricsCollector> collector = new BuildInfoExporter().export("FIXTURE");
         assertTrue(collector.isPresent());
-        assertThat(((MetricsCollectorFixture) collector.get()).getValue(), is(2d));
+        assertThat(((MetricsCollectorFixture) collector.get()).getValue(), is(1d));
     }
 }
