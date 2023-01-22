2d7dcb6fadb6b241603088051e559ffa9acbe661
Liang Zhang <zhangliang@apache.org>
2023-01-22 13:50:48 +0800

    Refactor ExecuteProcessEngine (#23700)

--- infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/process/ExecuteProcessEngine.java
+++ infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/process/ExecuteProcessEngine.java
@@ -50,21 +50,26 @@ public final class ExecuteProcessEngine {
      *
      * @param grantee grantee
      * @param databaseName database name
-     * @return execution id
+     * @return execution ID
      */
     public static String initializeConnection(final Grantee grantee, final String databaseName) {
-        ExecutionGroupContext<SQLExecutionUnit> executionGroupContext = new ExecutionGroupContext<>(Collections.emptyList());
-        executionGroupContext.setExecutionID(new UUID(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong()).toString().replace("-", ""));
-        executionGroupContext.setGrantee(grantee);
-        executionGroupContext.setDatabaseName(databaseName);
+        ExecutionGroupContext<SQLExecutionUnit> executionGroupContext = createExecutionGroupContext(grantee, databaseName);
         OptionalSPIRegistry.findRegisteredService(ExecuteProcessReporter.class).ifPresent(optional -> optional.report(executionGroupContext));
         return executionGroupContext.getExecutionID();
+    }
+    
+    private static ExecutionGroupContext<SQLExecutionUnit> createExecutionGroupContext(final Grantee grantee, final String databaseName) {
+        ExecutionGroupContext<SQLExecutionUnit> result = new ExecutionGroupContext<>(Collections.emptyList());
+        result.setExecutionID(new UUID(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong()).toString().replace("-", ""));
+        result.setGrantee(grantee);
+        result.setDatabaseName(databaseName);
+        return result;
     }
     
     /**
      * Finish connection.
      *
-     * @param executionID execution id
+     * @param executionID execution ID
      */
     public static void finishConnection(final String executionID) {
         OptionalSPIRegistry.findRegisteredService(ExecuteProcessReporter.class).ifPresent(optional -> optional.reportRemove(executionID));
