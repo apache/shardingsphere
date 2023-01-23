33e8944926fa16f5e07cc84415dd143a0a25bc5e
zhangliang <zhangliang@apache.org>
2023-01-23 22:22:42 +0800

    Refactor ReadwriteSplittingRuleStatementChecker

--- features/readwrite-splitting/distsql/handler/src/main/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/checker/ReadwriteSplittingRuleStatementChecker.java
+++ features/readwrite-splitting/distsql/handler/src/main/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/checker/ReadwriteSplittingRuleStatementChecker.java
@@ -203,14 +203,19 @@ public final class ReadwriteSplittingRuleStatementChecker {
     }
     
     private static void checkDuplicateReadDataSourceNames(final String databaseName, final Collection<ReadwriteSplittingRuleSegment> segments, final Collection<String> readDataSourceNames) {
-        segments.forEach(each -> {
+        for (ReadwriteSplittingRuleSegment each : segments) {
             if (null != each.getReadDataSources()) {
-                each.getReadDataSources().forEach(readDataSource -> {
-                    ShardingSpherePreconditions.checkState(readDataSourceNames.add(readDataSource), () -> new InvalidRuleConfigurationException("Readwrite splitting", each.getName(),
-                            String.format("Can not config duplicate read storage unit `%s` in database `%s`", readDataSource, databaseName)));
-                });
+                checkDuplicateReadDataSourceNames(databaseName, each, readDataSourceNames);
             }
-        });
+        }
+    }
+    
+    private static void checkDuplicateReadDataSourceNames(final String databaseName, final ReadwriteSplittingRuleSegment segment, final Collection<String> readDataSourceNames) {
+        for (String each : segment.getReadDataSources()) {
+            ShardingSpherePreconditions.checkState(readDataSourceNames.add(each),
+                    () -> new InvalidRuleConfigurationException(
+                            "Readwrite splitting", segment.getName(), String.format("Can not config duplicate read storage unit `%s` in database `%s`.", each, databaseName)));
+        }
     }
     
     private static void checkLoadBalancers(final Collection<ReadwriteSplittingRuleSegment> segments) {
