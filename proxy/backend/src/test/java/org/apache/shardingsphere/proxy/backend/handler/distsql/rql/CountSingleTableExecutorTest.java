8ac7bfdbb3bbdb23bc0bb078c5ffa95ad08b5307
Zichao <57847965+Qianyi951015@users.noreply.github.com>
2023-01-28 20:35:05 +1300

    Replace `CountSingleTableResultSet` with `CountSingleTableExecutor` (#23773)
    * Replace `CountSingleTableResultSet` with `CountSingleTableExecutor`
    
    * Replace `CountSingleTableResultSet` with `CountSingleTableExecutor`

--- proxy/backend/src/test/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/CountSingleTableExecutorTest.java
+++ proxy/backend/src/test/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/CountSingleTableExecutorTest.java
@@ -17,38 +17,47 @@
 
 package org.apache.shardingsphere.proxy.backend.handler.distsql.rql;
 
+import org.apache.shardingsphere.distsql.handler.query.RQLExecutor;
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.CountSingleTableStatement;
+import org.apache.shardingsphere.infra.merge.result.impl.local.LocalDataQueryResultRow;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
-import org.apache.shardingsphere.proxy.backend.handler.distsql.rql.rule.CountSingleTableResultSet;
+import org.apache.shardingsphere.proxy.backend.handler.distsql.rql.rule.CountSingleTableExecutor;
 import org.apache.shardingsphere.single.rule.SingleRule;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public final class CountSingleTableResultSetTest {
+public final class CountSingleTableExecutorTest {
     
     @Test
     public void assertGetRowData() {
-        CountSingleTableResultSet resultSet = new CountSingleTableResultSet();
-        resultSet.init(mockDatabase(), mock(CountSingleTableStatement.class));
-        assertTrue(resultSet.next());
-        List<Object> actual = new ArrayList<>(resultSet.getRowData());
-        assertThat(actual.size(), is(2));
-        assertThat(actual.get(0), is("db_1"));
-        assertThat(actual.get(1), is(2));
-        assertFalse(resultSet.next());
+        RQLExecutor<CountSingleTableStatement> executor = new CountSingleTableExecutor();
+        Collection<LocalDataQueryResultRow> actual = executor.getRows(mockDatabase(), mock(CountSingleTableStatement.class));
+        assertThat(actual.size(), is(1));
+        Iterator<LocalDataQueryResultRow> iterator = actual.iterator();
+        LocalDataQueryResultRow row = iterator.next();
+        assertThat(row.getCell(1), is("db_1"));
+        assertThat(row.getCell(2), is(2));
+    }
+    
+    @Test
+    public void assertGetColumnNames() {
+        RQLExecutor<CountSingleTableStatement> executor = new CountSingleTableExecutor();
+        Collection<String> columns = executor.getColumnNames();
+        assertThat(columns.size(), is(2));
+        Iterator<String> iterator = columns.iterator();
+        assertThat(iterator.next(), is("database"));
+        assertThat(iterator.next(), is("count"));
     }
     
     private ShardingSphereDatabase mockDatabase() {
