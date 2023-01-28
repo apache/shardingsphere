bec2550f1a97c488aac4832c51620d5f3016f5d0
Zichao <57847965+Qianyi951015@users.noreply.github.com>
2023-01-28 21:53:35 +1300

    Replace `LogicalTableResultSet` with `ShowLogicalTableExecutor` (#23778)

--- proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/rule/ShowLogicalTableExecutor.java
+++ proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/rule/ShowLogicalTableExecutor.java
@@ -17,55 +17,46 @@
 
 package org.apache.shardingsphere.proxy.backend.handler.distsql.rql.rule;
 
-import org.apache.shardingsphere.distsql.handler.resultset.DatabaseDistSQLResultSet;
+import org.apache.shardingsphere.distsql.handler.query.RQLExecutor;
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.ShowLogicalTablesStatement;
 import org.apache.shardingsphere.infra.database.type.SchemaSupportedDatabaseType;
+import org.apache.shardingsphere.infra.merge.result.impl.local.LocalDataQueryResultRow;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.proxy.backend.util.RegularUtil;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.util.SQLUtil;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 /**
- * Result set for show logical table.
+ * Show logical table executor.
  */
-public final class LogicalTableResultSet implements DatabaseDistSQLResultSet {
-    
-    private Iterator<String> data = Collections.emptyIterator();
+public final class ShowLogicalTableExecutor implements RQLExecutor<ShowLogicalTablesStatement> {
     
     @Override
-    public void init(final ShardingSphereDatabase database, final SQLStatement sqlStatement) {
+    public Collection<LocalDataQueryResultRow> getRows(final ShardingSphereDatabase database, final ShowLogicalTablesStatement sqlStatement) {
         String schemaName = database.getName();
         if (database.getProtocolType() instanceof SchemaSupportedDatabaseType) {
             schemaName = ((SchemaSupportedDatabaseType) database.getProtocolType()).getDefaultSchema();
         }
+        if (null == database.getSchema(schemaName)) {
+            return Collections.emptyList();
+        }
         Collection<String> tables = database.getSchema(schemaName).getAllTableNames();
-        ShowLogicalTablesStatement showLogicalTablesStatement = (ShowLogicalTablesStatement) sqlStatement;
-        if (showLogicalTablesStatement.getLikePattern().isPresent()) {
-            String pattern = SQLUtil.convertLikePatternToRegex(showLogicalTablesStatement.getLikePattern().get());
+        if (sqlStatement.getLikePattern().isPresent()) {
+            String pattern = SQLUtil.convertLikePatternToRegex(sqlStatement.getLikePattern().get());
             tables = tables.stream().filter(each -> RegularUtil.matchesCaseInsensitive(pattern, each)).collect(Collectors.toList());
         }
-        data = tables.iterator();
+        Collection<LocalDataQueryResultRow> result = new LinkedList<>();
+        tables.forEach(each -> result.add(new LocalDataQueryResultRow(each)));
+        return result;
     }
     
     @Override
     public Collection<String> getColumnNames() {
         return Collections.singletonList("table_name");
-    }
-    
-    @Override
-    public boolean next() {
-        return data.hasNext();
-    }
-    
-    @Override
-    public Collection<Object> getRowData() {
-        String next = data.next();
-        return Collections.singletonList(next);
     }
     
     @Override
