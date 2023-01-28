fa30aa50c0ccf830b02dd1423e5884cfc283cf40
Zichao <qianyiwang53@gmail.com>
2023-01-28 15:14:02 +0800

    Replace `CountSingleTableResultSet` with `CountSingleTableExecutor`

--- proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/rule/CountSingleTableExecutor.java
+++ proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/rule/CountSingleTableExecutor.java
@@ -17,57 +17,40 @@
 
 package org.apache.shardingsphere.proxy.backend.handler.distsql.rql.rule;
 
-import org.apache.shardingsphere.distsql.handler.resultset.DatabaseDistSQLResultSet;
+import org.apache.shardingsphere.distsql.handler.query.RQLExecutor;
 import org.apache.shardingsphere.distsql.parser.statement.rql.show.CountSingleTableStatement;
+import org.apache.shardingsphere.infra.merge.result.impl.local.LocalDataQueryResultRow;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.single.rule.SingleRule;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.LinkedList;
 import java.util.Optional;
 
 /**
- * Result set for single table.
+ * Count single table executor.
  */
-public final class CountSingleTableResultSet implements DatabaseDistSQLResultSet {
-    
-    private Iterator<Entry<String, Integer>> data = Collections.emptyIterator();
+public final class CountSingleTableExecutor implements RQLExecutor<CountSingleTableStatement> {
     
     private String databaseName;
+    
+    @Override
+    public Collection<LocalDataQueryResultRow> getRows(final ShardingSphereDatabase database, final CountSingleTableStatement sqlStatement) {
+        Optional<SingleRule> rule = database.getRuleMetaData().findSingleRule(SingleRule.class);
+        databaseName = database.getName();
+        Collection<LocalDataQueryResultRow> result = new LinkedList<>();
+        rule.ifPresent(optional -> addSingleTableData(result, databaseName, rule.get()));
+        return result;
+    }
     
     @Override
     public Collection<String> getColumnNames() {
         return Arrays.asList("database", "count");
     }
     
-    @Override
-    public void init(final ShardingSphereDatabase database, final SQLStatement sqlStatement) {
-        Optional<SingleRule> rule = database.getRuleMetaData().findSingleRule(SingleRule.class);
-        databaseName = database.getName();
-        Map<String, Integer> result = new LinkedHashMap<>();
-        rule.ifPresent(optional -> addSingleTableData(result, databaseName, rule.get()));
-        data = result.entrySet().iterator();
-    }
-    
-    private void addSingleTableData(final Map<String, Integer> rowMap, final String databaseName, final SingleRule rule) {
-        rowMap.put(databaseName, rule.getAllTables().size());
-    }
-    
-    @Override
-    public boolean next() {
-        return data.hasNext();
-    }
-    
-    @Override
-    public Collection<Object> getRowData() {
-        Entry<String, Integer> entry = data.next();
-        return Arrays.asList(entry.getKey(), entry.getValue());
+    private void addSingleTableData(final Collection<LocalDataQueryResultRow> row, final String databaseName, final SingleRule rule) {
+        row.add(new LocalDataQueryResultRow(databaseName, rule.getAllTables().size()));
     }
     
     @Override
