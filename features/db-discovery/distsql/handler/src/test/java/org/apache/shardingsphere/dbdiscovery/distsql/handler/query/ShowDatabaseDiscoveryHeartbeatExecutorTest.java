6074ea51be968c815b470efaf1ca6c9133cb40d6
Zichao <qianyiwang53@gmail.com>
2023-01-29 12:03:59 +0800

    Replace `DatabaseDiscoveryHeartbeatResultSet` with `ShowDatabaseDiscoveryHeartbeatExecutor`

--- features/db-discovery/distsql/handler/src/test/java/org/apache/shardingsphere/dbdiscovery/distsql/handler/query/ShowDatabaseDiscoveryHeartbeatExecutorTest.java
+++ features/db-discovery/distsql/handler/src/test/java/org/apache/shardingsphere/dbdiscovery/distsql/handler/query/ShowDatabaseDiscoveryHeartbeatExecutorTest.java
@@ -43,7 +43,7 @@ import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public final class DatabaseDiscoveryHeartbeatResultSetTest {
+public final class ShowDatabaseDiscoveryHeartbeatExecutorTest {
     
     @Test
     public void assertGetRowData() {
