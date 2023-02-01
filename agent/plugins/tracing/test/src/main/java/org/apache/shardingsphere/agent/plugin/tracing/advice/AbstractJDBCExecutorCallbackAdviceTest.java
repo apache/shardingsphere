67be57f7985e015ba75b8e74caff393e304770a2
jiangML <1060319118@qq.com>
2023-02-01 16:39:29 +0800

    add AttributesConstants

--- agent/plugins/tracing/test/src/main/java/org/apache/shardingsphere/agent/plugin/tracing/advice/AbstractJDBCExecutorCallbackAdviceTest.java
+++ agent/plugins/tracing/test/src/main/java/org/apache/shardingsphere/agent/plugin/tracing/advice/AbstractJDBCExecutorCallbackAdviceTest.java
@@ -93,7 +93,7 @@ public abstract class AbstractJDBCExecutorCallbackAdviceTest implements AdviceTe
     }
     
     /**
-     * get database type.
+     * Get database type.
      *
      * @param databaseName database name
      * @return database type
