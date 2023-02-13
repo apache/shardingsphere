177215720c04c973d2a3f1c7d5d1e6cb456df16a
Ling Hengqian <linghengqian@outlook.com>
2023-02-13 15:01:08 +0800

    Fix nightly builds of ShardingSphere Proxy Native (#23957)

--- infra/util-groovy/src/main/java/org/apache/shardingsphere/infra/util/groovy/expr/HotspotInlineExpressionParser.java
+++ infra/util-groovy/src/main/java/org/apache/shardingsphere/infra/util/groovy/expr/HotspotInlineExpressionParser.java
@@ -36,7 +36,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 /**
- * Inline expression parser.
+ * Hotspot Inline expression parser.
  */
 @RequiredArgsConstructor
 public final class HotspotInlineExpressionParser {
