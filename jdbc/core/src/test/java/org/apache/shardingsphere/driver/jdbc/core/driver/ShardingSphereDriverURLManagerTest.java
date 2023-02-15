e514f46293a99b7cf61172bab363c8d10cd225bd
totalo <totalo@apache.org>
2023-02-15 22:37:33 +0800

    Abstract the spi of shardingsphere driver url

--- jdbc/core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/driver/ShardingSphereDriverURLManagerTest.java
+++ jdbc/core/src/test/java/org/apache/shardingsphere/driver/jdbc/core/driver/ShardingSphereDriverURLManagerTest.java
@@ -24,32 +24,25 @@ import java.util.Objects;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public final class ShardingSphereDriverURLTest {
+public final class ShardingSphereDriverURLManagerTest {
     
     private final int fooDriverConfigLength = 995;
     
     @Test(expected = IllegalArgumentException.class)
     public void assertNewConstructorWithEmptyURL() {
-        new ShardingSphereDriverURL("jdbc:shardingsphere:");
+        ShardingSphereDriverURLManager.getContent("jdbc:shardingsphere:");
     }
     
     @Test
     public void assertToClasspathConfigurationFile() {
-        ShardingSphereDriverURL actual = new ShardingSphereDriverURL("jdbc:shardingsphere:classpath:config/driver/foo-driver-fixture.yaml");
-        assertThat(actual.toConfigurationBytes().length, is(fooDriverConfigLength));
+        byte[] actual = ShardingSphereDriverURLManager.getContent("jdbc:shardingsphere:classpath:config/driver/foo-driver-fixture.yaml");
+        assertThat(actual.length, is(fooDriverConfigLength));
     }
     
     @Test
-    public void assertToConfigurationFile() {
-        String absolutePath = Objects.requireNonNull(ShardingSphereDriverURLTest.class.getClassLoader().getResource("config/driver/foo-driver-fixture.yaml")).getPath();
-        ShardingSphereDriverURL actual = new ShardingSphereDriverURL("jdbc:shardingsphere:" + absolutePath);
-        assertThat(actual.toConfigurationBytes().length, is(fooDriverConfigLength));
-    }
-    
-    @Test
-    public void assertToConfigurationFileWithOtherParameters() {
-        String absolutePath = Objects.requireNonNull(ShardingSphereDriverURLTest.class.getClassLoader().getResource("config/driver/foo-driver-fixture.yaml")).getPath();
-        ShardingSphereDriverURL actual = new ShardingSphereDriverURL("jdbc:shardingsphere:" + absolutePath + "?xxx=xxx&yyy=yyy");
-        assertThat(actual.toConfigurationBytes().length, is(fooDriverConfigLength));
+    public void assertToAbsolutePathConfigurationFile() {
+        String absolutePath = Objects.requireNonNull(ShardingSphereDriverURLManagerTest.class.getClassLoader().getResource("config/driver/foo-driver-fixture.yaml")).getPath();
+        byte[] actual = ShardingSphereDriverURLManager.getContent("jdbc:shardingsphere:absolutepath:" + absolutePath);
+        assertThat(actual.length, is(fooDriverConfigLength));
     }
 }
