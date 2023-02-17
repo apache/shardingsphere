70967610542f468597dd50803f102218b82a9e2d
RaigorJiang <raigor.jiang@gmail.com>
2023-02-17 10:40:47 +0800

    Refactor LabelComputeNodeHandler

--- proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/ral/updatable/LabelComputeNodeUpdater.java
+++ proxy/backend/src/main/java/org/apache/shardingsphere/proxy/backend/handler/distsql/ral/updatable/LabelComputeNodeUpdater.java
@@ -17,6 +17,7 @@
 
 package org.apache.shardingsphere.proxy.backend.handler.distsql.ral.updatable;
 
+import org.apache.shardingsphere.distsql.handler.ral.update.RALUpdater;
 import org.apache.shardingsphere.distsql.parser.statement.ral.updatable.LabelComputeNodeStatement;
 import org.apache.shardingsphere.infra.instance.ComputeNodeInstance;
 import org.apache.shardingsphere.infra.util.exception.ShardingSpherePreconditions;
@@ -24,33 +25,39 @@ import org.apache.shardingsphere.infra.util.exception.external.sql.type.generic.
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.mode.manager.cluster.coordinator.registry.status.compute.event.LabelsChangedEvent;
 import org.apache.shardingsphere.mode.metadata.persist.MetaDataPersistService;
-import org.apache.shardingsphere.mode.repository.standalone.StandalonePersistRepository;
+import org.apache.shardingsphere.mode.repository.cluster.ClusterPersistRepository;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
-import org.apache.shardingsphere.proxy.backend.handler.distsql.ral.UpdatableRALBackendHandler;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.Optional;
 
 /**
- * Label compute node handler.
+ * Label compute node updater.
  */
-public final class LabelComputeNodeHandler extends UpdatableRALBackendHandler<LabelComputeNodeStatement> {
+public final class LabelComputeNodeUpdater implements RALUpdater<LabelComputeNodeStatement> {
     
     @Override
-    public void update(final ContextManager contextManager) {
-        MetaDataPersistService persistService = ProxyContext.getInstance().getContextManager().getMetaDataContexts().getPersistService();
-        ShardingSpherePreconditions.checkState(null != persistService && null != persistService.getRepository() && !(persistService.getRepository() instanceof StandalonePersistRepository),
+    public void executeUpdate(final String databaseName, final LabelComputeNodeStatement sqlStatement) throws SQLException {
+        ContextManager contextManager = ProxyContext.getInstance().getContextManager();
+        MetaDataPersistService persistService = contextManager.getMetaDataContexts().getPersistService();
+        ShardingSpherePreconditions.checkState(null != persistService && null != persistService.getRepository() && persistService.getRepository() instanceof ClusterPersistRepository,
                 () -> new UnsupportedSQLOperationException("Labels can only be added in cluster mode"));
-        String instanceId = getSqlStatement().getInstanceId();
+        String instanceId = sqlStatement.getInstanceId();
         Optional<ComputeNodeInstance> computeNodeInstance = contextManager.getInstanceContext().getComputeNodeInstanceById(instanceId);
         if (computeNodeInstance.isPresent()) {
-            Collection<String> labels = new LinkedHashSet<>(getSqlStatement().getLabels());
-            if (!getSqlStatement().isOverwrite()) {
+            Collection<String> labels = new LinkedHashSet<>(sqlStatement.getLabels());
+            if (!sqlStatement.isOverwrite()) {
                 labels.addAll(computeNodeInstance.get().getLabels());
             }
-            contextManager.getInstanceContext().getEventBusContext().post(new LabelsChangedEvent(instanceId, new ArrayList<>(labels)));
+            contextManager.getInstanceContext().getEventBusContext().post(new LabelsChangedEvent(instanceId, new LinkedList<>(labels)));
         }
+    }
+    
+    @Override
+    public String getType() {
+        return LabelComputeNodeStatement.class.getName();
     }
 }
