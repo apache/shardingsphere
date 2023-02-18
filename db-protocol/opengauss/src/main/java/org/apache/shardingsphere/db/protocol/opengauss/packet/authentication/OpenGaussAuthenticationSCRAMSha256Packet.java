85866655e7abb74af9f10054c786fac07d766a62
Liang Zhang <zhangliang@apache.org>
2023-02-18 22:23:05 +0800

    Add OpenGaussMacCalculator (#24226)

--- db-protocol/opengauss/src/main/java/org/apache/shardingsphere/db/protocol/opengauss/packet/authentication/OpenGaussAuthenticationSCRAMSha256Packet.java
+++ db-protocol/opengauss/src/main/java/org/apache/shardingsphere/db/protocol/opengauss/packet/authentication/OpenGaussAuthenticationSCRAMSha256Packet.java
@@ -17,33 +17,16 @@
 
 package org.apache.shardingsphere.db.protocol.opengauss.packet.authentication;
 
-import lombok.SneakyThrows;
 import org.apache.shardingsphere.db.protocol.opengauss.constant.OpenGaussProtocolVersion;
 import org.apache.shardingsphere.db.protocol.postgresql.packet.identifier.PostgreSQLIdentifierPacket;
 import org.apache.shardingsphere.db.protocol.postgresql.packet.identifier.PostgreSQLIdentifierTag;
 import org.apache.shardingsphere.db.protocol.postgresql.packet.identifier.PostgreSQLMessagePacketType;
 import org.apache.shardingsphere.db.protocol.postgresql.payload.PostgreSQLPacketPayload;
 
-import javax.crypto.Mac;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.Locale;
-
 /**
  * Authentication request SCRAM SHA-256 for openGauss.
  */
 public final class OpenGaussAuthenticationSCRAMSha256Packet implements PostgreSQLIdentifierPacket {
-    
-    private static final String PBKDF2_WITH_HMAC_SHA1_ALGORITHM = "PBKDF2WithHmacSHA1";
-    
-    private static final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
-    
-    private static final String SERVER_KEY = "Server Key";
     
     private static final int AUTH_REQ_SHA256 = 10;
     
@@ -61,58 +44,7 @@ public final class OpenGaussAuthenticationSCRAMSha256Packet implements PostgreSQ
         this.version = version;
         this.serverIteration = serverIteration;
         this.authHexData = authHexData;
-        serverSignature = version >= OpenGaussProtocolVersion.PROTOCOL_350.getVersion() ? "" : generateServerSignature(password);
-    }
-    
-    private String generateServerSignature(final String password) {
-        byte[] k = generateKFromPBKDF2(password);
-        byte[] serverKey = getKeyFromHmac(k, SERVER_KEY.getBytes(StandardCharsets.UTF_8));
-        byte[] result = getKeyFromHmac(serverKey, toHexBytes(authHexData.getNonce()));
-        return bytesToHexString(result);
-    }
-    
-    @SneakyThrows({NoSuchAlgorithmException.class, InvalidKeySpecException.class})
-    private byte[] generateKFromPBKDF2(final String password) {
-        char[] chars = password.toCharArray();
-        byte[] salt = toHexBytes(authHexData.getSalt());
-        PBEKeySpec spec = new PBEKeySpec(chars, salt, serverIteration, 32 * 8);
-        SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_WITH_HMAC_SHA1_ALGORITHM);
-        return skf.generateSecret(spec).getEncoded();
-    }
-    
-    private byte[] toHexBytes(final String hexString) {
-        int length = hexString.length() / 2;
-        char[] hexChars = hexString.toUpperCase(Locale.ENGLISH).toCharArray();
-        byte[] result = new byte[length];
-        for (int i = 0; i < length; i++) {
-            int pos = i * 2;
-            result[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
-        }
-        return result;
-    }
-    
-    private byte charToByte(final char c) {
-        return (byte) "0123456789ABCDEF".indexOf(c);
-    }
-    
-    @SneakyThrows({NoSuchAlgorithmException.class, InvalidKeyException.class})
-    private byte[] getKeyFromHmac(final byte[] key, final byte[] data) {
-        SecretKeySpec signingKey = new SecretKeySpec(key, HMAC_SHA256_ALGORITHM);
-        Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
-        mac.init(signingKey);
-        return mac.doFinal(data);
-    }
-    
-    private String bytesToHexString(final byte[] src) {
-        StringBuilder result = new StringBuilder();
-        for (byte each : src) {
-            String hex = Integer.toHexString(each & 255);
-            if (hex.length() < 2) {
-                result.append(0);
-            }
-            result.append(hex);
-        }
-        return result.toString();
+        serverSignature = version >= OpenGaussProtocolVersion.PROTOCOL_350.getVersion() ? "" : OpenGaussMacCalculator.requestServerMac(password, authHexData, serverIteration);
     }
     
     @Override
