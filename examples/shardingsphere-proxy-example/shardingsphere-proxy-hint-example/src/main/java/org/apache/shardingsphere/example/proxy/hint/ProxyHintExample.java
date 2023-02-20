3b4fe9d60901b1a6c99f96514f79be94da52bced
anmui <893724023@qq.com>
2023-02-20 19:01:05 +0800

    del:shardingsphere-examples distsql Hint

--- examples/shardingsphere-proxy-example/shardingsphere-proxy-hint-example/src/main/java/org/apache/shardingsphere/example/proxy/hint/ProxyHintExample.java
+++ examples/shardingsphere-proxy-example/shardingsphere-proxy-hint-example/src/main/java/org/apache/shardingsphere/example/proxy/hint/ProxyHintExample.java
@@ -30,10 +30,6 @@ import java.sql.Statement;
 
 public final class ProxyHintExample {
     
-    private static final HintType TYPE = HintType.DATABASE_TABLES;
-//    private static final HintType TYPE = HintType.DATABASE_ONLY;
-//    private static final HintType TYPE = HintType.WRITE_ONLY;
-//    private static final HintType TYPE = HintType.SQL_HINT_DATASOURCE;
     
     public static void main(final String[] args) throws SQLException, IOException {
         DataSource dataSource = getDataSource();
@@ -44,18 +40,7 @@ public final class ProxyHintExample {
     }
     
     private static DataSource getDataSource() throws IOException {
-        switch (TYPE) {
-            case DATABASE_TABLES:
-                return YamlDataSourceFactory.createDataSource(getFile("/META-INF/hint-databases-tables.yaml"));
-            case DATABASE_ONLY:
-                return YamlDataSourceFactory.createDataSource(getFile("/META-INF/hint-databases-only.yaml"));
-            case WRITE_ONLY:
-                return YamlDataSourceFactory.createDataSource(getFile("/META-INF/hint-write-only.yaml"));
-            case SQL_HINT_DATASOURCE:
-                return YamlDataSourceFactory.createDataSource(getFile("/META-INF/sql-hint-data-source.yaml"));
-            default:
-                throw new UnsupportedOperationException("unsupported type");
-        }
+        return YamlDataSourceFactory.createDataSource(getFile("/META-INF/sql-hint-data-source.yaml"));
     }
     
     private static File getFile(final String configFile) {
@@ -69,35 +54,11 @@ public final class ProxyHintExample {
     private static void processWithHintValue(final DataSource dataSource) throws SQLException {
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement()) {
-            if (TYPE == HintType.SQL_HINT_DATASOURCE) {
-                statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */select * from t_order");
-                statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */SELECT i.* FROM t_order o, t_order_item i WHERE o.order_id = i.order_id");
-                statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */select * from t_order_item");
-                statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */INSERT INTO t_order (user_id, address_id, status) VALUES (1, 1, 'init')");
-            } else {
-                setHintValue(statement);
-                statement.execute("select * from t_order");
-                statement.execute("SELECT i.* FROM t_order o, t_order_item i WHERE o.order_id = i.order_id");
-                statement.execute("select * from t_order_item");
-                statement.execute("INSERT INTO t_order (user_id, address_id, status) VALUES (1, 1, 'init')");
-            }
+            statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */select * from t_order");
+            statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */SELECT i.* FROM t_order o, t_order_item i WHERE o.order_id = i.order_id");
+            statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */select * from t_order_item");
+            statement.execute("/* ShardingSphere hint: dataSourceName=ds_1 */INSERT INTO t_order (user_id, address_id, status) VALUES (1, 1, 'init')");
         }
     }
     
-    private static void setHintValue(final Statement statement) throws SQLException {
-        switch (TYPE) {
-            case DATABASE_TABLES:
-                statement.execute("add sharding hint database_value t_order=1");
-                statement.execute("add sharding hint table_value t_order=1");
-                return;
-            case DATABASE_ONLY:
-                statement.execute("set sharding hint database_value=1");
-                return;
-            case WRITE_ONLY:
-                statement.execute("set readwrite_splitting hint source=write");
-                return;
-            default:
-                throw new UnsupportedOperationException("unsupported type");
-        }
-    }
 }
