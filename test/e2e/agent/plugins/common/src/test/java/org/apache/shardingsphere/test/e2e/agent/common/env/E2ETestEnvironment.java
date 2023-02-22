f1a442a65c22fcde0788cb85d6b96f3be3d70ba7
jiangML <1060319118@qq.com>
2023-02-22 10:10:47 +0800

    Optimize agent e2e test (#24283)
    * optimize agent e2e test
    
    * optimize agent e2e test

--- test/e2e/agent/plugins/common/src/test/java/org/apache/shardingsphere/test/e2e/agent/common/env/E2ETestEnvironment.java
+++ test/e2e/agent/plugins/common/src/test/java/org/apache/shardingsphere/test/e2e/agent/common/env/E2ETestEnvironment.java
@@ -63,7 +63,7 @@ public final class E2ETestEnvironment {
     /**
      * Create data source.
      */
-    public void createDataSource() {
+    public synchronized void createDataSource() {
         if (isEnvironmentPrepared && null == dataSource) {
             if (waitForEnvironmentReady(props)) {
                 dataSource = createHikariCP(props);
@@ -76,7 +76,7 @@ public final class E2ETestEnvironment {
     private boolean waitForEnvironmentReady(final Properties props) {
         log.info("Proxy with agent environment initializing ...");
         try {
-            Awaitility.await().atMost(4, TimeUnit.MINUTES).pollInterval(2, TimeUnit.SECONDS).until(() -> isProxyReady(props));
+            Awaitility.await().atMost(4, TimeUnit.MINUTES).pollInterval(5, TimeUnit.SECONDS).until(() -> isProxyReady(props));
         } catch (final ConditionTimeoutException ignored) {
             log.info("Proxy with agent environment initialization failed ...");
             return false;
