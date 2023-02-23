9fab7963550aa2d756d50d30c32ee3b946c83b63
yx9o <yangx_soft@163.com>
2023-02-23 21:06:09 +0800

    Refactor YamlPluginsConfigurationSwapperTest (#24324)

--- agent/core/src/test/java/org/apache/shardingsphere/agent/core/plugin/config/yaml/swapper/YamlPluginsConfigurationSwapperTest.java
+++ agent/core/src/test/java/org/apache/shardingsphere/agent/core/plugin/config/yaml/swapper/YamlPluginsConfigurationSwapperTest.java
@@ -17,109 +17,105 @@
 
 package org.apache.shardingsphere.agent.core.plugin.config.yaml.swapper;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.TreeMap;
-
 import org.apache.shardingsphere.agent.api.PluginConfiguration;
 import org.apache.shardingsphere.agent.core.plugin.config.yaml.entity.YamlAgentConfiguration;
 import org.apache.shardingsphere.agent.core.plugin.config.yaml.entity.YamlPluginCategoryConfiguration;
 import org.apache.shardingsphere.agent.core.plugin.config.yaml.entity.YamlPluginConfiguration;
 import org.junit.Test;
 
-public class YamlPluginsConfigurationSwapperTest {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public final class YamlPluginsConfigurationSwapperTest {
     
     @Test
     public void testEmptyPluginConfigurationSwap() {
-        YamlPluginCategoryConfiguration yamlPluginCategoryConfiguration = new YamlPluginCategoryConfiguration();
-        yamlPluginCategoryConfiguration.setLogging(new TreeMap<>());
-        yamlPluginCategoryConfiguration.setMetrics(new TreeMap<>());
-        yamlPluginCategoryConfiguration.setTracing(new TreeMap<>());
-        YamlAgentConfiguration yamlAgentConfiguration = new YamlAgentConfiguration();
-        yamlAgentConfiguration.setPlugins(yamlPluginCategoryConfiguration);
-        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfiguration).isEmpty());
+        YamlPluginCategoryConfiguration yamlPluginCategoryConfig = new YamlPluginCategoryConfiguration();
+        yamlPluginCategoryConfig.setLogging(new TreeMap<>());
+        yamlPluginCategoryConfig.setMetrics(new TreeMap<>());
+        yamlPluginCategoryConfig.setTracing(new TreeMap<>());
+        YamlAgentConfiguration yamlAgentConfig = new YamlAgentConfiguration();
+        yamlAgentConfig.setPlugins(yamlPluginCategoryConfig);
+        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfig).isEmpty());
     }
     
     @Test
     public void assertTestSwapHandlesNullPlugins() {
-        YamlAgentConfiguration yamlAgentConfiguration = new YamlAgentConfiguration();
-        yamlAgentConfiguration.setPlugins(null);
-        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfiguration).isEmpty());
+        YamlAgentConfiguration yamlAgentConfig = new YamlAgentConfiguration();
+        yamlAgentConfig.setPlugins(null);
+        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfig).isEmpty());
     }
     
     @Test
     public void assertTestSwapWithNullPlugins() {
-        YamlPluginCategoryConfiguration yamlPluginCategoryConfiguration = new YamlPluginCategoryConfiguration();
-        yamlPluginCategoryConfiguration.setTracing(null);
-        yamlPluginCategoryConfiguration.setLogging(null);
-        yamlPluginCategoryConfiguration.setMetrics(null);
-        YamlAgentConfiguration yamlAgentConfiguration = new YamlAgentConfiguration();
-        yamlAgentConfiguration.setPlugins(yamlPluginCategoryConfiguration);
-        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfiguration).isEmpty());
+        YamlPluginCategoryConfiguration yamlPluginCategoryConfig = new YamlPluginCategoryConfiguration();
+        yamlPluginCategoryConfig.setTracing(null);
+        yamlPluginCategoryConfig.setLogging(null);
+        yamlPluginCategoryConfig.setMetrics(null);
+        YamlAgentConfiguration yamlAgentConfig = new YamlAgentConfiguration();
+        yamlAgentConfig.setPlugins(yamlPluginCategoryConfig);
+        assertTrue(YamlPluginsConfigurationSwapper.swap(yamlAgentConfig).isEmpty());
     }
     
     @Test
     public void assertTestSwapWithSinglePluginConfiguration() {
-        YamlPluginConfiguration yamlPluginConfiguration = new YamlPluginConfiguration();
-        yamlPluginConfiguration.setHost("localhost");
-        yamlPluginConfiguration.setPassword("random");
-        yamlPluginConfiguration.setPort(8080);
-        yamlPluginConfiguration.setProps(new Properties());
-        Map<String, YamlPluginConfiguration> yamlPluginConfigurations = new HashMap<>();
-        yamlPluginConfigurations.put("Key", yamlPluginConfiguration);
-        YamlPluginCategoryConfiguration yamlPluginCategoryConfiguration = new YamlPluginCategoryConfiguration();
-        yamlPluginCategoryConfiguration.setLogging(yamlPluginConfigurations);
-        yamlPluginCategoryConfiguration.setMetrics(new TreeMap<>());
-        yamlPluginCategoryConfiguration.setTracing(new TreeMap<>());
-        YamlAgentConfiguration yamlAgentConfiguration = new YamlAgentConfiguration();
-        yamlAgentConfiguration.setPlugins(yamlPluginCategoryConfiguration);
-        Map<String, PluginConfiguration> actualSwapResult = YamlPluginsConfigurationSwapper.swap(yamlAgentConfiguration);
-        assertEquals(1, actualSwapResult.size());
-        PluginConfiguration getResult = actualSwapResult.get("Key");
-        assertEquals("localhost", getResult.getHost());
-        assertTrue(getResult.getProps().isEmpty());
-        assertEquals(8080, getResult.getPort());
-        assertEquals("random", getResult.getPassword());
+        YamlPluginConfiguration yamlPluginConfig = createYamlPluginConfiguration("localhost", "random", 8080, new Properties());
+        Map<String, YamlPluginConfiguration> yamlPluginConfigs = new HashMap<>();
+        yamlPluginConfigs.put("Key", yamlPluginConfig);
+        YamlPluginCategoryConfiguration yamlPluginCategoryConfig = new YamlPluginCategoryConfiguration();
+        yamlPluginCategoryConfig.setLogging(yamlPluginConfigs);
+        yamlPluginCategoryConfig.setMetrics(new TreeMap<>());
+        yamlPluginCategoryConfig.setTracing(new TreeMap<>());
+        YamlAgentConfiguration yamlAgentConfig = new YamlAgentConfiguration();
+        yamlAgentConfig.setPlugins(yamlPluginCategoryConfig);
+        Map<String, PluginConfiguration> actual = YamlPluginsConfigurationSwapper.swap(yamlAgentConfig);
+        assertThat(actual.size(), is(1));
+        PluginConfiguration pluginConfig = actual.get("Key");
+        assertThat(pluginConfig.getHost(), is("localhost"));
+        assertThat(pluginConfig.getPassword(), is("random"));
+        assertThat(pluginConfig.getPort(), is(8080));
+        assertTrue(pluginConfig.getProps().isEmpty());
     }
     
     @Test
     public void assertTestSwapWithMultiplePluginConfigurations() {
-        YamlPluginConfiguration yamlPluginConfiguration = new YamlPluginConfiguration();
-        yamlPluginConfiguration.setHost("localhost");
-        yamlPluginConfiguration.setPassword("random");
-        yamlPluginConfiguration.setPort(8080);
-        Properties properties = new Properties();
-        yamlPluginConfiguration.setProps(properties);
-        YamlPluginConfiguration yamlPluginConfiguration1 = new YamlPluginConfiguration();
-        yamlPluginConfiguration1.setHost("localhost");
-        yamlPluginConfiguration1.setPassword("random");
-        yamlPluginConfiguration1.setPort(8080);
-        yamlPluginConfiguration1.setProps(new Properties());
-        Map<String, YamlPluginConfiguration> stringYamlPluginConfigurationMap = new TreeMap<>();
-        stringYamlPluginConfigurationMap.put("42", yamlPluginConfiguration1);
-        stringYamlPluginConfigurationMap.put("Key", yamlPluginConfiguration);
-        YamlPluginCategoryConfiguration yamlPluginCategoryConfiguration = new YamlPluginCategoryConfiguration();
-        yamlPluginCategoryConfiguration.setLogging(stringYamlPluginConfigurationMap);
-        yamlPluginCategoryConfiguration.setMetrics(new TreeMap<>());
-        yamlPluginCategoryConfiguration.setTracing(new TreeMap<>());
-        YamlAgentConfiguration yamlAgentConfiguration = new YamlAgentConfiguration();
-        yamlAgentConfiguration.setPlugins(yamlPluginCategoryConfiguration);
-        Map<String, PluginConfiguration> actualSwapResult = YamlPluginsConfigurationSwapper.swap(yamlAgentConfiguration);
-        assertEquals(2, actualSwapResult.size());
-        PluginConfiguration getResult = actualSwapResult.get("42");
-        Properties props = getResult.getProps();
-        assertEquals(properties, props);
-        PluginConfiguration getResult1 = actualSwapResult.get("Key");
-        assertEquals(props, getResult1.getProps());
-        assertEquals(8080, getResult1.getPort());
-        assertEquals("random", getResult1.getPassword());
-        assertEquals("localhost", getResult1.getHost());
-        assertEquals(8080, getResult.getPort());
-        assertEquals("random", getResult.getPassword());
-        assertEquals("localhost", getResult.getHost());
+        YamlPluginConfiguration yamlPluginConfig1 = createYamlPluginConfiguration("localhost", "random", 8080, new Properties());
+        YamlPluginConfiguration yamlPluginConfig2 = createYamlPluginConfiguration("localhost", "random", 8080, new Properties());
+        Map<String, YamlPluginConfiguration> stringYamlPluginConfigMap = new TreeMap<>();
+        stringYamlPluginConfigMap.put("42", yamlPluginConfig1);
+        stringYamlPluginConfigMap.put("Key", yamlPluginConfig2);
+        YamlPluginCategoryConfiguration yamlPluginCategoryConfig = new YamlPluginCategoryConfiguration();
+        yamlPluginCategoryConfig.setLogging(stringYamlPluginConfigMap);
+        yamlPluginCategoryConfig.setMetrics(new TreeMap<>());
+        yamlPluginCategoryConfig.setTracing(new TreeMap<>());
+        YamlAgentConfiguration yamlAgentConfig = new YamlAgentConfiguration();
+        yamlAgentConfig.setPlugins(yamlPluginCategoryConfig);
+        Map<String, PluginConfiguration> actual = YamlPluginsConfigurationSwapper.swap(yamlAgentConfig);
+        assertThat(actual.size(), is(2));
+        PluginConfiguration pluginConfig1 = actual.get("42");
+        assertThat(pluginConfig1.getHost(), is("localhost"));
+        assertThat(pluginConfig1.getPassword(), is("random"));
+        assertThat(pluginConfig1.getPort(), is(8080));
+        assertTrue(pluginConfig1.getProps().isEmpty());
+        PluginConfiguration pluginConfig2 = actual.get("Key");
+        assertThat(pluginConfig2.getHost(), is("localhost"));
+        assertThat(pluginConfig2.getPassword(), is("random"));
+        assertThat(pluginConfig2.getPort(), is(8080));
+        assertTrue(pluginConfig2.getProps().isEmpty());
+    }
+    
+    private YamlPluginConfiguration createYamlPluginConfiguration(final String host, final String password, final int port, final Properties props) {
+        YamlPluginConfiguration result = new YamlPluginConfiguration();
+        result.setHost(host);
+        result.setPassword(password);
+        result.setPort(port);
+        result.setProps(props);
+        return result;
     }
 }
