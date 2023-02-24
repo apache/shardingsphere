3ed39b3d67fd2764d22cf237e2b7b68de211896c
jiangML <1060319118@qq.com>
2023-02-24 15:31:27 +0800

    add agent logging e2e test

---
+++ test/e2e/agent/plugins/logging/file/src/test/java/org/apache/shardingsphere/test/e2e/agent/file/FilePluginE2EIT.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.test.e2e.agent.file;
+
+import lombok.SneakyThrows;
+import org.apache.shardingsphere.test.e2e.agent.common.BasePluginE2EIT;
+import org.apache.shardingsphere.test.e2e.agent.file.asserts.ContentAssert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public final class FilePluginE2EIT extends BasePluginE2EIT {
+    
+    @SneakyThrows(IOException.class)
+    @Test
+    public void assertProxyWithAgent() {
+        super.assertProxyWithAgent();
+        Collection<String> expectedLogRegexs = getExpectedLogRegex();
+        expectedLogRegexs.forEach(ContentAssert::assertIs);
+    }
+    
+    private Collection<String> getExpectedLogRegex() {
+        Collection<String> result = new LinkedList<>();
+        result.add("Build meta data contexts finished, cost [1-9]* milliseconds");
+        return result;
+    }
+}
