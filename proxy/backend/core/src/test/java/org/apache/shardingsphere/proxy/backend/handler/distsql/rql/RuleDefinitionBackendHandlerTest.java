8c2018b14e2f566e94d9fcda7c51823852d0e075
zhangliang <zhangliang@apache.org>
2023-02-24 15:40:25 +0800

    Remove ProxyContextRestorer on RuleDefinitionBackendHandlerTest

--- proxy/backend/core/src/test/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/RuleDefinitionBackendHandlerTest.java
+++ proxy/backend/core/src/test/java/org/apache/shardingsphere/proxy/backend/handler/distsql/rql/RuleDefinitionBackendHandlerTest.java
@@ -23,15 +23,14 @@ import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
+import org.apache.shardingsphere.proxy.backend.handler.distsql.fixture.CreateFixtureRuleStatement;
+import org.apache.shardingsphere.proxy.backend.handler.distsql.rdl.rule.RuleDefinitionBackendHandler;
 import org.apache.shardingsphere.proxy.backend.response.header.ResponseHeader;
 import org.apache.shardingsphere.proxy.backend.response.header.update.UpdateResponseHeader;
 import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
-import org.apache.shardingsphere.proxy.backend.handler.distsql.fixture.CreateFixtureRuleStatement;
-import org.apache.shardingsphere.proxy.backend.handler.distsql.rdl.rule.RuleDefinitionBackendHandler;
-import org.apache.shardingsphere.proxy.backend.util.ProxyContextRestorer;
 import org.apache.shardingsphere.transaction.api.TransactionType;
-import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
 
 import java.sql.SQLException;
 import java.util.Collections;
@@ -41,32 +40,39 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 
-public final class RuleDefinitionBackendHandlerTest extends ProxyContextRestorer {
-    
-    @Before
-    public void setUp() {
-        ProxyContext.init(mockContextManager());
-    }
-    
-    private ContextManager mockContextManager() {
-        ContextManager result = mock(ContextManager.class, RETURNS_DEEP_STUBS);
-        ShardingSphereDatabase database = mock(ShardingSphereDatabase.class, RETURNS_DEEP_STUBS);
-        ShardingSphereRuleMetaData ruleMetaData = mock(ShardingSphereRuleMetaData.class, RETURNS_DEEP_STUBS);
-        when(ruleMetaData.getConfigurations()).thenReturn(Collections.emptyList());
-        when(database.getRuleMetaData()).thenReturn(ruleMetaData);
-        when(result.getMetaDataContexts().getMetaData().containsDatabase("test")).thenReturn(true);
-        when(result.getMetaDataContexts().getMetaData().getDatabase("test")).thenReturn(database);
-        return result;
-    }
+public final class RuleDefinitionBackendHandlerTest {
     
     @Test
     public void assertExecute() throws SQLException {
         ConnectionSession connectionSession = new ConnectionSession(mock(MySQLDatabaseType.class), TransactionType.LOCAL, new DefaultAttributeMap());
         connectionSession.setCurrentDatabase("test");
-        ResponseHeader response = new RuleDefinitionBackendHandler<>(new CreateFixtureRuleStatement(), connectionSession).execute();
-        assertThat(response, instanceOf(UpdateResponseHeader.class));
-        assertThat(connectionSession.getTransactionStatus().getTransactionType(), is(TransactionType.LOCAL));
+        ShardingSphereDatabase database = mockDatabase();
+        ContextManager contextManager = mockContextManager(database);
+        try (MockedStatic<ProxyContext> proxyContext = mockStatic(ProxyContext.class, RETURNS_DEEP_STUBS)) {
+            proxyContext.when(() -> ProxyContext.getInstance().getContextManager()).thenReturn(contextManager);
+            proxyContext.when(() -> ProxyContext.getInstance().databaseExists("test")).thenReturn(true);
+            proxyContext.when(() -> ProxyContext.getInstance().getDatabase("test")).thenReturn(database);
+            ResponseHeader response = new RuleDefinitionBackendHandler<>(new CreateFixtureRuleStatement(), connectionSession).execute();
+            assertThat(response, instanceOf(UpdateResponseHeader.class));
+            assertThat(connectionSession.getTransactionStatus().getTransactionType(), is(TransactionType.LOCAL));
+        }
+    }
+    
+    private ContextManager mockContextManager(final ShardingSphereDatabase database) {
+        ContextManager result = mock(ContextManager.class, RETURNS_DEEP_STUBS);
+        when(result.getMetaDataContexts().getMetaData().containsDatabase("test")).thenReturn(true);
+        when(result.getMetaDataContexts().getMetaData().getDatabase("test")).thenReturn(database);
+        return result;
+    }
+    
+    private static ShardingSphereDatabase mockDatabase() {
+        ShardingSphereDatabase result = mock(ShardingSphereDatabase.class, RETURNS_DEEP_STUBS);
+        ShardingSphereRuleMetaData ruleMetaData = mock(ShardingSphereRuleMetaData.class, RETURNS_DEEP_STUBS);
+        when(ruleMetaData.getConfigurations()).thenReturn(Collections.emptyList());
+        when(result.getRuleMetaData()).thenReturn(ruleMetaData);
+        return result;
     }
 }
