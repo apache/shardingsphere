d0ef4801ee4cb498e461a67966f7bb6a7823996c
Liang Zhang <zhangliang@apache.org>
2023-03-02 17:21:34 +0800

    Use junit 5.x's API to refactor InternalUnsupportedSQLParserIT (#24421)

--- sql-parser/dialect/oracle/src/test/java/org/apache/shardingsphere/test/it/sql/parser/it/oracle/internal/InternalUnsupportedOracleParserIT.java
+++ sql-parser/dialect/oracle/src/test/java/org/apache/shardingsphere/test/it/sql/parser/it/oracle/internal/InternalUnsupportedOracleParserIT.java
@@ -17,23 +17,9 @@
 
 package org.apache.shardingsphere.test.it.sql.parser.it.oracle.internal;
 
+import org.apache.shardingsphere.test.it.sql.parser.internal.InternalSQLParserITSettings;
 import org.apache.shardingsphere.test.it.sql.parser.internal.InternalUnsupportedSQLParserIT;
-import org.apache.shardingsphere.test.it.sql.parser.internal.InternalSQLParserTestParameter;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
+@InternalSQLParserITSettings("Oracle")
 public final class InternalUnsupportedOracleParserIT extends InternalUnsupportedSQLParserIT {
-    
-    public InternalUnsupportedOracleParserIT(final InternalSQLParserTestParameter testParam) {
-        super(testParam);
-    }
-    
-    @Parameters(name = "{0}")
-    public static Collection<InternalSQLParserTestParameter> getTestParameters() {
-        return getTestParameters("Oracle");
-    }
 }
