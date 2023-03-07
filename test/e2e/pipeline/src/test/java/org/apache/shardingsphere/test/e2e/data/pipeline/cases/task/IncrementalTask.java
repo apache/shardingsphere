8e2fa56dc5f58fd094a356b3fba13d004de0d4ba
azexcy <13588031592@qq.com>
2023-03-07 20:20:48 +0800

    Improve CI

--- test/e2e/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/data/pipeline/cases/task/IncrementalTask.java
+++ test/e2e/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/data/pipeline/cases/task/IncrementalTask.java
@@ -36,6 +36,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.Year;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
@@ -44,12 +45,12 @@ import java.util.concurrent.ThreadLocalRandom;
 @Slf4j
 public final class IncrementalTask extends BaseIncrementTask {
     
-    private static final String MYSQL_UPDATE_ORDER_BY_ID = "UPDATE `%s` SET status = ?, t_mediumint=?, t_smallint=?, t_tinyint=?, t_unsigned_int=?, t_unsigned_mediumint=?, t_unsigned_smallint=?, "
-            + "t_unsigned_tinyint=?, t_float=?, t_double=?, t_decimal=?, t_timestamp=?, t_datetime=?, t_date=?, t_time=?, t_year=?, t_bit=?, t_binary=?, t_varbinary=?, t_blob=?, t_mediumblob=?, "
-            + "t_char=?, t_text=?, t_mediumtext=?, t_enum=?, t_set=?, t_json=? WHERE order_id = ?";
+    private static final List<String> MYSQL_COLUMN_NAMES = Arrays.asList("status", "t_mediumint", "t_smallint", "t_tinyint", "t_unsigned_int", "t_unsigned_mediumint", "t_unsigned_smallint",
+            "t_unsigned_tinyint", "t_float", "t_double", "t_decimal", "t_timestamp", "t_datetime", "t_date", "t_time", "t_year", "t_bit", "t_binary", "t_varbinary", "t_blob", "t_mediumblob",
+            "t_char", "t_text", "t_mediumtext", "t_enum", "t_set", "t_json");
     
-    private static final String POSTGRESQL_UPDATE_ORDER_BY_ID = "UPDATE %S SET status=?, t_int2=?, t_numeric=?, t_bool=?, t_bytea=?, t_char=?, t_float=?, t_double=?, t_json=?, t_jsonb=?, t_text=?, "
-            + "t_date=?, t_time=?, t_timestamp=?, t_timestamptz=? WHERE order_id = ?";
+    private static final List<String> POSTGRESQL_COLUMN_NAMES = Arrays.asList("status", "t_int2", "t_numeric", "t_bool", "t_bytea", "t_char", "t_float", "t_double", "t_json", "t_jsonb", "t_text",
+            "t_date", "t_time", "t_timestamp", "t_timestamptz");
     
     private final DataSource dataSource;
     
@@ -76,11 +77,9 @@ public final class IncrementalTask extends BaseIncrementTask {
             // TODO 0000-00-00 00:00:00 now will cause consistency check failed of MySQL.
             // DataSourceUtil.execute(dataSource, String.format("UPDATE %s SET t_datetime='0000-00-00 00:00:00' WHERE order_id = ?", orderTableName)
             updateOrderById(primaryKeys.get(random.nextInt(0, primaryKeys.size())));
-            if (databaseType instanceof MySQLDatabaseType) {
-                setNullToUnsignedFields(primaryKeys.get(random.nextInt(0, primaryKeys.size())));
-            }
         }
         for (int i = 0; i < Math.max(1, loopCount / 5); i++) {
+            setNullToAllFields(primaryKeys.get(random.nextInt(0, primaryKeys.size())));
             deleteOrderById(primaryKeys.get(random.nextInt(0, primaryKeys.size())));
         }
         log.info("increment task runnable execute successfully.");
@@ -89,20 +88,31 @@ public final class IncrementalTask extends BaseIncrementTask {
     private void updateOrderById(final Object orderId) {
         int randomInt = ThreadLocalRandom.current().nextInt(-100, 100);
         if (databaseType instanceof MySQLDatabaseType) {
-            String updateSql = String.format(MYSQL_UPDATE_ORDER_BY_ID, orderTableName);
+            String sql = String.format(buildUpdateSql(MYSQL_COLUMN_NAMES, "?"), orderTableName);
             int randomUnsignedInt = ThreadLocalRandom.current().nextInt(10, 100);
             LocalDateTime now = LocalDateTime.now();
-            DataSourceExecuteUtil.execute(dataSource, updateSql, new Object[]{"中文测试", randomInt, randomInt, randomInt, randomUnsignedInt, randomUnsignedInt, randomUnsignedInt,
-                    randomUnsignedInt, 1.0F, 1.0, new BigDecimal("999"), now, now, now.toLocalDate(), now.toLocalTime(), Year.now().getValue() + 1, "U".getBytes(), "P".getBytes(),
+            DataSourceExecuteUtil.execute(dataSource, sql, new Object[]{"中文测试", randomInt, randomInt, randomInt, randomUnsignedInt, randomUnsignedInt, randomUnsignedInt,
+                    // TODO if the bytes contain negative number just like -1, parse not correctly, need to be resolved
+                    randomUnsignedInt, 1.0F, 1.0, new BigDecimal("999"), now, now, now.toLocalDate(), now.toLocalTime(), Year.now().getValue() + 1, new byte[]{}, new byte[]{1, 2, -1, -3},
                     "D".getBytes(), "A".getBytes(), "T".getBytes(), "E", "text", "mediumText", "3", "3", PipelineCaseHelper.generateJsonString(32, true), orderId});
             return;
         }
         if (databaseType instanceof PostgreSQLDatabaseType || databaseType instanceof OpenGaussDatabaseType) {
-            String updateSql = String.format(POSTGRESQL_UPDATE_ORDER_BY_ID, orderTableName);
-            DataSourceExecuteUtil.execute(dataSource, updateSql, new Object[]{"中文测试", randomInt, BigDecimal.valueOf(10000), true, "bytea-update".getBytes(), "update", 1.0F, 2.0,
+            String sql = String.format(buildUpdateSql(POSTGRESQL_COLUMN_NAMES, "?"), orderTableName);
+            DataSourceExecuteUtil.execute(dataSource, sql, new Object[]{"中文测试", randomInt, BigDecimal.valueOf(10000), true, new byte[]{}, "update", 1.0F, 2.0,
                     PipelineCaseHelper.generateJsonString(10, true), PipelineCaseHelper.generateJsonString(20, true), "text-update", LocalDate.now(),
                     LocalTime.now(), Timestamp.valueOf(LocalDateTime.now()), OffsetDateTime.now(), orderId});
         }
+    }
+    
+    private String buildUpdateSql(final List<String> columnNames, final String placeholder) {
+        StringBuilder sql = new StringBuilder("UPDATE `%s` SET ");
+        for (String each : columnNames) {
+            sql.append(each).append("=").append(placeholder).append(",");
+        }
+        sql.setLength(sql.length() - 1);
+        sql.append(" WHERE order_id=?");
+        return sql.toString();
     }
     
     private void deleteOrderById(final Object orderId) {
@@ -110,8 +120,10 @@ public final class IncrementalTask extends BaseIncrementTask {
         DataSourceExecuteUtil.execute(dataSource, sql, new Object[]{orderId});
     }
     
-    private void setNullToUnsignedFields(final Object orderId) {
-        DataSourceExecuteUtil.execute(dataSource, String.format("UPDATE %s SET t_unsigned_int = null, t_unsigned_tinyint = null WHERE order_id = ?", orderTableName),
-                new Object[]{orderId});
+    private void setNullToAllFields(final Object orderId) {
+        if (databaseType instanceof MySQLDatabaseType) {
+            String sql = buildUpdateSql(MYSQL_COLUMN_NAMES, "null");
+            DataSourceExecuteUtil.execute(dataSource, sql, new Object[]{orderId});
+        }
     }
 }
