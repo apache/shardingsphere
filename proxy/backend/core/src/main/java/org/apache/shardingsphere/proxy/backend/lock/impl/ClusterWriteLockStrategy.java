8879b1a2f5da5b9c49b2614db596ee7eecf3ed90
Pace2Car <Pace2Car@163.com>
2023-03-22 14:32:06 +0800

    Refactor cluster lock

--- proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/lock/impl/ClusterWriteLockStrategy.java
+++ proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/lock/impl/ClusterWriteLockStrategy.java
@@ -36,9 +36,14 @@ public class ClusterWriteLockStrategy implements ClusterLockStrategy {
     public void lock() {
         ContextManager contextManager = ProxyContext.getInstance().getContextManager();
         LockContext lockContext = contextManager.getInstanceContext().getLockContext();
-        if (lockContext.tryLock(new GlobalLockDefinition(GlobalLockNames.CLUSTER_LOCK.getLockName()), -1)) {
-            contextManager.getInstanceContext().getEventBusContext().post(new ClusterStatusChangedEvent(ClusterState.READ_ONLY));
-            // TODO lock snapshot info
+        GlobalLockDefinition lockDefinition = new GlobalLockDefinition(GlobalLockNames.CLUSTER_LOCK.getLockName());
+        if (lockContext.tryLock(lockDefinition, 3000L)) {
+            try {
+                contextManager.getInstanceContext().getEventBusContext().post(new ClusterStatusChangedEvent(ClusterState.READ_ONLY));
+                // TODO lock snapshot info
+            } finally {
+                lockContext.unlock(lockDefinition);
+            }
         }
     }
     
