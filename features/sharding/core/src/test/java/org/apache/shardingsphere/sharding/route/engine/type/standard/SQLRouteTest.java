90b5efb6464c2c1c3bbfed3b99d792c255d7063b
Liang Zhang <zhangliang@apache.org>
2023-03-27 14:59:28 +0800

    Refactor ShardingRouteAssert (#24855)

--- features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/route/engine/type/standard/SQLRouteTest.java
+++ features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/route/engine/type/standard/SQLRouteTest.java
@@ -17,40 +17,42 @@
 
 package org.apache.shardingsphere.sharding.route.engine.type.standard;
 
+import org.apache.shardingsphere.sharding.route.engine.type.standard.assertion.ShardingRouteAssert;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-class SQLRouteTest extends AbstractSQLRouteTest {
+// TODO add assertion for ShardingRouteAssert.assertRoute
+class SQLRouteTest {
     
     @Test
     void assertNoTableUnicastRandomDataSource() {
         String sql = "SELECT id,name ";
-        assertRoute(sql, Collections.singletonList(1));
+        ShardingRouteAssert.assertRoute(sql, Collections.singletonList(1));
     }
     
     @Test
     void assertWithBroadcastTable() {
         String sql = "SELECT id,name from t_order_item a join t_product b on a.product_id = b.product_id where user_id = ?";
-        assertRoute(sql, Collections.singletonList(1));
+        ShardingRouteAssert.assertRoute(sql, Collections.singletonList(1));
     }
     
     @Test
     void assertAllBindingWithBroadcastTable() {
         String sql = "SELECT id,name from t_order a join t_order_item b on a.order_id = b.order_id join t_product c on b.product_id = c.product_id where a.user_id = ?";
-        assertRoute(sql, Collections.singletonList(1));
+        ShardingRouteAssert.assertRoute(sql, Collections.singletonList(1));
     }
     
     @Test
     void assertComplexTableWithBroadcastTable() {
         String sql = "SELECT id,name from t_order a join t_user b on a.user_id = b.user_id join t_product c on a.product_id = c.product_id where a.user_id = ? and b.user_id =?";
-        assertRoute(sql, Arrays.asList(1, 1));
+        ShardingRouteAssert.assertRoute(sql, Arrays.asList(1, 1));
     }
     
     @Test
     void assertInsertTable() {
         String sql = "INSERT INTO t_order (order_id, user_id) VALUES (?, ?)";
-        assertRoute(sql, Arrays.asList(1, 1));
+        ShardingRouteAssert.assertRoute(sql, Arrays.asList(1, 1));
     }
 }
