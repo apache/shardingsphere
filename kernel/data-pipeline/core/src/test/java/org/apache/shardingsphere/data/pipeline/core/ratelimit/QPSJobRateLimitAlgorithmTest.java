6b70742798012ff1efe20a51336292122bbc6f02
insist777 <3307763375@qq.com>
2023-04-03 23:59:55 +0800

    Improve code style

--- kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/core/ratelimit/QPSJobRateLimitAlgorithmTest.java
+++ kernel/data-pipeline/core/src/test/java/org/apache/shardingsphere/data/pipeline/core/ratelimit/QPSJobRateLimitAlgorithmTest.java
@@ -31,7 +31,7 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class QPSJobRateLimitAlgorithmTest {
+class QPSJobRateLimitAlgorithmTest {
 
     private QPSJobRateLimitAlgorithm qpsJobRateLimitAlgorithm;
     
