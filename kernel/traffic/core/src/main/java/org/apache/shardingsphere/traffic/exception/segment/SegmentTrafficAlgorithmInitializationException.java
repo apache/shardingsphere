4c05a81330a2cb7baac61654fd3fc2bb6d5be141
Netter99 <2501259757@qq.com>
2023-04-07 18:42:27 +0800

    change CRLF style to LF style

--- kernel/traffic/core/src/main/java/org/apache/shardingsphere/traffic/exception/segment/SegmentTrafficAlgorithmInitializationException.java
+++ kernel/traffic/core/src/main/java/org/apache/shardingsphere/traffic/exception/segment/SegmentTrafficAlgorithmInitializationException.java
@@ -5,7 +5,9 @@ import org.apache.shardingsphere.traffic.exception.TrafficException;
 
 public class SegmentTrafficAlgorithmInitializationException extends TrafficException {
 
-    public SegmentTrafficAlgorithmInitializationException(final String methodName, final String reason) {
-        super(XOpenSQLState.GENERAL_ERROR, 98, "Segmentation traffic algorithm `%s` initialization failed, reason is: %s.", methodName, reason);
+    private static final long serialVersionUID = 7514112348846701284L;
+
+    public SegmentTrafficAlgorithmInitializationException(final String segmentTrafficType, final String reason) {
+        super(XOpenSQLState.GENERAL_ERROR, 98, "Segmentation traffic algorithm `%s` initialization failed, reason is: %s.", segmentTrafficType, reason);
     }
 }
