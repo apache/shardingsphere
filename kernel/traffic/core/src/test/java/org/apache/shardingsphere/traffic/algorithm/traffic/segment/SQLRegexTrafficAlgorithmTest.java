475c6a9798367b0bef2bcccdae87e9967aaeed55
Netter99 <2501259757@qq.com>
2023-04-09 16:23:02 +0800

    modify the properties of RegexTest

--- kernel/traffic/core/src/test/java/org/apache/shardingsphere/traffic/algorithm/traffic/segment/SQLRegexTrafficAlgorithmTest.java
+++ kernel/traffic/core/src/test/java/org/apache/shardingsphere/traffic/algorithm/traffic/segment/SQLRegexTrafficAlgorithmTest.java
@@ -63,7 +63,7 @@ class SQLRegexTrafficAlgorithmTest {
 
     @Test
     void assertInitWithIllegalProps() {
-        assertThrows(IllegalArgumentException.class, () -> sqlRegexAlgorithm = TypedSPILoader.getService(SQLRegexTrafficAlgorithm.class, "SQL_MATCH",
-                PropertiesBuilder.build(new Property("sql", ""))));
+        assertThrows(IllegalArgumentException.class, () -> sqlRegexAlgorithm = TypedSPILoader.getService(SQLRegexTrafficAlgorithm.class, "SQL_REGEX",
+                PropertiesBuilder.build(new Property("regex", ""))));
     }
 }
