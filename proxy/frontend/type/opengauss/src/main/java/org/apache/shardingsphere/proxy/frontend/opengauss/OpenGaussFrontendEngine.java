728437c12c4cdf6061515b34d4170454e16c01eb
吴伟杰 <wuweijie@apache.org>
2023-04-10 18:58:53 +0800

    Unify Proxy backend thread model

--- proxy/frontend/type/opengauss/src/main/java/org/apache/shardingsphere/proxy/frontend/opengauss/OpenGaussFrontendEngine.java
+++ proxy/frontend/type/opengauss/src/main/java/org/apache/shardingsphere/proxy/frontend/opengauss/OpenGaussFrontendEngine.java
@@ -23,7 +23,6 @@ import org.apache.shardingsphere.db.protocol.opengauss.codec.OpenGaussPacketCode
 import org.apache.shardingsphere.db.protocol.postgresql.constant.PostgreSQLServerInfo;
 import org.apache.shardingsphere.dialect.exception.transaction.InTransactionException;
 import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
-import org.apache.shardingsphere.proxy.frontend.context.FrontendContext;
 import org.apache.shardingsphere.proxy.frontend.opengauss.authentication.OpenGaussAuthenticationEngine;
 import org.apache.shardingsphere.proxy.frontend.opengauss.command.OpenGaussCommandExecuteEngine;
 import org.apache.shardingsphere.proxy.frontend.postgresql.PostgreSQLFrontendEngine;
@@ -43,11 +42,6 @@ public final class OpenGaussFrontendEngine implements DatabaseProtocolFrontendEn
     private final OpenGaussCommandExecuteEngine commandExecuteEngine = new OpenGaussCommandExecuteEngine();
     
     private final OpenGaussPacketCodecEngine codecEngine = new OpenGaussPacketCodecEngine();
-    
-    @Override
-    public FrontendContext getFrontendContext() {
-        return postgreSQLFrontendEngine.getFrontendContext();
-    }
     
     @Override
     public void setDatabaseVersion(final String databaseName, final String databaseVersion) {
