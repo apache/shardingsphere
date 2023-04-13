48370f1fa73c0bf1cc3e37132b6e8e54a3ce00e1
azexcy <13588031592@qq.com>
2023-04-13 19:45:01 +0800

    Fix the parameter may not be in effect at jdbcUrl

--- infra/common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/creator/DataSourceReflection.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/datasource/pool/creator/DataSourceReflection.java
@@ -173,8 +173,6 @@ public final class DataSourceReflection {
             String defaultPropertyValue = entry.getValue().toString();
             if (!containsDefaultProperty(defaultPropertyKey, jdbcConnectionProps.get(), queryProps)) {
                 jdbcConnectionProps.get().setProperty(defaultPropertyKey, defaultPropertyValue);
-            } else if (queryProps.containsKey(defaultPropertyKey) && jdbcConnectionProps.get().containsKey(defaultPropertyKey)) {
-                jdbcConnectionProps.get().setProperty(defaultPropertyKey, queryProps.getProperty(defaultPropertyKey));
             }
         }
     }
