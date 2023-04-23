03f0e42452fb4e008deafdfbdc43600ff7049395
ZhangCheng <flyingzc@outlook.com>
2023-04-23 10:39:57 +0800

    fix

--- infra/common/src/main/java/org/apache/shardingsphere/infra/metadata/database/schema/builder/SystemSchemaBuilderRule.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/metadata/database/schema/builder/SystemSchemaBuilderRule.java
@@ -128,7 +128,7 @@ public enum SystemSchemaBuilderRule {
      * Value of builder rule.
      *
      * @param databaseType database type
-     * @param schema       schema
+     * @param schema schema
      * @return builder rule
      */
     public static SystemSchemaBuilderRule valueOf(final String databaseType, final String schema) {
@@ -141,7 +141,7 @@ public enum SystemSchemaBuilderRule {
     /**
      * Judge whether current table is system table or not.
      *
-     * @param schema    schema
+     * @param schema schema
      * @param tableName table name
      * @return whether current table is system table or not
      */
