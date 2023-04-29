90ea218f198ef153fcbd82b2813231c3e8c4c447
totalo <totalo@apache.org>
2023-04-29 14:57:14 +0800

    for code review

--- test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/external/loader/ExternalSQLParserTestParameterLoader.java
+++ test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/external/loader/ExternalSQLParserTestParameterLoader.java
@@ -37,6 +37,7 @@ public final class ExternalSQLParserTestParameterLoader extends AbstractTestPara
     
     /**
      * create external SQL parser test parameters.
+     * 
      * @param sqlCaseFileName SQL case file name
      * @param sqlCaseFileContent SQL case file content
      * @param resultFileContent result file content
