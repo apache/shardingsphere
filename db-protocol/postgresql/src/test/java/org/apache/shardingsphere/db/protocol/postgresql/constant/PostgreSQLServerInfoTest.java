10db2fdc273e054dfd0f0d72931aedc28247b55f
zhangliang <zhangliang@apache.org>
2023-05-09 12:54:44 +0800

    Fix sonar could issue of PostgreSQLServerInfo

--- db-protocol/postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/constant/PostgreSQLServerInfoTest.java
+++ db-protocol/postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/constant/PostgreSQLServerInfoTest.java
@@ -31,7 +31,7 @@ class PostgreSQLServerInfoTest {
         PostgreSQLServerInfo.setServerVersion("foo_db", "13.2");
         assertThat(PostgreSQLServerInfo.getServerVersion("foo_db"), is("13.2-ShardingSphere-Proxy 5.0.0"));
     }
-
+    
     @Test
     void assertSetServerVersionForNull() {
         CommonConstants.PROXY_VERSION.set("5.0.0");
