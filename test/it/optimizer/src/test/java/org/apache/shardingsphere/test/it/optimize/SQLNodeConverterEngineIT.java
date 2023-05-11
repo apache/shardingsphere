009002ae1fc6b4c904700edb36b9859f43623c2d
kanha gupta <92207457+kanha-gupta@users.noreply.github.com>
2023-05-11 14:22:10 +0530

    support for boolean primary SQL (#25564)

--- test/it/optimizer/src/test/java/org/apache/shardingsphere/test/it/optimize/SQLNodeConverterEngineIT.java
+++ test/it/optimizer/src/test/java/org/apache/shardingsphere/test/it/optimize/SQLNodeConverterEngineIT.java
@@ -206,6 +206,8 @@ class SQLNodeConverterEngineIT {
             result.add("select_where_with_bit_expr_with_plus_interval");
             result.add("select_where_with_bit_expr_with_minus_interval");
             result.add("select_where_with_predicate_with_in_subquery");
+            result.add("select_where_with_boolean_primary_with_is");
+            result.add("select_where_with_boolean_primary_with_is_not");
             return result;
         }
         // CHECKSTYLE:ON
