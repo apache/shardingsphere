1e85c24e56c5d161ed0e447f29c3e58f1a23fc39
Cong Hu <iamhucong@qq.com>
2023-05-12 16:26:49 +0800

    Rename ExternalPostgreSQLTestParameterLoader to DefaultExternalTestParameterLoader. (#25599)
    * Rename ExternalPostgreSQLTestParameterLoader to DefaultExternalTestParameterLoader.
    
    * Update DefaultExternalTestParameterLoader.java
    
    ---------
    
    Co-authored-by: Zhengqiang Duan <duanzhengqiang@apache.org>

--- parser/sql/dialect/postgresql/src/test/java/org/apache/shardingsphere/test/it/sql/parser/it/postgresql/external/ExternalPostgreSQLParserIT.java
+++ parser/sql/dialect/postgresql/src/test/java/org/apache/shardingsphere/test/it/sql/parser/it/postgresql/external/ExternalPostgreSQLParserIT.java
@@ -18,10 +18,10 @@
 package org.apache.shardingsphere.test.it.sql.parser.it.postgresql.external;
 
 import org.apache.shardingsphere.test.it.sql.parser.external.ExternalSQLParserIT;
-import org.apache.shardingsphere.test.it.sql.parser.external.loader.ExternalPostgreSQLTestParameterLoader;
+import org.apache.shardingsphere.test.it.sql.parser.external.loader.DefaultExternalTestParameterLoader;
 import org.apache.shardingsphere.test.loader.ExternalCaseSettings;
 
-@ExternalCaseSettings(value = "PostgreSQL", caseURL = ExternalPostgreSQLParserIT.CASE_URL, resultURL = ExternalPostgreSQLParserIT.RESULT_URL, testParameterLoader = ExternalPostgreSQLTestParameterLoader.class)
+@ExternalCaseSettings(value = "PostgreSQL", caseURL = ExternalPostgreSQLParserIT.CASE_URL, resultURL = ExternalPostgreSQLParserIT.RESULT_URL, testParameterLoader = DefaultExternalTestParameterLoader.class)
 class ExternalPostgreSQLParserIT extends ExternalSQLParserIT {
     
     static final String CASE_URL = "https://github.com/postgres/postgres/tree/master/src/test/regress/sql";
