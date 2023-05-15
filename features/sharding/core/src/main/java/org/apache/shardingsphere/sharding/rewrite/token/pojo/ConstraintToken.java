c4549c0bc09e3d5512b91a1b9a661f6925955204
zhangliang <zhangliang@apache.org>
2023-05-15 12:22:03 +0800

    Fix sonar issue of TokenUtilsTest

--- features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/rewrite/token/pojo/ConstraintToken.java
+++ features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/rewrite/token/pojo/ConstraintToken.java
@@ -57,7 +57,7 @@ public final class ConstraintToken extends SQLToken implements Substitutable, Ro
     
     private String getConstraintValue(final RouteUnit routeUnit) {
         StringBuilder result = new StringBuilder(identifier.getValue());
-        Map<String, String> logicAndActualTables = TokenUtils.getLogicAndActualTables(routeUnit, sqlStatementContext, shardingRule);
+        Map<String, String> logicAndActualTables = TokenUtils.getLogicAndActualTableMap(routeUnit, sqlStatementContext, shardingRule);
         sqlStatementContext.getTablesContext().getTableNames().stream().findFirst().map(optional -> logicAndActualTables.get(optional.toLowerCase()))
                 .ifPresent(optional -> result.append('_').append(optional));
         return result.toString();
