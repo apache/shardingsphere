404bb6ce58c489978f948634fdcfc7baf29960df
吴伟杰 <wuweijie@apache.org>
2023-05-22 11:05:48 +0800

    Cleanup unused import

--- proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/connector/DatabaseConnector.java
+++ proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/connector/DatabaseConnector.java
@@ -93,7 +93,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Database connector.
