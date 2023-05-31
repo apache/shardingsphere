37ea45d302d75b82f71f5af5f12b90f131704060
azexcy <13588031592@qq.com>
2023-05-31 15:50:51 +0800

    Improve sleep

--- kernel/data-pipeline/dialect/opengauss/src/main/java/org/apache/shardingsphere/data/pipeline/opengauss/ingest/OpenGaussWALDumper.java
+++ kernel/data-pipeline/dialect/opengauss/src/main/java/org/apache/shardingsphere/data/pipeline/opengauss/ingest/OpenGaussWALDumper.java
@@ -93,8 +93,10 @@ public final class OpenGaussWALDumper extends AbstractLifecycleExecutor implemen
                 break;
             } catch (final SQLException ex) {
                 int times = reconnectTimes.incrementAndGet();
-                Thread.sleep(Math.min(10 * 1000L, 1000L << times));
                 log.error("Connect failed, reconnect times={}", times, ex);
+                if (isRunning()) {
+                    Thread.sleep(5000);
+                }
                 if (times >= 5) {
                     throw new IngestException(ex);
                 }
