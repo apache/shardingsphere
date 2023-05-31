2c033f9a15108cd8dda99fe41666e8b3362091a4
niu niu <zihaoAK47@aliyun.com>
2023-05-31 16:45:49 +0800

    Change drop index parser rule (#25852)

--- parser/sql/dialect/mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/statement/type/MySQLDDLStatementVisitor.java
+++ parser/sql/dialect/mysql/src/main/java/org/apache/shardingsphere/sql/parser/mysql/visitor/statement/type/MySQLDDLStatementVisitor.java
@@ -632,11 +632,13 @@ public final class MySQLDDLStatementVisitor extends MySQLStatementVisitor implem
         result.setTable((SimpleTableSegment) visit(ctx.tableName()));
         IndexNameSegment indexName = new IndexNameSegment(ctx.indexName().start.getStartIndex(), ctx.indexName().stop.getStopIndex(), new IdentifierValue(ctx.indexName().getText()));
         result.getIndexes().add(new IndexSegment(ctx.indexName().start.getStartIndex(), ctx.indexName().stop.getStopIndex(), indexName));
-        if (null != ctx.alterAlgorithmOption()) {
-            result.setAlgorithmSegment((AlgorithmTypeSegment) visit(ctx.alterAlgorithmOption()));
-        }
-        if (null != ctx.alterLockOption()) {
-            result.setLockTableSegment((LockTableSegment) visit(ctx.alterLockOption()));
+        if (null != ctx.algorithmOptionAndLockOption()) {
+            if (null != ctx.algorithmOptionAndLockOption().alterAlgorithmOption()) {
+                result.setAlgorithmSegment((AlgorithmTypeSegment) visit(ctx.algorithmOptionAndLockOption().alterAlgorithmOption()));
+            }
+            if (null != ctx.algorithmOptionAndLockOption().alterLockOption()) {
+                result.setLockTableSegment((LockTableSegment) visit(ctx.algorithmOptionAndLockOption().alterLockOption()));
+            }
         }
         return result;
     }
