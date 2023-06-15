274c368755c6c877733a6215a483443ab5f0cb05
zhaojinchao <zjcnb0215@gmail.com>
2023-06-15 16:41:02 +0800

    Fix ci

--- mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/coordinator/subscriber/NewDataSourceChangedSubscriber.java
+++ mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/coordinator/subscriber/NewDataSourceChangedSubscriber.java
@@ -17,19 +17,7 @@
 
 package org.apache.shardingsphere.mode.manager.cluster.coordinator.subscriber;
 
-import com.google.common.eventbus.Subscribe;
-import org.apache.shardingsphere.infra.config.rule.global.event.AlterGlobalRuleConfigurationEvent;
-import org.apache.shardingsphere.infra.config.rule.global.event.DeleteGlobalRuleConfigurationEvent;
-import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
-import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
-import org.apache.shardingsphere.infra.rule.builder.database.DatabaseRulesBuilder;
-import org.apache.shardingsphere.infra.rule.builder.global.GlobalRulesBuilder;
-import org.apache.shardingsphere.mode.event.config.DatabaseRuleConfigurationChangedEvent;
 import org.apache.shardingsphere.mode.manager.ContextManager;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
 
 /**
  * TODO Rename DataSourceChangedSubscriber when metadata structure adjustment completed. #25485
@@ -38,64 +26,7 @@ import java.util.LinkedList;
 @SuppressWarnings("UnstableApiUsage")
 public final class NewDataSourceChangedSubscriber {
     
-    private final ContextManager contextManager;
-    
     public NewDataSourceChangedSubscriber(final ContextManager contextManager) {
-        this.contextManager = contextManager;
         contextManager.getInstanceContext().getEventBusContext().register(this);
-    }
-    
-    /**
-     * Renew for database rule configuration.
-     *
-     * @param event database rule changed event
-     */
-    @Subscribe
-    public synchronized void renew(final DatabaseRuleConfigurationChangedEvent event) {
-        String databaseName = event.getDatabaseName();
-        ShardingSphereDatabase database = contextManager.getMetaDataContexts().getMetaData().getDatabase(databaseName);
-        Collection<ShardingSphereRule> rules = new LinkedList<>(database.getRuleMetaData().getRules());
-        rules.addAll(DatabaseRulesBuilder.build(databaseName, database.getResourceMetaData().getDataSources(), database.getRuleMetaData().getRules(),
-                event.getRuleConfig(), contextManager.getInstanceContext()));
-        database.getRuleMetaData().getRules().addAll(rules);
-    }
-    
-    /**
-     * Renew for global rule configuration.
-     *
-     * @param event global rule alter event
-     */
-    @Subscribe
-    public synchronized void renew(final AlterGlobalRuleConfigurationEvent event) {
-        ShardingSphereDatabase database = contextManager.getMetaDataContexts().getMetaData().getDatabase(event.getDatabaseName());
-        Collection<ShardingSphereRule> rules = removeSingleGlobalRule(database, event.getRuleSimpleName());
-        rules.addAll(GlobalRulesBuilder.buildRules(Collections.singletonList(event.getConfig()), contextManager.getMetaDataContexts().getMetaData().getDatabases(),
-                contextManager.getMetaDataContexts().getMetaData().getProps()));
-        database.getRuleMetaData().getRules().clear();
-        database.getRuleMetaData().getRules().addAll(rules);
-    }
-    
-    /**
-     * Renew for global rule configuration.
-     *
-     * @param event global rule delete event
-     */
-    @Subscribe
-    public synchronized void renew(final DeleteGlobalRuleConfigurationEvent event) {
-        ShardingSphereDatabase database = contextManager.getMetaDataContexts().getMetaData().getDatabase(event.getDatabaseName());
-        Collection<ShardingSphereRule> rules = removeSingleGlobalRule(database, event.getRuleSimpleName());
-        database.getRuleMetaData().getRules().clear();
-        database.getRuleMetaData().getRules().addAll(rules);
-    }
-    
-    private Collection<ShardingSphereRule> removeSingleGlobalRule(final ShardingSphereDatabase database, final String ruleSimpleName) {
-        Collection<ShardingSphereRule> result = new LinkedList<>(database.getRuleMetaData().getRules());
-        for (ShardingSphereRule each : result) {
-            if (!each.getType().equals(ruleSimpleName)) {
-                continue;
-            }
-            result.remove(each);
-        }
-        return result;
     }
 }
