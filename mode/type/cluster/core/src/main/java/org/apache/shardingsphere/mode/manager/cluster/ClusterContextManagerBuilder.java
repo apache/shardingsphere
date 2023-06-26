94c695a974e9b05091f071aff2d8dcc883862b73
zhaojinchao <zjcnb0215@gmail.com>
2023-06-26 16:07:56 +0800

    Rename cluster

--- mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/ClusterContextManagerBuilder.java
+++ mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/ClusterContextManagerBuilder.java
@@ -91,6 +91,6 @@ public final class ClusterContextManagerBuilder implements ContextManagerBuilder
     
     @Override
     public String getType() {
-        return "Stale_Cluster";
+        return "Compatible_Cluster";
     }
 }
