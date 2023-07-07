6053065f3479acf1ad2afd131d8bef3c8c0861a8
jiangML <1060319118@qq.com>
2023-07-07 14:42:03 +0800

    optimize parsed_sql_total and routed_sql_total metrics

---
+++ agent/plugins/core/src/main/java/org/apache/shardingsphere/agent/plugin/core/util/SQLStatementUtil.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.agent.plugin.core.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.shardingsphere.agent.plugin.core.enums.SQLStatementType;
+import org.apache.shardingsphere.distsql.parser.statement.DistSQLStatement;
+import org.apache.shardingsphere.distsql.parser.statement.ral.RALStatement;
+import org.apache.shardingsphere.distsql.parser.statement.rdl.RDLStatement;
+import org.apache.shardingsphere.distsql.parser.statement.rql.RQLStatement;
+import org.apache.shardingsphere.distsql.parser.statement.rul.RULStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dcl.DCLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.ddl.DDLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.DMLStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.DeleteStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.InsertStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.UpdateStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.tcl.TCLStatement;
+
+/**
+ * SQL statement util.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SQLStatementUtil {
+    
+    /**
+     * Get SQL type.
+     *
+     * @param sqlStatement SQL statement
+     * @return SQL statement type
+     */
+    public static SQLStatementType getType(final SQLStatement sqlStatement) {
+        if (null == sqlStatement) {
+            return SQLStatementType.OTHER;
+        }
+        if (sqlStatement instanceof DMLStatement) {
+            return getDMLType(sqlStatement);
+        }
+        if (sqlStatement instanceof DDLStatement) {
+            return SQLStatementType.DDL;
+        }
+        if (sqlStatement instanceof DCLStatement) {
+            return SQLStatementType.DCL;
+        }
+        if (sqlStatement instanceof DALStatement) {
+            return SQLStatementType.DAL;
+        }
+        if (sqlStatement instanceof TCLStatement) {
+            return SQLStatementType.TCL;
+        }
+        if (sqlStatement instanceof DistSQLStatement) {
+            return getDistSQLType(sqlStatement);
+        }
+        return SQLStatementType.OTHER;
+    }
+    
+    private static SQLStatementType getDMLType(final SQLStatement sqlStatement) {
+        if (sqlStatement instanceof SelectStatement) {
+            return SQLStatementType.SELECT;
+        }
+        if (sqlStatement instanceof InsertStatement) {
+            return SQLStatementType.INSERT;
+        }
+        if (sqlStatement instanceof UpdateStatement) {
+            return SQLStatementType.UPDATE;
+        }
+        if (sqlStatement instanceof DeleteStatement) {
+            return SQLStatementType.DELETE;
+        }
+        return SQLStatementType.DML;
+    }
+    
+    private static SQLStatementType getDistSQLType(final SQLStatement sqlStatement) {
+        if (sqlStatement instanceof RQLStatement) {
+            return SQLStatementType.RQL;
+        }
+        if (sqlStatement instanceof RDLStatement) {
+            return SQLStatementType.RDL;
+        }
+        if (sqlStatement instanceof RALStatement) {
+            return SQLStatementType.RAL;
+        }
+        if (sqlStatement instanceof RULStatement) {
+            return SQLStatementType.RUL;
+        }
+        return SQLStatementType.OTHER;
+    }
+}
--- agent/plugins/metrics/core/src/main/java/org/apache/shardingsphere/agent/plugin/metrics/core/advice/SQLParseCountAdvice.java
+++ agent/plugins/metrics/core/src/main/java/org/apache/shardingsphere/agent/plugin/metrics/core/advice/SQLParseCountAdvice.java
@@ -19,23 +19,12 @@ package org.apache.shardingsphere.agent.plugin.metrics.core.advice;
 
 import org.apache.shardingsphere.agent.api.advice.TargetAdviceObject;
 import org.apache.shardingsphere.agent.api.advice.type.InstanceMethodAdvice;
+import org.apache.shardingsphere.agent.plugin.core.util.SQLStatementUtil;
 import org.apache.shardingsphere.agent.plugin.metrics.core.collector.MetricsCollectorRegistry;
 import org.apache.shardingsphere.agent.plugin.metrics.core.collector.type.CounterMetricsCollector;
 import org.apache.shardingsphere.agent.plugin.metrics.core.config.MetricCollectorType;
 import org.apache.shardingsphere.agent.plugin.metrics.core.config.MetricConfiguration;
-import org.apache.shardingsphere.distsql.parser.statement.ral.RALStatement;
-import org.apache.shardingsphere.distsql.parser.statement.rdl.RDLStatement;
-import org.apache.shardingsphere.distsql.parser.statement.rql.RQLStatement;
-import org.apache.shardingsphere.distsql.parser.statement.rul.RULStatement;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dcl.DCLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.ddl.DDLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.DeleteStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.InsertStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.UpdateStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.tcl.TCLStatement;
 
 import java.lang.reflect.Method;
 import java.util.Collections;
@@ -55,42 +44,6 @@ public final class SQLParseCountAdvice implements InstanceMethodAdvice {
     }
     
     private Optional<String> getSQLType(final SQLStatement sqlStatement) {
-        if (sqlStatement instanceof InsertStatement) {
-            return Optional.of("INSERT");
-        }
-        if (sqlStatement instanceof UpdateStatement) {
-            return Optional.of("UPDATE");
-        }
-        if (sqlStatement instanceof DeleteStatement) {
-            return Optional.of("DELETE");
-        }
-        if (sqlStatement instanceof SelectStatement) {
-            return Optional.of("SELECT");
-        }
-        if (sqlStatement instanceof DDLStatement) {
-            return Optional.of("DDL");
-        }
-        if (sqlStatement instanceof DCLStatement) {
-            return Optional.of("DCL");
-        }
-        if (sqlStatement instanceof DALStatement) {
-            return Optional.of("DAL");
-        }
-        if (sqlStatement instanceof TCLStatement) {
-            return Optional.of("TCL");
-        }
-        if (sqlStatement instanceof RQLStatement) {
-            return Optional.of("RQL");
-        }
-        if (sqlStatement instanceof RDLStatement) {
-            return Optional.of("RDL");
-        }
-        if (sqlStatement instanceof RALStatement) {
-            return Optional.of("RAL");
-        }
-        if (sqlStatement instanceof RULStatement) {
-            return Optional.of("RUL");
-        }
-        return Optional.empty();
+        return Optional.of(SQLStatementUtil.getType(sqlStatement).name());
     }
 }
