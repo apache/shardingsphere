e05e0e8fcaeebf8fb6d2ea97bc18e85c80f044f3
zhangliang <zhangliang@apache.org>
2023-07-18 14:31:27 +0800

    Move TrunkDatabaseType and BranchDatabaseType to core module

--- test/fixture/infra/src/main/java/org/apache/shardingsphere/test/fixture/infra/database/type/MockedDatabaseType.java
+++ test/fixture/infra/src/main/java/org/apache/shardingsphere/test/fixture/infra/database/type/MockedDatabaseType.java
@@ -18,7 +18,7 @@
 package org.apache.shardingsphere.test.fixture.infra.database.type;
 
 import org.apache.shardingsphere.infra.database.spi.DataSourceMetaData;
-import org.apache.shardingsphere.infra.database.spi.TrunkDatabaseType;
+import org.apache.shardingsphere.infra.database.core.type.TrunkDatabaseType;
 import org.apache.shardingsphere.sql.parser.sql.common.enums.QuoteCharacter;
 
 import java.util.Collection;
