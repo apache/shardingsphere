61a1c4a5359f745529682a9a3b1b1ba2cb9f1968
zhangliang <zhangliang@apache.org>
2023-07-22 18:21:46 +0800

    Remove DatabaseTypeChecker

--- infra/database/core/src/main/java/org/apache/shardingsphere/infra/database/core/type/UnsupportedStorageTypeException.java
+++ infra/database/core/src/main/java/org/apache/shardingsphere/infra/database/core/type/UnsupportedStorageTypeException.java
@@ -30,8 +30,4 @@ public final class UnsupportedStorageTypeException extends KernelSQLException {
     public UnsupportedStorageTypeException(final String url) {
         super(XOpenSQLState.FEATURE_NOT_SUPPORTED, 3, 40, "Unsupported storage type of URL `%s`.", url);
     }
-    
-    public UnsupportedStorageTypeException(final String databaseName, final String dataSourceName) {
-        super(XOpenSQLState.FEATURE_NOT_SUPPORTED, 3, 40, "Unsupported storage type of `%s.%s`.", databaseName, dataSourceName);
-    }
 }
