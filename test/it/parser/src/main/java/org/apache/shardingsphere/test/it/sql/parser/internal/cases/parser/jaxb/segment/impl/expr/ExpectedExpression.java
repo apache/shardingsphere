a3e727c93927438ced36825bd400f7ec793bec6e
Zichao <57847965+Qianyi951015@users.noreply.github.com>
2023-07-22 12:10:12 +1200

    add Oracle Join Operator SQL Parse (#27356)
    * add Oracle Join Operator Parse
    
    * add Oracle Join Operator Parse

--- test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/internal/cases/parser/jaxb/segment/impl/expr/ExpectedExpression.java
+++ test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/internal/cases/parser/jaxb/segment/impl/expr/ExpectedExpression.java
@@ -104,4 +104,7 @@ public final class ExpectedExpression extends AbstractExpectedSQLSegment {
     
     @XmlElement(name = "match-expression")
     private ExpectedMatchExpression matchExpression;
+    
+    @XmlElement(name = "colum-with-join-operator-segment")
+    private ExpectedColumnWithJoinOperatorSegment columnWithJoinOperatorSegment;
 }
