147e43d07caa41ec9ea86ee7c8b89d5479794125
Cong Hu <iamhucong@qq.com>
2023-07-28 15:52:02 +0800

    Remove federate table in E2E. (#27508)
    * Remove t_order_item_federate_sharding in E2E.
    
    * Remove t_order_federate_sharding in E2E.
    
    * Remove t_order_federate in E2E.
    
    * Remove t_order_item_federate in E2E.
    
    * Remove t_user_encrypt_federate_sharding in E2E.
    
    * Remove t_user_encrypt_federate in E2E.
    
    * Fix RQLE2EIT errors.
    
    * Fix GeneralDQLE2EIT errors.

--- test/e2e/sql/src/test/resources/env/scenario/tbl/data/expected/init-sql/h2/01-expected-init.sql
+++ test/e2e/sql/src/test/resources/env/scenario/tbl/data/expected/init-sql/h2/01-expected-init.sql
@@ -19,14 +19,10 @@ DROP TABLE IF EXISTS t_order;
 DROP TABLE IF EXISTS t_order_item;
 DROP TABLE IF EXISTS t_single_table;
 DROP TABLE IF EXISTS t_broadcast_table;
-DROP TABLE IF EXISTS t_order_federate_sharding;
-DROP TABLE IF EXISTS t_order_item_federate_sharding;
 
 CREATE TABLE t_order(order_id BIGINT PRIMARY KEY, user_id INT NOT NULL, status VARCHAR(50) NOT NULL, merchant_id INT NOT NULL, remark VARCHAR(50) NOT NULL, creation_date DATE NOT NULL);
 CREATE TABLE t_order_item(item_id BIGINT PRIMARY KEY, order_id BIGINT NOT NULL, user_id INT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, creation_date DATE NOT NULL);
 CREATE TABLE t_single_table (single_id INT NOT NULL, id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (single_id));
 CREATE TABLE t_broadcast_table (id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (id));
-CREATE TABLE t_order_federate_sharding (order_id_sharding INT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, PRIMARY KEY (order_id_sharding));
-CREATE TABLE t_order_item_federate_sharding (item_id BIGINT NOT NULL, order_id BIGINT NOT NULL, user_id INT NOT NULL, status VARCHAR(45) NULL, remarks VARCHAR(45) NULL, PRIMARY KEY (item_id));
 
 CREATE INDEX order_index_t_order ON t_order (order_id);
