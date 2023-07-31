b0b4cd211cdcf8433a475fd0dbc889f8bfeee1a1
zhangliang <zhangliang@apache.org>
2023-07-31 21:52:56 +0800

    Move ShardingSphereAlgorithm into common module

--- infra/common/src/main/java/org/apache/shardingsphere/infra/algorithm/ShardingSphereAlgorithm.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/algorithm/ShardingSphereAlgorithm.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.infra.spi.type.typed.algorithm;
+package org.apache.shardingsphere.infra.algorithm;
 
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPI;
 
