037432d76c1819a2ad1903ab6414e652738201ea
niu niu <zihaoAK47@aliyun.com>
2023-10-06 07:36:24 +0800

    Change sql federation insert statement conversion (#28631)
    * Change sql federation insert statement conversion
    
    * Format code
    
    * Comment test case

--- kernel/sql-federation/core/src/main/java/org/apache/shardingsphere/sqlfederation/compiler/converter/statement/insert/InsertStatementConverter.java
+++ kernel/sql-federation/core/src/main/java/org/apache/shardingsphere/sqlfederation/compiler/converter/statement/insert/InsertStatementConverter.java
@@ -21,30 +21,22 @@ import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlInsert;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
-import org.apache.calcite.sql.SqlSelect;
 import org.apache.calcite.sql.SqlValuesOperator;
 import org.apache.calcite.sql.fun.SqlRowOperator;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.assignment.InsertValuesSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.column.ColumnSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.ExpressionSegment;
-import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.expr.subquery.SubquerySegment;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.InsertStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
-import org.apache.shardingsphere.sql.parser.sql.dialect.handler.dml.SelectStatementHandler;
 import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.expression.ExpressionConverter;
 import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.expression.impl.ColumnConverter;
 import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.from.TableConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.groupby.GroupByConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.groupby.HavingConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.projection.DistinctConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.projection.ProjectionsConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.where.WhereConverter;
-import org.apache.shardingsphere.sqlfederation.compiler.converter.segment.window.WindowConverter;
 import org.apache.shardingsphere.sqlfederation.compiler.converter.statement.SQLStatementConverter;
+import org.apache.shardingsphere.sqlfederation.compiler.converter.statement.select.SelectStatementConverter;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -60,28 +52,18 @@ public final class InsertStatementConverter implements SQLStatementConverter<Ins
     
     private SqlInsert convertInsert(final InsertStatement insertStatement) {
         SqlNode table = new TableConverter().convert(insertStatement.getTable()).orElseThrow(IllegalStateException::new);
-        SqlParserPos position = SqlParserPos.ZERO;
-        SqlNodeList keywords = new SqlNodeList(position);
-        SqlNode source;
-        if (insertStatement.getInsertSelect().isPresent()) {
-            source = convertSelect(insertStatement.getInsertSelect().get());
-        } else {
-            source = convertValues(insertStatement.getValues());
-        }
+        SqlNodeList keywords = new SqlNodeList(SqlParserPos.ZERO);
+        SqlNode source = convertSource(insertStatement);
         SqlNodeList columnList = convertColumn(insertStatement.getColumns());
         return new SqlInsert(SqlParserPos.ZERO, keywords, table, source, columnList);
     }
     
-    private SqlNode convertSelect(final SubquerySegment subquerySegment) {
-        SelectStatement selectStatement = subquerySegment.getSelect();
-        SqlNodeList distinct = new DistinctConverter().convert(selectStatement.getProjections()).orElse(null);
-        SqlNodeList projection = new ProjectionsConverter().convert(selectStatement.getProjections()).orElseThrow(IllegalStateException::new);
-        SqlNode from = new TableConverter().convert(selectStatement.getFrom()).orElse(null);
-        SqlNode where = selectStatement.getWhere().flatMap(optional -> new WhereConverter().convert(optional)).orElse(null);
-        SqlNodeList groupBy = selectStatement.getGroupBy().flatMap(optional -> new GroupByConverter().convert(optional)).orElse(null);
-        SqlNode having = selectStatement.getHaving().flatMap(optional -> new HavingConverter().convert(optional)).orElse(null);
-        SqlNodeList window = SelectStatementHandler.getWindowSegment(selectStatement).flatMap(new WindowConverter()::convert).orElse(SqlNodeList.EMPTY);
-        return new SqlSelect(SqlParserPos.ZERO, distinct, projection, from, where, groupBy, having, window, null, null, null, null, SqlNodeList.EMPTY);
+    private SqlNode convertSource(final InsertStatement insertStatement) {
+        if (insertStatement.getInsertSelect().isPresent()) {
+            return new SelectStatementConverter().convert(insertStatement.getInsertSelect().get().getSelect());
+        } else {
+            return convertValues(insertStatement.getValues());
+        }
     }
     
     private SqlNode convertValues(final Collection<InsertValuesSegment> insertValuesSegments) {
@@ -91,17 +73,13 @@ public final class InsertStatementConverter implements SQLStatementConverter<Ins
                 values.add(convertExpression(value));
             }
         }
-        List<SqlNode> operands = new ArrayList<>();
-        operands.add(new SqlBasicCall(new SqlRowOperator("ROW"), values, SqlParserPos.ZERO));
+        List<SqlNode> operands = Collections.singletonList(new SqlBasicCall(new SqlRowOperator("ROW"), values, SqlParserPos.ZERO));
         return new SqlBasicCall(new SqlValuesOperator(), operands, SqlParserPos.ZERO);
     }
     
     private SqlNodeList convertColumn(final Collection<ColumnSegment> columnSegments) {
         List<SqlNode> columns = columnSegments.stream().map(each -> new ColumnConverter().convert(each).orElseThrow(IllegalStateException::new)).collect(Collectors.toList());
-        if (columns.isEmpty()) {
-            return SqlNodeList.EMPTY;
-        }
-        return new SqlNodeList(columns, SqlParserPos.ZERO);
+        return columns.isEmpty() ? null : new SqlNodeList(columns, SqlParserPos.ZERO);
     }
     
     private SqlNode convertExpression(final ExpressionSegment expressionSegment) {
