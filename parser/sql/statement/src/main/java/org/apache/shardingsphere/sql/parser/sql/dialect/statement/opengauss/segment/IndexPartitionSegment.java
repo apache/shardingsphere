3294591fbe8667e7b303ada5073e3d6d388c0ec5
tmyVvit <terryteng0106@gmail.com>
2023-10-19 11:40:48 +0800

    fix some minor code issues

--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/opengauss/segment/IndexPartitionSegment.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/opengauss/segment/IndexPartitionSegment.java
@@ -43,6 +43,7 @@ public final class IndexPartitionSegment implements SQLSegment {
 
     /**
      * Get tablespace.
+     *
      * @return tablespace
      */
     public Optional<TablespaceSegment> getTablespace() {
