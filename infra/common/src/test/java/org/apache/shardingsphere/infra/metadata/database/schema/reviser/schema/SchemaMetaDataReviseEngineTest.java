40f5933ba152d9d1a4c7292fbdd6dd1bcf56da87
Harsh Sawarkar <harshsawarkar111@gmail.com>
2023-11-30 15:11:25 +0530

    Added unit tests for to test SchemaMetaDataReviseEngine its public functions to improve unit test coverage.

---
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/database/schema/reviser/schema/SchemaMetaDataReviseEngineTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.metadata.database.schema.reviser.schema;
+
+import org.apache.shardingsphere.infra.config.props.ConfigurationProperties;
+import org.apache.shardingsphere.infra.database.core.metadata.data.model.SchemaMetaData;
+import org.apache.shardingsphere.infra.database.core.metadata.data.model.TableMetaData;
+import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
+import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+class SchemaMetaDataReviseEngineTest {
+    
+    @Mock
+    private SchemaMetaDataReviseEngine schemaMetaDataReviseEngine;
+    
+    @Test
+    void assertRevise() {
+        Collection<ShardingSphereRule> rules = new ArrayList<>();
+        Collection<TableMetaData> tableMetaData = new ArrayList<>();
+        ConfigurationProperties props = mock(ConfigurationProperties.class);
+        DatabaseType databaseType = mock(DatabaseType.class);
+        DataSource dataSource = mock(DataSource.class);
+        SchemaMetaData schemaMetaData = new SchemaMetaData("expected", tableMetaData);
+        schemaMetaDataReviseEngine = mock(SchemaMetaDataReviseEngine.class);
+        doReturn(schemaMetaData).when(schemaMetaDataReviseEngine);
+        SchemaMetaData actual = new SchemaMetaDataReviseEngine(rules, props, databaseType, dataSource).revise(schemaMetaData);
+        assertEquals(schemaMetaData.getName(), actual.getName());
+        assertEquals(schemaMetaData.getTables(), actual.getTables());
+        
+    }
+}
