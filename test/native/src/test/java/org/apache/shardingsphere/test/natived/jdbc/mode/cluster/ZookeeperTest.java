4fd6850ae03520c774cc396320e4fe5e0827e5aa
Ling Hengqian <linghengqian@outlook.com>
2023-12-20 08:20:11 +0800

    Add GraalVM Reachability Metadata and corresponding nativeTest for Zookeeper integration (#29425)

--- test/native/src/test/java/org/apache/shardingsphere/test/natived/jdbc/mode/cluster/ZookeeperTest.java
+++ test/native/src/test/java/org/apache/shardingsphere/test/natived/jdbc/mode/cluster/ZookeeperTest.java
@@ -15,13 +15,15 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.test.natived.jdbc.databases;
+package org.apache.shardingsphere.test.natived.jdbc.mode.cluster;
 
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
-import com.mysql.cj.jdbc.exceptions.CommunicationsException;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
 import org.apache.shardingsphere.test.natived.jdbc.commons.FileTestUtils;
 import org.apache.shardingsphere.test.natived.jdbc.commons.TestShardingService;
@@ -33,25 +35,11 @@ import org.testcontainers.utility.DockerImageName;
 
 import javax.sql.DataSource;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.Duration;
-import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Unable to use `org.testcontainers:mysql:1.19.3` under GraalVM Native Image.
- * Background comes from <a href="https://github.com/testcontainers/testcontainers-java/issues/7954">testcontainers/testcontainers-java#7954</a>.
- */
-public class MySQLTest {
-    
-    private static final String USERNAME = "root";
-    
-    private static final String PASSWORD = "123456";
-    
-    private static final String DATABASE = "test";
-    
-    private static final String JDBC_URL = "jdbc:mysql://localhost:65107/" + DATABASE;
+public class ZookeeperTest {
     
     private TestShardingService testShardingService;
     
@@ -60,13 +48,11 @@ public class MySQLTest {
     @EnabledInNativeImage
     void assertShardingInLocalTransactions() throws SQLException, IOException {
         try (
-                GenericContainer<?> mySQLContainer = new GenericContainer<>(DockerImageName.parse("mysql:8.2.0-oracle"))
-                        .withEnv("MYSQL_DATABASE", DATABASE)
-                        .withEnv("MYSQL_ROOT_PASSWORD", PASSWORD)
-                        .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(65107), new ExposedPort(3306)))))) {
-            mySQLContainer.start();
-            beforeAll();
-            DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(FileTestUtils.readFromFileURLString("test-native/yaml/databases/mysql.yaml"));
+                GenericContainer<?> zookeeperContainer = new GenericContainer<>(DockerImageName.parse("zookeeper:3.9.1-jre-17"))
+                        .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(62372), new ExposedPort(2181)))))) {
+            zookeeperContainer.start();
+            this.beforeAll();
+            DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(FileTestUtils.readFromFileURLString("test-native/yaml/mode/cluster/zookeeper.yaml"));
             testShardingService = new TestShardingService(dataSource);
             this.initEnvironment();
             testShardingService.processSuccess();
@@ -83,26 +69,13 @@ public class MySQLTest {
         testShardingService.getAddressRepository().truncateTable();
     }
     
-    private static Connection openConnection() throws SQLException {
-        Properties props = new Properties();
-        props.setProperty("user", USERNAME);
-        props.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(JDBC_URL, props);
-    }
-    
-    @SuppressWarnings({"SqlDialectInspection", "SqlNoDataSourceInspection"})
     private void beforeAll() {
-        Awaitility.await().atMost(Duration.ofMinutes(1)).ignoreExceptionsMatching(e -> e instanceof CommunicationsException)
-                .until(() -> {
-                    openConnection().close();
-                    return true;
-                });
-        try (Connection connection = openConnection()) {
-            connection.createStatement().executeUpdate("CREATE DATABASE demo_ds_0;");
-            connection.createStatement().executeUpdate("CREATE DATABASE demo_ds_1;");
-            connection.createStatement().executeUpdate("CREATE DATABASE demo_ds_2;");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        Awaitility.await().atMost(Duration.ofMinutes(1)).ignoreExceptions().until(() -> {
+            CuratorFramework client = CuratorFrameworkFactory.newClient("localhost:" + 62372, new ExponentialBackoffRetry(1000, 3));
+            client.start();
+            boolean connected = client.blockUntilConnected(5, TimeUnit.SECONDS);
+            client.close();
+            return connected;
+        });
     }
 }
