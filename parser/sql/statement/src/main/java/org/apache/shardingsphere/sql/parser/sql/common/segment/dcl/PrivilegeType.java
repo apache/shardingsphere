f9e5a9a555c925ab0148f4259c247819588337b0
Liang Zhang <zhangliang@apache.org>
2024-01-02 23:36:47 +0800

    Rename PrivilegeType (#29634)

--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/segment/dcl/PrivilegeType.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/segment/dcl/PrivilegeType.java
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.sql.parser.sql.common.segment.generic;
+package org.apache.shardingsphere.sql.parser.sql.common.segment.dcl;
 
 /**
- * Privilege type enum.
+ * Privilege type.
  */
-public enum PrivilegeTypeEnum {
+public enum PrivilegeType {
     
     SELECT,
     
