4e700cd0b303bef0291cc2242de81dc7c91e94cb
Mukul Sharma <113163591+cb-mukulsharma@users.noreply.github.com>
2024-01-08 11:37:01 +0530

    Checkstyle failure fixed

--- test/e2e/sql/src/test/java/org/apache/shardingsphere/test/e2e/cases/value/SQLValue.java
+++ test/e2e/sql/src/test/java/org/apache/shardingsphere/test/e2e/cases/value/SQLValue.java
@@ -28,7 +28,6 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 /**
