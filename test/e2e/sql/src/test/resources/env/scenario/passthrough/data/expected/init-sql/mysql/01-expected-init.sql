0eef11871429a424602caf87cb7cebbef2c6e9d2
Mukul Sharma <113163591+cb-mukulsharma@users.noreply.github.com>
2024-01-16 08:35:24 +0530

    Supporting UUID column type in shardingsphere (#29662)
    * Supporting UUID column type in shardingsphere
    
    * E2E tests for uuid type
    
    * Newline added
    
    * Checkstyle failure fixed
    
    * Passthrough uuid test case fixed

--- test/e2e/sql/src/test/resources/env/scenario/passthrough/data/expected/init-sql/mysql/01-expected-init.sql
+++ test/e2e/sql/src/test/resources/env/scenario/passthrough/data/expected/init-sql/mysql/01-expected-init.sql
@@ -28,3 +28,4 @@ CREATE TABLE expected_dataset.t_with_generated_id (id INT AUTO_INCREMENT PRIMARY
 CREATE TABLE expected_dataset.t_data_type_money (id  INT PRIMARY KEY, val NUMERIC(16, 2));
 CREATE TABLE expected_dataset.t_data_type_bytea (id INT PRIMARY KEY, val BLOB NOT NULL);
 CREATE TABLE expected_dataset.t_data_type_date (id INT PRIMARY KEY, creation_date DATE NOT NULL, update_date DATETIME NOT NULL);
+CREATE TABLE expected_dataset.t_data_type_uuid (id INT PRIMARY KEY, val VARCHAR(36) NOT NULL);
