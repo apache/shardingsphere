002d8c7c83294b1b2acff04bda2ba6faa620a6f7
jiangML <1060319118@qq.com>
2024-01-18 14:49:39 +0800

    Update doc of AgentPluginEnable

--- agent/api/src/main/java/org/apache/shardingsphere/agent/api/plugin/AgentPluginEnable.java
+++ agent/api/src/main/java/org/apache/shardingsphere/agent/api/plugin/AgentPluginEnable.java
@@ -22,7 +22,7 @@ public interface AgentPluginEnable {
     /**
      * Is the plugin enabled.
      *
-     * @param isEnhancedForProxy is enhanced for proxy
+     * @param isEnhancedForProxy true represents the proxy environment, false represents the jdbc environment
      * @return true or false
      */
     default boolean isPluginEnabled(boolean isEnhancedForProxy) {
