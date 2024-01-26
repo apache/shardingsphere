4213df242c9834329194f8742010db230a0af11c
LotusMoon <chenyang_ma@aliyun.com>
2024-01-26 14:48:42 +0800

    Support parsing SQL Server select into segment (#29860)
    * Support parsing SQL Server default schema
    
    * Support parsing SQL Server select into segment
    
    * add visit into segment
    
    * apply spotless apply
    
    * Add a new line between javadoc and param doc

--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/sqlserver/dml/SQLServerSelectStatement.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/sqlserver/dml/SQLServerSelectStatement.java
@@ -20,6 +20,7 @@ package org.apache.shardingsphere.sql.parser.sql.dialect.statement.sqlserver.dml
 import lombok.Setter;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.pagination.limit.LimitSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.WithSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.generic.table.TableSegment;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.dml.SelectStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.sqlserver.SQLServerStatement;
 
@@ -34,6 +35,8 @@ public final class SQLServerSelectStatement extends SelectStatement implements S
     private LimitSegment limit;
     
     private WithSegment withSegment;
+    
+    private TableSegment intoSegment;
     
     /**
      * Get order by segment.
@@ -51,5 +54,14 @@ public final class SQLServerSelectStatement extends SelectStatement implements S
      */
     public Optional<WithSegment> getWithSegment() {
         return Optional.ofNullable(withSegment);
+    }
+    
+    /**
+     * Get into segment.
+     *
+     * @return into segment
+     */
+    public Optional<TableSegment> getIntoSegment() {
+        return Optional.ofNullable(intoSegment);
     }
 }
