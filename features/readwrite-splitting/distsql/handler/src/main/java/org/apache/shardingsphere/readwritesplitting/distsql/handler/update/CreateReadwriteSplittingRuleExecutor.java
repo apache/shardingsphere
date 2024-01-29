67d535e4aec43701f0be2d97bf7c1383823b3d9b
zhangliang <zhangliang@apache.org>
2024-01-29 13:12:02 +0800

    Refactor DatabaseRuleCreateExecutor

--- features/readwrite-splitting/distsql/handler/src/main/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/update/CreateReadwriteSplittingRuleExecutor.java
+++ features/readwrite-splitting/distsql/handler/src/main/java/org/apache/shardingsphere/readwritesplitting/distsql/handler/update/CreateReadwriteSplittingRuleExecutor.java
@@ -46,7 +46,8 @@ public final class CreateReadwriteSplittingRuleExecutor implements DatabaseRuleC
     
     @Override
     public ReadwriteSplittingRuleConfiguration buildToBeCreatedRuleConfiguration(
-            final CreateReadwriteSplittingRuleStatement sqlStatement, final ReadwriteSplittingRuleConfiguration currentRuleConfig) {
+                                                                                 final CreateReadwriteSplittingRuleStatement sqlStatement,
+                                                                                 final ReadwriteSplittingRuleConfiguration currentRuleConfig) {
         Collection<ReadwriteSplittingRuleSegment> segments = sqlStatement.getRules();
         if (sqlStatement.isIfNotExists()) {
             Collection<String> duplicatedRuleNames = getDuplicatedRuleNames(currentRuleConfig, sqlStatement.getRules());
