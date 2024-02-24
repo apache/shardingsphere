7e9b04d8d5fb7d02d177d04873355a7f65f1aa21
Ilyas Ahsan <meilyasahsan@gmail.com>
2024-02-24 15:47:48 +0700

    Fix code style to follow formatter rules.

--- infra/url/core/src/test/java/org/apache/shardingsphere/infra/url/core/ShardingSphereURLLoadEngineTest.java
+++ infra/url/core/src/test/java/org/apache/shardingsphere/infra/url/core/ShardingSphereURLLoadEngineTest.java
@@ -30,6 +30,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
+
 public class ShardingSphereURLLoadEngineTest {
     
     @Test
