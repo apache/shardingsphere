14cfc41fbd6f3e97aba60dc3f2a5cf929d8f5b94
Liang Zhang <zhangliang@apache.org>
2024-02-25 18:16:36 +0800

    Add javadoc on AbstractStaticMethodAdvice (#30282)
    * Revise AgentYamlEngineTest
    
    * Refactor AdviceExecutorFactory
    
    * Add javadoc on AbstractStaticMethodAdvice

--- agent/plugins/core/src/main/java/org/apache/shardingsphere/agent/plugin/core/advice/AbstractStaticMethodAdvice.java
+++ agent/plugins/core/src/main/java/org/apache/shardingsphere/agent/plugin/core/advice/AbstractStaticMethodAdvice.java
@@ -21,6 +21,9 @@ import org.apache.shardingsphere.agent.api.plugin.AgentPluginEnable;
 import org.apache.shardingsphere.agent.api.advice.type.StaticMethodAdvice;
 import org.apache.shardingsphere.agent.plugin.core.context.PluginContext;
 
+/**
+ * Abstract static method advice.
+ */
 public abstract class AbstractStaticMethodAdvice implements StaticMethodAdvice, AgentPluginEnable {
     
     @Override
