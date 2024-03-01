2c3f2e7b7a4b9a8366b2d069e439aa311695f086
linghengqian <linghengqian@outlook.com>
2024-03-01 12:18:34 +0800

    Fixes ClasspathResourceDirectoryReaderTest failure on Windows 11

--- infra/util/src/test/java/org/apache/shardingsphere/infra/util/directory/ClasspathResourceDirectoryReaderTest.java
+++ infra/util/src/test/java/org/apache/shardingsphere/infra/util/directory/ClasspathResourceDirectoryReaderTest.java
@@ -19,6 +19,7 @@ package org.apache.shardingsphere.infra.util.directory;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,6 +42,8 @@ class ClasspathResourceDirectoryReaderTest {
     void read() {
         List<String> resourceNameList = ClasspathResourceDirectoryReader.read("yaml").collect(Collectors.toList());
         assertThat(resourceNameList.size(), is(4));
-        assertThat(resourceNameList, hasItems("yaml/accepted-class.yaml", "yaml/customized-obj.yaml", "yaml/empty-config.yaml", "yaml/shortcuts-fixture.yaml"));
+        final String separator = File.separator;
+        assertThat(resourceNameList, hasItems("yaml" + separator + "accepted-class.yaml", "yaml" + separator + "customized-obj.yaml", "yaml" + separator + "empty-config.yaml",
+                "yaml" + separator + "shortcuts-fixture.yaml"));
     }
 }
