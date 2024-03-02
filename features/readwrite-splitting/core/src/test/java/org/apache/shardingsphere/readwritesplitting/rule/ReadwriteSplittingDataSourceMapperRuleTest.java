99fd0b21d5981e4ac3df29370aeb930f663bbd3c
zhangliang <zhangliang@apache.org>
2024-03-02 17:03:50 +0800

    Add ReadwriteSplittingDataSourceMapperRuleTest

--- features/readwrite-splitting/core/src/test/java/org/apache/shardingsphere/readwritesplitting/rule/ReadwriteSplittingDataSourceMapperRuleTest.java
+++ features/readwrite-splitting/core/src/test/java/org/apache/shardingsphere/readwritesplitting/rule/ReadwriteSplittingDataSourceMapperRuleTest.java
@@ -17,9 +17,6 @@
 
 package org.apache.shardingsphere.readwritesplitting.rule;
 
-import org.apache.shardingsphere.infra.algorithm.core.config.AlgorithmConfiguration;
-import org.apache.shardingsphere.infra.instance.InstanceContext;
-import org.apache.shardingsphere.readwritesplitting.api.ReadwriteSplittingRuleConfiguration;
 import org.apache.shardingsphere.readwritesplitting.api.rule.ReadwriteSplittingDataSourceRuleConfiguration;
 import org.junit.jupiter.api.Test;
 
@@ -27,26 +24,21 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
 
 class ReadwriteSplittingDataSourceMapperRuleTest {
     
     @Test
     void assertGetDataSourceMapper() {
-        ReadwriteSplittingRule readwriteSplittingRule = createReadwriteSplittingRule();
-        Map<String, Collection<String>> actual = readwriteSplittingRule.getDataSourceMapperRule().getDataSourceMapper();
+        Map<String, Collection<String>> actual = new ReadwriteSplittingDataSourceMapperRule(Collections.singleton(createReadwriteSplittingRule())).getDataSourceMapper();
         Map<String, Collection<String>> expected = Collections.singletonMap("readwrite", Arrays.asList("write_ds", "read_ds_0", "read_ds_1"));
         assertThat(actual, is(expected));
     }
     
-    private ReadwriteSplittingRule createReadwriteSplittingRule() {
-        ReadwriteSplittingDataSourceRuleConfiguration config =
-                new ReadwriteSplittingDataSourceRuleConfiguration("readwrite", "write_ds", Arrays.asList("read_ds_0", "read_ds_1"), "random");
-        return new ReadwriteSplittingRule("logic_db", new ReadwriteSplittingRuleConfiguration(
-                Collections.singleton(config), Collections.singletonMap("random", new AlgorithmConfiguration("RANDOM", new Properties()))), mock(InstanceContext.class));
+    private ReadwriteSplittingDataSourceRule createReadwriteSplittingRule() {
+        return new ReadwriteSplittingDataSourceRule(
+                new ReadwriteSplittingDataSourceRuleConfiguration("readwrite", "write_ds", Arrays.asList("read_ds_0", "read_ds_1"), "random"), null, null);
     }
 }
