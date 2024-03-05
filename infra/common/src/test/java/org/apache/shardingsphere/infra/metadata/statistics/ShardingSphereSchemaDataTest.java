88e216c0a6eb89c923064b73a006b22ca14626b7
Archit Shinde <archits581@gmail.com>
2024-03-05 21:13:34 +0530

    Fix checkstyle and add license to ShardingSphereSchemaDataTest

--- infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/statistics/ShardingSphereSchemaDataTest.java
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/statistics/ShardingSphereSchemaDataTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.infra.metadata.statistics;
 
 import org.junit.jupiter.api.Test;
@@ -9,9 +26,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ShardingSphereSchemaDataTest {
 
-    private final static String TEST_TABLE_NAME = "TEST_TABLE_NAME";
-    private final static String TEST_TABLE_2 = "TEST_TABLE_2";
-    private final static String NON_EXISTENT_TABLE = "NON_EXISTENT_TABLE";
+    private static final String TEST_TABLE_NAME = "TEST_TABLE_NAME";
+
+    private static final String TEST_TABLE_2 = "TEST_TABLE_2";
+
+    private static final String NON_EXISTENT_TABLE = "NON_EXISTENT_TABLE";
 
     @Test
     void assertGetTable() {
