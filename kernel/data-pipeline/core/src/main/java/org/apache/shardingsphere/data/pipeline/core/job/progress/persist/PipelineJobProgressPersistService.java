bcec30905dbf8c8b2d0845d4c2aefeb9142277a9
Hongsheng Zhong <zhonghongsheng@apache.org>
2024-03-09 17:33:43 +0800

    Recover previous persisting nonnull check on force persisting progress to avoid invalid progress overwriting

--- kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/job/progress/persist/PipelineJobProgressPersistService.java
+++ kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/job/progress/persist/PipelineJobProgressPersistService.java
@@ -101,7 +101,10 @@ public final class PipelineJobProgressPersistService {
      */
     public static void persistNow(final String jobId, final int shardingItem) {
         getPersistContext(jobId, shardingItem).ifPresent(persistContext -> {
-            // TODO Recover persistContext.getBeforePersistingProgressMillis() null check after compatible with PostgreSQLMigrationGeneralE2EIT
+            if (null == persistContext.getBeforePersistingProgressMillis().get()) {
+                log.warn("Force persisting progress is not permitted since there is no previous persisting, jobId={}, shardingItem={}", jobId, shardingItem);
+                return;
+            }
             notifyPersist(persistContext);
             PersistJobContextRunnable.persist(jobId, shardingItem, persistContext);
         });
