apache/shardingsphere

commit 605d3f4dfb878acc9ec4a8c0f74461b30c068332

Modify local transactions to be executed in parallel (#30462)
* Modify local transactions to be executed in parallel

* Fix force startup e2e

* Fix force startup e2e

* Fix force startup e2e

infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/execute/engine/driver/jdbc/JDBCExecutor.java
@@ -35,7 +35,6 @@ public final class JDBCExecutor {
     
     private final ExecutorEngine executorEngine;
     
-    // TODO add transaction type to ConnectionContext
     private final ConnectionContext connectionContext;
     
     /**
@@ -64,7 +63,7 @@ public final class JDBCExecutor {
     public <T> List<T> execute(final ExecutionGroupContext<JDBCExecutionUnit> executionGroupContext,
                                final JDBCExecutorCallback<T> firstCallback, final JDBCExecutorCallback<T> callback) throws SQLException {
         try {
-            return executorEngine.execute(executionGroupContext, firstCallback, callback, connectionContext.getTransactionContext().isInTransaction());
+            return executorEngine.execute(executionGroupContext, firstCallback, callback, connectionContext.getTransactionContext().isInXATransaction());
         } catch (final SQLException ex) {
             SQLExecutorExceptionHandler.handleException(ex);
             return Collections.emptyList();

infra/session/src/main/java/org/apache/shardingsphere/infra/session/connection/transaction/TransactionConnectionContext.java
@@ -24,14 +24,36 @@ import lombok.Setter;
  * Transaction connection context.
  */
 @Getter
-@Setter
 public final class TransactionConnectionContext implements AutoCloseable {
+    
+    private volatile String transactionType;
     
     private volatile boolean inTransaction;
     
+    @Setter
     private volatile long beginMills;
     
+    @Setter
     private volatile String readWriteSplitReplicaRoute;
+    
+    /**
+     * Begin transaction.
+     *
+     * @param transactionType transaction type 
+     */
+    public void beginTransaction(final String transactionType) {
+        this.transactionType = transactionType;
+        inTransaction = true;
+    }
+    
+    /**
+     * Judge is in XA transaction or not.
+     *
+     * @return in XA transaction or not
+     */
+    public boolean isInXATransaction() {
+        return inTransaction && "XA".equals(transactionType);
+    }
     
     @Override
     public void close() {

infra/session/src/main/java/org/apache/shardingsphere/infra/session/connection/ConnectionContext.java
@@ -78,14 +78,14 @@ public final class ConnectionContext implements AutoCloseable {
     /**
      * Clear cursor connection context.
      */
-    public void clearCursorConnectionContext() {
+    public void clearCursorContext() {
         cursorContext.close();
     }
     
     /**
      * Clear transaction connection context.
      */
-    public void clearTransactionConnectionContext() {
+    public void clearTransactionContext() {
         transactionContext.close();
     }
     
@@ -112,7 +112,7 @@ public final class ConnectionContext implements AutoCloseable {
     @Override
     public void close() {
         trafficInstanceId = null;
-        clearCursorConnectionContext();
-        clearTransactionConnectionContext();
+        clearCursorContext();
+        clearTransactionContext();
     }
 }

jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/connection/ShardingSphereConnection.java
@@ -170,7 +170,7 @@ public final class ShardingSphereConnection extends AbstractConnectionAdapter {
     private void processLocalTransaction() throws SQLException {
         databaseConnectionManager.setAutoCommit(autoCommit);
         if (!autoCommit) {
-            getConnectionContext().getTransactionContext().setInTransaction(true);
+            getConnectionContext().getTransactionContext().beginTransaction(String.valueOf(databaseConnectionManager.getConnectionTransaction().getTransactionType()));
         }
     }
     
@@ -190,7 +190,7 @@ public final class ShardingSphereConnection extends AbstractConnectionAdapter {
     private void beginDistributedTransaction() throws SQLException {
         databaseConnectionManager.close();
         databaseConnectionManager.getConnectionTransaction().begin();
-        getConnectionContext().getTransactionContext().setInTransaction(true);
+        getConnectionContext().getTransactionContext().beginTransaction(String.valueOf(databaseConnectionManager.getConnectionTransaction().getTransactionType()));
     }
     
     /**
@@ -203,7 +203,7 @@ public final class ShardingSphereConnection extends AbstractConnectionAdapter {
             if (TransactionType.isDistributedTransaction(databaseConnectionManager.getConnectionTransaction().getTransactionType())) {
                 beginDistributedTransaction();
             } else {
-                getConnectionContext().getTransactionContext().setInTransaction(true);
+                getConnectionContext().getTransactionContext().beginTransaction(String.valueOf(databaseConnectionManager.getConnectionTransaction().getTransactionType()));
             }
         }
     }

proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/connector/DatabaseConnector.java
@@ -261,7 +261,7 @@ public final class DatabaseConnector implements DatabaseBackendHandler {
             prepareCursorStatementContext(statementContext, connectionSession, cursorName);
         }
         if (statementContext instanceof CloseStatementContext && ((CloseStatementContext) statementContext).getSqlStatement().isCloseAll()) {
-            connectionSession.getConnectionContext().clearCursorConnectionContext();
+            connectionSession.getConnectionContext().clearCursorContext();
         }
     }
     

proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/connector/jdbc/transaction/BackendTransactionManager.java
@@ -65,7 +65,7 @@ public final class BackendTransactionManager implements TransactionManager {
     public void begin() {
         if (!connection.getConnectionSession().getTransactionStatus().isInTransaction()) {
             connection.getConnectionSession().getTransactionStatus().setInTransaction(true);
-            getTransactionContext().setInTransaction(true);
+            getTransactionContext().beginTransaction(String.valueOf(transactionType));
             connection.closeHandlers(true);
             connection.closeConnections(false);
         }
@@ -100,8 +100,8 @@ public final class BackendTransactionManager implements TransactionManager {
                 }
                 connection.getConnectionSession().getTransactionStatus().setInTransaction(false);
                 connection.getConnectionSession().getTransactionStatus().setExceptionOccur(false);
-                connection.getConnectionSession().getConnectionContext().clearTransactionConnectionContext();
-                connection.getConnectionSession().getConnectionContext().clearCursorConnectionContext();
+                connection.getConnectionSession().getConnectionContext().clearTransactionContext();
+                connection.getConnectionSession().getConnectionContext().clearCursorContext();
             }
         }
     }
@@ -124,8 +124,8 @@ public final class BackendTransactionManager implements TransactionManager {
                 }
                 connection.getConnectionSession().getTransactionStatus().setInTransaction(false);
                 connection.getConnectionSession().getTransactionStatus().setExceptionOccur(false);
-                connection.getConnectionSession().getConnectionContext().clearTransactionConnectionContext();
-                connection.getConnectionSession().getConnectionContext().clearCursorConnectionContext();
+                connection.getConnectionSession().getConnectionContext().clearTransactionContext();
+                connection.getConnectionSession().getConnectionContext().clearCursorContext();
             }
         }
     }

proxy/backend/core/src/main/java/org/apache/shardingsphere/proxy/backend/handler/transaction/TransactionXAHandler.java
@@ -85,7 +85,7 @@ public final class TransactionXAHandler implements ProxyBackendHandler {
     private ResponseHeader begin() throws SQLException {
         ShardingSpherePreconditions.checkState(!connectionSession.getTransactionStatus().isInTransaction(), XATransactionNestedBeginException::new);
         ResponseHeader result = backendHandler.execute();
-        connectionSession.getConnectionContext().getTransactionContext().setInTransaction(true);
+        connectionSession.getConnectionContext().getTransactionContext().beginTransaction(String.valueOf(connectionSession.getTransactionStatus().getTransactionType()));
         return result;
     }
     
@@ -93,8 +93,8 @@ public final class TransactionXAHandler implements ProxyBackendHandler {
         try {
             return backendHandler.execute();
         } finally {
-            connectionSession.getConnectionContext().clearTransactionConnectionContext();
-            connectionSession.getConnectionContext().clearCursorConnectionContext();
+            connectionSession.getConnectionContext().clearTransactionContext();
+            connectionSession.getConnectionContext().clearCursorContext();
         }
     }
 }

commit fe5348c64bb28f547d1257e0f973bc6076fc9cb7

Fixes for unjudged Seata AT transactions caused by #30462

infra/executor/src/main/java/org/apache/shardingsphere/infra/executor/sql/execute/engine/driver/jdbc/JDBCExecutor.java
@@ -63,7 +63,7 @@ public final class JDBCExecutor {
     public <T> List<T> execute(final ExecutionGroupContext<JDBCExecutionUnit> executionGroupContext,
                                final JDBCExecutorCallback<T> firstCallback, final JDBCExecutorCallback<T> callback) throws SQLException {
         try {
-            return executorEngine.execute(executionGroupContext, firstCallback, callback, connectionContext.getTransactionContext().isInXATransaction());
+            return executorEngine.execute(executionGroupContext, firstCallback, callback, connectionContext.getTransactionContext().isInDistributedTransaction());
         } catch (final SQLException ex) {
             SQLExecutorExceptionHandler.handleException(ex);
             return Collections.emptyList();

infra/session/src/main/java/org/apache/shardingsphere/infra/session/connection/transaction/TransactionConnectionContext.java
@@ -47,12 +47,12 @@ public final class TransactionConnectionContext implements AutoCloseable {
     }
     
     /**
-     * Judge is in XA transaction or not.
+     * Judge is in distributed transaction or not.
      *
-     * @return in XA transaction or not
+     * @return in distributed transaction or not
      */
-    public boolean isInXATransaction() {
-        return inTransaction && "XA".equals(transactionType);
+    public boolean isInDistributedTransaction() {
+        return inTransaction && ("XA".equals(transactionType) || "BASE".equals(transactionType));
     }
     
     @Override

pom.xml
@@ -146,7 +146,7 @@
         <dockerfile-maven.version>1.4.13</dockerfile-maven.version>
         <docker-compose-maven-plugin.version>4.0.0</docker-compose-maven-plugin.version>
         <os-maven-plugin.version>1.6.2</os-maven-plugin.version>
-        <native-maven-plugin.version>0.10.0</native-maven-plugin.version>
+        <native-maven-plugin.version>0.10.1</native-maven-plugin.version>
         
         <!-- Compile plugin versions -->
         <templating-maven-plugin.version>1.0.0</templating-maven-plugin.version>