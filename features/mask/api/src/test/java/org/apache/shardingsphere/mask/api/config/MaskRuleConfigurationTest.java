a3bddaaff8d9cfbdbd5a6ba2614ac5388cfe2a3b
zhangliang <zhangliang@apache.org>
2024-03-21 14:51:32 +0800

    Add test cases for MaskRuleConfiguration

--- features/mask/api/src/test/java/org/apache/shardingsphere/mask/api/config/MaskRuleConfigurationTest.java
+++ features/mask/api/src/test/java/org/apache/shardingsphere/mask/api/config/MaskRuleConfigurationTest.java
@@ -22,7 +22,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class MaskRuleConfigurationTest {
