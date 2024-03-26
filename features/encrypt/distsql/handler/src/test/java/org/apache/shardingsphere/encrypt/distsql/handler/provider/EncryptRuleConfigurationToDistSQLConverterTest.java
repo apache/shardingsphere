547e4caf7fcdd2a94b3e15ae467590942acf7620
roopsai surampudi <roopsai@outlook.in>
2024-03-26 17:46:40 +0530

    Reduce access specifiers

--- features/encrypt/distsql/handler/src/test/java/org/apache/shardingsphere/encrypt/distsql/handler/provider/EncryptRuleConfigurationToDistSQLConverterTest.java
+++ features/encrypt/distsql/handler/src/test/java/org/apache/shardingsphere/encrypt/distsql/handler/provider/EncryptRuleConfigurationToDistSQLConverterTest.java
@@ -34,7 +34,7 @@ import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class EncryptRuleConfigurationToDistSQLConverterTest {
+class EncryptRuleConfigurationToDistSQLConverterTest {
     
     @Test
     void assertConvertWithEmptyTables() {
