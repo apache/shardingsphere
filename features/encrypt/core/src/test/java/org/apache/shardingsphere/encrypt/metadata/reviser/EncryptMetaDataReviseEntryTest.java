890cb2a50dff438ba5c7234261dedd6e3e1f482d
Shubham Jaiswal <shubham-j-sde@github.com>
2024-04-03 19:50:36 +0530

    Fixing CodeStyle Checks

--- features/encrypt/core/src/test/java/org/apache/shardingsphere/encrypt/metadata/reviser/EncryptMetaDataReviseEntryTest.java
+++ features/encrypt/core/src/test/java/org/apache/shardingsphere/encrypt/metadata/reviser/EncryptMetaDataReviseEntryTest.java
@@ -1,4 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.encrypt.metadata.reviser;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
 
 import org.apache.shardingsphere.encrypt.api.config.EncryptRuleConfiguration;
 import org.apache.shardingsphere.encrypt.api.config.rule.EncryptTableRuleConfiguration;
@@ -8,22 +33,15 @@ import org.apache.shardingsphere.encrypt.metadata.reviser.index.EncryptIndexRevi
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Optional;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class EncryptMetaDataReviseEntryTest {
     
     private final EncryptMetaDataReviseEntry reviseEntry = new EncryptMetaDataReviseEntry();
+    
     private final EncryptRule rule = createEncryptRule();
     
     private EncryptRule createEncryptRule() {
-        EncryptRuleConfiguration ruleConfig =
-                new EncryptRuleConfiguration(Collections.singleton(new EncryptTableRuleConfiguration("test_table", Collections.emptyList())),new HashMap<>());
+        EncryptRuleConfiguration ruleConfig = new EncryptRuleConfiguration(Collections.singleton(
+                new EncryptTableRuleConfiguration("test_table", Collections.emptyList())), new HashMap<>());
         
         return new EncryptRule("test_database", ruleConfig);
     }
