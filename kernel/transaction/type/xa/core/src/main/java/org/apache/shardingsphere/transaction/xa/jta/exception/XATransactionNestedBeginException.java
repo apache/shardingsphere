8ba5d298e492dd2971d2abfda3a440658c43b3b6
zhangliang <zhangliang@apache.org>
2024-04-06 11:42:08 +0800

    Add XATransactionSQLException

--- kernel/transaction/type/xa/core/src/main/java/org/apache/shardingsphere/transaction/xa/jta/exception/XATransactionNestedBeginException.java
+++ kernel/transaction/type/xa/core/src/main/java/org/apache/shardingsphere/transaction/xa/jta/exception/XATransactionNestedBeginException.java
@@ -18,16 +18,15 @@
 package org.apache.shardingsphere.transaction.xa.jta.exception;
 
 import org.apache.shardingsphere.infra.exception.core.external.sql.sqlstate.XOpenSQLState;
-import org.apache.shardingsphere.infra.exception.core.external.sql.type.kernel.category.TransactionSQLException;
 
 /**
  * XA transaction nested begin exception.
  */
-public final class XATransactionNestedBeginException extends TransactionSQLException {
+public final class XATransactionNestedBeginException extends XATransactionSQLException {
     
     private static final long serialVersionUID = 7761100591709104351L;
     
     public XATransactionNestedBeginException() {
-        super(XOpenSQLState.INVALID_TRANSACTION_STATE, 200, "Can not start new XA transaction in a active transaction.");
+        super(XOpenSQLState.INVALID_TRANSACTION_STATE, 1, "Can not start new XA transaction in a active transaction.");
     }
 }
