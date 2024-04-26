9005b0f5fbd5b98b2c904f315ca80e26db3b27b5
ZhangCheng <chengzhang@apache.org>
2024-04-26 11:45:15 +0800

    Replace TransactionStatus.transactionType with transactionRule.defaultType

--- proxy/frontend/core/src/main/java/org/apache/shardingsphere/proxy/frontend/state/impl/OKProxyState.java
+++ proxy/frontend/core/src/main/java/org/apache/shardingsphere/proxy/frontend/state/impl/OKProxyState.java
@@ -42,7 +42,6 @@ public final class OKProxyState implements ProxyState {
     }
     
     private ExecutorService determineSuitableExecutorService(final ConnectionSession connectionSession) {
-        
         return requireOccupyThreadForConnection(connectionSession) ? ConnectionThreadExecutorGroup.getInstance().get(connectionSession.getConnectionId())
                 : UserExecutorGroup.getInstance().getExecutorService();
     }
