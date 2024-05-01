59bd2fe7ed553c4d952546546e02329bc88cd968
ilyas ahsan <meilyasahsan@gmail.com>
2024-05-01 12:13:41 +0700

    Enhance AlterGlobalClockRuleExecutorTest test case (#31087)

--- kernel/global-clock/distsql/handler/src/test/java/org/apache/shardingsphere/globalclock/distsql/handler/update/AlterGlobalClockRuleExecutorTest.java
+++ kernel/global-clock/distsql/handler/src/test/java/org/apache/shardingsphere/globalclock/distsql/handler/update/AlterGlobalClockRuleExecutorTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -45,6 +46,8 @@ class AlterGlobalClockRuleExecutorTest {
         assertThat(actual.getProvider(), is("redis"));
         assertTrue(actual.isEnabled());
         assertThat(actual.getProps().size(), is(1));
+        assertEquals(GlobalClockRule.class, executor.getRuleClass());
+        assertEquals(AlterGlobalClockRuleStatement.class, executor.getType());
     }
     
     private GlobalClockRuleConfiguration getSQLParserRuleConfiguration() {
