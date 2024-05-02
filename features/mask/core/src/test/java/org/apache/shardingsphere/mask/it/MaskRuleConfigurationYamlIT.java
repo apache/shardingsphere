31aa13459fa817f6edb29bc83fa915ea7a0eb916
Liang Zhang <zhangliang@apache.org>
2024-05-02 19:33:59 +0800

    Refactor MaskRuleConfigurationYamlIT (#31100)

--- features/mask/core/src/test/java/org/apache/shardingsphere/mask/it/MaskRuleConfigurationYamlIT.java
+++ features/mask/core/src/test/java/org/apache/shardingsphere/mask/it/MaskRuleConfigurationYamlIT.java
@@ -17,12 +17,17 @@
 
 package org.apache.shardingsphere.mask.it;
 
+import org.apache.shardingsphere.infra.algorithm.core.yaml.YamlAlgorithmConfiguration;
 import org.apache.shardingsphere.infra.yaml.config.pojo.YamlRootConfiguration;
 import org.apache.shardingsphere.mask.yaml.config.YamlMaskRuleConfiguration;
+import org.apache.shardingsphere.mask.yaml.config.rule.YamlMaskTableRuleConfiguration;
 import org.apache.shardingsphere.test.it.yaml.YamlRuleConfigurationIT;
+
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MaskRuleConfigurationYamlIT extends YamlRuleConfigurationIT {
     
@@ -36,19 +41,22 @@ class MaskRuleConfigurationYamlIT extends YamlRuleConfigurationIT {
     }
     
     private void assertMaskRule(final YamlMaskRuleConfiguration actual) {
-        assertTables(actual);
-        assertMaskAlgorithm(actual);
+        assertTables(actual.getTables());
+        assertMaskAlgorithm(actual.getMaskAlgorithms());
     }
     
-    private void assertTables(final YamlMaskRuleConfiguration actual) {
-        assertThat(actual.getTables().size(), is(1));
-        assertThat(actual.getTables().get("t_user").getColumns().size(), is(2));
+    private void assertTables(final Map<String, YamlMaskTableRuleConfiguration> actual) {
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get("t_user").getColumns().size(), is(2));
+        assertThat(actual.get("t_user").getColumns().get("telephone").getMaskAlgorithm(), is("keep_first_n_last_m_mask"));
+        assertThat(actual.get("t_user").getColumns().get("password").getMaskAlgorithm(), is("md5_mask"));
     }
     
-    private void assertMaskAlgorithm(final YamlMaskRuleConfiguration actual) {
-        assertThat(actual.getMaskAlgorithms().size(), is(2));
-        assertThat(actual.getTables().get("t_user").getColumns().get("telephone").getMaskAlgorithm(), is("keep_first_n_last_m_mask"));
-        assertThat(actual.getMaskAlgorithms().get("keep_first_n_last_m_mask").getType(), is("KEEP_FIRST_N_LAST_M"));
-        assertThat(actual.getMaskAlgorithms().get("keep_first_n_last_m_mask").getProps().get("replace-char"), is("*"));
+    private void assertMaskAlgorithm(final Map<String, YamlAlgorithmConfiguration> actual) {
+        assertThat(actual.size(), is(2));
+        assertThat(actual.get("keep_first_n_last_m_mask").getType(), is("KEEP_FIRST_N_LAST_M"));
+        assertThat(actual.get("keep_first_n_last_m_mask").getProps().get("replace-char"), is("*"));
+        assertThat(actual.get("md5_mask").getType(), is("MD5"));
+        assertTrue(actual.get("md5_mask").getProps().isEmpty());
     }
 }
