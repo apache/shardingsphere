e11fd97ecdbc4321dc638db977b3598cad17a4b5
Liang Zhang <zhangliang@apache.org>
2024-05-02 17:36:10 +0800

    Add SingleRuleConfigurationRepositoryTupleSwapperIT (#31096)

---
+++ kernel/single/core/src/test/java/org/apache/shardingsphere/single/it/SingleRuleConfigurationRepositoryTupleSwapperIT.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.single.it;
+
+import org.apache.shardingsphere.infra.util.yaml.datanode.RepositoryTuple;
+import org.apache.shardingsphere.single.yaml.config.swapper.SingleRuleConfigurationRepositoryTupleSwapper;
+import org.apache.shardingsphere.test.it.yaml.RepositoryTupleSwapperIT;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class SingleRuleConfigurationRepositoryTupleSwapperIT extends RepositoryTupleSwapperIT {
+    
+    SingleRuleConfigurationRepositoryTupleSwapperIT() {
+        super("yaml/single-rule.yaml", new SingleRuleConfigurationRepositoryTupleSwapper(), false);
+    }
+    
+    @Override
+    protected void assertRepositoryTuples(final Collection<RepositoryTuple> actualRepositoryTuples) {
+        assertThat(actualRepositoryTuples.size(), is(1));
+        Iterator<RepositoryTuple> iterator = actualRepositoryTuples.iterator();
+        assertSingle(iterator.next());
+    }
+    
+    private void assertSingle(final RepositoryTuple actual) {
+        assertThat(actual.getKey(), is("tables"));
+        assertThat(actual.getValue(), is("defaultDataSource: foo_ds\ntables:\n- foo_tbl\n"));
+    }
+}
