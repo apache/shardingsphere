f565a17c0fcb685a9e9d2279fdfb7bc87ef6e19d
Liang Zhang <zhangliang@apache.org>
2024-06-03 12:33:00 +0800

    Refactor ShardingSphereStatement (#31544)
    * Refactor ShardingSphereStatement
    
    * Refactor ShardingSphereStatement

--- jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/statement/ShardingSpherePreparedStatement.java
+++ jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/statement/ShardingSpherePreparedStatement.java
@@ -302,9 +302,9 @@ public final class ShardingSpherePreparedStatement extends AbstractPreparedState
         if (null != currentResultSet) {
             return currentResultSet;
         }
-        Optional<ResultSet> advancedResultSet = executor.getResultSet();
-        if (advancedResultSet.isPresent()) {
-            return advancedResultSet.get();
+        Optional<ResultSet> resultSet = executor.getResultSet();
+        if (resultSet.isPresent()) {
+            return resultSet.get();
         }
         if (sqlStatementContext instanceof SelectStatementContext || sqlStatementContext.getSqlStatement() instanceof DALStatement) {
             List<ResultSet> resultSets = getResultSets();
