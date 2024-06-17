830016050cce5141e1cd86bde5f00a4a567458f0
duanzhengqiang <duanzhengqiang@apache.org>
2024-06-17 18:27:11 +0800

    comment sql federation view e2e test case

--- test/e2e/sql/src/test/resources/env/scenario/db_tbl_sql_federation/data/expected/init-sql/opengauss/01-expected-init.sql
+++ test/e2e/sql/src/test/resources/env/scenario/db_tbl_sql_federation/data/expected/init-sql/opengauss/01-expected-init.sql
@@ -30,10 +30,11 @@ DROP TABLE IF EXISTS t_product;
 DROP TABLE IF EXISTS t_product_detail;
 DROP TABLE IF EXISTS t_product_category;
 DROP TABLE IF EXISTS t_country;
-DROP VIEW IF EXISTS t_order_item_join_view;
-DROP VIEW IF EXISTS t_order_subquery_view;
-DROP VIEW IF EXISTS t_order_aggregation_view;
-DROP VIEW IF EXISTS t_order_union_view;
+-- TODO open this comment when refresh metadata support view without push down execute
+-- DROP VIEW IF EXISTS t_order_item_join_view;
+-- DROP VIEW IF EXISTS t_order_subquery_view;
+-- DROP VIEW IF EXISTS t_order_aggregation_view;
+-- DROP VIEW IF EXISTS t_order_union_view;
 
 CREATE TABLE t_order(order_id INT PRIMARY KEY, user_id INT NOT NULL, status VARCHAR(50) NOT NULL, merchant_id INT NOT NULL, remark VARCHAR(50) NOT NULL, creation_date DATE NOT NULL);
 CREATE TABLE t_order_item(item_id INT PRIMARY KEY, order_id INT NOT NULL, user_id INT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, creation_date DATE NOT NULL);
@@ -43,9 +44,9 @@ CREATE TABLE t_product (product_id INT PRIMARY KEY, product_name VARCHAR(50) NOT
 CREATE TABLE t_product_detail (detail_id INT PRIMARY KEY, product_id INT NOT NULL, description VARCHAR(50) NOT NULL, creation_date DATE NOT NULL);
 CREATE TABLE t_product_category( category_id INT PRIMARY KEY, category_name VARCHAR(50) NOT NULL, parent_id INT NOT NULL, level INT NOT NULL, creation_date DATE NOT NULL);
 CREATE TABLE t_country (country_id INT PRIMARY KEY, country_name VARCHAR(50), continent_name VARCHAR(50), creation_date DATE NOT NULL);
-CREATE VIEW t_order_item_join_view AS SELECT o.order_id, o.user_id, i.item_id FROM t_order o INNER JOIN t_order_item i ON o.order_id = i.order_id ORDER BY o.order_id, i.item_id;
-CREATE VIEW t_order_subquery_view AS SELECT * FROM t_order o WHERE o.order_id IN (SELECT i.order_id FROM t_order_item i INNER JOIN t_product p ON i.product_id = p.product_id WHERE p.product_id = 10);
-CREATE VIEW t_order_aggregation_view AS SELECT MAX(p.price) AS max_price, MIN(p.price) AS min_price, SUM(p.price) AS sum_price, AVG(p.price) AS avg_price, COUNT(1) AS count FROM t_order o INNER JOIN t_order_item i ON o.order_id = i.order_id INNER JOIN t_product p ON i.product_id = p.product_id GROUP BY o.order_id HAVING SUM(p.price) > 10000 ORDER BY max_price;
-CREATE VIEW t_order_union_view AS SELECT * FROM t_order WHERE order_id > 2000 UNION SELECT * FROM t_order WHERE order_id > 1500;
+-- CREATE VIEW t_order_item_join_view AS SELECT o.order_id, o.user_id, i.item_id FROM t_order o INNER JOIN t_order_item i ON o.order_id = i.order_id ORDER BY o.order_id, i.item_id;
+-- CREATE VIEW t_order_subquery_view AS SELECT * FROM t_order o WHERE o.order_id IN (SELECT i.order_id FROM t_order_item i INNER JOIN t_product p ON i.product_id = p.product_id WHERE p.product_id = 10);
+-- CREATE VIEW t_order_aggregation_view AS SELECT MAX(p.price) AS max_price, MIN(p.price) AS min_price, SUM(p.price) AS sum_price, AVG(p.price) AS avg_price, COUNT(1) AS count FROM t_order o INNER JOIN t_order_item i ON o.order_id = i.order_id INNER JOIN t_product p ON i.product_id = p.product_id GROUP BY o.order_id HAVING SUM(p.price) > 10000 ORDER BY max_price;
+-- CREATE VIEW t_order_union_view AS SELECT * FROM t_order WHERE order_id > 2000 UNION SELECT * FROM t_order WHERE order_id > 1500;
 
 CREATE INDEX order_index_t_order ON t_order (order_id);
