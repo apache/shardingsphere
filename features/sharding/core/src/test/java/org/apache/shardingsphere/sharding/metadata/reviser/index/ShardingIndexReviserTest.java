46cabff0ea9abe9c94e4cd2208a39a8d61e1e88c
zhangliang <zhangliang@apache.org>
2024-06-17 17:22:11 +0800

    Fix sonar issue

--- features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/metadata/reviser/index/ShardingIndexReviserTest.java
+++ features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/metadata/reviser/index/ShardingIndexReviserTest.java
@@ -38,7 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class ShardingIndexReviserTest {
+class ShardingIndexReviserTest {
     
     private ShardingRule shardingRule;
     
