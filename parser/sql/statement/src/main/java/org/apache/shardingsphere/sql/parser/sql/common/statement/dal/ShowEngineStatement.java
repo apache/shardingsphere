080e2cc8af19c49f77e625392a735f9b43e7e93e
duanzhengqiang <duanzhengqiang@apache.org>
2024-06-20 10:30:41 +0800

    Extract ShowEngineStatement to reduce duplicate logic for MySQL and doris

--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/statement/dal/ShowEngineStatement.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/statement/dal/ShowEngineStatement.java
@@ -15,13 +15,18 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dal;
+package org.apache.shardingsphere.sql.parser.sql.common.statement.dal;
 
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.ShowEngineStatement;
-import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.MySQLStatement;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.AbstractSQLStatement;
 
 /**
- * MySQL show engine statement.
+ * Show engine statement.
  */
-public final class MySQLShowEngineStatement extends ShowEngineStatement implements MySQLStatement {
+@Getter
+@Setter
+public abstract class ShowEngineStatement extends AbstractSQLStatement implements DALStatement {
+    
+    private String engineName;
 }
--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/doris/dal/DorisShowEngineStatement.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/doris/dal/DorisShowEngineStatement.java
@@ -17,18 +17,11 @@
 
 package org.apache.shardingsphere.sql.parser.sql.dialect.statement.doris.dal;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.AbstractSQLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.ShowEngineStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.doris.DorisStatement;
 
 /**
  * Doris show engine statement.
  */
-@Getter
-@Setter
-public final class DorisShowEngineStatement extends AbstractSQLStatement implements DALStatement, DorisStatement {
-    
-    private String engineName;
+public final class DorisShowEngineStatement extends ShowEngineStatement implements DorisStatement {
 }
