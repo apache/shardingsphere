5dea56855411ca5b2e5a97149ab3b85843cd23bb
duanzhengqiang <duanzhengqiang@apache.org>
2024-06-20 10:32:29 +0800

    Extract ShowErrorsStatement to reduce duplicate logic for MySQL and doris

--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/statement/dal/ShowErrorsStatement.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/common/statement/dal/ShowErrorsStatement.java
@@ -15,13 +15,19 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.dal;
+package org.apache.shardingsphere.sql.parser.sql.common.statement.dal;
 
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.ShowErrorsStatement;
-import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.MySQLStatement;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.pagination.limit.LimitSegment;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.AbstractSQLStatement;
 
 /**
- * MySQL show errors statement.
+ * Show errors statement.
  */
-public final class MySQLShowErrorsStatement extends ShowErrorsStatement implements MySQLStatement {
+@Getter
+@Setter
+public abstract class ShowErrorsStatement extends AbstractSQLStatement implements DALStatement {
+    
+    private LimitSegment limit;
 }
--- parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/doris/dal/DorisShowErrorsStatement.java
+++ parser/sql/statement/src/main/java/org/apache/shardingsphere/sql/parser/sql/dialect/statement/doris/dal/DorisShowErrorsStatement.java
@@ -17,19 +17,11 @@
 
 package org.apache.shardingsphere.sql.parser.sql.dialect.statement.doris.dal;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.apache.shardingsphere.sql.parser.sql.common.segment.dml.pagination.limit.LimitSegment;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.AbstractSQLStatement;
-import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.DALStatement;
+import org.apache.shardingsphere.sql.parser.sql.common.statement.dal.ShowErrorsStatement;
 import org.apache.shardingsphere.sql.parser.sql.dialect.statement.doris.DorisStatement;
 
 /**
  * Doris show errors statement.
  */
-@Getter
-@Setter
-public final class DorisShowErrorsStatement extends AbstractSQLStatement implements DALStatement, DorisStatement {
-    
-    private LimitSegment limit;
+public final class DorisShowErrorsStatement extends ShowErrorsStatement implements DorisStatement {
 }
