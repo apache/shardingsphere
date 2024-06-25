60c7363bc5c72e942b4a62af25040850759da28a
terrymanu <zhangliang@apache.org>
2024-06-26 01:18:55 +0800

    use interface return type for ShardingSphereResultSetFactory

--- jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/resultset/ShardingSphereResultSetFactory.java
+++ jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/resultset/ShardingSphereResultSetFactory.java
@@ -62,8 +62,8 @@ public final class ShardingSphereResultSetFactory {
      * @return created instance
      * @throws SQLException SQL exception
      */
-    public ShardingSphereResultSet newInstance(final ShardingSphereDatabase database, final QueryContext queryContext, final List<QueryResult> queryResults,
-                                               final Statement statement, final Map<String, Integer> columnLabelAndIndexMap) throws SQLException {
+    public ResultSet newInstance(final ShardingSphereDatabase database, final QueryContext queryContext, final List<QueryResult> queryResults,
+                                 final Statement statement, final Map<String, Integer> columnLabelAndIndexMap) throws SQLException {
         List<ResultSet> resultSets = getResultSets();
         MergedResult mergedResult = new MergeEngine(globalRuleMetaData, database, props, connectionContext).merge(queryResults, queryContext.getSqlStatementContext());
         return new ShardingSphereResultSet(resultSets, mergedResult, statement, queryContext.getSqlStatementContext(),
