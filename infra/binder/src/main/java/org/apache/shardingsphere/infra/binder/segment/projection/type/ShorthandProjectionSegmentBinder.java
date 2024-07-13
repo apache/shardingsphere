862030ca88cfae6ef10070382ba0fc38549440a7
zhangliang <zhangliang@apache.org>
2024-07-13 11:45:56 +0800

    Refactor ProjectionsSegmentBinder

--- infra/binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/projection/type/ShorthandProjectionSegmentBinder.java
+++ infra/binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/projection/type/ShorthandProjectionSegmentBinder.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.infra.binder.segment.projection.impl;
+package org.apache.shardingsphere.infra.binder.segment.projection.type;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
--- infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/segment/projection/type/ShorthandProjectionSegmentBinderTest.java
+++ infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/segment/projection/type/ShorthandProjectionSegmentBinderTest.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.infra.binder.segment.projection.impl;
+package org.apache.shardingsphere.infra.binder.segment.projection.type;
 
 import org.apache.shardingsphere.infra.binder.segment.from.SimpleTableSegmentBinderContext;
 import org.apache.shardingsphere.infra.binder.segment.from.TableSegmentBinderContext;
