eeef5aea7185025cd22bb54b1b414779ebc9a699
RaigorJiang <raigor.jiang@gmail.com>
2024-07-17 16:10:39 +0800

    Add database environment checker for MySQL

---
+++ infra/database/core/src/main/java/org/apache/shardingsphere/infra/database/core/checker/DialectDatabaseEnvironmentChecker.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.database.core.checker;
+
+import org.apache.shardingsphere.infra.database.core.spi.DatabaseTypedSPI;
+import org.apache.shardingsphere.infra.spi.annotation.SingletonSPI;
+
+import javax.sql.DataSource;
+
+/**
+ * Dialect database environment checker.
+ */
+@SingletonSPI
+public interface DialectDatabaseEnvironmentChecker extends DatabaseTypedSPI {
+    
+    /**
+     * Check user privileges.
+     *
+     * @param dataSource data source to be checked
+     * @param privilegeCheckType privilege check type
+     */
+    void checkPrivilege(DataSource dataSource, PrivilegeCheckType privilegeCheckType);
+    
+    /**
+     * Check variables.
+     *
+     * @param dataSource data source to be checked
+     */
+    void checkVariable(DataSource dataSource);
+}
---
+++ infra/database/type/mysql/src/test/java/org/apache/shardingsphere/infra/database/mysql/checker/MySQLDatabaseEnvironmentCheckerTest.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.database.mysql.checker;
+
+import org.apache.shardingsphere.infra.database.core.checker.PrivilegeCheckType;
+import org.apache.shardingsphere.infra.database.core.exception.CheckDatabaseEnvironmentFailedException;
+import org.apache.shardingsphere.infra.database.core.exception.MissingRequiredPrivilegeException;
+import org.apache.shardingsphere.infra.database.core.exception.UnexpectedVariableValueException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MySQLDatabaseEnvironmentCheckerTest {
+    
+    @Mock
+    private PreparedStatement preparedStatement;
+    
+    @Mock
+    private ResultSet resultSet;
+    
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private DataSource dataSource;
+    
+    @BeforeEach
+    void setUp() throws SQLException {
+        when(dataSource.getConnection().prepareStatement(anyString())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    }
+    
+    @Test
+    void assertCheckPrivilegeWithParticularSuccess() throws SQLException {
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString(1)).thenReturn("GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO '%'@'%'");
+        new MySQLDatabaseEnvironmentChecker().checkPrivilege(dataSource, PrivilegeCheckType.PIPELINE);
+        verify(preparedStatement).executeQuery();
+    }
+    
+    @Test
+    void assertCheckPrivilegeWithAllSuccess() throws SQLException {
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString(1)).thenReturn("GRANT ALL PRIVILEGES CLIENT ON *.* TO '%'@'%'");
+        new MySQLDatabaseEnvironmentChecker().checkPrivilege(dataSource, PrivilegeCheckType.PIPELINE);
+        verify(preparedStatement).executeQuery();
+    }
+    
+    @Test
+    void assertCheckPrivilegeLackPrivileges() {
+        assertThrows(MissingRequiredPrivilegeException.class, () -> new MySQLDatabaseEnvironmentChecker().checkPrivilege(dataSource, PrivilegeCheckType.PIPELINE));
+    }
+    
+    @Test
+    void assertCheckPrivilegeFailure() throws SQLException {
+        when(resultSet.next()).thenThrow(new SQLException(""));
+        assertThrows(CheckDatabaseEnvironmentFailedException.class, () -> new MySQLDatabaseEnvironmentChecker().checkPrivilege(dataSource, PrivilegeCheckType.PIPELINE));
+    }
+    
+    @Test
+    void assertCheckVariableSuccess() throws SQLException {
+        when(resultSet.next()).thenReturn(true, true, true, false);
+        when(resultSet.getString(1)).thenReturn("LOG_BIN", "BINLOG_FORMAT", "BINLOG_ROW_IMAGE");
+        when(resultSet.getString(2)).thenReturn("ON", "ROW", "FULL");
+        assertDoesNotThrow(() -> new MySQLDatabaseEnvironmentChecker().checkVariable(dataSource));
+        verify(preparedStatement, times(1)).executeQuery();
+    }
+    
+    @Test
+    void assertCheckVariableWithWrongVariable() throws SQLException {
+        when(resultSet.next()).thenReturn(true, true, false);
+        when(resultSet.getString(1)).thenReturn("BINLOG_FORMAT", "LOG_BIN");
+        when(resultSet.getString(2)).thenReturn("ROW", "OFF");
+        assertThrows(UnexpectedVariableValueException.class, () -> new MySQLDatabaseEnvironmentChecker().checkVariable(dataSource));
+    }
+    
+    @Test
+    void assertCheckVariableFailure() throws SQLException {
+        when(resultSet.next()).thenThrow(new SQLException(""));
+        assertThrows(CheckDatabaseEnvironmentFailedException.class, () -> new MySQLDatabaseEnvironmentChecker().checkVariable(dataSource));
+    }
+}
