d8effde0aa972d952e1ebf79af2d4dadd82b9339
zhangliang <zhangliang@apache.org>
2024-07-30 09:00:40 +0800

    Refactor ProjectionUtils.getIdentifierValue()

--- infra/binder/src/main/java/org/apache/shardingsphere/infra/binder/context/segment/select/projection/util/ProjectionUtils.java
+++ infra/binder/src/main/java/org/apache/shardingsphere/infra/binder/context/segment/select/projection/util/ProjectionUtils.java
@@ -35,7 +35,7 @@ public final class ProjectionUtils {
     
     /**
      * Get identifier value.
-     * 
+     *
      * @param identifierValue identifier value
      * @param databaseType database type
      * @return identifier value
