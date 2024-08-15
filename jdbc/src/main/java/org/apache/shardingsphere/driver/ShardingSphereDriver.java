57ddd3a57c56d3ff0bb12448a4d9311a3d75ad02
jiangML <1060319118@qq.com>
2024-08-15 16:02:53 +0800

    Remove unused methods (#32522)

--- jdbc/src/main/java/org/apache/shardingsphere/driver/ShardingSphereDriver.java
+++ jdbc/src/main/java/org/apache/shardingsphere/driver/ShardingSphereDriver.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.driver;
 
 import org.apache.shardingsphere.driver.exception.DriverRegisterException;
-import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
 import org.apache.shardingsphere.driver.jdbc.core.driver.DriverDataSourceCache;
 import org.apache.shardingsphere.infra.annotation.HighFrequencyInvocation;
 
@@ -27,11 +26,8 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 /**
  * ShardingSphere driver.
@@ -53,16 +49,6 @@ public final class ShardingSphereDriver implements Driver {
         } catch (final SQLException ex) {
             throw new DriverRegisterException(ex);
         }
-    }
-    
-    /**
-     * Get ShardingSphere data sources.
-     *
-     * @return ShardingSphere data source map
-     */
-    public Map<String, ShardingSphereDataSource> getShardingSphereDataSources() {
-        return dataSourceCache.getDataSourceMap().entrySet().stream()
-                .filter(entry -> entry.getValue() instanceof ShardingSphereDataSource).collect(Collectors.toMap(Entry::getKey, entry -> (ShardingSphereDataSource) entry.getValue()));
     }
     
     @HighFrequencyInvocation(canBeCached = true)
