94ba73c3086390ddfafff11841eeeb7ced73709e
Runz Li <98505130+AkaLrz@users.noreply.github.com>
2024-08-22 19:38:06 -0700

    Scan and create aggregation Jacoco report for ShardingSphere (#32396)
    * Add : add JDK env for jacoco #31940
    
    * Update : Update permision for shell file on CI #31940
    
    * Modified : Modified shell script to fit GithubAction Env#31940
    
    * Update Permission #31940
    
    * Make code-coverage-merge.sh executable#31940
    
    * Make code-coverage-merge.sh executable#31940
    
    * Fixed: syntax error #31940
    
    * Update: Add token for personal test codecov#31940
    
    * Update: Change CodeCov Update time for test#31940
    
    * Reverse:Change back #31940
    
    * Update: Update Codecov timeout#31940
    
    * Update: update codecov file #31940
    
    * update: pom file#31940
    
    * update: update to v3 for test codecov#31940
    
    * Update: remove duplicate line#31940
    
    * Update: Pom up to date
    
    * update jacoco version
    
    * change back to jacoco 0.8.8
    
    * Add details on script
    
    * Show token
    
    * Tested as mentor requested
    
    * change script to test
    
    * change for test
    
    * change codecov config
    
    * Change file path
    
    * Fixed path error
    
    * Update codeCov to V4
    
    * Fix codecov
    
    * fix error
    
    * Move-Jacoco.yml to Nightly-check
    
    * Remove mistake

---
+++ .github/workflows/resources/scripts/unit-test-coverage-merge/code-coverage-merge.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#Get project path
+project="$1"
+target="$project/target"
+echo $target
+
+# Create corresponding folders
+mkdir -p $target/mergeClassFile
+mkdir -p $target/mergeExecFile
+mkdir -p $target/mergeReport
+mkdir -p $target/cli
+
+# Download jacoco's cli using curl
+curl -O https://repo1.maven.org/maven2/org/jacoco/jacoco/0.8.11/jacoco-0.8.11.zip
+
+# Extract the downloaded zip file
+unzip -o jacoco-0.8.11.zip -d jacoco-0.8.11
+
+# Move the decompressed jacococli.jar to the cli folder
+mv jacoco-0.8.11/lib/jacococli.jar $target/cli/jacococli.jar
+
+jacococli="$target/cli/jacococli.jar"
+
+execMergePath="$target/mergeExecFile/merge.exec"
+echo $execMergePath
+echo 'Start searching for jacoco.exec files'
+
+# Search all jacoco.exec files in the whole project
+execFiles=$(find $project -name 'jacoco.exec')
+
+# Check if files exist
+if [ -z "$execFiles" ]; then
+  echo "Can't find jacoco.exec file in the specified directory"
+  exit 1
+fi
+
+# Create an array to store file paths
+execFileArray=()
+
+# Traverse the files and add file paths to the array
+for execFile in $execFiles; do
+  execFileArray+=("$execFile")
+done
+
+echo "Found ${#execFileArray[@]} jacoco.exec files"
+
+# Make sure the folder is empty
+rm -rf "$execMergePath"/*
+
+echo "Starting aggregation of jacoco.exec files"
+# Use jacococli to merge files and output them to the specified file
+java -jar $jacococli merge "${execFileArray[@]}" --destfile $execMergePath
+
+echo "Aggregation Success, output file at: $execMergePath"
+
+echo "Starting aggregation and compilation of code"
+# Start aggregation of all compiled code
+classOutPutDir="$target/mergeClassFile"
+
+# Find all classes folders
+classFolders=$(find $project -type d -name 'classes')
+
+# Check if folders are found
+if [ -z "$classFolders" ]; then
+  echo "The classes folder was not found in the given directory"
+  exit 1
+fi
+
+# Create an array to store folder paths
+classFolderArray=()
+
+# Traverse the folders and add the paths to the array
+for classFolder in $classFolders; do
+  classFolderArray+=("$classFolder")
+done
+
+# Make sure the folder is empty
+rm -rf "$classOutPutDir"/*
+
+echo "Found ${#classFolderArray[@]} classes folders"
+# Copy the folders to the specified directory in order, and add the copied folders in the following order: 1, 2, 3...
+counter=1
+for classFolder in "${classFolderArray[@]}"; do
+  cp -r "$classFolder" "$classOutPutDir/$counter"
+  let counter++
+done
+
+echo "Copy Success, the compilation merge output folder is located at: $classOutPutDir"
+
+echo "Start generating overall coverage reports"
+mergeReportDir="$target/mergeReport"
+# Make sure the folder is empty
+rm -rf "$mergeReportDir"/*
+# Use jacococli to generate HTML1
+java -jar $jacococli report $execMergePath --classfiles $classOutPutDir --sourcefiles $project --html $mergeReportDir
+# Use jacococli to generate XML
+java -jar $jacococli report $execMergePath --classfiles $classOutPutDir --sourcefiles $project --xml $mergeReportDir/jacoco.xml
+
+echo "Build success, report is at: $mergeReportDir"
