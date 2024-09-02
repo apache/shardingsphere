661872db6bf263c01954b28e5252aa97434ba0c2
Blue dot <814127107@qq.com>
2024-09-03 00:52:34 +0800

    Return result is null

--- features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dal/show/ShowTableStatusMergedResult.java
+++ features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dal/show/ShowTableStatusMergedResult.java
@@ -75,14 +75,14 @@ public final class ShowTableStatusMergedResult extends MemoryMergedResult<Shardi
     
     private BigInteger sum(final Object num1, final Object num2) {
         if (num1 == null || num2 == null){
-            return BigInteger.ZERO;
+            return null;
         }
         return ((BigInteger) num1).add((BigInteger) num2);
     }
     
     private BigInteger avg(final Object sum, final Object number) {
         if (sum == null || number == null){
-            return BigInteger.ZERO;
+            return null;
         }
         return BigInteger.ZERO.equals(number) ? BigInteger.ZERO : ((BigInteger) sum).divide((BigInteger) number);
     }
