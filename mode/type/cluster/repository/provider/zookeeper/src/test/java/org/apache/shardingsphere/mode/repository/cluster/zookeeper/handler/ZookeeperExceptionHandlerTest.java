5cbb330e7fc68cd29b589914644b322a3750d721
zhangliang <zhangliang@apache.org>
2024-09-08 22:48:52 +0800

    Add more test cases on ZookeeperExceptionHandler

--- mode/type/cluster/repository/provider/zookeeper/src/test/java/org/apache/shardingsphere/mode/repository/cluster/zookeeper/handler/ZookeeperExceptionHandlerTest.java
+++ mode/type/cluster/repository/provider/zookeeper/src/test/java/org/apache/shardingsphere/mode/repository/cluster/zookeeper/handler/ZookeeperExceptionHandlerTest.java
@@ -18,27 +18,63 @@
 package org.apache.shardingsphere.mode.repository.cluster.zookeeper.handler;
 
 import org.apache.shardingsphere.mode.repository.cluster.exception.ClusterRepositoryPersistException;
+import org.apache.zookeeper.KeeperException.ConnectionLossException;
 import org.apache.zookeeper.KeeperException.NoNodeException;
+import org.apache.zookeeper.KeeperException.NodeExistsException;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ZookeeperExceptionHandlerTest {
     
     @Test
-    void assertHandleException() {
-        ZookeeperExceptionHandler.handleException(null);
-        ZookeeperExceptionHandler.handleException(new NoNodeException(""));
-        ZookeeperExceptionHandler.handleException(new Exception(new NoNodeException("")));
-        try {
-            ZookeeperExceptionHandler.handleException(new Exception(""));
-            fail("must be failed handle new Exception().");
-            // CHECKSTYLE:OFF
-        } catch (final Exception ex) {
-            // CHECKSTYLE:ON
-            assertThat(ex, instanceOf(ClusterRepositoryPersistException.class));
-        }
+    void assertHandleNullException() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(null));
+    }
+    
+    @Test
+    void assertHandleConnectionLossException() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new ConnectionLossException()));
+    }
+    
+    @Test
+    void assertHandleConnectionLossExceptionWithCause() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new Exception(new ConnectionLossException())));
+    }
+    
+    @Test
+    void assertHandleNoNodeException() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new NoNodeException()));
+    }
+    
+    @Test
+    void assertHandleNoNodeExceptionWithCause() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new Exception(new NoNodeException())));
+    }
+    
+    @Test
+    void assertHandleNodeExistsException() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new NodeExistsException()));
+    }
+    
+    @Test
+    void assertHandleNodeExistsExceptionWithCause() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new Exception(new NodeExistsException())));
+    }
+    
+    @Test
+    void assertHandleInterruptedException() {
+        assertDoesNotThrow(() -> ZookeeperExceptionHandler.handleException(new InterruptedException()));
+    }
+    
+    @Test
+    void assertHandleUnIgnoredException() {
+        assertThrows(ClusterRepositoryPersistException.class, () -> ZookeeperExceptionHandler.handleException(new Exception("")));
+    }
+    
+    @Test
+    void assertHandleUnIgnoredExceptionWithCause() {
+        assertThrows(ClusterRepositoryPersistException.class, () -> ZookeeperExceptionHandler.handleException(new Exception(new RuntimeException(""))));
     }
 }
