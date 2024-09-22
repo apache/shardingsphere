c5dc97cdb1911791d4e4a47fe847dd546a8c8808
Liang Zhang <zhangliang@apache.org>
2024-09-22 13:21:02 +0800

    Refactor DataNode.format() (#32955)

--- infra/common/src/main/java/org/apache/shardingsphere/infra/datanode/DataNode.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/datanode/DataNode.java
@@ -105,15 +105,6 @@ public final class DataNode {
     /**
      * Format data node as string.
      *
-     * @return formatted data node
-     */
-    public String format() {
-        return dataSourceName + DELIMITER + tableName;
-    }
-    
-    /**
-     * Format data node as string.
-     *
      * @param databaseType database type
      * @return formatted data node
      */
