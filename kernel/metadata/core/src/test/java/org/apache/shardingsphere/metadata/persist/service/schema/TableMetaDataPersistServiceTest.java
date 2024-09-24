573531d69570191693099fa5d405576eca9e536e
Liang Zhang <zhangliang@apache.org>
2024-09-24 20:14:59 +0800

    Add test cases on TableMetaDataPersistService (#32982)

---
+++ kernel/metadata/core/src/test/java/org/apache/shardingsphere/metadata/persist/service/schema/TableMetaDataPersistServiceTest.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.metadata.persist.service.schema;
+
+import org.apache.shardingsphere.infra.metadata.database.schema.model.ShardingSphereTable;
+import org.apache.shardingsphere.metadata.persist.node.metadata.TableMetaDataNode;
+import org.apache.shardingsphere.metadata.persist.service.version.MetaDataVersionPersistService;
+import org.apache.shardingsphere.mode.spi.PersistRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TableMetaDataPersistServiceTest {
+    
+    private TableMetaDataPersistService persistService;
+    
+    @Mock
+    private PersistRepository repository;
+    
+    @Mock
+    private MetaDataVersionPersistService metaDataVersionPersistService;
+    
+    @BeforeEach
+    void setUp() {
+        persistService = new TableMetaDataPersistService(repository, metaDataVersionPersistService);
+    }
+    
+    @Test
+    void assertPersist() {
+        persistService.persist("foo_db", "foo_schema", Collections.singletonMap("foo_tbl", mock(ShardingSphereTable.class)));
+        verify(repository).persist("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/versions/0", "{}" + System.lineSeparator());
+        verify(repository).persist("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/active_version", "0");
+        verify(metaDataVersionPersistService).switchActiveVersion(any());
+    }
+    
+    @Test
+    void assertLoadWithEmptyTables() {
+        assertTrue(persistService.load("foo_db", "foo_schema").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithTables() {
+        when(repository.getChildrenKeys("/metadata/foo_db/schemas/foo_schema/tables")).thenReturn(Collections.singletonList("foo_tbl"));
+        when(repository.query("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/active_version")).thenReturn("0");
+        when(repository.query("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/versions/0")).thenReturn("{name: foo_tbl}");
+        Map<String, ShardingSphereTable> actual = persistService.load("foo_db", "foo_schema");
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get("foo_tbl").getName(), is("foo_tbl"));
+    }
+    
+    @Test
+    void assertLoadWithTable() {
+        when(repository.query("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/active_version")).thenReturn("0");
+        when(repository.query("/metadata/foo_db/schemas/foo_schema/tables/foo_tbl/versions/0")).thenReturn("{name: foo_tbl}");
+        Map<String, ShardingSphereTable> actual = persistService.load("foo_db", "foo_schema", "foo_tbl");
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get("foo_tbl").getName(), is("foo_tbl"));
+    }
+    
+    @Test
+    void assertDelete() {
+        persistService.delete("foo_db", "foo_schema", "foo_tbl");
+        verify(repository).delete(TableMetaDataNode.getTableNode("foo_db", "foo_schema", "foo_tbl"));
+    }
+}
