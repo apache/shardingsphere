6a26b21304ea9847a6da7a114c770be9fd26e73a
zhangliang <zhangliang@apache.org>
2024-10-01 10:40:34 +0800

    Refactor PrivilegeProvider.build() as single Grantee

--- kernel/authority/provider/simple/src/test/java/org/apache/shardingsphere/authority/provider/simple/AllPermittedPrivilegeProviderTest.java
+++ kernel/authority/provider/simple/src/test/java/org/apache/shardingsphere/authority/provider/simple/AllPermittedPrivilegeProviderTest.java
@@ -44,7 +44,7 @@ class AllPermittedPrivilegeProviderTest {
     void assertBuild(final String name, final PrivilegeProvider provider) {
         AuthorityRuleConfiguration ruleConfig = new AuthorityRuleConfiguration(
                 Collections.singleton(new UserConfiguration("root", "", "%", null, false)), mock(AlgorithmConfiguration.class), Collections.emptyMap(), null);
-        ShardingSpherePrivileges actual = provider.build(ruleConfig, (new Grantee("root@%")));
+        ShardingSpherePrivileges actual = provider.build(ruleConfig, new Grantee("root@%"));
         assertThat(actual, instanceOf(AllPermittedPrivileges.class));
     }
     
