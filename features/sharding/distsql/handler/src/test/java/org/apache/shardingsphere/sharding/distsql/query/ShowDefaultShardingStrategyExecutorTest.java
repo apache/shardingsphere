24f7126febc078423dfc5ddb6e73a78481ea7641
zhangliang <zhangliang@apache.org>
2024-10-04 14:01:06 +0800

    Refactor ShowDefaultShardingStrategyExecutorTest

--- features/sharding/distsql/handler/src/test/java/org/apache/shardingsphere/sharding/distsql/query/ShowDefaultShardingStrategyExecutorTest.java
+++ features/sharding/distsql/handler/src/test/java/org/apache/shardingsphere/sharding/distsql/query/ShowDefaultShardingStrategyExecutorTest.java
@@ -22,6 +22,7 @@ import org.apache.shardingsphere.distsql.handler.engine.query.DistSQLQueryExecut
 import org.apache.shardingsphere.infra.algorithm.core.config.AlgorithmConfiguration;
 import org.apache.shardingsphere.infra.merge.result.impl.local.LocalDataQueryResultRow;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
+import org.apache.shardingsphere.infra.metadata.database.rule.RuleMetaData;
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
 import org.apache.shardingsphere.sharding.api.config.strategy.sharding.ComplexShardingStrategyConfiguration;
@@ -35,9 +36,9 @@ import org.apache.shardingsphere.test.util.PropertiesBuilder.Property;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -47,46 +48,56 @@ import static org.mockito.Mockito.when;
 
 class ShowDefaultShardingStrategyExecutorTest {
     
-    private DistSQLQueryExecuteEngine engine;
-    
-    DistSQLQueryExecuteEngine setUp(final ShardingRuleConfiguration ruleConfig) {
-        return new DistSQLQueryExecuteEngine(mock(ShowDefaultShardingStrategyStatement.class), "foo_db", mockContextManager(ruleConfig), mock(DistSQLConnectionContext.class));
+    @Test
+    void assertExecuteQueryWithNullShardingStrategy() throws SQLException {
+        DistSQLQueryExecuteEngine engine = new DistSQLQueryExecuteEngine(
+                mock(ShowDefaultShardingStrategyStatement.class), "foo_db", mockContextManager(createRuleConfigurationWithNullShardingStrategy()), mock(DistSQLConnectionContext.class));
+        engine.executeQuery();
+        List<LocalDataQueryResultRow> actual = new ArrayList<>(engine.getRows());
+        assertThat(actual.size(), is(2));
+        assertThat(actual.get(0).getCell(1), is("TABLE"));
+        assertThat(actual.get(0).getCell(2), is(""));
+        assertThat(actual.get(0).getCell(3), is(""));
+        assertThat(actual.get(0).getCell(4), is(""));
+        assertThat(actual.get(0).getCell(5), is(""));
+        assertThat(actual.get(0).getCell(6), is(""));
+        assertThat(actual.get(1).getCell(1), is("DATABASE"));
+        assertThat(actual.get(1).getCell(2), is("COMPLEX"));
+        assertThat(actual.get(1).getCell(3), is("use_id, order_id"));
+        assertThat(actual.get(1).getCell(4), is("database_inline"));
+        assertThat(actual.get(1).getCell(5), is("INLINE"));
+        assertThat(actual.get(1).getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
     }
     
-    private ContextManager mockContextManager(final ShardingRuleConfiguration ruleConfig) {
-        ContextManager result = mock(ContextManager.class, RETURNS_DEEP_STUBS);
-        ShardingSphereDatabase database = mock(ShardingSphereDatabase.class, RETURNS_DEEP_STUBS);
-        when(result.getDatabase("foo_db")).thenReturn(database);
-        ShardingRule rule = mock(ShardingRule.class);
-        when(rule.getConfiguration()).thenReturn(ruleConfig);
-        when(database.getRuleMetaData().findSingleRule(ShardingRule.class)).thenReturn(Optional.of(rule));
+    private ShardingRuleConfiguration createRuleConfigurationWithNullShardingStrategy() {
+        ShardingRuleConfiguration result = new ShardingRuleConfiguration();
+        result.getShardingAlgorithms().put("database_inline", new AlgorithmConfiguration("INLINE", PropertiesBuilder.build(new Property("algorithm-expression", "ds_${user_id % 2}"))));
+        result.setDefaultDatabaseShardingStrategy(new ComplexShardingStrategyConfiguration("use_id, order_id", "database_inline"));
         return result;
     }
     
     @Test
-    void assertGetRowData1() throws SQLException {
-        engine = setUp(createRuleConfiguration1());
+    void assertExecuteQueryWithNoneShardingStrategyType() throws SQLException {
+        DistSQLQueryExecuteEngine engine = new DistSQLQueryExecuteEngine(
+                mock(ShowDefaultShardingStrategyStatement.class), "foo_db", mockContextManager(createRuleConfigurationWithNoneShardingStrategyType()), mock(DistSQLConnectionContext.class));
         engine.executeQuery();
-        Collection<LocalDataQueryResultRow> actual = engine.getRows();
+        List<LocalDataQueryResultRow> actual = new ArrayList<>(engine.getRows());
         assertThat(actual.size(), is(2));
-        Iterator<LocalDataQueryResultRow> iterator = actual.iterator();
-        LocalDataQueryResultRow row = iterator.next();
-        assertThat(row.getCell(1), is("TABLE"));
-        assertThat(row.getCell(2), is("NONE"));
-        assertThat(row.getCell(3), is(""));
-        assertThat(row.getCell(4), is(""));
-        assertThat(row.getCell(5), is(""));
-        assertThat(row.getCell(6), is(""));
-        row = iterator.next();
-        assertThat(row.getCell(1), is("DATABASE"));
-        assertThat(row.getCell(2), is("COMPLEX"));
-        assertThat(row.getCell(3), is("use_id, order_id"));
-        assertThat(row.getCell(4), is("database_inline"));
-        assertThat(row.getCell(5), is("INLINE"));
-        assertThat(row.getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
+        assertThat(actual.get(0).getCell(1), is("TABLE"));
+        assertThat(actual.get(0).getCell(2), is("NONE"));
+        assertThat(actual.get(0).getCell(3), is(""));
+        assertThat(actual.get(0).getCell(4), is(""));
+        assertThat(actual.get(0).getCell(5), is(""));
+        assertThat(actual.get(0).getCell(6), is(""));
+        assertThat(actual.get(1).getCell(1), is("DATABASE"));
+        assertThat(actual.get(1).getCell(2), is("COMPLEX"));
+        assertThat(actual.get(1).getCell(3), is("use_id, order_id"));
+        assertThat(actual.get(1).getCell(4), is("database_inline"));
+        assertThat(actual.get(1).getCell(5), is("INLINE"));
+        assertThat(actual.get(1).getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
     }
     
-    private ShardingRuleConfiguration createRuleConfiguration1() {
+    private ShardingRuleConfiguration createRuleConfigurationWithNoneShardingStrategyType() {
         ShardingRuleConfiguration result = new ShardingRuleConfiguration();
         result.getShardingAlgorithms().put("database_inline", new AlgorithmConfiguration("INLINE", PropertiesBuilder.build(new Property("algorithm-expression", "ds_${user_id % 2}"))));
         result.setDefaultTableShardingStrategy(new NoneShardingStrategyConfiguration());
@@ -95,33 +106,41 @@ class ShowDefaultShardingStrategyExecutorTest {
     }
     
     @Test
-    void assertGetRowData2() throws SQLException {
-        engine = setUp(createRuleConfiguration2());
+    void assertExecuteQueryWithShardingStrategyType() throws SQLException {
+        DistSQLQueryExecuteEngine engine = new DistSQLQueryExecuteEngine(
+                mock(ShowDefaultShardingStrategyStatement.class), "foo_db", mockContextManager(createRuleConfigurationWithShardingStrategyType()), mock(DistSQLConnectionContext.class));
         engine.executeQuery();
-        Collection<LocalDataQueryResultRow> actual = engine.getRows();
+        List<LocalDataQueryResultRow> actual = new ArrayList<>(engine.getRows());
         assertThat(actual.size(), is(2));
-        Iterator<LocalDataQueryResultRow> iterator = actual.iterator();
-        LocalDataQueryResultRow row = iterator.next();
-        assertThat(row.getCell(1), is("TABLE"));
-        assertThat(row.getCell(2), is("STANDARD"));
-        assertThat(row.getCell(3), is("use_id"));
-        assertThat(row.getCell(4), is("database_inline"));
-        assertThat(row.getCell(5), is("INLINE"));
-        assertThat(row.getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
-        row = iterator.next();
-        assertThat(row.getCell(1), is("DATABASE"));
-        assertThat(row.getCell(2), is("HINT"));
-        assertThat(row.getCell(3), is(""));
-        assertThat(row.getCell(4), is("database_inline"));
-        assertThat(row.getCell(5), is("INLINE"));
-        assertThat(row.getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
+        assertThat(actual.get(0).getCell(1), is("TABLE"));
+        assertThat(actual.get(0).getCell(2), is("STANDARD"));
+        assertThat(actual.get(0).getCell(3), is("use_id"));
+        assertThat(actual.get(0).getCell(4), is("database_inline"));
+        assertThat(actual.get(0).getCell(5), is("INLINE"));
+        assertThat(actual.get(0).getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
+        assertThat(actual.get(1).getCell(1), is("DATABASE"));
+        assertThat(actual.get(1).getCell(2), is("HINT"));
+        assertThat(actual.get(1).getCell(3), is(""));
+        assertThat(actual.get(1).getCell(4), is("database_inline"));
+        assertThat(actual.get(1).getCell(5), is("INLINE"));
+        assertThat(actual.get(1).getCell(6), is("{\"algorithm-expression\":\"ds_${user_id % 2}\"}"));
     }
     
-    private ShardingRuleConfiguration createRuleConfiguration2() {
+    private ShardingRuleConfiguration createRuleConfigurationWithShardingStrategyType() {
         ShardingRuleConfiguration result = new ShardingRuleConfiguration();
         result.getShardingAlgorithms().put("database_inline", new AlgorithmConfiguration("INLINE", PropertiesBuilder.build(new Property("algorithm-expression", "ds_${user_id % 2}"))));
         result.setDefaultTableShardingStrategy(new StandardShardingStrategyConfiguration("use_id", "database_inline"));
         result.setDefaultDatabaseShardingStrategy(new HintShardingStrategyConfiguration("database_inline"));
+        return result;
+    }
+    
+    private ContextManager mockContextManager(final ShardingRuleConfiguration ruleConfig) {
+        ContextManager result = mock(ContextManager.class, RETURNS_DEEP_STUBS);
+        ShardingSphereDatabase database = mock(ShardingSphereDatabase.class, RETURNS_DEEP_STUBS);
+        when(result.getDatabase("foo_db")).thenReturn(database);
+        ShardingRule rule = mock(ShardingRule.class);
+        when(rule.getConfiguration()).thenReturn(ruleConfig);
+        when(database.getRuleMetaData()).thenReturn(new RuleMetaData(Collections.singleton(rule)));
         return result;
     }
 }
