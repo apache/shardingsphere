2db95e06df6a433f55153143824c3116c4a0d7a0
zhangliang <zhangliang@apache.org>
2024-10-08 12:49:55 +0800

    Add SQLRouteExecutorDecider

--- infra/route/src/test/java/org/apache/shardingsphere/infra/route/engine/dialect/MySQLSQLRouteExecutorDeciderTest.java
+++ infra/route/src/test/java/org/apache/shardingsphere/infra/route/engine/dialect/MySQLSQLRouteExecutorDeciderTest.java
@@ -22,7 +22,8 @@ import org.apache.shardingsphere.sql.parser.statement.mysql.dal.MySQLShowTableSt
 import org.apache.shardingsphere.sql.parser.statement.mysql.dal.MySQLShowTablesStatement;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class MySQLSQLRouteExecutorDeciderTest {
