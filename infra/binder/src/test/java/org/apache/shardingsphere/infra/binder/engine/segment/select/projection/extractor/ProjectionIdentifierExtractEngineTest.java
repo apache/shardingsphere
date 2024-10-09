8a37b1a5b8c96448349c66419ffd10652479933c
Liang Zhang <zhangliang@apache.org>
2024-10-09 19:47:07 +0800

    Refactor ProjectionIdentifierExtractEngine (#33192)
    * Add more test cases on ProjectionsContext
    
    * Refactor ProjectionIdentifierExtractEngine
    
    * Refactor ProjectionIdentifierExtractEngine

--- infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/engine/segment/select/projection/extractor/ProjectionIdentifierExtractEngineTest.java
+++ infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/engine/segment/select/projection/extractor/ProjectionIdentifierExtractEngineTest.java
@@ -21,11 +21,14 @@ import org.apache.shardingsphere.infra.binder.context.segment.select.projection.
 import org.apache.shardingsphere.infra.database.core.metadata.database.enums.QuoteCharacter;
 import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.expr.subquery.SubquerySegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.item.SubqueryProjectionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.value.identifier.IdentifierValue;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
 
 class ProjectionIdentifierExtractEngineTest {
     
@@ -50,5 +53,23 @@ class ProjectionIdentifierExtractEngineTest {
                 is("FUNCTIONEXPRESSION"));
         assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "MySQL")).getColumnNameFromFunction(functionName, functionExpression),
                 is("FunctionExpression"));
+    }
+    
+    @Test
+    void assertGetColumnNameFromExpression() {
+        String expression = "expression";
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "PostgreSQL")).getColumnNameFromExpression(expression), is("?column?"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "openGauss")).getColumnNameFromExpression(expression), is("?column?"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "Oracle")).getColumnNameFromExpression(expression), is("EXPRESSION"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "MySQL")).getColumnNameFromExpression(expression), is("expression"));
+    }
+    
+    @Test
+    void assertGetColumnNameFromSubquery() {
+        SubqueryProjectionSegment projectionSegment = new SubqueryProjectionSegment(mock(SubquerySegment.class), "text");
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "PostgreSQL")).getColumnNameFromSubquery(projectionSegment), is("text"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "openGauss")).getColumnNameFromSubquery(projectionSegment), is("text"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "Oracle")).getColumnNameFromSubquery(projectionSegment), is("TEXT"));
+        assertThat(new ProjectionIdentifierExtractEngine(TypedSPILoader.getService(DatabaseType.class, "MySQL")).getColumnNameFromSubquery(projectionSegment), is("text"));
     }
 }
