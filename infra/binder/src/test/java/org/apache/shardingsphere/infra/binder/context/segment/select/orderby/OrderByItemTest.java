2ed5376787cdeeb9d8206bb70e59591ad97ec5f9
zhangliang <zhangliang@apache.org>
2024-10-11 07:30:29 +0800

    Add more test cases on OrderByItem

--- infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/context/segment/select/orderby/OrderByItemTest.java
+++ infra/binder/src/test/java/org/apache/shardingsphere/infra/binder/context/segment/select/orderby/OrderByItemTest.java
@@ -17,17 +17,55 @@
 
 package org.apache.shardingsphere.infra.binder.context.segment.select.orderby;
 
+import org.apache.shardingsphere.sql.parser.statement.core.enums.OrderDirection;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.order.item.OrderByItemSegment;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@SuppressWarnings("SimplifiableAssertion")
 class OrderByItemTest {
     
-    @SuppressWarnings({"SimplifiableAssertion", "ConstantValue"})
+    @SuppressWarnings("ConstantValue")
     @Test
-    void assertEqualsWhenObjIsNull() {
+    void assertEqualsWithNullObject() {
         assertFalse(new OrderByItem(mock(OrderByItemSegment.class)).equals(null));
+    }
+    
+    @Test
+    void assertEqualsWithDifferentOrderDirections() {
+        OrderByItemSegment segment1 = mock(OrderByItemSegment.class);
+        when(segment1.getOrderDirection()).thenReturn(OrderDirection.ASC);
+        OrderByItemSegment segment2 = mock(OrderByItemSegment.class);
+        when(segment2.getOrderDirection()).thenReturn(OrderDirection.DESC);
+        assertFalse(new OrderByItem(segment1).equals(new OrderByItem(segment2)));
+    }
+    
+    @Test
+    void assertEqualsWithDifferentIndexes() {
+        OrderByItem orderByItem1 = new OrderByItem(mock(OrderByItemSegment.class));
+        orderByItem1.setIndex(1);
+        OrderByItem orderByItem2 = new OrderByItem(mock(OrderByItemSegment.class));
+        orderByItem2.setIndex(2);
+        assertFalse(orderByItem1.equals(orderByItem2));
+    }
+    
+    @Test
+    void assertEquals() {
+        assertTrue(new OrderByItem(mock(OrderByItemSegment.class)).equals(new OrderByItem(mock(OrderByItemSegment.class))));
+    }
+    
+    @Test
+    void assertHashcode() {
+        OrderByItemSegment segment1 = mock(OrderByItemSegment.class);
+        when(segment1.getOrderDirection()).thenReturn(OrderDirection.ASC);
+        OrderByItemSegment segment2 = mock(OrderByItemSegment.class);
+        when(segment2.getOrderDirection()).thenReturn(OrderDirection.DESC);
+        assertThat(new OrderByItem(segment1).hashCode(), is(new OrderByItem(segment1).hashCode()));
     }
 }
