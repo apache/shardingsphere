6afa20c26f1605674f4f37eae827ddc3a762243d
ZhangCheng <chengzhang@apache.org>
2024-10-21 18:19:14 +0800

    Remove unnecessary toLowercase when using CaseInsensitiveMap

--- infra/rewrite/src/main/java/org/apache/shardingsphere/infra/rewrite/sql/token/common/pojo/generic/SubstitutableColumnNameToken.java
+++ infra/rewrite/src/main/java/org/apache/shardingsphere/infra/rewrite/sql/token/common/pojo/generic/SubstitutableColumnNameToken.java
@@ -80,7 +80,7 @@ public final class SubstitutableColumnNameToken extends SQLToken implements Subs
         }
         Map<String, String> result = new CaseInsensitiveMap<>();
         for (RouteMapper each : routeUnit.getTableMappers()) {
-            result.put(each.getLogicName().toLowerCase(), each.getActualName());
+            result.put(each.getLogicName(), each.getActualName());
         }
         return result;
     }
