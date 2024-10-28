ba7ba6fb46ec6ebf0630b89841684809556ff315
Liang Zhang <zhangliang@apache.org>
2024-10-28 18:11:51 +0800

    Add more test cases on MaskMergedResult (#33439)

--- features/mask/core/src/test/java/org/apache/shardingsphere/mask/merge/dql/MaskMergedResultTest.java
+++ features/mask/core/src/test/java/org/apache/shardingsphere/mask/merge/dql/MaskMergedResultTest.java
@@ -27,6 +27,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.io.InputStream;
 import java.io.Reader;
@@ -46,6 +48,7 @@ import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class MaskMergedResultTest {
     
     @Mock
@@ -58,6 +61,11 @@ class MaskMergedResultTest {
     
     @Test
     void assertGetValue() throws SQLException {
+        assertNull(new MaskMergedResult(mockMaskRule(), mockSelectStatementContext(), mergedResult).getValue(1, String.class));
+    }
+    
+    @Test
+    void assertGetValueWithNull() throws SQLException {
         when(mergedResult.getValue(1, Object.class)).thenReturn("VALUE");
         assertThat(new MaskMergedResult(mockMaskRule(), mockSelectStatementContext(), mergedResult).getValue(1, String.class), is("MASK_VALUE"));
     }
