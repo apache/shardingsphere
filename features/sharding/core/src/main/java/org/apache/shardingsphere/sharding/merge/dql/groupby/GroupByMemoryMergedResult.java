24d44a69ea571f4b364a49bc2158605ce98c3119
duanzhengqiang <duanzhengqiang@apache.org>
2024-10-31 09:45:16 +0800

    Revise pr#33380 codestyle and update release note

--- features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/groupby/GroupByMemoryMergedResult.java
+++ features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/merge/dql/groupby/GroupByMemoryMergedResult.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.sharding.merge.dql.groupby;
 
 import org.apache.shardingsphere.infra.binder.context.segment.select.projection.Projection;
-
 import org.apache.shardingsphere.infra.binder.context.segment.select.projection.impl.AggregationDistinctProjection;
 import org.apache.shardingsphere.infra.binder.context.segment.select.projection.impl.AggregationProjection;
 import org.apache.shardingsphere.infra.binder.context.statement.SQLStatementContext;
@@ -141,18 +140,15 @@ public final class GroupByMemoryMergedResult extends MemoryMergedResult<Sharding
     
     private List<MemoryQueryResultRow> getMemoryResultSetRows(final SelectStatementContext selectStatementContext,
                                                               final Map<GroupByValue, MemoryQueryResultRow> dataMap, final List<Boolean> valueCaseSensitive) {
-        Object[] data = generateReturnData(selectStatementContext);
-        
         if (dataMap.isEmpty()) {
+            Object[] data = generateReturnData(selectStatementContext);
             boolean hasGroupBy = !selectStatementContext.getGroupByContext().getItems().isEmpty();
             boolean hasAggregations = !selectStatementContext.getProjectionsContext().getAggregationProjections().isEmpty();
-            
             if (hasGroupBy || !hasAggregations) {
                 return Collections.emptyList();
             }
             return Collections.singletonList(new MemoryQueryResultRow(data));
         }
-        
         List<MemoryQueryResultRow> result = new ArrayList<>(dataMap.values());
         result.sort(new GroupByRowComparator(selectStatementContext, valueCaseSensitive));
         return result;
