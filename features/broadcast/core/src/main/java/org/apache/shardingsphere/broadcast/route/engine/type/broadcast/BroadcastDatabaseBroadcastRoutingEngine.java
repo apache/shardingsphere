cd9b8cec311a754f2434499cee52c66ed8425698
Liang Zhang <zhangliang@apache.org>
2024-11-01 19:40:59 +0800

    Add HighFrequencyInvocation annotation on Broadcast (#33501)
    * Add HighFrequencyInvocation annotation on Broadcast
    
    * Add HighFrequencyInvocation annotation on Broadcast

--- features/broadcast/core/src/main/java/org/apache/shardingsphere/broadcast/route/engine/type/broadcast/BroadcastDatabaseBroadcastRoutingEngine.java
+++ features/broadcast/core/src/main/java/org/apache/shardingsphere/broadcast/route/engine/type/broadcast/BroadcastDatabaseBroadcastRoutingEngine.java
@@ -19,6 +19,7 @@ package org.apache.shardingsphere.broadcast.route.engine.type.broadcast;
 
 import org.apache.shardingsphere.broadcast.route.engine.type.BroadcastRouteEngine;
 import org.apache.shardingsphere.broadcast.rule.BroadcastRule;
+import org.apache.shardingsphere.infra.annotation.HighFrequencyInvocation;
 import org.apache.shardingsphere.infra.route.context.RouteContext;
 import org.apache.shardingsphere.infra.route.context.RouteMapper;
 import org.apache.shardingsphere.infra.route.context.RouteUnit;
@@ -28,6 +29,7 @@ import java.util.Collections;
 /**
  * Broadcast routing engine for database.
  */
+@HighFrequencyInvocation
 public final class BroadcastDatabaseBroadcastRoutingEngine implements BroadcastRouteEngine {
     
     @Override
