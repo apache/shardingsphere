8174b3fcdef699fdf035f3b96ff64a4062a560b3
Liang Zhang <zhangliang@apache.org>
2024-11-04 18:28:48 +0800

    Remove useless DataNodeBuilder (#33537)
    * Remove useless DataNodeBuilder
    
    * Remove useless DataNodeBuilder

--- infra/common/src/main/java/org/apache/shardingsphere/infra/datanode/DataNodes.java
+++ infra/common/src/main/java/org/apache/shardingsphere/infra/datanode/DataNodes.java
@@ -21,12 +21,13 @@ import lombok.RequiredArgsConstructor;
 import org.apache.shardingsphere.infra.annotation.HighFrequencyInvocation;
 import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
 import org.apache.shardingsphere.infra.rule.attribute.datanode.DataNodeRuleAttribute;
-import org.apache.shardingsphere.infra.spi.type.ordered.OrderedSPILoader;
+import org.apache.shardingsphere.infra.rule.attribute.datasource.DataSourceMapperRuleAttribute;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Optional;
 
 /**
  * Data nodes.
@@ -36,14 +37,6 @@ public final class DataNodes {
     
     private final Collection<ShardingSphereRule> rules;
     
-    @SuppressWarnings("rawtypes")
-    private final Map<ShardingSphereRule, DataNodeBuilder> dataNodeBuilders;
-    
-    public DataNodes(final Collection<ShardingSphereRule> rules) {
-        this.rules = rules;
-        dataNodeBuilders = OrderedSPILoader.getServices(DataNodeBuilder.class, rules);
-    }
-    
     /**
      * Get data nodes.
      *
@@ -51,14 +44,16 @@ public final class DataNodes {
      * @return data nodes
      */
     @HighFrequencyInvocation
-    @SuppressWarnings({"unchecked", "rawtypes"})
     public Collection<DataNode> getDataNodes(final String tableName) {
         Collection<DataNode> result = getDataNodesByTableName(tableName);
         if (result.isEmpty()) {
             return result;
         }
-        for (Entry<ShardingSphereRule, DataNodeBuilder> entry : dataNodeBuilders.entrySet()) {
-            result = entry.getValue().build(result, entry.getKey());
+        for (ShardingSphereRule each : rules) {
+            Optional<DataSourceMapperRuleAttribute> dataSourceMapperRuleAttribute = each.getAttributes().findAttribute(DataSourceMapperRuleAttribute.class);
+            if (dataSourceMapperRuleAttribute.isPresent()) {
+                result = buildDataNodes(result, dataSourceMapperRuleAttribute.get());
+            }
         }
         return result;
     }
@@ -75,5 +70,14 @@ public final class DataNodes {
     
     private Collection<DataNode> getDataNodesByTableName(final ShardingSphereRule rule, final String tableName) {
         return rule.getAttributes().findAttribute(DataNodeRuleAttribute.class).map(optional -> optional.getDataNodesByTableName(tableName)).orElse(Collections.emptyList());
+    }
+    
+    private Collection<DataNode> buildDataNodes(final Collection<DataNode> dataNodes, final DataSourceMapperRuleAttribute dataSourceMapperRuleAttribute) {
+        Collection<DataNode> result = new LinkedList<>();
+        Map<String, Collection<String>> dataSourceMapper = dataSourceMapperRuleAttribute.getDataSourceMapper();
+        for (DataNode each : dataNodes) {
+            result.addAll(DataNodeUtils.buildDataNode(each, dataSourceMapper));
+        }
+        return result;
     }
 }
