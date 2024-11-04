530df2d3284db8c3ed6f824ae38b892bf6f3413c
Liang Zhang <zhangliang@apache.org>
2024-11-04 20:41:48 +0800

    Refactor HintShadowAlgorithmDeterminer (#33542)

--- features/shadow/core/src/main/java/org/apache/shardingsphere/shadow/route/determiner/ColumnShadowAlgorithmDeterminer.java
+++ features/shadow/core/src/main/java/org/apache/shardingsphere/shadow/route/determiner/ColumnShadowAlgorithmDeterminer.java
@@ -19,6 +19,7 @@ package org.apache.shardingsphere.shadow.route.determiner;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.shardingsphere.infra.annotation.HighFrequencyInvocation;
 import org.apache.shardingsphere.shadow.condition.ShadowCondition;
 import org.apache.shardingsphere.shadow.spi.ShadowOperationType;
 import org.apache.shardingsphere.shadow.spi.column.ColumnShadowAlgorithm;
@@ -31,6 +32,7 @@ import java.util.LinkedList;
 /**
  * Column shadow algorithm determiner.
  */
+@HighFrequencyInvocation
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ColumnShadowAlgorithmDeterminer {
     
