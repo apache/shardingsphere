1488b806113317726fd18df4c9315b87eb5b0b9f
Liang Zhang <zhangliang@apache.org>
2024-11-16 16:48:03 +0800

    Add more test cases on ShardingConstraintTokenGenerator (#33689)

--- features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/rewrite/token/generator/impl/ShardingConstraintTokenGeneratorTest.java
+++ features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/rewrite/token/generator/impl/ShardingConstraintTokenGeneratorTest.java
@@ -17,7 +17,7 @@
 
 package org.apache.shardingsphere.sharding.rewrite.token.generator.impl;
 
-import org.apache.shardingsphere.infra.binder.context.statement.UnknownSQLStatementContext;
+import org.apache.shardingsphere.infra.binder.context.statement.SQLStatementContext;
 import org.apache.shardingsphere.infra.binder.context.statement.ddl.AlterTableStatementContext;
 import org.apache.shardingsphere.infra.rewrite.sql.token.common.pojo.SQLToken;
 import org.apache.shardingsphere.sharding.rewrite.token.pojo.ConstraintToken;
@@ -28,27 +28,43 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ShardingConstraintTokenGeneratorTest {
     
     @Test
+    void assertIsNotGenerateSQLTokenWithNotConstraintAvailable() {
+        ShardingConstraintTokenGenerator generator = new ShardingConstraintTokenGenerator(mock(ShardingRule.class));
+        assertFalse(generator.isGenerateSQLToken(mock(SQLStatementContext.class)));
+    }
+    
+    @Test
+    void assertIsNotGenerateSQLTokenWithEmptyConstraint() {
+        ShardingConstraintTokenGenerator generator = new ShardingConstraintTokenGenerator(mock(ShardingRule.class));
+        AlterTableStatementContext alterTableStatementContext = mock(AlterTableStatementContext.class, RETURNS_DEEP_STUBS);
+        when(alterTableStatementContext.getConstraints().isEmpty()).thenReturn(true);
+        assertFalse(generator.isGenerateSQLToken(alterTableStatementContext));
+    }
+    
+    @Test
     void assertIsGenerateSQLToken() {
         ShardingConstraintTokenGenerator generator = new ShardingConstraintTokenGenerator(mock(ShardingRule.class));
-        assertFalse(generator.isGenerateSQLToken(mock(UnknownSQLStatementContext.class)));
-        AlterTableStatementContext alterTableStatementContext = mock(AlterTableStatementContext.class);
-        Collection<ConstraintSegment> constraintSegments = new LinkedList<>();
-        when(alterTableStatementContext.getConstraints()).thenReturn(constraintSegments);
-        assertFalse(generator.isGenerateSQLToken(alterTableStatementContext));
-        constraintSegments.add(mock(ConstraintSegment.class));
+        AlterTableStatementContext alterTableStatementContext = mock(AlterTableStatementContext.class, RETURNS_DEEP_STUBS);
         assertTrue(generator.isGenerateSQLToken(alterTableStatementContext));
+    }
+    
+    @Test
+    void assertGenerateSQLTokensWithNotConstraintAvailable() {
+        ShardingConstraintTokenGenerator generator = new ShardingConstraintTokenGenerator(mock(ShardingRule.class));
+        Collection<SQLToken> actual = generator.generateSQLTokens(mock(SQLStatementContext.class));
+        assertTrue(actual.isEmpty());
     }
     
     @Test
@@ -66,7 +82,7 @@ class ShardingConstraintTokenGeneratorTest {
         assertConstraintToken((ConstraintToken) actual.iterator().next());
     }
     
-    private static void assertConstraintToken(final ConstraintToken actual) {
+    private void assertConstraintToken(final ConstraintToken actual) {
         assertThat(actual.getStartIndex(), is(1));
         assertThat(actual.getStopIndex(), is(3));
     }
