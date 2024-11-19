235d3d5cababdedf2b8a499a51491691a9f59f97
Karok365 <anshsinghh365@gmail.com>
2024-11-19 15:00:57 +0530

    added LICENSE

--- kernel/transaction/type/xa/core/src/test/java/org/apache/shardingsphere/transaction/xa/jta/connection/dialect/OracleXAConnectionWrapperTest.java
+++ kernel/transaction/type/xa/core/src/test/java/org/apache/shardingsphere/transaction/xa/jta/connection/dialect/OracleXAConnectionWrapperTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.shardingsphere.transaction.xa.jta.connection.dialect;
 
 import oracle.jdbc.internal.OracleConnection;
@@ -18,15 +35,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class OracleXAConnectionWrapperTest {
-
+    
     private OracleXAConnectionWrapper xaConnectionWrapper;
-
+    
     @BeforeEach
     void setUp() {
         xaConnectionWrapper = new OracleXAConnectionWrapper();
         xaConnectionWrapper.init(new Properties());
     }
-
+    
     @Test
     void assertWrap() throws SQLException {
         XADataSource xaDataSource = mock(XADataSource.class);
@@ -34,12 +51,12 @@ class OracleXAConnectionWrapperTest {
         XAConnection actual = xaConnectionWrapper.wrap(xaDataSource, connection);
         assertThat(actual, instanceOf(XAConnection.class));
     }
-
+    
     @Test
     void assertGetDatabaseType() {
         assertThat(xaConnectionWrapper.getDatabaseType(), is("Oracle"));
     }
-
+    
     private Connection mockConnection() throws SQLException {
         Connection mockConnection = mock(Connection.class);
         when(mockConnection.unwrap(OracleConnection.class))
