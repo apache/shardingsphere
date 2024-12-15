fe1a3f0fc071033a64ef7f64f95e6a0c817dcb75
zhangliang <zhangliang@apache.org>
2024-12-15 10:55:35 +0800

    Move ClusterEventSubscriberRegistry to mode-core module

--- mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/event/ClusterEventSubscriberRegistryTest.java
+++ mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/event/ClusterEventSubscriberRegistryTest.java
@@ -15,28 +15,25 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.mode.event.subsciber;
+package org.apache.shardingsphere.mode.manager.cluster.event;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.shardingsphere.infra.util.eventbus.EventBusContext;
 import org.apache.shardingsphere.infra.util.eventbus.EventSubscriber;
+import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
+import java.util.Collections;
 
-/**
- * Event subscriber registry.
- */
-@RequiredArgsConstructor
-public final class EventSubscriberRegistry {
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class ClusterEventSubscriberRegistryTest {
     
-    private final EventBusContext eventBusContext;
-    
-    /**
-     * Register subscribers.
-     *
-     * @param subscribers to be registered subscribers
-     */
-    public void register(final Collection<EventSubscriber> subscribers) {
-        subscribers.forEach(eventBusContext::register);
+    @Test
+    void assertRegister() {
+        EventBusContext eventBusContext = mock(EventBusContext.class);
+        ClusterEventSubscriberRegistry registry = new ClusterEventSubscriberRegistry(eventBusContext);
+        EventSubscriber subscriber = mock(EventSubscriber.class);
+        registry.register(Collections.singleton(subscriber));
+        verify(eventBusContext).register(subscriber);
     }
 }
