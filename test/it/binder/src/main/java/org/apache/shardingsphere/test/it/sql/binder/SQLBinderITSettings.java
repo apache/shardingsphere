368d98e6a554f67d98b0bbe3414275643e58b0c5
duanzhengqiang <duanzhengqiang@apache.org>
2024-12-17 14:00:06 +0800

    revise java doc

--- test/it/binder/src/main/java/org/apache/shardingsphere/test/it/sql/binder/SQLBinderITSettings.java
+++ test/it/binder/src/main/java/org/apache/shardingsphere/test/it/sql/binder/SQLBinderITSettings.java
@@ -22,7 +22,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Internal SQL parser integrate test settings.
+ * SQL binder integrate test settings.
  */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
