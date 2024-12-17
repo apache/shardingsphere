8c33423979537ea2456509f26a4ae1565a2e06f4
Liang Zhang <zhangliang@apache.org>
2024-12-17 18:30:49 +0800

    Remove useless codes on SQLStatementType (#34087)

--- parser/sql/statement/core/src/main/java/org/apache/shardingsphere/sql/parser/statement/core/statement/SQLStatementType.java
+++ parser/sql/statement/core/src/main/java/org/apache/shardingsphere/sql/parser/statement/core/statement/SQLStatementType.java
@@ -17,48 +17,10 @@
 
 package org.apache.shardingsphere.sql.parser.statement.core.statement;
 
-import org.apache.shardingsphere.sql.parser.statement.core.statement.ddl.DDLStatement;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.dml.DMLStatement;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.dml.SelectStatement;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * SQL statement type.
  */
 public enum SQLStatementType {
     
-    DML, DDL, TCL, DCL, DAL, RL;
-    
-    private static final Collection<Class<? extends SQLStatement>> INVOLVE_DATA_CHANGES_STATEMENTS = Collections.newSetFromMap(new ConcurrentHashMap<>());
-    
-    private static final Collection<Class<? extends SQLStatement>> NOT_INVOLVE_DATA_CHANGES_STATEMENTS = Collections.newSetFromMap(new ConcurrentHashMap<>());
-    
-    /**
-     * Judge whether involves data changes.
-     *
-     * @param sqlStatement SQL statement
-     * @return is statement involves data changes or not
-     */
-    public static boolean involvesDataChanges(final SQLStatement sqlStatement) {
-        Class<? extends SQLStatement> sqlStatementClass = sqlStatement.getClass();
-        if (NOT_INVOLVE_DATA_CHANGES_STATEMENTS.contains(sqlStatementClass)) {
-            return false;
-        }
-        if (INVOLVE_DATA_CHANGES_STATEMENTS.contains(sqlStatementClass)) {
-            return true;
-        }
-        if (sqlStatement instanceof SelectStatement) {
-            NOT_INVOLVE_DATA_CHANGES_STATEMENTS.add(sqlStatementClass);
-            return false;
-        }
-        if (sqlStatement instanceof DMLStatement || sqlStatement instanceof DDLStatement) {
-            INVOLVE_DATA_CHANGES_STATEMENTS.add(sqlStatementClass);
-            return true;
-        }
-        NOT_INVOLVE_DATA_CHANGES_STATEMENTS.add(sqlStatementClass);
-        return false;
-    }
+    DML, DDL, TCL, DCL, DAL, RL
 }
