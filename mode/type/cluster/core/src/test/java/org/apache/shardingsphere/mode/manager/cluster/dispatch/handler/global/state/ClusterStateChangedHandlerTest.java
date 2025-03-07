baef034a840c21778faea61b66c007eea94798ed
zhangliang <zhangliang@apache.org>
2025-03-08 06:31:18 +0800

    Fix ClusterStateChangedHandlerTest

--- mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/dispatch/handler/global/state/ClusterStateChangedHandlerTest.java
+++ mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/dispatch/handler/global/state/ClusterStateChangedHandlerTest.java
@@ -55,7 +55,7 @@ class ClusterStateChangedHandlerTest {
     
     @Test
     void assertHandleWithInvalidClusterState() {
-        handler.handle(contextManager, new DataChangedEvent("/states/cluster_state", "INVALID", Type.UPDATED));
+        handler.handle(contextManager, new DataChangedEvent("/states/cluster_state", "", Type.UPDATED));
         verify(contextManager.getStateContext()).switchState(ClusterState.OK);
     }
 }
