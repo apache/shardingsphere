38b8a7905c63a64e12a452a78d3878b5c8b23aed
Haoran Meng <menghaoranss@gmail.com>
2025-03-10 16:18:25 +0800

    Move MetaDataVersion from common to mode node

--- mode/core/src/main/java/org/apache/shardingsphere/mode/metadata/persist/version/VersionPersistService.java
+++ mode/core/src/main/java/org/apache/shardingsphere/mode/metadata/persist/version/VersionPersistService.java
@@ -19,7 +19,7 @@ package org.apache.shardingsphere.mode.metadata.persist.version;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.infra.metadata.version.MetaDataVersion;
+import org.apache.shardingsphere.mode.node.path.version.MetaDataVersion;
 import org.apache.shardingsphere.mode.node.path.version.VersionNodePath;
 import org.apache.shardingsphere.mode.spi.repository.PersistRepository;
 
