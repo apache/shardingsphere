1ca76c1e95eeefbd5158d51dc4e99e3f39e9e515
zhangliang <zhangliang@apache.org>
2025-03-14 15:24:31 +0800

    Refactor ClusterPersistRepository

--- mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/persist/service/ClusterComputeNodePersistServiceTest.java
+++ mode/type/cluster/core/src/test/java/org/apache/shardingsphere/mode/manager/cluster/persist/service/ClusterComputeNodePersistServiceTest.java
@@ -23,7 +23,7 @@ import org.apache.shardingsphere.infra.instance.metadata.proxy.ProxyInstanceMeta
 import org.apache.shardingsphere.infra.instance.yaml.YamlComputeNodeData;
 import org.apache.shardingsphere.infra.state.instance.InstanceState;
 import org.apache.shardingsphere.infra.util.yaml.YamlEngine;
-import org.apache.shardingsphere.mode.spi.repository.PersistRepository;
+import org.apache.shardingsphere.mode.repository.cluster.ClusterPersistRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -50,7 +50,7 @@ class ClusterComputeNodePersistServiceTest {
     private ClusterComputeNodePersistService computeNodePersistService;
     
     @Mock
-    private PersistRepository repository;
+    private ClusterPersistRepository repository;
     
     @BeforeEach
     void setUp() {
