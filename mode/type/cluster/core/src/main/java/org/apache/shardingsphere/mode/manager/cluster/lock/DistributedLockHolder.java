079ca14c36faee619bf6e5ef418164e12b3d1e03
zhangliang <zhangliang@apache.org>
2025-03-31 17:33:59 +0800

    Move DistributedLockHolder to cluster-mode-core module

--- mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/lock/DistributedLockHolder.java
+++ mode/type/cluster/core/src/main/java/org/apache/shardingsphere/mode/manager/cluster/lock/DistributedLockHolder.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.mode.repository.cluster.core.lock;
+package org.apache.shardingsphere.mode.manager.cluster.lock;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
