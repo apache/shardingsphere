86874ffa69d34e38f7ca17ada30af9bf9fa601f6
Liang Zhang <zhangliang@apache.org>
2025-05-05 00:45:23 +0800

    Generalize OpenGaussContainer database type checking (#35324)
    * Replace MockedDatabaseType with TypedSPILoader in tests
    
    - Remove usage of MockedDatabaseType from multiple test files
    - Use TypedSPILoader.getService(DatabaseType.class, "FIXTURE") to get DatabaseType instance
    - Update import statements and test logic to use the new DatabaseType instance
    - Improve test readability and reduce reliance on mock classes
    
    * Remove redundant method and optimize test structure
    
    - Remove getDialectTableMetaDataLoader() method
    - Directly use TypedSPILoader and DatabaseTypedSPILoader services
    - Add databaseType and dialectMetaDataLoader as test class fields
    - Update test methods to use the new approach
    - Add JDBCResourceOpenedButNotSafelyClosed annotation to relevant tests
    
    * Generalize OpenGaussContainer database type checking
    
    - Replace specific OpenGaussDatabaseType checks with a generic getDatabaseType() method
    - Improve code readability and maintainability by removing class type comparisons
    
    * Generalize OpenGaussContainer database type checking
    
    - Replace specific OpenGaussDatabaseType checks with a generic getDatabaseType() method
    - Improve code readability and maintainability by removing class type comparisons

--- test/e2e/env/src/test/java/org/apache/shardingsphere/test/e2e/env/container/atomic/storage/impl/OpenGaussContainer.java
+++ test/e2e/env/src/test/java/org/apache/shardingsphere/test/e2e/env/container/atomic/storage/impl/OpenGaussContainer.java
@@ -19,7 +19,6 @@ package org.apache.shardingsphere.test.e2e.env.container.atomic.storage.impl;
 
 import com.google.common.base.Strings;
 import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
-import org.apache.shardingsphere.infra.database.opengauss.type.OpenGaussDatabaseType;
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.apache.shardingsphere.test.e2e.env.container.atomic.constants.StorageContainerConstants;
 import org.apache.shardingsphere.test.e2e.env.container.atomic.storage.DockerStorageContainer;
@@ -64,13 +63,13 @@ public final class OpenGaussContainer extends DockerStorageContainer {
     @Override
     protected Collection<String> getDatabaseNames() {
         return storageContainerConfig.getDatabaseTypes().entrySet().stream()
-                .filter(entry -> entry.getValue() instanceof OpenGaussDatabaseType).map(Map.Entry::getKey).collect(Collectors.toList());
+                .filter(entry -> entry.getValue() == getDatabaseType()).map(Map.Entry::getKey).collect(Collectors.toList());
     }
     
     @Override
     protected Collection<String> getExpectedDatabaseNames() {
         return storageContainerConfig.getExpectedDatabaseTypes().entrySet().stream()
-                .filter(entry -> entry.getValue() instanceof OpenGaussDatabaseType).map(Map.Entry::getKey).collect(Collectors.toList());
+                .filter(entry -> entry.getValue() == getDatabaseType()).map(Map.Entry::getKey).collect(Collectors.toList());
     }
     
     @Override
