a67df15e371d7d516d926d9e46828b3ff4e37c8a
Liang Zhang <zhangliang@apache.org>
2025-05-05 15:47:56 +0800

    Replace H2DatabaseType with DatabaseType in H2MetaDataLoaderTest (#35333)
    * Replace H2DatabaseType with DatabaseType in H2MetaDataLoaderTest
    
    - Remove direct usage of H2DatabaseType class
    - Use DatabaseType interface with TypedSPILoader to get H2 database type- Update test cases to use the new DatabaseType instance
    
    * Replace H2DatabaseType with TypedSPILoader for DatabaseType
    
    - Use TypedSPILoader.getService(DatabaseType.class, "FIXTURE") instead of H2DatabaseType
    - This change improves code flexibility and decouples the sharding route engine from H2DatabaseType

--- features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/route/engine/fixture/ShardingRouteEngineFixtureBuilder.java
+++ features/sharding/core/src/test/java/org/apache/shardingsphere/sharding/route/engine/fixture/ShardingRouteEngineFixtureBuilder.java
@@ -23,10 +23,11 @@ import lombok.SneakyThrows;
 import org.apache.groovy.util.Maps;
 import org.apache.shardingsphere.infra.algorithm.core.config.AlgorithmConfiguration;
 import org.apache.shardingsphere.infra.binder.context.statement.SQLStatementContext;
-import org.apache.shardingsphere.infra.database.h2.type.H2DatabaseType;
+import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.instance.ComputeNodeInstanceContext;
 import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
 import org.apache.shardingsphere.infra.rule.attribute.datanode.MutableDataNodeRuleAttribute;
+import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
 import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableReferenceRuleConfiguration;
 import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
@@ -293,7 +294,7 @@ public final class ShardingRouteEngineFixtureBuilder {
      */
     public static SingleRule createSingleRule(final Collection<ShardingSphereRule> rules) {
         Map<String, DataSource> dataSourceMap = createDataSourceMap();
-        SingleRule result = new SingleRule(new SingleRuleConfiguration(), "foo_db", new H2DatabaseType(), dataSourceMap, rules);
+        SingleRule result = new SingleRule(new SingleRuleConfiguration(), "foo_db", TypedSPILoader.getService(DatabaseType.class, "FIXTURE"), dataSourceMap, rules);
         result.getAttributes().getAttribute(MutableDataNodeRuleAttribute.class).put(dataSourceMap.keySet().iterator().next(), "foo_db", "t_category");
         return result;
     }
