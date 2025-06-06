cfd9642df1a63b10ef7fb9213b0dd58341113e00
Liang Zhang <zhangliang@apache.org>
2025-06-06 22:52:11 +0800

    Remove NullMarked annotations and simplify cache loader implementations (#35610)

--- infra/parser/src/main/java/org/apache/shardingsphere/infra/parser/cache/SQLStatementCacheLoader.java
+++ infra/parser/src/main/java/org/apache/shardingsphere/infra/parser/cache/SQLStatementCacheLoader.java
@@ -23,8 +23,6 @@ import org.apache.shardingsphere.infra.parser.sql.SQLStatementParserExecutor;
 import org.apache.shardingsphere.sql.parser.api.CacheOption;
 import org.apache.shardingsphere.sql.parser.statement.core.statement.SQLStatement;
 
-import org.jspecify.annotations.NullMarked;
-
 /**
  * SQL statement cache loader.
  */
@@ -45,7 +43,6 @@ public final class SQLStatementCacheLoader implements CacheLoader<String, SQLSta
         sqlStatementParserExecutor.updateCacheOption(parseTreeCacheOption);
     }
     
-    @NullMarked
     @Override
     public SQLStatement load(final String sql) {
         return sqlStatementParserExecutor.parse(sql);
