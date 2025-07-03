ecee35cc94001bcf4c33c53b566057f123cf0f93
s1eep <vijayian@126.com>
2025-07-03 22:43:54 +0800

    Refactor PostgreSQLBinaryProtocolValue related test cases #35841

--- db-protocol/postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/packet/command/query/extended/bind/protocol/PostgreSQLInt4BinaryProtocolValueTest.java
+++ db-protocol/postgresql/src/test/java/org/apache/shardingsphere/db/protocol/postgresql/packet/command/query/extended/bind/protocol/PostgreSQLInt4BinaryProtocolValueTest.java
@@ -33,13 +33,23 @@ class PostgreSQLInt4BinaryProtocolValueTest {
     
     @Mock
     private PostgreSQLPacketPayload payload;
-    
+
     @Test
-    void assertNewInstance() {
+    void assertGetColumnLength() {
         PostgreSQLInt4BinaryProtocolValue actual = new PostgreSQLInt4BinaryProtocolValue();
         assertThat(actual.getColumnLength(payload, null), is(4));
+    }
+
+    @Test
+    void assertRead() {
+        PostgreSQLInt4BinaryProtocolValue actual = new PostgreSQLInt4BinaryProtocolValue();
         when(payload.readInt4()).thenReturn(1);
         assertThat(actual.read(payload, 4), is(1));
+    }
+
+    @Test
+    void assertWrite() {
+        PostgreSQLInt4BinaryProtocolValue actual = new PostgreSQLInt4BinaryProtocolValue();
         actual.write(payload, 1);
         verify(payload).writeInt4(1);
     }
