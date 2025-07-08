3f99c2345548d44b42c270544e6c802805b1c0e4
Haoran Meng <menghaoranss@gmail.com>
2025-07-08 15:57:54 +0800

    Fix checkstyle

--- mode/type/standalone/repository/provider/memory/src/main/java/org/apache/shardingsphere/mode/repository/standalone/memory/MemoryRepository.java
+++ mode/type/standalone/repository/provider/memory/src/main/java/org/apache/shardingsphere/mode/repository/standalone/memory/MemoryRepository.java
@@ -27,16 +27,16 @@ import java.util.stream.Collectors;
  * Memory repository.
  */
 public final class MemoryRepository implements StandalonePersistRepository {
-
+    
     // CHECKSTYLE:OFF
     private final TreeMap<String, String> metaDataSortedMap = new TreeMap<>();
     // CHECKSTYLE:ON
-
+    
     @Override
     public String query(final String key) {
         return metaDataSortedMap.get(key);
     }
-
+    
     @Override
     public List<String> getChildrenKeys(final String key) {
         String searchPrefix;
@@ -60,34 +60,34 @@ public final class MemoryRepository implements StandalonePersistRepository {
                 .distinct()
                 .collect(Collectors.toList());
     }
-
+    
     @Override
     public boolean isExisted(final String key) {
         return metaDataSortedMap.containsKey(key);
     }
-
+    
     @Override
     public void persist(final String key, final String value) {
         metaDataSortedMap.put(key, value);
     }
-
+    
     @Override
     public void update(final String key, final String value) {
         if (metaDataSortedMap.containsKey(key)) {
             metaDataSortedMap.put(key, value);
         }
     }
-
+    
     @Override
     public void delete(final String key) {
         metaDataSortedMap.remove(key);
     }
-
+    
     @Override
     public void close() {
 
     }
-
+    
     @Override
     public Object getType() {
         return "Memory";
--- mode/type/standalone/repository/provider/memory/src/test/java/org/apache/shardingsphere/mode/repository/standalone/memory/MemoryRepositoryTest.java
+++ mode/type/standalone/repository/provider/memory/src/test/java/org/apache/shardingsphere/mode/repository/standalone/memory/MemoryRepositoryTest.java
@@ -26,9 +26,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class MemoryRepositoryTest {
-
+    
     private final MemoryRepository memoryRepository = new MemoryRepository();
-
+    
     @BeforeEach
     void setUp() {
         memoryRepository.persist("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0", "t_order");
@@ -36,27 +36,27 @@ class MemoryRepositoryTest {
         memoryRepository.persist("/metadata/encrypt_db/schemas/encrypt_db/tables/t_encrypt_01/versions/0", "t_encrypt_01");
         memoryRepository.persist("/metadata/encrypt_db/schemas/encrypt_db/tables/t_encrypt_02/versions/0", "t_encrypt_02");
     }
-
+    
     @Test
     void assertPersist() {
         memoryRepository.persist("testKey", "testValue");
         assertThat(memoryRepository.query("testKey"), is("testValue"));
     }
-
+    
     @Test
     void assertUpdate() {
         memoryRepository.update("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0", "t_order_updated");
         assertThat(memoryRepository.query("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0"),
                 is("t_order_updated"));
     }
-
+    
     @Test
     void assertDelete() {
         memoryRepository.delete("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0");
         assertThat(memoryRepository.isExisted("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0"), is(false));
         assertThat(memoryRepository.query("/metadata/sharding_db/schemas/sharding_db/tables/t_order/versions/0"), is((String) null));
     }
-
+    
     @Test
     void assertGetChildrenKeys() {
         assertThat(memoryRepository.getChildrenKeys("/metadata").size(), is(2));
