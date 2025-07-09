731b4a7ba3372e91ad8097831b66aeb8c68e4e3a
zhangliang <zhangliang@apache.org>
2025-07-10 00:57:38 +0800

    Decouple CDCDataNodeUtils and specific rules

--- kernel/data-pipeline/scenario/cdc/core/src/test/java/org/apache/shardingsphere/data/pipeline/cdc/util/CDCDataNodeUtilsTest.java
+++ kernel/data-pipeline/scenario/cdc/core/src/test/java/org/apache/shardingsphere/data/pipeline/cdc/util/CDCDataNodeUtilsTest.java
@@ -17,27 +17,18 @@
 
 package org.apache.shardingsphere.data.pipeline.cdc.util;
 
-import org.apache.shardingsphere.broadcast.rule.BroadcastRule;
 import org.apache.shardingsphere.infra.datanode.DataNode;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.RuleMetaData;
-import org.apache.shardingsphere.infra.rule.attribute.RuleAttributes;
 import org.apache.shardingsphere.infra.rule.attribute.datanode.DataNodeRuleAttribute;
-import org.apache.shardingsphere.sharding.rule.ShardingRule;
-import org.apache.shardingsphere.sharding.rule.ShardingTable;
-import org.apache.shardingsphere.single.rule.SingleRule;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -45,32 +36,13 @@ class CDCDataNodeUtilsTest {
     
     @Test
     void assertBuildTableAndDataNodesMap() {
-        ShardingSphereDatabase mockDatabase = mock(ShardingSphereDatabase.class);
-        RuleMetaData mockRuleMetaData = mock(RuleMetaData.class);
-        ShardingRule mockShardingRule = mock(ShardingRule.class);
-        ShardingTable mockShardingTable = mock(ShardingTable.class);
-        when(mockShardingTable.getActualDataNodes()).thenReturn(Collections.singletonList(new DataNode("ds_0.t_order")));
-        when(mockShardingRule.findShardingTable("t_order")).thenReturn(Optional.of(mockShardingTable));
-        when(mockShardingRule.getShardingTable("t_order")).thenReturn(mockShardingTable);
-        when(mockRuleMetaData.findSingleRule(ShardingRule.class)).thenReturn(Optional.of(mockShardingRule));
-        SingleRule singleRule = mock(SingleRule.class);
-        when(mockRuleMetaData.findSingleRule(SingleRule.class)).thenReturn(Optional.of(singleRule));
-        DataNodeRuleAttribute singleDataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
-        when(singleDataNodeRuleAttribute.getAllDataNodes()).thenReturn(Collections.singletonMap("t_order_item", Collections.singletonList(new DataNode("single.t_order_item"))));
-        when(singleRule.getAttributes()).thenReturn(new RuleAttributes(singleDataNodeRuleAttribute));
-        when(mockDatabase.getRuleMetaData()).thenReturn(mockRuleMetaData);
-        BroadcastRule broadcastRule = mock(BroadcastRule.class, RETURNS_DEEP_STUBS);
-        when(mockRuleMetaData.findSingleRule(BroadcastRule.class)).thenReturn(Optional.of(broadcastRule));
-        DataNodeRuleAttribute broadcastDataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
-        when(broadcastDataNodeRuleAttribute.findFirstActualTable("t_address")).thenReturn(Optional.of("broadcast.t_address"));
-        when(broadcastDataNodeRuleAttribute.getAllDataNodes()).thenReturn(Collections.singletonMap("t_address", Collections.singletonList(new DataNode("broadcast.t_address"))));
-        when(broadcastRule.getAttributes()).thenReturn(new RuleAttributes(broadcastDataNodeRuleAttribute));
-        Map<String, List<DataNode>> actual = CDCDataNodeUtils.buildTableAndDataNodesMap(mockDatabase, Arrays.asList("t_order", "t_order_item", "t_address"));
-        assertTrue(actual.containsKey("t_order"));
-        assertTrue(actual.containsKey("t_order_item"));
-        assertTrue(actual.containsKey("t_address"));
-        assertThat(actual.get("t_order").get(0).getDataSourceName(), is("ds_0"));
-        assertThat(actual.get("t_order_item").get(0).getDataSourceName(), is("single"));
-        assertThat(actual.get("t_address").get(0).getDataSourceName(), is("broadcast"));
+        RuleMetaData ruleMetaData = mock(RuleMetaData.class);
+        DataNodeRuleAttribute dataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
+        when(dataNodeRuleAttribute.getAllDataNodes()).thenReturn(Collections.singletonMap("foo_tbl", Collections.singletonList(new DataNode("foo_ds.foo_tbl"))));
+        when(ruleMetaData.getAttributes(DataNodeRuleAttribute.class)).thenReturn(Collections.singleton(dataNodeRuleAttribute));
+        ShardingSphereDatabase database = mock(ShardingSphereDatabase.class);
+        when(database.getRuleMetaData()).thenReturn(ruleMetaData);
+        Map<String, List<DataNode>> actual = CDCDataNodeUtils.buildTableAndDataNodesMap(database, Collections.singleton("foo_tbl"));
+        assertThat(actual, is(Collections.singletonMap("foo_tbl", Collections.singletonList(new DataNode("foo_ds.foo_tbl")))));
     }
 }
