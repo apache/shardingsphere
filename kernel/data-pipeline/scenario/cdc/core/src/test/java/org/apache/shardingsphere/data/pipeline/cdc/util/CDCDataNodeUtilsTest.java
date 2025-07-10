9d5f7b8f376d58713263a731354ba6e90e75dd8f
zhangliang <zhangliang@apache.org>
2025-07-10 14:09:08 +0800

    Decouple CDCDataNodeUtils and specific rules

--- kernel/data-pipeline/scenario/cdc/core/src/test/java/org/apache/shardingsphere/data/pipeline/cdc/util/CDCDataNodeUtilsTest.java
+++ kernel/data-pipeline/scenario/cdc/core/src/test/java/org/apache/shardingsphere/data/pipeline/cdc/util/CDCDataNodeUtilsTest.java
@@ -17,18 +17,21 @@
 
 package org.apache.shardingsphere.data.pipeline.cdc.util;
 
+import org.apache.shardingsphere.data.pipeline.core.exception.param.PipelineInvalidParameterException;
 import org.apache.shardingsphere.infra.datanode.DataNode;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.RuleMetaData;
 import org.apache.shardingsphere.infra.rule.attribute.datanode.DataNodeRuleAttribute;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,12 +40,26 @@ class CDCDataNodeUtilsTest {
     @Test
     void assertBuildTableAndDataNodesMap() {
         RuleMetaData ruleMetaData = mock(RuleMetaData.class);
-        DataNodeRuleAttribute dataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
-        when(dataNodeRuleAttribute.getAllDataNodes()).thenReturn(Collections.singletonMap("foo_tbl", Collections.singletonList(new DataNode("foo_ds.foo_tbl"))));
-        when(ruleMetaData.getAttributes(DataNodeRuleAttribute.class)).thenReturn(Collections.singleton(dataNodeRuleAttribute));
+        DataNodeRuleAttribute notReplicaBasedDistributionDataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
+        when(notReplicaBasedDistributionDataNodeRuleAttribute.getDataNodesByTableName("foo_tbl")).thenReturn(Collections.singleton(new DataNode("foo_ds.foo_tbl")));
+        DataNodeRuleAttribute replicaBasedDistributionDataNodeRuleAttribute = mock(DataNodeRuleAttribute.class);
+        when(replicaBasedDistributionDataNodeRuleAttribute.getDataNodesByTableName("bar_tbl")).thenReturn(Arrays.asList(new DataNode("foo_ds.bar_tbl"), new DataNode("bar_ds.bar_tbl")));
+        when(replicaBasedDistributionDataNodeRuleAttribute.isReplicaBasedDistribution()).thenReturn(true);
+        when(ruleMetaData.getAttributes(DataNodeRuleAttribute.class)).thenReturn(Arrays.asList(notReplicaBasedDistributionDataNodeRuleAttribute, replicaBasedDistributionDataNodeRuleAttribute));
         ShardingSphereDatabase database = mock(ShardingSphereDatabase.class);
         when(database.getRuleMetaData()).thenReturn(ruleMetaData);
-        Map<String, List<DataNode>> actual = CDCDataNodeUtils.buildTableAndDataNodesMap(database, Collections.singleton("foo_tbl"));
-        assertThat(actual, is(Collections.singletonMap("foo_tbl", Collections.singletonList(new DataNode("foo_ds.foo_tbl")))));
+        Map<String, List<DataNode>> actual = CDCDataNodeUtils.buildTableAndDataNodesMap(database, Arrays.asList("foo_tbl", "bar_tbl"));
+        assertThat(actual.size(), is(2));
+        assertThat(actual.get("foo_tbl"), is(Collections.singletonList(new DataNode("foo_ds.foo_tbl"))));
+        assertThat(actual.get("bar_tbl"), is(Collections.singletonList(new DataNode("foo_ds.bar_tbl"))));
+    }
+    
+    @Test
+    void assertBuildTableAndDataNodesMapWithNotExistedTable() {
+        RuleMetaData ruleMetaData = mock(RuleMetaData.class);
+        when(ruleMetaData.getAttributes(DataNodeRuleAttribute.class)).thenReturn(Collections.emptyList());
+        ShardingSphereDatabase database = mock(ShardingSphereDatabase.class);
+        when(database.getRuleMetaData()).thenReturn(ruleMetaData);
+        assertThrows(PipelineInvalidParameterException.class, () -> CDCDataNodeUtils.buildTableAndDataNodesMap(database, Collections.singleton("foo_tbl")));
     }
 }
