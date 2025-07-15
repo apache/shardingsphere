f3a7500a0c6f9fec20a68912cd0d49ad651ee209
timofey.saharovskiy <timofey.saharovskiy@red-soft.ru>
2025-07-15 21:38:42 +0300

    RS-242327. rename salt variable

--- proxy/frontend/type/firebird/src/main/java/org/apache/shardingsphere/proxy/frontend/firebird/authentication/authenticator/impl/FirebirdLegacyAuthenticator.java
+++ proxy/frontend/type/firebird/src/main/java/org/apache/shardingsphere/proxy/frontend/firebird/authentication/authenticator/impl/FirebirdLegacyAuthenticator.java
@@ -29,13 +29,13 @@ import org.firebirdsql.gds.ng.wire.auth.legacy.UnixCrypt;
  */
 public final class FirebirdLegacyAuthenticator implements FirebirdAuthenticator {
     
-    private static final String LEGACY_PASSWORD_SALT = "9z";
+    private static final String SALT = "9z";
     
     @Override
     public boolean authenticate(final ShardingSphereUser user, final Object[] authInfo) {
         // TODO update when version 6 of jaybird comes out
         String password = (String) authInfo[0];
-        String expectedPassword = UnixCrypt.crypt(user.getPassword(), LEGACY_PASSWORD_SALT).substring(2, 13);
+        String expectedPassword = UnixCrypt.crypt(user.getPassword(), SALT).substring(2, 13);
         return expectedPassword.equals(password);
     }
     
