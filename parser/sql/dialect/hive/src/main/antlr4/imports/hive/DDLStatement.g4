3e7a272d22166b7a31ad690b93e1cec0b3408320
Claire <claire040217@163.com>
2025-07-21 11:45:52 +0800

    fix:Support Hive CREATE TABLE statement parse (#36040)
    * support create table statement parse
    
    * update RELEASE-NOTES
    
    * update code
    
    * update code
    
    * update code

--- parser/sql/dialect/hive/src/main/antlr4/imports/hive/DDLStatement.g4
+++ parser/sql/dialect/hive/src/main/antlr4/imports/hive/DDLStatement.g4
@@ -17,7 +17,7 @@
 
 grammar DDLStatement;
 
-import BaseRule;
+import BaseRule, DMLStatement;
 
 createDatabase
     : CREATE REMOTE? (DATABASE | SCHEMA) ifNotExists? identifier (COMMENT string_)? (LOCATION string_)? (MANAGEDLOCATION string_)? (WITH DBPROPERTIES LP_ dbProperties RP_)?
@@ -33,6 +33,11 @@ alterDatabase
 
 use
     : USE (identifier | DEFAULT)
+    ;
+
+createTable
+    : createTableCommonClause createDefinitionClause? tableComment? partitionedBy? clusteredBy? skewedBy? rowFormat? storedAs? storageLocation? tblProperties? (AS select)?
+    | createTableCommonClause LIKE existingTableName storageLocation?
     ;
 
 dropTable
@@ -60,4 +65,214 @@ dbProperty
 
 ifExists
     : IF EXISTS
+    ;
+
+createTableCommonClause
+    : CREATE createTableSpecification? TABLE ifNotExists? tableNameWithDb
+    ;
+
+createTableSpecification
+    : TEMPORARY? EXTERNAL?
+    ;
+
+tableNameWithDb
+    : (identifier DOT_)? identifier
+    ;
+
+existingTableName
+    : (identifier DOT_)? identifier
+    ;
+
+createDefinitionClause
+    : LP_ columnDefinition (COMMA_ columnDefinition)* (COMMA_ tableConstraint)* RP_
+    ;
+
+columnDefinition
+    : columnName dataTypeClause columnConstraintSpecification* (COMMENT string_)?
+    ;
+
+columnName
+    : identifier
+    ;
+
+dataTypeClause
+    : primitiveType
+    | arrayType
+    | mapType
+    | structType
+    | unionType
+    ;
+
+primitiveType
+    : TINYINT
+    | SMALLINT
+    | INT
+    | BIGINT
+    | BOOLEAN
+    | FLOAT
+    | DOUBLE (PRECISION)?
+    | STRING
+    | BINARY
+    | TIMESTAMP
+    | DECIMAL (LP_ NUMBER_ (COMMA_ NUMBER_)? RP_)?
+    | DATE
+    | VARCHAR (LP_ NUMBER_ RP_)?
+    | CHAR (LP_ NUMBER_ RP_)?
+    ;
+
+arrayType
+    : ARRAY LT_ dataTypeClause GT_
+    ;
+
+mapType
+    : MAP LT_ primitiveType COMMA_ dataTypeClause GT_
+    ;
+
+structType
+    : STRUCT LT_ structField (COMMA_ structField)* GT_
+    ;
+
+structField
+    : identifier COLON_ dataTypeClause (COMMENT string_)?
+    ;
+
+unionType
+    : UNIONTYPE LT_ dataTypeClause (COMMA_ dataTypeClause)* GT_
+    ;
+
+columnConstraintSpecification
+    : primaryKeyConstraint
+    | uniqueConstraint
+    | notNullConstraint
+    | defaultConstraint
+    | checkConstraint
+    ;
+
+primaryKeyConstraint
+    : PRIMARY KEY constraintAttributes?
+    ;
+
+uniqueConstraint
+    : UNIQUE constraintAttributes?
+    ;
+
+notNullConstraint
+    : NOT NULL
+    ;
+
+defaultConstraint
+    : DEFAULT defaultValue
+    ;
+
+defaultValue
+    : literals
+    | CURRENT_USER LP_ RP_
+    | CURRENT_DATE LP_ RP_
+    | CURRENT_TIMESTAMP LP_ RP_
+    | NULL
+    ;
+
+checkConstraint
+    : CHECK LP_ expr RP_ constraintAttributes?
+    ;
+
+constraintAttributes
+    : (ENABLE | DISABLE) NOVALIDATE (RELY | NORELY)?
+    ;
+
+tableConstraint
+    : (CONSTRAINT constraintName)? tableConstraintOption
+    ;
+
+constraintName
+    : identifier
+    ;
+
+tableConstraintOption
+    : PRIMARY KEY LP_ columnNames RP_ constraintAttributes?
+    | UNIQUE LP_ columnNames RP_ constraintAttributes?
+    | CONSTRAINT identifier FOREIGN KEY LP_ columnNames RP_ REFERENCES tableName LP_ columnNames RP_ constraintAttributes?
+    | CONSTRAINT identifier CHECK LP_ expr RP_ constraintAttributes?
+    ;
+
+tableComment
+    : COMMENT string_
+    ;
+
+partitionedBy
+    : PARTITIONED BY LP_ partitionColumn (COMMA_ partitionColumn)* RP_
+    ;
+
+partitionColumn
+    : columnName dataTypeClause (COMMENT string_)?
+    ;
+
+clusteredBy
+    : CLUSTERED BY LP_ columnNames RP_ (SORTED BY LP_ sortedByItem (COMMA_ sortedByItem)* RP_)? INTO NUMBER_ BUCKETS
+    ;
+
+sortedByItem
+    : columnName (ASC | DESC)?
+    ;
+
+skewedBy
+    : SKEWED BY LP_ columnNames RP_ ON LP_ skewedValueList RP_ (STORED AS DIRECTORIES)?
+    ;
+
+skewedValueList
+    : skewedValue (COMMA_ skewedValue)*
+    ;
+
+skewedValue
+    : LP_ literals (COMMA_ literals)* RP_
+    | literals
+    ;
+
+rowFormat
+    : ROW FORMAT rowFormatType
+    ;
+
+rowFormatType
+    : DELIMITED rowFormatDelimited
+    | SERDE string_ (WITH SERDEPROPERTIES LP_ propertyList RP_)?
+    ;
+
+rowFormatDelimited
+    : (COLUMNS TERMINATED BY string_ (ESCAPED BY string_)?)?
+      (COLLECTION ITEMS TERMINATED BY string_)?
+      (MAP KEYS TERMINATED BY string_)?
+      (LINES TERMINATED BY string_)?
+      (NULL DEFINED AS string_)?
+    ;
+
+storedAs
+    : STORED AS fileFormat
+    | STORED BY string_ (WITH SERDEPROPERTIES LP_ propertyList RP_)?
+    ;
+
+fileFormat
+    : SEQUENCEFILE
+    | TEXTFILE
+    | RCFILE
+    | ORC
+    | PARQUET
+    | AVRO
+    | JSONFILE
+    | INPUTFORMAT string_ OUTPUTFORMAT string_
+    ;
+
+storageLocation
+    : LOCATION string_
+    ;
+
+tblProperties
+    : TBLPROPERTIES LP_ propertyList RP_
+    ;
+
+propertyList
+    : property (COMMA_ property)*
+    ;
+
+property
+    : string_ EQ_ string_
     ;
--- parser/sql/dialect/hive/src/main/java/org/apache/shardingsphere/sql/parser/hive/visitor/statement/type/HiveDDLStatementVisitor.java
+++ parser/sql/dialect/hive/src/main/java/org/apache/shardingsphere/sql/parser/hive/visitor/statement/type/HiveDDLStatementVisitor.java
@@ -17,19 +17,34 @@
 
 package org.apache.shardingsphere.sql.parser.hive.visitor.statement.type;
 
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
 import org.apache.shardingsphere.sql.parser.api.ASTNode;
 import org.apache.shardingsphere.sql.parser.api.visitor.statement.type.DDLStatementVisitor;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.AlterDatabaseContext;
 import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.CreateDatabaseContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.CreateTableContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.CreateDefinitionClauseContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.ColumnDefinitionContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.TableConstraintContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.DataTypeClauseContext;
 import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.DropDatabaseContext;
 import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.DropTableContext;
-import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.AlterDatabaseContext;
+import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.TableNameWithDbContext;
 import org.apache.shardingsphere.sql.parser.hive.visitor.statement.HiveStatementVisitor;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.CreateDatabaseStatement;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.DropDatabaseStatement;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.AlterDatabaseStatement;
-import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.DropTableStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.ddl.column.ColumnDefinitionSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.ddl.constraint.ConstraintDefinitionSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.ddl.CreateDefinitionSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.column.ColumnSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.DataTypeSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.table.SimpleTableSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.table.TableNameSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.AlterDatabaseStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.CreateDatabaseStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.CreateTableStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.database.DropDatabaseStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.DropTableStatement;
 import org.apache.shardingsphere.sql.parser.statement.core.value.collection.CollectionValue;
 import org.apache.shardingsphere.sql.parser.statement.core.value.identifier.IdentifierValue;
 
@@ -64,5 +79,87 @@ public final class HiveDDLStatementVisitor extends HiveStatementVisitor implemen
         result.setIfExists(null != ctx.ifExists());
         result.getTables().addAll(((CollectionValue<SimpleTableSegment>) visit(ctx.tableList())).getValue());
         return result;
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public ASTNode visitCreateTable(final CreateTableContext ctx) {
+        CreateTableStatement result = new CreateTableStatement(getDatabaseType());
+        result.setTable((SimpleTableSegment) visit(ctx.createTableCommonClause().tableNameWithDb()));
+        result.setIfNotExists(null != ctx.createTableCommonClause().ifNotExists());
+        if (null != ctx.createDefinitionClause()) {
+            CollectionValue<CreateDefinitionSegment> createDefinitions = (CollectionValue<CreateDefinitionSegment>) visit(ctx.createDefinitionClause());
+            for (CreateDefinitionSegment each : createDefinitions.getValue()) {
+                if (each instanceof ColumnDefinitionSegment) {
+                    result.getColumnDefinitions().add((ColumnDefinitionSegment) each);
+                } else if (each instanceof ConstraintDefinitionSegment) {
+                    result.getConstraintDefinitions().add((ConstraintDefinitionSegment) each);
+                }
+            }
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitCreateDefinitionClause(final CreateDefinitionClauseContext ctx) {
+        CollectionValue<CreateDefinitionSegment> result = new CollectionValue<>();
+        for (ColumnDefinitionContext each : ctx.columnDefinition()) {
+            result.getValue().add((ColumnDefinitionSegment) visit(each));
+        }
+        for (TableConstraintContext each : ctx.tableConstraint()) {
+            result.getValue().add((ConstraintDefinitionSegment) visit(each));
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitColumnDefinition(final ColumnDefinitionContext ctx) {
+        ColumnSegment column = new ColumnSegment(ctx.columnName().getStart().getStartIndex(), ctx.columnName().getStop().getStopIndex(),
+                new IdentifierValue(ctx.columnName().getText()));
+        DataTypeSegment dataType = (DataTypeSegment) visit(ctx.dataTypeClause());
+        return new ColumnDefinitionSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), column, dataType, false, false, getText(ctx));
+    }
+    
+    private String getText(final ParserRuleContext ctx) {
+        return ctx.start.getInputStream().getText(new Interval(ctx.start.getStartIndex(), ctx.stop.getStopIndex()));
+    }
+    
+    @Override
+    public ASTNode visitDataTypeClause(final DataTypeClauseContext ctx) {
+        DataTypeSegment result = new DataTypeSegment();
+        result.setStartIndex(ctx.getStart().getStartIndex());
+        result.setStopIndex(ctx.getStop().getStopIndex());
+        if (null != ctx.primitiveType()) {
+            result.setDataTypeName(ctx.primitiveType().getText());
+        } else if (null != ctx.arrayType()) {
+            result.setDataTypeName(ctx.arrayType().getText());
+        } else if (null != ctx.mapType()) {
+            result.setDataTypeName(ctx.mapType().getText());
+        } else if (null != ctx.structType()) {
+            result.setDataTypeName(ctx.structType().getText());
+        } else if (null != ctx.unionType()) {
+            result.setDataTypeName(ctx.unionType().getText());
+        }
+        return result;
+    }
+    
+    @Override
+    public ASTNode visitTableNameWithDb(final TableNameWithDbContext ctx) {
+        if (1 == ctx.identifier().size()) {
+            return new SimpleTableSegment(new TableNameSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(),
+                    new IdentifierValue(ctx.identifier(0).getText())));
+        } else {
+            SimpleTableSegment result = new SimpleTableSegment(new TableNameSegment(ctx.identifier(1).getStart().getStartIndex(),
+                    ctx.identifier(1).getStop().getStopIndex(), new IdentifierValue(ctx.identifier(1).getText())));
+            result.setOwner(new org.apache.shardingsphere.sql.parser.statement.core.segment.generic.OwnerSegment(
+                    ctx.identifier(0).getStart().getStartIndex(), ctx.identifier(0).getStop().getStopIndex(),
+                    new IdentifierValue(ctx.identifier(0).getText())));
+            return result;
+        }
+    }
+    
+    @Override
+    public ASTNode visitTableConstraint(final TableConstraintContext ctx) {
+        return new ConstraintDefinitionSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex());
     }
 }
