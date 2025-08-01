461c9958a10f529adfc0e6b1957f28452cbdb3ac
vasilevskyy <marvo.rate2016@yandex.ru>
2025-08-01 12:30:39 +0300

    RS-241306. complete implementation of FirebirdPacketCodecEngine

--- db-protocol/firebird/src/test/java/org/apache/shardingsphere/db/protocol/firebird/codec/FirebirdPacketCodecEngineTest.java
+++ db-protocol/firebird/src/test/java/org/apache/shardingsphere/db/protocol/firebird/codec/FirebirdPacketCodecEngineTest.java
@@ -17,13 +17,56 @@
 
 package org.apache.shardingsphere.db.protocol.firebird.codec;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.AttributeKey;
+import org.apache.shardingsphere.db.protocol.firebird.constant.FirebirdConstant;
+import org.apache.shardingsphere.db.protocol.firebird.constant.protocol.FirebirdProtocolVersion;
+import org.apache.shardingsphere.db.protocol.firebird.packet.command.FirebirdCommandPacketType;
+import org.apache.shardingsphere.db.protocol.firebird.payload.FirebirdPacketPayload;
+import org.apache.shardingsphere.db.protocol.packet.DatabasePacket;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class FirebirdPacketCodecEngineTest {
     
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private ChannelHandlerContext context;
+    
+    @BeforeEach
+    void setup() {
+        when(context.channel().attr(AttributeKey.<Charset>valueOf(Charset.class.getName())).get()).thenReturn(StandardCharsets.UTF_8);
+        when(context.channel().attr(FirebirdConstant.CONNECTION_PROTOCOL_VERSION).get()).thenReturn(FirebirdProtocolVersion.PROTOCOL_VERSION10);
+        when(context.alloc().compositeBuffer(anyInt())).thenAnswer(invocation -> new CompositeByteBuf(UnpooledByteBufAllocator.DEFAULT, false, invocation.getArgument(0)));
+    }
+
     @Test
     void assertIsValidHeader() {
         assertTrue(new FirebirdPacketCodecEngine().isValidHeader(50));
@@ -34,5 +77,59 @@ class FirebirdPacketCodecEngineTest {
         assertFalse(new FirebirdPacketCodecEngine().isValidHeader(3));
     }
     
-    // TODO Add unit tests for the remaining FirebirdPacketCodecEngine logic
+    @Test
+    void assertDecodeSingleAllocateStatement() {
+        ByteBuf byteBuf = Unpooled.buffer(8);
+        byteBuf.writeInt(FirebirdCommandPacketType.ALLOCATE_STATEMENT.getValue());
+        byteBuf.writeInt(1);
+        List<Object> out = new LinkedList<>();
+        new FirebirdPacketCodecEngine().decode(context, byteBuf, out);
+        assertThat(out.size(), is(1));
+        assertThat(((ByteBuf) out.get(0)).readableBytes(), is(8));
+    }
+    
+    @Test
+    void assertDecodeMultiplePackets() {
+        ByteBuf byteBuf = Unpooled.buffer(16);
+        byteBuf.writeInt(FirebirdCommandPacketType.ALLOCATE_STATEMENT.getValue());
+        byteBuf.writeInt(1);
+        byteBuf.writeInt(FirebirdCommandPacketType.ALLOCATE_STATEMENT.getValue());
+        byteBuf.writeInt(2);
+        List<Object> out = new LinkedList<>();
+        new FirebirdPacketCodecEngine().decode(context, byteBuf, out);
+        assertThat(out.size(), is(2));
+    }
+    
+    @Test
+    void assertDecodeWithPartialPacket() {
+        ByteBuf byteBuf = Unpooled.buffer(16);
+        byteBuf.writeInt(FirebirdCommandPacketType.TRANSACTION.getValue());
+        byteBuf.writeInt(1);
+        List<Object> out = new LinkedList<>();
+        new FirebirdPacketCodecEngine().decode(context, byteBuf, out);
+        assertThat(out.size(), is(1));
+    }
+    
+    @Test
+    void assertEncode() {
+        ByteBuf out = mock(ByteBuf.class);
+        DatabasePacket packet = mock(DatabasePacket.class);
+        new FirebirdPacketCodecEngine().encode(context, packet, out);
+        verify(packet).write(any(FirebirdPacketPayload.class));
+    }
+    
+    @Test
+    void assertEncodeOccursException() {
+        ByteBuf out = mock(ByteBuf.class);
+        DatabasePacket packet = mock(DatabasePacket.class);
+        doThrow(RuntimeException.class).when(packet).write(any(FirebirdPacketPayload.class));
+        new FirebirdPacketCodecEngine().encode(context, packet, out);
+        verify(out).resetWriterIndex();
+    }
+    
+    @Test
+    void assertCreatePacketPayload() {
+        ByteBuf byteBuf = Unpooled.buffer();
+        assertThat(new FirebirdPacketCodecEngine().createPacketPayload(byteBuf, StandardCharsets.UTF_8).getByteBuf(), is(byteBuf));
+    }
 }
