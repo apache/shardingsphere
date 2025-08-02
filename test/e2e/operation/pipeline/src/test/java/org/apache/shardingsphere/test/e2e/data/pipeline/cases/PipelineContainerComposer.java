7a151ca5fc025d3f31e75387c4a77eaf54c7fb7f
Liang Zhang <zhangliang@apache.org>
2025-08-02 18:59:37 +0800

    Remove useless method on PipelineContainerComposer (#36150)

--- test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/data/pipeline/cases/PipelineContainerComposer.java
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/data/pipeline/cases/PipelineContainerComposer.java
@@ -259,17 +259,6 @@ public final class PipelineContainerComposer implements AutoCloseable {
     }
     
     /**
-     * Add resource.
-     *
-     * @param distSQL dist SQL
-     * @throws SQLException SQL exception
-     */
-    // TODO Use registerStorageUnit instead, and remove the method, keep it now
-    public void addResource(final String distSQL) throws SQLException {
-        proxyExecuteWithLog(distSQL, 2);
-    }
-    
-    /**
      * Show storage units names.
      *
      * @return storage units names
@@ -400,6 +389,17 @@ public final class PipelineContainerComposer implements AutoCloseable {
      */
     public void proxyExecuteWithLog(final String sql, final int sleepSeconds) throws SQLException {
         log.info("proxy execute: {}", sql);
+        proxyExecute(sql, sleepSeconds);
+    }
+    
+    /**
+     * Proxy execute.
+     *
+     * @param sql SQL
+     * @param sleepSeconds sleep seconds
+     * @throws SQLException SQL exception
+     */
+    public void proxyExecute(final String sql, final int sleepSeconds) throws SQLException {
         List<String> sqlList = Splitter.on(";").trimResults().omitEmptyStrings().splitToList(sql);
         try (Connection connection = proxyDataSource.getConnection()) {
             for (String each : sqlList) {
