f67a433caf654c449c57fb07a6cc8b52d5c51824
zhangliang <zhangliang@apache.org>
2025-08-04 10:48:17 +0800

    Refactor SQLStatementCompilerEngine

--- kernel/sql-federation/compiler/src/main/java/org/apache/shardingsphere/sqlfederation/compiler/compiler/SQLStatementCompilerEngine.java
+++ kernel/sql-federation/compiler/src/main/java/org/apache/shardingsphere/sqlfederation/compiler/compiler/SQLStatementCompilerEngine.java
@@ -45,7 +45,7 @@ public final class SQLStatementCompilerEngine {
      */
     public SQLFederationExecutionPlan compile(final ExecutionPlanCacheKey cacheKey, final boolean useCache) {
         if (log.isDebugEnabled()) {
-            String cacheExists = null != executionPlanCache.get(cacheKey) ? "exists" : "not exists";
+            String cacheExists = null == executionPlanCache.get(cacheKey) ? "not exists" : "exists";
             log.debug("Execution plan cache {} for SQL: {}, useCache: {}.", cacheExists, cacheKey.getSql(), useCache);
         }
         return useCache ? executionPlanCache.get(cacheKey) : cacheKey.getSqlStatementCompiler().compile(cacheKey.getSqlStatement(), cacheKey.getSqlStatement().getDatabaseType().getType());
