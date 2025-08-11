6581808ceb14fe2e0a41406d9fb1c03428070661
Claire <claire040217@163.com>
2025-08-11 09:07:40 +0800

    support hive drop function statement

--- parser/sql/dialect/hive/src/main/java/org/apache/shardingsphere/sql/parser/hive/visitor/statement/type/HiveDALStatementVisitor.java
+++ parser/sql/dialect/hive/src/main/java/org/apache/shardingsphere/sql/parser/hive/visitor/statement/type/HiveDALStatementVisitor.java
@@ -20,62 +20,23 @@ package org.apache.shardingsphere.sql.parser.hive.visitor.statement.type;
 import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
 import org.apache.shardingsphere.sql.parser.api.ASTNode;
 import org.apache.shardingsphere.sql.parser.api.visitor.statement.type.DALStatementVisitor;
-import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.ShowConnectorsContext;
-import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.ShowDatabasesContext;
-import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.ShowLikeContext;
 import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.UseContext;
-import org.apache.shardingsphere.sql.parser.autogen.HiveStatementParser.ShowTablesContext;
 import org.apache.shardingsphere.sql.parser.hive.visitor.statement.HiveStatementVisitor;
-import org.apache.shardingsphere.sql.parser.statement.core.segment.dal.ShowFilterSegment;
-import org.apache.shardingsphere.sql.parser.statement.core.segment.dal.ShowLikeSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.value.identifier.IdentifierValue;
-import org.apache.shardingsphere.sql.parser.statement.core.value.literal.impl.StringLiteralValue;
-import org.apache.shardingsphere.sql.parser.statement.hive.dal.HiveUseStatement;
-import org.apache.shardingsphere.sql.parser.statement.hive.dal.show.HiveShowConnectorsStatement;
-import org.apache.shardingsphere.sql.parser.statement.hive.dal.show.HiveShowDatabasesStatement;
-import org.apache.shardingsphere.sql.parser.statement.hive.dal.show.HiveShowTablesStatement;
 import org.apache.shardingsphere.sql.parser.statement.mysql.dal.MySQLUseStatement;
 
 /**
  * DAL statement visitor for Hive.
  */
 public final class HiveDALStatementVisitor extends HiveStatementVisitor implements DALStatementVisitor {
-    
+
     public HiveDALStatementVisitor(final DatabaseType databaseType) {
         super(databaseType);
     }
-    
+
     @Override
     public ASTNode visitUse(final UseContext ctx) {
         String database = null == ctx.DEFAULT() ? new IdentifierValue(ctx.identifier().getText()).getValue() : "default";
         return new MySQLUseStatement(getDatabaseType(), database);
-    }
-    
-    @Override
-    public ASTNode visitShowDatabases(final ShowDatabasesContext ctx) {
-        ShowFilterSegment filter = null;
-        if (null != ctx.showLike()) {
-            filter = new ShowFilterSegment(ctx.showLike().getStart().getStartIndex(), ctx.showLike().getStop().getStopIndex());
-            filter.setLike((ShowLikeSegment) visit(ctx.showLike()));
-        }
-        HiveShowDatabasesStatement result = new HiveShowDatabasesStatement(getDatabaseType(), filter);
-        result.addParameterMarkers(getParameterMarkerSegments());
-        return result;
-    }
-    
-    @Override
-    public ASTNode visitShowLike(final ShowLikeContext ctx) {
-        StringLiteralValue literalValue = (StringLiteralValue) visit(ctx.stringLiterals());
-        return new ShowLikeSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), literalValue.getValue());
-    }
-    
-    @Override
-    public ASTNode visitShowConnectors(final ShowConnectorsContext ctx) {
-        return new HiveShowConnectorsStatement(getDatabaseType());
-    }
-    
-    @Override
-    public ASTNode visitShowTables(final ShowTablesContext ctx) {
-        return new HiveShowTablesStatement(getDatabaseType());
     }
 }
