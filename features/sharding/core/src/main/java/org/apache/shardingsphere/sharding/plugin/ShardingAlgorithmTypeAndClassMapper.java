0c4700414911ae992dfb626f053afb0600a1aa66
zhangliang <zhangliang@apache.org>
2025-08-16 17:11:36 +0800

    Move PluginTypeAndClassMapper into shardingsphere-infra-spi module

---
+++ features/sharding/core/src/main/java/org/apache/shardingsphere/sharding/plugin/ShardingAlgorithmTypeAndClassMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sharding.plugin;
+
+import org.apache.shardingsphere.infra.spi.type.typed.plugin.PluginTypeAndClassMapper;
+import org.apache.shardingsphere.sharding.spi.ShardingAlgorithm;
+
+/**
+ * Sharding algorithm type and class mapper.
+ */
+public final class ShardingAlgorithmTypeAndClassMapper implements PluginTypeAndClassMapper {
+    
+    @Override
+    public Class<ShardingAlgorithm> getPluginClass() {
+        return ShardingAlgorithm.class;
+    }
+    
+    @Override
+    public String getType() {
+        return "SHARDING_ALGORITHM";
+    }
+}
