a408bbb53c88fb58affa272fd12730de928a9a86
zhangliang <zhangliang@apache.org>
2025-08-24 13:07:54 +0800

    Add DialectDefaultQueryPropertiesProvider to decuple ConnectionProperties.defaultQueryProperties

--- database/connector/core/src/main/java/org/apache/shardingsphere/database/connector/core/jdbcurl/DialectDefaultQueryPropertiesProvider.java
+++ database/connector/core/src/main/java/org/apache/shardingsphere/database/connector/core/jdbcurl/DialectDefaultQueryPropertiesProvider.java
@@ -30,7 +30,7 @@ public interface DialectDefaultQueryPropertiesProvider extends DatabaseTypedSPI 
     
     /**
      * Get default query properties.
-     * 
+     *
      * @return default query properties
      */
     Properties getDefaultQueryProperties();
