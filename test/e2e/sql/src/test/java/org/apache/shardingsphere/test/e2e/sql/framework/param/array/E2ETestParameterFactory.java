1b4452e9a721e5ea9ea18ad0ba7fcf4242a68ccf
zhangliang <zhangliang@apache.org>
2025-09-20 14:32:31 +0800

    Refactor AdapterMode

--- test/e2e/sql/src/test/java/org/apache/shardingsphere/test/e2e/sql/framework/param/array/E2ETestParameterFactory.java
+++ test/e2e/sql/src/test/java/org/apache/shardingsphere/test/e2e/sql/framework/param/array/E2ETestParameterFactory.java
@@ -45,11 +45,11 @@ public final class E2ETestParameterFactory {
     public static Collection<AssertionTestParameter> getAssertionTestParameters(final SQLCommandType sqlCommandType) {
         Collection<AssertionTestParameter> result = new LinkedList<>();
         for (String each : ENV.getRunModes()) {
-            if (AdapterMode.STANDALONE.getValue().equalsIgnoreCase(each)) {
+            if (AdapterMode.STANDALONE.name().equalsIgnoreCase(each)) {
                 result.addAll(isDistSQLCommandType(sqlCommandType)
                         ? ProxyStandaloneTestParameterGenerator.getAssertionTestParameter(sqlCommandType)
                         : JdbcStandaloneTestParameterGenerator.getAssertionTestParameter(sqlCommandType));
-            } else if (AdapterMode.CLUSTER.getValue().equalsIgnoreCase(each)) {
+            } else if (AdapterMode.CLUSTER.name().equalsIgnoreCase(each)) {
                 result.addAll(ClusterTestParameterArrayGenerator.getAssertionTestParameter(sqlCommandType));
             }
         }
@@ -65,11 +65,11 @@ public final class E2ETestParameterFactory {
     public static Collection<E2ETestParameter> getCaseTestParameters(final SQLCommandType sqlCommandType) {
         Collection<E2ETestParameter> result = new LinkedList<>();
         for (String each : ENV.getRunModes()) {
-            if (AdapterMode.STANDALONE.getValue().equalsIgnoreCase(each)) {
+            if (AdapterMode.STANDALONE.name().equalsIgnoreCase(each)) {
                 result.addAll(isDistSQLCommandType(sqlCommandType)
                         ? ProxyStandaloneTestParameterGenerator.getCaseTestParameter(sqlCommandType)
                         : JdbcStandaloneTestParameterGenerator.getCaseTestParameter(sqlCommandType));
-            } else if (AdapterMode.CLUSTER.getValue().equalsIgnoreCase(each)) {
+            } else if (AdapterMode.CLUSTER.name().equalsIgnoreCase(each)) {
                 result.addAll(ClusterTestParameterArrayGenerator.getCaseTestParameter(sqlCommandType));
             }
         }
@@ -87,6 +87,6 @@ public final class E2ETestParameterFactory {
      */
     public static boolean containsTestParameter() {
         return E2ETestEnvironment.getInstance().getRunModes().stream()
-                .anyMatch(each -> AdapterMode.STANDALONE.getValue().equalsIgnoreCase(each) || AdapterMode.CLUSTER.getValue().equalsIgnoreCase(each));
+                .anyMatch(each -> AdapterMode.STANDALONE.name().equalsIgnoreCase(each) || AdapterMode.CLUSTER.name().equalsIgnoreCase(each));
     }
 }
