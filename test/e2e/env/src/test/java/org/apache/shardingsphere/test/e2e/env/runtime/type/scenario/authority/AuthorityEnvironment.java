4ab4d97c39bfba02c8c536a2ae4efae174d70edc
zhangliang <zhangliang@apache.org>
2025-10-01 17:33:58 +0800

    Refactor AuthorityEnvironment

--- test/e2e/env/src/test/java/org/apache/shardingsphere/test/e2e/env/runtime/type/scenario/authority/AuthorityEnvironment.java
+++ test/e2e/env/src/test/java/org/apache/shardingsphere/test/e2e/env/runtime/type/scenario/authority/AuthorityEnvironment.java
@@ -23,9 +23,10 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 /**
- * Authority root xml entry.
+ * Authority root XML entry.
  */
 @XmlRootElement(name = "authority")
 public final class AuthorityEnvironment {
@@ -34,30 +35,22 @@ public final class AuthorityEnvironment {
     private final Collection<AuthoritySQLSet> sqlSets = new LinkedList<>();
     
     /**
-     * Get init SQLs of this database type.
+     * Get init SQLs.
      *
      * @param databaseType database type
-     * @return init SQLs of this database type
+     * @return init SQLs
      */
     public Collection<String> getInitSQLs(final DatabaseType databaseType) {
-        Collection<String> result = new LinkedList<>();
-        for (AuthoritySQLSet each : sqlSets) {
-            result.addAll(each.getCreateUserSQLs(databaseType));
-        }
-        return result;
+        return sqlSets.stream().flatMap(each -> each.getCreateUserSQLs(databaseType).stream()).collect(Collectors.toList());
     }
     
     /**
-     * Get clean SQLs of this database type.
+     * Get clean SQLs.
      *
      * @param databaseType database type
-     * @return clean SQLs of this database type
+     * @return clean SQLs
      */
     public Collection<String> getCleanSQLs(final DatabaseType databaseType) {
-        Collection<String> result = new LinkedList<>();
-        for (AuthoritySQLSet each : sqlSets) {
-            result.addAll(each.getDropUserSQLs(databaseType));
-        }
-        return result;
+        return sqlSets.stream().flatMap(each -> each.getDropUserSQLs(databaseType).stream()).collect(Collectors.toList());
     }
 }
