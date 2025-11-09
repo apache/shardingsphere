bd8a6fbe1713aafbb59a777e1f4443536f90d735
Liang Zhang <zhangliang@apache.org>
2025-11-09 21:45:47 +0800

    Add more test cases on DatabaseRulesBuilder (#37057)
    * Add more test cases on DatabaseRulesBuilder
    
    * Add more test cases on DatabaseRulesBuilder
    
    * Add more test cases on DatabaseRulesBuilder

--- infra/common/src/test/java/org/apache/shardingsphere/infra/rule/builder/database/DatabaseRulesBuilderTest.java
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/rule/builder/database/DatabaseRulesBuilderTest.java
@@ -21,23 +21,31 @@ import org.apache.shardingsphere.infra.config.database.impl.DataSourceProvidedDa
 import org.apache.shardingsphere.infra.fixture.FixtureRule;
 import org.apache.shardingsphere.infra.fixture.FixtureRuleConfiguration;
 import org.apache.shardingsphere.infra.rule.ShardingSphereRule;
+import org.apache.shardingsphere.infra.rule.builder.fixture.FixtureDatabaseRuleConfiguration;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isA;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
 class DatabaseRulesBuilderTest {
     
     @Test
-    void assertBuild() {
-        Iterator<ShardingSphereRule> actual = DatabaseRulesBuilder.build("foo_db", mock(),
-                new DataSourceProvidedDatabaseConfiguration(Collections.emptyMap(), Collections.singleton(new FixtureRuleConfiguration())), mock(), mock()).iterator();
-        assertThat(actual.next(), isA(FixtureRule.class));
-        assertFalse(actual.hasNext());
+    void assertBuildMultipleRules() {
+        List<ShardingSphereRule> actual = new ArrayList<>(DatabaseRulesBuilder.build("foo_db", mock(),
+                new DataSourceProvidedDatabaseConfiguration(Collections.emptyMap(), Collections.singleton(new FixtureRuleConfiguration())), mock(), mock()));
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get(0), isA(FixtureRule.class));
+    }
+    
+    @Test
+    void assertBuildSingleRule() {
+        ShardingSphereRule actual = DatabaseRulesBuilder.build("foo_db", mock(), Collections.emptyList(), new FixtureDatabaseRuleConfiguration(), mock(), mock());
+        assertThat(actual, isA(FixtureRule.class));
     }
 }
