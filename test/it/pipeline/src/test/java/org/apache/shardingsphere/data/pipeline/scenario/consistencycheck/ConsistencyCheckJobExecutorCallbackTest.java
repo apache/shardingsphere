25d0be3cc70425f4c259e31f88a2599f919d7170
zhangliang <zhangliang@apache.org>
2025-11-14 18:57:32 +0800

    Add more test cases on ConsistencyCheckJobExecutorCallbackTest

--- test/it/pipeline/src/test/java/org/apache/shardingsphere/data/pipeline/scenario/consistencycheck/ConsistencyCheckJobExecutorCallbackTest.java
+++ test/it/pipeline/src/test/java/org/apache/shardingsphere/data/pipeline/scenario/consistencycheck/ConsistencyCheckJobExecutorCallbackTest.java
@@ -27,11 +27,15 @@ import org.apache.shardingsphere.data.pipeline.core.job.id.PipelineJobIdUtils;
 import org.apache.shardingsphere.data.pipeline.core.job.progress.ConsistencyCheckJobItemProgress;
 import org.apache.shardingsphere.data.pipeline.core.job.progress.yaml.config.YamlConsistencyCheckJobItemProgress;
 import org.apache.shardingsphere.data.pipeline.core.job.service.PipelineJobItemManager;
+import org.apache.shardingsphere.data.pipeline.core.task.runner.PipelineTasksRunner;
 import org.apache.shardingsphere.data.pipeline.scenario.consistencycheck.config.ConsistencyCheckJobConfiguration;
 import org.apache.shardingsphere.data.pipeline.scenario.consistencycheck.config.yaml.config.YamlConsistencyCheckJobConfiguration;
 import org.apache.shardingsphere.data.pipeline.scenario.consistencycheck.config.yaml.swapper.YamlConsistencyCheckJobConfigurationSwapper;
 import org.apache.shardingsphere.data.pipeline.scenario.consistencycheck.context.ConsistencyCheckJobItemContext;
+import org.apache.shardingsphere.data.pipeline.scenario.consistencycheck.task.ConsistencyCheckTasksRunner;
+import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.instance.metadata.InstanceType;
+import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.apache.shardingsphere.infra.util.yaml.YamlEngine;
 import org.apache.shardingsphere.test.it.data.pipeline.core.util.JobConfigurationBuilder;
 import org.apache.shardingsphere.test.it.data.pipeline.core.util.PipelineContextUtils;
@@ -41,12 +45,18 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
 
 class ConsistencyCheckJobExecutorCallbackTest {
+    
+    private final DatabaseType databaseType = TypedSPILoader.getService(DatabaseType.class, "Fixture");
     
     @BeforeAll
     static void beforeClass() {
@@ -72,6 +82,18 @@ class ConsistencyCheckJobExecutorCallbackTest {
         for (int i = 0; i < actualYamlTableCheckPositions.size(); i++) {
             assertThat(actualYamlTableCheckPositions.get(i), is(expectedYamlTableCheckRangePositions.get(i)));
         }
+    }
+    
+    @Test
+    void assertBuildTasksRunner() {
+        ConsistencyCheckJobConfiguration jobConfig = new ConsistencyCheckJobConfiguration("check_job", "parent_job", "DATA_MATCH", new Properties(), databaseType);
+        PipelineTasksRunner actual = new ConsistencyCheckJobExecutorCallback().buildTasksRunner(new ConsistencyCheckJobItemContext(jobConfig, 0, JobStatus.RUNNING, null));
+        assertThat(actual, instanceOf(ConsistencyCheckTasksRunner.class));
+    }
+    
+    @Test
+    void assertPrepare() {
+        assertDoesNotThrow(() -> new ConsistencyCheckJobExecutorCallback().prepare(mock()));
     }
     
     private YamlTableCheckRangePosition createYamlTableCheckRangePosition() {
