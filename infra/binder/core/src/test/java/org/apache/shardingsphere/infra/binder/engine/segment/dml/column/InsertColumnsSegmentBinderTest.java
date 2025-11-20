e4d9f907dce485756e2e662112c96a75bed4eae4
zhangliang <zhangliang@apache.org>
2025-11-21 00:08:54 +0800

    Add more test cases on SegmentBinder

---
+++ infra/binder/core/src/test/java/org/apache/shardingsphere/infra/binder/engine/segment/dml/column/InsertColumnsSegmentBinderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.infra.binder.engine.segment.dml.column;
+
+import com.cedarsoftware.util.CaseInsensitiveMap.CaseInsensitiveString;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import org.apache.shardingsphere.infra.binder.engine.segment.SegmentType;
+import org.apache.shardingsphere.infra.binder.engine.segment.dml.expression.type.ColumnSegmentBinder;
+import org.apache.shardingsphere.infra.binder.engine.segment.dml.from.context.TableSegmentBinderContext;
+import org.apache.shardingsphere.infra.binder.engine.statement.SQLStatementBinderContext;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.column.ColumnSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.column.InsertColumnsSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.value.identifier.IdentifierValue;
+import org.apache.shardingsphere.test.infra.framework.extension.mock.AutoMockExtension;
+import org.apache.shardingsphere.test.infra.framework.extension.mock.StaticMockSettings;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(AutoMockExtension.class)
+@StaticMockSettings(ColumnSegmentBinder.class)
+class InsertColumnsSegmentBinderTest {
+    
+    @Test
+    void assertBind() {
+        ColumnSegment firstColumn = new ColumnSegment(0, 1, new IdentifierValue("order_id"));
+        ColumnSegment secondColumn = new ColumnSegment(2, 3, new IdentifierValue("user_id"));
+        InsertColumnsSegment segment = new InsertColumnsSegment(5, 8, Arrays.asList(firstColumn, secondColumn));
+        ColumnSegment boundFirstColumn = new ColumnSegment(0, 1, new IdentifierValue("order_id"));
+        ColumnSegment boundSecondColumn = new ColumnSegment(2, 3, new IdentifierValue("user_id"));
+        SQLStatementBinderContext binderContext = mock(SQLStatementBinderContext.class);
+        Multimap<CaseInsensitiveString, TableSegmentBinderContext> tableBinderContexts = LinkedHashMultimap.create();
+        when(ColumnSegmentBinder.bind(eq(firstColumn), eq(SegmentType.INSERT_COLUMNS), eq(binderContext), eq(tableBinderContexts), any(Multimap.class))).thenReturn(boundFirstColumn);
+        when(ColumnSegmentBinder.bind(eq(secondColumn), eq(SegmentType.INSERT_COLUMNS), eq(binderContext), eq(tableBinderContexts), any(Multimap.class))).thenReturn(boundSecondColumn);
+        InsertColumnsSegment actual = InsertColumnsSegmentBinder.bind(segment, binderContext, tableBinderContexts);
+        assertThat(actual.getStartIndex(), is(5));
+        assertThat(actual.getStopIndex(), is(8));
+        Collection<ColumnSegment> actualColumns = actual.getColumns();
+        assertThat(actualColumns.size(), is(2));
+        assertThat(actualColumns.iterator().next(), is(boundFirstColumn));
+        assertThat(actualColumns.stream().skip(1).findFirst().orElse(null), is(boundSecondColumn));
+    }
+}
--- infra/binder/core/src/test/java/org/apache/shardingsphere/infra/binder/engine/segment/dml/expression/type/FunctionExpressionSegmentBinderTest.java
+++ infra/binder/core/src/test/java/org/apache/shardingsphere/infra/binder/engine/segment/dml/expression/type/FunctionExpressionSegmentBinderTest.java
@@ -17,28 +17,75 @@
 
 package org.apache.shardingsphere.infra.binder.engine.segment.dml.expression.type;
 
+import com.cedarsoftware.util.CaseInsensitiveMap.CaseInsensitiveString;
 import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 import org.apache.shardingsphere.infra.binder.engine.segment.SegmentType;
+import org.apache.shardingsphere.infra.binder.engine.segment.dml.expression.ExpressionSegmentBinder;
+import org.apache.shardingsphere.infra.binder.engine.segment.dml.from.context.TableSegmentBinderContext;
 import org.apache.shardingsphere.infra.binder.engine.statement.SQLStatementBinderContext;
-import org.apache.shardingsphere.infra.hint.HintValueContext;
-import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.expr.ExpressionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.expr.FunctionSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.OwnerSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.WindowItemSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.value.identifier.IdentifierValue;
+import org.apache.shardingsphere.test.infra.framework.extension.mock.AutoMockExtension;
+import org.apache.shardingsphere.test.infra.framework.extension.mock.StaticMockSettings;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Iterator;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(AutoMockExtension.class)
+@StaticMockSettings(ExpressionSegmentBinder.class)
 class FunctionExpressionSegmentBinderTest {
     
     @Test
-    void assertBindFunctionExpressionSegment() {
-        FunctionSegment functionSegment = new FunctionSegment(0, 0, "CONCAT", "('%','abc','%')");
-        SQLStatementBinderContext binderContext = new SQLStatementBinderContext(new ShardingSphereMetaData(), "foo_db", new HintValueContext(), mock());
-        FunctionSegment actual = FunctionExpressionSegmentBinder.bind(functionSegment, SegmentType.PROJECTION, binderContext, LinkedHashMultimap.create(), LinkedHashMultimap.create());
-        assertThat(actual.getStartIndex(), is(functionSegment.getStartIndex()));
-        assertThat(actual.getStopIndex(), is(functionSegment.getStopIndex()));
-        assertThat(actual.getFunctionName(), is("CONCAT"));
-        assertThat(actual.getText(), is("('%','abc','%')"));
+    void assertBindSkipsParameterForXmlElement() {
+        FunctionSegment functionSegment = new FunctionSegment(0, 5, "XMLELEMENT", "XMLELEMENT()");
+        OwnerSegment ownerSegment = new OwnerSegment(0, 0, new IdentifierValue("schema"));
+        functionSegment.setOwner(ownerSegment);
+        WindowItemSegment windowItemSegment = new WindowItemSegment(0, 0);
+        functionSegment.setWindow(windowItemSegment);
+        ExpressionSegment skippedParameter = mock(ExpressionSegment.class);
+        ExpressionSegment parameterToBind = mock(ExpressionSegment.class);
+        functionSegment.getParameters().add(skippedParameter);
+        functionSegment.getParameters().add(parameterToBind);
+        ExpressionSegment boundParameter = mock(ExpressionSegment.class);
+        SQLStatementBinderContext binderContext = mock(SQLStatementBinderContext.class);
+        Multimap<CaseInsensitiveString, TableSegmentBinderContext> tableBinderContexts = LinkedHashMultimap.create();
+        Multimap<CaseInsensitiveString, TableSegmentBinderContext> outerTableBinderContexts = LinkedHashMultimap.create();
+        when(ExpressionSegmentBinder.bind(parameterToBind, SegmentType.PROJECTION, binderContext, tableBinderContexts, outerTableBinderContexts)).thenReturn(boundParameter);
+        FunctionSegment actual = FunctionExpressionSegmentBinder.bind(functionSegment, SegmentType.PROJECTION, binderContext, tableBinderContexts, outerTableBinderContexts);
+        Iterator<ExpressionSegment> parameters = actual.getParameters().iterator();
+        assertThat(parameters.next(), is(skippedParameter));
+        assertThat(parameters.next(), is(boundParameter));
+        assertThat(actual.getOwner(), is(ownerSegment));
+        assertThat(actual.getWindow().orElse(null), is(windowItemSegment));
+    }
+    
+    @Test
+    void assertBindWithoutSkip() {
+        FunctionSegment functionSegment = new FunctionSegment(3, 8, "ABS", "ABS()");
+        ExpressionSegment firstParameter = mock(ExpressionSegment.class);
+        ExpressionSegment secondParameter = mock(ExpressionSegment.class);
+        functionSegment.getParameters().add(firstParameter);
+        functionSegment.getParameters().add(secondParameter);
+        ExpressionSegment boundFirstParameter = mock(ExpressionSegment.class);
+        ExpressionSegment boundSecondParameter = mock(ExpressionSegment.class);
+        SQLStatementBinderContext binderContext = mock(SQLStatementBinderContext.class);
+        Multimap<CaseInsensitiveString, TableSegmentBinderContext> tableBinderContexts = LinkedHashMultimap.create();
+        Multimap<CaseInsensitiveString, TableSegmentBinderContext> outerTableBinderContexts = LinkedHashMultimap.create();
+        when(ExpressionSegmentBinder.bind(firstParameter, SegmentType.PROJECTION, binderContext, tableBinderContexts, outerTableBinderContexts)).thenReturn(boundFirstParameter);
+        when(ExpressionSegmentBinder.bind(secondParameter, SegmentType.PROJECTION, binderContext, tableBinderContexts, outerTableBinderContexts)).thenReturn(boundSecondParameter);
+        FunctionSegment actual = FunctionExpressionSegmentBinder.bind(functionSegment, SegmentType.PROJECTION, binderContext, tableBinderContexts, outerTableBinderContexts);
+        Iterator<ExpressionSegment> parameters = actual.getParameters().iterator();
+        assertThat(parameters.next(), is(boundFirstParameter));
+        assertThat(parameters.next(), is(boundSecondParameter));
     }
 }
