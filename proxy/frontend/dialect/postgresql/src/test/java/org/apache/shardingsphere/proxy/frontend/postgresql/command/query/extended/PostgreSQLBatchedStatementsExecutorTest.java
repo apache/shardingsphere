df06c91a1c8580daceebddedd9cfdd89cf136690
zhangliang <zhangliang@apache.org>
2025-11-30 22:41:54 +0800

    Fix typo

--- proxy/frontend/dialect/postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/extended/PostgreSQLBatchedStatementsExecutorTest.java
+++ proxy/frontend/dialect/postgresql/src/test/java/org/apache/shardingsphere/proxy/frontend/postgresql/command/query/extended/PostgreSQLBatchedStatementsExecutorTest.java
@@ -162,6 +162,6 @@ class PostgreSQLBatchedStatementsExecutorTest {
     @SneakyThrows(ReflectiveOperationException.class)
     private void prepareExecutionUnitParameters(final PostgreSQLBatchedStatementsExecutor target, final List<List<Object>> parameterSets) {
         ((Map<ExecutionUnit, List<List<Object>>>) Plugins.getMemberAccessor().get(PostgreSQLBatchedStatementsExecutor.class.getDeclaredField("executionUnitParams"), target))
-                .replaceAll((key, vvalue) -> parameterSets);
+                .replaceAll((key, value) -> parameterSets);
     }
 }
