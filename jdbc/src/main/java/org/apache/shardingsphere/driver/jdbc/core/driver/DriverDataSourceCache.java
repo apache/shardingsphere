493aa8dc1534eed69a5bb7f7960198a2fa2440c9
Haoran Meng <menghaoranss@gmail.com>
2025-12-04 19:04:02 +0800

    Refactor DriverDataSourceCache

--- jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/driver/DriverDataSourceCache.java
+++ jdbc/src/main/java/org/apache/shardingsphere/driver/jdbc/core/driver/DriverDataSourceCache.java
@@ -34,9 +34,9 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Getter
 public final class DriverDataSourceCache {
-    
+
     private final Map<String, DataSource> dataSourceMap = new ConcurrentHashMap<>();
-    
+
     /**
      * Get data source.
      *
@@ -50,14 +50,14 @@ public final class DriverDataSourceCache {
         }
         return dataSourceMap.computeIfAbsent(url, driverUrl -> createDataSource(ShardingSphereURL.parse(driverUrl.substring(urlPrefix.length()))));
     }
-    
+
     @SuppressWarnings("unchecked")
     private <T extends Throwable> DataSource createDataSource(final ShardingSphereURL url) throws T {
         try {
             ShardingSphereURLLoadEngine urlLoadEngine = new ShardingSphereURLLoadEngine(url);
             Object loadedContent = urlLoadEngine.loadContent();
             return loadedContent instanceof ModeConfiguration
-                    ? ShardingSphereDataSourceFactory.createDataSource((ModeConfiguration) loadedContent)
+                    ? ShardingSphereDataSourceFactory.createDataSource(url.getQueryProps().getProperty("databaseName"), (ModeConfiguration) loadedContent)
                     : YamlShardingSphereDataSourceFactory.createDataSource((byte[]) loadedContent);
         } catch (final IOException ex) {
             throw (T) new SQLException(ex);
