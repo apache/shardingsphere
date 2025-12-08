0b7cfcc0b61c6e62d439bddf2277f2583cefe3b7
zhangliang <zhangliang@apache.org>
2025-12-08 21:35:32 +0800

    Add SQLFederationWrapperAdapterTest

---
+++ kernel/sql-federation/core/src/test/java/org/apache/shardingsphere/sqlfederation/resultset/SQLFederationWrapperAdapterTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sqlfederation.resultset;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SQLFederationWrapperAdapterTest {
+    
+    private final WrapperFixture fixture = new WrapperFixture();
+    
+    @Test
+    void assertUnwrapSuccess() throws SQLException {
+        assertThat(fixture, is(fixture.unwrap(Object.class)));
+    }
+    
+    @Test
+    void assertUnwrapFailure() {
+        SQLFeatureNotSupportedException ex = assertThrows(SQLFeatureNotSupportedException.class, () -> fixture.unwrap(Comparable.class));
+        assertThat(ex.getMessage(), is("`org.apache.shardingsphere.sqlfederation.resultset.SQLFederationWrapperAdapterTest$WrapperFixture` cannot be unwrapped as `java.lang.Comparable`"));
+    }
+    
+    @Test
+    void assertIsWrapperFor() {
+        assertTrue(fixture.isWrapperFor(Object.class));
+        assertFalse(fixture.isWrapperFor(Comparable.class));
+    }
+    
+    private static final class WrapperFixture extends SQLFederationWrapperAdapter {
+    }
+}
