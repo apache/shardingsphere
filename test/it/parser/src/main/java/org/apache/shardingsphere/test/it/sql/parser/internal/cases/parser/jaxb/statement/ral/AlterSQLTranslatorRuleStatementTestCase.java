8e959132ca6bb108c941bea90f2427b36ef2dfaf
zhangliang <zhangliang@apache.org>
2025-12-14 18:40:04 +0800

    Add document and test cases on DistSQL of manage SQL_TRANSLATOR rule

--- test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/internal/cases/parser/jaxb/statement/ral/AlterSQLTranslatorRuleStatementTestCase.java
+++ test/it/parser/src/main/java/org/apache/shardingsphere/test/it/sql/parser/internal/cases/parser/jaxb/statement/ral/AlterSQLTranslatorRuleStatementTestCase.java
@@ -18,6 +18,7 @@
 package org.apache.shardingsphere.test.it.sql.parser.internal.cases.parser.jaxb.statement.ral;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.shardingsphere.test.it.sql.parser.internal.cases.parser.jaxb.SQLParserTestCase;
 import org.apache.shardingsphere.test.it.sql.parser.internal.cases.parser.jaxb.segment.impl.distsql.ExpectedAlgorithm;
 
@@ -27,6 +28,7 @@ import javax.xml.bind.annotation.XmlElement;
  * Alter SQL translator rule statement test case.
  */
 @Getter
+@Setter
 public final class AlterSQLTranslatorRuleStatementTestCase extends SQLParserTestCase {
     
     @XmlElement(name = "provider")
