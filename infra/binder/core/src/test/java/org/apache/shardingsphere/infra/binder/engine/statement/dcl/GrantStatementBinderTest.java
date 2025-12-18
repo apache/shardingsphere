6c5b0e46efc8d0a30675cc82261e062f15eaa6f4
Jackie Yan <chakk6870@gmail.com>
2025-12-18 17:19:11 +0800

    fix: databaseType import in GrantStatementBinderTest

--- infra/binder/core/src/test/java/org/apache/shardingsphere/infra/binder/engine/statement/dcl/GrantStatementBinderTest.java
+++ infra/binder/core/src/test/java/org/apache/shardingsphere/infra/binder/engine/statement/dcl/GrantStatementBinderTest.java
@@ -17,8 +17,8 @@
 
 package org.apache.shardingsphere.infra.binder.engine.statement.dcl;
 
+import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.binder.engine.statement.SQLStatementBinderContext;
-import org.apache.shardingsphere.infra.database.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.hint.HintValueContext;
 import org.apache.shardingsphere.infra.metadata.ShardingSphereMetaData;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
