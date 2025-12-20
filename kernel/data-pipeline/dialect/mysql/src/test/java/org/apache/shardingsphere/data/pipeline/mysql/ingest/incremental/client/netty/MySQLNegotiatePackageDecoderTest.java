48c9fb9f9c3f16d6da5b268de89bf3ccdda921af
Liang Zhang <zhangliang@apache.org>
2025-12-20 13:11:17 +0800

    Add more test cases on MySQLNegotiatePackageDecoderTest (#37445)
    * Remove useless H2InventoryColumnValueReader
    
    * Add more test cases on MySQLNegotiatePackageDecoderTest

--- kernel/data-pipeline/dialect/mysql/src/test/java/org/apache/shardingsphere/data/pipeline/mysql/ingest/incremental/client/netty/MySQLNegotiatePackageDecoderTest.java
+++ kernel/data-pipeline/dialect/mysql/src/test/java/org/apache/shardingsphere/data/pipeline/mysql/ingest/incremental/client/netty/MySQLNegotiatePackageDecoderTest.java
@@ -21,10 +21,14 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
 import org.apache.shardingsphere.database.protocol.constant.CommonConstants;
+import org.apache.shardingsphere.database.protocol.mysql.packet.generic.MySQLErrPacket;
+import org.apache.shardingsphere.database.protocol.mysql.packet.generic.MySQLOKPacket;
 import org.apache.shardingsphere.database.protocol.mysql.packet.handshake.MySQLAuthMoreDataPacket;
 import org.apache.shardingsphere.database.protocol.mysql.packet.handshake.MySQLAuthSwitchRequestPacket;
 import org.apache.shardingsphere.database.protocol.mysql.packet.handshake.MySQLHandshakePacket;
+import org.apache.shardingsphere.infra.exception.generic.UnsupportedSQLOperationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,6 +45,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isA;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,29 +55,35 @@ class MySQLNegotiatePackageDecoderTest {
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private ChannelHandlerContext channelHandlerContext;
     
-    @Mock
-    private ByteBuf byteBuf;
-    
     @BeforeEach
     void setup() {
         when(channelHandlerContext.channel().attr(CommonConstants.CHARSET_ATTRIBUTE_KEY).get()).thenReturn(StandardCharsets.UTF_8);
     }
     
     @Test
-    void assertDecodeUnsupportedProtocolVersion() {
-        MySQLNegotiatePackageDecoder commandPacketDecoder = new MySQLNegotiatePackageDecoder();
-        assertThrows(IllegalArgumentException.class, () -> commandPacketDecoder.decode(channelHandlerContext, byteBuf, null));
+    void assertDecodeUnsupportedResponseHeader() throws ReflectiveOperationException {
+        MySQLNegotiatePackageDecoder negotiatePackageDecoder = new MySQLNegotiatePackageDecoder();
+        Plugins.getMemberAccessor().set(MySQLNegotiatePackageDecoder.class.getDeclaredField("handshakeReceived"), negotiatePackageDecoder, true);
+        List<Object> actual = new LinkedList<>();
+        assertThrows(UnsupportedSQLOperationException.class, () -> negotiatePackageDecoder.decode(channelHandlerContext, unsupportedHeaderPacket(), actual));
+        assertTrue(actual.isEmpty());
+    }
+    
+    private ByteBuf unsupportedHeaderPacket() {
+        ByteBuf result = Unpooled.buffer();
+        result.writeByte(0x10);
+        return result;
     }
     
     @Test
     void assertDecodeHandshakePacket() {
         MySQLNegotiatePackageDecoder commandPacketDecoder = new MySQLNegotiatePackageDecoder();
         List<Object> actual = new LinkedList<>();
-        commandPacketDecoder.decode(channelHandlerContext, mockHandshakePacket(), actual);
+        commandPacketDecoder.decode(channelHandlerContext, handshakePacket(), actual);
         assertHandshakePacket(actual);
     }
     
-    private ByteBuf mockHandshakePacket() {
+    private ByteBuf handshakePacket() {
         String handshakePacket = "0a352e372e32312d6c6f6700090000004a592a1f725a0d0900fff7210200ff8115000000000000000000001a437b30323a4d2b514b5870006d"
                 + "7973716c5f6e61746976655f70617373776f72640000000002000000";
         byte[] handshakePacketBytes = ByteBufUtil.decodeHexDump(handshakePacket);
@@ -104,10 +116,13 @@ class MySQLNegotiatePackageDecoderTest {
     }
     
     private ByteBuf authSwitchRequestPacket() {
-        when(byteBuf.readUnsignedByte()).thenReturn((short) MySQLAuthSwitchRequestPacket.HEADER);
-        when(byteBuf.getByte(0)).thenReturn((byte) MySQLAuthSwitchRequestPacket.HEADER);
-        when(byteBuf.bytesBefore((byte) 0)).thenReturn(20);
-        return byteBuf;
+        ByteBuf result = Unpooled.buffer();
+        result.writeByte(MySQLAuthSwitchRequestPacket.HEADER);
+        result.writeBytes("mysql_native_password".getBytes(StandardCharsets.UTF_8));
+        result.writeByte(0);
+        result.writeBytes("foo_auth_plugin_data_1234".getBytes(StandardCharsets.UTF_8));
+        result.writeByte(0);
+        return result;
     }
     
     @Test
@@ -120,9 +135,54 @@ class MySQLNegotiatePackageDecoderTest {
     }
     
     private ByteBuf authMoreDataPacket() {
-        when(byteBuf.readUnsignedByte()).thenReturn((short) MySQLAuthMoreDataPacket.HEADER);
-        when(byteBuf.getByte(0)).thenReturn((byte) MySQLAuthMoreDataPacket.HEADER);
-        return byteBuf;
+        ByteBuf result = Unpooled.buffer();
+        result.writeByte(MySQLAuthMoreDataPacket.HEADER);
+        result.writeBytes(new byte[]{1, 2, 3});
+        return result;
+    }
+    
+    @Test
+    void assertDecodeErrPacket() throws ReflectiveOperationException {
+        MySQLNegotiatePackageDecoder negotiatePackageDecoder = new MySQLNegotiatePackageDecoder();
+        Plugins.getMemberAccessor().set(MySQLNegotiatePackageDecoder.class.getDeclaredField("handshakeReceived"), negotiatePackageDecoder, true);
+        List<Object> actual = new LinkedList<>();
+        negotiatePackageDecoder.decode(channelHandlerContext, errPacket(), actual);
+        assertPacketByType(actual, MySQLErrPacket.class);
+        MySQLErrPacket actualPacket = (MySQLErrPacket) actual.get(0);
+        assertThat(actualPacket.getErrorCode(), is(1234));
+        assertThat(actualPacket.getSqlState(), is("HY000"));
+        assertThat(actualPacket.getErrorMessage(), is("foo_error"));
+    }
+    
+    private ByteBuf errPacket() {
+        ByteBuf result = Unpooled.buffer();
+        result.writeByte(MySQLErrPacket.HEADER);
+        result.writeShortLE(1234);
+        result.writeByte('#');
+        result.writeBytes("HY000".getBytes(StandardCharsets.UTF_8));
+        result.writeBytes("foo_error".getBytes(StandardCharsets.UTF_8));
+        return result;
+    }
+    
+    @Test
+    void assertDecodeOkPacketAndRemoveDecoder() throws ReflectiveOperationException {
+        MySQLNegotiatePackageDecoder negotiatePackageDecoder = new MySQLNegotiatePackageDecoder();
+        Plugins.getMemberAccessor().set(MySQLNegotiatePackageDecoder.class.getDeclaredField("handshakeReceived"), negotiatePackageDecoder, true);
+        List<Object> actual = new LinkedList<>();
+        negotiatePackageDecoder.decode(channelHandlerContext, okPacket(), actual);
+        assertPacketByType(actual, MySQLOKPacket.class);
+        ChannelPipeline actualPipeline = channelHandlerContext.channel().pipeline();
+        verify(actualPipeline).remove(negotiatePackageDecoder);
+    }
+    
+    private ByteBuf okPacket() {
+        ByteBuf result = Unpooled.buffer();
+        result.writeByte(MySQLOKPacket.HEADER);
+        result.writeByte(0);
+        result.writeByte(0);
+        result.writeShortLE(2);
+        result.writeShortLE(0);
+        return result;
     }
     
     private void assertPacketByType(final List<Object> actual, final Class<?> clazz) {
