bcdbe2d4f52aa5443923808103103b1786a89d90
Hongsheng Zhong <zhonghongsheng@apache.org>
2025-12-26 21:05:47 +0800

    Revert

--- kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/preparer/inventory/calculator/position/estimated/InventoryPositionEstimatedCalculator.java
+++ kernel/data-pipeline/core/src/main/java/org/apache/shardingsphere/data/pipeline/core/preparer/inventory/calculator/position/estimated/InventoryPositionEstimatedCalculator.java
@@ -82,7 +82,7 @@ public final class InventoryPositionEstimatedCalculator {
         }
         List<IngestPosition> result = new LinkedList<>();
         long splitCount = tableRecordsCount / shardingSize + (tableRecordsCount % shardingSize > 0 ? 1 : 0);
-        long stepSize = BigInteger.valueOf(maximum).subtract(BigInteger.valueOf(minimum)).add(BigInteger.ONE).divide(BigInteger.valueOf(splitCount)).longValue();
+        long stepSize = BigInteger.valueOf(maximum).subtract(BigInteger.valueOf(minimum)).divide(BigInteger.valueOf(splitCount)).longValue();
         IntegerRangeSplittingIterator rangeIterator = new IntegerRangeSplittingIterator(minimum, maximum, stepSize);
         while (rangeIterator.hasNext()) {
             Range<Long> range = rangeIterator.next();
