30e5ff9d5793ff7266118c525fd7476c77588567
Hongsheng Zhong <zhonghongsheng@apache.org>
2026-01-26 21:07:33 +0800

    Add OrderItemDAO and SQL builders

---
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/dao/orderitem/OrderItemDAO.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.test.e2e.operation.pipeline.dao.orderitem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.database.connector.core.spi.DatabaseTypedSPILoader;
+import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
+import org.apache.shardingsphere.test.e2e.operation.pipeline.dao.orderitem.sqlbuilder.OrderItemSQLBuilder;
+import org.apache.shardingsphere.test.e2e.operation.pipeline.util.DataSourceExecuteUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+@Slf4j
+public final class OrderItemDAO {
+    
+    private final DataSource dataSource;
+    
+    private final OrderItemSQLBuilder sqlBuilder;
+    
+    public OrderItemDAO(final DataSource dataSource, final DatabaseType databaseType) {
+        this.dataSource = dataSource;
+        sqlBuilder = DatabaseTypedSPILoader.getService(OrderItemSQLBuilder.class, databaseType);
+    }
+    
+    /**
+     * Create order_item table.
+     *
+     * @throws SQLException SQL exception
+     */
+    public void createTable() throws SQLException {
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.execute(sqlBuilder.buildCreateTableSQL());
+        }
+    }
+    
+    /**
+     * Batch insert order items.
+     *
+     * @param params insert parameters
+     * @throws SQLException SQL exception
+     */
+    // TODO Generate params internally
+    public void batchInsert(final List<Object[]> params) throws SQLException {
+        DataSourceExecuteUtils.execute(dataSource, sqlBuilder.buildPreparedInsertSQL(), params);
+    }
+}
---
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/dao/orderitem/sqlbuilder/MySQLOrderItemSQLBuilder.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.test.e2e.operation.pipeline.dao.orderitem.sqlbuilder;
+
+public final class MySQLOrderItemSQLBuilder implements OrderItemSQLBuilder {
+    
+    @Override
+    public String buildCreateTableSQL() {
+        return """
+                CREATE TABLE t_order_item (
+                item_id bigint NOT NULL,
+                order_id bigint NOT NULL,
+                user_id int NOT NULL,
+                status varchar(50) DEFAULT NULL,
+                PRIMARY KEY (item_id)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+                """;
+    }
+    
+    @Override
+    public String buildPreparedInsertSQL() {
+        return "INSERT INTO t_order_item (item_id, order_id, user_id, status) VALUES (?, ?, ?, ?)";
+    }
+    
+    @Override
+    public String getDatabaseType() {
+        return "MySQL";
+    }
+}
