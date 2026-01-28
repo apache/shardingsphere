500ccc121ece638cd0235c6e51f28081cde92554
zhangliang <zhangliang@apache.org>
2026-01-28 21:09:56 +0800

    Add more test cases on ConnectionThreadExecutorGroupTest

--- proxy/frontend/core/src/test/java/org/apache/shardingsphere/proxy/frontend/executor/ConnectionThreadExecutorGroupTest.java
+++ proxy/frontend/core/src/test/java/org/apache/shardingsphere/proxy/frontend/executor/ConnectionThreadExecutorGroupTest.java
@@ -19,8 +19,13 @@ package org.apache.shardingsphere.proxy.frontend.executor;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ConnectionThreadExecutorGroupTest {
     
@@ -33,10 +38,45 @@ class ConnectionThreadExecutorGroupTest {
     }
     
     @Test
-    void assertUnregister() {
+    void assertUnregisterWithRegisteredConnectionId() {
         int connectionId = 2;
         ConnectionThreadExecutorGroup.getInstance().register(connectionId);
         ConnectionThreadExecutorGroup.getInstance().unregisterAndAwaitTermination(connectionId);
+        assertNull(ConnectionThreadExecutorGroup.getInstance().get(connectionId));
+    }
+    
+    @Test
+    void assertUnregisterWithoutRegisteredConnectionId() {
+        int connectionId = 3;
+        ConnectionThreadExecutorGroup.getInstance().unregisterAndAwaitTermination(connectionId);
+        assertNull(ConnectionThreadExecutorGroup.getInstance().get(connectionId));
+    }
+    
+    @Test
+    void assertUnregisterWithInterruptedAwait() throws InterruptedException {
+        int connectionId = 4;
+        CountDownLatch taskStartedLatch = new CountDownLatch(1);
+        CountDownLatch blockTaskLatch = new CountDownLatch(1);
+        ConnectionThreadExecutorGroup.getInstance().register(connectionId);
+        ExecutorService executorService = ConnectionThreadExecutorGroup.getInstance().get(connectionId);
+        executorService.execute(() -> {
+            taskStartedLatch.countDown();
+            try {
+                blockTaskLatch.await();
+            } catch (final InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        taskStartedLatch.await();
+        Thread.currentThread().interrupt();
+        try {
+            ConnectionThreadExecutorGroup.getInstance().unregisterAndAwaitTermination(connectionId);
+            assertTrue(Thread.currentThread().isInterrupted());
+        } finally {
+            Thread.interrupted();
+            blockTaskLatch.countDown();
+            executorService.awaitTermination(1L, TimeUnit.SECONDS);
+        }
         assertNull(ConnectionThreadExecutorGroup.getInstance().get(connectionId));
     }
 }
