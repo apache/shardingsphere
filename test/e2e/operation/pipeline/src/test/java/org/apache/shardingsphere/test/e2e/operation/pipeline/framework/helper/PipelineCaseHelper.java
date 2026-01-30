7cf6eed17ca4395e9da5d5e832e110a5955289b4
Hongsheng Zhong <zhonghongsheng@apache.org>
2026-01-30 17:06:02 +0800

    Refactor PipelineCaseHelper.generateOrderInsertData into IntPkLargeOrderSQLBuilder impls

--- test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/framework/helper/PipelineCaseHelper.java
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/framework/helper/PipelineCaseHelper.java
@@ -21,29 +21,14 @@ import com.google.common.base.Strings;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
-import org.apache.shardingsphere.database.connector.mariadb.type.MariaDBDatabaseType;
-import org.apache.shardingsphere.database.connector.mysql.type.MySQLDatabaseType;
-import org.apache.shardingsphere.database.connector.opengauss.type.OpenGaussDatabaseType;
-import org.apache.shardingsphere.database.connector.postgresql.type.PostgreSQLDatabaseType;
 import org.apache.shardingsphere.infra.algorithm.core.context.AlgorithmSQLContext;
 import org.apache.shardingsphere.infra.algorithm.keygen.spi.KeyGenerateAlgorithm;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.Year;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.mockito.Mockito.mock;
@@ -53,63 +38,23 @@ import static org.mockito.Mockito.mock;
 public final class PipelineCaseHelper {
     
     /**
-     * Generate order insert data.
+     * Generate a pseudorandom integer in the specified range.
      *
-     * @param databaseType database type
-     * @param keyGenerateAlgorithm key generate algorithm
-     * @param recordCount record count
-     * @return order insert data
-     * @throws UnsupportedOperationException Unsupported operation exception
+     * @param min lower bound (inclusive)
+     * @param max upper bound (exclusive)
+     * @return a pseudorandom int between {@code min} (inclusive) and {@code max} (exclusive)
      */
-    // TODO Refactor to use SPI
-    public static List<Object[]> generateOrderInsertData(final DatabaseType databaseType, final KeyGenerateAlgorithm keyGenerateAlgorithm, final int recordCount) {
-        List<Object[]> result = new ArrayList<>(recordCount);
-        String emojiText = "☠️x☺️x✋x☹️";
-        if (databaseType instanceof MySQLDatabaseType || databaseType instanceof MariaDBDatabaseType) {
-            for (int i = 0; i < recordCount; i++) {
-                int randomInt = generateInt(-100, 100);
-                Object orderId = keyGenerateAlgorithm.generateKeys(mock(AlgorithmSQLContext.class), 1).iterator().next();
-                int randomUnsignedInt = generateInt(0, 100);
-                LocalDateTime now = LocalDateTime.now();
-                Object[] addObjs = {orderId, generateInt(0, 100), generateString(6), randomInt, randomInt, randomInt,
-                        randomUnsignedInt, randomUnsignedInt, randomUnsignedInt, randomUnsignedInt, generateFloat(), generateDouble(),
-                        BigDecimal.valueOf(generateDouble()), now, now, now.toLocalDate(), now.toLocalTime(), Year.now().getValue(), "1", "t", "e", "s", "t", generateString(2),
-                        emojiText, generateString(1), "1", "2", generateJsonString(32, false)};
-                result.add(addObjs);
-            }
-            return result;
-        }
-        if (databaseType instanceof PostgreSQLDatabaseType) {
-            for (int i = 0; i < recordCount; i++) {
-                Object orderId = keyGenerateAlgorithm.generateKeys(mock(AlgorithmSQLContext.class), 1).iterator().next();
-                result.add(new Object[]{orderId, generateInt(0, 100), generateString(6), generateInt(-128, 127),
-                        BigDecimal.valueOf(generateDouble()), true, "bytea".getBytes(), generateString(2), generateString(2), generateFloat(), generateDouble(),
-                        generateJsonString(8, false), generateJsonString(12, true), emojiText, LocalDate.now(),
-                        LocalTime.now(), Timestamp.valueOf(LocalDateTime.now()), OffsetDateTime.now()});
-            }
-            return result;
-        }
-        if (databaseType instanceof OpenGaussDatabaseType) {
-            for (int i = 0; i < recordCount; i++) {
-                Object orderId = keyGenerateAlgorithm.generateKeys(mock(AlgorithmSQLContext.class), 1).iterator().next();
-                byte[] bytesValue = {Byte.MIN_VALUE, -1, 0, 1, Byte.MAX_VALUE};
-                result.add(new Object[]{orderId, generateInt(0, 1000), "'status'" + i, generateInt(-1000, 9999), generateInt(0, 100), generateFloat(), generateDouble(),
-                        BigDecimal.valueOf(generateDouble()), false, generateString(6), "texts", bytesValue, bytesValue, LocalDate.now(), LocalTime.now(), "2001-10-01",
-                        Timestamp.valueOf(LocalDateTime.now()), OffsetDateTime.now(), "0 years 0 mons 1 days 2 hours 3 mins 4 secs", "{1, 2, 3}", generateJsonString(8, false),
-                        generateJsonString(8, true), UUID.randomUUID().toString(), DigestUtils.md5Hex(orderId.toString()), "'rat' 'sat'", "tsquery", "0000", "[1,1000)", "[2020-01-02,2021-01-01)",
-                        "[2020-01-01 00:00:00,2021-01-01 00:00:00)", "1 years 1 mons 10 days -06:00:00", "2000-01-02 00:00:00+00", "(1.0,1.0)", "[(0.0,0.0),(2.0,2.0)]", "(3.0,3.0),(1.0,1.0)",
-                        "<(5.0,5.0),5.0>", "1111", "192.168.0.0/16", "192.168.1.1", "08:00:2b:01:02:03", "\\x484c4c00000000002b05000000000000000000000000000000000000", 999});
-            }
-            return result;
-        }
-        throw new UnsupportedOperationException(String.format("Not support generate %s insert data", databaseType.getType()));
-    }
-    
-    private static int generateInt(final int min, final int max) {
+    public static int generateInt(final int min, final int max) {
         return ThreadLocalRandom.current().nextInt(min, max);
     }
     
-    private static String generateString(final int strLength) {
+    /**
+     * Generate a random alphanumeric string of the given length.
+     *
+     * @param strLength desired string length
+     * @return random alphanumeric string
+     */
+    public static String generateString(final int strLength) {
         return RandomStringUtils.randomAlphanumeric(strLength);
     }
     
