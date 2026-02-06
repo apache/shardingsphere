4a56ea815620ef96e51e403f40f9088a870bb98f
Claire <claire040217@163.com>
2026-02-06 16:05:38 +0800

    support new function

---
+++ parser/sql/statement/core/src/main/java/org/apache/shardingsphere/sql/parser/statement/core/segment/dml/pagination/limit/ExpressionLimitValueSegment.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.sql.parser.statement.core.segment.dml.pagination.limit;
+
+import lombok.Getter;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.expr.ExpressionSegment;
+import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.pagination.ExpressionPaginationValueSegment;
+
+/**
+ * Limit value segment for expression.
+ */
+@Getter
+public final class ExpressionLimitValueSegment extends LimitValueSegment implements ExpressionPaginationValueSegment {
+    
+    private final ExpressionSegment expression;
+    
+    public ExpressionLimitValueSegment(final int startIndex, final int stopIndex, final ExpressionSegment expression) {
+        super(startIndex, stopIndex);
+        this.expression = expression;
+    }
+}
