97bf89a91448619b49336ed9cefb9a3f8ea37286
zhangliang <zhangliang@apache.org>
2026-02-16 11:14:18 +0800

    Use isA instead of instanceOf

--- infra/common/src/test/java/org/apache/shardingsphere/infra/rule/attribute/RuleAttributesTest.java
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/rule/attribute/RuleAttributesTest.java
@@ -22,8 +22,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -50,7 +50,7 @@ class RuleAttributesTest {
         RuleAttribute attribute = mock(RuleAttribute.class);
         RuleAttributes attributes = new RuleAttributes(attribute);
         RuleAttribute actual = attributes.getAttribute(RuleAttribute.class);
-        assertThat(actual, instanceOf(RuleAttribute.class));
+        assertThat(actual, isA(RuleAttribute.class));
     }
     
     @Test
