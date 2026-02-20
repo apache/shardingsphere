b3d8ab1d7fe5f3bf08e20cb943d53590c5debf18
zhangliang <zhangliang@apache.org>
2026-02-20 12:59:08 +0800

    Add more test cases on ResultSetMapperTest

--- database/connector/core/src/test/java/org/apache/shardingsphere/database/connector/core/resultset/ResultSetMapperTest.java
+++ database/connector/core/src/test/java/org/apache/shardingsphere/database/connector/core/resultset/ResultSetMapperTest.java
@@ -45,8 +45,8 @@ import java.sql.Types;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -75,38 +75,6 @@ class ResultSetMapperTest {
         when(metaData.getColumnType(1)).thenReturn(Types.BOOLEAN);
         when(resultSet.getBoolean(1)).thenReturn(true);
         assertTrue((Boolean) new ResultSetMapper(databaseType).load(resultSet, 1));
-    }
-    
-    @Test
-    void assertLoadTinyIntValueWithDialectResultSetMapper() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.TINYINT);
-        DialectResultSetMapper dialectResultSetMapper = mock(DialectResultSetMapper.class);
-        when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
-        when(dialectResultSetMapper.getSmallintValue(resultSet, 1)).thenReturn(1);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1));
-    }
-    
-    @Test
-    void assertLoadTinyIntValueWithoutDialectResultSetMapper() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.TINYINT);
-        when(resultSet.getInt(1)).thenReturn(1);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1));
-    }
-    
-    @Test
-    void assertLoadSmallIntValueWithDialectResultSetMapper() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.SMALLINT);
-        DialectResultSetMapper dialectResultSetMapper = mock(DialectResultSetMapper.class);
-        when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
-        when(dialectResultSetMapper.getSmallintValue(resultSet, 1)).thenReturn(1);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1));
-    }
-    
-    @Test
-    void assertLoadSmallIntValueWithoutDialectResultSetMapper() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.SMALLINT);
-        when(resultSet.getInt(1)).thenReturn(1);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1));
     }
     
     @Test
@@ -147,42 +115,6 @@ class ResultSetMapperTest {
     }
     
     @Test
-    void assertLoadNumericValue() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.NUMERIC);
-        when(resultSet.getBigDecimal(1)).thenReturn(new BigDecimal("1"));
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(new BigDecimal("1")));
-    }
-    
-    @Test
-    void assertLoadDecimalValue() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.DECIMAL);
-        when(resultSet.getBigDecimal(1)).thenReturn(new BigDecimal("1"));
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(new BigDecimal("1")));
-    }
-    
-    @Test
-    void assertLoadFloatValue() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.FLOAT);
-        when(resultSet.getDouble(1)).thenReturn(1D);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1D));
-    }
-    
-    @Test
-    void assertLoadDoubleValue() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.DOUBLE);
-        when(resultSet.getDouble(1)).thenReturn(1D);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1D));
-    }
-    
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("stringTypeArguments")
-    void assertLoadStringValue(final String name, final int columnType) throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(columnType);
-        when(resultSet.getString(1)).thenReturn("foo");
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is("foo"));
-    }
-    
-    @Test
     void assertLoadDateValueWithoutDialectResultSetMapper() throws SQLException {
         when(metaData.getColumnType(1)).thenReturn(Types.DATE);
         when(resultSet.getDate(1)).thenReturn(new Date(0L));
@@ -196,6 +128,16 @@ class ResultSetMapperTest {
         when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
         when(dialectResultSetMapper.getDateValue(resultSet, 1)).thenReturn(new Date(0L));
         assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(new Date(0L)));
+    }
+    
+    @Test
+    void assertLoadObjectValueWithDialectResultSetMapper() throws SQLException {
+        when(metaData.getColumnType(1)).thenReturn(Types.JAVA_OBJECT);
+        DialectResultSetMapper dialectResultSetMapper = mock(DialectResultSetMapper.class);
+        Object object = new Object();
+        when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
+        when(dialectResultSetMapper.getDefaultValue(resultSet, 1, Types.JAVA_OBJECT)).thenReturn(object);
+        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(object));
     }
     
     @Test
@@ -228,15 +170,6 @@ class ResultSetMapperTest {
         assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(clob));
     }
     
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("binaryTypeArguments")
-    void assertLoadBinaryValue(final String name, final int columnType) throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(columnType);
-        byte[] bytes = new byte[0];
-        when(resultSet.getBytes(1)).thenReturn(bytes);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(bytes));
-    }
-    
     @Test
     void assertLoadArrayValue() throws SQLException {
         when(metaData.getColumnType(1)).thenReturn(Types.ARRAY);
@@ -246,21 +179,71 @@ class ResultSetMapperTest {
     }
     
     @Test
-    void assertLoadObjectValueWithDialectResultSetMapper() throws SQLException {
-        when(metaData.getColumnType(1)).thenReturn(Types.JAVA_OBJECT);
-        DialectResultSetMapper dialectResultSetMapper = mock(DialectResultSetMapper.class);
-        Object object = new Object();
-        when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
-        when(dialectResultSetMapper.getDefaultValue(resultSet, 1, Types.JAVA_OBJECT)).thenReturn(object);
-        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(object));
-    }
-    
-    @Test
     void assertLoadObjectValue() throws SQLException {
         when(metaData.getColumnType(1)).thenReturn(Types.JAVA_OBJECT);
         Object object = new Object();
         when(resultSet.getObject(1)).thenReturn(object);
         assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(object));
+    }
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("smallintTypeArguments")
+    void assertLoadSmallintValue(final String name, final int columnType, final boolean hasDialectResultSetMapper) throws SQLException {
+        when(metaData.getColumnType(1)).thenReturn(columnType);
+        if (hasDialectResultSetMapper) {
+            DialectResultSetMapper dialectResultSetMapper = mock(DialectResultSetMapper.class);
+            when(DatabaseTypedSPILoader.findService(DialectResultSetMapper.class, databaseType)).thenReturn(Optional.of(dialectResultSetMapper));
+            when(dialectResultSetMapper.getSmallintValue(resultSet, 1)).thenReturn(1);
+        } else {
+            when(resultSet.getInt(1)).thenReturn(1);
+        }
+        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(1));
+    }
+    
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("numericAndFloatingTypeArguments")
+    void assertLoadNumericAndFloatingValue(final String name, final int columnType, final Object expected) throws SQLException {
+        when(metaData.getColumnType(1)).thenReturn(columnType);
+        if (Types.FLOAT == columnType || Types.DOUBLE == columnType) {
+            when(resultSet.getDouble(1)).thenReturn((Double) expected);
+        } else {
+            when(resultSet.getBigDecimal(1)).thenReturn((BigDecimal) expected);
+        }
+        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(expected));
+    }
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("stringTypeArguments")
+    void assertLoadStringValue(final String name, final int columnType) throws SQLException {
+        when(metaData.getColumnType(1)).thenReturn(columnType);
+        when(resultSet.getString(1)).thenReturn("foo");
+        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is("foo"));
+    }
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("binaryTypeArguments")
+    void assertLoadBinaryValue(final String name, final int columnType) throws SQLException {
+        when(metaData.getColumnType(1)).thenReturn(columnType);
+        byte[] bytes = new byte[0];
+        when(resultSet.getBytes(1)).thenReturn(bytes);
+        assertThat(new ResultSetMapper(databaseType).load(resultSet, 1), is(bytes));
+    }
+    
+    private static Stream<Arguments> smallintTypeArguments() {
+        return Stream.of(
+                Arguments.of("tinyint with dialect result set mapper", Types.TINYINT, true),
+                Arguments.of("tinyint without dialect result set mapper", Types.TINYINT, false),
+                Arguments.of("smallint with dialect result set mapper", Types.SMALLINT, true),
+                Arguments.of("smallint without dialect result set mapper", Types.SMALLINT, false));
+    }
+    
+    private static Stream<Arguments> numericAndFloatingTypeArguments() {
+        return Stream.of(
+                Arguments.of("numeric", Types.NUMERIC, new BigDecimal("1")),
+                Arguments.of("decimal", Types.DECIMAL, new BigDecimal("1")),
+                Arguments.of("float", Types.FLOAT, 1D),
+                Arguments.of("double", Types.DOUBLE, 1D));
     }
     
     private static Stream<Arguments> stringTypeArguments() {
