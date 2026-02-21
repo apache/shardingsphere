6f0ab0175db95cc7207ae2af3176ab90823c6424
zhangliang <zhangliang@apache.org>
2026-02-21 20:27:47 +0800

    Add more test cases on FirebirdNonFixedLengthColumnSizeLoaderTest

--- database/connector/dialect/firebird/src/test/java/org/apache/shardingsphere/database/connector/firebird/metadata/data/loader/FirebirdNonFixedLengthColumnSizeLoaderTest.java
+++ database/connector/dialect/firebird/src/test/java/org/apache/shardingsphere/database/connector/firebird/metadata/data/loader/FirebirdNonFixedLengthColumnSizeLoaderTest.java
@@ -23,6 +23,9 @@ import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -32,20 +35,18 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(MockitoExtension.class)
 class FirebirdNonFixedLengthColumnSizeLoaderTest {
-    
-    private static final Collection<String> TABLES = Collections.singleton("test_table");
     
     @Mock
     private DataSource dataSource;
@@ -62,30 +63,107 @@ class FirebirdNonFixedLengthColumnSizeLoaderTest {
     private MetaDataLoaderMaterial material;
     
     @BeforeEach
-    void setUp() throws SQLException {
+    void setUp() {
         DatabaseType databaseType = TypedSPILoader.getService(DatabaseType.class, "Firebird");
-        material = new MetaDataLoaderMaterial(TABLES, "logic_ds", dataSource, databaseType, "schema");
+        material = new MetaDataLoaderMaterial(Collections.singleton("foo_tbl"), "logic_ds", dataSource, databaseType, "schema");
+    }
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("recordedColumnSizeArguments")
+    void assertLoadWithColumnMetadata(final String name, final int dataType, final String columnName, final int columnSize, final Map<String, Integer> expected) throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("FOO_TBL");
+        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(dataType);
+        when(columnsResultSet.getString("COLUMN_NAME")).thenReturn(columnName);
+        when(columnsResultSet.getInt("COLUMN_SIZE")).thenReturn(columnSize);
+        when(columnsResultSet.wasNull()).thenReturn(false);
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertThat(actual.get("foo_tbl"), is(expected));
+    }
+    
+    @Test
+    void assertLoadWithMismatchedTableName() throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("BAR_TBL");
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertTrue(actual.get("foo_tbl").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithFixedLengthType() throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("FOO_TBL");
+        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(Types.BIGINT);
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertTrue(actual.get("foo_tbl").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithNullColumnName() throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("FOO_TBL");
+        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(Types.CHAR);
+        when(columnsResultSet.getString("COLUMN_NAME")).thenReturn(null);
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertTrue(actual.get("foo_tbl").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithNullColumnSize() throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("FOO_TBL");
+        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(Types.CHAR);
+        when(columnsResultSet.getString("COLUMN_NAME")).thenReturn("char_col");
+        when(columnsResultSet.getInt("COLUMN_SIZE")).thenReturn(128);
+        when(columnsResultSet.wasNull()).thenReturn(true);
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertTrue(actual.get("foo_tbl").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithNonPositiveColumnSize() throws SQLException {
+        mockLoadPrerequisites();
+        when(columnsResultSet.next()).thenReturn(true, false);
+        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("FOO_TBL");
+        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(Types.CHAR);
+        when(columnsResultSet.getString("COLUMN_NAME")).thenReturn("char_col");
+        when(columnsResultSet.getInt("COLUMN_SIZE")).thenReturn(0);
+        when(columnsResultSet.wasNull()).thenReturn(false);
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertThat(actual, hasKey("foo_tbl"));
+        assertTrue(actual.get("foo_tbl").isEmpty());
+    }
+    
+    @Test
+    void assertLoadWithNoTables() throws SQLException {
+        material = new MetaDataLoaderMaterial(Collections.emptyList(), "logic_ds", dataSource,
+                TypedSPILoader.getService(DatabaseType.class, "Firebird"), "schema");
+        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
+        assertTrue(actual.isEmpty());
+    }
+    
+    private static Stream<Arguments> recordedColumnSizeArguments() {
+        return Stream.of(
+                Arguments.of("records char column size", Types.CHAR, "char_col", 128, Collections.singletonMap("CHAR_COL", 128)),
+                Arguments.of("records varchar column size", Types.VARCHAR, "varchar_col", 96, Collections.singletonMap("VARCHAR_COL", 96)),
+                Arguments.of("records varbinary column size", Types.VARBINARY, "varbinary_col", 64, Collections.singletonMap("VARBINARY_COL", 64)));
+    }
+    
+    private void mockLoadPrerequisites() throws SQLException {
         when(dataSource.getConnection()).thenReturn(connection);
         when(connection.getMetaData()).thenReturn(databaseMetaData);
         when(connection.getCatalog()).thenReturn("catalog");
         when(connection.getSchema()).thenReturn("schema");
-        when(databaseMetaData.getColumns("catalog", "schema", "TEST_TABLE", "%")).thenReturn(columnsResultSet);
-    }
-    
-    @Test
-    void assertLoadReturnsNonFixedLengthSizes() throws SQLException {
-        when(columnsResultSet.next()).thenReturn(true, true, false);
-        when(columnsResultSet.getString("TABLE_NAME")).thenReturn("TEST_TABLE", "TEST_TABLE");
-        when(columnsResultSet.getInt("DATA_TYPE")).thenReturn(Types.CHAR, Types.VARBINARY);
-        when(columnsResultSet.getString("COLUMN_NAME")).thenReturn("char_col", "varbinary_col");
-        when(columnsResultSet.getInt("COLUMN_SIZE")).thenReturn(128, 64);
-        when(columnsResultSet.wasNull()).thenReturn(false, false);
-        Map<String, Map<String, Integer>> actual = new FirebirdNonFixedLengthColumnSizeLoader(material).load();
-        assertThat(actual, hasKey("test_table"));
-        Map<String, Integer> actualTableSizes = actual.get("test_table");
-        assertThat(actualTableSizes.size(), is(2));
-        assertThat(actualTableSizes.get("CHAR_COL"), is(128));
-        assertFalse(actualTableSizes.containsKey("BIGINT_COL"));
-        assertThat(actualTableSizes.get("VARBINARY_COL"), is(64));
+        when(databaseMetaData.getColumns("catalog", "schema", "FOO_TBL", "%")).thenReturn(columnsResultSet);
     }
 }
