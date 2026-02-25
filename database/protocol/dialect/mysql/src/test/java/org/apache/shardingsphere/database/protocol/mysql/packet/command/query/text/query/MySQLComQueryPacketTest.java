e909eda6fa46ac99518f32a23e856aa736be55cb
zhangliang <zhangliang@apache.org>
2026-02-25 14:27:16 +0800

    Add more test cases on MySQLComQueryPacketTest and MySQLTextResultSetRowPacketTest

--- database/protocol/dialect/mysql/src/test/java/org/apache/shardingsphere/database/protocol/mysql/packet/command/query/text/query/MySQLComQueryPacketTest.java
+++ database/protocol/dialect/mysql/src/test/java/org/apache/shardingsphere/database/protocol/mysql/packet/command/query/text/query/MySQLComQueryPacketTest.java
@@ -21,11 +21,16 @@ import org.apache.shardingsphere.database.protocol.mysql.packet.command.MySQLCom
 import org.apache.shardingsphere.database.protocol.mysql.payload.MySQLPacketPayload;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.hamcrest.Matchers.is;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,19 +40,46 @@ class MySQLComQueryPacketTest {
     @Mock
     private MySQLPacketPayload payload;
     
-    @Test
-    void assertNew() {
-        when(payload.readStringEOF()).thenReturn("SELECT id FROM tbl");
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("newWithStringArguments")
+    void assertNewWithString(final String name, final String inputSQL, final String expectedSQL, final boolean expectedWriteRouteOnly) {
+        MySQLComQueryPacket actual = new MySQLComQueryPacket(inputSQL);
+        assertThat(actual.getSQL(), is(expectedSQL));
+        assertThat(actual.getHintValueContext().isWriteRouteOnly(), is(expectedWriteRouteOnly));
+    }
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("newWithPayloadArguments")
+    void assertNewWithPayload(final String name, final String inputSQL, final String expectedSQL, final boolean expectedWriteRouteOnly) {
+        when(payload.readStringEOF()).thenReturn(inputSQL);
         MySQLComQueryPacket actual = new MySQLComQueryPacket(payload);
-        assertThat(actual.getSQL(), is("SELECT id FROM tbl"));
+        assertThat(actual.getSQL(), is(expectedSQL));
+        assertThat(actual.getHintValueContext().isWriteRouteOnly(), is(expectedWriteRouteOnly));
     }
     
     @Test
     void assertWrite() {
-        when(payload.readStringEOF()).thenReturn("SELECT id FROM tbl");
-        MySQLComQueryPacket actual = new MySQLComQueryPacket(payload);
-        actual.write(payload);
+        new MySQLComQueryPacket("SELECT id FROM tbl").write(payload);
         verify(payload).writeInt1(MySQLCommandPacketType.COM_QUERY.getValue());
         verify(payload).writeStringEOF("SELECT id FROM tbl");
+    }
+    
+    @Test
+    void assertGetSQL() {
+        assertThat(new MySQLComQueryPacket("/* SHARDINGSPHERE_HINT: WRITE_ROUTE_ONLY=true */ SELECT id FROM tbl").getSQL(), is("SELECT id FROM tbl"));
+    }
+    
+    private static Stream<Arguments> newWithStringArguments() {
+        return Stream.of(
+                Arguments.of("WithoutHint", "SELECT id FROM tbl", "SELECT id FROM tbl", false),
+                Arguments.of("WithHintToken", "/* SHARDINGSPHERE_HINT: WRITE_ROUTE_ONLY=true */ SELECT id FROM tbl", "SELECT id FROM tbl", true),
+                Arguments.of("WithHintAlias", "/* ShardingSphere hint: WRITE_ROUTE_ONLY=true */ SELECT id FROM tbl", "SELECT id FROM tbl", true));
+    }
+    
+    private static Stream<Arguments> newWithPayloadArguments() {
+        return Stream.of(
+                Arguments.of("WithoutHint", "SELECT id FROM tbl", "SELECT id FROM tbl", false),
+                Arguments.of("WithHintToken", "/* SHARDINGSPHERE_HINT: WRITE_ROUTE_ONLY=true */ SELECT id FROM tbl", "SELECT id FROM tbl", true),
+                Arguments.of("WithHintAlias", "/* ShardingSphere hint: WRITE_ROUTE_ONLY=true */ SELECT id FROM tbl", "SELECT id FROM tbl", true));
     }
 }
