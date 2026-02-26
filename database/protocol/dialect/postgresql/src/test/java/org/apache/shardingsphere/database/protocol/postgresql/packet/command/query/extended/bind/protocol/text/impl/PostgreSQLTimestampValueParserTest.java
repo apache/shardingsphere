9328fb7b05cd19ca44a62e0cab0f2353287df15e
zhangliang <zhangliang@apache.org>
2026-02-26 17:32:22 +0800

    Add more test cases on bind.protocol.text package

--- database/protocol/dialect/postgresql/src/test/java/org/apache/shardingsphere/database/protocol/postgresql/packet/command/query/extended/bind/protocol/text/impl/PostgreSQLTimestampValueParserTest.java
+++ database/protocol/dialect/postgresql/src/test/java/org/apache/shardingsphere/database/protocol/postgresql/packet/command/query/extended/bind/protocol/text/impl/PostgreSQLTimestampValueParserTest.java
@@ -17,59 +17,48 @@
 
 package org.apache.shardingsphere.database.protocol.postgresql.packet.command.query.extended.bind.protocol.text.impl;
 
-import org.junit.jupiter.api.extension.ExtensionContext;
+import org.apache.shardingsphere.infra.exception.external.sql.type.wrapper.SQLWrapperException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.support.ParameterDeclarations;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isA;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PostgreSQLTimestampValueParserTest {
     
     @ParameterizedTest(name = "{0}")
-    @ArgumentsSource(TestCaseArgumentsProvider.class)
-    void assertGetLocalDateTimeNoExceptionOccurs(final String input, final Timestamp expected) {
+    @MethodSource("parseSuccessArguments")
+    void assertParse(final String name, final String input, final Timestamp expected) {
         assertThat(new PostgreSQLTimestampValueParser().parse(input), is(expected));
     }
     
-    private static final class TestCaseArgumentsProvider implements ArgumentsProvider {
-        
-        @Override
-        public Stream<? extends Arguments> provideArguments(final ParameterDeclarations parameters, final ExtensionContext context) {
-            return Stream.of(Arguments.of("20211012 2323", Timestamp.valueOf("2021-10-12 23:23:00")),
-                    Arguments.of("20211012 23:23", Timestamp.valueOf("2021-10-12 23:23:00")),
-                    Arguments.of("20211012 232323", Timestamp.valueOf("2021-10-12 23:23:23")),
-                    Arguments.of("2021-10-12 23:23:23", Timestamp.valueOf("2021-10-12 23:23:23")),
-                    Arguments.of("2021-10-12 23:23:23.1", Timestamp.valueOf("2021-10-12 23:23:23.1")),
-                    Arguments.of("2021-10-12 23:23:23.12", Timestamp.valueOf("2021-10-12 23:23:23.12")),
-                    Arguments.of("2021-10-12 23:23:23.123", Timestamp.valueOf("2021-10-12 23:23:23.123")),
-                    Arguments.of("2021-10-12 23:23:23.123+08", Timestamp.valueOf("2021-10-12 23:23:23.123")),
-                    Arguments.of("2021-10-12T23:23:23.123+08", Timestamp.valueOf("2021-10-12 23:23:23.123")),
-                    Arguments.of("2021-10-12 23:23:23.12345", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("2021-10-12 23:23:23.12345+0800", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("20211012 23:23:23.12345+0800", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("2021-10-12 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("211012 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("10/12/21 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
-                    Arguments.of("2021-10-12 23:23:23.123456", Timestamp.valueOf("2021-10-12 23:23:23.123456")),
-                    Arguments.of("2021-10-12 23:23:23.1234567", Timestamp.valueOf("2021-10-12 23:23:23.1234567")),
-                    Arguments.of("2021-10-12 23:23:23.12345678", Timestamp.valueOf("2021-10-12 23:23:23.12345678")),
-                    Arguments.of("2021-10-12 23:23:23.123456789", Timestamp.valueOf("2021-10-12 23:23:23.123456789")),
-                    Arguments.of("2021-10-12 23:23:23.123456 +08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456")),
-                    Arguments.of("2021-10-12 23:23:23.1234567 +08:00", Timestamp.valueOf("2021-10-12 23:23:23.1234567")),
-                    Arguments.of("2021-10-12 23:23:23.12345678 +08:00", Timestamp.valueOf("2021-10-12 23:23:23.12345678")),
-                    Arguments.of("2021-10-12 23:23:23.123456789+08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456789")),
-                    Arguments.of("2021-10-12 23:23:23.123456789 +08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456789")),
-                    Arguments.of("2021-10-12 23:23:23.123456 -08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456")),
-                    Arguments.of("2021-3-3 23:23:23.123456", Timestamp.valueOf("2021-03-03 23:23:23.123456")),
-                    Arguments.of("infinity", new Timestamp(9223372036825200000L)),
-                    Arguments.of("-infinity", new Timestamp(-9223372036832400000L)));
-        }
+    @Test
+    void assertParseWithTimestampUtilsException() {
+        assertThat(assertThrows(SQLWrapperException.class, () -> new PostgreSQLTimestampValueParser().parse("bad")).getCause(), isA(SQLException.class));
+    }
+    
+    private static Stream<Arguments> parseSuccessArguments() {
+        return Stream.of(
+                Arguments.of("date and short time", "20211012 2323", Timestamp.valueOf("2021-10-12 23:23:00")),
+                Arguments.of("date and time", "2021-10-12 23:23:23", Timestamp.valueOf("2021-10-12 23:23:23")),
+                Arguments.of("date and nanos", "2021-10-12 23:23:23.123456789", Timestamp.valueOf("2021-10-12 23:23:23.123456789")),
+                Arguments.of("timestamp with T and offset", "2021-10-12T23:23:23.123+08", Timestamp.valueOf("2021-10-12 23:23:23.123")),
+                Arguments.of("timestamp with extended offset", "2021-10-12 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
+                Arguments.of("timestamp with compact date", "20211012 23:23:23.12345+0800", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
+                Arguments.of("timestamp with short date", "211012 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
+                Arguments.of("timestamp with slash date", "10/12/21 23:23:23.12345+08:00:00", Timestamp.valueOf("2021-10-12 23:23:23.12345")),
+                Arguments.of("timestamp with positive zone spacing", "2021-10-12 23:23:23.123456 +08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456")),
+                Arguments.of("timestamp with negative zone spacing", "2021-10-12 23:23:23.123456 -08:00", Timestamp.valueOf("2021-10-12 23:23:23.123456")),
+                Arguments.of("single-digit month and day", "2021-3-3 23:23:23.123456", Timestamp.valueOf("2021-03-03 23:23:23.123456")),
+                Arguments.of("positive infinity fallback", "infinity", new Timestamp(9223372036825200000L)),
+                Arguments.of("negative infinity fallback", "-infinity", new Timestamp(-9223372036832400000L)));
     }
 }
