dadc69d56b2024ba7765bb4984244a160adf667a
zhangliang <zhangliang@apache.org>
2026-03-04 13:15:20 +0800

    Refactor constructor of DropViewStatement to empty buildAttributes

--- mode/core/src/test/java/org/apache/shardingsphere/mode/metadata/refresher/pushdown/type/view/DropViewPushDownMetaDataRefresherTest.java
+++ mode/core/src/test/java/org/apache/shardingsphere/mode/metadata/refresher/pushdown/type/view/DropViewPushDownMetaDataRefresherTest.java
@@ -55,8 +55,7 @@ class DropViewPushDownMetaDataRefresherTest {
         ShardingSphereDatabase database = new ShardingSphereDatabase(
                 "foo_db", databaseType, new ResourceMetaData(Collections.emptyMap()), new RuleMetaData(Collections.emptyList()), Collections.emptyList());
         DropViewStatement sqlStatement = new DropViewStatement(databaseType, Arrays.asList(
-                new SimpleTableSegment(new TableNameSegment(0, 0, new IdentifierValue("foo_view"))), new SimpleTableSegment(new TableNameSegment(0, 0, new IdentifierValue("bar_view"))) 
-        ), false);
+                new SimpleTableSegment(new TableNameSegment(0, 0, new IdentifierValue("foo_view"))), new SimpleTableSegment(new TableNameSegment(0, 0, new IdentifierValue("bar_view")))), false);
         refresher.refresh(metaDataManagerPersistService, database, "logic_ds", "foo_schema", databaseType, sqlStatement, new ConfigurationProperties(new Properties()));
         verify(metaDataManagerPersistService).dropTables(database, "foo_schema", Arrays.asList("foo_view", "bar_view"));
         verify(metaDataManagerPersistService).dropViews(database, "foo_schema", Arrays.asList("foo_view", "bar_view"));
