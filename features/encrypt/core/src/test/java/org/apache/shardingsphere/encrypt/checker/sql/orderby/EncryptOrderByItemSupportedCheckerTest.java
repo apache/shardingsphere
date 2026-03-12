0e1035df7c712792c64320a8a3e09ee2504debf1
duanzhengqiang <duanzhengqiang@apache.org>
2026-03-12 19:26:24 +0800

    Fix unit test

--- features/encrypt/core/src/test/java/org/apache/shardingsphere/encrypt/checker/sql/orderby/EncryptOrderByItemSupportedCheckerTest.java
+++ features/encrypt/core/src/test/java/org/apache/shardingsphere/encrypt/checker/sql/orderby/EncryptOrderByItemSupportedCheckerTest.java
@@ -19,7 +19,7 @@ package org.apache.shardingsphere.encrypt.checker.sql.orderby;
 
 import org.apache.shardingsphere.database.connector.core.metadata.database.enums.NullsOrderType;
 import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
-import org.apache.shardingsphere.encrypt.exception.metadata.MissingMatchedEncryptQueryAlgorithmException;
+import org.apache.shardingsphere.encrypt.exception.syntax.UnsupportedEncryptSQLException;
 import org.apache.shardingsphere.encrypt.rule.EncryptRule;
 import org.apache.shardingsphere.encrypt.rule.column.EncryptColumn;
 import org.apache.shardingsphere.encrypt.rule.table.EncryptTable;
@@ -95,7 +95,7 @@ class EncryptOrderByItemSupportedCheckerTest {
     
     @Test
     void assertCheckFailed() {
-        assertThrows(MissingMatchedEncryptQueryAlgorithmException.class, () -> new EncryptOrderByItemSupportedChecker()
+        assertThrows(UnsupportedEncryptSQLException.class, () -> new EncryptOrderByItemSupportedChecker()
                 .check(mockEncryptRule(), mock(), mock(), mockSelectStatementContext("foo_tbl")));
     }
     
