de4aa92fe21e8ea51158b3b3bb2d7df96b227e5a
zhangliang <zhangliang@apache.org>
2026-03-14 20:43:21 +0800

    Add more test cases on ShardingSphereDatabaseFactoryTest

--- infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/database/ShardingSphereDatabaseFactoryTest.java
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/database/ShardingSphereDatabaseFactoryTest.java
@@ -18,41 +18,89 @@
 package org.apache.shardingsphere.infra.metadata.database;
 
 import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
+import org.apache.shardingsphere.infra.config.database.DatabaseConfiguration;
+import org.apache.shardingsphere.infra.config.database.impl.DataSourceProvidedDatabaseConfiguration;
 import org.apache.shardingsphere.infra.config.props.ConfigurationProperties;
-import org.apache.shardingsphere.infra.spi.exception.ServiceProviderNotFoundException;
+import org.apache.shardingsphere.infra.fixture.FixtureRuleConfiguration;
+import org.apache.shardingsphere.infra.instance.ComputeNodeInstanceContext;
+import org.apache.shardingsphere.infra.metadata.database.schema.model.ShardingSphereSchema;
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.stream.Stream;
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 class ShardingSphereDatabaseFactoryTest {
     
-    private final DatabaseType databaseType = TypedSPILoader.getService(DatabaseType.class, "FIXTURE");
+    private final DatabaseType fixtureDatabaseType = TypedSPILoader.getService(DatabaseType.class, "FIXTURE");
     
-    @Test
-    void assertCreateSystemDatabase() {
-        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.create("system_db", databaseType, new ConfigurationProperties(new Properties()));
-        assertThat(actual.getName(), is("system_db"));
-        assertThat(actual.getProtocolType(), is(databaseType));
+    private final DatabaseType postgresqlDatabaseType = TypedSPILoader.getService(DatabaseType.class, "PostgreSQL");
+    
+    private final ConfigurationProperties props = new ConfigurationProperties(new Properties());
+    
+    private final DatabaseConfiguration databaseConfig = new DataSourceProvidedDatabaseConfiguration(Collections.emptyMap(), Collections.singleton(new FixtureRuleConfiguration()));
+    
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("createWithSystemDatabaseArguments")
+    void assertCreateWithSystemDatabase(final String name, final DatabaseType protocolType, final String databaseName, final String expectedDefaultSchemaName, final int expectedSchemaCount) {
+        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.create(databaseName, protocolType, props);
+        assertThat(actual.getName(), is(databaseName));
+        assertThat(actual.getProtocolType(), is(protocolType));
+        assertThat(actual.getAllSchemas().size(), is(expectedSchemaCount));
+        assertTrue(actual.containsSchema(expectedDefaultSchemaName));
         assertTrue(actual.getRuleMetaData().getRules().isEmpty());
     }
     
     @Test
-    void assertCreateDatabaseWithComputeNodeInstanceContext() {
-        assertThrows(ServiceProviderNotFoundException.class, () -> ShardingSphereDatabaseFactory.create(null, mock(), mock(), new ConfigurationProperties(new Properties()), mock()));
+    void assertCreateWithDatabaseConfiguration() throws SQLException {
+        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.create("foo_db", postgresqlDatabaseType, databaseConfig, props, mock(ComputeNodeInstanceContext.class));
+        assertThat(actual.getName(), is("foo_db"));
+        assertThat(actual.getProtocolType(), is(postgresqlDatabaseType));
+        assertThat(actual.getAllSchemas().size(), is(4));
+        assertThat(actual.getRuleMetaData().getRules().size(), is(1));
+        assertTrue(actual.containsSchema("public"));
+        assertTrue(actual.containsSchema("information_schema"));
+        assertTrue(actual.containsSchema("pg_catalog"));
+        assertTrue(actual.containsSchema("shardingsphere"));
     }
     
     @Test
-    void assertCreateDatabaseWithSchemas() {
-        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.create("foo_db", databaseType, mock(), mock(), Collections.emptyList());
+    void assertCreateWithSchemas() {
+        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.create(
+                "foo_db", fixtureDatabaseType, databaseConfig, mock(ComputeNodeInstanceContext.class), Collections.singletonList(new ShardingSphereSchema("foo_schema", fixtureDatabaseType)));
         assertThat(actual.getName(), is("foo_db"));
-        assertThat(actual.getProtocolType(), is(databaseType));
+        assertThat(actual.getProtocolType(), is(fixtureDatabaseType));
+        assertThat(actual.getAllSchemas().size(), is(1));
+        assertThat(actual.getRuleMetaData().getRules().size(), is(1));
+        assertTrue(actual.containsSchema("foo_schema"));
+    }
+    
+    @Test
+    void assertCreateWithoutSystemSchema() throws SQLException {
+        ShardingSphereDatabase actual = ShardingSphereDatabaseFactory.createWithoutSystemSchema("foo_db", postgresqlDatabaseType, databaseConfig, props, mock(ComputeNodeInstanceContext.class));
+        assertThat(actual.getName(), is("foo_db"));
+        assertThat(actual.getProtocolType(), is(postgresqlDatabaseType));
+        assertThat(actual.getAllSchemas().size(), is(1));
+        assertThat(actual.getRuleMetaData().getRules().size(), is(1));
+        assertTrue(actual.containsSchema("public"));
+        assertFalse(actual.containsSchema("information_schema"));
+    }
+    
+    private static Stream<Arguments> createWithSystemDatabaseArguments() {
+        return Stream.of(
+                Arguments.of("fixture database adds default schema", TypedSPILoader.getService(DatabaseType.class, "FIXTURE"), "system_db", "system_db", 1),
+                Arguments.of("mysql system database keeps existing default schema", TypedSPILoader.getService(DatabaseType.class, "MySQL"), "mysql", "mysql", 1),
+                Arguments.of("postgresql database adds public schema", TypedSPILoader.getService(DatabaseType.class, "PostgreSQL"), "sharding_db", "public", 4));
     }
 }
