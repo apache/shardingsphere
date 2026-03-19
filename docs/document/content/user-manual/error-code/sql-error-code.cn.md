a2cc4e4741e574f74ab599e4c966cae62e0445e1
Raigor <raigor.jiang@gmail.com>
2026-03-19 14:26:03 +0800

    Support non ColumnProjection (like ExpressionProjection) for MySQL prepared statement in Proxy (#38507)
    * Support non ColumnProjection (like ExpressionProjection) for MySQL prepared statement in Proxy
    
    * Add test, update RELEASE-NOTES.md

--- docs/document/content/user-manual/error-code/sql-error-code.cn.md
+++ docs/document/content/user-manual/error-code/sql-error-code.cn.md
@@ -22,6 +22,7 @@ SQL 错误码以标准的 SQL State，Vendor Code 和详细错误信息提供，
 | 10005       | 42S01     | Index '%s' already exists.                                                          |
 | 10010       | HY000     | Rule and storage meta data mismatched, reason is: %s.                               |
 | 10012       | HY000     | Load table meta data failed for database '%s' and tables '%s'.                      |
+| 10024       | HY000     | Can not resolve prepared statement metadata because %s.                             |
 | 10100       | HY000     | Can not %s storage units '%s'.                                                      |
 | 10101       | 42S02     | There is no storage unit in database '%s'.                                          |
 | 10102       | 44000     | Storage units '%s' do not exist in database '%s'.                                   |
--- proxy/frontend/dialect/mysql/src/main/java/org/apache/shardingsphere/proxy/frontend/mysql/command/query/binary/prepare/MySQLPreparedStatementMetadataFactory.java
+++ proxy/frontend/dialect/mysql/src/main/java/org/apache/shardingsphere/proxy/frontend/mysql/command/query/binary/prepare/MySQLPreparedStatementMetadataFactory.java
@@ -15,13 +15,10 @@
  * limitations under the License.
  */
 
-package org.apache.shardingsphere.proxy.frontend.postgresql.command.query.extended;
+package org.apache.shardingsphere.proxy.frontend.mysql.command.query.binary.prepare;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.apache.shardingsphere.infra.binder.context.aware.ParameterAware;
-import org.apache.shardingsphere.infra.binder.context.statement.SQLStatementContext;
-import org.apache.shardingsphere.infra.binder.engine.SQLBindEngine;
 import org.apache.shardingsphere.infra.connection.kernel.KernelProcessor;
 import org.apache.shardingsphere.infra.exception.ShardingSpherePreconditions;
 import org.apache.shardingsphere.infra.exception.kernel.metadata.PreparedStatementMetadataResolutionException;
@@ -33,43 +30,39 @@ import org.apache.shardingsphere.infra.session.query.QueryContext;
 import org.apache.shardingsphere.proxy.backend.connector.ProxyDatabaseConnectionManager;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
+import org.apache.shardingsphere.proxy.frontend.mysql.command.query.binary.MySQLServerPreparedStatement;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Metadata factory for PostgreSQL prepared statements.
+ * Metadata factory for MySQL prepared statements.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class PostgreSQLPreparedStatementMetadataFactory {
+public final class MySQLPreparedStatementMetadataFactory {
     
     /**
      * Load actual prepared statement for metadata access.
      *
      * @param connectionSession connection session
      * @param preparedStatement prepared statement
-     * @param parameters parameters
      * @return actual prepared statement
      * @throws SQLException SQL exception
      */
-    public static PreparedStatement load(final ConnectionSession connectionSession, final PostgreSQLServerPreparedStatement preparedStatement, final List<Object> parameters) throws SQLException {
+    public static PreparedStatement load(final ConnectionSession connectionSession, final MySQLServerPreparedStatement preparedStatement) throws SQLException {
         ShardingSphereMetaData metaData = ProxyContext.getInstance().getContextManager().getMetaDataContexts().getMetaData();
-        SQLStatementContext sqlStatementContext = new SQLBindEngine(metaData, connectionSession.getCurrentDatabaseName(), preparedStatement.getHintValueContext())
-                .bind(preparedStatement.getSqlStatementContext().getSqlStatement());
-        if (sqlStatementContext instanceof ParameterAware) {
-            ((ParameterAware) sqlStatementContext).bindParameters(parameters);
-        }
-        QueryContext queryContext = new QueryContext(
-                sqlStatementContext, preparedStatement.getSql(), parameters, preparedStatement.getHintValueContext(), connectionSession.getConnectionContext(), metaData);
+        QueryContext queryContext = new QueryContext(preparedStatement.getSqlStatementContext(), preparedStatement.getSql(),
+                Collections.emptyList(), preparedStatement.getHintValueContext(), connectionSession.getConnectionContext(), metaData);
         ExecutionContext executionContext = new KernelProcessor().generateExecutionContext(queryContext, metaData.getGlobalRuleMetaData(), metaData.getProps());
         ShardingSpherePreconditions.checkNotEmpty(executionContext.getExecutionUnits(),
                 () -> new PreparedStatementMetadataResolutionException("no execution unit was generated"));
         ExecutionUnit executionUnit = executionContext.getExecutionUnits().iterator().next();
         ProxyDatabaseConnectionManager databaseConnectionManager = connectionSession.getDatabaseConnectionManager();
-        List<Connection> connections =
-                databaseConnectionManager.getConnections(connectionSession.getUsedDatabaseName(), executionUnit.getDataSourceName(), 0, 1, ConnectionMode.CONNECTION_STRICTLY);
+        List<Connection> connections = databaseConnectionManager.getConnections(
+                connectionSession.getUsedDatabaseName(), executionUnit.getDataSourceName(), 0, 1, ConnectionMode.CONNECTION_STRICTLY);
         ShardingSpherePreconditions.checkNotEmpty(connections,
                 () -> new PreparedStatementMetadataResolutionException("no backend connection was acquired"));
         return connections.iterator().next().prepareStatement(executionUnit.getSqlUnit().getSql());
