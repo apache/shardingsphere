9a8cc568cdd85bd5893bc2ce3e1b904135e86dc9
Haoran Meng <menghaoranss@gmail.com>
2026-03-27 16:26:06 +0800

    Rename attach identifier context to refresh (#38571)

--- infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/database/schema/model/ShardingSphereTableIdentifierTest.java
+++ infra/common/src/test/java/org/apache/shardingsphere/infra/metadata/database/schema/model/ShardingSphereTableIdentifierTest.java
@@ -36,7 +36,7 @@ class ShardingSphereTableIdentifierTest {
     void assertContainsColumn() {
         ShardingSphereColumn column = new ShardingSphereColumn("foo_col", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.singleton(column), Collections.emptyList(), Collections.emptyList());
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
         assertTrue(table.containsColumn("FOO_COL"));
     }
     
@@ -44,7 +44,7 @@ class ShardingSphereTableIdentifierTest {
     void assertContainsColumnWithOracleRule() {
         ShardingSphereColumn column = new ShardingSphereColumn("FOO_COL", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.singleton(column), Collections.emptyList(), Collections.emptyList());
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newUpperCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newUpperCaseRuleSet()));
         assertTrue(table.containsColumn("foo_col"));
     }
     
@@ -52,7 +52,7 @@ class ShardingSphereTableIdentifierTest {
     void assertGetColumn() {
         ShardingSphereColumn column = new ShardingSphereColumn("foo_col", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.singleton(column), Collections.emptyList(), Collections.emptyList());
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
         assertThat(table.getColumn("FOO_COL"), is(column));
     }
     
@@ -61,7 +61,7 @@ class ShardingSphereTableIdentifierTest {
         ShardingSphereColumn column1 = new ShardingSphereColumn("foo_col", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereColumn column2 = new ShardingSphereColumn("bar_col", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Arrays.asList(column1, column2), Collections.emptyList(), Collections.emptyList());
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
         assertThat(table.findColumnNamesIfNotExistedFrom(Collections.singleton("FOO_COL")), is(Collections.singleton("bar_col")));
     }
     
@@ -69,7 +69,7 @@ class ShardingSphereTableIdentifierTest {
     void assertGetIndex() {
         ShardingSphereIndex index = new ShardingSphereIndex("foo_idx", Collections.emptyList(), false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.emptyList(), Collections.singleton(index), Collections.emptyList());
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
         assertThat(table.getIndex("FOO_IDX"), is(index));
     }
     
@@ -77,16 +77,16 @@ class ShardingSphereTableIdentifierTest {
     void assertContainsConstraint() {
         ShardingSphereConstraint constraint = new ShardingSphereConstraint("FOO_FK", "ref_tbl");
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.emptyList(), Collections.emptyList(), Collections.singleton(constraint));
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newUpperCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newUpperCaseRuleSet()));
         assertTrue(table.containsConstraint("foo_fk"));
     }
     
     @Test
-    void assertAttachIdentifierContext() {
+    void assertRefreshIdentifierContext() {
         ShardingSphereColumn column = new ShardingSphereColumn("Foo_Col", Types.INTEGER, false, true, false, true, false, false);
         ShardingSphereTable table = new ShardingSphereTable("foo_tbl", Collections.singleton(column), Collections.emptyList(), Collections.emptyList());
         assertTrue(table.containsColumn("FOO_COL"));
-        table.attachIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
+        table.refreshIdentifierContext(new DatabaseIdentifierContext(IdentifierCaseRuleSets.newLowerCaseRuleSet()));
         assertFalse(table.containsColumn("FOO_COL"));
     }
 }
