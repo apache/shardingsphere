086dd78b51c7c5b12ae74a52166f7f735f32b651
daguimu <daguimu.geek@gmail.com>
2026-03-27 11:51:05 +0800

    Fix test parameter parsing treating leading-zero strings as octal numbers
    Fixes #31001

---
+++ test/it/rewriter/src/test/java/org/apache/shardingsphere/test/it/rewriter/engine/parameter/SQLRewriteEngineTestParametersBuilderTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.test.it.rewriter.engine.parameter;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+class SQLRewriteEngineTestParametersBuilderTest {
+
+    @Test
+    void assertCreateInputParameterWithRegularInteger() throws Exception {
+        assertThat(invokeCreateInputParameter("1"), is(1));
+    }
+
+    @Test
+    void assertCreateInputParameterWithLargeNumber() throws Exception {
+        assertThat(invokeCreateInputParameter("1000"), is(1000));
+    }
+
+    @Test
+    void assertCreateInputParameterWithLeadingZeroDigits() throws Exception {
+        Object result = invokeCreateInputParameter("04844448888");
+        assertInstanceOf(Long.class, result);
+        assertThat(result, is(4844448888L));
+    }
+
+    @Test
+    void assertCreateInputParameterWithLeadingZeroValidOctalLikeDigits() throws Exception {
+        Object result = invokeCreateInputParameter("04100001111");
+        assertInstanceOf(Long.class, result);
+        assertThat(result, is(4100001111L));
+    }
+
+    @Test
+    void assertCreateInputParameterWithSingleZero() throws Exception {
+        assertThat(invokeCreateInputParameter("0"), is(0));
+    }
+
+    @Test
+    void assertCreateInputParameterWithString() throws Exception {
+        assertThat(invokeCreateInputParameter("aaa"), is("aaa"));
+    }
+
+    @Test
+    void assertCreateInputParameterWithNull() throws Exception {
+        assertThat(invokeCreateInputParameter("NULL"), is(nullValue()));
+    }
+
+    @Test
+    void assertCreateInputParameterWithLongOverflow() throws Exception {
+        Object result = invokeCreateInputParameter("99999999999999999999");
+        assertInstanceOf(String.class, result);
+    }
+
+    private static Object invokeCreateInputParameter(final String inputParam) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = SQLRewriteEngineTestParametersBuilder.class.getDeclaredMethod("createInputParameter", String.class);
+        method.setAccessible(true);
+        return method.invoke(null, inputParam);
+    }
+}
