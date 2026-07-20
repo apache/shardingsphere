920414aa7483b10687de8117a5d435f8b1858259
makssent <igoshinreceived@yandex.ru>
2026-07-20 10:24:08 +0300

    fix seek_blob review

---
+++ proxy/frontend/dialect/firebird/src/test/java/org/apache/shardingsphere/proxy/frontend/firebird/command/query/blob/FirebirdSeekBlobCommandExecutorTest.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.shardingsphere.proxy.frontend.firebird.command.query.blob;
+
+import org.apache.shardingsphere.database.protocol.firebird.exception.FirebirdProtocolException;
+import org.apache.shardingsphere.database.protocol.firebird.packet.command.query.blob.FirebirdSeekBlobCommandPacket;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FirebirdSeekBlobCommandExecutorTest {
+    
+    @Mock
+    private FirebirdSeekBlobCommandPacket packet;
+    
+    @Test
+    void assertExecuteThrowsUnsupported() {
+        when(packet.getBlobHandle()).thenReturn(42);
+        FirebirdSeekBlobCommandExecutor executor = new FirebirdSeekBlobCommandExecutor(packet);
+        FirebirdProtocolException actual = assertThrows(FirebirdProtocolException.class, executor::execute);
+        assertThat(actual.getMessage(), is("SEEK BLOB is not supported at the moment, blob handle: 42"));
+    }
+}
