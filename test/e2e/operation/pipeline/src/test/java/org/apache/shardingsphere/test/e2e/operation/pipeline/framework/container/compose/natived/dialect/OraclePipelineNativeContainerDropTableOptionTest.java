f28df3b4b85a5c060abfc8af82c64ae01f221af6
Liang Zhang <zhangliang@apache.org>
2026-07-31 00:53:08 +0800

    Remove useless NativeStorageContainerOption (#39284)

--- test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/framework/container/compose/natived/dialect/OraclePipelineNativeContainerDropTableOptionTest.java
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/framework/container/compose/natived/dialect/OraclePipelineNativeContainerDropTableOptionTest.java
@@ -17,15 +17,11 @@
 
 package org.apache.shardingsphere.test.e2e.operation.pipeline.framework.container.compose.natived.dialect;
 
-import org.apache.shardingsphere.database.connector.core.spi.DatabaseTypedSPILoader;
 import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.spi.type.typed.TypedSPILoader;
-import org.apache.shardingsphere.test.e2e.env.container.storage.option.NativeStorageContainerOption;
 import org.apache.shardingsphere.test.e2e.env.container.storage.option.StorageContainerConnectOption;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
-
-import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -39,27 +35,9 @@ class OraclePipelineNativeContainerDropTableOptionTest {
     void assertGetJdbcUrl() {
         DatabaseType databaseType = mock(DatabaseType.class);
         StorageContainerConnectOption connectOption = mock(StorageContainerConnectOption.class);
-        NativeStorageContainerOption nativeOption = mock(NativeStorageContainerOption.class);
-        when(nativeOption.getAccessURL(connectOption, "localhost", 1521, "pipeline_e2e_0")).thenReturn("jdbc:oracle:thin:@localhost:1521:XE");
-        try (
-                MockedStatic<TypedSPILoader> typedSPILoader = mockStatic(TypedSPILoader.class);
-                MockedStatic<DatabaseTypedSPILoader> databaseTypedSPILoader = mockStatic(DatabaseTypedSPILoader.class)) {
-            typedSPILoader.when(() -> TypedSPILoader.getService(DatabaseType.class, "Oracle")).thenReturn(databaseType);
-            databaseTypedSPILoader.when(() -> DatabaseTypedSPILoader.findService(NativeStorageContainerOption.class, databaseType)).thenReturn(Optional.of(nativeOption));
-            assertThat(new OraclePipelineNativeContainerDropTableOption().getJdbcUrl(connectOption, 1521, "pipeline_e2e_0"), is("jdbc:oracle:thin:@localhost:1521:XE"));
-        }
-    }
-    
-    @Test
-    void assertGetJdbcUrlWithoutNativeOption() {
-        DatabaseType databaseType = mock(DatabaseType.class);
-        StorageContainerConnectOption connectOption = mock(StorageContainerConnectOption.class);
         when(connectOption.getURL("localhost", 1521, "")).thenReturn("jdbc:oracle:thin:@localhost:1521:XE");
-        try (
-                MockedStatic<TypedSPILoader> typedSPILoader = mockStatic(TypedSPILoader.class);
-                MockedStatic<DatabaseTypedSPILoader> databaseTypedSPILoader = mockStatic(DatabaseTypedSPILoader.class)) {
+        try (MockedStatic<TypedSPILoader> typedSPILoader = mockStatic(TypedSPILoader.class)) {
             typedSPILoader.when(() -> TypedSPILoader.getService(DatabaseType.class, "Oracle")).thenReturn(databaseType);
-            databaseTypedSPILoader.when(() -> DatabaseTypedSPILoader.findService(NativeStorageContainerOption.class, databaseType)).thenReturn(Optional.empty());
             assertThat(new OraclePipelineNativeContainerDropTableOption().getJdbcUrl(connectOption, 1521, "pipeline_e2e_0"), is("jdbc:oracle:thin:@localhost:1521:XE"));
         }
     }
