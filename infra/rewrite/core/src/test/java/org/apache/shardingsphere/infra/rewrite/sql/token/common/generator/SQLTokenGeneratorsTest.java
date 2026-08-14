af206619580b5997224dba85bc75e4142d4cceb4
Haoran Meng <menghaoranss@gmail.com>
2026-08-14 15:10:19 +0800

    Use database metadata for logical default schema resolution (#39416)

--- infra/rewrite/core/src/test/java/org/apache/shardingsphere/infra/rewrite/sql/token/common/generator/SQLTokenGeneratorsTest.java
+++ infra/rewrite/core/src/test/java/org/apache/shardingsphere/infra/rewrite/sql/token/common/generator/SQLTokenGeneratorsTest.java
@@ -21,6 +21,7 @@ import org.apache.shardingsphere.database.connector.core.type.DatabaseType;
 import org.apache.shardingsphere.infra.binder.context.statement.SQLStatementContext;
 import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.schema.model.ShardingSphereSchema;
+import org.apache.shardingsphere.infra.rewrite.sql.token.common.generator.aware.SchemaMetaDataAware;
 import org.apache.shardingsphere.infra.rewrite.sql.token.common.pojo.SQLToken;
 import org.apache.shardingsphere.infra.session.connection.ConnectionContext;
 import org.junit.jupiter.api.Test;
@@ -28,6 +29,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,24 +37,29 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 class SQLTokenGeneratorsTest {
     
     @Test
     @SuppressWarnings("unchecked")
     void assertGenerateSQLTokensWithOptionalSQLTokenGenerator() {
-        OptionalSQLTokenGenerator<SQLStatementContext> generator = mock(OptionalSQLTokenGenerator.class);
+        OptionalSQLTokenGenerator<SQLStatementContext> generator = mock(OptionalSQLTokenGenerator.class, withSettings().extraInterfaces(SchemaMetaDataAware.class));
         SQLToken expectedToken = mock(SQLToken.class);
         when(generator.generateSQLToken(any(SQLStatementContext.class))).thenReturn(expectedToken);
         SQLTokenGenerators generators = new SQLTokenGenerators();
         generators.addAll(Collections.singleton(generator));
         ShardingSphereDatabase database = mock(ShardingSphereDatabase.class);
-        when(database.getName()).thenReturn("sharding_db");
-        when(database.getSchema("test")).thenReturn(new ShardingSphereSchema("test", mock(DatabaseType.class)));
+        ShardingSphereSchema defaultSchema = new ShardingSphereSchema("test", mock(DatabaseType.class));
+        when(database.getAllSchemas()).thenReturn(Collections.singleton(defaultSchema));
+        when(database.findDefaultSchema()).thenReturn(Optional.of(defaultSchema));
         Collection<SQLToken> actualSqlTokens = generators.generateSQLTokens(database, mock(SQLStatementContext.class, RETURNS_DEEP_STUBS), Collections.emptyList(), mock(ConnectionContext.class));
         assertThat(actualSqlTokens.size(), is(1));
         assertThat(actualSqlTokens.iterator().next(), is(expectedToken));
+        verify((SchemaMetaDataAware) generator).setSchemas(Collections.singletonMap("test", defaultSchema));
+        verify((SchemaMetaDataAware) generator).setDefaultSchema(defaultSchema);
     }
     
     @Test
