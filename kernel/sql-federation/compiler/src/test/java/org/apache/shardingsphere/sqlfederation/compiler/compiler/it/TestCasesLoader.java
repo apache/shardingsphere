2e17ad78006ded94bbf846aae19e844fa767e25e
Liang Zhang <zhangliang@apache.org>
2026-08-15 14:48:06 +0800

    Remove dependency on jackson-dataformat-xml (#39466)
    Replace JDBC repository and SQL Federation XML loading with secure JDK StAX parsing.
    Clean obsolete native-image metadata and Proxy distribution license entries.

--- kernel/sql-federation/compiler/src/test/java/org/apache/shardingsphere/sqlfederation/compiler/compiler/it/TestCasesLoader.java
+++ kernel/sql-federation/compiler/src/test/java/org/apache/shardingsphere/sqlfederation/compiler/compiler/it/TestCasesLoader.java
@@ -17,15 +17,19 @@
 
 package org.apache.shardingsphere.sqlfederation.compiler.compiler.it;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 /**
  * Integration test cases loader.
@@ -33,8 +37,6 @@ import java.util.Objects;
 public final class TestCasesLoader {
     
     private static final TestCasesLoader INSTANCE = new TestCasesLoader();
-    
-    private static final ObjectMapper XML_MAPPER = XmlMapper.builder().build();
     
     /**
      * Get singleton instance.
@@ -55,15 +57,148 @@ public final class TestCasesLoader {
         Collection<TestCase> result = new LinkedList<>();
         URL queryCaseUrl = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("cases/federation-query-sql-cases.xml"));
         URL deleteCaseUrl = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("cases/federation-delete-sql-cases.xml"));
-        result.addAll(loadTestCase(queryCaseUrl));
-        result.addAll(loadTestCase(deleteCaseUrl));
+        XMLInputFactory factory = createXMLInputFactory();
+        result.addAll(loadTestCase(factory, queryCaseUrl));
+        result.addAll(loadTestCase(factory, deleteCaseUrl));
         return result;
     }
     
-    private Collection<TestCase> loadTestCase(final URL url) throws IOException {
-        try (FileReader reader = new FileReader(url.getFile())) {
-            TestCases testCases = XML_MAPPER.readValue(reader, TestCases.class);
-            return testCases.getTestCases();
+    private XMLInputFactory createXMLInputFactory() {
+        XMLInputFactory result = XMLInputFactory.newFactory();
+        result.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+        result.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+        result.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        result.setXMLResolver((publicId, systemId, baseUri, namespace) -> {
+            throw new XMLStreamException("External XML resources are not allowed.");
+        });
+        return result;
+    }
+    
+    private Collection<TestCase> loadTestCase(final XMLInputFactory factory, final URL url) throws IOException {
+        try (InputStream inputStream = url.openStream()) {
+            return parse(factory, inputStream);
+        }
+    }
+    
+    private Collection<TestCase> parse(final XMLInputFactory factory, final InputStream inputStream) throws IOException {
+        try {
+            XMLStreamReader reader = factory.createXMLStreamReader(inputStream);
+            try {
+                return parse(reader);
+            } finally {
+                reader.close();
+            }
+        } catch (final XMLStreamException ex) {
+            throw new IOException("Failed to parse SQL federation test cases XML.", ex);
+        }
+    }
+    
+    private Collection<TestCase> parse(final XMLStreamReader reader) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            rejectDTD(event);
+            if (XMLStreamConstants.START_ELEMENT == event) {
+                validateRootElement(reader);
+                Collection<TestCase> result = new LinkedList<>();
+                parseTestCases(reader, result);
+                return result;
+            }
+        }
+        throw new XMLStreamException("SQL federation test cases root element is missing.");
+    }
+    
+    private void validateRootElement(final XMLStreamReader reader) throws XMLStreamException {
+        if (!"test-cases".equals(reader.getLocalName())) {
+            throw new XMLStreamException("Unexpected SQL federation test cases root element: " + reader.getLocalName());
+        }
+        if (0 != reader.getAttributeCount()) {
+            throw new XMLStreamException("Unexpected attribute on SQL federation test cases root element.");
+        }
+    }
+    
+    private void parseTestCases(final XMLStreamReader reader, final Collection<TestCase> result) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            rejectDTD(event);
+            if (XMLStreamConstants.START_ELEMENT == event) {
+                if (!"test-case".equals(reader.getLocalName())) {
+                    throw new XMLStreamException("Unexpected SQL federation test cases element: " + reader.getLocalName());
+                }
+                result.add(parseTestCase(reader));
+            } else if (XMLStreamConstants.END_ELEMENT == event) {
+                validateDocumentEnd(reader);
+                return;
+            } else {
+                rejectText(event, reader, "SQL federation test cases");
+            }
+        }
+        throw new XMLStreamException("SQL federation test cases root element is not closed.");
+    }
+    
+    private TestCase parseTestCase(final XMLStreamReader reader) throws XMLStreamException {
+        validateAttributes(reader, "sql");
+        TestCase result = new TestCase();
+        result.setSql(reader.getAttributeValue(null, "sql"));
+        List<TestCaseAssertion> assertions = null;
+        while (reader.hasNext()) {
+            int event = reader.next();
+            rejectDTD(event);
+            if (XMLStreamConstants.START_ELEMENT == event) {
+                if (!"assertion".equals(reader.getLocalName())) {
+                    throw new XMLStreamException("Unexpected SQL federation test case element: " + reader.getLocalName());
+                }
+                if (null == assertions) {
+                    assertions = new LinkedList<>();
+                }
+                assertions.add(parseAssertion(reader));
+            } else if (XMLStreamConstants.END_ELEMENT == event) {
+                result.setAssertion(assertions);
+                return result;
+            } else {
+                rejectText(event, reader, "SQL federation test case");
+            }
+        }
+        throw new XMLStreamException("SQL federation test case element is not closed.");
+    }
+    
+    private TestCaseAssertion parseAssertion(final XMLStreamReader reader) throws XMLStreamException {
+        validateAttributes(reader, "expected-result");
+        TestCaseAssertion result = new TestCaseAssertion();
+        result.setExpectedResult(reader.getAttributeValue(null, "expected-result"));
+        if (!reader.getElementText().trim().isEmpty()) {
+            throw new XMLStreamException("Unexpected text in SQL federation test case assertion.");
+        }
+        return result;
+    }
+    
+    private void validateAttributes(final XMLStreamReader reader, final String expectedAttribute) throws XMLStreamException {
+        for (int i = 0; i < reader.getAttributeCount(); i++) {
+            if (!expectedAttribute.equals(reader.getAttributeLocalName(i))) {
+                throw new XMLStreamException("Unexpected attribute on SQL federation test case element: " + reader.getAttributeLocalName(i));
+            }
+        }
+    }
+    
+    private void rejectDTD(final int event) throws XMLStreamException {
+        if (XMLStreamConstants.DTD == event) {
+            throw new XMLStreamException("DTD is not allowed.");
+        }
+    }
+    
+    private void rejectText(final int event, final XMLStreamReader reader, final String element) throws XMLStreamException {
+        if ((XMLStreamConstants.CHARACTERS == event || XMLStreamConstants.CDATA == event) && !reader.isWhiteSpace()) {
+            throw new XMLStreamException("Unexpected text in " + element + " element.");
+        }
+    }
+    
+    private void validateDocumentEnd(final XMLStreamReader reader) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            rejectDTD(event);
+            if (XMLStreamConstants.START_ELEMENT == event) {
+                throw new XMLStreamException("Unexpected content after SQL federation test cases root element.");
+            }
+            rejectText(event, reader, "SQL federation test cases document");
         }
     }
 }
