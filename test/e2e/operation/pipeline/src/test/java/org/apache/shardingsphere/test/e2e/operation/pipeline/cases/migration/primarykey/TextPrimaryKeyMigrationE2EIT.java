1f81010ea5dccf5266d264175aea8ff311939f04
Hongsheng Zhong <zhonghongsheng@apache.org>
2026-08-20 08:38:42 +0800

    Fix empty string primary key E2E for openGauss

--- test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/cases/migration/primarykey/TextPrimaryKeyMigrationE2EIT.java
+++ test/e2e/operation/pipeline/src/test/java/org/apache/shardingsphere/test/e2e/operation/pipeline/cases/migration/primarykey/TextPrimaryKeyMigrationE2EIT.java
@@ -20,6 +20,7 @@ package org.apache.shardingsphere.test.e2e.operation.pipeline.cases.migration.pr
 import com.google.common.collect.ImmutableMap;
 import org.apache.shardingsphere.data.pipeline.scenario.migration.MigrationJobType;
 import org.apache.shardingsphere.database.connector.mysql.type.MySQLDatabaseType;
+import org.apache.shardingsphere.database.connector.postgresql.type.PostgreSQLDatabaseType;
 import org.apache.shardingsphere.infra.metadata.database.schema.QualifiedTable;
 import org.apache.shardingsphere.test.e2e.operation.pipeline.cases.PipelineContainerComposer;
 import org.apache.shardingsphere.test.e2e.operation.pipeline.cases.migration.AbstractMigrationE2EIT;
@@ -57,7 +58,9 @@ class TextPrimaryKeyMigrationE2EIT extends AbstractMigrationE2EIT {
                     containerComposer.getDatabaseType(), new QualifiedTable(null, getSourceTableName(containerComposer)));
             orderDAO.createTable();
             orderDAO.batchInsert(PipelineContainerComposer.TABLE_INIT_ROW_COUNT);
-            orderDAO.insert("", 0, "emptyPrimaryKey");
+            if (containerComposer.getDatabaseType() instanceof MySQLDatabaseType || containerComposer.getDatabaseType() instanceof PostgreSQLDatabaseType) {
+                orderDAO.insert("", 0, "emptyPrimaryKey");
+            }
             PipelineE2EDistSQLFacade distSQLFacade = new PipelineE2EDistSQLFacade(containerComposer, new MigrationJobType());
             distSQLFacade.alterPipelineRule();
             addMigrationSourceResource(containerComposer);
