14fb7871702431f4a748b0f580854b64ec1c5e31
zhangliang <zhangliang@apache.org>
2026-09-06 16:32:14 +0800

    Fix PostgreSQL and openGauss CTE column alias parsing

--- parser/sql/engine/dialect/opengauss/src/main/java/org/apache/shardingsphere/sql/parser/engine/opengauss/visitor/statement/OpenGaussStatementVisitor.java
+++ parser/sql/engine/dialect/opengauss/src/main/java/org/apache/shardingsphere/sql/parser/engine/opengauss/visitor/statement/OpenGaussStatementVisitor.java
@@ -1026,9 +1026,13 @@ public abstract class OpenGaussStatementVisitor extends OpenGaussStatementParser
     
     @Override
     public ASTNode visitCommonTableExpr(final CommonTableExprContext ctx) {
-        return new CommonTableExpressionSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), (AliasSegment) visit(ctx.alias()),
+        CommonTableExpressionSegment result = new CommonTableExpressionSegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), (AliasSegment) visit(ctx.alias()),
                 new SubquerySegment(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex(), (SelectStatement) visit(ctx.preparableStmt().select()),
                         getOriginalText(ctx.preparableStmt().select())));
+        if (null != ctx.optNameList().nameList()) {
+            result.getColumns().addAll(generateUsingColumn(ctx.optNameList().nameList()));
+        }
+        return result;
     }
     
     @Override
