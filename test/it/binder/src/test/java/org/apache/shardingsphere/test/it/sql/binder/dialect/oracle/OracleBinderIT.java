7928ec48171ab8b7c457affa32b8310ea85633ab
Joelle Mahfoud <joelle_mahfoud@live.com>
2026-09-16 01:11:23 +0300

    fix Oracle SYS fallback ignores object role and quoted-identifier semantics
    Signed-off-by: Joelle Mahfoud <joelle_mahfoud@live.com>

--- test/it/binder/src/test/java/org/apache/shardingsphere/test/it/sql/binder/dialect/oracle/OracleBinderIT.java
+++ test/it/binder/src/test/java/org/apache/shardingsphere/test/it/sql/binder/dialect/oracle/OracleBinderIT.java
@@ -19,11 +19,14 @@ package org.apache.shardingsphere.test.it.sql.binder.dialect.oracle;
 
 import org.apache.shardingsphere.infra.config.props.ConfigurationProperties;
 import org.apache.shardingsphere.infra.config.props.temporary.TemporaryConfigurationPropertyKey;
+import org.apache.shardingsphere.infra.exception.kernel.metadata.TableNotFoundException;
 import org.apache.shardingsphere.infra.util.props.PropertiesBuilder;
 import org.apache.shardingsphere.infra.util.props.PropertiesBuilder.Property;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.item.ColumnProjectionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.item.ProjectionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.bound.ColumnSegmentBoundInfo;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.CreateTableStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.view.CreateViewStatement;
 import org.apache.shardingsphere.sql.parser.statement.core.statement.type.dml.SelectStatement;
 import org.apache.shardingsphere.test.it.sql.binder.SQLBinderIT;
 import org.apache.shardingsphere.test.it.sql.binder.SQLBinderITSettings;
@@ -35,6 +38,7 @@ import java.util.Properties;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isA;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SQLBinderITSettings("Oracle")
 class OracleBinderIT extends SQLBinderIT {
@@ -102,6 +106,33 @@ class OracleBinderIT extends SQLBinderIT {
         String sql = "SELECT SEQUENCE_NAME FROM ALL_SEQUENCES";
         SelectStatement actual = (SelectStatement) bindSQLStatement("Oracle", sql, props);
         assertColumnBound(actual.getProjections().getProjections().get(0), "ALL_SEQUENCES", "SEQUENCE_NAME");
+    }
+    
+    @Test
+    void assertBindCreateTableNotShadowedByDictionaryViewName() {
+        String sql = "CREATE TABLE ALL_TABLES (FOO_COL NUMBER)";
+        CreateTableStatement actual = (CreateTableStatement) bindSQLStatement("Oracle", sql, new ConfigurationProperties(new Properties()));
+        assertThat(actual.getTable().getTableName().getTableBoundInfo().get().getOriginalSchema().getValue(), is("FOO_DB_1"));
+    }
+    
+    @Test
+    void assertBindQuotedIdentifierDoesNotMatchDictionaryView() {
+        String sql = "SELECT SEQUENCE_NAME FROM \"all_sequences\"";
+        assertThrows(TableNotFoundException.class, () -> bindSQLStatement("Oracle", sql, new ConfigurationProperties(new Properties())));
+    }
+    
+    @Test
+    void assertBindQuotedIdentifierMatchesDictionaryViewWithExactCase() {
+        String sql = "SELECT SEQUENCE_NAME FROM \"ALL_SEQUENCES\"";
+        SelectStatement actual = (SelectStatement) bindSQLStatement("Oracle", sql, new ConfigurationProperties(new Properties()));
+        assertColumnBound(actual.getProjections().getProjections().get(0), "ALL_SEQUENCES", "SEQUENCE_NAME");
+    }
+    
+    @Test
+    void assertBindCreateViewSelectingFromDictionaryView() {
+        String sql = "CREATE VIEW v1 AS SELECT READ_ONLY FROM ALL_TABLES";
+        CreateViewStatement actual = (CreateViewStatement) bindSQLStatement("Oracle", sql, new ConfigurationProperties(new Properties()));
+        assertColumnBound(actual.getSelect().getProjections().getProjections().get(0), "ALL_TABLES", "READ_ONLY");
     }
     
     private void assertColumnBound(final ProjectionSegment actualProjection, final String expectedTable, final String expectedColumn) {
--- test/it/binder/src/test/java/org/apache/shardingsphere/test/it/sql/binder/dialect/postgresql/PostgreSQLBinderIT.java
+++ test/it/binder/src/test/java/org/apache/shardingsphere/test/it/sql/binder/dialect/postgresql/PostgreSQLBinderIT.java
@@ -17,9 +17,13 @@
 
 package org.apache.shardingsphere.test.it.sql.binder.dialect.postgresql;
 
+import org.apache.shardingsphere.infra.exception.kernel.metadata.TableNotFoundException;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.item.ColumnProjectionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.dml.item.ProjectionSegment;
 import org.apache.shardingsphere.sql.parser.statement.core.segment.generic.bound.ColumnSegmentBoundInfo;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.index.CreateIndexStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.AlterTableStatement;
+import org.apache.shardingsphere.sql.parser.statement.core.statement.type.ddl.table.CreateTableStatement;
 import org.apache.shardingsphere.sql.parser.statement.core.statement.type.dml.SelectStatement;
 import org.apache.shardingsphere.test.it.sql.binder.SQLBinderIT;
 import org.apache.shardingsphere.test.it.sql.binder.SQLBinderITSettings;
@@ -27,6 +31,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SQLBinderITSettings("PostgreSQL")
 class PostgreSQLBinderIT extends SQLBinderIT {
@@ -39,5 +44,52 @@ class PostgreSQLBinderIT extends SQLBinderIT {
         ColumnSegmentBoundInfo actualColumnBoundInfo = ((ColumnProjectionSegment) actualProjection).getColumn().getColumnBoundInfo();
         assertThat(actualColumnBoundInfo.getOriginalTable().getValue(), is("pg_class"));
         assertThat(actualColumnBoundInfo.getOriginalSchema().getValue(), is("pg_catalog"));
+    }
+    
+    @Test
+    void assertBindCreateTableNotShadowedBySystemCatalogTableName() {
+        String sql = "CREATE TABLE pg_extension (foo_col text)";
+        CreateTableStatement actual = (CreateTableStatement) bindSQLStatement("PostgreSQL", sql);
+        assertThat(actual.getTable().getTableName().getTableBoundInfo().get().getOriginalSchema().getValue(), is("public"));
+    }
+    
+    @Test
+    void assertBindQuotedSystemCatalogTableNameStillMatchesDictionary() {
+        String sql = "SELECT relname FROM \"pg_class\"";
+        SelectStatement actual = (SelectStatement) bindSQLStatement("PostgreSQL", sql);
+        ProjectionSegment actualProjection = actual.getProjections().getProjections().get(0);
+        ColumnSegmentBoundInfo actualColumnBoundInfo = ((ColumnProjectionSegment) actualProjection).getColumn().getColumnBoundInfo();
+        assertThat(actualColumnBoundInfo.getOriginalTable().getValue(), is("pg_class"));
+        assertThat(actualColumnBoundInfo.getOriginalSchema().getValue(), is("pg_catalog"));
+    }
+    
+    @Test
+    void assertBindCreateIndexOnExistingSystemCatalogTableStillResolvesToDictionary() {
+        String sql = "CREATE INDEX idx_pg_extension_extname ON pg_extension (extname)";
+        CreateIndexStatement actual = (CreateIndexStatement) bindSQLStatement("PostgreSQL", sql);
+        assertThat(actual.getTable().getTableName().getTableBoundInfo().get().getOriginalSchema().getValue(), is("pg_catalog"));
+    }
+    
+    @Test
+    void assertBindOwnerQualifiedQuotedIdentifierDoesNotMatchDictionaryView() {
+        String sql = "SELECT relname FROM pg_catalog.\"PG_CLASS\"";
+        assertThrows(TableNotFoundException.class, () -> bindSQLStatement("PostgreSQL", sql));
+    }
+    
+    @Test
+    void assertBindOwnerQualifiedQuotedIdentifierMatchesDictionaryViewWithExactCase() {
+        String sql = "SELECT relname FROM pg_catalog.\"pg_class\"";
+        SelectStatement actual = (SelectStatement) bindSQLStatement("PostgreSQL", sql);
+        ProjectionSegment actualProjection = actual.getProjections().getProjections().get(0);
+        ColumnSegmentBoundInfo actualColumnBoundInfo = ((ColumnProjectionSegment) actualProjection).getColumn().getColumnBoundInfo();
+        assertThat(actualColumnBoundInfo.getOriginalTable().getValue(), is("pg_class"));
+        assertThat(actualColumnBoundInfo.getOriginalSchema().getValue(), is("pg_catalog"));
+    }
+    
+    @Test
+    void assertBindRenameTargetNotShadowedBySystemCatalogTableName() {
+        String sql = "ALTER TABLE t_order RENAME TO pg_extension";
+        AlterTableStatement actual = (AlterTableStatement) bindSQLStatement("PostgreSQL", sql);
+        assertThat(actual.getRenameTable().get().getTableName().getTableBoundInfo().get().getOriginalSchema().getValue(), is("public"));
     }
 }
